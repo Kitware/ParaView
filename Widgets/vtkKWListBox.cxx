a694c10aa9d703fca19751f3122811bc91059a7a
Bill Hoffman <bill.hoffman@kitware.com>
2000-02-28 18:16:49 -0500

    ENH: Add a list box widget class

---
+++ Widgets/vtkKWListBox.cxx
@@ -0,0 +1,172 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkKWListBox.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+#include "vtkKWApplication.h"
+#include "vtkKWListBox.h"
+#include "vtkObjectFactory.h"
+
+
+//------------------------------------------------------------------------------
+vtkKWListBox* vtkKWListBox::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkKWListBox");
+  if(ret)
+    {
+    return (vtkKWListBox*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkKWListBox;
+}
+
+
+int vtkKWListBoxCommand(ClientData cd, Tcl_Interp *interp,
+			int argc, char *argv[]);
+
+vtkKWListBox::vtkKWListBox()
+{   
+  this->CurrentSelection = 0;
+  this->Item = 0; 
+  this->CommandFunction = vtkKWListBoxCommand;
+}
+
+vtkKWListBox::~vtkKWListBox()
+{
+  delete [] this->Item;
+  delete [] this->CurrentSelection;
+}
+
+
+int vtkKWListBox::GetNumberOfItems()
+{
+  this->Script("%s.list size", this->GetWidgetName());
+  char* result = this->Application->GetMainInterp()->result;
+  return atoi(result);
+}
+
+void vtkKWListBox::DeleteRange(int start, int end)
+{
+  this->Script("%s.list delete %d %d", this->GetWidgetName(), start, end);
+}
+
+const char* vtkKWListBox::GetItem(int index)
+{
+  this->Script("%s.list get %d", this->GetWidgetName(), index);
+  char* result = this->Application->GetMainInterp()->result;
+  this->Item = strcpy(new char[strlen(result)+1], result);
+  return this->Item;
+}
+
+
+const char *vtkKWListBox::GetSelection()
+{
+  this->Script("%s.list get [%s.list curselection]", this->GetWidgetName(),
+	       this->GetWidgetName());
+  char* result = this->Application->GetMainInterp()->result;
+  this->CurrentSelection = strcpy(new char[strlen(result)+1], result);
+  return this->CurrentSelection;
+}
+
+
+void vtkKWListBox::InsertEntry(int index, const char *name)
+{
+  this->Script("%s.list insert %d {%s}", this->GetWidgetName(), index, name);
+}
+
+
+ 
+void vtkKWListBox::SetDoubleClickCallback(vtkKWObject* obj, 
+					  const char* methodAndArgs)
+{
+  this->Script("bind %s.list <Double-1> {%s %s}", this->GetWidgetName(),
+	       obj->GetTclName(), methodAndArgs);
+}
+
+
+int vtkKWListBox::AppendUnique(const char* name)
+{
+  int size = this->GetNumberOfItems();
+  int found = 0;
+  for(int i =0; i < size; i++)
+    {
+    if(strcmp(this->GetItem(i), name) == 0)
+      {
+      found = 1;
+      break;
+      }
+    }
+  if(!found)
+    {
+    this->InsertEntry(size, name);
+    }
+  return !found;
+}
+
+
+
+void vtkKWListBox::Create(vtkKWApplication *app, char *args)
+{
+  const char *wname;
+
+  // must set the application
+  if (this->Application)
+    {
+    vtkErrorMacro("OptionListBox already created");
+    return;
+    }
+
+  this->SetApplication(app);
+
+  // create the top level
+  wname = this->GetWidgetName();
+  
+  this->Script("frame %s ", wname);
+  this->Script("pack %s", wname);
+  this->Script("scrollbar %s.scroll -command \"%s.list yview\"", 
+	       wname, wname);
+  this->Script("listbox %s.list  -yscroll \"%s.scroll set\" -setgrid 1 %s", 
+	       wname, wname, args);
+  this->Script("pack %s.scroll -side right -fill y", wname);
+  this->Script("pack %s.list -side left -expand 1 -fill both", wname);
+}
+
+
+void vtkKWListBox::SetWidth(int w)
+{
+  this->Script("%s.list configure -width %d", this->GetWidgetName(), w);
+}
+
+void vtkKWListBox::SetHeight(int h)
+{
+  this->Script("%s.list configure -height %d", this->GetWidgetName(), h);
+}
+
+void vtkKWListBox::DeleteAll()
+{
+  int n =  this->GetNumberOfItems();
+  this->DeleteRange(0, n);
+}
---
+++ Widgets/vtkKWListBox.h
@@ -0,0 +1,104 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkKWListBox.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+// .NAME vtkKWListBox
+// .SECTION Description
+// A widget that looks like a button but when pressed provides a list
+// of options that the user can select.
+
+#ifndef __vtkKWListBox_h
+#define __vtkKWListBox_h
+
+#include "vtkKWWidget.h"
+class vtkKWApplication;
+
+class VTK_EXPORT vtkKWListBox : public vtkKWWidget
+{
+public:
+  static vtkKWListBox* New();
+  vtkTypeMacro(vtkKWListBox,vtkKWWidget);
+
+  // Description:
+  // Create a Tk widget
+  virtual void Create(vtkKWApplication *app, char *args);
+
+  // Description:
+  // Get the current selected string in the list
+  const char *GetSelection();
+
+  // Description:
+  // Add entries to an option menu, with or without a command.
+  void InsertEntry(int index, const char *name);
+
+  // Description:
+  // Append a unique string to the list.  If the string exists,
+  // it will not be appended
+  int AppendUnique(const char* name);
+  
+  // Description:
+  // Set callback for double click on a list item.
+  void SetDoubleClickCallback(vtkKWObject* obj, const char* methodAndArgs);
+  
+  // Description:
+  // Get number of items in the list.
+  int GetNumberOfItems();
+  
+  // Description: 
+  // Get the item at the given index.
+  const char* GetItem(int index);
+  
+  // Description:
+  // Delete a range of items in the list.
+  void DeleteRange(int start, int end);
+  
+  // Description:
+  // Delete all items from the list.
+  void DeleteAll();
+  
+  // Description: 
+  // Set the width of the list box.  If the width is less than or equal to 0,
+  // then the width is set to the size of the largest string.
+  void SetWidth(int);
+
+  // Description: 
+  // Set the height of the list box.  If the height is less than or equal to 0,
+  // then the height is set to the size of the number of items in the listbox.
+  void SetHeight(int);
+
+protected:
+  vtkKWListBox();
+  ~vtkKWListBox();
+  vtkKWListBox(const vtkKWListBox&) {};
+  void operator=(const vtkKWListBox&) {};
+  char* CurrentSelection;	// store last call of CurrentSelection
+  char* Item;			// store last call of GetItem
+};
+
+
+#endif
+
+
