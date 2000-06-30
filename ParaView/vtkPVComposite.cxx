ec1062df2353ff9784aed51ad55f450b16e1509b
Amy Squillacote <amy.squillacote@kitware.com>
2000-06-30 10:58:04 -0400

    vtkPVComposite is a subclass of vtkKWComposite, specific to ParaView.

---
+++ ParaView/vtkPVComposite.cxx
@@ -0,0 +1,117 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPVComposite.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+
+#include "vtkPVComposite.h"
+#include "vtkKWLabel.h"
+#include "vtkPVApplication.h"
+#include "vtkKWView.h"
+
+vtkPVComposite::vtkPVComposite()
+{
+  this->Notebook = vtkKWNotebook::New();
+  this->Image = vtkPVImage::New();
+  this->ImageReader = vtkPVImageReader::New();
+  
+  this->PropertiesCreated = 0;
+  this->NotebookCreated = 0;
+}
+
+vtkPVComposite* vtkPVComposite::New()
+{
+  return new vtkPVComposite();
+}
+
+
+vtkPVComposite::~vtkPVComposite()
+{
+  this->Notebook->SetParent(NULL);
+  this->Notebook->Delete();
+  this->Notebook = NULL;
+  
+  this->Image->Delete();
+  this->Image = NULL;
+  
+  this->ImageReader->Delete();
+  this->ImageReader = NULL;
+}
+
+
+void vtkPVComposite::Select(vtkKWView *view)
+{
+}
+
+void vtkPVComposite::Deselect(vtkKWView *view)
+{
+}
+
+vtkProp* vtkPVComposite::GetProp()
+{
+  return this->Image->GetProp();
+}
+
+void vtkPVComposite::CreateProperties(vtkKWApplication *app, char *args)
+{ 
+  this->SetApplication(app);
+  
+  if (this->NotebookCreated)
+    {
+    return;
+    }
+  this->NotebookCreated = 1;
+  
+  this->Notebook->Create(app, args);
+  this->Notebook->AddPage("Image");
+  this->Notebook->AddPage("Image Reader");
+  
+  this->Script("pack %s -pady 2 -padx 2 -fill both -expand yes -anchor n",
+               this->Notebook->GetWidgetName());
+  this->Notebook->Raise("Image");
+  
+  this->Image->SetParent(this->Notebook->GetFrame("Image"));
+  this->Image->Create(app, "");
+  this->Script("pack %s", this->Image->GetWidgetName());
+
+  this->ImageReader->SetParent(this->Notebook->GetFrame("Image Reader"));
+  this->ImageReader->Create(app, "");
+  this->Script("pack %s", this->ImageReader->GetWidgetName());
+}
+
+void vtkPVComposite::SetPropertiesParent(vtkKWWidget *parent)
+{
+  this->Notebook->SetParent(parent);
+}
+
+vtkKWWidget *vtkPVComposite::GetPropertiesParent()
+{
+  return this->Notebook->GetParent();
+}
+
+vtkKWWidget *vtkPVComposite::GetProperties()
+{
+  return this->Notebook;
+}
---
+++ ParaView/vtkPVComposite.h
@@ -0,0 +1,80 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPVComposite.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+// .NAME vtkPVComposite - an element in a view containing props / properties
+// .SECTION Description
+// A composite represents an element in the view. It is very similar to
+// the notion of an actor in a renderer. The composite is different in 
+// that it combines the Actor (vtkProp actually) with some user interface
+// code called the properties, and it may also contain more complex
+// elements such as filters etc.
+
+#ifndef __vtkPVComposite_h
+#define __vtkPVComposite_h
+
+#include "vtkKWComposite.h"
+#include "vtkOutlineSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkKWLabel.h"
+#include "vtkPVImage.h"
+#include "vtkPVImageReader.h"
+
+class VTK_EXPORT vtkPVComposite : public vtkKWComposite
+{
+public:
+  static vtkPVComposite* New();
+  
+  // Description:
+  // Get the Prop for this class.
+  virtual vtkProp *GetProp();
+
+  // Description:
+  // Create the properties object, called by InitializeProperties.
+  virtual void CreateProperties(vtkKWApplication *app, char *args);
+
+  void SetPropertiesParent(vtkKWWidget *parent);
+  vtkKWWidget *GetPropertiesParent();
+  vtkKWWidget *GetProperties();
+  
+  void Select(vtkKWView *);
+  void Deselect(vtkKWView *);
+  
+protected:
+  vtkPVComposite();
+  ~vtkPVComposite();
+  
+  vtkKWNotebook *Notebook;
+  vtkPVImage *Image;
+  vtkPVImageReader *ImageReader;
+  vtkActor *Actor;
+  
+  int PropertiesCreated;
+  int NotebookCreated;
+};
+
+#endif
