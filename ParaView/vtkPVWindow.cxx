90e74c72a7d07eabc2624f5fd04c8a327bd042e9
Kitware <kitware@kitware.com>
2001-05-04 10:23:38 -0400

    source page gets displayed when you go from Hide Properties to Source

--- ParaView/vtkPVWindow.cxx
+++ ParaView/vtkPVWindow.cxx
@@ -1008,6 +1008,8 @@ void vtkPVWindow::ShowWindowProperties()
 //----------------------------------------------------------------------------
 void vtkPVWindow::ShowCurrentSourceProperties()
 {
+  this->ShowProperties();
+  
   // We need to update the properties-menu radio button too!
   this->GetMenuProperties()->CheckRadioButton(
     this->GetMenuProperties(), "Radio", 2);
