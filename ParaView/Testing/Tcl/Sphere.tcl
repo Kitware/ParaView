9a23736d63fab06fced02dc2dfacb8fde4b6ac83
Berk Geveci <berk.geveci@kitware.com>
2001-08-14 11:28:02 -0400

    Regression

--- ParaView/Testing/Tcl/Sphere.tcl
+++ ParaView/Testing/Tcl/Sphere.tcl
@@ -1,3 +1,27 @@
+catch {set PARAVIEW_BASELINE $env(PARAVIEW_BASELINE)}
+
+proc decipadString { str before total } {
+    set x [string first "." $str]
+    if { $x == -1 } { 
+	set str "${str}.0"
+    }
+
+    set x [string first "." $str]
+    while { $x >= 0 && $x < $before } {
+	set str " $str"
+	set x [string first "." $str]
+    }
+
+    if { [string length $str] >= $total } {
+        return [string range $str 0 [expr $total - 1]]
+    }
+
+    while { [string length $str] < $total } {
+        set str "${str}0"
+    }
+    return $str
+}
+
 # hack to get the window.
 set pvWindow [lindex [vtkPVWindow ListInstances] 0]
 
@@ -9,14 +33,35 @@ $pvSourceMenu Invoke [$pvSourceMenu GetIndex "SphereSource"]
 set pvSphere [$pvWindow GetCurrentPVSource]
 $pvSphere AcceptCallback
 
-#vtkWindowToImageFilter winToImage
-#  winToImage SetInput RenWin1
+if {[info exists PARAVIEW_BASELINE]} {
+    vtkWindowToImageFilter winToImage
+    winToImage SetInput RenWin1
+    
+    vtkPNGReader reader
+    reader SetFileName "${PARAVIEW_BASELINE}/Sphere.png"
+    
+    vtkImageDifference imgdiff
+    imgdiff SetInput [winToImage GetOutput]
+    imgdiff SetImage [reader GetOutput]
+    imgdiff Update
+    
+    set threshold 10
+    
+    set imageError [decipadString [imgdiff GetThresholdedError] 4 9]
+    
+    if {[imgdiff GetThresholdedError] > $threshold} {
+	puts "Failed Image Test with error: $imageError"
+	exit 1; 
+    } else {
+	exit
+    }
+	
+} else {
+    puts "A valid image could not be found."
+    exit 1;
+}
 
-#vtkPNGWriter writer
-#  writer SetInput [winToImage GetOutput]
-#  writer SetFileName "sphere.png"
-#  writer Write
 
-exit
+exit 0;
 
 
