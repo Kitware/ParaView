d9080ba82aab2ebaa58c7f980477c35bf0a6e3b5
Charles Law <charles.law@kitware.com>
2001-08-22 10:11:08 -0400

    Added a split frame.

---
+++ Widgets/vtkKWSplitFrame.cxx
@@ -0,0 +1,289 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkKWSplitFrame.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+#include "vtkKWApplication.h"
+#include "vtkKWSplitFrame.h"
+#include "vtkObjectFactory.h"
+
+
+
+//------------------------------------------------------------------------------
+vtkKWSplitFrame* vtkKWSplitFrame::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkKWSplitFrame");
+  if(ret)
+    {
+    return (vtkKWSplitFrame*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkKWSplitFrame;
+}
+
+
+
+
+int vtkKWSplitFrameCommand(ClientData cd, Tcl_Interp *interp,
+		      int argc, char *argv[]);
+
+vtkKWSplitFrame::vtkKWSplitFrame()
+{
+  this->CommandFunction = vtkKWSplitFrameCommand;
+
+  this->Frame1 = vtkKWWidget::New();
+  this->Frame1->SetParent(this);
+  this->Separator = vtkKWWidget::New();
+  this->Separator->SetParent(this);
+  this->Frame2 = vtkKWWidget::New();
+  this->Frame2->SetParent(this);
+
+  this->Frame1Width = 100;
+  this->Frame2Width = 100;
+  this->Frame1MinimumWidth = 50;
+  this->Frame2MinimumWidth = 50;
+
+  this->Width = 106;
+}
+
+vtkKWSplitFrame::~vtkKWSplitFrame()
+{
+  this->Frame1->Delete();
+  this->Frame1 = NULL;
+  this->Separator->Delete();
+  this->Separator = NULL;
+  this->Frame2->Delete();
+  this->Frame2 = NULL;
+}
+
+
+void vtkKWSplitFrame::Create(vtkKWApplication *app)
+{
+  const char *wname;
+  
+  // must set the application
+  if (this->Application)
+    {
+    vtkErrorMacro("SplitFrame already created");
+    return;
+    }
+
+  this->SetApplication(app);
+
+  // create the top level
+  wname = this->GetWidgetName();
+  this->Script("frame %s -borderwidth 0 -relief flat -width 200 -height 100",
+               wname);
+
+  this->Frame1->Create(app,"frame","-borderwidth 0 -relief flat");
+  this->Separator->Create(app,"frame","-borderwidth 2 -relief raised");
+  this->Frame2->Create(app,"frame","-borderwidth 0 -relief flat");
+  
+  this->Update();
+
+  this->Script("bind %s <B1-Motion> {%s DragCallback}",
+               this->Separator->GetWidgetName(), this->GetTclName());
+  this->Script("bind %s <Configure> {%s ConfigureCallback}",
+               this->Separator->GetWidgetName(), this->GetTclName());
+
+
+  // Setup the cursor to indication an action associatyed with the separator. 
+#ifdef _WIN32  
+  this->Script("%s configure -cursor size_we",
+               this->Separator->GetWidgetName());
+#else
+  this->Script("%s configure -cursor sb_h_double_arrow",
+               this->Separator->GetWidgetName());
+#endif
+}
+
+void vtkKWSplitFrame::ConfigureCallback()
+{
+  int tmp;
+  int width;
+
+  this->Script( "winfo width %s", this->GetWidgetName());
+  width = vtkKWObject::GetIntegerResult(this->Application);   
+
+  vtkErrorMacro("configure " << width);
+
+  // First check to see if we have to make the window larger.
+  tmp = this->Frame1MinimumWidth + this->Frame2MinimumWidth + 6;
+  if (width < tmp)
+    {
+    this->Frame1Width = this->Frame1MinimumWidth;
+    this->Frame2Width = this->Frame2MinimumWidth;
+    this->Width = tmp;
+    this->Script("%s configure -width %d", this->GetWidgetName(), this->Width);
+    this->Update();
+    return;
+    }
+
+  // Width of second frame
+  this->Width = width;
+  tmp = width - this->Frame1Width - 6;
+
+  // Do we have to adjust the first frame width?
+  if (tmp < this->Frame2MinimumWidth)
+    {
+    this->Frame2Width = this->Frame2MinimumWidth;
+    this->Frame1Width = width - this->Frame2Width - 6;
+    this->Update();
+    return;
+    }
+
+  this->Frame2Width = tmp;
+  this->Update();
+}
+
+
+
+
+
+void vtkKWSplitFrame::DragCallback()
+{
+  int width, x, xmin, tmp;
+
+  this->Script( "winfo width %s", this->GetWidgetName());
+  width = vtkKWObject::GetIntegerResult(this->Application); 
+
+  vtkErrorMacro("DragCallback width " << width);
+
+  this->Width = width;
+
+  // get the relative position of the render in the window
+  this->Script( "winfo rootx %s",this->GetWidgetName());
+  xmin = vtkKWObject::GetIntegerResult(this->Application);
+  // Get the position of the mouse in the renderer.
+  this->Script( "winfo pointerx %s", this->GetWidgetName());
+  x = vtkKWObject::GetIntegerResult(this->Application);
+  // Relative position.
+  x = x - xmin;
+
+  // Constraints on the window.
+  tmp = this->Frame1MinimumWidth + 3;
+  if (x < tmp)
+    {
+    x = tmp;
+    }
+  tmp = width - this->Frame2MinimumWidth - 3;
+  if (x > tmp)
+    {
+    x = tmp;
+    }
+
+  this->Frame1Width = x-3;
+  this->Frame2Width = width-x-3;
+  this->Update();
+}
+
+
+void vtkKWSplitFrame::SetFrame1MinimumWidth(int minWidth)
+{
+  if (this->Frame1MinimumWidth == minWidth)
+    {
+    return;
+    }
+
+  this->Frame1MinimumWidth = minWidth;
+
+  if (this->Frame1Width < minWidth)
+    {
+    this->Frame1Width = minWidth;
+    // Shrink frame2 if possible.
+    this->Frame2Width = this->Width - this->Frame1Width - 6;
+    // Will the top frame actually grow because of this (configure).
+    if (this->Frame2Width < this->Frame2MinimumWidth)
+      {
+      this->Frame2Width = this->Frame2MinimumWidth;
+      this->Width = this->Frame1Width + this->Frame2Width + 6;
+      this->Script("%s configure -width %d", this->GetWidgetName(), this->Width);
+      }
+    this->Update();
+    }
+}
+
+void vtkKWSplitFrame::SetFrame2MinimumWidth(int minWidth)
+{
+  if (this->Frame2MinimumWidth == minWidth)
+    {
+    return;
+    }
+
+  this->Frame2MinimumWidth = minWidth;
+
+  if (this->Frame2Width < minWidth)
+    {
+    this->Frame2Width = minWidth;
+    // Shrink frame1 if possible.
+    this->Frame1Width = this->Width - this->Frame1Width - 6;
+    // Will the top frame actually grow because of this (configure).
+    if (this->Frame1Width < this->Frame1MinimumWidth)
+      {
+      this->Frame1Width = this->Frame1MinimumWidth;
+      this->Width = this->Frame1Width + this->Frame2Width + 6;
+      this->Script("%s configure -width %d", this->GetWidgetName(), this->Width);
+      }
+    this->Update();
+    }
+}
+
+void vtkKWSplitFrame::SetFrame1Width(int width)
+{
+  if (width < this->Frame1MinimumWidth)
+    {
+    width = this->Frame1MinimumWidth;
+    }
+
+  if (this->Frame1Width == width)
+    {
+    return;
+    }
+
+  this->Frame1Width = width;
+  this->Frame2Width = this->Width - this->Frame1Width - 6;
+  if (this->Frame2Width < this->Frame2MinimumWidth)
+    {
+    this->Frame2Width = this->Frame2MinimumWidth;
+    this->Width = this->Frame1Width + this->Frame2Width + 6;
+    this->Script("%s configure -width %d", this->GetWidgetName(), this->Width);
+    }
+  this->Update();
+}
+
+
+void vtkKWSplitFrame::Update()
+{
+  vtkErrorMacro("Updating with width " << this->Width);
+
+  this->Script("place %s -relx 0 -rely 0 -width %d -relheight 1.0",
+               this->Frame1->GetWidgetName(), this->Frame1Width);
+  this->Script("place %s -x %d -rely 0 -width 6 -relheight 1.0",
+               this->Separator->GetWidgetName(), this->Frame1Width);
+  this->Script("place %s -x %d -rely 0 -width %d -relheight 1.0",
+               this->Frame2->GetWidgetName(), this->Frame1Width+6, 
+               this->Frame2Width);
+}
---
+++ Widgets/vtkKWSplitFrame.h
@@ -0,0 +1,101 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkKWSplitFrame.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+// .NAME vtkKWSplitFrame - A Frame that contains two adjustable sub frames.
+// .SECTION Description
+// The split frame allows the use to select the size of the two frames.
+// It uses a spearator that can be dragged interactively.
+
+
+#ifndef __vtkKWSplitFrame_h
+#define __vtkKWSplitFrame_h
+
+#include "vtkKWWidget.h"
+class vtkKWApplication;
+
+class VTK_EXPORT vtkKWSplitFrame : public vtkKWWidget
+{
+public:
+  static vtkKWSplitFrame* New();
+  vtkTypeMacro(vtkKWSplitFrame,vtkKWWidget);
+
+  // Description:
+  // Create a Tk widget
+  void Create(vtkKWApplication *app);
+  
+  // Description:
+  // Get the vtkKWWidget for the left internal frame.
+  vtkKWWidget *GetFrame1() {return this->Frame1;};
+
+  // Description:
+  // Get the vtkKWWidget for the right internal frame.
+  vtkKWWidget *GetFrame2() {return this->Frame2;};
+
+  // Description:
+  // Set/Get The minimum widths for the two frames.
+  // They default to 50 pixels.
+  vtkGetMacro(Frame1MinimumWidth, int);
+  vtkGetMacro(Frame2MinimumWidth, int);
+  void SetFrame1MinimumWidth(int minWidth);
+  void SetFrame2MinimumWidth(int minWidth);
+
+  // Description:
+  // Set/Get the width of the left frame.  
+  // For now, we cannot direclty set the width of the second frame.
+  vtkGetMacro(Frame1Width, int);
+  vtkGetMacro(Frame2Width, int);
+  void SetFrame1Width(int minWidth);
+
+  // Callbacks used internally to adjust the widths,
+  void DragCallback();
+  void ConfigureCallback();
+
+protected:
+  vtkKWSplitFrame();
+  ~vtkKWSplitFrame();
+  vtkKWSplitFrame(const vtkKWSplitFrame&) {};
+  void operator=(const vtkKWSplitFrame&) {};
+
+  vtkKWWidget *Frame1;
+  vtkKWWidget *Separator;
+  vtkKWWidget *Frame2;
+
+  int Width;
+  int Frame1Width;
+  int Frame2Width;
+
+  int Frame1MinimumWidth;
+  int Frame2MinimumWidth;
+
+  // Reset the actual windows to match our width IVars.
+  void Update();
+};
+
+
+#endif
+
+
