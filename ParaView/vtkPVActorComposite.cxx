394a77162b308c1dd8d9fcca5766f30e620e2b4c
Charles Law <charles.law@kitware.com>
2001-09-18 15:58:19 -0400

    vtkPVActorComposite.cxx

--- ParaView/vtkPVActorComposite.cxx
+++ ParaView/vtkPVActorComposite.cxx
@@ -205,7 +205,7 @@ void vtkPVActorComposite::CreateParallelTclObjects(vtkPVApplication *pvApp)
   pvApp->MakeTclObject("vtkPolyDataMapper", tclName);
   this->LODMapperTclName = NULL;
   this->SetLODMapperTclName(tclName);
-  pvApp->BroadcastScript("%s ImmediateModeRenderingOff", this->LODMapperTclName);
+  pvApp->BroadcastScript("%s ImmediateModeRenderingOn", this->LODMapperTclName);
 
   this->Script("%s SetLookupTable [%s GetLookupTable]",
                this->GetScalarBarTclName(), this->MapperTclName);
