ffaefaab728c3c9431f03f02dd9b5b520537304f
Charles Law <charles.law@kitware.com>
2001-12-18 11:28:01 -0500

    Load this script to increase compositing performance.

---
+++ ParaView/ParaView/FastComposite.tcl
@@ -0,0 +1,5 @@
+# This causes the composite to use char instead of floats for RGB.
+# It will cause the composite delay to be smaller, but may introduce
+# quantization artifacts into the rendering.
+
+Application BroadcastSimpleScript {TreeComp1 UseCharOn}
