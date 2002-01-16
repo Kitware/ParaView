99df84bb32a16382673cfd6515eb5dc5bed48d0c
Ken Martin <ken.martin@kitware.com>
2002-01-16 15:44:21 -0500

    added dll spec file

---
+++ ParaView/Widgets/vtkKWWidgets.h
@@ -0,0 +1,9 @@
+#if defined(WIN32) && !defined(VTKSTATIC)
+ #if defined(vtkKWWidgetsTCL_EXPORTS)
+  #define KW_WIDGETS_EXPORT __declspec( dllexport ) 
+ #else
+  #define KW_WIDGETS_EXPORT __declspec( dllimport ) 
+ #endif
+#else
+  #define KW_WIDGETS_EXPORT
+#endif
