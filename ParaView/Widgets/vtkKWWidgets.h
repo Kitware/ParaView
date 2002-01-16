2c8fe1fba71635441baa1082f95cb2c0962de45c
Ken Martin <ken.martin@kitware.com>
2002-01-16 16:31:44 -0500

    added include gaurds

--- ParaView/Widgets/vtkKWWidgets.h
+++ ParaView/Widgets/vtkKWWidgets.h
@@ -1,3 +1,6 @@
+#ifndef __vtkKWWidgets_h
+#define __vtkKWWidgets_h
+
 #if defined(WIN32) && !defined(VTKSTATIC)
  #if defined(vtkKWWidgetsTCL_EXPORTS)
   #define KW_WIDGETS_EXPORT __declspec( dllexport ) 
@@ -6,4 +9,6 @@
  #endif
 #else
   #define KW_WIDGETS_EXPORT
+#endif
+
 #endif
