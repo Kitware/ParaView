8a078c0ea8bb92a7219b93f91e64f973dd44c7cf
Bill Hoffman <bill.hoffman@kitware.com>
2002-01-18 09:30:18 -0500

    io stuff and cmakelists that work with static volview and shared dll

--- ParaView/Widgets/vtkKWWidgets.h
+++ ParaView/Widgets/vtkKWWidgets.h
@@ -1,7 +1,8 @@
 #ifndef __vtkKWWidgets_h
 #define __vtkKWWidgets_h
+#include "vtkKWWidgetsConfigure.h"
 
-#if defined(WIN32) && !defined(VTKSTATIC)
+#if defined(WIN32) && !defined(KWSTATIC)
  #if defined(vtkKWWidgetsTCL_EXPORTS)
   #define KW_WIDGETS_EXPORT __declspec( dllexport ) 
  #else
