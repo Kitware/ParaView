3d2f8c9fe7ec7f11127a089273a6209ad51ad739
Berk Geveci <berk.geveci@kitware.com>
2002-03-26 15:07:41 -0500

    Cleaned up.

--- ParaView/ParaView/vtkPVSource.cxx
+++ ParaView/ParaView/vtkPVSource.cxx
@@ -403,7 +403,6 @@ void vtkPVSource::CreateProperties()
   // Set up the pages of the notebook.
   this->Notebook->AddPage("Parameters");
   this->Notebook->AddPage("Display");
-  //  this->Notebook->SetMinimumHeight(500);
   this->Properties->SetParent(this->Notebook->GetFrame("Parameters"));
   this->Properties->Create(this->Application,"frame","");
   this->Script("pack %s -pady 2 -fill x -expand yes",
