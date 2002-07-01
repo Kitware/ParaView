eeeafd2fc745a351898c3037a98405a99e92a661
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-07-01 12:29:45 -0400

    Fx default normal

--- ParaView/ParaView/vtkPVPlaneWidget.cxx
+++ ParaView/ParaView/vtkPVPlaneWidget.cxx
@@ -435,7 +435,7 @@ void vtkPVPlaneWidget::ChildCreate(vtkPVApplication* pvApp)
       pvApp->BroadcastScript("%s SetOrigin %f %f %f", planeTclName,
                              0.5*(bds[0]+bds[1]), 0.5*(bds[2]+bds[3]),
 			     0.5*(bds[4]+bds[5]));
-      pvApp->BroadcastScript("%s SetNormal 1 0 0", planeTclName);
+      pvApp->BroadcastScript("%s SetNormal 0 0 1", planeTclName);
       this->Reset();
       }
     }
