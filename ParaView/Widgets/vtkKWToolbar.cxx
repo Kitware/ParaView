8adaa78cbdf499f6cffc703a7ce7e4ddd0f1e781
Sebastien Barre <sebastien.barre@kitware.com>
2002-08-15 15:12:09 -0400

    ENH: tweak the selectcolor so that it looks good with flat buttons, and default Windows aspect to flat (most win32 app are)

--- ParaView/Widgets/vtkKWToolbar.cxx
+++ ParaView/Widgets/vtkKWToolbar.cxx
@@ -54,8 +54,13 @@ template class VTK_EXPORT vtkVectorIterator<vtkKWWidget*>;
 
 #endif
 
+#ifdef _WIN32
+static int vtkKWToolbarGlobalFlatAspect = 1;
+static int vtkKWToolbarGlobalWidgetsFlatAspect = 1;
+#else
 static int vtkKWToolbarGlobalFlatAspect = 0;
 static int vtkKWToolbarGlobalWidgetsFlatAspect = 0;
+#endif
 
 int vtkKWToolbar::GetGlobalFlatAspect() 
 { 
@@ -77,7 +82,7 @@ void vtkKWToolbar::SetGlobalWidgetsFlatAspect(int val)
 
 //------------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWToolbar );
-vtkCxxRevisionMacro(vtkKWToolbar, "1.21");
+vtkCxxRevisionMacro(vtkKWToolbar, "1.22");
 
 
 int vtkKWToolbarCommand(ClientData cd, Tcl_Interp *interp,
@@ -267,6 +272,19 @@ void vtkKWToolbar::UpdateWidgetsAspect()
           int bd = this->GetIntegerResult(this->Application);
           s << widget->GetWidgetName() << " config -bd "
             << (this->WidgetsFlatAspect ? -abs(bd) : abs(bd)) << endl;
+          }
+        }
+      if (widget->HasConfigurationOption("-selectcolor"))
+        {
+        if (this->WidgetsFlatAspect)
+          {
+          this->Script("%s configure -selectcolor [%s cget -bg]", 
+                       widget->GetWidgetName(), widget->GetWidgetName());
+          }
+        else
+          {
+          this->Script("%s configure -selectcolor [lindex [%s configure -selectcolor] 3]", 
+                       widget->GetWidgetName(), widget->GetWidgetName());
           }
         }
       }
