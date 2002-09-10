a2e0aee3c141cf20d2c7189a37681e5b8a5c8da7
Sebastien Barre <sebastien.barre@kitware.com>
2002-09-10 07:31:03 -0400

    FIX: wrong case for include file. Do'h.

--- ParaView/ParaView/vtkPVWindowResources.cxx
+++ ParaView/ParaView/vtkPVWindowResources.cxx
@@ -42,7 +42,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkPVWindow.h"
 
 #include "vtkPVApplication.h"
-#include "vtkKwTkUtilities.h"
+#include "vtkKWTkUtilities.h"
 
 // Logo
 
