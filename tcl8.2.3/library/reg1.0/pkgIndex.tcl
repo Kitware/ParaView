74ee5cfac5fec79905174c02a70ac61a575a233f
Berk Geveci <berk.geveci@kitware.com>
2002-09-10 01:21:38 -0400

    Create ParaView 0.6 branch
    This branch was originally created with only README.html, VTK, and a
    subset of the ParaView directory.  Later files were added to the
    branch, but only after some modification in the main tree since the
    branch point.  This commit was manufactured during conversion from CVS
    to represent the initial branch.

--- tcl8.2.3/library/reg1.0/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[info exists tcl_platform(debug)]} {
-    package ifneeded registry 1.0 \
-	    [list load [file join $dir tclreg82d.dll] registry]
-} else {
-    package ifneeded registry 1.0 \
-	    [list load [file join $dir tclreg82.dll] registry]
-}
