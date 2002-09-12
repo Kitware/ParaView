d35a8b83af8978efe6038d5c387b4df72719bd38
Berk Geveci <berk.geveci@kitware.com>
2002-09-12 09:23:38 -0400

    Should not try to print null

--- ParaView/Widgets/vtkKWWindow.cxx
+++ ParaView/Widgets/vtkKWWindow.cxx
@@ -68,7 +68,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VTK_KW_SHOW_PROPERTIES_LABEL "Show Left Panel"
 #define VTK_KW_EXIT_DIALOG_NAME "ExitApplication"
 
-vtkCxxRevisionMacro(vtkKWWindow, "1.118");
+vtkCxxRevisionMacro(vtkKWWindow, "1.119");
 vtkCxxSetObjectMacro(vtkKWWindow, PropertiesParent, vtkKWWidget);
 
 class vtkKWWindowMenuEntry
@@ -1159,7 +1159,7 @@ void vtkKWWindow::SerializeRevision(ostream& os, vtkIndent indent)
 {
   vtkKWWidget::SerializeRevision(os,indent);
   os << indent << "vtkKWWindow ";
-  this->ExtractRevision(os,"$Revision: 1.118 $");
+  this->ExtractRevision(os,"$Revision: 1.119 $");
 }
 
 int vtkKWWindow::ExitDialog()
@@ -1545,5 +1545,6 @@ void vtkKWWindow::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "StatusFrame: " << this->GetStatusFrame() << endl;
   os << indent << "ViewFrame: " << this->GetViewFrame() << endl;
   os << indent << "WindowClass: " << this->GetWindowClass() << endl;  
-  os << indent << "Title: " << this->GetTitle() << endl;  
+  os << indent << "Title: " 
+     << ( this->GetTitle() ? this->GetTitle() : "(none)" ) << endl;  
 }
