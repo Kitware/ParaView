972d27f99fb8cd1df0655b27bd0cefb2cbe8feab
Berk Geveci <berk.geveci@kitware.com>
2002-10-14 09:39:17 -0400

    What can I say ?

--- ParaView/Widgets/vtkKWCornerAnnotation.cxx
+++ ParaView/Widgets/vtkKWCornerAnnotation.cxx
@@ -57,7 +57,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //-----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWCornerAnnotation );
-vtkCxxRevisionMacro(vtkKWCornerAnnotation, "1.29.2.7");
+vtkCxxRevisionMacro(vtkKWCornerAnnotation, "1.29.2.8");
 
 vtkSetObjectImplementationMacro(vtkKWCornerAnnotation,View,vtkKWView);
 
@@ -163,7 +163,8 @@ void vtkKWCornerAnnotation::Create(vtkKWApplication *app)
     this->CornerTopFrame->GetWidgetName(),
     this->CornerBottomFrame->GetWidgetName() );
 
-  for (int i = 0; i < 4; i++)
+  int i;
+  for (i = 0; i < 4; i++)
     {
     this->CornerFrame[i]->SetParent( 
       (i<2)?(this->CornerBottomFrame):(this->CornerTopFrame) );
@@ -477,7 +478,7 @@ void vtkKWCornerAnnotation::SerializeToken(istream& is,
 void vtkKWCornerAnnotation::SerializeRevision(ostream& os, vtkIndent indent)
 {
   os << indent << "vtkKWCornerAnnotation ";
-  this->ExtractRevision(os,"$Revision: 1.29.2.7 $");
+  this->ExtractRevision(os,"$Revision: 1.29.2.8 $");
   vtkKWLabeledFrame::SerializeRevision(os,indent);
 }
 
