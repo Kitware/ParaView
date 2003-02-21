b4edfe18befbae0f6e6cfd853cf35f0f5c383e2c
Sebastien Barre <sebastien.barre@kitware.com>
2003-02-21 17:32:12 -0500

    ENH: - move common icons/graphics resources from VolView icons/ directory to KWCommon/Resources, where icons.h already was. - ZLIB/BASE64 all icons (divide icons.h size by 10) - add help bubble icon

---
+++ ParaView/KWCommon/Resources/convert.bat
@@ -0,0 +1,4 @@
+SET CONVERTIMAGE=ConvertImage
+SET OPTIONS=ZLIB BASE64 %1
+
+%CONVERTIMAGE% icons.h annotate.png contours.png cut.png error.png expand.png filters.png general.png helpbubble.png layout.png macros.png material.png preferences.png question.png shrink.png smallerror.png smallerrorred.png transfer.png warning.png %OPTIONS%
