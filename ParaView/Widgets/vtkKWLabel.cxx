99964a525f0713c5221f26cfff3a86e1523cc9a4
Sebastien Barre <sebastien.barre@kitware.com>
2003-04-03 15:34:41 -0500

    FIX: this was actually the wrong thing to do :)

--- ParaView/Widgets/vtkKWLabel.cxx
+++ ParaView/Widgets/vtkKWLabel.cxx
@@ -49,7 +49,7 @@ int vtkKWLabelCommand(ClientData cd, Tcl_Interp *interp,
 
 //-----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWLabel );
-vtkCxxRevisionMacro(vtkKWLabel, "1.18");
+vtkCxxRevisionMacro(vtkKWLabel, "1.19");
 
 //-----------------------------------------------------------------------------
 vtkKWLabel::vtkKWLabel()
@@ -110,7 +110,7 @@ void vtkKWLabel::Create(vtkKWApplication *app, const char *args)
     }
   else
     {
-    this->Script("label %s -text {%s} -anchor nw -justify left -width %d %s", 
+    this->Script("label %s -text {%s} -justify left -width %d %s", 
                  wname, this->Label, this->Width, (args?args:""));
     }
 
