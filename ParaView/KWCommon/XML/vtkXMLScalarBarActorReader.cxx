f527bff5daa3776ff2e5dfd02ae7a312033e4c3d
Sebastien Barre <sebastien.barre@kitware.com>
2003-04-15 16:12:26 -0400

    ENH: add vtkScalarBarActor XML reader/writer

---
+++ ParaView/KWCommon/XML/vtkXMLScalarBarActorReader.cxx
@@ -0,0 +1,151 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkXMLScalarBarActorReader.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific 
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkXMLScalarBarActorReader.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkScalarBarActor.h"
+#include "vtkTextProperty.h"
+#include "vtkXMLDataElement.h"
+#include "vtkXMLTextPropertyReader.h"
+
+vtkStandardNewMacro(vtkXMLScalarBarActorReader);
+vtkCxxRevisionMacro(vtkXMLScalarBarActorReader, "1.1");
+
+//----------------------------------------------------------------------------
+char* vtkXMLScalarBarActorReader::GetRootElementName()
+{
+  return "ScalarBarActor";
+}
+
+//----------------------------------------------------------------------------
+int vtkXMLScalarBarActorReader::Parse(vtkXMLDataElement *elem)
+{
+  if (!this->Superclass::Parse(elem))
+    {
+    return 0;
+    }
+
+  vtkScalarBarActor *obj = vtkScalarBarActor::SafeDownCast(this->Object);
+  if (!obj)
+    {
+    vtkWarningMacro(<< "The ScalarBarActor is not set!");
+    return 0;
+    }
+
+  // Get attributes
+
+  int ival;
+  const char *cptr;
+
+  if (elem->GetScalarAttribute("MaximumNumberOfColors", ival))
+    {
+    obj->SetMaximumNumberOfColors(ival);
+    }
+
+  if (elem->GetScalarAttribute("NumberOfLabels", ival))
+    {
+    obj->SetNumberOfLabels(ival);
+    }
+
+  if (elem->GetScalarAttribute("Orientation", ival))
+    {
+    obj->SetOrientation(ival);
+    }
+  
+  cptr = elem->GetAttribute("LabelFormat");
+  if (cptr)
+    {
+    obj->SetLabelFormat(cptr);
+    }
+
+  cptr = elem->GetAttribute("Title");
+  if (cptr)
+    {
+    obj->SetTitle(cptr);
+    }
+  
+  // Get nested elements
+  
+  vtkXMLDataElement *nested_elem, *nested_elem2;
+  
+  // Title text property
+
+  vtkTextProperty *tprop = obj->GetTitleTextProperty();
+  if (tprop)
+    {
+    nested_elem = elem->FindNestedElementWithName("Title");
+    if (nested_elem)
+      {
+      vtkXMLTextPropertyReader *xmlr = vtkXMLTextPropertyReader::New();
+      nested_elem2 = nested_elem->FindNestedElementWithName(
+        xmlr->GetRootElementName());
+      if (nested_elem2)
+        {
+        xmlr->SetObject(tprop);
+        xmlr->Parse(nested_elem2);
+        }
+      xmlr->Delete();
+      }
+    }
+
+  // Label text property
+
+  vtkTextProperty *lprop = obj->GetLabelTextProperty();
+  if (lprop)
+    {
+    nested_elem = elem->FindNestedElementWithName("Label");
+    if (nested_elem)
+      {
+      vtkXMLTextPropertyReader *xmlr = vtkXMLTextPropertyReader::New();
+      nested_elem2 = nested_elem->FindNestedElementWithName(
+        xmlr->GetRootElementName());
+      if (nested_elem2)
+        {
+        xmlr->SetObject(lprop);
+        xmlr->Parse(nested_elem2);
+        }
+      xmlr->Delete();
+      }
+    }
+
+  return 1;
+}
---
+++ ParaView/KWCommon/XML/vtkXMLScalarBarActorWriter.cxx
@@ -0,0 +1,140 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkXMLScalarBarActorWriter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific 
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkXMLScalarBarActorWriter.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkScalarBarActor.h"
+#include "vtkTextProperty.h"
+#include "vtkXMLDataElement.h"
+#include "vtkXMLTextPropertyWriter.h"
+
+vtkStandardNewMacro(vtkXMLScalarBarActorWriter);
+vtkCxxRevisionMacro(vtkXMLScalarBarActorWriter, "1.1");
+
+//----------------------------------------------------------------------------
+char* vtkXMLScalarBarActorWriter::GetRootElementName()
+{
+  return "ScalarBarActor";
+}
+
+//----------------------------------------------------------------------------
+int vtkXMLScalarBarActorWriter::AddAttributes(vtkXMLDataElement *elem)
+{
+  if (!this->Superclass::AddAttributes(elem))
+    {
+    return 0;
+    }
+
+  vtkScalarBarActor *obj = vtkScalarBarActor::SafeDownCast(this->Object);
+  if (!obj)
+    {
+    vtkWarningMacro(<< "The ScalarBarActor is not set!");
+    return 0;
+    }
+
+  elem->SetIntAttribute(
+    "MaximumNumberOfColors", obj->GetMaximumNumberOfColors());
+
+  elem->SetIntAttribute("NumberOfLabels", obj->GetNumberOfLabels());
+
+  elem->SetIntAttribute("Orientation", obj->GetOrientation());
+
+  elem->SetAttribute("LabelFormat", obj->GetLabelFormat());
+
+  elem->SetAttribute("Title", obj->GetTitle());
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkXMLScalarBarActorWriter::AddNestedElements(vtkXMLDataElement *elem)
+{
+  if (!this->Superclass::AddNestedElements(elem))
+    {
+    return 0;
+    }
+
+  vtkScalarBarActor *obj = vtkScalarBarActor::SafeDownCast(this->Object);
+  if (!obj)
+    {
+    vtkWarningMacro(<< "The ScalarBarActor is not set!");
+    return 0;
+    }
+
+  // Title text property
+
+  vtkTextProperty *tprop = obj->GetTitleTextProperty();
+  if (tprop)
+    {
+    vtkXMLDataElement *nested_elem = vtkXMLDataElement::New();
+    elem->AddNestedElement(nested_elem);
+    nested_elem->SetName("Title");
+    nested_elem->Delete();
+    vtkXMLDataElement *nested_elem2 = vtkXMLDataElement::New();
+    nested_elem->AddNestedElement(nested_elem2);
+    nested_elem2->Delete();
+    vtkXMLTextPropertyWriter *xmlw = vtkXMLTextPropertyWriter::New();
+    xmlw->SetObject(tprop);
+    xmlw->Create(nested_elem2);
+    xmlw->Delete();
+    }
+ 
+  // Label text property
+
+  vtkTextProperty *lprop = obj->GetLabelTextProperty();
+  if (lprop)
+    {
+    vtkXMLDataElement *nested_elem = vtkXMLDataElement::New();
+    elem->AddNestedElement(nested_elem);
+    nested_elem->SetName("Label");
+    nested_elem->Delete();
+    vtkXMLDataElement *nested_elem2 = vtkXMLDataElement::New();
+    nested_elem->AddNestedElement(nested_elem2);
+    nested_elem2->Delete();
+    vtkXMLTextPropertyWriter *xmlw = vtkXMLTextPropertyWriter::New();
+    xmlw->SetObject(lprop);
+    xmlw->Create(nested_elem2);
+    xmlw->Delete();
+    }
+ 
+  return 1;
+}
