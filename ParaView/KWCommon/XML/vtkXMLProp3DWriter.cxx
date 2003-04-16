b895faa5e8b4529a0d7c423f60048450a6f637ae
Sebastien Barre <sebastien.barre@kitware.com>
2003-04-16 14:22:18 -0400

    ENH: add vtkProp3D XML reader/writer

---
+++ ParaView/KWCommon/XML/vtkXMLProp3DWriter.cxx
@@ -0,0 +1,81 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkXMLProp3DWriter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific 
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkXMLProp3DWriter.h"
+
+#include "vtkProp3D.h"
+#include "vtkObjectFactory.h"
+#include "vtkXMLDataElement.h"
+
+vtkStandardNewMacro(vtkXMLProp3DWriter);
+vtkCxxRevisionMacro(vtkXMLProp3DWriter, "1.1");
+
+//----------------------------------------------------------------------------
+char* vtkXMLProp3DWriter::GetRootElementName()
+{
+  return "Prop3D";
+}
+
+//----------------------------------------------------------------------------
+int vtkXMLProp3DWriter::AddAttributes(vtkXMLDataElement *elem)
+{
+  if (!this->Superclass::AddAttributes(elem))
+    {
+    return 0;
+    }
+
+  vtkProp3D *obj = vtkProp3D::SafeDownCast(this->Object);
+  if (!obj)
+    {
+    vtkWarningMacro(<< "The Prop3D is not set!");
+    return 0;
+    }
+
+  elem->SetVectorAttribute("Position", 3, obj->GetPosition());
+
+  elem->SetVectorAttribute("Origin", 3, obj->GetOrigin());
+
+  elem->SetVectorAttribute("Scale", 3, obj->GetScale());
+
+  elem->SetVectorAttribute("Orientation", 3, obj->GetOrientation());
+
+  return 1;
+}
