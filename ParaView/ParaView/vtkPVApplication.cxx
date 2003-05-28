4a8365b72604528b7895a58e876d1173cf76ad0a
Brad King <brad.king@kitware.com>
2003-05-28 15:08:41 -0400

    BUG: The main ParaView output window should not be used during program exit.  It prevents warnings during exit from showing up.

--- ParaView/ParaView/vtkPVApplication.cxx
+++ ParaView/ParaView/vtkPVApplication.cxx
@@ -118,7 +118,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVApplication);
-vtkCxxRevisionMacro(vtkPVApplication, "1.193");
+vtkCxxRevisionMacro(vtkPVApplication, "1.194");
 vtkCxxSetObjectMacro(vtkPVApplication, RenderModule, vtkPVRenderModule);
 
 
@@ -1321,6 +1321,16 @@ void vtkPVApplication::Start(int argc, char*argv[])
 //----------------------------------------------------------------------------
 void vtkPVApplication::Exit()
 {  
+  // Avoid recursive exit calls during window destruction.
+  if(this->InExit)
+    {
+    return;
+    }
+  
+  // Remove the ParaView output window so errors during exit will
+  // still be displayed.
+  vtkOutputWindow::SetInstance(0);
+  
   this->vtkKWApplication::Exit();
   if ( this->AboutDialog )
     {
