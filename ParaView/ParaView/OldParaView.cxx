0f5db45869f85eb919923a6c7765658b2a2d1f5b
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-06-19 15:01:18 -0400

    ENH: Change executable name to lowercase

---
+++ ParaView/ParaView/OldParaView.cxx
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  printf(
+    "***************************************\n"
+    "*                                     *\n"
+    "* ParaView executable has changed!    *\n"
+    "* Please use lowercase executable     *\n"
+    "* name.                               *\n"
+    "*                                     *\n"
+    "* Thank you.                          *\n"
+    "*                                     *\n"
+    "*        ParaView Development Team.   *\n"
+    "*                                     *\n"
+    "***************************************\n");
+  return 0;
+}
