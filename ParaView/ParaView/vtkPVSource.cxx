35eba2c9fb1b608ec4f58eded17110a41b06d8e2
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-06-23 16:34:25 -0400

    BUG: Fix deleting sources that are used in animations

--- ParaView/ParaView/vtkPVSource.cxx
+++ ParaView/ParaView/vtkPVSource.cxx
@@ -81,7 +81,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVSource);
-vtkCxxRevisionMacro(vtkPVSource, "1.309");
+vtkCxxRevisionMacro(vtkPVSource, "1.310");
 
 int vtkPVSourceCommand(ClientData cd, Tcl_Interp *interp,
                            int argc, char *argv[]);
@@ -1417,7 +1417,7 @@ void vtkPVSource::DeleteCallback()
   vtkPVSource *current = 0;
   vtkPVWindow *window = this->GetPVWindow();
 
-  window->GetAnimationInterface()->Stop();
+  window->GetAnimationInterface()->DeleteSource(this);
 
   if (this->GetPVOutput())
     {  
