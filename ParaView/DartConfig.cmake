16462ec52907c2a018fb84234ee4c4a0019341bd
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-07-02 10:49:24 -0400

    ENH: add a proper link to bugtracking

--- ParaView/DartConfig.cmake
+++ ParaView/DartConfig.cmake
@@ -25,7 +25,7 @@ SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/ParaView/")
 SET (CVS_WEB_CVSROOT "ParaView")
 SET (USE_DOXYGEN "On")
 SET (DOXYGEN_URL "${PROJECT_URL}/doc" )
-SET (GNATS_WEB_URL "http://public.kitware.com/Bug/bug.php")
+SET (GNATS_WEB_URL "http://public.kitware.com/Bug/index.php")
 SET (USE_GNATS "On")
 
 # copy over the testing logo
