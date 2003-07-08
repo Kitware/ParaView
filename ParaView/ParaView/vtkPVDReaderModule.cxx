a688c09796f08433ce90c128f7510a521797e745
Brad King <brad.king@kitware.com>
2003-07-08 17:23:03 -0400

    ENH: Added vtkPVDReaderModule to support reading all kinds of ParaView Data files (pvd).  The first special support is for time steps in an animation.

---
+++ ParaView/ParaView/vtkPVDReaderModule.cxx
@@ -0,0 +1,152 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVDReaderModule.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific 
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkPVDReaderModule.h"
+
+#include "vtkKWFrame.h"
+#include "vtkObjectFactory.h"
+#include "vtkPVApplication.h"
+#include "vtkPVProcessModule.h"
+#include "vtkPVScale.h"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkPVDReaderModule);
+vtkCxxRevisionMacro(vtkPVDReaderModule, "1.1.2.1");
+
+//----------------------------------------------------------------------------
+vtkPVDReaderModule::vtkPVDReaderModule()
+{
+  this->HaveTime = 0;
+  this->TimeScale = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkPVDReaderModule::~vtkPVDReaderModule()
+{
+  if(this->TimeScale)
+    {
+    this->TimeScale->Delete();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkPVDReaderModule::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+
+//----------------------------------------------------------------------------
+int vtkPVDReaderModule::Finalize(const char* fname)
+{
+  // If we have time, we need to behave as an advanced reader module.
+  if(this->HaveTime)
+    {
+    return this->Superclass::Finalize(fname);
+    }
+  else
+    {
+    return this->vtkPVReaderModule::Finalize(fname);
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkPVDReaderModule::ReadFileInformation(const char* fname)
+{
+  // Make sure the reader's file name is set.
+  this->SetReaderFileName(fname);
+  
+  // Check whether the input file has a "timestep" attribute.
+  vtkPVProcessModule* pm = this->GetPVApplication()->GetProcessModule();
+  pm->ServerScript("%s UpdateAttributes", this->GetVTKSourceTclName());
+  pm->RootScript(
+    "namespace eval ::paraview::vtkPVDReaderModule {\n"
+    "  proc GetTimeAttributeIndex {reader} {\n"
+    "    set n [$reader GetNumberOfAttributes]\n"
+    "    for {set i 0} {$i < $n} {incr i} {\n"
+    "      if {[$reader GetAttributeName $i] == {timestep}} {\n"
+    "        return $i\n"
+    "      }\n"
+    "    }\n"
+    "    return -1\n"
+    "  }\n"
+    "  GetTimeAttributeIndex {%s}\n"
+    "}\n", this->GetVTKSourceTclName());
+  int index = atoi(pm->GetRootResult());
+  this->HaveTime = (index >= 0)?1:0;
+
+  // If we have time, we need to behave as an advanced reader module.
+  if(this->HaveTime)
+    {
+    pm->ServerScript("%s SetRestrictionAsIndex timestep 0",
+                     this->GetVTKSourceTclName());
+    pm->RootScript("%s GetNumberOfAttributeValues %d",
+                   this->GetVTKSourceTclName(), index);
+    int max = atoi(pm->GetRootResult())-1;
+    this->TimeScale = vtkPVScale::New();
+    this->TimeScale->SetLabel("Timestep");
+    this->TimeScale->SetPVSource(this);
+    this->TimeScale->SetRange(0, max);
+    this->TimeScale->SetParent(this->GetParameterFrame()->GetFrame());
+    this->TimeScale->SetModifiedCommand(this->GetTclName(), 
+                                        "SetAcceptButtonColorToRed");
+    this->TimeScale->SetObjectVariable(this->GetVTKSourceTclName(),
+                                       "RestrictionAsIndex timestep");
+    this->TimeScale->Create(this->GetPVApplication());
+    this->TimeScale->DisplayEntry();
+    this->AddPVWidget(this->TimeScale);
+    if(this->TimeScale->GetParent()->GetNumberOfPackedChildren() > 0)
+      {
+      this->Script("pack %s -pady 10 -side top -fill x -expand t -before [lindex [pack slaves %s] 0]", 
+                   this->TimeScale->GetWidgetName(), 
+                   this->TimeScale->GetParent()->GetWidgetName());
+      }
+    else
+      {
+      this->Script("pack %s -side top -fill x -expand t", 
+                   this->TimeScale->GetWidgetName());
+      }
+    return this->Superclass::ReadFileInformation(fname);
+    }
+  else
+    {
+    return this->vtkPVReaderModule::ReadFileInformation(fname);
+    }
+}
