0c95ca7f7a2edf3e8e412424773b34faf1c39c4c
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-07-15 08:08:01 -0400

    ENH: Link directly to ParaView bugs and fix //

--- ParaView/DartConfig.cmake
+++ ParaView/DartConfig.cmake
@@ -17,7 +17,7 @@ SET (TRIGGER_SITE
        "http://${DROP_SITE}/cgi-bin/Submit-ParaView-TestingResults.pl")
 
 # Project Home Page
-SET (PROJECT_URL "http://www.paraview.org/")
+SET (PROJECT_URL "http://www.paraview.org")
 
 # Dart server configuration 
 SET (ROLLUP_URL "http://${DROP_SITE}/cgi-bin/paraview-rollup-dashboard.sh")
@@ -25,7 +25,7 @@ SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/ParaView/")
 SET (CVS_WEB_CVSROOT "ParaView")
 SET (USE_DOXYGEN "On")
 SET (DOXYGEN_URL "${PROJECT_URL}/doc" )
-SET (GNATS_WEB_URL "http://public.kitware.com/Bug/index.php")
+SET (GNATS_WEB_URL "${PROJECT_URL}/Bug/query.php?projects=3&status%5B%5D=1&status%5B%5D=2&status%5B%5D=3&status%5B%5D=4&status%5B%5D=6&op=doquery")
 SET (USE_GNATS "On")
 
 # copy over the testing logo
