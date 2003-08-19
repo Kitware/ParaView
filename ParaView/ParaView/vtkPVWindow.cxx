f8e73ef3d6dd66cfcd20231338287da99e2e0c76
Amy Squillacote <amy.squillacote@kitware.com>
2003-08-19 15:17:47 -0400

    removing lines I commented out

--- ParaView/ParaView/vtkPVWindow.cxx
+++ ParaView/ParaView/vtkPVWindow.cxx
@@ -141,7 +141,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //-----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVWindow);
-vtkCxxRevisionMacro(vtkPVWindow, "1.475.2.1");
+vtkCxxRevisionMacro(vtkPVWindow, "1.475.2.2");
 
 int vtkPVWindowCommand(ClientData cd, Tcl_Interp *interp,
                              int argc, char *argv[]);
@@ -3186,9 +3186,6 @@ void vtkPVWindow::UpdateSelectMenu()
               source->GetTclName());
       this->GlyphMenu->AddCommand(source->GetName(), this, methodAndArg,
                                   source->GetSourceClassName());
-//                                  source->GetVTKSource() ?
-//                                  source->GetVTKSource()->GetClassName()+3
-//                                  : 0);
       numGlyphs++;
       it->GoToNextItem();
       }
@@ -3209,9 +3206,6 @@ void vtkPVWindow::UpdateSelectMenu()
               source->GetTclName());
       this->SelectMenu->AddCommand(source->GetName(), this, methodAndArg,
                                    source->GetSourceClassName());
-//                                   source->GetVTKSource() ?
-//                                   source->GetVTKSource()->GetClassName()+3
-//                                   : 0);
       numSources++;
       it->GoToNextItem();
       }
