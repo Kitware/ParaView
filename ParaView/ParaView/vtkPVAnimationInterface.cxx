f31bf422b3c3c6988dbc0c5d4e9aee0e0c7afd26
Amy Squillacote <amy.squillacote@kitware.com>
2003-10-03 15:48:22 -0400

    Cache Geometry works on this branch now.

--- ParaView/ParaView/vtkPVAnimationInterface.cxx
+++ ParaView/ParaView/vtkPVAnimationInterface.cxx
@@ -180,7 +180,7 @@ public:
 
 //-----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVAnimationInterface);
-vtkCxxRevisionMacro(vtkPVAnimationInterface, "1.86.2.1");
+vtkCxxRevisionMacro(vtkPVAnimationInterface, "1.86.2.2");
 
 vtkCxxSetObjectMacro(vtkPVAnimationInterface,ControlledWidget, vtkPVWidget);
 
@@ -924,28 +924,42 @@ void vtkPVAnimationInterface::SetCurrentTime(int time)
       this->ControlledWidget->Reset();
       }
     
+    vtkCollectionIterator* it = this->AnimationEntriesIterator;
+    for ( it->InitTraversal(); !it->IsDoneWithTraversal(); it->GoToNextItem() )
+      {
+      vtkPVAnimationInterfaceEntry* entry
+        = vtkPVAnimationInterfaceEntry::SafeDownCast(it->GetObject());
+      vtkPVWidgetProperty *prop = entry->GetCurrentProperty();
+      if (prop)
+        {
+        this->Script(entry->GetTimeEquation());
+        prop->SetAnimationTime(vtkKWObject::GetFloatResult(pvApp));
+        prop->GetWidget()->ModifiedCallback();
+        }
+      if ( entry->GetPVSource() )
+        {
+        entry->GetPVSource()->UpdateVTKSourceParameters();
+        }
+      }
+    
     // Generate the cache, or use previous cache.
     if (this->GetCacheGeometry())
       {
       this->Window->CacheUpdate(time, this->NumberOfFrames);
+      if (this->View)
+        {
+        this->View->EventuallyRender();
+        }
       }
     else
       {
-      vtkCollectionIterator* it = this->AnimationEntriesIterator;
       for ( it->InitTraversal(); !it->IsDoneWithTraversal(); it->GoToNextItem() )
         {
         vtkPVAnimationInterfaceEntry* entry
           = vtkPVAnimationInterfaceEntry::SafeDownCast(it->GetObject());
-        vtkPVWidgetProperty *prop = entry->GetCurrentProperty();
-        if (prop)
-          {
-          this->Script(entry->GetTimeEquation());
-          prop->SetAnimationTime(vtkKWObject::GetFloatResult(pvApp));
-          prop->GetWidget()->ModifiedCallback();
-          }
         if ( entry->GetPVSource() )
           {
-          entry->GetPVSource()->AcceptCallback();
+          entry->GetPVSource()->Accept();
           }
         }
       }
