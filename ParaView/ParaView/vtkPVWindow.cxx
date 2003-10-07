97a502f649ffd421ae83c945bead7fccc0e32715
Berk Geveci <berk.geveci@kitware.com>
2003-10-07 11:27:52 -0400

    BUG: When opening a file with a custom reader, the last path was not saved

--- ParaView/ParaView/vtkPVWindow.cxx
+++ ParaView/ParaView/vtkPVWindow.cxx
@@ -143,7 +143,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //-----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVWindow);
-vtkCxxRevisionMacro(vtkPVWindow, "1.486");
+vtkCxxRevisionMacro(vtkPVWindow, "1.487");
 
 int vtkPVWindowCommand(ClientData cd, Tcl_Interp *interp,
                              int argc, char *argv[]);
@@ -1826,6 +1826,7 @@ int vtkPVWindow::Open(char *openFileName, int store)
   error << "Could not find an appropriate reader for file "
         << openFileName << ". Would you like to manually select "
         << "the reader for this file?" << ends;
+  error.rdbuf()->freeze(0);     
   if (this->UseMessageDialog)
     {
     if ( vtkKWMessageDialog::PopupOkCancel(this->Application, this,
@@ -1858,6 +1859,8 @@ int vtkPVWindow::Open(char *openFileName, int store)
         str << "OpenCustom \"" << reader->GetModuleName() << "\"" <<ends;
         this->AddRecentFile(openFileName, this, str.str());
         str.rdbuf()->freeze(0);
+        dialog->Delete();
+        return VTK_OK;
         }
       // Cleanup
       dialog->Delete();
@@ -1867,7 +1870,6 @@ int vtkPVWindow::Open(char *openFileName, int store)
     {
     vtkErrorMacro(<<error.str());
     }
-  error.rdbuf()->freeze(0);     
 
   return VTK_ERROR;
 }
