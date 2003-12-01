c1e42ceb622a2a32e0f0ff1cff3863e2a310ad17
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-12-01 09:16:45 -0500

    ENH: Initial import

---
+++ ParaView/Widgets/vtkKWListSelectOrder.h
@@ -0,0 +1,101 @@
+/*=========================================================================
+
+Copyright (c) 1998-2003 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkKWListSelectOrder - a single line text entry widget
+// .SECTION Description
+// A simple widget used for collecting keyboard input from the user. This
+// widget provides support for single line input.
+
+#ifndef __vtkKWListSelectOrder_h
+#define __vtkKWListSelectOrder_h
+
+#include "vtkKWWidget.h"
+
+class vtkKWApplication;
+class vtkKWListBox;
+class vtkKWPushButton;
+
+class VTK_EXPORT vtkKWListSelectOrder : public vtkKWWidget
+{
+public:
+  static vtkKWListSelectOrder* New();
+  vtkTypeRevisionMacro(vtkKWListSelectOrder,vtkKWWidget);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Create a Tk widget
+  virtual void Create(vtkKWApplication *app, const char *args);
+
+  // Description:
+  // Add a string element to the source list.
+  virtual void AddElement(const char*);
+
+  // Description:
+  // Callbacks.
+  virtual void AddCallback();
+  virtual void AddAllCallback();
+  virtual void RemoveCallback();
+  virtual void RemoveAllCallback();
+  virtual void UpCallback();
+  virtual void DownCallback();
+
+protected:
+  vtkKWListSelectOrder();
+  ~vtkKWListSelectOrder();
+
+  vtkKWListBox* SourceList;
+  vtkKWListBox* FinalList;
+
+  vtkKWPushButton* AddButton;
+  vtkKWPushButton* AddAllButton;
+  vtkKWPushButton* RemoveButton;
+  vtkKWPushButton* RemoveAllButton;
+  vtkKWPushButton* UpButton;
+  vtkKWPushButton* DownButton;
+
+  void MoveWholeList(vtkKWListBox* l1, vtkKWListBox* l2);
+  void MoveSelectedList(vtkKWListBox* l1, vtkKWListBox* l2);
+  void MoveList(vtkKWListBox* l1, vtkKWListBox* l2, const char* list);
+  
+private:
+  vtkKWListSelectOrder(const vtkKWListSelectOrder&); // Not implemented
+  void operator=(const vtkKWListSelectOrder&); // Not Implemented
+};
+
+
+#endif
+
+
+
