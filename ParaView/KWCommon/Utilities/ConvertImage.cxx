e5b6790c2ac4fbc7dfa415673a22a2bf5c4c0448
Brad King <brad.king@kitware.com>
2003-12-15 16:04:49 -0500

    ENH: Unified KWCommon copyright.

--- ParaView/KWCommon/Utilities/ConvertImage.cxx
+++ ParaView/KWCommon/Utilities/ConvertImage.cxx
@@ -1,3 +1,16 @@
+/*=========================================================================
+
+  Module:    ConvertImage.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkBase64Utilities.h"
 #include "vtkImageData.h"
 #include "vtkImageFlip.h"
