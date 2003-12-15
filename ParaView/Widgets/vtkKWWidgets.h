6b493b5a66ca530f5471bd3159a4c457f1a04544
Brad King <brad.king@kitware.com>
2003-12-15 16:20:10 -0500

    ENH: Unified Widgets copyright.

--- ParaView/Widgets/vtkKWWidgets.h
+++ ParaView/Widgets/vtkKWWidgets.h
@@ -1,15 +1,28 @@
+/*=========================================================================
+
+  Module:    vtkKWWidgets.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #ifndef __vtkKWWidgets_h
 #define __vtkKWWidgets_h
 #include "vtkKWWidgetsConfigure.h"
 
 #if defined(WIN32) && !defined(KWSTATIC)
- #if defined(vtkKWWidgetsTCL_EXPORTS)
-  #define KW_WIDGETS_EXPORT __declspec( dllexport ) 
- #else
-  #define KW_WIDGETS_EXPORT __declspec( dllimport ) 
- #endif
+# if defined(vtkKWWidgetsTCL_EXPORTS)
+#  define KW_WIDGETS_EXPORT __declspec(dllexport)
+# else
+#  define KW_WIDGETS_EXPORT __declspec(dllimport)
+# endif
 #else
-  #define KW_WIDGETS_EXPORT
+# define KW_WIDGETS_EXPORT
 #endif
 
 #endif
