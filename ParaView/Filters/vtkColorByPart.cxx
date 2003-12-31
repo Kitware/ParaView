3d77f0fd966a0acedbb6a0b96eb12dd82a1f2f9f
Berk Geveci <berk.geveci@kitware.com>
2003-12-31 12:36:11 -0500

    Fixed SetGet bug

--- ParaView/Filters/vtkColorByPart.cxx
+++ ParaView/Filters/vtkColorByPart.cxx
@@ -21,7 +21,7 @@
 #include "vtkFieldData.h"
 #include "vtkIntArray.h"
 
-vtkCxxRevisionMacro(vtkColorByPart, "1.4");
+vtkCxxRevisionMacro(vtkColorByPart, "1.5");
 vtkStandardNewMacro(vtkColorByPart);
 
 
@@ -51,6 +51,10 @@ vtkDataSet* vtkColorByPart::GetOutput(int idxOut)
   vtkDataObject* output;
 
   input = this->GetInput(idxOut);
+  if (!input)
+    {
+    return NULL;
+    }
   output = this->Superclass::GetOutput(idxOut);
   if (output == NULL)
     { // Create a new output.
