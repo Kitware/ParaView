78ca49beafa0da5f90ef7de39ab8e5da243eb6b6
Charles Law <charles.law@kitware.com>
2004-01-23 14:29:39 -0500

    IceT is working with duplication!

--- ParaView/ParaView/vtkIceTRenderManager.cxx
+++ ParaView/ParaView/vtkIceTRenderManager.cxx
@@ -45,7 +45,7 @@ const int ICET_INFO_SIZE = sizeof(struct IceTInformation)/sizeof(int);
 // vtkIceTRenderManager implementation.
 //******************************************************************
 
-vtkCxxRevisionMacro(vtkIceTRenderManager, "1.8");
+vtkCxxRevisionMacro(vtkIceTRenderManager, "1.9");
 vtkStandardNewMacro(vtkIceTRenderManager);
 
 vtkCxxSetObjectMacro(vtkIceTRenderManager, SortingKdTree, vtkPKdTree);
@@ -547,8 +547,6 @@ void vtkIceTRenderManager::PreRenderProcessing()
     int tileIdx = this->Controller->GetLocalProcessId();
     y = tileIdx/this->NumTilesX;
     x = tileIdx - y*this->NumTilesX;
-    // Flip the y axis to match IceT
-    y = this->NumTilesY-1-y;
     // Setup the camera for this tile.
     cam->SetWindowCenter(1.0-(double)(this->NumTilesX) + 2.0*(double)x,
                          1.0-(double)(this->NumTilesY) + 2.0*(double)y);
@@ -611,6 +609,25 @@ void vtkIceTRenderManager::PostRenderProcessing()
 {
   vtkDebugMacro("PostRenderProcessing");
 
+  // Try to put the camera back the way it was.
+  if (!this->UseCompositing)
+    {
+    vtkCamera* cam;
+    vtkRenderWindow* renWin = this->RenderWindow;
+    vtkRendererCollection *rens;
+    vtkRenderer* ren;
+
+    rens = renWin->GetRenderers();
+    rens->InitTraversal();
+    ren = rens->GetNextItem();
+    if (ren)
+      {
+      cam = ren->GetActiveCamera();
+      cam->SetWindowCenter(0.0, 0.0);
+      return;
+      }
+    }
+  
   this->Controller->Barrier();
 
   if (this->WriteBackImages)
