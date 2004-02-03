190ef2f4392d8163e660f669fc8503b1f9088005
Lee Ann Fisk <lafisk@sandia.gov>
2004-02-03 17:22:39 -0500

    sorry - I accidently checked in a copy that turns timing on by default

--- ParaView/Filters/vtkDistributedDataFilter.cxx
+++ ParaView/Filters/vtkDistributedDataFilter.cxx
@@ -92,7 +92,7 @@ static char * makeEntry(const char *s)
 
 // Timing data ---------------------------------------------
 
-vtkCxxRevisionMacro(vtkDistributedDataFilter, "1.21");
+vtkCxxRevisionMacro(vtkDistributedDataFilter, "1.22");
 
 vtkStandardNewMacro(vtkDistributedDataFilter);
 
@@ -119,7 +119,7 @@ vtkDistributedDataFilter::vtkDistributedDataFilter()
   this->IncludeAllIntersectingCells = 0;
   this->ClipCells = 0;
 
-  this->Timing = 1;
+  this->Timing = 0;
   this->TimerLog = NULL;
 }
 
