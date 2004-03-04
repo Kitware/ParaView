4e9bd3f24c68329d9dbaf10ce4eb0a2b71f77dc4
Charles Law <charles.law@kitware.com>
2004-03-04 10:11:36 -0500

    Fixed warnings when not using MPI

--- ParaView/Filters/vtkMPIDuplicatePolyData.cxx
+++ ParaView/Filters/vtkMPIDuplicatePolyData.cxx
@@ -29,7 +29,7 @@
 #include "vtkMPICommunicator.h"
 #endif
 
-vtkCxxRevisionMacro(vtkMPIDuplicatePolyData, "1.8");
+vtkCxxRevisionMacro(vtkMPIDuplicatePolyData, "1.9");
 vtkStandardNewMacro(vtkMPIDuplicatePolyData);
 
 vtkCxxSetObjectMacro(vtkMPIDuplicatePolyData,Controller, vtkMultiProcessController);
@@ -168,9 +168,6 @@ void vtkMPIDuplicatePolyData::ServerExecute(vtkPolyDataReader* reader,
   numProcs = this->Controller->GetNumberOfProcesses();
   vtkPolyData* input;
   vtkPolyData* pd;
-  int idx;
-  int sum;
-  int myId = this->Controller->GetLocalProcessId();
   
   // First marshal our input.
   input = this->GetInput();
@@ -188,6 +185,9 @@ void vtkMPIDuplicatePolyData::ServerExecute(vtkPolyDataReader* reader,
   
 #ifdef VTK_USE_MPI
 
+  int idx;
+  int sum;
+  int myId = this->Controller->GetLocalProcessId();
   vtkMPICommunicator *com = NULL;
   if (this->Controller)
     {
