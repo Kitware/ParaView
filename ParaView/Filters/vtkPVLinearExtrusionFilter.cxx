8fb54f5d4ee15020de2ca9f72b163c8823490ce8
Amy Squillacote <amy.squillacote@kitware.com>
2004-03-08 12:48:22 -0500

    change the defaults of the linear extrusion filter

---
+++ ParaView/Filters/vtkPVLinearExtrusionFilter.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVLinearExtrusionFilter.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVLinearExtrusionFilter.h"
+
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro(vtkPVLinearExtrusionFilter, "1.1");
+vtkStandardNewMacro(vtkPVLinearExtrusionFilter);
+
+vtkPVLinearExtrusionFilter::vtkPVLinearExtrusionFilter()
+{
+  this->ExtrusionType = VTK_VECTOR_EXTRUSION;
+}
+
+void vtkPVLinearExtrusionFilter::PrintSelf(ostream &os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ ParaView/Filters/vtkPVLinearExtrusionFilter.h
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVLinearExtrusionFilter.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPVLinearExtrusionFilter - change a default value
+// .SECTION Description
+// vtkPVLinearExtrusionFilter is a subclass of vtkPLinearExtrusionFilter.
+// The only difference is changing the default extrusion type to vector
+// extrusion
+
+#ifndef __vtkPVLinearExtrusionFilter_h
+#define __vtkPVLinearExtrusionFilter_h
+
+#include "vtkPLinearExtrusionFilter.h"
+
+class VTK_EXPORT vtkPVLinearExtrusionFilter : public vtkPLinearExtrusionFilter
+{
+public:
+  static vtkPVLinearExtrusionFilter *New();
+  vtkTypeRevisionMacro(vtkPVLinearExtrusionFilter, vtkPLinearExtrusionFilter);
+  void PrintSelf(ostream &os, vtkIndent indent);
+  
+protected:
+  vtkPVLinearExtrusionFilter();
+  ~vtkPVLinearExtrusionFilter() {};
+  
+private:
+  vtkPVLinearExtrusionFilter(const vtkPVLinearExtrusionFilter&); // Not implemented.
+  void operator=(const vtkPVLinearExtrusionFilter); // Not implemented.
+};
+
+#endif
