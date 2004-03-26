352bce26fdf48b5ec48f744a24da30ba97fef745
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-03-26 17:58:20 -0500

    ENH: Need for ParaView testing

---
+++ GUI/Widgets/vtkKWTesting.cxx
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+Copyright (c) 1998-2003 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkKWTesting.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkKWView.h"
+#include "vtkTesting.h"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro( vtkKWTesting );
+vtkCxxRevisionMacro(vtkKWTesting, "1.1");
+vtkCxxSetObjectMacro(vtkKWTesting,RenderView,vtkKWView);
+
+//----------------------------------------------------------------------------
+vtkKWTesting::vtkKWTesting()
+{
+  this->Testing = vtkTesting::New();
+  this->RenderView = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkKWTesting::~vtkKWTesting()
+{
+  this->SetRenderView(0);
+  this->Testing->Delete();
+  this->Testing = 0;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTesting::AddArgument(const char* arg)
+{
+  this->Testing->AddArgument(arg);
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTesting::RegressionTest(float thresh)
+{
+  this->Testing->SetRenderWindow(this->RenderView->GetRenderWindow());
+  return this->Testing->RegressionTest(thresh) != vtkTesting::PASSED;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTesting::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+  os << indent << "RenderView: " << this->RenderView << endl;
+}
+
+
