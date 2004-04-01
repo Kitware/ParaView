069466f1ca72c2d138fd9fb7fd783112102c1260
Charles Law <charles.law@kitware.com>
2004-04-01 14:25:53 -0500

    Support for picking.

---
+++ Servers/Filters/vtkMPIDuplicateUnstructuredGrid.cxx
@@ -0,0 +1,446 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMPIDuplicateUnstructuredGrid.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkMPIDuplicateUnstructuredGrid.h"
+
+#include "vtkAppendFilter.h"
+#include "vtkCellData.h"
+#include "vtkMultiProcessController.h"
+#include "vtkUnstructuredGridWriter.h"
+#include "vtkUnstructuredGridReader.h"
+#include "vtkCharArray.h"
+#include "vtkObjectFactory.h"
+#include "vtkPointData.h"
+#include "vtkUnstructuredGrid.h"
+#include "vtkSocketController.h"
+#include "vtkTimerLog.h"
+#include "vtkToolkits.h"
+#ifdef VTK_USE_MPI
+#include "vtkMPICommunicator.h"
+#endif
+
+vtkCxxRevisionMacro(vtkMPIDuplicateUnstructuredGrid, "1.1");
+vtkStandardNewMacro(vtkMPIDuplicateUnstructuredGrid);
+
+vtkCxxSetObjectMacro(vtkMPIDuplicateUnstructuredGrid,Controller, vtkMultiProcessController);
+vtkCxxSetObjectMacro(vtkMPIDuplicateUnstructuredGrid,SocketController, vtkSocketController);
+vtkCxxSetObjectMacro(vtkMPIDuplicateUnstructuredGrid,RenderServerSocketController, vtkSocketController);
+
+//-----------------------------------------------------------------------------
+vtkMPIDuplicateUnstructuredGrid::vtkMPIDuplicateUnstructuredGrid()
+{
+  // Controller keeps a reference to this object as well.
+  this->Controller = NULL;
+  this->SetController(vtkMultiProcessController::GetGlobalController());  
+
+  this->SocketController = NULL;
+  this->ClientFlag = 0;
+
+  this->RenderServerSocketController = NULL;
+  this->RenderServerFlag = 0;
+
+  this->PassThrough = 0;
+  this->ZeroEmpty = 0;
+  //this->MemorySize = 0;
+  
+  // Client has no inputs.
+  this->NumberOfRequiredInputs = 0;
+}
+
+//-----------------------------------------------------------------------------
+vtkMPIDuplicateUnstructuredGrid::~vtkMPIDuplicateUnstructuredGrid()
+{
+  this->SetController(0);
+  this->SetSocketController(0);
+  this->SetRenderServerSocketController(0);
+}
+
+
+#define vtkDPDPow2(j) (1 << (j))
+static inline int vtkDPDLog2(int j, int& exact)
+{
+  int counter=0;
+  exact = 1;
+  while(j)
+    {
+    if ( ( j & 1 ) && (j >> 1) )
+      {
+      exact = 0;
+      }
+    j = j >> 1;
+    counter++;
+    }
+  return counter-1;
+}
+
+
+//-----------------------------------------------------------------------------
+void vtkMPIDuplicateUnstructuredGrid::ExecuteInformation()
+{
+  if (this->GetOutput() == NULL)
+    {
+    vtkErrorMacro("Missing output");
+    return;
+    }
+  this->GetOutput()->SetMaximumNumberOfPieces(-1);
+}
+
+//-----------------------------------------------------------------------------
+void vtkMPIDuplicateUnstructuredGrid::ComputeInputUpdateExtents(vtkDataObject *output)
+{
+  vtkUnstructuredGrid *input = this->GetInput();
+  int piece = output->GetUpdatePiece();
+  int numPieces = output->GetUpdateNumberOfPieces();
+  int ghostLevel = output->GetUpdateGhostLevel();
+
+  if (input == NULL)
+    {
+    return;
+    }
+  input->SetUpdatePiece(piece);
+  input->SetUpdateNumberOfPieces(numPieces);
+  input->SetUpdateGhostLevel(ghostLevel);
+}
+
+
+  
+
+//-----------------------------------------------------------------------------
+void vtkMPIDuplicateUnstructuredGrid::Execute()
+{
+  vtkUnstructuredGrid *input = this->GetInput();
+  
+  vtkTimerLog::MarkStartEvent("MPIGather");
+  
+  if (this->PassThrough)
+    {
+    if (input == NULL)
+      {
+      return;
+      }
+    vtkUnstructuredGrid* output = this->GetOutput();
+    output->CopyStructure(input);
+    output->GetPointData()->ShallowCopy(input->GetPointData());
+    output->GetCellData()->ShallowCopy(input->GetCellData());
+    return;
+    }
+  
+  // Setup a reader.
+  vtkUnstructuredGridReader *reader = vtkUnstructuredGridReader::New();
+  reader->ReadFromInputStringOn();
+  vtkCharArray* mystring = vtkCharArray::New();
+  reader->SetInputArray(mystring);
+  mystring->Delete();
+
+  if (this->SocketController && this->ClientFlag)
+    {
+    this->ClientExecute(reader);
+    reader->Delete();
+    reader = NULL;
+    return;
+    }
+  
+  // Create the writer.
+  vtkUnstructuredGridWriter *writer = vtkUnstructuredGridWriter::New();
+  writer->SetFileTypeToBinary();
+  writer->WriteToOutputStringOn();
+
+  // Render server just receives the data from the data server.
+  if (this->RenderServerFlag)
+    {
+    this->RenderServerExecute(reader);
+    }
+
+  // Node 0 gathers all the the data and the broadcast it to all procs.
+  this->ServerExecute(reader, writer);
+
+  reader->Delete();
+  reader = NULL;
+  writer->Delete();
+  writer = NULL;
+
+  vtkTimerLog::MarkEndEvent("MPIGather");
+}
+
+
+//-----------------------------------------------------------------------------
+#ifdef VTK_USE_MPI
+void vtkMPIDuplicateUnstructuredGrid::ServerExecute(vtkUnstructuredGridReader* reader,
+                                            vtkUnstructuredGridWriter* writer)
+#else
+void vtkMPIDuplicateUnstructuredGrid::ServerExecute(vtkUnstructuredGridReader* ,
+                                            vtkUnstructuredGridWriter* writer)
+#endif
+{
+  int numProcs;
+  numProcs = this->Controller->GetNumberOfProcesses();
+  vtkUnstructuredGrid* input;
+  vtkUnstructuredGrid* pd;
+  
+  // First marshal our input.
+  input = this->GetInput();
+  pd = vtkUnstructuredGrid::New();
+  if (input)
+    {
+    pd->CopyStructure(input);
+    pd->GetPointData()->PassData(input->GetPointData());
+    pd->GetCellData()->PassData(input->GetCellData());
+    }
+  writer->SetInput(pd);
+  writer->Write();
+  int size = writer->GetOutputStringLength();
+  char* buf = writer->RegisterAndGetOutputString();
+  
+  pd->Delete();
+  pd = NULL;
+  
+#ifdef VTK_USE_MPI
+  int idx;
+  int sum;
+  int myId = this->Controller->GetLocalProcessId();
+
+  vtkMPICommunicator *com = NULL;
+  if (this->Controller)
+    {
+    com = vtkMPICommunicator::SafeDownCast(
+      this->Controller->GetCommunicator());
+    }
+
+  if (com)
+    {
+    // Allocate arrays used by the AllGatherV call.
+    int* recvLengths = new int[numProcs * 2];
+    // Use a single array for lengths and offsets to avoid
+    // another send to the client.
+    int* recvOffsets = recvLengths+numProcs;
+  
+    // Compute the degenerate input offsets and lengths.
+    // Broadcast our size to all other processes.
+    com->AllGather(&size, recvLengths, 1);
+
+    // Compute the displacements.
+    sum = 0;
+    for (idx = 0; idx < numProcs; ++idx)
+      {
+      recvOffsets[idx] = sum;
+      sum += recvLengths[idx];
+      }
+  
+    // Gather the marshaled data sets from all procs.
+    char* allbuffers = new char[sum];
+    com->AllGatherV(buf, allbuffers, size, recvLengths, recvOffsets);
+    if (myId == 0 && this->SocketController)
+      {
+      // Send the string to the client.
+      this->SocketController->Send(&numProcs, 1, 1, 948344);
+      this->SocketController->Send(recvLengths, numProcs*2, 1, 948345);
+      this->SocketController->Send(allbuffers, sum, 1, 948346);
+      }
+    if (this->RenderServerSocketController)
+      {
+      // Send the string to the render server.
+      this->RenderServerSocketController->Send(&numProcs, 1, 1, 948344);
+      this->RenderServerSocketController->Send(recvLengths, numProcs*2, 1, 948345);
+      this->RenderServerSocketController->Send(allbuffers, sum, 1, 948346);
+      }
+    this->ReconstructOutput(reader, numProcs, allbuffers, 
+                            recvLengths, recvOffsets);
+    delete [] allbuffers;
+    allbuffers = NULL;
+    delete [] recvLengths;
+    recvLengths = NULL;
+    // recvOffsets is part of the recvLengths array.
+    recvOffsets = NULL;
+    delete [] buf;
+    buf = NULL;
+    return;
+    }
+
+#endif
+
+  // Server must be a single process!
+  if (this->RenderServerSocketController)
+    {
+    this->RenderServerSocketController->Send(&numProcs, 1, 1, 948344);
+    int tmp[2];
+    tmp[0] = size;
+    tmp[1] = 0;
+    this->RenderServerSocketController->Send(tmp, 2, 1, 948345);
+    this->RenderServerSocketController->Send(buf, size, 1, 948346);
+    }
+
+  // Server must be a single process!
+  if (this->SocketController)
+    {
+    this->SocketController->Send(&numProcs, 1, 1, 948344);
+    int tmp[2];
+    tmp[0] = size;
+    tmp[1] = 0;
+    this->SocketController->Send(tmp, 2, 1, 948345);
+    this->SocketController->Send(buf, size, 1, 948346);
+    }
+  // Degenerate reconstruct output.
+  if (input)
+    {
+    this->GetOutput()->ShallowCopy(input);
+    }
+  delete [] buf;
+  buf = NULL;
+}
+
+
+
+//-----------------------------------------------------------------------------
+void vtkMPIDuplicateUnstructuredGrid::ReconstructOutput(vtkUnstructuredGridReader* reader,
+                                                int numProcs, char* recv,
+                                                int* recvLengths, 
+                                                int* recvOffsets)
+{
+  vtkUnstructuredGrid* output;
+  vtkUnstructuredGrid* pd;
+  int idx;
+
+  // Reconstruct the poly datas and append them together.
+  vtkAppendFilter *append = vtkAppendFilter::New();
+  // First append the input from this process.
+  for (idx = 0; idx < numProcs; ++idx)
+    {
+    // vtkCharArray should not delete the string when it's done.
+    reader->Modified();
+    reader->GetInputArray()->SetArray(recv+recvOffsets[idx],
+                                      recvLengths[idx], 1);
+    output = reader->GetOutput();
+    output->Update();
+    pd = vtkUnstructuredGrid::New();
+    pd->CopyStructure(output);
+    pd->GetPointData()->PassData(output->GetPointData());
+    pd->GetCellData()->PassData(output->GetCellData());
+    append->AddInput(pd);
+    pd->Delete();
+    pd = NULL;
+    }
+
+  // Append
+  output = append->GetOutput();
+  output->Update();
+
+  // Copy results to our output;
+  pd = this->GetOutput();
+  pd->CopyStructure(output);
+  pd->GetPointData()->PassData(output->GetPointData());
+  pd->GetCellData()->PassData(output->GetCellData());
+
+  append->Delete();
+  append = NULL;
+}
+
+
+//-----------------------------------------------------------------------------
+void vtkMPIDuplicateUnstructuredGrid::ClientExecute(vtkUnstructuredGridReader* reader)
+{
+  int numProcs;
+  int* recvLengths;
+  int* recvOffsets;
+  int totalLength, idx;
+  char* buffers;
+
+  // Receive the numer of processes.
+  this->SocketController->Receive(&numProcs, 1, 1, 948344);
+
+  // Receive information about the lengths/offsets of each marshaled data set.
+  recvLengths = new int[numProcs*2];
+  recvOffsets = recvLengths + numProcs;
+  this->SocketController->Receive(recvLengths, numProcs*2, 1, 948345);
+
+  // Receive the actual buffers.
+  totalLength = 0;
+  for (idx = 0; idx < numProcs; ++idx)
+    {
+    totalLength += recvLengths[idx];
+    }
+  buffers = new char[totalLength];
+  this->SocketController->Receive(buffers, totalLength, 1, 948346);
+
+  this->ReconstructOutput(reader, numProcs, buffers, 
+                          recvLengths, recvOffsets);
+  delete [] recvLengths;
+  recvLengths = NULL;
+  delete [] buffers;
+  buffers = NULL;
+}
+
+
+//-----------------------------------------------------------------------------
+// The way this is implemented is inefficient.
+// It would be better to have node zero receive the duplicate data and
+// broadcast the data to all procs using MPI.
+void vtkMPIDuplicateUnstructuredGrid::RenderServerExecute(vtkUnstructuredGridReader* reader)
+{
+  int numProcs;
+  int* recvLengths;
+  int* recvOffsets;
+  int totalLength, idx;
+  char* buffers;
+
+  if (this->RenderServerSocketController == NULL)
+    {
+    vtkErrorMacro("Missing socket to data server.");
+    }
+
+  // Receive the numer of processes.
+  this->RenderServerSocketController->Receive(&numProcs, 1, 1, 948344);
+
+  // Receive information about the lengths/offsets of each marshaled data set.
+  recvLengths = new int[numProcs*2];
+  recvOffsets = recvLengths + numProcs;
+  this->RenderServerSocketController->Receive(recvLengths, numProcs*2, 1, 948345);
+
+  // Receive the actual buffers.
+  totalLength = 0;
+  for (idx = 0; idx < numProcs; ++idx)
+    {
+    totalLength += recvLengths[idx];
+    }
+  buffers = new char[totalLength];
+  this->RenderServerSocketController->Receive(buffers, totalLength, 1, 948346);
+
+  this->ReconstructOutput(reader, numProcs, buffers, 
+                          recvLengths, recvOffsets);
+  delete [] recvLengths;
+  recvLengths = NULL;
+  delete [] buffers;
+  buffers = NULL;
+}
+
+//-----------------------------------------------------------------------------
+void vtkMPIDuplicateUnstructuredGrid::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+  
+  os << indent << "Controller: (" << this->Controller << ")\n";
+  if (this->SocketController)
+    {
+    os << indent << "SocketController: (" << this->SocketController << ")\n";
+    os << indent << "ClientFlag: " << this->ClientFlag << endl;
+    }
+  if (this->RenderServerSocketController)
+    {
+    os << indent << "RenderServerSocketController: (" 
+       << this->RenderServerSocketController << ")\n";
+    os << indent << "RenderServerFlag: " << this->RenderServerFlag << endl;
+    }
+  os << indent << "PassThrough: " << this->PassThrough << endl;
+  os << indent << "ZeroEmpty: " << this->ZeroEmpty << endl;
+}
+
---
+++ Servers/Filters/vtkMPIDuplicateUnstructuredGrid.h
@@ -0,0 +1,125 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMPIDuplicateUnstructuredGrid.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkMPIDuplicateUnstructuredGrid - For distributed tiled displays.
+// .DESCRIPTION
+// This filter collects poly data and duplicates it on every node.
+// Converts data parallel so every node has a complete copy of the data.
+// The filter is used at the end of a pipeline for driving a tiled
+// display.  This version uses MPI Gather and Broadcast.
+
+
+#ifndef __vtkMPIDuplicateUnstructuredGrid_h
+#define __vtkMPIDuplicateUnstructuredGrid_h
+
+#include "vtkUnstructuredGridToUnstructuredGridFilter.h"
+class vtkSocketController;
+class vtkMultiProcessController;
+class vtkUnstructuredGridWriter;
+class vtkUnstructuredGridReader;
+
+
+class VTK_EXPORT vtkMPIDuplicateUnstructuredGrid : public vtkUnstructuredGridToUnstructuredGridFilter
+{
+public:
+  static vtkMPIDuplicateUnstructuredGrid *New();
+  vtkTypeRevisionMacro(vtkMPIDuplicateUnstructuredGrid, vtkUnstructuredGridToUnstructuredGridFilter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  
+  // Description:
+  // By defualt this filter uses the global controller,
+  // but this method can be used to set another instead.
+  virtual void SetController(vtkMultiProcessController*);
+  vtkGetObjectMacro(Controller, vtkMultiProcessController);
+
+  // Description:
+  // This duplicate filter works in client server mode when this
+  // controller is set.  We have a client flag to diferentiate the
+  // client and server because the socket controller is odd:
+  // Both processes think their id is 0.
+  vtkSocketController *GetSocketController() {return this->SocketController;}
+  void SetSocketController (vtkSocketController *controller);
+  vtkSetMacro(ClientFlag,int);
+  vtkGetMacro(ClientFlag,int);
+
+  // Description:
+  // These methods extend this filter to work with a render server.
+  // If the RenderServerSocket is set, then the fitlers assumes it 
+  // is operating in render server mode. 
+  // The RenderServerFlag differentiates between data and render servers.
+  // The client does not care.
+  vtkSocketController *GetRenderServerSocketController() 
+    {return this->RenderServerSocketController;}
+  void SetRenderServerSocketController (vtkSocketController *controller);
+  vtkSetMacro(RenderServerFlag,int);
+  vtkGetMacro(RenderServerFlag,int);
+
+  // Description:
+  // Turn the filter on or off.  ParaView disable this filter when it will
+  // use compositing instead of local rendering.  This flag is off by default.
+  vtkSetMacro(PassThrough,int);
+  vtkGetMacro(PassThrough,int);
+  vtkBooleanMacro(PassThrough,int);
+
+  // Description:
+  // This flag should be set on all processes when MPI root
+  // is used as client.
+  vtkSetMacro(ZeroEmpty,int);
+  vtkGetMacro(ZeroEmpty,int);
+  vtkBooleanMacro(ZeroEmpty,int);
+
+  // Description:
+  // This returns to size of the output (on this process).
+  // This method is not really used.  It is needed to have
+  // the same API as vtkCollectUnstructuredGrid.
+  //vtkGetMacro(MemorySize, unsigned long);
+  
+  
+protected:
+  vtkMPIDuplicateUnstructuredGrid();
+  ~vtkMPIDuplicateUnstructuredGrid();
+
+  // Data generation method
+  void ComputeInputUpdateExtents(vtkDataObject *output);
+  void Execute();
+  void ServerExecute(vtkUnstructuredGridReader* reader, 
+                     vtkUnstructuredGridWriter* writer);
+  void RenderServerExecute(vtkUnstructuredGridReader* reader);
+  void ClientExecute(vtkUnstructuredGridReader* reader);
+  void ReconstructOutput(vtkUnstructuredGridReader* reader, int numProcs,
+                         char* recv, int* recvLengths, int* recvOffsets);
+  void ExecuteInformation();
+
+  vtkMultiProcessController *Controller;
+
+  // For client server mode.
+  vtkSocketController *SocketController;
+  int ClientFlag;
+
+  // For render server mode.
+  vtkSocketController *RenderServerSocketController;
+  int RenderServerFlag;
+
+  //unsigned long MemorySize;
+  int PassThrough;
+  int ZeroEmpty;
+
+
+private:
+  vtkMPIDuplicateUnstructuredGrid(const vtkMPIDuplicateUnstructuredGrid&); // Not implemented
+  void operator=(const vtkMPIDuplicateUnstructuredGrid&); // Not implemented
+};
+
+#endif
+
