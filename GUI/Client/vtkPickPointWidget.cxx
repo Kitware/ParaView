b5051045c033eb2e63da6b7934725b7069799525
Charles Law <charles.law@kitware.com>
2004-04-19 12:07:06 -0400

    Revert to old behavior, but add keypress pick.

--- GUI/Client/vtkPickPointWidget.cxx
+++ GUI/Client/vtkPickPointWidget.cxx
@@ -25,7 +25,7 @@
 
 
 vtkStandardNewMacro(vtkPickPointWidget);
-vtkCxxRevisionMacro(vtkPickPointWidget, "1.2");
+vtkCxxRevisionMacro(vtkPickPointWidget, "1.3");
 vtkCxxSetObjectMacro(vtkPickPointWidget,RenderModule,vtkPVRenderModule);
 
 
@@ -33,6 +33,7 @@ vtkCxxSetObjectMacro(vtkPickPointWidget,RenderModule,vtkPVRenderModule);
 //----------------------------------------------------------------------------
 vtkPickPointWidget::vtkPickPointWidget()
 {
+  this->EventCallbackCommand->SetCallback(vtkPickPointWidget::ProcessEvents);
   this->RenderModule = 0;
 }
 
@@ -49,20 +50,67 @@ void vtkPickPointWidget::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "RenderModule: (" << this->RenderModule << ")\n";
 }
 
+
 //----------------------------------------------------------------------------
-void vtkPickPointWidget::OnRightButtonDown()
+void vtkPickPointWidget::SetEnabled(int enabling)
 {
-  if (this->RenderModule == NULL)
+  if ( ! this->Interactor )
     {
-    vtkErrorMacro("Cannot pick without a render module.");
+    vtkErrorMacro(<<"The interactor must be set prior to enabling/disabling widget");
     return;
     }
-  int X = this->Interactor->GetEventPosition()[0];
-  int Y = this->Interactor->GetEventPosition()[1];
-  float z = this->RenderModule->GetZBufferValue(X, Y);
-  double pt[4];
-  this->ComputeDisplayToWorld(double(X),double(Y),double(z),pt);
-  this->Cursor3D->SetFocalPoint(pt);
-  this->InvokeEvent(vtkCommand::EndInteractionEvent,NULL);
+
+  if ( enabling && ! this->Enabled)
+    {
+    // listen for the following events
+    vtkRenderWindowInteractor *i = this->Interactor;
+    i->AddObserver(vtkCommand::KeyPressEvent, 
+                   this->EventCallbackCommand, this->Priority);
+    }
+
+  this->Superclass::SetEnabled(enabling);
 }
+
+//----------------------------------------------------------------------------
+void vtkPickPointWidget::ProcessEvents(vtkObject* object, 
+                                       unsigned long event,
+                                       void* clientdata, 
+                                       void* calldata)
+{
+  vtkInteractorObserver* self 
+    = reinterpret_cast<vtkInteractorObserver *>( clientdata );
+
+  vtkPointWidget::ProcessEvents(object, event, clientdata, calldata);
+
+  //look for char and delete events
+  switch(event)
+    {
+    case vtkCommand::CharEvent:
+      self->OnChar();
+      break;
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkPickPointWidget::OnChar()
+{
+  if (this->Interactor->GetKeyCode() == 'p' )
+    {
+    if (this->RenderModule == NULL)
+      {
+      vtkErrorMacro("Cannot pick without a render module.");
+      return;
+      }
+    int X = this->Interactor->GetEventPosition()[0];
+    int Y = this->Interactor->GetEventPosition()[1];
+    float z = this->RenderModule->GetZBufferValue(X, Y);
+    double pt[4];
+    this->ComputeDisplayToWorld(double(X),double(Y),double(z),pt);
+    this->Cursor3D->SetFocalPoint(pt);
+    this->InvokeEvent(vtkCommand::EndInteractionEvent,NULL);
+    return;
+    }
+}
+
+
 
--- GUI/Client/vtkPickPointWidget.h
+++ GUI/Client/vtkPickPointWidget.h
@@ -47,13 +47,24 @@ public:
   void SetRenderModule(vtkPVRenderModule* rm);
   vtkPVRenderModule* GetRenderModule() { return this->RenderModule;}
 
+  // Description:
+  // We have to look for key press events too.
+  virtual void SetEnabled(int);
+
 protected:
   vtkPickPointWidget();
   ~vtkPickPointWidget();
 
   // For picking.  Use a proxy in the future.
   vtkPVRenderModule* RenderModule;
-  virtual void OnRightButtonDown();
+
+  virtual void OnChar();
+
+  // Handles the events
+  static void ProcessEvents(vtkObject* object, 
+                            unsigned long event,
+                            void* clientdata, 
+                            void* calldata);
 
 private:
   vtkPickPointWidget(const vtkPickPointWidget&); // Not implemented
