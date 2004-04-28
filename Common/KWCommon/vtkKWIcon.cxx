fa73392c270698967449db7115f73e26dc75b5b9
Brad King <brad.king@kitware.com>
2004-04-28 17:05:31 -0400

    ERR: Updates for new vtk zlib.

--- Common/KWCommon/vtkKWIcon.cxx
+++ Common/KWCommon/vtkKWIcon.cxx
@@ -18,13 +18,13 @@
 #include "vtkImageFlip.h"
 #include "vtkObjectFactory.h"
 #include "vtkBase64Utilities.h"
-#include "zlib.h"
+#include "vtk_zlib.h"
 
 #include "Resources/icons.h"
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWIcon );
-vtkCxxRevisionMacro(vtkKWIcon, "1.24");
+vtkCxxRevisionMacro(vtkKWIcon, "1.25");
 
 //----------------------------------------------------------------------------
 vtkKWIcon::vtkKWIcon()
