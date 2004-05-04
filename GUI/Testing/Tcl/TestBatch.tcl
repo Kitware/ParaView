eeb17a75cbbe14bda27d166cdc672693560958d2
Berk Geveci <berk.geveci@kitware.com>
2004-05-04 15:19:49 -0400

    Changed the way batch tests are done. Close window - save image - run batch - compare

--- GUI/Testing/Tcl/TestBatch.tcl
+++ GUI/Testing/Tcl/TestBatch.tcl
@@ -7,23 +7,28 @@ if { ![$Application GetExitStatus] } {
             set batchName [lindex $argv [expr $i + 1]]
         }
     }
-
     if { $doBatch } {
+
+        update
+        [[$Application GetMainWindow] GetMainView] ForceRender
+        [[$Application GetMainWindow] GetMainView] SaveAsImage ${batchName}session.png
 
         [$Application GetMainWindow] SaveBatchScript "$batchName.pvb" 0 "$batchName.png" {}
         $Application ExitOnReturnOff
+        $Application Exit
+        update
+
         $Application LoadScript "$batchName.pvb"
         catch { file delete -force "$batchName.pvb" }
         $Application ExitOnReturnOn
-        update
-        [[$Application GetMainWindow] GetMainView] ForceRender
 
         for {set i  1} {$i < [expr $argc - 1]} {incr i} {
             if {[lindex $argv $i] == "-BC"} {
                 source [lindex $argv [expr $i + 1]]
-                $Application SetExitStatus [CompareImage [[$Application GetMainWindow] GetMainView]]
+                $Application SetExitStatus [ComparePNG ${batchName}session]
             }
         }
         catch { file delete -force "$batchName.png" }
+        catch { file delete -force "$batchNamesession.png" }
     }
 }
