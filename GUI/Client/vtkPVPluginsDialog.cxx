1ad0e21a3c0214e4835f2ec31c8a0df5edf1f72c
Francois Bertel <francois.bertel@kitware.com>
2004-05-06 17:53:35 -0400

    first version

---
+++ GUI/Client/vtkPVPluginsDialog.cxx
@@ -0,0 +1,226 @@
+/*=========================================================================
+
+  Module:    vtkPVPluginsDialog.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVPluginsDialog.h"
+
+#include "vtkKWFrame.h"
+#include "vtkKWLabel.h"
+#include "vtkKWLabeledEntry.h"
+#include "vtkObjectFactory.h"
+#include "vtkKWLabeledFrame.h"
+#include "vtkKWPushButton.h"
+#include "vtkKWFrame.h"
+#include "vtkKWCheckButton.h"
+
+//----------------------------------------------------------------------------
+
+vtkStandardNewMacro( vtkPVPluginsDialog );
+vtkCxxRevisionMacro(vtkPVPluginsDialog, "1.1");
+
+int vtkPVPluginsDialogCommand(ClientData cd, Tcl_Interp *interp,
+                                  int argc, char *argv[]);
+
+//----------------------------------------------------------------------------
+vtkPVPluginsDialog::vtkPVPluginsDialog()
+{
+  this->CommandFunction = vtkPVPluginsDialogCommand;
+
+  // Build constants widgets
+  this->PluginsFrame = vtkKWLabeledFrame::New();
+  this->NameButton = vtkKWPushButton::New();
+  this->TypeButton = vtkKWPushButton::New();
+  this->LoadedButton = vtkKWPushButton::New();
+  this->AutoLoadButton = vtkKWPushButton::New();
+  this->PathButton = vtkKWPushButton::New();
+  this->InfoButton = vtkKWPushButton::New();
+
+  this->ButtonsFrame = vtkKWFrame::New();
+  this->AddPluginButton = vtkKWPushButton::New();
+  this->HelpButton = vtkKWPushButton::New();
+  this->CloseButton = vtkKWPushButton::New();
+}
+
+//----------------------------------------------------------------------------
+vtkPVPluginsDialog::~vtkPVPluginsDialog()
+{
+  this->PluginsFrame->Delete();
+  this->NameButton->Delete();
+  this->TypeButton->Delete();
+  this->LoadedButton->Delete();
+  this->AutoLoadButton->Delete();
+  this->PathButton->Delete();
+  this->InfoButton->Delete();
+  this->ButtonsFrame->Delete();
+  this->AddPluginButton->Delete();
+  this->HelpButton->Delete();
+  this->CloseButton->Delete();
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPluginsDialog::Create(vtkKWApplication *app, const char *args)
+{
+  // Invoke super method
+  this->Superclass::Create(app, args);
+
+  SetTitle("Kitware Paraview Plugins");
+
+  this->PluginsFrame->SetParent(this);
+  this->PluginsFrame->Create(app,"-bd 10");
+  this->PluginsFrame->SetLabel("Plugins");
+  this->Script("grid config %s -column 0 -row 0 -columnspan 1 -rowspan 1 -sticky \"nw\"",this->PluginsFrame->GetWidgetName());
+
+  this->NameButton->SetParent(this->PluginsFrame->GetFrame());
+  this->NameButton->Create(app,0);
+  this->NameButton->SetLabel("Name");
+
+  this->TypeButton->SetParent(this->PluginsFrame->GetFrame());
+  this->TypeButton->Create(app,0);
+  this->TypeButton->SetLabel("Type");
+  
+  this->LoadedButton->SetParent(this->PluginsFrame->GetFrame());
+  this->LoadedButton->Create(app,0);
+  this->LoadedButton->SetLabel("Loaded");
+
+  this->AutoLoadButton->SetParent(this->PluginsFrame->GetFrame());
+  this->AutoLoadButton->Create(app,0);
+  this->AutoLoadButton->SetLabel("Auto load");
+
+  this->PathButton->SetParent(this->PluginsFrame->GetFrame());
+  this->PathButton->Create(app,0);
+  this->PathButton->SetLabel("Path");
+
+  this->InfoButton->SetParent(this->PluginsFrame->GetFrame());
+  this->InfoButton->Create(app,0);
+  this->InfoButton->SetLabel("Information");
+
+
+  this->Script("grid config %s -column 0 -row 0 -columnspan 1 -rowspan 1 -sticky \"news\"",this->NameButton->GetWidgetName());
+  this->Script("grid config %s -column 1 -row 0 -columnspan 1 -rowspan 1 -sticky \"news\"",this->TypeButton->GetWidgetName());
+  this->Script("grid config %s -column 2 -row 0 -columnspan 1 -rowspan 1 -sticky \"news\"",this->LoadedButton->GetWidgetName());
+  this->Script("grid config %s -column 3 -row 0 -columnspan 1 -rowspan 1 -sticky \"news\"",this->AutoLoadButton->GetWidgetName());
+  this->Script("grid config %s -column 4 -row 0 -columnspan 1 -rowspan 1 -sticky \"news\"",this->PathButton->GetWidgetName());
+  this->Script("grid config %s -column 5 -row 0 -columnspan 1 -rowspan 1 -sticky \"news\"",this->InfoButton->GetWidgetName());
+
+  this->Script("grid columnconfigure . 0 -weight 1");
+  this->Script("grid rowconfigure . 2 -weight 1");
+
+  // bottom buttons: Add a plugin, Help, Close
+  this->ButtonsFrame->SetParent(this);
+  this->ButtonsFrame->Create(app,"-bd 10");
+  this->Script("grid config %s -column 0 -row 1 -columnspan 1 -rowspan 1 -sticky \"news\"",this->ButtonsFrame->GetWidgetName());
+
+  this->AddPluginButton->SetParent(this->ButtonsFrame);
+  this->AddPluginButton->Create(app,0);
+  this->AddPluginButton->SetLabel("Add a plug-in");
+  this->Script("pack %s -side left -expand true -fill x",this->AddPluginButton->GetWidgetName());
+
+  this->CloseButton->SetParent(this->ButtonsFrame);
+  this->CloseButton->Create(app,0);
+  this->CloseButton->SetLabel("Close");
+  this->Script("pack %s -side right -expand true -fill x",this->CloseButton->GetWidgetName());
+  this->CloseButton->SetCommand(this, "Cancel"); // Cannot be OK: closing the dialog with the window X button returns Cancel.
+
+  this->HelpButton->SetParent(this->ButtonsFrame);
+  this->HelpButton->Create(app,0);
+  this->HelpButton->SetLabel("Help");
+  this->Script("pack %s -side right -expand true -fill x",this->HelpButton->GetWidgetName());
+
+  // THE FOLLOWING LINES ARE FAKE
+
+  const char *FooFilename="libfoo.so";
+  const int FooType=1; // 1 means filter
+  const int FooIsLoaded=1; // boolean
+  const int FooAutoLoad=1; // boolean
+  const char *FooPath="/usr/local/paraview/plugins";
+
+  int PluginIndex=1;
+  char *PluginTypeText=0;
+
+  vtkKWLabel *PluginName=vtkKWLabel::New();
+  PluginName->SetParent(this->PluginsFrame->GetFrame());
+  PluginName->Create(app,0);
+  PluginName->SetLabel(FooFilename);
+  this->Script("grid config %s -column 0 -row %d -columnspan 1 -rowspan 1 -sticky \"news\"",PluginName->GetWidgetName(),PluginIndex);
+  
+  vtkKWLabel *PluginType=vtkKWLabel::New();
+  PluginType->SetParent(this->PluginsFrame->GetFrame());
+  PluginType->Create(app,0);
+  switch(FooType)
+    {
+    case 1:
+      PluginTypeText="Filter";
+      break;
+    case 2:
+      PluginTypeText="DataSetAdaptor";
+      break;
+    default:
+      ;// error
+    }
+  PluginType->SetLabel(PluginTypeText);
+  this->Script("grid config %s -column 1 -row %d -columnspan 1 -rowspan 1 -sticky \"news\"",PluginType->GetWidgetName(),PluginIndex);
+
+  vtkKWCheckButton *PluginIsLoaded=vtkKWCheckButton::New();
+  PluginIsLoaded->SetParent(this->PluginsFrame->GetFrame());
+  PluginIsLoaded->Create(app,0);
+  PluginIsLoaded->SetState(FooIsLoaded);
+  this->Script("grid config %s -column 2 -row %d -columnspan 1 -rowspan 1 -sticky \"news\"",PluginIsLoaded->GetWidgetName(),PluginIndex);
+
+  vtkKWCheckButton *PluginAutoLoad=vtkKWCheckButton::New();
+  PluginAutoLoad->SetParent(this->PluginsFrame->GetFrame());
+  PluginAutoLoad->Create(app,0);
+  PluginAutoLoad->SetState(FooAutoLoad);
+  this->Script("grid config %s -column 3 -row %d -columnspan 1 -rowspan 1 -sticky \"news\"",PluginAutoLoad->GetWidgetName(),PluginIndex);
+  
+  vtkKWLabel *PluginPath=vtkKWLabel::New();
+  PluginPath->SetParent(this->PluginsFrame->GetFrame());
+  PluginPath->Create(app,0);
+  PluginPath->SetLabel(FooPath);
+  this->Script("grid config %s -column 4 -row %d -columnspan 1 -rowspan 1 -sticky \"news\"",PluginPath->GetWidgetName(),PluginIndex);
+
+  vtkKWPushButton *PluginInfo=vtkKWPushButton::New();
+  PluginInfo->SetParent(this->PluginsFrame->GetFrame());
+  PluginInfo->Create(app,"-bitmap info -foreground #0a0");
+  this->Script("grid config %s -column 5 -row %d -columnspan 1 -rowspan 1 -sticky \"news\"",PluginInfo->GetWidgetName(),PluginIndex);
+}
+
+//----------------------------------------------------------------------------
+int vtkPVPluginsDialog::Invoke()
+{
+  if (this->IsCreated()) // should be require("is_created",this->IsCreated());
+    {
+      // Display one row per plugin
+#if 0
+      if(cached_plugins==0)
+        {
+          cached_plugins=new Cached_plugin;
+        }
+      it=cached_plugins->iterator();
+      it->start();
+      while(!it->is_off())
+        {
+          it->item();
+          it->forth();
+        }
+#endif
+    }
+  
+  return this->Superclass::Invoke();
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPluginsDialog::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+
+  //  os << indent << "Entry: " << this->Entry << endl;
+}
---
+++ GUI/Client/vtkPVPluginsDialog.h
@@ -0,0 +1,85 @@
+/*=========================================================================
+  
+  Program:   ParaView
+  Module:    vtkPVPluginsDialog.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPVPluginsDialog - ParaView Plugins user interface.
+// .SECTION Description
+// An object of this class represents a dialog box for managing available
+// plug-ins.
+// 
+// The caption is "Plug-ins". The bottom consists of 3 buttons: "Add a
+// plug-in" to open a browsering dialog box to load a plug-in  manually,
+// and a "Help" and "Close" button.
+// The main window consists of a list of plug-ins. Each row is composed of
+// six columns: name, type, loaded, auto load, path and information. The first
+// row displays title for each column.
+//
+// .SECTION See Also
+// vtkPVWindow
+
+#ifndef __vtkPVPluginsDialog_h
+#define __vtkPVPluginsDialog_h
+
+#include "vtkKWDialog.h"
+
+class vtkKWLabeledFrame;
+class vtkKWPushButton;
+class vtkKWFrame;
+
+class VTK_EXPORT vtkPVPluginsDialog
+  : public vtkKWDialog
+{
+ public:
+  static vtkPVPluginsDialog* New();
+  vtkTypeRevisionMacro(vtkPVPluginsDialog,vtkKWDialog);
+  void PrintSelf(ostream& os,
+                 vtkIndent indent);
+  
+  // Description:
+  // Create the widget
+  virtual void Create(vtkKWApplication *app,
+                      const char *args);
+  
+  // Description:
+  // Invoke the dialog and display it in a modal manner. 
+  // This method returns a zero if the dialog was killed or 
+  // canceled, nonzero otherwise.
+  virtual int Invoke();
+  
+ protected:
+  // Description:
+  // Default constructor.
+  vtkPVPluginsDialog();
+  // Description:
+  // Destructor.
+  virtual ~vtkPVPluginsDialog();
+  
+  vtkKWLabeledFrame *PluginsFrame;
+  vtkKWPushButton *NameButton;
+  vtkKWPushButton *TypeButton;
+  vtkKWPushButton *LoadedButton;
+  vtkKWPushButton *AutoLoadButton;
+  vtkKWPushButton *PathButton;
+  vtkKWPushButton *InfoButton;
+  
+  vtkKWFrame *ButtonsFrame;
+  vtkKWPushButton *AddPluginButton;
+  vtkKWPushButton *HelpButton;
+  vtkKWPushButton *CloseButton;
+  
+ private:
+  vtkPVPluginsDialog(const vtkPVPluginsDialog&); // Not implemented
+  void operator=(const vtkPVPluginsDialog&); // Not implemented
+};
+
+#endif // #ifndef __vtkPVPluginsDialog_h
