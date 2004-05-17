5d0980584ccb2fd4da5f5f35cab26ddff9934307
Ken Martin <ken.martin@kitware.com>
2004-05-17 10:24:21 -0400

    fix warning

--- GUI/Client/pvTestDriver.cxx
+++ GUI/Client/pvTestDriver.cxx
@@ -12,17 +12,13 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-#include "pvTestDriver.h"
-#include "pvTestDriverConfig.h"
 
 #include "vtkSystemIncludes.h"
 
-#if defined(_WIN32) && !defined(__CYGWIN__)
-# include <windows.h>
-# if defined(_MSC_VER) && !defined(VTK_DISPLAY_WIN32_WARNINGS)
-#  pragma warning ( disable : 4786 )
-# endif //VTK_DISPLAY_WIN32_WARNINGS
-#else
+#include "pvTestDriver.h"
+#include "pvTestDriverConfig.h"
+
+#if !defined(_WIN32) || defined(__CYGWIN__)
 # include <unistd.h>
 # include <sys/wait.h>
 #endif
