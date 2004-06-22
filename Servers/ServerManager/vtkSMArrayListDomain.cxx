90f115b266d30fe4f1964a3acffe6eef669ca6c3
Berk Geveci <berk.geveci@kitware.com>
2004-06-22 09:46:26 -0400

    Fixed warning

--- Servers/ServerManager/vtkSMArrayListDomain.cxx
+++ Servers/ServerManager/vtkSMArrayListDomain.cxx
@@ -24,7 +24,7 @@
 #include "vtkSMSourceProxy.h"
 
 vtkStandardNewMacro(vtkSMArrayListDomain);
-vtkCxxRevisionMacro(vtkSMArrayListDomain, "1.1");
+vtkCxxRevisionMacro(vtkSMArrayListDomain, "1.2");
 
 //---------------------------------------------------------------------------
 vtkSMArrayListDomain::vtkSMArrayListDomain()
@@ -40,7 +40,7 @@ vtkSMArrayListDomain::~vtkSMArrayListDomain()
 void vtkSMArrayListDomain::AddArrays(vtkPVDataSetAttributesInformation* info, 
                                      vtkSMInputArrayDomain* iad)
 {
-  int attrIdx=-1;
+  //int attrIdx=-1;
   //vtkPVArrayInformation* attrInfo = info->GetAttributeInformation(
   //this->AttributeType);
   int num = info->GetNumberOfArrays();
