71866c014906ee27ccf7ff55c8eab8bd6e90a5cc
Berk Geveci <berk.geveci@kitware.com>
2004-07-22 14:09:52 -0400

    Create a prototype only if it is not already there

--- Servers/ServerManager/vtkSMProxyManager.cxx
+++ Servers/ServerManager/vtkSMProxyManager.cxx
@@ -28,7 +28,7 @@
 #include "vtkSMProxyManagerInternals.h"
 
 vtkStandardNewMacro(vtkSMProxyManager);
-vtkCxxRevisionMacro(vtkSMProxyManager, "1.15");
+vtkCxxRevisionMacro(vtkSMProxyManager, "1.16");
 
 //---------------------------------------------------------------------------
 vtkSMProxyManager::vtkSMProxyManager()
@@ -65,9 +65,12 @@ void vtkSMProxyManager::InstantiateGroupPrototypes(const char* groupName)
     for(; it2 != it->second.end(); it2++)
       {
       vtkPVXMLElement* element = it2->second.GetPointer();
-      vtkSMProxy* proxy = this->NewProxy(element, groupName);
-      this->RegisterProxy(newgroupname.str(), it2->first.c_str(), proxy);
-      proxy->Delete();
+      if (!this->GetProxy(newgroupname.str(), it2->first.c_str()))
+        {
+        vtkSMProxy* proxy = this->NewProxy(element, groupName);
+        this->RegisterProxy(newgroupname.str(), it2->first.c_str(), proxy);
+        proxy->Delete();
+        }
       }
 
     }
