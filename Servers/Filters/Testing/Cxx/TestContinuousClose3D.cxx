a7b9e70840f9f4b326b2a12902ae14da40106efb
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-08-19 13:58:57 -0400

    Copyright

--- Servers/Filters/Testing/Cxx/TestContinuousClose3D.cxx
+++ Servers/Filters/Testing/Cxx/TestContinuousClose3D.cxx
@@ -1,3 +1,17 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    TestContinuousClose3D.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkImageReader.h"
 #include "vtkPVImageContinuousDilate3D.h"
 #include "vtkPVImageContinuousErode3D.h"
