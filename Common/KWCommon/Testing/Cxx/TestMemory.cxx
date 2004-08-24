0ca4d139aeaf95fbd5773dcca058d8373371df13
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-08-24 13:15:44 -0400

    ENH: Reenable KWCommon testing

--- Common/KWCommon/Testing/Cxx/TestMemory.cxx
+++ Common/KWCommon/Testing/Cxx/TestMemory.cxx
@@ -1,3 +1,17 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    TestMemory.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 #include "vtkKWProcessStatistics.h"
 
 int main()
