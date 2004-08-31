bbfc0683ee4c08edb015c0aff31dc60652f15e52
Francois Bertel <francois.bertel@kitware.com>
2004-08-31 11:41:04 -0400

    Fixed missing header files for old gcc version

--- Common/KWCommon/vtkKWProcessStatistics.cxx
+++ Common/KWCommon/vtkKWProcessStatistics.cxx
@@ -13,7 +13,7 @@
 =========================================================================*/
 #include "vtkKWProcessStatistics.h"
 
-vtkCxxRevisionMacro(vtkKWProcessStatistics, "1.6");
+vtkCxxRevisionMacro(vtkKWProcessStatistics, "1.7");
 
 #ifdef __linux
 #include <sys/procfs.h>
@@ -21,6 +21,8 @@ vtkCxxRevisionMacro(vtkKWProcessStatistics, "1.6");
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/utsname.h> // int uname(struct utsname *buf);
+#include <ctype.h> // int isdigit(int c);
+#include <errno.h> // extern int errno;
 #elif __hpux
 #include <sys/param.h>
 #include <sys/pstat.h>
