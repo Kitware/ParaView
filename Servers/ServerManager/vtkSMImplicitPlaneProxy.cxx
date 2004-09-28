b4792644c0fe5fa782a7cb1446445c04367aac84
Berk Geveci <berk.geveci@kitware.com>
2004-09-28 14:54:43 -0400

    ENH: Added new proxy for implicit plane. This proxy adds an offset property that allows the user to specify a displacement for the center

---
+++ Servers/ServerManager/vtkSMImplicitPlaneProxy.cxx
@@ -0,0 +1,86 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMImplicitPlaneProxy.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMImplicitPlaneProxy.h"
+
+#include "vtkClientServerStream.h"
+#include "vtkObjectFactory.h"
+#include "vtkProcessModule.h"
+#include "vtkSMDoubleVectorProperty.h"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkSMImplicitPlaneProxy);
+vtkCxxRevisionMacro(vtkSMImplicitPlaneProxy, "1.1");
+
+//----------------------------------------------------------------------------
+vtkSMImplicitPlaneProxy::vtkSMImplicitPlaneProxy()
+{
+  this->Origin[0] = this->Origin[1] = this->Origin[2] = 0;
+  this->Offset = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkSMImplicitPlaneProxy::~vtkSMImplicitPlaneProxy()
+{  
+}
+
+//----------------------------------------------------------------------------
+void vtkSMImplicitPlaneProxy::UpdateVTKObjects()
+{
+  this->Superclass::UpdateVTKObjects();
+
+  double origin[3];
+
+  vtkSMDoubleVectorProperty* normal = vtkSMDoubleVectorProperty::SafeDownCast(
+    this->GetProperty("Normal"));
+  if (!normal || normal->GetNumberOfElements() != 3)
+    {
+    vtkErrorMacro("A Normal property with 3 components could not be found. "
+                  "Please make sure that the configuration file is correct.");
+    return;
+    }
+  for (unsigned int i=0; i<3; i++)
+    {
+    origin[i] = this->Origin[i] + this->Offset*normal->GetElement(i);
+    }
+
+  vtkClientServerStream str;
+  int numObjects = this->GetNumberOfIDs();
+  for (int i=0; i<numObjects; i++)
+    {
+    str << vtkClientServerStream::Invoke
+        << this->GetID(i) << "SetOrigin" << origin[0] << origin[1] << origin[2]
+        << vtkClientServerStream::End;
+    }
+
+  if (str.GetNumberOfMessages() > 0)
+    {
+    vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
+    pm->SendStream(this->Servers, str, 0);
+    }
+
+}
+ 
+//----------------------------------------------------------------------------
+void vtkSMImplicitPlaneProxy::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+
+  os << indent << "Offset: " << this->Offset << endl;
+  os << indent << "Origin: " << this->Origin << endl;
+}
+
+
+
+
---
+++ Servers/ServerManager/vtkSMImplicitPlaneProxy.h
@@ -0,0 +1,60 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMImplicitPlaneProxy.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMImplicitPlaneProxy - proxy for vtkPlane
+// .SECTION Description
+// vtkSMImplicitPlaneProxy adds an offset setting to vtkPlane.
+// This offset is used together with normal and origin when
+// setting parameters on the represented object.
+
+#ifndef __vtkSMImplicitPlaneProxy_h
+#define __vtkSMImplicitPlaneProxy_h
+
+#include "vtkSMProxy.h"
+
+class VTK_EXPORT vtkSMImplicitPlaneProxy : public vtkSMProxy
+{
+public:
+  static vtkSMImplicitPlaneProxy* New();
+  vtkTypeRevisionMacro(vtkSMImplicitPlaneProxy, vtkSMProxy);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // The origin is shifted in the direction of the normal
+  // by the offset.
+  vtkSetMacro(Offset, double);
+  vtkGetMacro(Offset, double);
+
+  // Description:
+  // The origin of the plane.
+  vtkSetVector3Macro(Origin, double);
+  vtkGetVector3Macro(Origin, double);
+
+  // Description:
+  // Push values to the VTK objects.
+  virtual void UpdateVTKObjects();
+
+protected:
+  vtkSMImplicitPlaneProxy();
+  ~vtkSMImplicitPlaneProxy();
+
+  double Origin[3];
+  double Offset;
+
+private:
+  vtkSMImplicitPlaneProxy(const vtkSMImplicitPlaneProxy&); // Not implemented
+  void operator=(const vtkSMImplicitPlaneProxy&); // Not implemented
+};
+
+#endif
