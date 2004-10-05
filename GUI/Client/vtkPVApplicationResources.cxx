587e949c91e3e5ed2fe8ca4f4825e8aa1f875a73
Ken Martin <ken.martin@kitware.com>
2004-10-05 09:52:39 -0400

    ENH: updated splash screen

--- GUI/Client/vtkPVApplicationResources.cxx
+++ GUI/Client/vtkPVApplicationResources.cxx
@@ -254,11 +254,16 @@ void vtkPVApplication::CreateButtonPhotos()
 //----------------------------------------------------------------------------
 void vtkPVApplication::CreateSplashScreen()
 {
+  // copy the image from the header file into memory
   unsigned char *buffer = 
     new unsigned char [image_PVSplashScreen_buffer_length];
   memcpy(buffer, image_PVSplashScreen1, sizeof(image_PVSplashScreen1) - 1);
   memcpy(buffer + sizeof(image_PVSplashScreen1) - 1, image_PVSplashScreen2, 
          sizeof(image_PVSplashScreen2) - 1);
+  memcpy(buffer + sizeof(image_PVSplashScreen1) 
+         + sizeof(image_PVSplashScreen2)- 2, image_PVSplashScreen3, 
+         sizeof(image_PVSplashScreen3) - 1);
+
   this->CreatePhoto("PVSplashScreen", 
                     buffer, 
                     image_PVSplashScreen_width, 
