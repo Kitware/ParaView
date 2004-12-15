8d7e3454553ea6af5579d634f7f4c0fbd49d7e4f
Charles Law <charles.law@kitware.com>
2004-12-15 11:46:50 -0500

    BUG: Trying to quickly fix a compile problem.

--- Servers/Filters/vtkCTHAMRContour.cxx
+++ Servers/Filters/vtkCTHAMRContour.cxx
@@ -41,7 +41,7 @@
 #include "vtkGarbageCollector.h"
 
 
-vtkCxxRevisionMacro(vtkCTHAMRContour, "1.4");
+vtkCxxRevisionMacro(vtkCTHAMRContour, "1.5");
 vtkStandardNewMacro(vtkCTHAMRContour);
 
 //----------------------------------------------------------------------------
@@ -135,7 +135,7 @@ void vtkCTHAMRContour::Execute()
   this->UpdateProgress(.95);
   output = this->GetOutput();
  
-  if (tmp->GetNumberOfInputConnections() > 0)
+  if (tmp->GetNumberOfInputConnections(0) > 0)
     {
     vtkTimerLog::MarkStartEvent("BlockAppend");               
     tmp->Update();
