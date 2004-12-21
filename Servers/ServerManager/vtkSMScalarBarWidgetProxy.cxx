bcc8cbc97e70fd50cf7758d97ff4d9cc2dc1784f
Ken Moreland <kmorel@sandia.gov>
2004-12-21 15:07:08 -0500

    COMP: Fix a compilation warning about accessing the third element of the Position1 and Position2 arrays when they are only length 2.

--- Servers/ServerManager/vtkSMScalarBarWidgetProxy.cxx
+++ Servers/ServerManager/vtkSMScalarBarWidgetProxy.cxx
@@ -27,7 +27,7 @@
 #include "vtkSMProxyProperty.h"
 
 vtkStandardNewMacro(vtkSMScalarBarWidgetProxy);
-vtkCxxRevisionMacro(vtkSMScalarBarWidgetProxy, "1.2");
+vtkCxxRevisionMacro(vtkSMScalarBarWidgetProxy, "1.3");
 
 //----------------------------------------------------------------------------
 vtkSMScalarBarWidgetProxy::vtkSMScalarBarWidgetProxy()
@@ -725,10 +725,8 @@ void vtkSMScalarBarWidgetProxy::PrintSelf(ostream& os, vtkIndent indent)
   this->Superclass::PrintSelf(os,indent);
 
   os << indent << "Position1: " 
-    << this->Position1[0] << ", " << this->Position1[1]
-    << ", " << this->Position1[2] << endl;
+    << this->Position1[0] << ", " << this->Position1[1] << endl;
   os << indent << "Position2: " 
-    << this->Position2[0] << ", " << this->Position2[1]
-    << ", " << this->Position2[2] << endl;
+    << this->Position2[0] << ", " << this->Position2[1] << endl;
   os << indent << "Orientation: " << this->Orientation << endl;
 }
