1351b2928c95b2325318ebc523fe3ffb44d83211
Brad King <brad.king@kitware.com>
2005-01-04 16:47:48 -0500

    COMP: Added definition of VTK_WRAPPING_CXX to the top of all generated wrapper .cxx files.  Using the definition to silence deprecation warnings on wrapped calls to legacy methods.

--- Utilities/VTKClientServer/Wrapping/vtkWrapClientServer.c
+++ Utilities/VTKClientServer/Wrapping/vtkWrapClientServer.c
@@ -394,6 +394,7 @@ void vtkParseOutput(FILE *fp, FileInfo *data)
   int i;
 
   fprintf(fp,"// ClientServer wrapper for %s object\n//\n",data->ClassName);
+  fprintf(fp,"#define VTK_WRAPPING_CXX\n");
   if(strcmp("vtkObjectBase", data->ClassName) != 0)
     {
     /* Block inclusion of full streams. */
