1697912e27d11c2d9c14293774ff8726b8a315d4
Berk Geveci <berk.geveci@kitware.com>
2005-02-25 09:15:39 -0500

    ENH: Better writer support in server manager

--- Servers/ServerManager/vtkSMWriterProxy.cxx
+++ Servers/ServerManager/vtkSMWriterProxy.cxx
@@ -1,15 +1,15 @@
 /*=========================================================================
 
-  Program:   ParaView
-  Module:    vtkSMWriterProxy.cxx
+Program:   ParaView
+Module:    vtkSMWriterProxy.cxx
 
-  Copyright (c) Kitware, Inc.
-  All rights reserved.
-  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+Copyright (c) Kitware, Inc.
+All rights reserved.
+See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
 #include "vtkSMWriterProxy.h"
@@ -18,71 +18,13 @@
 #include "vtkClientServerStream.h"
 #include "vtkObjectFactory.h"
 #include "vtkProcessModule.h"
-#include "vtkSMPart.h"
-#include "vtkSMSourceProxy.h"
 
 vtkStandardNewMacro(vtkSMWriterProxy);
 vtkCxxRevisionMacro(vtkSMWriterProxy, "Revision: 1.1 $");
 
-void vtkSMWriterProxy::AddInput(vtkSMSourceProxy *input,
-                                const char *method,
-                                int vtkNotUsed(portIdx),
-                                int hasMultipleInputs)
+void vtkSMWriterProxy::UpdatePipeline()
 {
-
-  if (!input)
-    {
-    return;
-    }
-
-  input->CreateParts();
-  int numInputs = input->GetNumberOfParts();
-
-  vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
-
-  vtkClientServerStream stream;
-  if (hasMultipleInputs)
-    {
-    // One filter, multiple inputs
-    this->CreateVTKObjects(1);
-    vtkClientServerID sourceID = this->GetID(0);
-    for (int partIdx = 0; partIdx < numInputs; ++partIdx)
-      {
-      vtkSMPart* part = input->GetPart(partIdx);
-      stream << vtkClientServerStream::Invoke 
-             << sourceID << method << part->GetID(0) 
-             << vtkClientServerStream::End;
-      }
-    pm->SendStream(this->Servers, stream);
-    }
-  else
-    {
-    // n inputs, n filters
-    this->CreateVTKObjects(numInputs);
-    int numSources = this->GetNumberOfIDs();
-    for (int sourceIdx = 0; sourceIdx < numSources; ++sourceIdx)
-      {
-      vtkClientServerID sourceID = this->GetID(sourceIdx);
-      // This is to handle the case when there are multiple
-      // inputs and the first one has multiple parts. For
-      // example, in the Glyph filter, when the input has multiple
-      // parts, the glyph source has to be applied to each.
-      // NOTE: Make sure that you set the input which has as
-      // many parts as there will be filters first. OR call
-      // CreateVTKObjects() with the right number of inputs.
-      int partIdx = sourceIdx % numInputs;
-      vtkSMPart* part = input->GetPart(partIdx);
-      stream << vtkClientServerStream::Invoke 
-             << sourceID << method << part->GetID(0) 
-             << vtkClientServerStream::End;
-      }
-    pm->SendStream(this->Servers, stream);
-    }
-}
-
-void vtkSMWriterProxy::UpdateVTKObjects()
-{
-  this->Superclass::UpdateVTKObjects();
+  this->Superclass::UpdatePipeline();
 
   vtkProcessModule *pm = vtkProcessModule::GetProcessModule();
   vtkClientServerStream str;
--- Servers/ServerManager/vtkSMWriterProxy.h
+++ Servers/ServerManager/vtkSMWriterProxy.h
@@ -1,15 +1,15 @@
 /*=========================================================================
 
-  Program:   ParaView
-  Module:    vtkSMWriterProxy.h
+Program:   ParaView
+Module:    vtkSMWriterProxy.h
 
-  Copyright (c) Kitware, Inc.
-  All rights reserved.
-  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+Copyright (c) Kitware, Inc.
+All rights reserved.
+See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
 // .NAME vtkSMWriterProxy - proxy for a VTK writer on a server
@@ -20,37 +20,18 @@
 #ifndef __vtkSMWriterProxy_h
 #define __vtkSMWriterProxy_h
 
-#include "vtkSMProxy.h"
+#include "vtkSMSourceProxy.h"
 
-class vtkSMSourceProxy;
-
-class VTK_EXPORT vtkSMWriterProxy : public vtkSMProxy
+class VTK_EXPORT vtkSMWriterProxy : public vtkSMSourceProxy
 {
 public:
   static vtkSMWriterProxy* New();
-  vtkTypeRevisionMacro(vtkSMWriterProxy, vtkSMProxy);
+  vtkTypeRevisionMacro(vtkSMWriterProxy, vtkSMSourceProxy);
   void PrintSelf(ostream& os, vtkIndent indent);
 
   // Description:
-  // Connects filters/sinks to an input. If the filter(s) is not
-  // created, this will create it. If hasMultipleInputs is
-  // true,  only one filter is created, even if the input has
-  // multiple parts. All the inputs are added using the method
-  // name provided. If hasMultipleInputs is not true, one filter
-  // is created for each input. NOTE: The filter(s) is created
-  // when SetInput is called the first and if the it wasn't already
-  // created. If the filter has two inputs and one is multi-block
-  // whereas the other one is not, SetInput() should be called with
-  // the multi-block input first. Otherwise, it will create only
-  // on filter and can not apply to the multi-block input.
-  void AddInput(vtkSMSourceProxy* input, 
-                const char* method,
-                int portIdx,
-                int hasMultipleInputs);
-
-  // Description:
-  // Push the properties on to the vtk objects.
-  virtual void UpdateVTKObjects();
+  // Updates the pipeline and writes the file(s).
+  virtual void UpdatePipeline();
 
 protected:
   vtkSMWriterProxy() {}
