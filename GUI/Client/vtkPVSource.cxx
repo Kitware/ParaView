442d234233d652ab0003e48f792502ccc7375799
Amy Squillacote <amy.squillacote@kitware.com>
2005-03-25 08:45:32 -0500

    ENH: adding OverideAutoAccept to PrintSelf

--- GUI/Client/vtkPVSource.cxx
+++ GUI/Client/vtkPVSource.cxx
@@ -69,7 +69,7 @@
 
 
 vtkStandardNewMacro(vtkPVSource);
-vtkCxxRevisionMacro(vtkPVSource, "1.416.2.2");
+vtkCxxRevisionMacro(vtkPVSource, "1.416.2.3");
 vtkCxxSetObjectMacro(vtkPVSource,Notebook,vtkPVSourceNotebook);
 vtkCxxSetObjectMacro(vtkPVSource,PartDisplay,vtkSMPartDisplay);
 
@@ -2557,4 +2557,5 @@ void vtkPVSource::PrintSelf(ostream& os, vtkIndent indent)
     }
   os << indent << "CubeAxesVisibility: " << this->CubeAxesVisibility << endl;
   os << indent << "PointLabelVisibility: " << this->PointLabelVisibility << endl;
+  os << indent << "OverideAutoAccept: " << this->OverideAutoAccept << endl;
 }
