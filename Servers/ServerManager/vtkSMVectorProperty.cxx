1793a6d6ff948f53b152466b6b6dc328f79678cc
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2005-04-15 14:44:17 -0400

    BUG: fixed printself error writing null string

--- Servers/ServerManager/vtkSMVectorProperty.cxx
+++ Servers/ServerManager/vtkSMVectorProperty.cxx
@@ -16,7 +16,7 @@
 
 #include "vtkPVXMLElement.h"
 
-vtkCxxRevisionMacro(vtkSMVectorProperty, "1.4");
+vtkCxxRevisionMacro(vtkSMVectorProperty, "1.5");
 
 //---------------------------------------------------------------------------
 vtkSMVectorProperty::vtkSMVectorProperty()
@@ -96,7 +96,9 @@ void vtkSMVectorProperty::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "NumberOfElements: " << this->GetNumberOfElements() << endl;
   os << indent << "NumberOfElementsPerCommand: " 
      << this->GetNumberOfElementsPerCommand() << endl;
-  os << indent << "RepeatCommand: " << this->RepeatCommand << endl;
-  os << indent << "CleanCommand: " << this->CleanCommand << endl;
+  os << indent << "RepeatCommand: " << 
+    (this->RepeatCommand? this->RepeatCommand : "(null)" ) << endl;
+  os << indent << "CleanCommand: " << 
+    (this->CleanCommand? this->CleanCommand : "(null)" ) << endl;
   os << indent << "UseIndex: " << this->UseIndex << endl;
 }
