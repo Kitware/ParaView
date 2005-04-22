8f440d5584dc26c22679c7dd0f712cbcf01559c3
Ken Martin <ken.martin@kitware.com>
2005-04-22 10:50:30 -0400

    ENH: readded PVClipDataSet

---
+++ Servers/Filters/vtkPVClipDataSet.cxx
@@ -0,0 +1,38 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVClipDataSet.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVClipDataSet.h"
+
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro(vtkPVClipDataSet, "1.6");
+vtkStandardNewMacro(vtkPVClipDataSet);
+
+//----------------------------------------------------------------------------
+vtkPVClipDataSet::vtkPVClipDataSet(vtkImplicitFunction *vtkNotUsed(cf))
+{
+  // setting NumberOfOutputPorts to 1 because ParaView does not allow you to
+  // generate the clipped output
+  this->SetNumberOfOutputPorts(1);
+}
+
+//----------------------------------------------------------------------------
+vtkPVClipDataSet::~vtkPVClipDataSet()
+{
+}
+//----------------------------------------------------------------------------
+void vtkPVClipDataSet::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Servers/Filters/vtkPVClipDataSet.h
@@ -0,0 +1,43 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVClipDataSet.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPVClipDataSet - Clip filter
+//
+// .SECTION Description
+// This is a subclass of vtkClipDataSet that allows selection of input scalars
+
+#ifndef __vtkPVClipDataSet_h
+#define __vtkPVClipDataSet_h
+
+#include "vtkClipDataSet.h"
+
+class VTK_EXPORT vtkPVClipDataSet : public vtkClipDataSet
+{
+public:
+  vtkTypeRevisionMacro(vtkPVClipDataSet,vtkClipDataSet);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  static vtkPVClipDataSet *New();
+
+protected:
+  vtkPVClipDataSet(vtkImplicitFunction *cf=NULL);
+  ~vtkPVClipDataSet();
+
+private:
+  vtkPVClipDataSet(const vtkPVClipDataSet&);  // Not implemented.
+  void operator=(const vtkPVClipDataSet&);  // Not implemented.
+};
+
+#endif
