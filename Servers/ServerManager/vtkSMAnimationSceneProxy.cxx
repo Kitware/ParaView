b00d947f9e76b2bcb0044685cb3f1d23307174a9
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2005-05-03 10:51:57 -0400

    BUG: invalid loop -- forgot to increment counter in certain cases while saving geometry

--- Servers/ServerManager/vtkSMAnimationSceneProxy.cxx
+++ Servers/ServerManager/vtkSMAnimationSceneProxy.cxx
@@ -46,7 +46,7 @@
 # include <io.h> /* unlink */
 #endif
 
-vtkCxxRevisionMacro(vtkSMAnimationSceneProxy, "1.6");
+vtkCxxRevisionMacro(vtkSMAnimationSceneProxy, "1.7");
 vtkStandardNewMacro(vtkSMAnimationSceneProxy);
 
 //----------------------------------------------------------------------------
@@ -321,7 +321,7 @@ int vtkSMAnimationSceneProxy::SaveGeometry(const char* filename)
   vtkSMProxyIterator* proxyIter = vtkSMProxyIterator::New();
   proxyIter->SetMode(vtkSMProxyIterator::ONE_GROUP);
   proxyIter->Begin("displays");
-  while (!proxyIter->IsAtEnd())
+  for (; !proxyIter->IsAtEnd(); proxyIter->Next())
     {
     vtkSMSimpleDisplayProxy* sDisp = vtkSMSimpleDisplayProxy::SafeDownCast(
       proxyIter->GetProxy());
@@ -335,7 +335,6 @@ int vtkSMAnimationSceneProxy::SaveGeometry(const char* filename)
       {
       sDisp->SetInputAsGeometryFilter(animWriter);
       }
-    proxyIter->Next();
     }
   proxyIter->Delete();
 
