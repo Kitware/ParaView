fa601d496a542f3ea395ccc7467a9de4ce113bea
Sebastien Barre <sebastien.barre@kitware.com>
2005-05-26 11:14:18 -0400

    ENH: cleanup, remove useless #include

--- GUI/Widgets/vtkKWSaveImageDialog.cxx
+++ GUI/Widgets/vtkKWSaveImageDialog.cxx
@@ -16,12 +16,11 @@
 #include "vtkKWApplication.h"
 #include "vtkKWLoadSaveDialog.h"
 #include "vtkKWMessageDialog.h"
-#include "vtkKWWindow.h"
 #include "vtkObjectFactory.h"
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWSaveImageDialog );
-vtkCxxRevisionMacro(vtkKWSaveImageDialog, "1.26");
+vtkCxxRevisionMacro(vtkKWSaveImageDialog, "1.27");
 
 int vtkKWSaveImageDialogCommand(ClientData cd, Tcl_Interp *interp,
                                 int argc, char *argv[]);
