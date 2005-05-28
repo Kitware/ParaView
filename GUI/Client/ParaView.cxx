7e79d1e3e060b52800f62622c68d292a2c9ccaa5
Sebastien Barre <sebastien.barre@kitware.com>
2005-05-27 22:17:19 -0400

    ENH: add convenience function to convert Windows command line args into Unix argc/argv. Pulled and cleaned from PV/VV/VJ init code

--- GUI/Client/ParaView.cxx
+++ GUI/Client/ParaView.cxx
@@ -64,6 +64,8 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkSMInstantiator.h"
 #include "vtkParaViewInstantiator.h"
 #include "vtkClientServerInterpreter.h"
+
+#include <kwsys/SystemTools.hxx>
         
 static void ParaViewInitializeInterpreter(vtkProcessModule* pm);
 
@@ -100,104 +102,17 @@ int __stdcall WinMain(HINSTANCE vtkNotUsed(hInstance),
                       HINSTANCE vtkNotUsed(hPrevInstance),
                       LPSTR lpCmdLine, int vtkNotUsed(nShowCmd))
 {
-  int          argc;
-  int          retVal;
-  char**       argv;
-  unsigned int i;
-  int          j;
+  int argc;
+  char **argv;
+  kwsys::SystemTools::ConvertWindowsCommandLineToUnixArguments(
+    lpCmdLine, &argc, &argv);
 
-  // convert the windows sytle arguments into unix style argc argv
-  // parse a few of the command line arguments
-  // a space delimites an argument except when it is inside a quote
-  argc = 1;
-  int pos = 0;
-  for (i = 0; i < strlen(lpCmdLine); i++)
-    {
-    while (lpCmdLine[i] == ' ' && i < strlen(lpCmdLine))
-      {
-      i++;
-      }
-    if (lpCmdLine[i] == '\"')
-      {
-      i++;
-      while (lpCmdLine[i] != '\"' && i < strlen(lpCmdLine))
-        {
-        i++;
-        pos++;
-        }
-      argc++;
-      pos = 0;
-      }
-    else
-      {
-      while (lpCmdLine[i] != ' ' && i < strlen(lpCmdLine))
-        {
-        i++;
-        pos++;
-        }
-      argc++;
-      pos = 0;
-      }
-    }
+  int retVal = MyMain(argc, argv);
 
-  argv = (char**)malloc(sizeof(char*)* (argc+1));
+  for (int i = 0; i < argc; i++) { delete [] argv[i]; }
+  delete [] argv;
 
-  argv[0] = (char*)malloc(1024);
-  ::GetModuleFileName(0, argv[0],1024);
-
-  for(j=1; j<argc; j++)
-    {
-    argv[j] = (char*)malloc(strlen(lpCmdLine)+10);
-    }
-  argv[argc] = 0;
-
-  argc = 1;
-  pos = 0;
-  for (i = 0; i < strlen(lpCmdLine); i++)
-    {
-    while (lpCmdLine[i] == ' ' && i < strlen(lpCmdLine))
-      {
-      i++;
-      }
-    if (lpCmdLine[i] == '\"')
-      {
-      i++;
-      while (lpCmdLine[i] != '\"' && i < strlen(lpCmdLine))
-        {
-        argv[argc][pos] = lpCmdLine[i];
-        i++;
-        pos++;
-        }
-      argv[argc][pos] = '\0';
-      argc++;
-      pos = 0;
-      }
-    else
-      {
-      while (lpCmdLine[i] != ' ' && i < strlen(lpCmdLine))
-        {
-        argv[argc][pos] = lpCmdLine[i];
-        i++;
-        pos++;
-        }
-      argv[argc][pos] = '\0';
-      argc++;
-      pos = 0;
-      }
-    }
-  argv[argc] = 0;
-
-  // Initialize the processes and start the application.
-  retVal = MyMain(argc, argv);
-
-  // Delete arguments
-  for(j=0; j<argc; j++)
-    {
-    free(argv[j]);
-    }
-  free(argv);
-
-  return retVal;;
+  return retVal;
 }
 #else
 int main(int argc, char *argv[])
