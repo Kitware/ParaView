4522493b3ef766c4ad1d1f367f88f712cd3e9cda
Sebastien Barre <sebastien.barre@kitware.com>
2005-06-18 19:03:09 -0400

    ENH: add a tree widget

---
+++ GUI/Widgets/vtkKWTree.cxx
@@ -0,0 +1,334 @@
+/*=========================================================================
+
+Copyright (c) 1998-2003 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkKWApplication.h"
+#include "vtkKWTree.h"
+#include "vtkObjectFactory.h"
+#include "vtkKWTkUtilities.h"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro( vtkKWTree );
+vtkCxxRevisionMacro(vtkKWTree, "1.1");
+
+//----------------------------------------------------------------------------
+void vtkKWTree::Create(vtkKWApplication *app)
+{
+  // Use BWidget's Tree class:
+  // http://aspn.activestate.com/ASPN/docs/ActiveTcl/bwidget/Tree.html
+
+  // Call the superclass to create the widget and set the appropriate flags
+
+  if (!this->Superclass::CreateSpecificTkWidget(app, "Tree"))
+    {
+    vtkErrorMacro("Failed creating widget " << this->GetClassName());
+    return;
+    }
+
+  // Update enable state
+
+  this->UpdateEnableState();
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetSelectionToNode(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    this->Script("%s selection set %s", this->GetWidgetName(), node);
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::ClearSelection()
+{
+  if (this->IsCreated())
+    {
+    this->Script("%s selection clear", this->GetWidgetName());
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SeeNode(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    this->Script("%s see %s", this->GetWidgetName(), node);
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::OpenNode(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    this->Script("%s opentree %s 0", this->GetWidgetName(), node);
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::CloseNode(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    this->Script("%s closetree %s 0", this->GetWidgetName(), node);
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::OpenFirstNode()
+{
+  if (this->IsCreated())
+    {
+    this->Script("catch {%s opentree [lindex [%s nodes root] 0]}", 
+                 this->GetWidgetName(), this->GetWidgetName());
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::CloseFirstNode()
+{
+  if (this->IsCreated())
+    {
+    this->Script("catch {%s closetree [lindex [%s nodes root] 0]}", 
+                 this->GetWidgetName(), this->GetWidgetName());
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTree::HasNode(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    return atoi(this->Script("%s exists %s", this->GetWidgetName(), node));
+    }
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::DeleteAllNodes()
+{
+  if (this->IsCreated())
+    {
+    this->Script("%s delete [%s nodes root]", 
+                 this->GetWidgetName(), this->GetWidgetName());
+    }
+}
+
+//----------------------------------------------------------------------------
+const char* vtkKWTree::GetNodeChildren(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    return this->Script("%s nodes %s", this->GetWidgetName(), node);
+    }
+  return NULL;
+}
+
+//----------------------------------------------------------------------------
+const char* vtkKWTree::GetNodeParent(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    return this->Script("%s parent %s", this->GetWidgetName(), node);
+    }
+  return NULL;
+}
+
+//----------------------------------------------------------------------------
+const char* vtkKWTree::GetNodeUserData(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    return this->Script("%s itemcget %s -data", this->GetWidgetName(), node);
+    }
+  return NULL;
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTree::GetNodeSelectableFlag(const char *node)
+{
+  if (this->IsCreated() && node)
+    {
+    return atoi(this->Script("%s itemcget %s -selectable", 
+                             this->GetWidgetName(), node));
+    }
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetWidth(int width)
+{
+  this->SetConfigurationOptionAsInt("-width", width);
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTree::GetWidth()
+{
+  return this->GetConfigurationOptionAsInt("-width");
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetHeight(int height)
+{
+  this->SetConfigurationOptionAsInt("-height", height);
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTree::GetHeight()
+{
+  return this->GetConfigurationOptionAsInt("-height");
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetRedrawOnIdle(int redraw)
+{
+  this->SetConfigurationOptionAsInt("-redraw", redraw);
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTree::GetRedrawOnIdle()
+{
+  return this->GetConfigurationOptionAsInt("-redraw");
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetSelectionFill(int arg)
+{
+  this->SetConfigurationOptionAsInt("-selectfill", arg);
+}
+
+//----------------------------------------------------------------------------
+int vtkKWTree::GetSelectionFill()
+{
+  return this->GetConfigurationOptionAsInt("-selectfill");
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::GetSelectionBackgroundColor(double *r, double *g, double *b)
+{
+  vtkKWTkUtilities::GetOptionColor(this, "-selectbackground", r, g, b);
+}
+
+//----------------------------------------------------------------------------
+double* vtkKWTree::GetSelectionBackgroundColor()
+{
+  static double rgb[3];
+  this->GetSelectionBackgroundColor(rgb, rgb + 1, rgb + 2);
+  return rgb;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetSelectionBackgroundColor(double r, double g, double b)
+{
+  vtkKWTkUtilities::SetOptionColor(this, "-selectbackground", r, g, b);
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::GetSelectionForegroundColor(double *r, double *g, double *b)
+{
+  vtkKWTkUtilities::GetOptionColor(this, "-selectforeground", r, g, b);
+}
+
+//----------------------------------------------------------------------------
+double* vtkKWTree::GetSelectionForegroundColor()
+{
+  static double rgb[3];
+  this->GetSelectionForegroundColor(rgb, rgb + 1, rgb + 2);
+  return rgb;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetSelectionForegroundColor(double r, double g, double b)
+{
+  vtkKWTkUtilities::SetOptionColor(this, "-selectforeground", r, g, b);
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetOpenCommand(vtkKWObject *obj, const char *method)
+{
+  if (!this->IsCreated())
+    {
+    return;
+    }
+
+  char *command = NULL;
+  this->SetObjectMethodCommand(&command, obj, method);
+  this->SetConfigurationOption("-opencmd", command);
+  delete [] command;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetCloseCommand(vtkKWObject *obj, const char *method)
+{
+  if (!this->IsCreated())
+    {
+    return;
+    }
+
+  char *command = NULL;
+  this->SetObjectMethodCommand(&command, obj, method);
+  this->SetConfigurationOption("-closecmd", command);
+  delete [] command;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::SetBindText(const char *event, 
+                            vtkKWObject *obj, 
+                            const char *method)
+{
+  if (!this->IsCreated() || !event)
+    {
+    return;
+    }
+
+  char *command = NULL;
+  this->SetObjectMethodCommand(&command, obj, method);
+  this->Script("%s bindText %s {%s}", this->GetWidgetName(), event, command);
+  delete [] command;
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::UpdateEnableState()
+{
+  this->Superclass::UpdateEnableState();
+
+  this->SetStateOption(this->GetEnabled());
+}
+
+//----------------------------------------------------------------------------
+void vtkKWTree::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ GUI/Widgets/vtkKWTree.h
@@ -0,0 +1,172 @@
+/*=========================================================================
+
+Copyright (c) 1998-2003 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkKWTree - tree widget
+// .SECTION Description
+// A simple tree widget
+
+#ifndef __vtkKWTree_h
+#define __vtkKWTree_h
+
+#include "vtkKWCoreWidget.h"
+
+class KWWIDGETS_EXPORT vtkKWTree : public vtkKWCoreWidget
+{
+public:
+  static vtkKWTree* New();
+  vtkTypeRevisionMacro(vtkKWTree,vtkKWCoreWidget);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Create the widget.
+  virtual void Create(vtkKWApplication *app);
+
+  // Description:
+  // Set the selection to node
+  virtual void SetSelectionToNode(const char *node);
+
+  // Description:
+  // Clear the selection
+  virtual void ClearSelection();
+
+  // Description:
+  // Arrange the tree to see a given node
+  virtual void SeeNode(const char *node);
+
+  // Description:
+  // Open/close a node.
+  virtual void OpenNode(const char *node);
+  virtual void CloseNode(const char *node);
+
+  // Description:
+  // Open/close the first node of the tree.
+  virtual void OpenFirstNode();
+  virtual void CloseFirstNode();
+
+  // Description:
+  // Query if given node exists in the tree
+  virtual int HasNode(const char *node);
+
+  // Description:
+  // Delete all nodes
+  virtual void DeleteAllNodes();
+
+  // Description:
+  // Get node's children as a space separated list of nodes
+  virtual const char* GetNodeChildren(const char *node);
+
+  // Description:
+  // Get node's parent
+  virtual const char* GetNodeParent(const char *node);
+
+  // Description:
+  // Get the node user data and if it is selectable or not
+  virtual const char* GetNodeUserData(const char *node);
+  virtual int GetNodeSelectableFlag(const char *node);
+
+  // Description:
+  // Set/Get the width/height.
+  virtual void SetWidth(int);
+  virtual int GetWidth();
+  virtual void SetHeight(int);
+  virtual int GetHeight();
+
+  // Description:
+  // Specifies wether or not the tree should be redrawn when entering idle. 
+  // Set it to false if you call update while modifying the tree
+  vtkBooleanMacro(RedrawOnIdle, int);
+  virtual void SetRedrawOnIdle(int);
+  virtual int GetRedrawOnIdle();
+
+  // Description:
+  // If true, the selection box will be drawn across the entire tree from
+  // left-to-right instead of just around the item text.
+  vtkBooleanMacro(SelectionFill, int);
+  virtual void SetSelectionFill(int);
+  virtual int GetSelectionFill();
+
+  // Description:
+  // Set/Get the selection foreground and background color
+  virtual void GetSelectionBackgroundColor(double *r, double *g, double *b);
+  virtual double* GetSelectionBackgroundColor();
+  virtual void SetSelectionBackgroundColor(double r, double g, double b);
+  virtual void SetSelectionBackgroundColor(double rgb[3])
+    { this->SetSelectionBackgroundColor(rgb[0], rgb[1], rgb[2]); };
+  virtual void GetSelectionForegroundColor(double *r, double *g, double *b);
+  virtual double* GetSelectionForegroundColor();
+  virtual void SetSelectionForegroundColor(double r, double g, double b);
+  virtual void SetSelectionForegroundColor(double rgb[3])
+    { this->SetSelectionForegroundColor(rgb[0], rgb[1], rgb[2]); };
+  
+  // Description:
+  // Set the method to be called when the user opens a node. The path of the
+  // opened node is appended to the command.
+  virtual void SetOpenCommand(vtkKWObject *obj, const char *method);
+
+  // Description:
+  // Set the method to be called when the user closes a node. The path of the
+  // closed node is appended to the command.
+  virtual void SetCloseCommand(vtkKWObject *obj, const char *method);
+
+  // Description:
+  // Associates a object/method to execute whenever the event sequence given 
+  // by 'event' occurs on the label of a node. The node idenfier on which
+  // the event occurs is appended to the command.
+  virtual void SetBindText(
+    const char *event, vtkKWObject *obj, const char *method);
+
+  // Description:
+  // Update the "enable" state of the object and its internal parts.
+  // Depending on different Ivars (this->Enabled, the application's 
+  // Limited Edition Mode, etc.), the "enable" state of the object is updated
+  // and propagated to its internal parts/subwidgets. This will, for example,
+  // enable/disable parts of the widget UI, enable/disable the visibility
+  // of 3D widgets, etc.
+  virtual void UpdateEnableState();
+
+protected:
+  vtkKWTree() {};
+  ~vtkKWTree() {};
+
+private:
+  vtkKWTree(const vtkKWTree&); // Not implemented
+  void operator=(const vtkKWTree&); // Not implemented
+};
+
+
+#endif
+
+
+
