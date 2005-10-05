a90b1cafe040df52b97d1ba9da6eef9ebc875fe7
Clinton Stimpson <clinton@elemtech.com>
2005-10-05 15:45:01 -0400

    STYLE:   Add copyright.

--- Client/pqRenderViewProxy.h
+++ Client/pqRenderViewProxy.h
@@ -1,6 +1,14 @@
+/*
+ * Copyright 2004 Sandia Corporation.
+ * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+ * license for use of this work by or on behalf of the
+ * U.S. Government. Redistribution and use in source and binary forms, with
+ * or without modification, are permitted provided that this Notice and any
+ * statement of authorship are reproduced on all copies.
+ */
 
-#ifndef VTK_PC_RENDERVIEWPROXY_H
-#define VTK_PC_RENDERVIEWPROXY_H
+#ifndef _pqRenderViewProxy_h
+#define _pqRenderViewProxy_h
 
 #include "vtkPVRenderViewProxy.h"
 
