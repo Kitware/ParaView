122383df0b6e51dd69fac0a7a4b84d620f8f62f7
Ken Moreland <kmorel@sandia.gov>
2005-10-11 09:33:46 -0400

    STYLE: Was printing the wrong function name in an error message.

--- Servers/Filters/vtkIceTRenderManager.cxx
+++ Servers/Filters/vtkIceTRenderManager.cxx
@@ -90,7 +90,7 @@ static void vtkIceTRenderManagerReconstructWindowImage(vtkObject *,
 // vtkIceTRenderManager implementation.
 //******************************************************************
 
-vtkCxxRevisionMacro(vtkIceTRenderManager, "1.28");
+vtkCxxRevisionMacro(vtkIceTRenderManager, "1.29");
 vtkStandardNewMacro(vtkIceTRenderManager);
 
 vtkCxxSetObjectMacro(vtkIceTRenderManager, TileViewportTransform,
@@ -577,7 +577,7 @@ void vtkIceTRenderManager::SetSortingKdTree(vtkPKdTree *tree)
 
   if (!this->RenderWindow)
     {
-    vtkErrorMacro("Must set the render window and its renderers before calling SetComposeOperation.");
+    vtkErrorMacro("Must set the render window and its renderers before calling SetSortingKdTree.");
     return;
     }
 
