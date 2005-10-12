694cf7f79ca33b9d0b34965d8cd5e8e49282d8dd
Berk Geveci <berk.geveci@kitware.com>
2005-10-12 11:51:57 -0400

    BUG: Movie writers need an input to initialize

--- Servers/ServerManager/vtkSMAnimationSceneProxy.cxx
+++ Servers/ServerManager/vtkSMAnimationSceneProxy.cxx
@@ -46,7 +46,7 @@
 # include <io.h> /* unlink */
 #endif
 
-vtkCxxRevisionMacro(vtkSMAnimationSceneProxy, "1.15");
+vtkCxxRevisionMacro(vtkSMAnimationSceneProxy, "1.16");
 vtkStandardNewMacro(vtkSMAnimationSceneProxy);
 
 //----------------------------------------------------------------------------
@@ -239,11 +239,15 @@ int vtkSMAnimationSceneProxy::SaveImages(const char* fileRoot,
   this->SaveFailed = 0;
   if (this->MovieWriter)
     {
+    vtkImageData* capture = 
+      this->RenderModuleProxy->CaptureWindow(this->Magnification);
     ostrstream str;
     str << fileRoot << "." << ext << ends;
     this->MovieWriter->SetFileName(str.str());
     str.rdbuf()->freeze(0);
+    this->MovieWriter->SetInput(capture);
     this->MovieWriter->Start();
+    capture->Delete();
     }
 
   // Play the animation.
