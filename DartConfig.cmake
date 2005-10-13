3f9bc87ce96697b4ed167385666309c82da075b9
Andy Cedilnik <andy.cedilnik@kitware.com>
2005-10-13 08:57:10 -0400

    ENH: Initial import of testing

---
+++ DartConfig.cmake
@@ -0,0 +1,17 @@
+INCLUDE(${CMAKE_CURRENT_SOURCE_DIR}/CTestConfig.cmake)
+
+# Dashboard is opened for submissions for a 24 hour period starting at
+# the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
+SET (NIGHTLY_START_TIME "${CTEST_NIGHTLY_START_TIME}")
+
+
+# Dart server to submit results (used by client)
+SET(DROP_METHOD http)
+IF(DROP_METHOD MATCHES http)
+  IF (NOT DEFINED DROP_SITE)
+    SET (DROP_SITE "${CTEST_DROP_SITE}")
+    SET (DROP_LOCATION "${CTEST_DROP_LOCATION}")
+  ENDIF (NOT DEFINED DROP_SITE)
+ENDIF(DROP_METHOD MATCHES http)
+
+SET (TRIGGER_SITE "${CTEST_TRIGGER_SITE}")
