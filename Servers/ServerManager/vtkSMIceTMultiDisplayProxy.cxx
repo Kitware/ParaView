4742bcd4a211795441d0dfe1a125d81ecfc8b17b
Ken Moreland <kmorel@sandia.gov>
2005-10-13 14:14:18 -0400

    BUG: Some objects need to be created before Input is set.

--- Servers/ServerManager/vtkSMIceTMultiDisplayProxy.cxx
+++ Servers/ServerManager/vtkSMIceTMultiDisplayProxy.cxx
@@ -22,7 +22,7 @@
 #include "vtkSMIntVectorProperty.h"
 #include "vtkSMProxy.h"
 
-vtkCxxRevisionMacro(vtkSMIceTMultiDisplayProxy, "1.1");
+vtkCxxRevisionMacro(vtkSMIceTMultiDisplayProxy, "1.2");
 vtkStandardNewMacro(vtkSMIceTMultiDisplayProxy);
 
 //-----------------------------------------------------------------------------
@@ -65,7 +65,7 @@ void vtkSMIceTMultiDisplayProxy::PrintSelf(ostream &os, vtkIndent indent)
 
 void vtkSMIceTMultiDisplayProxy::CreateVTKObjects(int numObjects)
 {
-  if (this->ObjectsCreated || !this->CanCreateProxy)
+  if (this->ObjectsCreated)
     {
     return;
     }
