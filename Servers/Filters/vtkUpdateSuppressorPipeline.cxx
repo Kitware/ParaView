1637d564d0aa78b5cdd9a2f9da1eb5a276302723
Brad King <brad.king@kitware.com>
2005-10-24 16:41:41 -0400

    ENH: Removed forward argument from signature of ProcessRequest method.  This partially addresses bug#2402.

--- Servers/Filters/vtkUpdateSuppressorPipeline.cxx
+++ Servers/Filters/vtkUpdateSuppressorPipeline.cxx
@@ -18,7 +18,7 @@
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkUpdateSuppressorPipeline, "1.4");
+vtkCxxRevisionMacro(vtkUpdateSuppressorPipeline, "1.5");
 vtkStandardNewMacro(vtkUpdateSuppressorPipeline);
 
 
@@ -40,7 +40,6 @@ void vtkUpdateSuppressorPipeline::PrintSelf(ostream& os, vtkIndent indent)
 
 //----------------------------------------------------------------------------
 int vtkUpdateSuppressorPipeline::ProcessRequest(vtkInformation* request,
-                                                int forward,
                                                 vtkInformationVector** inInfo,
                                                 vtkInformationVector* outInfo)
 {
@@ -57,8 +56,7 @@ int vtkUpdateSuppressorPipeline::ProcessRequest(vtkInformation* request,
       }
     return 1;
     }
-  return this->Superclass::ProcessRequest(request,forward,
-                                          inInfo,outInfo);
+  return this->Superclass::ProcessRequest(request, inInfo, outInfo);
 }
 
 
