e276c4bcfe2ecbfc5fd7eea5e01d813b66a1cc2c
Clinton Stimpson <clinton@elemtech.com>
2005-11-17 17:43:41 -0500

    STYLE: Add comments.

--- Qt/Widgets/pqMultiViewFrame.h
+++ Qt/Widgets/pqMultiViewFrame.h
@@ -5,38 +5,57 @@
 #include <QWidget>
 #include "ui_pqMultiViewFrameMenu.h"
 
+/// a holder for a widget in a multiview
 class pqMultiViewFrame : public QWidget
 {
   Q_OBJECT
-  Q_PROPERTY(bool autoHide READ autoHide WRITE setAutoHide)
+  Q_PROPERTY(bool menuAutoHide READ menuAutoHide WRITE setMenuAutoHide)
   Q_PROPERTY(bool active WRITE setActive READ active)
-  Q_PROPERTY(QColor color WRITE setColor READ color)
+  Q_PROPERTY(QColor borderColor WRITE setBorderColor READ borderColor)
 public:
   pqMultiViewFrame(QWidget* parent = NULL);
   ~pqMultiViewFrame();
 
-  bool autoHide() const;
-  void setAutoHide(bool);
+  /// whether the menu is auto hidden
+  bool menuAutoHide() const;
+  /// whether the menu is auto hidden
+  void setMenuAutoHide(bool);
 
+  /// set the main widget for this holder
   void setMainWidget(QWidget*);
+  /// get the main widget for this holder
   QWidget* mainWidget();
   
+  /// get whether active, if active, a border is drawn
   bool active() const;
-  QColor color() const;
+  /// get the color of the border
+  QColor borderColor() const;
 
 public slots:
+
+  /// close this frame, emits closePressed() so receiver does the actual remove
   void close();
+  /// maximize this frame, emits closePressed() so receiver does the actual maximize
   void maximize();
+  /// split this frame vertically, emits splitVerticalPressed so receiver does the actual split
   void splitVertical();
+  /// split this frame horizontally, emits splitVerticalPressed so receiver does the actual split
   void splitHorizontal();
-  void setColor(QColor);
+  /// sets the border color
+  void setBorderColor(QColor);
+  /// sets whether this frame is active.  if active, a border is drawn
   void setActive(bool);
 
 signals:
+  /// signal active state changed
   void activeChanged(bool);
+  /// signal close pressed
   void closePressed();
+  /// signal maximize pressed
   void maximizePressed();
+  /// signal split vertical pressed
   void splitVerticalPressed();
+  /// signal split horizontal pressed
   void splitHorizontalPressed();
 
 protected:
