9e4cb58c19cf21019b5cd410fbb2b0da0219ec35
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-11-21 16:27:23 -0500

    BUG: backdate changes

--- ParaView/GUI/Widgets/vtkKWCoreWidget.cxx
+++ ParaView/GUI/Widgets/vtkKWCoreWidget.cxx
@@ -21,7 +21,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWCoreWidget );
-vtkCxxRevisionMacro(vtkKWCoreWidget, "1.11");
+vtkCxxRevisionMacro(vtkKWCoreWidget, "1.12");
 
 //----------------------------------------------------------------------------
 void vtkKWCoreWidget::Create(vtkKWApplication *app)
@@ -375,7 +375,7 @@ const char* vtkKWCoreWidget::ConvertTclStringToInternalString(
 //----------------------------------------------------------------------------
 void vtkKWCoreWidget::SetTextOption(const char *option, const char *value)
 {
-  if (!option || !this->IsCreated() || value==NULL || strlen(value)==0)
+  if (!option || !this->IsCreated())
     {
     return;
     }
--- ParaView/GUI/Widgets/vtkKWText.cxx
+++ ParaView/GUI/Widgets/vtkKWText.cxx
@@ -34,7 +34,7 @@ const char *vtkKWText::TagFgDarkGreen = "_fg_dark_green_tag_";
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkKWText);
-vtkCxxRevisionMacro(vtkKWText, "1.44");
+vtkCxxRevisionMacro(vtkKWText, "1.45");
 
 //----------------------------------------------------------------------------
 class vtkKWTextInternals
@@ -266,10 +266,6 @@ void vtkKWText::AppendTextInternal(const char *s, const char *tag)
   // Don't check for this->Created() for speed, since it is called
   // by AppendText which does the check already
 
-  if( strlen(s) == 0 ) 
-    {
-    return;
-    }
   const char *val = this->ConvertInternalStringToTclString(
     s, vtkKWCoreWidget::ConvertStringEscapeInterpretable);
 
