27bef4bbd227bba25f03ad3d4e0aafc619a52ee8
Sebastien Barre <sebastien.barre@kitware.com>
2005-11-28 13:17:02 -0500

    ENH: add optional label (useful to display patient name in medical data, which filename can be very long/obscure), and option to separate path from filename

--- ParaView/GUI/Widgets/vtkKWMostRecentFilesManager.h
+++ ParaView/GUI/Widgets/vtkKWMostRecentFilesManager.h
@@ -42,7 +42,7 @@ public:
   vtkTypeRevisionMacro(vtkKWMostRecentFilesManager,vtkKWObject);
   void PrintSelf(ostream& os, vtkIndent indent);
 
-  // Description::
+  // Description:
   // Add a most recent file to the list.
   // Each most recent file is associated to a target object and a target 
   // command: when a most recent file is invoked (either programmatically or
@@ -50,10 +50,15 @@ public:
   // invoked on the associated target object. If one and/or the other was not
   // specified when the most recent file was added, the default target
   // object and/or default target command are used.
+  // An optional label can be specified too.
+  // This label will be used in the menu if LabelVisibilityInMenu is true.
+  // The entry is always added at the beginning of the list (thus, becoming
+  // the most recently used).
   virtual void AddFile(
     const char *filename, 
     vtkObject *target_object = NULL, 
-    const char *target_command = NULL);
+    const char *target_command = NULL,
+    const char *label = NULL);
   
   // Description:
   // Set/Get the default target object and command.
@@ -71,7 +76,7 @@ public:
   // Description:
   // Load/Save up to 'max_nb' most recent files from/to the registry under
   // the application's 'reg_key' key.
-  // The parameter-less methods use DefaultRegistryKey as 'reg_key' and
+  // The parameter-less methods use RegistryKey as 'reg_key' and
   // MaximumNumberOfFilesInRegistry as 'max_nb'.
   // Only the filename and target command are saved. When entries are loaded
   // make sure DefaultTargetObject is set to a valid object.
@@ -109,6 +114,23 @@ public:
   virtual void SetMaximumNumberOfFilesInMenu(int);
 
   // Description:
+  // Set a label for a specific file entry.
+  // This label will be used in the menu if LabelVisibilityInMenu is true.
+  virtual void SetFileLabel(const char *filename, const char *label);
+
+  // Description:
+  // Set/Get the label visibility in menu (Off by default).
+  virtual void SetLabelVisibilityInMenu(int);
+  vtkBooleanMacro(LabelVisibilityInMenu, int);
+  vtkGetMacro(LabelVisibilityInMenu, int);
+
+  // Description:
+  // Separate path from basename in in menu (Off by default).
+  virtual void SetSeparatePathInMenu(int);
+  vtkBooleanMacro(SeparatePathInMenu, int);
+  vtkGetMacro(SeparatePathInMenu, int);
+
+  // Description:
   // Disable/Enable the most recent files menu if it is a cascade in its
   // parent. If the menu is empty, it will be disabled.
   virtual void UpdateMenuStateInParent();
@@ -129,6 +151,8 @@ protected:
   char        *RegistryKey;
   int         MaximumNumberOfFilesInRegistry;
   int         MaximumNumberOfFilesInMenu;
+  int         LabelVisibilityInMenu;
+  int         SeparatePathInMenu;
 
   //BTX
 
@@ -139,15 +163,16 @@ protected:
 
   //ETX
 
-  // Description::
+  // Description:
   // Add a most recent file to the list (internal, do not update the menu
   // or save to the registry).
   virtual void AddFileInternal(
     const char *filename, 
     vtkObject *target_object = NULL, 
-    const char *target_command = NULL);
+    const char *target_command = NULL,
+    const char *label = NULL);
 
-  // Description::
+  // Description:
   // Update the most recent files menu
   virtual void UpdateMenu();
 
