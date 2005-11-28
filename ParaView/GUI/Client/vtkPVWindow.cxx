d2377f28681a4527adbe593ca46422c9622d8aeb
Andy Cedilnik <andy.cedilnik@kitware.com>
2005-11-28 15:17:25 -0500

    BUG: Fix crash when saying cancel to Xdmf reader

--- ParaView/GUI/Client/vtkPVWindow.cxx
+++ ParaView/GUI/Client/vtkPVWindow.cxx
@@ -136,7 +136,7 @@
 
 //-----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVWindow);
-vtkCxxRevisionMacro(vtkPVWindow, "1.773");
+vtkCxxRevisionMacro(vtkPVWindow, "1.774");
 
 const char* vtkPVWindow::ComparativeVisMenuLabel = "Comparative Vis Manager";
 
@@ -2436,12 +2436,12 @@ int vtkPVWindow::OpenWithReader(const char *fileName,
     }
   int retVal;
   retVal = this->ReadFileInformation(clone, fileName);
-  clone->GrabFocus();
-  this->UpdateEnableState();
   if (retVal != VTK_OK)
     {
     return retVal;
     }
+  clone->GrabFocus();
+  this->UpdateEnableState();
   int timesteps = clone->GetNumberOfTimeSteps(); 
   if (timesteps > 0)
     {
