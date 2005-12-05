cce0faa0398c3a918bf0975183b1df21bd936a8c
Amy Squillacote <amy.squillacote@kitware.com>
2005-12-05 11:43:10 -0500

    BUG: fixing PrintSelf defect

--- Servers/Common/vtkProcessModuleConnection.cxx
+++ Servers/Common/vtkProcessModuleConnection.cxx
@@ -57,7 +57,7 @@ protected:
 
 };
 
-vtkCxxRevisionMacro(vtkProcessModuleConnection, "1.3");
+vtkCxxRevisionMacro(vtkProcessModuleConnection, "1.4");
 //-----------------------------------------------------------------------------
 vtkProcessModuleConnection::vtkProcessModuleConnection()
 {
@@ -322,4 +322,5 @@ void vtkProcessModuleConnection::PrintSelf(ostream& os, vtkIndent indent)
     {
     os << "(none)" << endl;
     }
+  os << indent << "SelfID: " << SelfID << endl;
 }
