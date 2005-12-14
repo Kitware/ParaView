0959a106b58218d3bf00c5585cf0f0b52528307e
Mark Richardson <mark@elemtech.com>
2005-12-14 18:00:36 -0500

    COMP: Fixing windows build for exodus open dialog.

--- Qt/Widgets/pqOpenExodusOptions.h
+++ Qt/Widgets/pqOpenExodusOptions.h
@@ -2,12 +2,14 @@
 #ifndef _pqOpenExodusOptions_h
 #define _pqOpenExodusOptions_h
 
+#include "QtWidgetsExport.h"
 #include <QDialog>
 #include "ui_pqOpenExodusOptions.h"
 
 class vtkSMSourceProxy;
 
-class pqOpenExodusOptions : public QDialog, public Ui::pqOpenExodusOptions
+class QTWIDGETS_EXPORT pqOpenExodusOptions : public QDialog,
+    public Ui::pqOpenExodusOptions
 {
   Q_OBJECT
 public:
