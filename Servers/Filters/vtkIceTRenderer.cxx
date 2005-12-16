b121a03251749b475ddf3d5a8d23fe3a0de2b383
Ken Moreland <kmorel@sandia.gov>
2005-12-16 10:42:41 -0500

    BUG: Only do ordered compositing if the SortingKdTree has enough regions to order the processes.

--- Servers/Filters/vtkIceTRenderer.cxx
+++ Servers/Filters/vtkIceTRenderer.cxx
@@ -46,7 +46,7 @@ static vtkIceTRenderer *currentRenderer;
 // vtkIceTRenderer implementation.
 //******************************************************************
 
-vtkCxxRevisionMacro(vtkIceTRenderer, "1.19");
+vtkCxxRevisionMacro(vtkIceTRenderer, "1.20");
 vtkStandardNewMacro(vtkIceTRenderer);
 
 vtkCxxSetObjectMacro(vtkIceTRenderer, SortingKdTree, vtkPKdTree);
@@ -234,8 +234,10 @@ void vtkIceTRenderer::DeviceRender()
     }
 
   //Set up ordered compositing.
-  if (   this->SortingKdTree
-      && (this->ComposeOperation == vtkIceTRenderManager::ComposeOperationOver))
+  if (   (this->ComposeOperation == vtkIceTRenderManager::ComposeOperationOver)
+      && this->SortingKdTree
+      && (   this->SortingKdTree->GetNumberOfRegions()
+          >= this->Context->GetController()->GetNumberOfProcesses()) )
     {
     // Setup ICE-T context for correct sorting.
     icetEnable(ICET_ORDERED_COMPOSITE);
