90ea0b07a7e4e83cfaf178ddc9ba46a58d03000a
Sebastien Barre <sebastien.barre@kitware.com>
2006-01-12 16:21:54 -0500

    ENH: callbacks example

---
+++ ParaView/GUI/Widgets/Examples/Cxx/Callbacks/KWCallbackExample.cxx
@@ -0,0 +1,115 @@
+#include "vtkKWMyApplication.h"
+#include "vtkKWWindow.h"
+#include "vtkKWMyWidget.h"
+
+#include <vtksys/SystemTools.hxx>
+#include <vtksys/CommandLineArguments.hxx>
+
+extern "C" int Kwmylib_Init(Tcl_Interp *interp);
+int my_main(int argc, char *argv[])
+{
+  // Initialize Tcl
+
+  Tcl_Interp *interp = vtkKWApplication::InitializeTcl(argc, argv, &cerr);
+  if (!interp)
+    {
+    cerr << "Error: InitializeTcl failed" << endl ;
+    return 1;
+    }
+
+  // Initialize our Tcl library (i.e. our classes wrapped in Tcl)
+
+  Kwmylib_Init(interp);
+
+  // Process some command-line arguments
+  // The --test option here is used to run this example as a non-interactive 
+  // test for software quality purposes. You can ignore it.
+
+  int option_test = 0;
+  vtksys::CommandLineArguments args;
+  args.Initialize(argc, argv);
+  args.AddArgument(
+    "--test", vtksys::CommandLineArguments::NO_ARGUMENT, &option_test, "");
+  args.Parse();
+  
+  // Create the application
+  // If --test was provided, ignore all registry settings, and exit silently
+  // Restore the settings that have been saved to the registry, like
+  // the geometry of the user interface so far.
+
+  vtkKWMyApplication *app = vtkKWMyApplication::New();
+  app->SetName("KWCallbackExample");
+  if (option_test)
+    {
+    app->SetRegistryLevel(0);
+    app->PromptBeforeExitOff();
+    }
+  app->RestoreApplicationSettingsFromRegistry();
+
+  // Set a help link. Can be a remote link (URL), or a local file
+
+  app->SetHelpDialogStartingPage("http://www.kwwidgets.org");
+
+  // Add a window
+  // Set 'SupportHelp' to automatically add a menu entry for the help link
+
+  vtkKWWindow *win = vtkKWWindow::New();
+  win->SupportHelpOn();
+  app->AddWindow(win);
+  win->Create();
+  win->SecondaryPanelVisibilityOff();
+  win->MainPanelVisibilityOff();
+
+  // Add our widget
+  // It automatically sync with our app's MyValue variable
+  
+  vtkKWMyWidget *my_widget = vtkKWMyWidget::New();
+  my_widget->SetParent(win->GetViewFrame());
+  my_widget->Create();
+
+  app->Script("pack %s -side left -anchor c -expand y", 
+              my_widget->GetWidgetName());
+
+  app->SetMyValue(10.0);
+
+  // Start the application
+  // If --test was provided, do not enter the event loop and run this example
+  // as a non-interactive test for software quality purposes.
+
+  int ret = 0;
+  win->Display();
+  if (!option_test)
+    {
+    app->Start(argc, argv);
+    ret = app->GetExitStatus();
+    }
+  win->Close();
+
+  // Deallocate and exit
+
+  my_widget->Delete();
+  win->Delete();
+  app->Delete();
+  
+  return ret;
+}
+
+#ifdef _WIN32
+#include <windows.h>
+int __stdcall WinMain(HINSTANCE, HINSTANCE, LPSTR lpCmdLine, int)
+{
+  int argc;
+  char **argv;
+  vtksys::SystemTools::ConvertWindowsCommandLineToUnixArguments(
+    lpCmdLine, &argc, &argv);
+  int ret = my_main(argc, argv);
+  for (int i = 0; i < argc; i++) { delete [] argv[i]; }
+  delete [] argv;
+  return ret;
+}
+#else
+int main(int argc, char *argv[])
+{
+  return my_main(argc, argv);
+}
+#endif
