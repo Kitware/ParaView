86336219b9869ca3ecc840eff25a36f760c425fb
Sebastien Barre <sebastien.barre@kitware.com>
2006-02-06 18:43:29 -0500

    ENH: the non-Tcl-wrapped VTK in PV and VV will drive me nuts until I die.

--- GUI/Widgets/Utilities/CMake/KWWidgetsWrappingMacros.cmake
+++ GUI/Widgets/Utilities/CMake/KWWidgetsWrappingMacros.cmake
@@ -1,29 +1,29 @@
 MACRO(KWWidgets_WRAP_TCL target src_list_name sources commands)
 
-  IF(VTK_MAJOR_VERSION LESS 5)
-
+  IF(VTK_MAJOR_VERSION AND VTK_MAJOR_VERSION LESS 5)
+      
     VTK_WRAP_TCL2(
       ${target} 
       SOURCES ${src_list_name} "${sources}" 
       COMMANDS "${commands}")
-
-  ELSE(VTK_MAJOR_VERSION LESS 5)
-
+    
+  ELSE(VTK_MAJOR_VERSION AND VTK_MAJOR_VERSION LESS 5)
+    
     IF(NOT VTK_CMAKE_DIR)
       SET(VTK_CMAKE_DIR "${VTK_SOURCE_DIR}/CMake")
     ENDIF(NOT VTK_CMAKE_DIR)
     INCLUDE("${VTK_CMAKE_DIR}/vtkWrapTcl.cmake")
-
+    
     IF("${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}" EQUAL "5.0")
       SET(VTK_WRAP_TCL3_INIT_DIR "${VTK_SOURCE_DIR}/Wrapping")
     ENDIF("${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}" EQUAL "5.0")
-
+    
     VTK_WRAP_TCL3(
       ${target}
       ${src_list_name} "${sources}" 
       "${commands}" 
       ${ARGN})
-
-  ENDIF(VTK_MAJOR_VERSION LESS 5)
+    
+  ENDIF(VTK_MAJOR_VERSION AND VTK_MAJOR_VERSION LESS 5)
 
 ENDMACRO(KWWidgets_WRAP_TCL)
