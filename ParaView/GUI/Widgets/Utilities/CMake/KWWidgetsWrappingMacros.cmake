55ede7cd99d9d3c3045e4189644159187d31ed3b
Sebastien Barre <sebastien.barre@kitware.com>
2006-02-06 19:09:04 -0500

    ENH: create some macros to make wrapping more portable, and simplify some tests

--- ParaView/GUI/Widgets/Utilities/CMake/KWWidgetsWrappingMacros.cmake
+++ ParaView/GUI/Widgets/Utilities/CMake/KWWidgetsWrappingMacros.cmake
@@ -2,10 +2,14 @@ MACRO(KWWidgets_WRAP_TCL target src_list_name sources commands)
 
   IF(VTK_MAJOR_VERSION AND VTK_MAJOR_VERSION LESS 5)
       
+    # Attempt at supporting 4.4. 
+    # This won't work anyway, as the lexer in VTK 4.x was updated later on
+    # to support export macros not starting with VTK_ (say, KWWidgets_Export).
+
     VTK_WRAP_TCL2(
-      ${target} 
-      SOURCES ${src_list_name} "${sources}" 
-      COMMANDS "${commands}")
+      ${target}
+      SOURCES ${src_list_name} ${sources}
+      COMMANDS ${commands})
     
   ELSE(VTK_MAJOR_VERSION AND VTK_MAJOR_VERSION LESS 5)
     
