cea61dd5ccee68b5212c874ebd8264a080f8ace8
Sebastien Barre <sebastien.barre@kitware.com>
2006-02-15 18:21:56 -0500

    ENH: more dash stuff

--- GUI/Widgets/CTestConfig.cmake
+++ GUI/Widgets/CTestConfig.cmake
@@ -1,3 +1,7 @@
 SET(CTEST_PROJECT_NAME "KWWidgets")
 SET(CTEST_NIGHTLY_START_TIME "21:30:00 EDT")
 SET(CTEST_DART_SERVER_VERSION "2")
+SET(CTEST_DROP_METHOD "http")
+SET(CTEST_DROP_SITE "public.kitware.com")
+SET(CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
+SET(CTEST_TRIGGER_SITE "http://${DROP_SITE}/cgi-bin/Submit-KWWidgets-TestingResults.pl")
