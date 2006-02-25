11ec5f95c85fc49c674a74633342e0595491c2ad
Sebastien Barre <sebastien.barre@kitware.com>
2006-02-24 20:36:56 -0500

    ENH: use launchers for all the tests, to avoid problem with environment variables

--- ParaView/GUI/Widgets/Utilities/CMake/KWWidgetsTestingMacros.cmake
+++ ParaView/GUI/Widgets/Utilities/CMake/KWWidgetsTestingMacros.cmake
@@ -1,4 +1,90 @@
 # ---------------------------------------------------------------------------
+# KWWidgets_ADD_TEST_FROM_C_EXAMPLE
+# Add specific distribution-related C test
+
+MACRO(KWWidgets_ADD_TEST_FROM_C_EXAMPLE 
+    test_name
+    exe_name)
+
+  ADD_TEST(${test_name} ${EXECUTABLE_OUTPUT_PATH}/${exe_name} --test)
+
+  IF(KWWidgets_SOURCE_DIR)
+    KWWidgets_ADD_OUT_OF_SOURCE_TEST(
+      ${test_name}OutOfSource
+      ${PROJECT_NAME}
+      "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}OutOfSource"
+      ${exe_name} --test)
+  ENDIF(KWWidgets_SOURCE_DIR)
+
+ENDMACRO(KWWidgets_ADD_TEST_FROM_C_EXAMPLE)
+
+# ---------------------------------------------------------------------------
+# KWWidgets_ADD_TEST_WITH_LAUNCHER
+# Add specific distribution-related C test
+
+MACRO(KWWidgets_ADD_TEST_WITH_LAUNCHER
+    test_name
+    exe_name)
+
+  INCLUDE("${KWWidgets_CMAKE_DIR}/KWWidgetsPathsMacros.cmake")
+  SET(LAUNCHER_EXE_NAME "${exe_name}Launcher")
+  KWWidgets_GENERATE_SETUP_PATHS_LAUNCHER(
+    "${CMAKE_CURRENT_BINARY_DIR}" "${LAUNCHER_EXE_NAME}" 
+    "${EXECUTABLE_OUTPUT_PATH}" "${exe_name}")
+
+  ADD_TEST(${test_name} ${EXECUTABLE_OUTPUT_PATH}/${LAUNCHER_EXE_NAME})
+
+ENDMACRO(KWWidgets_ADD_TEST_WITH_LAUNCHER)
+
+# ---------------------------------------------------------------------------
+# KWWidgets_ADD_TEST_FROM_TCL_EXAMPLE
+# Add specific distribution-related Tcl test
+
+MACRO(KWWidgets_ADD_TEST_FROM_TCL_EXAMPLE 
+    test_name
+    script_name)
+
+  IF(KWWidgets_BUILD_SHARED_LIBS AND VTK_WRAP_TCL AND TCL_TCLSH)
+
+    INCLUDE("${KWWidgets_CMAKE_DIR}/KWWidgetsPathsMacros.cmake")
+    GET_FILENAME_COMPONENT(name_we "${script_name}" NAME_WE)
+    SET(LAUNCHER_EXE_NAME "${name_we}TclLauncher")
+    KWWidgets_GENERATE_SETUP_PATHS_LAUNCHER(
+      "${CMAKE_CURRENT_BINARY_DIR}" "${LAUNCHER_EXE_NAME}" 
+      "" "${TCL_TCLSH}")
+
+    ADD_TEST(${test_name}
+      ${EXECUTABLE_OUTPUT_PATH}/${LAUNCHER_EXE_NAME} "${script_name}" --test)
+
+  ENDIF(KWWidgets_BUILD_SHARED_LIBS AND VTK_WRAP_TCL AND TCL_TCLSH)
+
+ENDMACRO(KWWidgets_ADD_TEST_FROM_TCL_EXAMPLE)
+
+# ---------------------------------------------------------------------------
+# KWWidgets_ADD_TEST_FROM_PYTHON_EXAMPLE
+# Add specific distribution-related Python test
+
+MACRO(KWWidgets_ADD_TEST_FROM_PYTHON_EXAMPLE 
+    test_name
+    script_name)
+
+  IF(KWWidgets_BUILD_SHARED_LIBS AND VTK_WRAP_PYTHON AND PYTHON_EXECUTABLE)
+
+    INCLUDE("${KWWidgets_CMAKE_DIR}/KWWidgetsPathsMacros.cmake")
+    GET_FILENAME_COMPONENT(name_we "${script_name}" NAME_WE)
+    SET(LAUNCHER_EXE_NAME "${name_we}PythonLauncher")
+    KWWidgets_GENERATE_SETUP_PATHS_LAUNCHER(
+      "${CMAKE_CURRENT_BINARY_DIR}" "${LAUNCHER_EXE_NAME}" 
+      "" "${PYTHON_EXECUTABLE}")
+
+    ADD_TEST(${test_name}
+      ${EXECUTABLE_OUTPUT_PATH}/${LAUNCHER_EXE_NAME} "${script_name}" --test)
+
+  ENDIF(KWWidgets_BUILD_SHARED_LIBS AND VTK_WRAP_PYTHON AND PYTHON_EXECUTABLE)
+
+ENDMACRO(KWWidgets_ADD_TEST_FROM_PYTHON_EXAMPLE)
+
+# ---------------------------------------------------------------------------
 # KWWidgets_ADD_OUT_OF_SOURCE_TEST
 # Add an out-of-source test 
 
