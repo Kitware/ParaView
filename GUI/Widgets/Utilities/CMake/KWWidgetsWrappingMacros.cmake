72b2dfdc51bc2dacef9607dda6f5a84b347400d5
Sebastien Barre <sebastien.barre@kitware.com>
2006-03-03 00:05:52 -0500

    ENH: VTK 5.0 really need some help. The source of all evil, the VTK_WRAP_TCL3_INIT_DIR hack that was introduced to bypass Tcl wrapping for PV (but never tested on either the build tree or install tree), was fixed recently on the 5.0 branch but people have downloaded earlier 5.0 as well. Try to help. Furthermore, old 5.0 can not be used by external packages once it has been installed, since vtkWrapperInit.data.in is not installed properly: report that sad fact and suggest to updated.

--- GUI/Widgets/Utilities/CMake/KWWidgetsWrappingMacros.cmake
+++ GUI/Widgets/Utilities/CMake/KWWidgetsWrappingMacros.cmake
@@ -22,6 +22,22 @@ MACRO(KWWidgets_WRAP_TCL target src_list_name sources commands)
     ENDIF(NOT VTK_CMAKE_DIR)
     INCLUDE("${VTK_CMAKE_DIR}/vtkWrapTcl.cmake")
     
+    # VTK 5.0 really need some help. The evil VTK_WRAP_TCL3_INIT_DIR hack was
+    # fixed on the 5.0 branch but people have downloaded earlier 5.0 as well.
+    # Furthermore, old 5.0 can not be used once it has been installed, since
+    # vtkWrapperInit.data.in is not installed properly: report that sad fact.
+
+    IF("${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}" EQUAL "5.0")
+      IF(VTK_INSTALL_PREFIX)
+        IF(NOT EXISTS "${VTK_CMAKE_DIR}/vtkWrapperInit.data.in")
+          MESSAGE("Sorry, you are using a VTK 5.0 that can not be used properly once it has been installed. You can either download a more recent VTK 5.0 snapshot from the CVS repository, or simply point KWWidgets to your VTK build directory instead of your VTK install directory.")
+        ENDIF(NOT EXISTS "${VTK_CMAKE_DIR}/vtkWrapperInit.data.in")
+      ELSE(VTK_INSTALL_PREFIX)
+        SET(VTK_WRAP_TCL3_INIT_DIR "${VTK_SOURCE_DIR}/Wrapping")
+        SET(VTK_WRAP_PYTHON3_INIT_DIR "${VTK_SOURCE_DIR}/Wrapping")
+      ENDIF(VTK_INSTALL_PREFIX)
+    ENDIF("${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}" EQUAL "5.0")
+
     VTK_WRAP_TCL3(
       ${target}
       ${src_list_name} "${sources}" 
