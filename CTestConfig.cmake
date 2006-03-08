5ae6cff3f23940ddff5b498edc683b7b984aa498
Andy Cedilnik <andy.cedilnik@kitware.com>
2006-03-08 14:25:18 -0500

    ENH: Fix drop site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,5 +3,5 @@ SET (CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 SET (CTEST_DROP_METHOD "http")
 SET (CTEST_DROP_SITE "www.paraview.org")
 SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-SET (CTEST_TRIGGER_SITE "http://${DROP_SITE}/cgi-bin/Submit-ParaView-TestingResults.cgi")
+SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-ParaView-TestingResults.cgi")
 
