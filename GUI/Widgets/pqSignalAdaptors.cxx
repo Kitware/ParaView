8e1613ff2512896a71810236da7fa2b4da5968f0
Clinton Stimpson <clinton@elemtech.com>
2006-04-07 17:20:49 -0400

    ENH:  Testing RCS keywords

--- GUI/Widgets/pqSignalAdaptors.cxx
+++ GUI/Widgets/pqSignalAdaptors.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
    Program:   ParaQ
-   Module:    $RCS $
+   Module:    $RCSfile $
 
    Copyright (c) 2005,2006 Sandia Corporation, Kitware Inc.
    All rights reserved.
