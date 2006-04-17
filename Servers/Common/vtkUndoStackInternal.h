6649f9f0b84940bbf1bff6ac62603cf94149a837
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-04-17 15:27:35 -0400

    BUG: adding forgotten file.

---
+++ Servers/Common/vtkUndoStackInternal.h
@@ -0,0 +1,39 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkUndoStackInternal.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSmartPointer.h"
+#include "vtkUndoSet.h"
+
+#include <vtkstd/string>
+#include <vtkstd/vector>
+
+class vtkUndoStackInternal
+{
+public:
+  struct Element
+    {
+    vtkstd::string Label;
+    vtkSmartPointer<vtkUndoSet> UndoSet;
+    Element(const char* label, vtkUndoSet* set)
+      {
+      this->Label = label;
+      this->UndoSet = set;
+      }
+    };
+  typedef vtkstd::vector<Element> VectorOfElements;
+  VectorOfElements UndoStack;
+  VectorOfElements RedoStack;
+};
+//****************************************************************************
+
