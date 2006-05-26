e699a2368ecf96ea3ce117c6a4a458bf0e3ad04c
Sebastien Barre <sebastien.barre@kitware.com>
2006-05-26 12:03:31 -0400

    ENH: make sure tests and tools can be disabled when the lib is part of larger project.

--- Common/KWCommon/Testing/Cxx/TestLib.cxx
+++ Common/KWCommon/Testing/Cxx/TestLib.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   ParaView
-  Module:    CommonKWCommonPrintSelf.cxx
+  Module:    TestLib.cxx
 
   Copyright (c) Kitware, Inc.
   All rights reserved.
