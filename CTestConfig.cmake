73eef810c79f8b9eec3e3683f7579967229495bb
Andy Cedilnik <andy.cedilnik@kitware.com>
2006-06-20 14:55:36 -0400

    ENH: New Links

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
-SET (CTEST_PROJECT_NAME "ParaQ")
+SET (CTEST_PROJECT_NAME "ParaView3")
 SET (CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 SET (CTEST_DROP_METHOD "http")
-SET (CTEST_DROP_SITE "public.kitware.com")
+SET (CTEST_DROP_SITE "www.paraview.org")
 SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-ParaQ-TestingResults.cgi")
+SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-ParaView3-TestingResults.cgi")
 
