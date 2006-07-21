0813bf394904595ea57c3b309f71c152b19e637e
Sebastien Barre <sebastien.barre@kitware.com>
2006-07-21 08:26:57 -0400

    STYLE: Nightly Version update

--- GUI/Widgets/vtkKWWidgetsVersion.h
+++ GUI/Widgets/vtkKWWidgetsVersion.h
@@ -25,7 +25,7 @@
 #include "vtkKWWidgets.h" // Needed for export symbols directives
 #include "vtkObject.h"
 
-#define KWWidgets_SOURCE_VERSION "KWWidgets version " KWWidgets_VERSION ", KWWidgets source $Revision: 1.11 $, $Date: 2006-07-20 12:29:07 $ (GMT)"
+#define KWWidgets_SOURCE_VERSION "KWWidgets version " KWWidgets_VERSION ", KWWidgets source $Revision: 1.12 $, $Date: 2006-07-21 12:26:57 $ (GMT)"
 
 class KWWidgets_EXPORT vtkKWWidgetsVersion : public vtkObject
 {
