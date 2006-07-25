349fa9d85100a3a2d273e611ccf8d6a2a9eabe60
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-07-25 17:35:03 -0400

    ENH: Added code form vtkpython executable to pvpython executable for detecting the location of the "paraview" module. Now, if the pvpython executable is run from an installed location, it will attempt to locate the installed module.

--- Utilities/VTKPythonWrapping/Executable/vtkPVProcessModulePythonHelper.cxx
+++ Utilities/VTKPythonWrapping/Executable/vtkPVProcessModulePythonHelper.cxx
@@ -39,7 +39,7 @@ extern "C" {
 }
 
 
-vtkCxxRevisionMacro(vtkPVProcessModulePythonHelper, "1.7");
+vtkCxxRevisionMacro(vtkPVProcessModulePythonHelper, "1.8");
 vtkStandardNewMacro(vtkPVProcessModulePythonHelper);
 
 //----------------------------------------------------------------------------
@@ -66,6 +66,107 @@ void vtkPVProcessModulePythonHelper::PrintSelf(ostream& os, vtkIndent indent)
 }
 
 //----------------------------------------------------------------------------
+static void vtkPythonAppInitPrependPythonPath(const char* dir)
+{
+  // Convert slashes for this platform.
+  vtkstd::string out_dir = dir;
+#if defined(_WIN32) && !defined(__CYGWIN__)
+  for(vtkstd::string::size_type i = 0; i < out_dir.length(); ++i)
+    {
+    if(out_dir[i] == '/')
+      {
+      out_dir[i] = '\\';
+      }
+    }
+#endif
+
+  // Append the path to the python sys.path object.
+  char tmpPath[] = "path";
+  PyObject* path = PySys_GetObject(tmpPath);
+  PyObject* newpath;
+  newpath = PyString_FromString(out_dir.c_str());
+  PyList_Insert(path, 0, newpath);
+  Py_DECREF(newpath);
+}
+
+//----------------------------------------------------------------------------
+static void vtkPythonAppInitPrependPath(const char* self_dir)
+{
+  // Try to put the VTK python module location in sys.path.
+  vtkstd::string package_dir = self_dir;
+#if defined(CMAKE_INTDIR)
+  package_dir += "/..";
+#endif
+  package_dir += "/../Utilities/VTKPythonWrapping";
+  package_dir = vtksys::SystemTools::CollapseFullPath(package_dir.c_str());
+  if(vtksys::SystemTools::FileIsDirectory(package_dir.c_str()))
+    {
+    // This executable is running from the build tree.  Prepend the
+    // library directory and package directory to the search path.
+    vtkPythonAppInitPrependPythonPath(package_dir.c_str());
+    vtkPythonAppInitPrependPythonPath(VTK_PYTHON_LIBRARY_DIR);
+    }
+  else
+    {
+    // This executable is running from an install tree.  Check for
+    // possible VTK python module locations.  See
+    // http://python.org/doc/2.4.1/inst/alt-install-windows.html for
+    // information about possible install locations.  If the user
+    // changes the prefix to something other than VTK's prefix or
+    // python's native prefix then he/she will have to get the
+    // packages in sys.path himself/herself.
+    const char* inst_dirs[] = {
+      "/lib/python"/*TODO: added Python version */"/site-packages/paraview", // UNIX --prefix
+      "/lib/python/paraview", // UNIX --home
+      "/Lib/site-packages/paraview", "/Lib/paraview", // Windows
+      "/site-packages/paraview", "/paraview", // Windows
+      0
+    };
+    vtkstd::string prefix = vtksys::SystemTools::GetFilenamePath(self_dir);
+    for(const char** dir = inst_dirs; *dir; ++dir)
+      {
+      package_dir = prefix;
+      package_dir += *dir;
+      package_dir = vtksys::SystemTools::CollapseFullPath(package_dir.c_str());
+      if(vtksys::SystemTools::FileIsDirectory(package_dir.c_str()))
+        {
+        // We found the modules.  Add the location to sys.path, but
+        // without the "/vtk" suffix.
+        vtkstd::string path_dir =
+          vtksys::SystemTools::GetFilenamePath(package_dir);
+        vtkPythonAppInitPrependPythonPath(path_dir.c_str());
+        break;
+        }
+      }
+
+    // This executable does not actually link to the python wrapper
+    // libraries, though it probably should now that the stub-modules
+    // are separated from them.  Since it does not we have to make
+    // sure the wrapper libraries can be found by the dynamic loader
+    // when the stub-modules are loaded.  On UNIX this executable must
+    // be running in an environment where the main VTK libraries (to
+    // which this executable does link) have been found, so the
+    // wrapper libraries will also be found.  On Windows this
+    // executable may have simply found its .dll files next to itself
+    // so the wrapper libraries may not be found when the wrapper
+    // modules are loaded.  Solve this problem by adding this
+    // executable's location to the system PATH variable.  Note that
+    // this need only be done for an installed VTK because in the
+    // build tree the wrapper modules are in the same directory as the
+    // wrapper libraries.
+#if defined(_WIN32)
+    static char system_path[(VTK_PYTHON_MAXPATH+1)*10] = "PATH=";
+    strcat(system_path, self_dir);
+    if(char* oldpath = getenv("PATH"))
+      {
+      strcat(system_path, ";");
+      strcat(system_path, oldpath);
+      }
+    putenv(system_path);
+#endif
+    }
+}
+//----------------------------------------------------------------------------
 int vtkPVProcessModulePythonHelper::RunGUIStart(int argc, char **argv, int numServerProcs, int myId)
 {
   (void)myId;
@@ -90,30 +191,18 @@ int vtkPVProcessModulePythonHelper::RunGUIStart(int argc, char **argv, int numSe
 
   // If the location of the library path and wrapping path exist, add
   // them to the list.
-  
-  // Get the pointer to path list object, append both paths, and
-  // make sure to decrease reference counting for both path strings.
-  char tmpPath[5];
-  sprintf(tmpPath,"path");
-  PyObject* path = PySys_GetObject(tmpPath);
-  PyObject* newpath;
-  if ( vtksys::SystemTools::FileExists(VTK_PYTHON_LIBRARY_DIR) )
+ 
+  // Compute the directory containing this executable.  The python
+  // sys.executable variable contains the full path to the interpreter
+  // executable.
+  char tmpExe[] = "executable";
+  PyObject* executable = PySys_GetObject(tmpExe);
+  if(const char* exe_str = PyString_AsString(executable))
     {
-    newpath = PyString_FromString(VTK_PYTHON_LIBRARY_DIR);
-    PyList_Insert(path, 0, newpath);
-    Py_DECREF(newpath);
-    }
-  if ( vtksys::SystemTools::FileExists(VTK_PYTHON_PACKAGE_DIR) )
-    {
-    newpath = PyString_FromString(VTK_PYTHON_PACKAGE_DIR);
-    PyList_Insert(path, 0, newpath);
-    Py_DECREF(newpath);
-    }
-  if (vtksys::SystemTools::FileExists(PV_PYTHON_PACKAGE_DIR))
-    {
-    newpath = PyString_FromString(PV_PYTHON_PACKAGE_DIR);
-    PyList_Insert(path, 0, newpath);
-    Py_DECREF(newpath);
+    // Use the executable location to try to set sys.path to include
+    // the VTK python modules.
+    vtkstd::string self_dir = vtksys::SystemTools::GetFilenamePath(exe_str);
+    vtkPythonAppInitPrependPath(self_dir.c_str());
     }
 
   // Ok, all done, now enter python main.
