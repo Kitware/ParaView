55b720a7047bdfaf3a5979c322e593ab77ae31ea
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-07-25 11:24:54 -0400

    ENH: added support to create render modules, displays. STYLE: fixed indentation.

--- Utilities/VTKPythonWrapping/paraview/__init__.py
+++ Utilities/VTKPythonWrapping/paraview/__init__.py
@@ -24,6 +24,9 @@ class pyProxy:
     def __init__(self, proxy):
         "Constructor. Assigns proxy to self.SMProxy"
         self.SMProxy = proxy
+        doc = proxy.GetDocumentation().GetDescription()
+        if doc:
+          self.__doc__ = doc
 
     def __AddToProperty__(self, *args):
         """Generic method for adding a proxy to a proxy property.
@@ -39,16 +42,14 @@ class pyProxy:
             return
         if property.IsA("vtkSMProxyProperty"):
             for proxy in args:
-                try:
-                    getattr(proxy, "SMProxy")
-                except AttributeError:
-                    property.AddProxy(proxy)
-                else:
+                if isinstance(proxy, pyProxy):
                     property.AddProxy(proxy.SMProxy)
-                    
+                else:
+                    property.AddProxy(proxy)
         else:
             raise "AddTo works only with proxy properties"
         self.__LastAttrName = None
+        return
 
     def __SetProperty__(self, *args):
         """Generic method for setting the value of a property.
@@ -63,14 +64,15 @@ class pyProxy:
             print "Property %s not found. Cannot set" % self.__LastAttrName
             return
         if property.IsA("vtkSMProxyProperty"):
-            property.SetNumberOfProxies(len(args))
             for i in range(len(args)):
-                try:
-                    getattr(args[i], "SMProxy")
-                except AttributeError:
-                    property.SetProxy(i, args[i])
+                if isinstance(args[i], pyProxy):
+                    value_proxy = args[i].SMProxy
                 else:
-                    property.SetProxy(i, args[i].SMProxy)
+                    value_proxy = args[i]
+                if i < property.GetNumberOfProxies() :
+                    property.SetProxy(i, value_proxy)
+                else:
+                    property.AddProxy(value_proxy)
         else:
             for i in range(len(args)):
                 property.SetElement(i, args[i])
@@ -104,23 +106,27 @@ class pyProxy:
             return list 
         return []
       
-    def CreateDisplayProxy(self):
+    def __CreateDisplayProxy__(self):
         "Overload RenderModule's CreateDisplayProxy() to return a pyProxy"
         return pyProxy(self.SMProxy.CreateDisplayProxy())
 
-    def AddProxy(self, name, proxy):
+    def __AddProxy__(self, name, proxy):
         "Overload CompoundProxy's AddProxy()"
-        self.SMProxy.AddProxy(name, proxy.SMProxy)
+        if isinstance(proxy, pyProxy):
+            self.SMProxy.AddProxy(name, proxy.SMProxy)
+        else:
+            self.SMProxy.AddProxy(name, proxy)
+        return
 
     def ListProperties(self):
         """Returns a list of all properties on this proxy."""
         iter = self.NewPropertyIterator()
+        iter.UnRegister(None)
         iter.Begin()
         property_list = []
         while not iter.IsAtEnd():
           property_list.append(iter.GetKey())
           iter.Next()
-        iter.UnRegister(None)
         return property_list
 
     def __getattr__(self, name):
@@ -135,6 +141,10 @@ class pyProxy:
         if re.compile("^AddTo").match(name):
             self.__LastAttrName = name[5:]
             return self.__AddToProperty__
+        if name == "CreateDisplayProxy" and hasattr(self.SMProxy, "CreateDisplayProxy"):
+            return self.__CreateDisplayProxy__
+        if name == "AddProxy" and hasattr(self.SMProxy, "AddProxy"):
+            return self.__AddProxy__
         return getattr(self.SMProxy, name)
         
 class pyProxyManager:
@@ -149,12 +159,10 @@ class pyProxyManager:
     def RegisterProxy(self, group, name, proxy):
         """Registers a proxy (either SMProxy or pyProxy) with the
         server manager"""
-        try:
-            getattr(proxy, "SMProxy")
-        except:
-            self.SMProxyManager.RegisterProxy(group, name, proxy)
-        else:
+        if isinstance(proxy, pyProxy):
             self.SMProxyManager.RegisterProxy(group, name, proxy.SMProxy)
+        else:
+            self.SMProxyManager.RegisterProxy(group, name, proxy)
 
     def NewProxy(self, group, name):
         """Creates a new proxy of given group and name and returns a pyProxy
@@ -228,12 +236,18 @@ class pyProxyIterator:
     def __init__(self):
         self.SMIterator = vtkSMProxyIterator()
         self.SMIterator.Begin()
+        self.Proxy = None
+        self.Group = None
+        self.Key = None
 
     def __iter__(self):
         return self
 
     def next(self):
         if self.SMIterator.IsAtEnd():
+            self.Proxy = None
+            self.Group = None
+            self.Key = None
             raise StopIteration
             return None
         self.Proxy = self.SMIterator.GetProxy()
@@ -245,12 +259,15 @@ class pyProxyIterator:
         return self.Proxy
 
     def GetProxy(self):
+        """Returns the proxy last returned by the call to 'next()'"""
         return self.Proxy
 
     def GetKey(self):
+        """Returns the key for the proxy last returned by the call to 'next()' """
         return self.Key
 
     def GetGroup(self):
+        """Returns the group for the proxy last returned by the call to 'next()' """
         return self.Group
 
     def __getattr__(self, name):
@@ -258,30 +275,36 @@ class pyProxyIterator:
         return getattr(self.SMIterator, name)
 
 class pyConnection:
-  """
-    This is a representation for a connection on in the python client.
-    Eventually,  this may move to the server manager itself.
-  """
-  def __init__(self, connectionId):
-    self.ID = connectionId
-    self.Hostname = ""
-    self.Port = 0
-    self.RSHostname = None
-    self.RSPort = None
-    return
+    """
+      This is a representation for a connection on in the python client.
+      Eventually,  this may move to the server manager itself.
+    """
+    def __init__(self, connectionId):
+        self.ID = connectionId
+        self.Hostname = ""
+        self.Port = 0
+        self.RSHostname = None
+        self.RSPort = None
+        return
 
-  def SetHost(self, ds_host, ds_port, rs_host=None, rs_port=None):
-    self.Hostname = ds_host 
-    self.Port = ds_port
-    self.RSHostname = rs_host
-    self.RSPort = rs_port
-    return
+    def SetHost(self, ds_host, ds_port, rs_host=None, rs_port=None):
+        self.Hostname = ds_host 
+        self.Port = ds_port
+        self.RSHostname = rs_host
+        self.RSPort = rs_port
+        return
 
-  def __repr__(self):
-    if not self.RSHostname:
-      return "Connection (%s:%d)" % (self.Hostname, self.Port)
-    return "Connection data(%s:%d), render(%s:%d)" % \
-      (self.Hostname, self.Port, self.RSHostname, self.RSPort)
+    def __repr__(self):
+        if not self.RSHostname:
+            return "Connection (%s:%d)" % (self.Hostname, self.Port)
+        return "Connection data(%s:%d), render(%s:%d)" % \
+            (self.Hostname, self.Port, self.RSHostname, self.RSPort)
+
+    def isRemote(self):
+        pm = vtkProcessModule.GetProcessModule()
+        if pm.IsRemote(self.ID):
+            return True
+        return False
 
 # Users can set the active connection which will be used by API
 # to create proxies etc when no connection argument is passed.
@@ -292,75 +315,122 @@ ActiveConnection = None
 ## or simply create a built-in connection.
 
 def connect_server(host, port):
-  """Connect to a host:port. Returns the connection object if successfully connected 
-  with the server."""
-  pm =  vtkProcessModule.GetProcessModule()
-  cid = pm.ConnectToRemote(host, port)
-  if not cid:
-    return None
-  conn = pyConnection(cid)
-  conn.SetHost(host, port)
-  return conn 
+    """Connect to a host:port. Returns the connection object if successfully connected 
+    with the server."""
+    pm =  vtkProcessModule.GetProcessModule()
+    cid = pm.ConnectToRemote(host, port)
+    if not cid:
+        return None
+    conn = pyConnection(cid)
+    conn.SetHost(host, port)
+    return conn 
 
 def connect_ds_rs(ds_host, ds_port, rs_host, rs_port):
-  """Connect to a dataserver at (ds_host:ds_port) and to a render server
-  at (rs_host:rs_port). 
-  Returns the connection object if successfully connected 
-  with the server."""
-  pm =  vtkProcessModule.GetProcessModule()
-  cid = pm.ConnectToRemote(ds_host, ds_port, rs_host, rs_port)
-  if not cid:
-    return None
-  conn = pyConnection(cid)
-  conn.SetHost(ds_host, ds_port, rs_host, rs_port)
-  return conn 
+    """Connect to a dataserver at (ds_host:ds_port) and to a render server
+    at (rs_host:rs_port). 
+    Returns the connection object if successfully connected 
+    with the server."""
+    pm =  vtkProcessModule.GetProcessModule()
+    cid = pm.ConnectToRemote(ds_host, ds_port, rs_host, rs_port)
+    if not cid:
+        return None
+    conn = pyConnection(cid)
+    conn.SetHost(ds_host, ds_port, rs_host, rs_port)
+    return conn 
 
 def connect_self():
-  """Creates a new self connection."""
-  pm =  vtkProcessModule.GetProcessModule()
-  cid = pm.ConnectToSelf()
-  if not cid:
-    return None
-  conn = pyConnection(cid)
-  conn.SetHost("builtin", cid)
-  return conn
+    """Creates a new self connection."""
+    pm =  vtkProcessModule.GetProcessModule()
+    cid = pm.ConnectToSelf()
+    if not cid:
+        return None
+    conn = pyConnection(cid)
+    conn.SetHost("builtin", cid)
+    return conn
 
 def connect(ds_host=None, ds_port=None, rs_host=None, rs_port=None):
-  """
-    Unified API to connect to self, or server or dataserver and render server.
-  """
-  if ds_host == None:
-    return connect_self()
-  if rs_host == None:
-    return connect_server(ds_host, ds_port)
-  return connect_ds_rs(ds_host, ds_port, rs_host, rs_port)
+    """
+      Unified API to connect to self, or server or dataserver and render server.
+    """
+    if ds_host == None:
+        return connect_self()
+    if rs_host == None:
+        return connect_server(ds_host, ds_port)
+    return connect_ds_rs(ds_host, ds_port, rs_host, rs_port)
 
 def disconnect(connection):
-  """Disconnects the connection."""
-  pm =  vtkProcessModule.GetProcessModule()
-  pm.Disconnect(connection.ID)
-  return
+    """Disconnects the connection."""
+    pm =  vtkProcessModule.GetProcessModule()
+    pm.Disconnect(connection.ID)
+    return
 
 def createProxy(xml_group, xml_name, register_group=None, register_name=None, connection=None):
-  """Creates a proxy. If register_group is non-None, then the created
-     proxy is registered under that group. If connection is set, the proxy's
-     connection ID is set accordingly. If connection is None, ActiveConnection
-     is used, is present. If register_group is non-None, but register_name is None,
-     then the proxy's self id is used to create a new name.
-  """
-  global ActiveConnection
-  pxm = pyProxyManager()
-  proxy = pxm.NewProxy(xml_group, xml_name)
-  if not proxy:
-    return None
-  if not connection:
-    connection = ActiveConnection
-  if connection:
-    proxy.SMProxy.SetConnectionID(connection.ID)
-  if register_group:
-    if not register_name:
-      register_name = proxy.GetSelfIDAsString()
-    pxm.RegisterProxy(register_group, register_name, proxy.SMProxy)
-  return proxy
+    """Creates a proxy. If register_group is non-None, then the created
+       proxy is registered under that group. If connection is set, the proxy's
+       connection ID is set accordingly. If connection is None, ActiveConnection
+       is used, is present. If register_group is non-None, but register_name is None,
+       then the proxy's self id is used to create a new name.
+    """
+    global ActiveConnection
+    pxm = pyProxyManager()
+    proxy = pxm.NewProxy(xml_group, xml_name)
+    if not proxy:
+        return None
+    if not connection:
+        connection = ActiveConnection
+    if connection:
+        proxy.SMProxy.SetConnectionID(connection.ID)
+    if register_group:
+        if not register_name:
+            register_name = proxy.GetSelfIDAsString()
+        pxm.RegisterProxy(register_group, register_name, proxy.SMProxy)
+    return proxy
 
+def createRenderWindow(connection=None):
+    """Creates a render window on the particular connection. If connection is not specified,
+    then the active connection is used, if available."""
+    global ActiveConnection
+    if not connection:
+        connection = ActiveConnection
+    if not connection:
+        raise "Cannot create render window without connection."
+    pxm = pyProxyManager()
+    multi_view_render_module = None
+    for proxy in pxm.connection_iter(connection):
+        if proxy.IsA("vtkSMMultiViewRenderModuleProxy"):
+            multi_view_render_module = proxy
+            break
+    if not multi_view_render_module:
+        multi_view_render_module = \
+            createProxy("rendermodules", "MultiViewRenderModule", 
+              "render_modules", None, connection)
+        if connection.isRemote():
+            multi_view_render_module.SetRenderModuleName("IceTDesktopRenderModule")
+        else:
+            multi_view_render_module.SetRenderModuleName("LODRenderModule")
+    if not multi_view_render_module:
+        raise "Could not locate a MultiViewRenderModule for the connection."
+    ren_module = multi_view_render_module.NewRenderModule();
+    if not ren_module:
+        return None
+    ren_module.UnRegister(None)
+    pxm.RegisterProxy("render_modules", ren_module.GetSelfIDAsString(), ren_module)
+    ren_module.UpdateVTKObjects()
+    return pyProxy(ren_module)
 
+def createDisplay(proxy, renModule):
+    """Create a display for the proxy and adds it to the render module."""
+    if not proxy:
+        raise "proxy argument cannot be None."
+    if not renModule:
+        raise "render module argument cannot be None."
+    display = renModule.CreateDisplayProxy()
+    if not display:
+        return None
+    display.UnRegister(None)
+    pxm = pyProxyManager()
+    pxm.RegisterProxy("displays", display.GetSelfIDAsString(), display)
+    display.SetInput(proxy)
+    display.UpdateVTKObjects()
+    renModule.AddToDisplays(display)
+    return display
