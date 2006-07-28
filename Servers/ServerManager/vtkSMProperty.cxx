4974bad7f9542a5789b6ecc85bf82f9ae4396696
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-07-28 10:53:17 -0400

    BUG: typo

--- Servers/ServerManager/vtkSMProperty.cxx
+++ Servers/ServerManager/vtkSMProperty.cxx
@@ -33,7 +33,7 @@
 #include "vtkSMPropertyInternals.h"
 
 vtkStandardNewMacro(vtkSMProperty);
-vtkCxxRevisionMacro(vtkSMProperty, "1.46");
+vtkCxxRevisionMacro(vtkSMProperty, "1.47");
 
 vtkCxxSetObjectMacro(vtkSMProperty, Proxy, vtkSMProxy);
 vtkCxxSetObjectMacro(vtkSMProperty, InformationHelper, vtkSMInformationHelper);
@@ -525,7 +525,7 @@ void vtkSMProperty::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "InformationProperty: " << this->InformationProperty << endl;
   os << indent << "Animateable: " << this->Animateable << endl;
   os << indent << "Internal: " << this->IsInternal << endl;
-  os << Internal << "Documentation: " << this->Documentation << endl;
+  os << indent << "Documentation: " << this->Documentation << endl;
 
   vtkSMSubPropertyIterator* iter = vtkSMSubPropertyIterator::New();
   iter->SetProperty(this);
