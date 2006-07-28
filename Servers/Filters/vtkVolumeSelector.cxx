8a9a175d8e405fde0def83cd36c8508980b463cf
Berk Geveci <berk.geveci@kitware.com>
2006-07-28 10:50:51 -0400

    ENH: Replaced vtkPropAndCellSelect and vtkSelectDataSets with vtkVolumeSelector and cleaned up

---
+++ Servers/Filters/vtkVolumeSelector.cxx
@@ -0,0 +1,254 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkVolumeSelector.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkVolumeSelector.h"
+
+#include "vtkAlgorithm.h"
+#include "vtkCellData.h"
+#include "vtkCompositeDataPipeline.h"
+#include "vtkDataSet.h"
+#include "vtkExecutive.h"
+#include "vtkFrustumExtractor.h"
+#include "vtkIdTypeArray.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtkProcessModule.h"
+#include "vtkProp.h"
+#include "vtkSelection.h"
+#include "vtkSignedCharArray.h"
+
+#include "vtkSmartPointer.h"
+#include <vtkstd/vector>
+
+vtkCxxRevisionMacro(vtkVolumeSelector, "1.1");
+vtkStandardNewMacro(vtkVolumeSelector);
+
+vtkCxxSetObjectMacro(vtkVolumeSelector, Selection, vtkSelection);
+
+struct vtkVolumeSelectorInternals
+{
+  vtkstd::vector<vtkSmartPointer<vtkDataObject> > DataSets;
+  vtkstd::vector<vtkSmartPointer<vtkProp> > Props;
+};
+
+//----------------------------------------------------------------------------
+vtkVolumeSelector::vtkVolumeSelector()
+{
+  this->AtomExtractor = vtkFrustumExtractor::New();
+  this->AtomExtractor->SetPassThrough(1);
+  // Make sure that the extractor filter has the right executive and
+  // that it requests the right piece and number of pieces.
+  vtkCompositeDataPipeline* cdp = vtkCompositeDataPipeline::New();
+  this->AtomExtractor->SetExecutive(cdp);
+  vtkInformation* outInfo = 
+    cdp->GetOutputInformation()->GetInformationObject(0);
+  vtkProcessModule* processModule = vtkProcessModule::GetProcessModule();
+  cdp->SetUpdateNumberOfPieces(outInfo, processModule->GetNumberOfPartitions());
+  cdp->SetUpdatePiece(outInfo, processModule->GetPartitionId());
+  cdp->SetUpdateGhostLevel(outInfo, 0);
+  cdp->Delete();
+  this->Internal = new vtkVolumeSelectorInternals;
+  this->Selection = vtkSelection::New();
+}
+
+//----------------------------------------------------------------------------
+vtkVolumeSelector::~vtkVolumeSelector()
+{
+  this->AtomExtractor->Delete();
+  delete this->Internal;
+  if (this->Selection)
+    {
+    this->Selection->Delete();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::Select()
+{ 
+  if (!this->Selection)
+    {
+    this->Selection = vtkSelection::New();
+    }
+
+  this->Selection->Clear();
+  this->Selection->GetProperties()->Set(
+    vtkSelectionNode::CONTENT_TYPE(), vtkSelectionNode::SELECTIONS);
+
+  vtkProcessModule* processModule = vtkProcessModule::GetProcessModule();
+
+  //run the cell/point extractor on each selected dataset
+  unsigned int numDataSets = this->Internal->DataSets.size();
+  for (unsigned int i = 0; i < numDataSets; i++)
+    {
+    vtkDataSet* ds = vtkDataSet::SafeDownCast(this->Internal->DataSets[i]);
+    if (ds)
+      {
+      // First perform a selection with the dataset. This will use
+      // the frustrum to mark selected cells.
+
+      // We make a copy of the dataset so that we are not connected 
+      // to the ParaView pipeline
+      vtkDataSet* newDS = ds->NewInstance();
+      newDS->ShallowCopy(ds);
+      this->AtomExtractor->SetInput(newDS);
+      newDS->Delete();
+      this->AtomExtractor->Update();
+      vtkDataSet* output = this->AtomExtractor->GetOutput();
+      vtkSignedCharArray* insidedness = 
+        vtkSignedCharArray::SafeDownCast(
+          output->GetCellData()->GetArray("vtkInsidedness"));
+      // If nothing is selected, no need to proceed
+      if (!insidedness || insidedness->GetNumberOfTuples() == 0)
+        {
+        // Make sure that the input is released
+        this->AtomExtractor->SetInput((vtkDataSet*)0);
+        continue;
+        }
+
+      // Create and add a selection node
+      vtkSelectionNode* selection = vtkSelectionNode::New();
+      this->Selection->AddChild(selection);
+
+      // For now, we only support cell ids
+      selection->GetProperties()->Set(
+        vtkSelectionNode::CONTENT_TYPE(), vtkSelectionNode::CELL_IDS);
+
+      // Selected cell array
+      vtkIdTypeArray* selectedCells = vtkIdTypeArray::New();
+      vtkIdType numCells = insidedness->GetNumberOfTuples();
+      for (vtkIdType ii=0; ii<numCells; ii++)
+        {
+        if (insidedness->GetValue(ii) == 1)
+          {
+          selectedCells->InsertNextValue(ii);
+          }
+        }
+      selection->SetSelectionList(selectedCells);
+      selectedCells->Delete();
+
+      // Make sure that the input is released
+      this->AtomExtractor->SetInput((vtkDataSet*)0);
+
+      // Find the source of the geometry and add it's id as a
+      // property
+      vtkInformation* pInfo = ds->GetPipelineInformation();
+      if (pInfo)
+        {
+        vtkExecutive* pExec = pInfo->GetExecutive(vtkExecutive::PRODUCER());
+        if (pExec)
+          {
+          vtkAlgorithm* alg = pExec->GetAlgorithm();
+          if (alg)
+            {
+            vtkClientServerID id = processModule->GetIDFromObject(alg);
+            selection->GetProperties()->Set(
+              vtkSelectionNode::SOURCE_ID(), id.ID);
+            }
+          }
+        }
+      
+      if (this->Internal->Props.size() > i)
+        {
+        vtkClientServerID pid = processModule->GetIDFromObject(
+          this->Internal->Props[i]);
+        selection->GetProperties()->Set(
+          vtkSelectionNode::PROP_ID(), pid.ID);
+        }
+
+      // Add the process id as another property
+      if (processModule->GetPartitionId() >= 0)
+        {
+        selection->GetProperties()->Set(
+          vtkSelectionNode::PROCESS_ID(), processModule->GetPartitionId());
+        }
+
+      selection->Delete();
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::ClearDataSets()
+{
+  this->Internal->DataSets.clear();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::ClearProps()
+{
+  this->Internal->Props.clear();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::Initialize()
+{
+  this->ClearProps();
+  this->ClearDataSets();
+  this->Selection->Clear();
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::AddProp(vtkProp* prop)
+{
+  this->Internal->Props.push_back(prop);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::AddDataSet(vtkAlgorithm* alg)
+{
+  vtkDataObject *dObj = NULL;
+  if (alg)
+    {
+    dObj = alg->GetOutputDataObject(0);
+    if (!dObj)
+      {
+      vtkErrorMacro("Could not find algorithm's vtkDataObject output.");
+      return;
+      }
+    }
+
+  this->Internal->DataSets.push_back(dObj);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::CreateFrustum(double vertices[32])
+{
+  this->AtomExtractor->CreateFrustum(vertices);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::SetExactTest(int et)
+{
+  this->AtomExtractor->SetExactTest(et);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+int vtkVolumeSelector::GetExactTest()
+{
+  return this->AtomExtractor->GetExactTest();
+}
+
+//----------------------------------------------------------------------------
+void vtkVolumeSelector::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+  os << indent << "AtomExtractor: " << this->AtomExtractor << endl;
+}
+
---
+++ Servers/Filters/vtkVolumeSelector.h
@@ -0,0 +1,112 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkVolumeSelector.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkVolumeSelector - Given a frustrum and a list of datasets, creates a selection
+// .SECTION Description
+// vtkVolumeSelector selects cells that are in the given frustrum from a
+// list of datasets. Internally, it uses vtkFrustumExtractor to select
+// cells. The output is a vtkSelection tree. Each node in the tree contains
+// a SOURCE_ID (the proxy id of the producer of the dataset), a PROP_ID (if
+// assigned with AddProp), the PROCESS_ID and the selection list array.
+//
+// .SECTION See Also
+// vtkFrustumExtractor
+
+#ifndef __vtkVolumeSelector_h
+#define __vtkVolumeSelector_h
+
+#include "vtkObject.h"
+
+//BTX
+class vtkAlgorithm;
+class vtkFrustumExtractor;
+class vtkProp;
+class vtkSelection;
+struct vtkVolumeSelectorInternals;
+//ETX
+
+class VTK_EXPORT vtkVolumeSelector : public vtkObject
+{
+public:
+  vtkTypeRevisionMacro(vtkVolumeSelector,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Constructor
+  static vtkVolumeSelector *New();
+
+  // Description:
+  // Clears props and datasets.
+  void Initialize();
+
+  // Description:
+  // Clears datasets.
+  void ClearDataSets();
+
+  // Description:
+  // Tries to obtain a dataset from the pointer, and adds the dataset to the
+  // output.
+  void AddDataSet(vtkAlgorithm* alg);
+
+  // Description:
+  // Clears props.
+  void ClearProps();
+
+  // Description:
+  // Add a prop to the internal list. These are used to populate the
+  // selection. Make sure that props are added in the same order as
+  // datasets.
+  void AddProp(vtkProp* prop);
+
+  // Description:
+  // Given eight vertices, creates a frustum.
+  void CreateFrustum(double vertices[32]);
+
+  // Description:
+  // Set the selection object to be used to store the results of the
+  // selection operation.
+  void SetSelection(vtkSelection* selection);
+
+  // Description:
+  // Get the selection object to be used to store the results of the
+  // selection operation.
+  vtkGetObjectMacro(Selection, vtkSelection);
+
+  // Description:
+  // Sets/gets the intersection test type.
+  // Off extracts only those points and cells that are completely within the 
+  // frustum. On extracts all of the above as well as those cells that cross 
+  // the frustum along with all of their points.
+  // On is the default.
+  void SetExactTest(int et);
+  int GetExactTest();
+
+  // Description:
+  // Performs the selection.
+  void Select();
+
+protected:
+  vtkVolumeSelector();
+  ~vtkVolumeSelector();
+
+  vtkFrustumExtractor *AtomExtractor;
+  vtkVolumeSelectorInternals* Internal;
+  vtkSelection* Selection;
+
+private:
+  vtkVolumeSelector(const vtkVolumeSelector&);  // Not implemented.
+  void operator=(const vtkVolumeSelector&);  // Not implemented.
+};
+
+#endif
