78cdb58465955fc9c8ec89c97d28db0b132a1726
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-09-08 12:06:46 -0400

    ENH: Add DoubleArray information helper.

---
+++ Servers/ServerManager/vtkSMDoubleArrayInformationHelper.cxx
@@ -0,0 +1,94 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMDoubleArrayInformationHelper.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMDoubleArrayInformationHelper.h"
+
+#include "vtkClientServerStream.h"
+#include "vtkObjectFactory.h"
+#include "vtkProcessModule.h"
+#include "vtkSMDoubleVectorProperty.h"
+
+vtkStandardNewMacro(vtkSMDoubleArrayInformationHelper);
+vtkCxxRevisionMacro(vtkSMDoubleArrayInformationHelper, "1.1");
+//-----------------------------------------------------------------------------
+vtkSMDoubleArrayInformationHelper::vtkSMDoubleArrayInformationHelper()
+{
+}
+
+//-----------------------------------------------------------------------------
+vtkSMDoubleArrayInformationHelper::~vtkSMDoubleArrayInformationHelper()
+{
+}
+
+//-----------------------------------------------------------------------------
+void vtkSMDoubleArrayInformationHelper::UpdateProperty(
+  vtkIdType connectionId,  int serverIds, vtkClientServerID objectId, 
+  vtkSMProperty* prop)
+{
+  vtkSMDoubleVectorProperty* ivp = vtkSMDoubleVectorProperty::SafeDownCast(prop);
+  if (!ivp)
+    {
+    vtkErrorMacro("A null property or a property of a different type was "
+                  "passed when a vtkSMDoubleVectorProperty was needed.");
+    return;
+    }
+  vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
+
+  vtkClientServerStream stream;
+
+  vtkClientServerID serverSideID = 
+    pm->NewStreamObject("vtkPVServerArrayHelper", stream);
+
+  stream << vtkClientServerStream::Invoke
+    << serverSideID << "SetProcessModule" 
+    << pm->GetProcessModuleID() 
+    << vtkClientServerStream::End;
+
+  stream << vtkClientServerStream::Invoke
+    << serverSideID << "GetArray" << objectId << prop->GetCommand() 
+    << vtkClientServerStream::End;
+  pm->SendStream(connectionId, vtkProcessModule::GetRootId(serverIds), stream);
+
+  vtkClientServerStream values;
+  int retVal = pm->GetLastResult(connectionId,
+    vtkProcessModule::GetRootId(serverIds)).GetArgument(0, 0, &values);
+  pm->DeleteStreamObject(serverSideID, stream);
+  pm->SendStream(connectionId, vtkProcessModule::GetRootId(serverIds), stream);
+
+  if (!retVal)
+    {
+    vtkErrorMacro("Error getting array from server.");
+    return;
+    }
+
+  int numValues = values.GetNumberOfArguments(0);
+  ivp->SetNumberOfElements(numValues);
+  for (int i=0; i < numValues; ++i)
+    {
+    double value;
+    if (!values.GetArgument(0, i, &value))
+      {
+      vtkErrorMacro("Error getting value.");
+      break;
+      }
+    ivp->SetElement(i, value);
+    }
+}
+
+
+//-----------------------------------------------------------------------------
+void vtkSMDoubleArrayInformationHelper::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ Servers/ServerManager/vtkSMDoubleArrayInformationHelper.h
@@ -0,0 +1,60 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMDoubleArrayInformationHelper.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMDoubleArrayInformationHelper - information helper to populate
+// a vtkSMDoubleVectorProperty with the results of its command which 
+// returns a vtkDoubleArray.
+// .SECTION Description
+// vtkSMDoubleArrayInformationHelper only works with vtkSMDoubleVectorProperty.
+// It calls the property's Command on the root node on the associate server.
+// The command must returns a vtkDoubleArray. The helper the populates the
+// property with the values in the vtkDoubleArray.
+
+#ifndef __vtkSMDoubleArrayInformationHelper_h
+#define __vtkSMDoubleArrayInformationHelper_h
+
+#include "vtkSMInformationHelper.h"
+
+class VTK_EXPORT vtkSMDoubleArrayInformationHelper : public vtkSMInformationHelper
+{
+public:
+  static vtkSMDoubleArrayInformationHelper* New();
+  vtkTypeRevisionMacro(vtkSMDoubleArrayInformationHelper, vtkSMInformationHelper);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  //BTX
+  // Description:
+  // Updates the property using value obtained for server. It creates
+  // an instance of the server helper class vtkPVServerArraySelection
+  // and passes the objectId (which the helper class gets as a pointer)
+  // and populates the property using the values returned.
+  // Each array is represented by two components:
+  // name, state (on/off)  
+  virtual void UpdateProperty(
+    vtkIdType connectionId,
+    int serverIds, vtkClientServerID objectId, vtkSMProperty* prop);
+  //ETX
+protected:
+  vtkSMDoubleArrayInformationHelper();
+  ~vtkSMDoubleArrayInformationHelper();
+
+private:
+  vtkSMDoubleArrayInformationHelper(const vtkSMDoubleArrayInformationHelper&); // Not implemented.
+  void operator=(const vtkSMDoubleArrayInformationHelper&); // Not implemented.
+
+};
+
+
+#endif
+
