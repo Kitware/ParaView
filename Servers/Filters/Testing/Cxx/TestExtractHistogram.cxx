7f9c9fb501e415f6dabd4b1e71ea0dc231cbae61
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-09-29 14:29:14 -0400

    BUG: Fixes TestExtractHistogram test. Piece no. etc must be set only if it is present in the request.

--- Servers/Filters/Testing/Cxx/TestExtractHistogram.cxx
+++ Servers/Filters/Testing/Cxx/TestExtractHistogram.cxx
@@ -25,32 +25,35 @@
 int main(int, char*[])
 {
   vtkSmartPointer<vtkSphereSource> sphere = vtkSmartPointer<vtkSphereSource>::New();
-  
   vtkSmartPointer<vtkExtractHistogram> extraction = vtkSmartPointer<vtkExtractHistogram>::New();
   
   const int bin_count = 3;
   
   extraction->SetInputConnection(sphere->GetOutputPort());
-  extraction->SetInputArrayToProcess(0, 0, 0, vtkDataSet::FIELD_ASSOCIATION_POINTS_THEN_CELLS, "Normals");
+  extraction->SetInputArrayToProcess(0, 0, 0, vtkDataSet::FIELD_ASSOCIATION_POINTS_THEN_CELLS, 
+    "Normals");
   extraction->SetComponent(0);
   extraction->SetBinCount(bin_count);
+  extraction->Update();
 
   vtkRectilinearGrid* const histogram = extraction->GetOutput();
-  histogram->Update();
 
   vtkDoubleArray* const bin_extents = vtkDoubleArray::SafeDownCast(histogram->GetXCoordinates());
   if(!bin_extents)
     {
+    vtkGenericWarningMacro("No bin extents found.");
     return 1;
     }
 
   if(bin_extents->GetNumberOfComponents() != 1)
     {
+    vtkGenericWarningMacro("XCoordinates must be a  1 component array.");
     return 1;
     }
   
   if(bin_extents->GetNumberOfTuples() != bin_count + 1)
     {
+    vtkGenericWarningMacro("Incorrect number of  bins.");
     return 1;
     }
 
@@ -58,28 +61,34 @@ int main(int, char*[])
     histogram->GetCellData()->GetArray("bin_values"));
   if(!bin_values)
     {
+    vtkGenericWarningMacro("bin_values missing.");
     return 1;
     }
 
   if(bin_values->GetNumberOfComponents() != 1)
     {
+    vtkGenericWarningMacro("bin_values must be a 1 component array.")
     return 1;
     }
   if(bin_values->GetNumberOfTuples() != bin_count)
     {
+    vtkGenericWarningMacro("bin_count don;t have enough values.");
     return 1;
     }
 
   if(bin_values->GetComponent(0, 0) != 14)
     {
+    vtkGenericWarningMacro("incorrect bin value.");
     return 1;
     }
   if(bin_values->GetComponent(1, 0) != 22)
     {
+    vtkGenericWarningMacro("incorrect bin value.");
     return 1;
     }
   if(bin_values->GetComponent(2, 0) != 14)
     {
+    vtkGenericWarningMacro("incorrect bin value.");
     return 1;
     }
   return 0;
