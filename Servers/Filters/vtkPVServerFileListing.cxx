31ceec08f1cfbceb566c126665a64bf1d33b801a
Timothy M. Shead <tshead@sandia.gov>
2006-10-02 13:15:09 -0400

    BUG: Fix some subtle cross-platform issues with file dialogs. BUG: Win32 drive letter slashes point the right way. ENH: File dialog slashes point the right direction for the underlying server filesystem, rather than the client.  Rationale: reduce confusion, access files the way they're stored.

--- Servers/Filters/vtkPVServerFileListing.cxx
+++ Servers/Filters/vtkPVServerFileListing.cxx
@@ -42,7 +42,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVServerFileListing);
-vtkCxxRevisionMacro(vtkPVServerFileListing, "1.7");
+vtkCxxRevisionMacro(vtkPVServerFileListing, "1.8");
 
 //----------------------------------------------------------------------------
 class vtkPVServerFileListingInternals
@@ -139,10 +139,6 @@ const vtkClientServerStream& vtkPVServerFileListing::GetSpecial()
   char* end = start;
   for(;end != strings+n; ++end)
     {
-    if(*end == '\\')
-      {
-      *end = '/';
-      }
     if(!*end)
       {
       this->Internal->Result << vtkClientServerStream::Reply << start << start << 1 << vtkClientServerStream::End;
