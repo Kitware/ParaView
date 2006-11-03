8e8e71eb5917348b3b4123d808afa325a2a6cbea
Clinton Stimpson <clinton@elemtech.com>
2006-11-03 15:19:08 -0500

    COMP:  Another comple fix for static build on Windows.

--- Qt/Core/pqCoreInit.cxx
+++ Qt/Core/pqCoreInit.cxx
@@ -31,6 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ========================================================================*/
 
 #include "pqCoreInit.h"
+#include <QObject>
 
 void pqCoreInit()
 {
