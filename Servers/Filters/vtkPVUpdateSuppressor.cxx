d7da1e225f368c22c9d0dec2634198af312c72d7
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-11-23 08:52:49 -0500

    COMP: fixed warnings and typos.

--- Servers/Filters/vtkPVUpdateSuppressor.cxx
+++ Servers/Filters/vtkPVUpdateSuppressor.cxx
@@ -28,7 +28,7 @@
 #include "vtkUnstructuredGrid.h"
 #include "vtkUpdateSuppressorPipeline.h"
 
-vtkCxxRevisionMacro(vtkPVUpdateSuppressor, "1.39");
+vtkCxxRevisionMacro(vtkPVUpdateSuppressor, "1.40");
 vtkStandardNewMacro(vtkPVUpdateSuppressor);
 
 //----------------------------------------------------------------------------
@@ -232,9 +232,9 @@ vtkExecutive* vtkPVUpdateSuppressor::CreateDefaultExecutive()
 
 
 //----------------------------------------------------------------------------
-int vtkPVUpdateSuppressor::RequestUpdateExtent(vtkInformation* request,
+int vtkPVUpdateSuppressor::RequestUpdateExtent(vtkInformation* vtkNotUsed(request),
                                   vtkInformationVector** inputVector,
-                                  vtkInformationVector* outputVector)
+                                  vtkInformationVector* vtkNotUsed(outputVector))
 {
   if (!this->Enabled)
     {
