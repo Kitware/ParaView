9d2dbb26277f63803b7e1a4635931813acaac7c8
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-12-12 09:34:17 -0500

    BUG: Added SetDefaultValues to filelist domain.

--- Servers/ServerManager/vtkSMFileListDomain.h
+++ Servers/ServerManager/vtkSMFileListDomain.h
@@ -35,6 +35,16 @@ public:
   vtkTypeRevisionMacro(vtkSMFileListDomain, vtkSMStringListDomain);
   void PrintSelf(ostream& os, vtkIndent indent);
 
+  // Description:
+  // A vtkSMProperty is often defined with a default value in the
+  // XML itself. However, many times, the default value must be determined
+  // at run time. To facilitate this, domains can override this method
+  // to compute and set the default value for the property.
+  // Note that unlike the compile-time default values, the
+  // application must explicitly call this method to initialize the
+  // property.
+  // Returns 1 if the domain updated the property.
+  virtual int SetDefaultValues(vtkSMProperty*);
 protected:
   vtkSMFileListDomain();
   ~vtkSMFileListDomain();
