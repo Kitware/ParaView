a1f547e9280b94a410c0e718d482ef2752e29885
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-12-19 09:47:34 -0500

    ENH: Added framework to enable depthpeeling for 3D renderer, however, it is not currently enabled.

--- Servers/ServerManager/vtkSMRenderModuleProxy.cxx
+++ Servers/ServerManager/vtkSMRenderModuleProxy.cxx
@@ -53,7 +53,7 @@
 #include "vtkSelection.h"
 #include "vtkSMMPIRenderModuleProxy.h"
 
-vtkCxxRevisionMacro(vtkSMRenderModuleProxy, "1.55");
+vtkCxxRevisionMacro(vtkSMRenderModuleProxy, "1.56");
 //-----------------------------------------------------------------------------
 // This is a bit of a pain.  I do ResetCameraClippingRange as a call back
 // because the PVInteractorStyles call ResetCameraClippingRange 
@@ -324,6 +324,18 @@ void vtkSMRenderModuleProxy::CreateVTKObjects(int numObjects)
     return;
     }
   ivp->SetElement(0, 2);
+
+#if 0
+  // TODO: Enable this to enable depth peeling.
+  ivp = vtkSMIntVectorProperty::SafeDownCast(
+    this->RendererProxy->GetProperty("DepthPeeling"));
+  if (!ivp)
+    {
+    vtkErrorMacro("Failed to find property DepthPeeling.");
+    return;
+    }
+  ivp->SetElement(0, 1);
+#endif
    
   ivp = vtkSMIntVectorProperty::SafeDownCast(
     this->RenderWindowProxy->GetProperty("NumberOfLayers"));
