471fe8a05023ff7eaadbb460171c298828cde6cf
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-01-11 09:54:16 -0500

    ENH: Fixes BUG #4088. Added VTK depth peeling from the CVS head to the 2.6 branch (thanks to Francois). Also adding fixes to ServerManager to use DepthPeeling.
    BUG: OrderedCompositing was not updated correctly when a new display was added to render module that already used ordered compositing. Fixed that.

--- Servers/ServerManager/vtkSMRenderModuleProxy.cxx
+++ Servers/ServerManager/vtkSMRenderModuleProxy.cxx
@@ -53,7 +53,7 @@
 #include "vtkSelection.h"
 #include "vtkSMMPIRenderModuleProxy.h"
 
-vtkCxxRevisionMacro(vtkSMRenderModuleProxy, "1.54.2.1");
+vtkCxxRevisionMacro(vtkSMRenderModuleProxy, "1.54.2.2");
 //-----------------------------------------------------------------------------
 // This is a bit of a pain.  I do ResetCameraClippingRange as a call back
 // because the PVInteractorStyles call ResetCameraClippingRange 
@@ -323,7 +323,6 @@ void vtkSMRenderModuleProxy::CreateVTKObjects(int numObjects)
     }
   ivp->SetElement(0, 2);
 
-#if 0
   // TODO: Enable this to enable depth peeling.
   ivp = vtkSMIntVectorProperty::SafeDownCast(
     this->RendererProxy->GetProperty("DepthPeeling"));
@@ -333,7 +332,6 @@ void vtkSMRenderModuleProxy::CreateVTKObjects(int numObjects)
     return;
     }
   ivp->SetElement(0, 1);
-#endif
    
   ivp = vtkSMIntVectorProperty::SafeDownCast(
     this->RenderWindowProxy->GetProperty("NumberOfLayers"));
