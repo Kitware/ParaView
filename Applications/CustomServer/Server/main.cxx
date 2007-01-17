bb42062925ff8185bc5d075577b8e906f318e70a
Timothy M. Shead <tshead@sandia.gov>
2007-01-17 13:30:50 -0500

    BUG: Server executables weren't registering the Infovis client server wrappers

--- Applications/CustomServer/Server/main.cxx
+++ Applications/CustomServer/Server/main.cxx
@@ -65,6 +65,7 @@ extern "C" void vtkCommonCS_Initialize(vtkClientServerInterpreter*);
 extern "C" void vtkFilteringCS_Initialize(vtkClientServerInterpreter*);
 extern "C" void vtkGenericFilteringCS_Initialize(vtkClientServerInterpreter*);
 extern "C" void vtkImagingCS_Initialize(vtkClientServerInterpreter*);
+extern "C" void vtkInfovisCS_Initialize(vtkClientServerInterpreter*);
 extern "C" void vtkGraphicsCS_Initialize(vtkClientServerInterpreter*);
 extern "C" void vtkIOCS_Initialize(vtkClientServerInterpreter*);
 extern "C" void vtkRenderingCS_Initialize(vtkClientServerInterpreter*);
@@ -86,6 +87,7 @@ void ParaViewInitializeInterpreter(vtkProcessModule* pm)
   vtkFilteringCS_Initialize(pm->GetInterpreter());
   vtkGenericFilteringCS_Initialize(pm->GetInterpreter());
   vtkImagingCS_Initialize(pm->GetInterpreter());
+  vtkInfovisCS_Initialize(pm->GetInterpreter());
   vtkGraphicsCS_Initialize(pm->GetInterpreter());
   vtkIOCS_Initialize(pm->GetInterpreter());
   vtkRenderingCS_Initialize(pm->GetInterpreter());
