5440253e85a7d1532af3309751ced505d3035274
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-01-29 14:06:18 -0500

    BUG: Ensure that the cue is created before adding.

--- Servers/ServerManager/vtkSMAnimationSceneProxy.cxx
+++ Servers/ServerManager/vtkSMAnimationSceneProxy.cxx
@@ -100,7 +100,7 @@ public:
 };
 
 
-vtkCxxRevisionMacro(vtkSMAnimationSceneProxy, "1.36");
+vtkCxxRevisionMacro(vtkSMAnimationSceneProxy, "1.37");
 vtkStandardNewMacro(vtkSMAnimationSceneProxy);
 //----------------------------------------------------------------------------
 vtkSMAnimationSceneProxy::vtkSMAnimationSceneProxy()
@@ -291,6 +291,11 @@ void vtkSMAnimationSceneProxy::AddCue(vtkSMProxy* proxy)
     {
     vtkErrorMacro("Animation cue already present in the scene");
     return;
+    }
+  // ensure that Cue objects have been created.
+  if (!cue->GetObjectsCreated())
+    {
+    cue->UpdateVTKObjects();
     }
   scene->AddCue(cue->GetAnimationCue());
   this->AnimationCueProxies->AddItem(cue);
