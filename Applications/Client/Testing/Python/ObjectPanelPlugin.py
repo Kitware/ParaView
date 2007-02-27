ec19fabe02cf83046a84f0242772950d3177c1bd
Clinton Stimpson <clinton@elemtech.com>
2007-02-27 18:34:20 -0500

    ENH:  Add some tests for plugins.

---
+++ Applications/Client/Testing/Python/ObjectPanelPlugin.py
@@ -0,0 +1,30 @@
+#/usr/bin/env python
+
+import QtTesting
+import sys
+
+libname = 'libGUIConePanel.so'
+if sys.platform == 'win32':
+    libname = 'GUIConePanel.dll'
+
+if sys.platform == 'darwin':
+    libname = 'libGUIConePanel.dylib'
+
+object1 = 'MainWindow/menubar/menuTools'
+QtTesting.playCommand(object1, 'activate', 'actionManage_Plugins')
+object2 = 'MainWindow/pqPluginDialog/clientGroup/loadClient'
+QtTesting.playCommand(object2, 'activate', '')
+object3 = 'MainWindow/pqPluginDialog/pqFileDialog'
+QtTesting.playCommand(object3, 'filesSelected', libname)
+object4 = 'MainWindow/pqPluginDialog/buttonBox/1QPushButton0'
+QtTesting.playCommand(object4, 'activate', '')
+object5 = 'MainWindow/menubar/menuSources'
+QtTesting.playCommand(object5, 'activate', 'Cone')
+object6 = 'MainWindow/ServerStartupBrowser/connect'
+QtTesting.playCommand(object6, 'activate', '')
+
+object7 = 'MainWindow/objectInspectorDock/1pqProxyTabWidget0/qt_tabwidget_stackedwidget/objectInspector/ScrollArea/qt_scrollarea_viewport/PanelArea/Editor/1QLabel4'
+
+text = QtTesting.getProperty(object7, 'text')
+print text
+
---
+++ Applications/Client/Testing/Python/ToolBarPlugin.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python
+
+import QtTesting
+import sys
+
+libname = 'libGUIMyToolBar.so'
+if sys.platform == 'win32':
+    libname = 'GUIMyToolBar.dll'
+
+if sys.platform == 'darwin':
+    libname = 'libGUIMyToolBar.dylib'
+
+object1 = 'MainWindow/menubar/menuTools'
+QtTesting.playCommand(object1, 'activate', 'actionManage_Plugins')
+object2 = 'MainWindow/pqPluginDialog/clientGroup/loadClient'
+QtTesting.playCommand(object2, 'activate', '')
+object3 = 'MainWindow/pqPluginDialog/pqFileDialog'
+QtTesting.playCommand(object3, 'filesSelected', libname)
+object4 = 'MainWindow/pqPluginDialog/buttonBox/1QPushButton0'
+QtTesting.playCommand(object4, 'activate', '')
+object5 = 'MainWindow/MyActions/1QToolButton0'
+QtTesting.playCommand(object5, 'activate', '')
+object6 = '1QMessageBox0/qt_msgbox_buttonbox/1QPushButton0'
+QtTesting.playCommand(object6, 'activate', '')
+
