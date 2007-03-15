37748bfe68549998d07d84f7475be04524f6e04c
Ken Moreland <kmorel@sandia.gov>
2007-03-15 13:24:38 -0400

    COMP: Fix warning about order of ivar initialization.

--- Qt/Components/pqLookmarkModel.cxx
+++ Qt/Components/pqLookmarkModel.cxx
@@ -59,7 +59,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //-----------------------------------------------------------------------------
 pqLookmarkModel::pqLookmarkModel(QString name, vtkPVXMLElement *state, QObject* _parent /*=null*/)
-  : Name(name), QObject(_parent)
+  : QObject(_parent), Name(name)
 {
   this->RestoreCamera = false;
   this->RestoreData = false;
