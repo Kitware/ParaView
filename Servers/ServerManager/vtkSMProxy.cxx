603ebbdb4921523ff577a59ef0f965bede7ee616
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-03-21 16:00:12 -0400

    BUG: Fixed CopyIDs to create new IDs refereing to same objects rather  than simply copying IDs, so that both proxies can be deleted independently.

--- Servers/ServerManager/vtkSMProxy.cxx
+++ Servers/ServerManager/vtkSMProxy.cxx
@@ -37,7 +37,7 @@
 #include <vtkstd/string>
 
 vtkStandardNewMacro(vtkSMProxy);
-vtkCxxRevisionMacro(vtkSMProxy, "1.88");
+vtkCxxRevisionMacro(vtkSMProxy, "1.89");
 
 vtkCxxSetObjectMacro(vtkSMProxy, XMLElement, vtkPVXMLElement);
 vtkCxxSetObjectMacro(vtkSMProxy, Hints, vtkPVXMLElement);
@@ -2164,7 +2164,19 @@ void vtkSMProxy::CopyIDs(vtkSMProxy* copyTo)
     return;
     }
 
-  copyTo->Internals->IDs = this->Internals->IDs;
+  vtkClientServerStream stream;
+  vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
+  for (unsigned int cc=0; cc < this->Internals->IDs.size(); cc++)
+    {
+    vtkClientServerID newid = pm->GetUniqueID();
+    stream  << vtkClientServerStream::Assign
+      << newid
+      << this->Internals->IDs[cc]
+      << vtkClientServerStream::End;
+    copyTo->Internals->IDs.push_back(newid);
+    }
+  pm->SendStream(copyTo->GetConnectionID(), copyTo->GetServers(), stream);
+
   copyTo->ObjectsCreated = 1;
 }
 
