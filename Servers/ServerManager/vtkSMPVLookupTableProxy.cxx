239dd34d5ab0da4bbf1395c2a3cf5b3ee1599ded
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-03-23 11:59:16 -0400

    ENH: Fixing vtkPVLookupTable to be a vtkColorTransferFunction subclass. Now, vtkPVLookupTable works with actual scalar values instead of normalized scalar values when adding control points. SetRange() scales the control points if ScalePointsWithRange is true.

---
+++ Servers/ServerManager/vtkSMPVLookupTableProxy.cxx
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMPVLookupTableProxy.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMPVLookupTableProxy.h"
+
+#include "vtkObjectFactory.h"
+
+vtkStandardNewMacro(vtkSMPVLookupTableProxy);
+vtkCxxRevisionMacro(vtkSMPVLookupTableProxy, "1.1");
+//-----------------------------------------------------------------------------
+vtkSMPVLookupTableProxy::vtkSMPVLookupTableProxy()
+{
+}
+
+//-----------------------------------------------------------------------------
+vtkSMPVLookupTableProxy::~vtkSMPVLookupTableProxy()
+{
+}
+
+//-----------------------------------------------------------------------------
+void vtkSMPVLookupTableProxy::UpdateVTKObjects()
+{
+  this->Superclass::UpdateVTKObjects();
+  this->InvokeCommand("Build");
+}
+
+//-----------------------------------------------------------------------------
+void vtkSMPVLookupTableProxy::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ Servers/ServerManager/vtkSMPVLookupTableProxy.h
@@ -0,0 +1,46 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMPVLookupTableProxy.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMPVLookupTableProxy - proxy for vtkPVLookupTable.
+// .SECTION Description
+// vtkSMPVLookupTableProxy ensure that Build() is called on every 
+// UpdateVTKObjects();
+
+#ifndef __vtkSMPVLookupTableProxy_h
+#define __vtkSMPVLookupTableProxy_h
+
+#include "vtkSMProxy.h"
+
+class VTK_EXPORT vtkSMPVLookupTableProxy : public vtkSMProxy
+{
+public:
+  static vtkSMPVLookupTableProxy* New();
+  vtkTypeRevisionMacro(vtkSMPVLookupTableProxy, vtkSMProxy);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Overriden to call Build() on the vtkPVLookupTable.
+  virtual void UpdateVTKObjects();
+
+protected:
+  vtkSMPVLookupTableProxy();
+  ~vtkSMPVLookupTableProxy();
+
+private:
+  vtkSMPVLookupTableProxy(const vtkSMPVLookupTableProxy&); // Not implemented.
+  void operator=(const vtkSMPVLookupTableProxy&); // Not implemented.
+};
+
+#endif
+
