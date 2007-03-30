40f50588464b1fea5938a99c01d2660b345e294e
Berk Geveci <berk.geveci@kitware.com>
2007-03-30 10:34:43 -0400

    ENH: Added util module in the paraview module.

---
+++ Utilities/VTKPythonWrapping/paraview/util.py
@@ -0,0 +1,15 @@
+import paraview
+
+def SetOutputWholeExtent(algorithm, extent):
+    """
+    Convenience method to help set the WHOLE_EXTENT() in RequestInformation.
+    Commonly used by programmable filters. The arguments are the algorithm
+    and a tuple/list with 6 elements (xmin, xmax, ymin, ymax, zmin, zmax).
+    Example use:
+     import paraview.util
+     # The output will be of dimensions 10, 1, 1
+     paraview.util.SetOutputWholeExtent(algorithm, (0, 9, 0, 0, 0, 0)
+    """
+    if len(extent) != 6:
+        raise "Expected a sequence of length 6"
+    algorithm.GetExecutive().GetOutputInformation(0).Set(paraview.vtkStreamingDemandDrivenPipeline.WHOLE_EXTENT(), extent[0], extent[1], extent[2],extent[3], extent[4], extent[5])
