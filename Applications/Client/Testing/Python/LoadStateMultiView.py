1c449ed3e71e9537fabc27bc994cfcb6add75aea
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-04-11 19:09:47 -0400

    BUG: Fixed test to point to correct view.

--- Applications/Client/Testing/Python/LoadStateMultiView.py
+++ Applications/Client/Testing/Python/LoadStateMultiView.py
@@ -36,6 +36,6 @@ QtTesting.playCommand(object1, 'activate', 'actionFileLoadServerState')
 QtTesting.playCommand(object3, 'filesSelected', '$PARAVIEW_TEST_ROOT/TestMultiView.pvsm')
 
 QtTesting.wait(1000)
-snapshotWidget = 'MainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/1/Viewport'
+snapshotWidget = 'MainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/0/Viewport'
 QtTestingImage.compareImage(snapshotWidget, 'LoadStateMultiView.png', 200, 200);
 
