80bae058b9ea63e308d47e9bd80ca9be5e534237
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-04-17 15:48:57 -0400

    COMP: fixed errors

--- Qt/Widgets/pqSignalAdaptors.cxx
+++ Qt/Widgets/pqSignalAdaptors.cxx
@@ -34,6 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "pqSignalAdaptors.h"
 
 // Qt includes
+#include <QColor>
 #include <QComboBox>
 #include <QSlider>
 #include <QTextEdit>
