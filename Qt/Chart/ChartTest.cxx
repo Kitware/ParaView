df925bdc6342c4ab45e3523d2ed6dedf9e251b21
Clinton Stimpson <clinton@elemtech.com>
2007-05-21 18:34:51 -0400

    ENH:  Add coverage for color map stuff.

--- Qt/Chart/ChartTest.cxx
+++ Qt/Chart/ChartTest.cxx
@@ -3,6 +3,7 @@
 #include "pqChartValue.h"
 #include "pqColorMapColorChanger.h"
 #include "pqColorMapWidget.h"
+#include "pqColorMapModel.h"
 #include "pqHistogramChart.h"
 #include "pqHistogramListModel.h"
 #include "pqHistogramWidget.h"
@@ -26,17 +27,19 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
 
   // Set up the color map.
-  /*pqColorMapWidget *colorMap = new pqColorMapWidget();
+  pqColorMapWidget *colorMap = new pqColorMapWidget();
+  pqColorMapModel* cmodel = new pqColorMapModel(colorMap);
+  colorMap->setModel(cmodel);
   colorMap->resize(250, 50);
   //colorMap->setTableSize(13);
-  colorMap->addPoint(pqChartValue((double)0.0), QColor::fromHsv(240, 255, 255));
-  colorMap->addPoint(pqChartValue((double)1.0), QColor::fromHsv(0, 255, 255));
-  colorMap->addPoint(pqChartValue((double)0.5), QColor::fromHsv(60, 255, 255));
+  cmodel->addPoint(pqChartValue((double)0.0), QColor::fromHsv(240, 255, 255));
+  cmodel->addPoint(pqChartValue((double)1.0), QColor::fromHsv(0, 255, 255));
+  cmodel->addPoint(pqChartValue((double)0.5), QColor::fromHsv(60, 255, 255));
 
   // The color changer will be cleaned up when the color map is deleted.
   new pqColorMapColorChanger(colorMap);
 
-  colorMap->show();*/
+  colorMap->show();
 
   // Set up the histogram.
   pqHistogramWidget *histogram = new pqHistogramWidget();
@@ -105,7 +108,7 @@ int main(int argc, char *argv[])
     }
   int status = app.exec();
 
-  //delete colorMap;
+  delete colorMap;
   delete histogram;
   delete model;
   delete lines;
