07e5207fbc33cc626cc5c2891018ce285fa6a7c8
Amy Squillacote <amy.squillacote@kitware.com>
2007-06-14 14:02:00 -0400

    ENH: adding polygon cell

--- Servers/ServerManager/Testing/Python/CellIntegrator.py
+++ Servers/ServerManager/Testing/Python/CellIntegrator.py
@@ -33,35 +33,40 @@ reader2.GetProperty("FileName").SetElement(0, file2)
 reader2.UpdateVTKObjects()
 reader2Output = paraview.Fetch(reader2)
 
-# PolyLine
-if paraview.IntegrateCell(reader2Output, 1) != 1.0:
-    print "ERROR: incorrect result for cell 1 of 2nd dataset"
+# Line
+if paraview.IntegrateCell(reader2Output, 2) != 1.0:
+    print "ERROR: incorrect result for cell 2 of 2nd dataset"
     sys.exit(1)
 
 # Triangle
-if paraview.IntegrateCell(reader2Output, 3) != 0.5:
-    print "ERROR: incorrect result for cell 3 of 2nd dataset"
+if paraview.IntegrateCell(reader2Output, 4) != 0.5:
+    print "ERROR: incorrect result for cell 4 of 2nd dataset"
     sys.exit(1)
 
 # Quad
-if paraview.IntegrateCell(reader2Output, 5) != 1.0:
-    print "ERROR: incorrect result for cell 5 of 2nd dataset"
-    sys.exit(1)
-
-# Pixel
 if paraview.IntegrateCell(reader2Output, 6) != 1.0:
     print "ERROR: incorrect result for cell 6 of 2nd dataset"
     sys.exit(1)
 
-# Tetrahedron
-integVal = paraview.IntegrateCell(reader2Output, 7)
-if integVal < 0.166 or integVal > 0.167:
+# Pixel
+if paraview.IntegrateCell(reader2Output, 7) != 1.0:
     print "ERROR: incorrect result for cell 7 of 2nd dataset"
     sys.exit(1)
 
+# Tetrahedron
+integVal = paraview.IntegrateCell(reader2Output, 8)
+if integVal < 0.166 or integVal > 0.167:
+    print "ERROR: incorrect result for cell 8 of 2nd dataset"
+    sys.exit(1)
+
 # Voxel
-if paraview.IntegrateCell(reader2Output, 12) != 1.0:
-    print "ERROR: incorrect result for cell 12 of 2nd dataset"
+if paraview.IntegrateCell(reader2Output, 13) != 1.0:
+    print "ERROR: incorrect result for cell 13 of 2nd dataset"
+    sys.exit(1)
+
+# Polygon
+if paraview.IntegrateCell(reader2Output, 0) != 1.0:
+    print "ERROR: incorrect result for cell 0 of 2nd dataset"
     sys.exit(1)
 
 file3 = os.path.join(SMPythonTesting.DataDir, "Data/blow.vtk")
@@ -79,6 +84,7 @@ filter2.GetProperty("Input").AddProxy(filter1.SMProxy, 0)
 filter2.UpdateVTKObjects()
 filter2Output = paraview.Fetch(filter2)
 
+# Triangle Strip
 integVal = paraview.IntegrateCell(filter2Output, 200)
 if integVal < 0.569 or integVal > 0.570:
     print "ERROR: incorrect result for cell 200 of 3rd dataset"
