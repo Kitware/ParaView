6929880001aac9ae3069545542fb4f59e23b774f
Berk Geveci <berk.geveci@kitware.com>
2007-06-21 15:33:27 -0400

    BUG: Array list was not cleared before new arrays were added

--- Servers/Common/vtkPVDataSetAttributesInformation.cxx
+++ Servers/Common/vtkPVDataSetAttributesInformation.cxx
@@ -29,7 +29,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVDataSetAttributesInformation);
-vtkCxxRevisionMacro(vtkPVDataSetAttributesInformation, "1.11");
+vtkCxxRevisionMacro(vtkPVDataSetAttributesInformation, "1.12");
 
 //----------------------------------------------------------------------------
 vtkPVDataSetAttributesInformation::vtkPVDataSetAttributesInformation()
@@ -114,7 +114,14 @@ void
 vtkPVDataSetAttributesInformation
 ::CopyFromFieldData(vtkFieldData *da)
 {
-  // Copy Point Data
+  // Clear array information.
+  this->ArrayInformation->RemoveAllItems();
+  for (int idx = 0; idx < vtkDataSetAttributes::NUM_ATTRIBUTES; ++idx)
+    {
+    this->AttributeIndices[idx] = -1;
+    }
+
+  // Copy Field Data
   int num = da->GetNumberOfArrays();
   for (int idx = 0; idx < num; ++idx)
     {
