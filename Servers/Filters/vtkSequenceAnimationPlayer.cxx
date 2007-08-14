84fc270385a86e891fe0fdc5f7bcee7b26e66f2a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-08-14 09:35:22 -0400

    BUG: Frame no must be increamented on each GetNextTime() before the calculation since that method is called after the 1st tick.

--- Servers/Filters/vtkSequenceAnimationPlayer.cxx
+++ Servers/Filters/vtkSequenceAnimationPlayer.cxx
@@ -17,7 +17,7 @@
 #include "vtkObjectFactory.h"
 
 vtkStandardNewMacro(vtkSequenceAnimationPlayer);
-vtkCxxRevisionMacro(vtkSequenceAnimationPlayer, "1.2");
+vtkCxxRevisionMacro(vtkSequenceAnimationPlayer, "1.3");
 //----------------------------------------------------------------------------
 vtkSequenceAnimationPlayer::vtkSequenceAnimationPlayer()
 {
@@ -41,9 +41,9 @@ void vtkSequenceAnimationPlayer::StartLoop(double starttime, double endtime)
 //----------------------------------------------------------------------------
 double vtkSequenceAnimationPlayer::GetNextTime(double vtkNotUsed(curtime))
 {
+  this->FrameNo++;
   double time = this->StartTime + 
     ((this->EndTime - this->StartTime)*this->FrameNo)/(this->NumberOfFrames-1);
-  this->FrameNo++;
   return time;
 }
 
