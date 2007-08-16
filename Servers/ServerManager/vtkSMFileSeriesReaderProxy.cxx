a72ea02adca833a2a965a31c92ccd54ee0cfd353
Berk Geveci <berk.geveci@kitware.com>
2007-08-16 10:13:58 -0400

    ENH: Added proxy for vtkFileSeriesReader.

---
+++ Servers/ServerManager/vtkSMFileSeriesReaderProxy.cxx
@@ -0,0 +1,94 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMFileSeriesReaderProxy.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMFileSeriesReaderProxy.h"
+
+#include "vtkClientServerStream.h"
+#include "vtkObjectFactory.h"
+#include "vtkPVXMLElement.h"
+#include "vtkProcessModule.h"
+#include "vtkSMProxyProperty.h"
+
+vtkStandardNewMacro(vtkSMFileSeriesReaderProxy);
+vtkCxxRevisionMacro(vtkSMFileSeriesReaderProxy, "1.1");
+
+//-----------------------------------------------------------------------------
+vtkSMFileSeriesReaderProxy::vtkSMFileSeriesReaderProxy()
+{
+  this->FileNameMethod = 0;
+}
+
+//-----------------------------------------------------------------------------
+vtkSMFileSeriesReaderProxy::~vtkSMFileSeriesReaderProxy()
+{
+  this->SetFileNameMethod(0);
+}
+
+//-----------------------------------------------------------------------------
+void vtkSMFileSeriesReaderProxy::CreateVTKObjects()
+{
+  if (this->ObjectsCreated)
+    {
+    return;
+    }
+
+  this->Superclass::CreateVTKObjects();
+
+  if (!this->ObjectsCreated)
+    {
+    return;
+    }
+
+  vtkSMSourceProxy* reader = 
+    vtkSMSourceProxy::SafeDownCast(this->GetSubProxy("Reader"));
+  if (!reader)
+    {
+    vtkErrorMacro("Missing subproxy: Reader");
+    return;
+    }
+
+  vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
+  vtkClientServerStream stream;
+  stream << vtkClientServerStream::Invoke 
+         << this->GetID() << "SetReader" << reader->GetID() 
+         << vtkClientServerStream::End;
+  if (this->GetFileNameMethod())
+    {
+    stream << vtkClientServerStream::Invoke
+           << this->GetID() 
+           << "SetFileNameMethod" 
+           << this->GetFileNameMethod()
+           << vtkClientServerStream::End;
+    }
+  pm->SendStream(this->ConnectionID, this->Servers, stream);
+} 
+
+//---------------------------------------------------------------------------
+int vtkSMFileSeriesReaderProxy::ReadXMLAttributes(
+  vtkSMProxyManager* pm, vtkPVXMLElement* element)
+{
+  const char* setFileNameMethod = element->GetAttribute("file_name_method");
+  if(setFileNameMethod)
+    {
+    this->SetFileNameMethod(setFileNameMethod);
+    }
+
+  return this->Superclass::ReadXMLAttributes(pm, element);
+}
+
+//-----------------------------------------------------------------------------
+void vtkSMFileSeriesReaderProxy::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ Servers/ServerManager/vtkSMFileSeriesReaderProxy.h
@@ -0,0 +1,59 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMFileSeriesReaderProxy.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMFileSeriesReaderProxy - Proxy for vtkFileSeriesReader
+// .SECTION Description
+// vtkSMFileSeriesReaderProxy is used to manager vtkFileSeriesReaders.
+// It creates the internal reader as a sub-proxy and sets it on the
+// meta-reader in CreateVTKObjects(). It also sets FileNameMethod from
+// the xml attribute file_name_method.
+// .SECTION See Also
+// vtkFileSeriesReader
+
+#ifndef __vtkSMFileSeriesReaderProxy_h
+#define __vtkSMFileSeriesReaderProxy_h
+
+#include "vtkSMSourceProxy.h"
+
+class VTK_EXPORT vtkSMFileSeriesReaderProxy : public vtkSMSourceProxy
+{
+public:
+  static vtkSMFileSeriesReaderProxy* New();
+  vtkTypeRevisionMacro(vtkSMFileSeriesReaderProxy, vtkSMSourceProxy);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+  vtkSMFileSeriesReaderProxy();
+  ~vtkSMFileSeriesReaderProxy();
+
+  // Description:
+  // Read attributes from an XML element.
+  virtual int ReadXMLAttributes(vtkSMProxyManager* pm, vtkPVXMLElement* element);
+
+  virtual void CreateVTKObjects();
+
+  // This is the name of the method used to set the file name on the
+  // internal reader. See vtkFileSeriesReader for details.
+  vtkSetStringMacro(FileNameMethod);
+  vtkGetStringMacro(FileNameMethod);
+
+  char* FileNameMethod;
+
+private:
+  vtkSMFileSeriesReaderProxy(const vtkSMFileSeriesReaderProxy&); // Not implemented.
+  void operator=(const vtkSMFileSeriesReaderProxy&); // Not implemented.
+};
+
+#endif
+
