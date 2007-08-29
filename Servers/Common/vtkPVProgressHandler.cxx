33c7ea341fbc56ccc6f5d7a07421716aa7e437f1
Berk Geveci <berk.geveci@kitware.com>
2007-08-29 10:53:54 -0400

    BUG: Progress of wrong algorithm was reported when the current algorithm reached 100 percent. Fixed.

--- Servers/Common/vtkPVProgressHandler.cxx
+++ Servers/Common/vtkPVProgressHandler.cxx
@@ -32,7 +32,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVProgressHandler);
-vtkCxxRevisionMacro(vtkPVProgressHandler, "1.7");
+vtkCxxRevisionMacro(vtkPVProgressHandler, "1.8");
 
 //----------------------------------------------------------------------------
 //****************************************************************************
@@ -378,6 +378,15 @@ int vtkPVProgressHandler::ReceiveProgressFromSatellite(int *id, int* progress)
     }
   *progress = minprog;
   *id = filter;
+
+  // Remove the filter from the progress map once 100% is reported.
+  // Otherwise, it may be reported accidentally when another filter
+  // hits 100%
+  if (*progress == 100)
+    {
+    this->Internals->ProgressMap.erase(
+      this->Internals->ProgressMap.find(*id));
+    }
   return rec;
 }
 
