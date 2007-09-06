30a64d6b79ce7cdc00bae228f91781183ff59bde
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-09-06 13:51:59 -0400

    ENH: Moved properties/domains around so that the domains on the XYPlotRepresentation now show arrays produced by the PolyLineToRectilinearGrid filter.

---
+++ Servers/ServerManager/vtkSMXYPlotRepresentationProxy.cxx
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMXYPlotRepresentationProxy.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMXYPlotRepresentationProxy.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkSMSourceProxy.h"
+#include "vtkSMProxyProperty.h"
+
+vtkStandardNewMacro(vtkSMXYPlotRepresentationProxy);
+vtkCxxRevisionMacro(vtkSMXYPlotRepresentationProxy, "1.1");
+//----------------------------------------------------------------------------
+vtkSMXYPlotRepresentationProxy::vtkSMXYPlotRepresentationProxy()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkSMXYPlotRepresentationProxy::~vtkSMXYPlotRepresentationProxy()
+{
+}
+
+//----------------------------------------------------------------------------
+bool vtkSMXYPlotRepresentationProxy::EndCreateVTKObjects()
+{
+  if (!this->Superclass::EndCreateVTKObjects())
+    {
+    return false;
+    }
+
+  vtkSMProxy* subProxy = this->GetSubProxy("DummyConsumer");
+  vtkSMProxyProperty* pp = vtkSMProxyProperty::SafeDownCast(
+    subProxy->GetProperty("Input"));
+  pp->RemoveAllProxies();
+  pp->AddProxy(this->PostProcessorProxy);
+  pp->UpdateDependentDomains();
+}
+
+
+//----------------------------------------------------------------------------
+void vtkSMXYPlotRepresentationProxy::Update(vtkSMViewProxy* view)
+{
+  if (!this->ObjectsCreated)
+    {
+    vtkErrorMacro("Objects not created yet!");
+    return;
+    }
+
+  this->Superclass::Update(view);
+
+  vtkSMProxy* subProxy = this->GetSubProxy("DummyConsumer");
+  vtkSMProxyProperty* pp = vtkSMProxyProperty::SafeDownCast(
+    subProxy->GetProperty("Input"));
+  pp->UpdateDependentDomains();
+}
+
+//----------------------------------------------------------------------------
+void vtkSMXYPlotRepresentationProxy::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+
