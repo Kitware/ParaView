257bf9b76e2e27160347c26d9e3e7cb9d97107da
Clinton Stimpson <clinton@elemtech.com>
2007-09-20 16:26:50 -0400

    BUG: remove extra wait.

--- Applications/Client/Testing/Python/LoadStateMultiView.py
+++ Applications/Client/Testing/Python/LoadStateMultiView.py
@@ -37,7 +37,6 @@ QtTesting.playCommand(object1, 'activate', 'actionFileLoadServerState')
 #QtTesting.playCommand(object2, 'activate', '')
 QtTesting.playCommand(object3, 'filesSelected', '$PARAVIEW_TEST_ROOT/TestMultiView.pvsm')
 
-QtTesting.wait(1000)
 snapshotWidget = 'MainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/0/Viewport'
 QtTestingImage.compareImage(snapshotWidget, 'LoadStateMultiView.png', 200, 200);
 
