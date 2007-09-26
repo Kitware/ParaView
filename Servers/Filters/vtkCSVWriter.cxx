de1d811246fb12b6706a54084b9f0aa6c5e3c8e2
Jon Goldman <jgoldma@sandia.gov>
2007-09-26 18:17:10 -0400

    COMP: deprecated conversion warning

--- Servers/Filters/vtkCSVWriter.cxx
+++ Servers/Filters/vtkCSVWriter.cxx
@@ -31,7 +31,7 @@
 #include <vtkstd/vector>
 
 vtkStandardNewMacro(vtkCSVWriter);
-vtkCxxRevisionMacro(vtkCSVWriter, "1.7");
+vtkCxxRevisionMacro(vtkCSVWriter, "1.8");
 //-----------------------------------------------------------------------------
 vtkCSVWriter::vtkCSVWriter()
 {
@@ -155,7 +155,7 @@ void vtkCSVWriter::WriteData()
     vtkDataSet* ds = vtkDataSet::SafeDownCast(this->GetInput());
     if (ds)
       {
-      static char* pdInfoStr =
+      const char* const pdInfoStr =
         "vtkCSVWriter: input data type needs to be of type vtkPolyData";
       vtkPointSet* ps = vtkPointSet::SafeDownCast(ds);
       if (ps)
@@ -166,7 +166,7 @@ void vtkCSVWriter::WriteData()
           vtkPolyData* clone = vtkPolyData::New();
           clone->ShallowCopy(pd);
 
-          static char* infoStr =
+          const char* const infoStr =
             "input data type is a vtkPolyData."
             " Converting via vtkPolyLineToRectilinearGridFilter";
           vtkDebugMacro(<< infoStr);
