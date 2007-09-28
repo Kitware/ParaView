a9751e29cc0082851395c2e7da669c357939e5be
Clinton Stimpson <clinton@elemtech.com>
2007-09-28 15:09:59 -0400

    BUG:  Add debug statements to hopefully find reason for timeouts on DASH1.

--- Applications/Client/Testing/Python/ToolBarPlugin.py
+++ Applications/Client/Testing/Python/ToolBarPlugin.py
@@ -3,6 +3,8 @@
 import QtTesting
 import sys
 
+print 'start'
+
 libname = 'libGUIMyToolBar.so'
 if sys.platform == 'win32':
     libname = 'GUIMyToolBar.dll'
@@ -10,16 +12,25 @@ if sys.platform == 'win32':
 if sys.platform == 'darwin':
     libname = 'libGUIMyToolBar.dylib'
 
+print 'libname = ' + libname
+
 object1 = 'MainWindow/menubar/menuTools'
 QtTesting.playCommand(object1, 'activate', 'actionManage_Plugins')
+print 'played1'
 object2 = 'MainWindow/pqPluginDialog/clientGroup/loadClient'
 QtTesting.playCommand(object2, 'activate', '')
+print 'played2'
 object3 = 'MainWindow/pqPluginDialog/pqFileDialog'
 QtTesting.playCommand(object3, 'filesSelected', libname)
+print 'played3'
 object4 = 'MainWindow/pqPluginDialog/buttonBox/1QPushButton0'
 QtTesting.playCommand(object4, 'activate', '')
+print 'played4'
 object5 = 'MainWindow/MyActions/1QToolButton0'
 QtTesting.playCommand(object5, 'activate', '')
+print 'played4'
 object6 = '1QMessageBox0/qt_msgbox_buttonbox/1QPushButton0'
 QtTesting.playCommand(object6, 'activate', '')
+
+print 'end'
 
