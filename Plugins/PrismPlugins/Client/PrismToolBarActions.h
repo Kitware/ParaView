25ec88bf5142c27c416a8ab00614eb04c49aef4f
Clinton Stimpson <clinton@elemtech.com>
2007-10-26 14:22:08 -0400

    ENH:  Prism updates  (From Corey)

--- Plugins/PrismPlugins/Client/PrismToolBarActions.h
+++ Plugins/PrismPlugins/Client/PrismToolBarActions.h
@@ -1,13 +1,8 @@
+#ifndef _PrismToolBarActions_h
+#define _PrismToolBarActions_h
 
 #include <QActionGroup>
 #include <QString>
-class pqServerManagerModelItem;
-class pqPipelineSource;
-class pqServer;
-class pqOutputPort;
-class vtkObject;
-class vtkEventQtSlotConnect;
-class pqDataRepresentation;
 
 class PrismToolBarActions : public QActionGroup
 {
@@ -16,31 +11,6 @@ public:
   PrismToolBarActions(QObject* p);
   ~PrismToolBarActions();
 
-public slots:
-  void onSESAMEFileOpen();
-  void onSESAMEFileOpen(const QStringList&);
-  void onCreatePrismView();
-  void onCreatePrismView(const QStringList& files);
-
-private slots:
-  void onSelectionChanged();
-  void onGeometrySelection(vtkObject* caller, unsigned long vtk_event, void* client_data, void* call_data);
-  void onPrismSelection(vtkObject* caller, unsigned long vtk_event, void* client_data, void* call_data);
-  void onPrismRepresentationAdded(pqPipelineSource* source,pqDataRepresentation* repr, int srcOutputPort);
-  void onConnectionAdded(pqPipelineSource* source,pqPipelineSource* consumer);
-
-
-private:
-  pqServerManagerModelItem *getActiveObject() const;
-  pqPipelineSource *getActiveSource() const;
-  pqServer* getActiveServer() const;
-
- QAction *SesameViewAction;
- QAction *PrismViewAction;
-
- vtkEventQtSlotConnect* VTKConnections;
- bool ProcessingEvent;
-
-
 };
+#endif
 
