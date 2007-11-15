ba68c6389d604e59ec51b3bc10e608fdefbf5e84
Clinton Stimpson <clinton@elemtech.com>
2007-11-15 11:57:47 -0500

    BUG:  Fix Windows test failures with not finding files.

--- Servers/Common/vtkPVFileInformation.cxx
+++ Servers/Common/vtkPVFileInformation.cxx
@@ -52,7 +52,7 @@
 #include <vtkstd/string>
 
 vtkStandardNewMacro(vtkPVFileInformation);
-vtkCxxRevisionMacro(vtkPVFileInformation, "1.23");
+vtkCxxRevisionMacro(vtkPVFileInformation, "1.24");
 
 inline void vtkPVFileInformationAddTerminatingSlash(vtkstd::string& name)
 {
@@ -287,7 +287,14 @@ void vtkPVFileInformation::CopyFromObject(vtkObject* object)
   
   this->SetName(helper->GetPath());
 #if defined(_WIN32)
-  path = vtksys::SystemTools::ConvertToWindowsOutputPath(path.c_str());
+  vtkstd::string::size_type idx;
+  for(idx = path.find('/', 0);
+        idx != vtkstd::string::npos;
+        idx = path.find('/', idx))
+    {
+    path.replace(idx, 1, 1, '\\');
+    }
+
   int len = path.size();
   if(len > 4 && path.compare(len-4, 4, ".lnk") == 0)
     {
