b10137b977aad23850eaef5393650753cee039f5
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-01-18 14:04:20 -0500

    BUG: The error message is uncalled for.

--- Servers/ServerManager/vtkSMNetworkImageSourceProxy.cxx
+++ Servers/ServerManager/vtkSMNetworkImageSourceProxy.cxx
@@ -20,7 +20,7 @@
 #include <vtksys/SystemTools.hxx>
 
 vtkStandardNewMacro(vtkSMNetworkImageSourceProxy);
-vtkCxxRevisionMacro(vtkSMNetworkImageSourceProxy, "1.3");
+vtkCxxRevisionMacro(vtkSMNetworkImageSourceProxy, "1.4");
 //----------------------------------------------------------------------------
 vtkSMNetworkImageSourceProxy::vtkSMNetworkImageSourceProxy()
 {
@@ -84,7 +84,6 @@ void vtkSMNetworkImageSourceProxy::UpdateImage()
 {
   if (!this->FileName)
     {
-    vtkErrorMacro("FileName must be set.");
     return;
     }
 
