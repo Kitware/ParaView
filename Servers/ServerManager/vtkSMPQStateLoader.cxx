6a89dd81c529fa63bc9c2c4ca8a324f9ac969f96
Clinton Stimpson <clinton@elemtech.com>
2008-03-20 16:18:26 -0400

    COMP:  use vtkstd to fix compile error on some compilers.

--- Servers/ServerManager/vtkSMPQStateLoader.cxx
+++ Servers/ServerManager/vtkSMPQStateLoader.cxx
@@ -23,7 +23,7 @@
 #include <vtkstd/algorithm>
 
 vtkStandardNewMacro(vtkSMPQStateLoader);
-vtkCxxRevisionMacro(vtkSMPQStateLoader, "1.20");
+vtkCxxRevisionMacro(vtkSMPQStateLoader, "1.21");
 
 struct vtkSMPQStateLoaderInternals
 {
@@ -89,7 +89,7 @@ void vtkSMPQStateLoader::AddPreferredRenderView(vtkSMRenderViewProxy *renderView
     this->PQInternal->PreferredRenderViews.begin();
   vtkstd::list<vtkSmartPointer<vtkSMRenderViewProxy> >::iterator end = 
     this->PQInternal->PreferredRenderViews.end();
-  if(find(begin,end,renderView) == end)
+  if(vtkstd::find(begin,end,renderView) == end)
     {
     this->PQInternal->PreferredRenderViews.push_back(renderView);
     }
