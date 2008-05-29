f4a832bc3eab03e548f49739e4b2ecdef421109f
Philippe Pebay <pppebay@sandia.gov>
2008-05-29 18:20:24 -0400

    COMP: InitializeBinExtent's signature has changed, even for the superclass.

--- Servers/Filters/vtkPExtractHistogram.cxx
+++ Servers/Filters/vtkPExtractHistogram.cxx
@@ -34,7 +34,7 @@
 #include <vtksys/RegularExpression.hxx>
 
 vtkStandardNewMacro(vtkPExtractHistogram);
-vtkCxxRevisionMacro(vtkPExtractHistogram, "1.6");
+vtkCxxRevisionMacro(vtkPExtractHistogram, "1.7");
 vtkCxxSetObjectMacro(vtkPExtractHistogram, Controller, vtkMultiProcessController);
 //-----------------------------------------------------------------------------
 vtkPExtractHistogram::vtkPExtractHistogram()
@@ -62,7 +62,7 @@ bool vtkPExtractHistogram::InitializeBinExtents(
       bin_extents, min, max);
     }
 #ifndef VTK_USE_MPI
-  return this->Superclass::InitializeBinExtents(inputVector, bin_extents);
+  return this->Superclass::InitializeBinExtents( inputVector, bin_extents, min, max );
 
 #else
   int num_processes = this->Controller->GetNumberOfProcesses();
