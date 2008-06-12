3f4f7465546e3bd2f8e1ae87983c38a00a179783
Timothy M. Shead <tshead@sandia.gov>
2008-06-12 19:38:06 -0400

    ENH: Move vtkSMSelectionDeliveryRepresentationProxy into the ParaView source tree.

---
+++ Servers/ServerManager/vtkSMSelectionDeliveryRepresentationProxy.cxx
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMSelectionDeliveryRepresentationProxy.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMSelectionDeliveryRepresentationProxy.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkSMSourceProxy.h"
+
+vtkStandardNewMacro(vtkSMSelectionDeliveryRepresentationProxy);
+vtkCxxRevisionMacro(vtkSMSelectionDeliveryRepresentationProxy, "1.1");
+//----------------------------------------------------------------------------
+vtkSMSelectionDeliveryRepresentationProxy::vtkSMSelectionDeliveryRepresentationProxy()
+{
+  this->SelectionRepresentation = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkSMSelectionDeliveryRepresentationProxy::~vtkSMSelectionDeliveryRepresentationProxy()
+{
+}
+
+//-----------------------------------------------------------------------------
+bool vtkSMSelectionDeliveryRepresentationProxy::BeginCreateVTKObjects()
+{
+  if (!this->Superclass::BeginCreateVTKObjects())
+    {
+    return false;
+    }
+
+  this->SelectionRepresentation =
+    vtkSMClientDeliveryRepresentationProxy::SafeDownCast(
+      this->GetSubProxy("SelectionRepresentation"));
+  if (!this->SelectionRepresentation)
+    {
+    vtkErrorMacro("SelectionRepresentation must be defined in the xml configuration.");
+    return false;
+    }
+  return true;
+}
+
+//-----------------------------------------------------------------------------
+void vtkSMSelectionDeliveryRepresentationProxy::CreatePipeline(
+  vtkSMSourceProxy* input, 
+  int outputport)
+{
+  this->Superclass::CreatePipeline(input, outputport);
+
+  // Connect the selection output from the input to the SelectionRepresentation.
+
+  // Ensure that the source proxy has created extract selection filters.
+  input->CreateSelectionProxies();
+
+  vtkSMSourceProxy* esProxy = input->GetSelectionOutput(outputport);
+  if (!esProxy)
+    {
+    vtkErrorMacro("Input proxy does not support selection extraction.");
+    return;
+    }
+
+  // esProxy port 2 is the input vtkSelection. That's the one we are
+  // interested in.
+  this->Connect(esProxy, this->SelectionRepresentation, "Input", 2);
+}
+
+//----------------------------------------------------------------------------
+void vtkSMSelectionDeliveryRepresentationProxy::Update(vtkSMViewProxy* view)
+{
+  this->Superclass::Update(view);
+  this->SelectionRepresentation->Update(view);
+}
+
+//----------------------------------------------------------------------------
+void vtkSMSelectionDeliveryRepresentationProxy::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+
---
+++ Servers/ServerManager/vtkSMSelectionDeliveryRepresentationProxy.h
@@ -0,0 +1,70 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMSelectionDeliveryRepresentationProxy.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMSelectionDeliveryRepresentationProxy - representation that can be
+// used to show client delivery displays, such as a LineChart in a render view.
+// .SECTION Description
+// vtkSMSelectionDeliveryRepresentationProxy is a representation that can be used
+// to show displays that needs client delivery, such as a LineChart, etc.
+// It also delivers the selection to the client.
+
+#ifndef __vtkSMSelectionDeliveryRepresentationProxy_h
+#define __vtkSMSelectionDeliveryRepresentationProxy_h
+
+#include "vtkSMClientDeliveryRepresentationProxy.h"
+
+class VTK_EXPORT vtkSMSelectionDeliveryRepresentationProxy : 
+  public vtkSMClientDeliveryRepresentationProxy
+{
+public:
+  static vtkSMSelectionDeliveryRepresentationProxy* New();
+  vtkTypeRevisionMacro(vtkSMSelectionDeliveryRepresentationProxy, vtkSMClientDeliveryRepresentationProxy);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkGetObjectMacro(SelectionRepresentation, vtkSMClientDeliveryRepresentationProxy);
+
+  // Description:
+  // Called to update the Display. Default implementation does nothing.
+  // Argument is the view requesting the update. Can be null in the
+  // case when something other than a view is requesting the update.
+  // Overridden to update the internal SelectionRepresentation proxy as well. 
+  virtual void Update() { this->Superclass::Update(); };
+  virtual void Update(vtkSMViewProxy* view);
+
+//BTX
+protected:
+  vtkSMSelectionDeliveryRepresentationProxy();
+  ~vtkSMSelectionDeliveryRepresentationProxy();
+
+  // Description:
+  // This method is called at the beginning of CreateVTKObjects().
+  // If this method returns false, CreateVTKObjects() is aborted.
+  // Overridden to abort CreateVTKObjects() only if the input has
+  // been initialized correctly.
+  virtual bool BeginCreateVTKObjects();
+
+  // Description:
+  // Create the data pipeline.
+  virtual void CreatePipeline(vtkSMSourceProxy* input, int outputport);
+
+  vtkSMClientDeliveryRepresentationProxy* SelectionRepresentation;
+
+private:
+  vtkSMSelectionDeliveryRepresentationProxy(const vtkSMSelectionDeliveryRepresentationProxy&); // Not implemented
+  void operator=(const vtkSMSelectionDeliveryRepresentationProxy&); // Not implemented
+//ETX
+};
+
+#endif
+
