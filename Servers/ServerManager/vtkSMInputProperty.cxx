ed8d13e914b6ea0e159d2c36e205bebf93238def
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-07-03 09:00:48 -0400

    BUG: Fixed BUG # 7275. SetProxies was incorrectly updating the internal port numbers vector.

--- Servers/ServerManager/vtkSMInputProperty.cxx
+++ Servers/ServerManager/vtkSMInputProperty.cxx
@@ -25,7 +25,7 @@
 #include <vtkstd/vector>
 
 vtkStandardNewMacro(vtkSMInputProperty);
-vtkCxxRevisionMacro(vtkSMInputProperty, "1.22");
+vtkCxxRevisionMacro(vtkSMInputProperty, "1.23");
 
 int vtkSMInputProperty::InputsUpdateImmediately = 1;
 
@@ -180,8 +180,7 @@ void vtkSMInputProperty::SetProxies(unsigned int numProxies,
   vtkSMProxy* proxies[], unsigned int outputports[])
 {
   this->IPInternals->OutputPorts.clear();
-  this->IPInternals->OutputPorts.resize(numProxies, 0);
-  for (unsigned int cc=0; (outputports != 0) && (cc < numProxies); cc++)
+  for (unsigned int cc=0; cc < numProxies; cc++)
     {
     this->IPInternals->OutputPorts.push_back(outputports[cc]);
     }
