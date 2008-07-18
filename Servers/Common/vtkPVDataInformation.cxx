7c89ca5eb542db403716f9c95c74735346221262
Jeffrey Baumes <jeff.baumes@kitware.com>
2008-07-18 11:31:26 -0400

    BUG: Use row data to get vtkTable's information.

--- Servers/Common/vtkPVDataInformation.cxx
+++ Servers/Common/vtkPVDataInformation.cxx
@@ -47,7 +47,7 @@
 #include <vtkstd/vector>
 
 vtkStandardNewMacro(vtkPVDataInformation);
-vtkCxxRevisionMacro(vtkPVDataInformation, "1.55");
+vtkCxxRevisionMacro(vtkPVDataInformation, "1.56");
 
 //----------------------------------------------------------------------------
 vtkPVDataInformation::vtkPVDataInformation()
@@ -497,7 +497,7 @@ void vtkPVDataInformation::CopyFromTable(vtkTable* data)
   this->NumberOfRows = data->GetNumberOfRows();
 
   // Copy Point Data information
-  this->PointDataInformation->CopyFromFieldData(data->GetFieldData());
+  this->PointDataInformation->CopyFromFieldData(data->GetRowData());
 }
 
 //----------------------------------------------------------------------------
