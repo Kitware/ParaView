449364d0a7a245c8f598260338b0887203c76601
Burlen Loring <burlen.loring@gmail.com>
2008-07-31 11:01:48 -0400

    COMP: Older gcc barfs on a forward declaration when that type is used in a stl container.

--- Servers/Filters/vtkCTHFragmentIdList.h
+++ Servers/Filters/vtkCTHFragmentIdList.h
@@ -24,8 +24,7 @@
 #define __vtkCTHFragmentIdList_h
 
 #include <vtkstd/vector>
-
-class vtkCTHFragmentIdListItem;
+#include "vtkCTHFragmentIdListItem.h"
 
 class vtkCTHFragmentIdList
 {
@@ -68,85 +67,4 @@ class vtkCTHFragmentIdList
     bool IsInitialized;
     vtkstd::vector<vtkCTHFragmentIdListItem> IdList;
 };
-
-class vtkCTHFragmentIdListItem
-{
-  public:
-    enum {LOCAL_ID=0, GLOBAL_ID=1, SIZE=2};
-    //
-    vtkCTHFragmentIdListItem()
-    {
-      this->Clear();
-    }
-    vtkCTHFragmentIdListItem(int globalId)
-    {
-      this->Initialize(-1,globalId);
-    }
-    //
-    vtkCTHFragmentIdListItem(const vtkCTHFragmentIdListItem &other)
-    {
-      this->Initialize(other.GetLocalId(),other.GetGlobalId());
-    }
-    //
-    ~vtkCTHFragmentIdListItem()
-    {
-      this->Clear();
-    }
-    //
-    void Clear()
-    {
-      this->Data[LOCAL_ID]=-1;
-      this->Data[GLOBAL_ID]=-1;
-    }
-    //
-    void Initialize(int localId, int globalId)
-    {
-      this->Data[LOCAL_ID]=localId;
-      this->Data[GLOBAL_ID]=globalId;
-    }
-    //
-    int GetLocalId() const
-    {
-      return this->Data[LOCAL_ID];
-    }
-    //
-    int GetGlobalId() const
-    {
-      return this->Data[GLOBAL_ID];
-    }
-    // Comparison made by global id.
-    bool operator<(vtkCTHFragmentIdListItem &other)
-    {
-      return this->GetGlobalId()<other.GetGlobalId();
-    }
-    // Comparison made by global id.
-    bool operator<=(vtkCTHFragmentIdListItem &other)
-    {
-      return this->GetGlobalId()<=other.GetGlobalId();
-    }
-    // Comparison made by global id.
-    bool operator>(vtkCTHFragmentIdListItem &other)
-    {
-      return this->GetGlobalId()>other.GetGlobalId();
-    }
-    // Comparison made by global id.
-    bool operator>=(vtkCTHFragmentIdListItem &other)
-    {
-      return this->GetGlobalId()>=other.GetGlobalId();
-    }
-    // Comparison made by global id.
-    bool operator==(vtkCTHFragmentIdListItem &other)
-    {
-      return this->GetGlobalId()==other.GetGlobalId();
-    }
-    // 
-    vtkCTHFragmentIdListItem &operator=(const vtkCTHFragmentIdListItem &other)
-    {
-      this->Initialize(other.GetLocalId(),other.GetGlobalId());
-      return *this;
-    }
-  private:
-    int Data[SIZE];
-};
-
 #endif
---
+++ Servers/Filters/vtkCTHFragmentIdListItem.h
@@ -0,0 +1,102 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkCTHFragmentIdListItem.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkCTHFragmentIdListItem
+// .SECTION Description
+// Elemental data type for fragment id list conatiners.
+
+#ifndef __vtkCTHFragmentIdListItem_h
+#define __vtkCTHFragmentIdListItem_h
+
+
+class vtkCTHFragmentIdListItem
+{
+  public:
+    enum {LOCAL_ID=0, GLOBAL_ID=1, SIZE=2};
+    //
+    vtkCTHFragmentIdListItem()
+    {
+      this->Clear();
+    }
+    vtkCTHFragmentIdListItem(int globalId)
+    {
+      this->Initialize(-1,globalId);
+    }
+    //
+    vtkCTHFragmentIdListItem(const vtkCTHFragmentIdListItem &other)
+    {
+      this->Initialize(other.GetLocalId(),other.GetGlobalId());
+    }
+    //
+    ~vtkCTHFragmentIdListItem()
+    {
+      this->Clear();
+    }
+    //
+    void Clear()
+    {
+      this->Data[LOCAL_ID]=-1;
+      this->Data[GLOBAL_ID]=-1;
+    }
+    //
+    void Initialize(int localId, int globalId)
+    {
+      this->Data[LOCAL_ID]=localId;
+      this->Data[GLOBAL_ID]=globalId;
+    }
+    //
+    int GetLocalId() const
+    {
+      return this->Data[LOCAL_ID];
+    }
+    //
+    int GetGlobalId() const
+    {
+      return this->Data[GLOBAL_ID];
+    }
+    // Comparison made by global id.
+    bool operator<(vtkCTHFragmentIdListItem &other)
+    {
+      return this->GetGlobalId()<other.GetGlobalId();
+    }
+    // Comparison made by global id.
+    bool operator<=(vtkCTHFragmentIdListItem &other)
+    {
+      return this->GetGlobalId()<=other.GetGlobalId();
+    }
+    // Comparison made by global id.
+    bool operator>(vtkCTHFragmentIdListItem &other)
+    {
+      return this->GetGlobalId()>other.GetGlobalId();
+    }
+    // Comparison made by global id.
+    bool operator>=(vtkCTHFragmentIdListItem &other)
+    {
+      return this->GetGlobalId()>=other.GetGlobalId();
+    }
+    // Comparison made by global id.
+    bool operator==(vtkCTHFragmentIdListItem &other)
+    {
+      return this->GetGlobalId()==other.GetGlobalId();
+    }
+    // 
+    vtkCTHFragmentIdListItem &operator=(const vtkCTHFragmentIdListItem &other)
+    {
+      this->Initialize(other.GetLocalId(),other.GetGlobalId());
+      return *this;
+    }
+  private:
+    int Data[SIZE];
+};
+#endif
