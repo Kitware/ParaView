7f4015d1f5a34315e17142f660c791b622db0425
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-08-04 10:41:12 -0400

    BUG: import camera to ensure that View::GetActiveCamera returns a wrapped vtkCamera

--- Utilities/VTKPythonWrapping/paraview/vtk.py
+++ Utilities/VTKPythonWrapping/paraview/vtk.py
@@ -4,7 +4,9 @@ if os.name == "posix":
     from libvtkCommonPython import *
     from libvtkFilteringPython import *
     from libvtkGraphicsPython import *
+    from libvtkRenderingPython import vtkCamera
 else:
     from vtkCommonPython import *
     from vtkFilteringPython import *
     from vtkGraphicsPython import *
+    from vtkRenderingPython import vtkCamera
