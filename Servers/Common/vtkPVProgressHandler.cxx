cd8140e4d799905905db2b56557e0c0ba45c2466
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-08-21 09:28:59 -0400

    COMP: fixed warnings

--- Servers/Common/vtkPVProgressHandler.cxx
+++ Servers/Common/vtkPVProgressHandler.cxx
@@ -33,7 +33,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVProgressHandler);
-vtkCxxRevisionMacro(vtkPVProgressHandler, "1.10");
+vtkCxxRevisionMacro(vtkPVProgressHandler, "1.11");
 
 //----------------------------------------------------------------------------
 //****************************************************************************
@@ -247,7 +247,10 @@ void vtkPVProgressHandler::InvokeRootNodeProgressEvent(
     {
     this->HandleProgress(0, it->second, myprogress);
     }
-  while ( this->ReceiveProgressFromSatellite(&id, &progress) );
+  while ( this->ReceiveProgressFromSatellite(&id, &progress) ) 
+    {
+    // EMPTY
+    }
   if (id >= 0)
     {
     vtkClientServerID nid;
@@ -279,7 +282,10 @@ void vtkPVProgressHandler::InvokeRootNodeServerProgressEvent(
     {
     this->HandleProgress(0, it->second, myprogress);
     }
-  while ( this->ReceiveProgressFromSatellite(&id, &progress) );
+  while ( this->ReceiveProgressFromSatellite(&id, &progress) )
+    {
+    // EMPTY
+    }
   vtkClientServerID nid;
   nid.ID = id;
   vtkObjectBase* base = pm->GetInterpreter()->GetObjectFromID(nid, 1);
