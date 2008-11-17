fde1f931f559e60b8539097f2cef140a29b69357
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-11-17 16:54:25 -0500

    BUG: Cleanup -- make vtkSMTextSourceRepresentationProxy be a subclass of vtkSMClientDeliveryRepresentation and avoid code duplication.

--- Servers/ServerManager/vtkSMTextSourceRepresentationProxy.cxx
+++ Servers/ServerManager/vtkSMTextSourceRepresentationProxy.cxx
@@ -14,49 +14,34 @@
 =========================================================================*/
 #include "vtkSMTextSourceRepresentationProxy.h"
 
-#include "vtkAlgorithm.h"
-#include "vtkClientServerStream.h"
 #include "vtkObjectFactory.h"
-#include "vtkProcessModule.h"
-#include "vtkSMDoubleVectorProperty.h"
-#include "vtkSMInputProperty.h"
-#include "vtkSMIntVectorProperty.h"
-#include "vtkSMTextWidgetRepresentationProxy.h"
-#include "vtkSMProxyProperty.h"
-#include "vtkSMSourceProxy.h"
 #include "vtkSMStringVectorProperty.h"
+#include "vtkSMTextWidgetRepresentationProxy.h"
 #include "vtkSMViewProxy.h"
 #include "vtkTable.h"
 #include "vtkVariant.h"
-#include "vtkInformation.h"
 
 #include <vtkstd/string>
 
 vtkStandardNewMacro(vtkSMTextSourceRepresentationProxy);
-vtkCxxRevisionMacro(vtkSMTextSourceRepresentationProxy, "1.7");
+vtkCxxRevisionMacro(vtkSMTextSourceRepresentationProxy, "1.8");
 //----------------------------------------------------------------------------
 vtkSMTextSourceRepresentationProxy::vtkSMTextSourceRepresentationProxy()
 {
-  this->Dirty = true;
-  this->UpdateSuppressorProxy = 0;
   this->TextWidgetProxy = 0;
-  this->CollectProxy = 0;
 }
 
 //----------------------------------------------------------------------------
 vtkSMTextSourceRepresentationProxy::~vtkSMTextSourceRepresentationProxy()
 {
-  this->UpdateSuppressorProxy = 0;
   this->TextWidgetProxy = 0;
-  this->CollectProxy = 0;
 }
 
 //----------------------------------------------------------------------------
 bool vtkSMTextSourceRepresentationProxy::AddToView(vtkSMViewProxy* view)
 {
   this->CreateVTKObjects();
-
-  if(!this->TextWidgetProxy->AddToView(view))
+  if (!this->TextWidgetProxy->AddToView(view))
     {
     return false;
     }
@@ -67,11 +52,11 @@ bool vtkSMTextSourceRepresentationProxy::AddToView(vtkSMViewProxy* view)
 //----------------------------------------------------------------------------
 bool vtkSMTextSourceRepresentationProxy::RemoveFromView(vtkSMViewProxy* view)
 {
-  if(!this->TextWidgetProxy->RemoveFromView(view))
+  if (!this->TextWidgetProxy->RemoveFromView(view))
     {
     return false;
     }
-  
+
   return this->Superclass::RemoveFromView(view);
 }
 
@@ -83,23 +68,12 @@ bool vtkSMTextSourceRepresentationProxy::BeginCreateVTKObjects()
     return false;
     }
 
-  this->UpdateSuppressorProxy = vtkSMSourceProxy::SafeDownCast(
-    this->GetSubProxy("UpdateSuppressor"));
-  this->UpdateSuppressorProxy->SetServers(vtkProcessModule::DATA_SERVER
-    | vtkProcessModule::CLIENT);
-
   this->TextWidgetProxy = vtkSMTextWidgetRepresentationProxy::SafeDownCast(
     this->GetSubProxy("TextWidgetRepresentation"));
-
-  if(!this->TextWidgetProxy)
+  if (!this->TextWidgetProxy)
     {
     return false;
     }
-
-  this->CollectProxy = vtkSMSourceProxy::SafeDownCast(
-    this->GetSubProxy("Collect"));
-  this->CollectProxy->SetServers(
-    vtkProcessModule::DATA_SERVER|vtkProcessModule::CLIENT);
 
   return true;
 }
@@ -107,68 +81,14 @@ bool vtkSMTextSourceRepresentationProxy::BeginCreateVTKObjects()
 //----------------------------------------------------------------------------
 bool vtkSMTextSourceRepresentationProxy::EndCreateVTKObjects()
 {
-  // Init UpdateSuppressor properties.
-  // Seems like we can't use properties for this 
-  // to work properly.
-  vtkClientServerStream stream;
-  vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
-  stream << vtkClientServerStream::Invoke
-         << pm->GetProcessModuleID() << "GetNumberOfLocalPartitions"
-         << vtkClientServerStream::End
-         << vtkClientServerStream::Invoke
-         << this->UpdateSuppressorProxy->GetID() << "SetUpdateNumberOfPieces"
-         << vtkClientServerStream::LastResult
-         << vtkClientServerStream::End;
-  stream << vtkClientServerStream::Invoke
-         << pm->GetProcessModuleID() << "GetPartitionId"
-         << vtkClientServerStream::End
-         << vtkClientServerStream::Invoke
-         << this->UpdateSuppressorProxy->GetID() << "SetUpdatePiece"
-         << vtkClientServerStream::LastResult
-         << vtkClientServerStream::End;
-  pm->SendStream(this->ConnectionID,
-    this->UpdateSuppressorProxy->GetServers(), stream);
+  if (!this->Superclass::EndCreateVTKObjects())
+    {
+    return false;
+    }
 
-  stream << vtkClientServerStream::Invoke
-         << this->CollectProxy->GetID() << "SetProcessModuleConnection"
-         << pm->GetConnectionClientServerID(this->GetConnectionID())
-         << vtkClientServerStream::End;
-  pm->SendStream(this->ConnectionID, 
-                 this->CollectProxy->GetServers(), stream);
-  
-  return this->Superclass::EndCreateVTKObjects();
-}
-
-//----------------------------------------------------------------------------
-void vtkSMTextSourceRepresentationProxy::AddInput(unsigned int inputPort,
-                                                  vtkSMSourceProxy* input,
-                                                  unsigned int outputPort,
-                                                  const char* method)
-{
-  this->Superclass::AddInput(inputPort, input, outputPort, method);
-
-  vtkSMInputProperty* ip;
-  ip = vtkSMInputProperty::SafeDownCast(
-    this->CollectProxy->GetProperty("Input"));
-  ip->RemoveAllProxies();
-  ip->AddInputConnection(input, outputPort);
-
-  vtkSMIntVectorProperty* ivp = vtkSMIntVectorProperty::SafeDownCast(
-    this->CollectProxy->GetProperty("OutputDataType"));
-  ivp->SetElement(0, VTK_TABLE);
-  this->CollectProxy->UpdateVTKObjects();
-
-  // It is essential to update the CollectProxy before connecting it in the
-  // pipeline since we changed the OutputDataType.
-
-  ip = vtkSMInputProperty::SafeDownCast(
-    this->UpdateSuppressorProxy->GetProperty("Input"));
-  ip->RemoveAllProxies();
-  ip->AddProxy(this->CollectProxy);
-  this->UpdateSuppressorProxy->UpdateVTKObjects();
-  
-
-  this->Dirty = true;
+  // We want to deliver the text from the 1st node alone.
+  this->SetReductionType(FIRST_NODE_ONLY);
+  return true;
 }
 
 //----------------------------------------------------------------------------
@@ -180,37 +100,9 @@ void vtkSMTextSourceRepresentationProxy::Update(vtkSMViewProxy* view)
     return;
     }
 
-  // check if we should UseCache
-
-  if (this->ViewInformation->Has(vtkSMViewProxy::USE_CACHE()))
-    {
-    if(this->ViewInformation->Get(vtkSMViewProxy::USE_CACHE())>0)
-      {
-      if (this->ViewInformation->Has(vtkSMViewProxy::CACHE_TIME()))
-        {
-        vtkSMDoubleVectorProperty* dvp = vtkSMDoubleVectorProperty::SafeDownCast(
-          this->UpdateSuppressorProxy->GetProperty("CacheUpdate"));
-        dvp->SetElement(0, this->ViewInformation->Get(vtkSMViewProxy::CACHE_TIME()));
-        this->UpdateSuppressorProxy->UpdateProperty("CacheUpdate", 1);
-        return;
-        }
-      }
-    }
-
-  if (!this->Dirty)
-    {
-    return;
-    }
-
-  this->Dirty = false;
-  this->UpdateSuppressorProxy->InvokeCommand("ForceUpdate");
   this->Superclass::Update(view);
 
-  vtkProcessModule* pm  = vtkProcessModule::GetProcessModule();
-  vtkAlgorithm* dp = vtkAlgorithm::SafeDownCast(
-    pm->GetObjectFromID(this->CollectProxy->GetID()));
-
-  vtkTable* data = vtkTable::SafeDownCast(dp->GetOutputDataObject(0));
+  vtkTable* data = vtkTable::SafeDownCast(this->GetOutput());
   vtkstd::string text = "";
   if (data->GetNumberOfRows() > 0 && data->GetNumberOfColumns() > 0)
     {
@@ -224,28 +116,6 @@ void vtkSMTextSourceRepresentationProxy::Update(vtkSMViewProxy* view)
   this->TextWidgetProxy->UpdateProperty("Text");
 
  // this->InvokeEvent(vtkSMViewProxy::ForceUpdateEvent);
-}
-
-//----------------------------------------------------------------------------
-void vtkSMTextSourceRepresentationProxy::MarkDirty(vtkSMProxy* modifiedProxy)
-{
-  if (modifiedProxy != this)
-    {
-    this->Dirty = true;
-    }
-
-  this->Superclass::MarkDirty(modifiedProxy);
-}
-
-//-----------------------------------------------------------------------------
-void vtkSMTextSourceRepresentationProxy::SetUpdateTimeInternal(double time)
-{
-  vtkSMDoubleVectorProperty* dvp = vtkSMDoubleVectorProperty::SafeDownCast(
-    this->UpdateSuppressorProxy->GetProperty("UpdateTime"));
-  dvp->SetElement(0, time);
-
-  // Calls MarkUpstreamModified().
-  this->Superclass::SetUpdateTimeInternal(time);
 }
 
 //----------------------------------------------------------------------------
--- Servers/ServerManager/vtkSMTextSourceRepresentationProxy.h
+++ Servers/ServerManager/vtkSMTextSourceRepresentationProxy.h
@@ -19,37 +19,17 @@
 #ifndef __vtkSMTextSourceRepresentationProxy_h
 #define __vtkSMTextSourceRepresentationProxy_h
 
-#include "vtkSMDataRepresentationProxy.h"
+#include "vtkSMClientDeliveryRepresentationProxy.h"
 
 class vtkSMTextWidgetRepresentationProxy;
 class vtkSMViewProxy;
 
-class VTK_EXPORT vtkSMTextSourceRepresentationProxy : public vtkSMDataRepresentationProxy
+class VTK_EXPORT vtkSMTextSourceRepresentationProxy : public vtkSMClientDeliveryRepresentationProxy
 {
 public:
   static vtkSMTextSourceRepresentationProxy* New();
-  vtkTypeRevisionMacro(vtkSMTextSourceRepresentationProxy, vtkSMDataRepresentationProxy);
+  vtkTypeRevisionMacro(vtkSMTextSourceRepresentationProxy, vtkSMClientDeliveryRepresentationProxy);
   void PrintSelf(ostream& os, vtkIndent indent);
-
-  // Description:
-  // Called when setting input using the Input property.
-  // Subclasses must override this method to set the input 
-  // to the display pipeline.
-  virtual void AddInput(unsigned int inputPort,
-                        vtkSMSourceProxy* input,
-                        unsigned int outputPort,
-                        const char* method);
-  virtual void AddInput(vtkSMSourceProxy* input,
-                        const char* method)
-  {
-    this->AddInput(0, input, 0, method);
-  }
-
-  // Description:
-  // This method returns if the Update() or UpdateDistributedGeometry()
-  // calls will actually lead to an Update. This is used by the render module
-  // to decide if it can expect any pipeline updates.
-  virtual bool UpdateRequired() { return this->Dirty? true : false; }
 
   // Description:
   // Called to update the Display. Default implementation does nothing.
@@ -57,13 +37,6 @@ public:
   // case when something other than a view is requesting the update.
   virtual void Update() { this->Update(0); };
   virtual void Update(vtkSMViewProxy* view);
-
-  // Description:
-  // Overridden to make the Strategy modified as well.
-  // The strategy is not marked dirty if the modifiedProxy == this, 
-  // thus if the changes to representation itself invalidates the data pipelines
-  // it must explicity mark the strategy invalid.
-  virtual void MarkDirty(vtkSMProxy* modifiedProxy);
 
 // BTX
 protected:
@@ -76,11 +49,6 @@ protected:
   // on the subproxies.
   // If this method returns false, CreateVTKObjects() is aborted.
   virtual bool BeginCreateVTKObjects();
-
-  // Description:
-  // This method is called after CreateVTKObjects(). 
-  // This gives subclasses an opportunity to do some post-creation
-  // initialization.
   virtual bool EndCreateVTKObjects();
 
   // Description:
@@ -95,18 +63,7 @@ protected:
   // Currently a representation can be added to only one view.
   virtual bool RemoveFromView(vtkSMViewProxy* view);
 
-  // Description:
-  // Pass the actual update time to use to all strategies.
-  // When not using strategies, make sure that this method is overridden to pass
-  // the update time correctly.
-  virtual void SetUpdateTimeInternal(double time);
-
   vtkSMTextWidgetRepresentationProxy* TextWidgetProxy;
-
-  vtkSMSourceProxy* UpdateSuppressorProxy;
-  vtkSMSourceProxy* CollectProxy;
-  bool Dirty;
-
 private:
   vtkSMTextSourceRepresentationProxy(const vtkSMTextSourceRepresentationProxy&); // Not implemented
   void operator=(const vtkSMTextSourceRepresentationProxy&); // Not implemented
