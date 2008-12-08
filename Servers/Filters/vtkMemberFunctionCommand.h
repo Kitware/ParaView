6ab63e999f98da338ff4d8bf027b988ca3cedf65
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-12-08 10:36:26 -0500

    ENH: Making is possible to set up a callback that takes arguments.

--- Servers/Filters/vtkMemberFunctionCommand.h
+++ Servers/Filters/vtkMemberFunctionCommand.h
@@ -29,25 +29,20 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
-
-#include <vtkCommand.h>
-
-/////////////////////////////////////////////////////////////////////////
-// pqCommandMemFun
-
-// .NAME vtkMemberFunctionCommand - Call a class member method in response to a VTK event
+// .NAME vtkMemberFunctionCommand - Call a class member method in response to a
+// VTK event.
 // .SECTION Description
-// vtkMemberFunctionCommand is a vtkCommand-derivative that will listen for VTK events,
-// calling a class member function when a VTK event is received.
+// vtkMemberFunctionCommand is a vtkCommand-derivative that will listen for VTK
+// events, calling a class member function when a VTK event is received.
 //
 // It is generally more useful than vtkCallbackCommand, which can only call
 // non-member functions in response to a VTK event.
 //
-// Usage: create an instance of vtkMemberFunctionCommand, specialized for the class that
-// will receive events.  Use the SetCallback() method to pass the instance and
-// member function that will be called when an event is received.  Use
-// vtkObject::AddObserver() to control which VTK events the vtkMemberFunctionCommand
-// object will receive.
+// Usage: create an instance of vtkMemberFunctionCommand, specialized for the
+// class that will receive events.  Use the SetCallback() method to pass the
+// instance and member function that will be called when an event is received.
+// Use vtkObject::AddObserver() to control which VTK events the
+// vtkMemberFunctionCommand object will receive.
 //
 // Usage:
 //
@@ -57,65 +52,98 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // adapter->SetCallback(observer, &foo::bar);
 // subject->AddObserver(vtkCommand::AnyEvent, adapter);
 //
+// Alternative Usage
+//
+// vtkCommand* command = vtkMakeMemberFunctionCommand(*observer, &foo::Callback);
+// subject->AddObserver(vtkCommand::AnyEvent, command);
+//
+// There are two types of callback methods that could be defined.
+// \li void Callback() -- which takes no arguments.
+// \li void Callback(vtkObject* caller, unsigned long event, void* calldata) --
+// which is passed the same arguments as vtkCommand::Execute(), thus making it
+// possible to get additional details about the event.
 // .SECTION See Also
 // vtkCallbackCommand
+
+#include "vtkCommand.h"
+
 template<class ClassT>
 class VTK_EXPORT vtkMemberFunctionCommand :
   public vtkCommand
 {
   typedef vtkMemberFunctionCommand<ClassT> ThisT;
-  
+
 public:
   typedef vtkCommand Superclass;
-  
+
   virtual const char* GetClassNameInternal() const { return "vtkMemberFunctionCommand"; }
-  
+
   static ThisT* SafeDownCast(vtkObjectBase* o)
-  {
+    {
     return dynamic_cast<ThisT*>(o);
-  }
+    }
 
   static ThisT* New()
-  {
+    {
     return new ThisT();
-  }
+    }
 
   void PrintSelf(ostream& os, vtkIndent indent)
-  {
+    {
     vtkCommand::PrintSelf(os, indent);
-  }
+    }
 
   // Description:
   // Set which class instance and member function will be called when a VTK
   // event is received.
   void SetCallback(ClassT& object, void (ClassT::*method)())
-  {
+    {
     this->Object = &object;
     this->Method = method;
-  }
+    }
 
-  virtual void Execute(vtkObject*, unsigned long, void*)
-  {
-    if(this->Object && this->Method)
+  void SetCallback(ClassT& object,
+    void (ClassT::*method2)(vtkObject*, unsigned long, void*))
+    {
+    this->Object = &object;
+    this->Method2 = method2;
+    }
+
+  virtual void Execute(vtkObject* caller, unsigned long event, void* calldata)
+    {
+    if (this->Object && this->Method)
       {
       (this->Object->*this->Method)();
       }
-  }
+    if (this->Object && this->Method2)
+      {
+      (this->Object->*this->Method2)(caller, event, calldata);
+      }
+    }
+  void Reset()
+    {
+    this->Object = 0;
+    this->Method2 = 0;
+    this->Method = 0;
+    }
 
 private:
-  vtkMemberFunctionCommand() :
-    Object(0),
-    Method(0)
-  {
-  }
-  
+  vtkMemberFunctionCommand()
+    {
+    this->Object = 0;
+    this->Method = 0;
+    this->Method2 = 0;
+    }
+
   ~vtkMemberFunctionCommand()
-  {
-  }
-  
+    {
+    }
+
   ClassT* Object;
   void (ClassT::*Method)();
-  
+  void (ClassT::*Method2)(
+    vtkObject* caller, unsigned long event, void* calldata);
+
   vtkMemberFunctionCommand(const vtkMemberFunctionCommand&); // Not implemented
   void operator=(const vtkMemberFunctionCommand&); // Not implemented
 };
@@ -140,6 +168,14 @@ vtkMemberFunctionCommand<ClassT>* vtkMakeMemberFunctionCommand(
 {
   vtkMemberFunctionCommand<ClassT>* result = vtkMemberFunctionCommand<ClassT>::New();
   result->SetCallback(object, method);
-  
+  return result;
+}
+
+template<class ClassT>
+vtkMemberFunctionCommand<ClassT>* vtkMakeMemberFunctionCommand(
+  ClassT& object, void (ClassT::*method)(vtkObject*, unsigned long, void*))
+{
+  vtkMemberFunctionCommand<ClassT>* result = vtkMemberFunctionCommand<ClassT>::New();
+  result->SetCallback(object, method);
   return result;
 }
