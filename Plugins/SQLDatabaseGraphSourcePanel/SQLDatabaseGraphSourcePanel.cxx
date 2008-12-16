dbebad28a02f230cb7053ee6c71c0d439ccc8d28
William C. McLendon <wcmclen@sandia.gov>
2008-12-16 18:48:25 -0500

    ENH: Added an extra field for link vertices and link edges and added a check-box to the SQLDatabaseGraphSourcePanel to allow the graph to be directed or undirected.

--- Plugins/SQLDatabaseGraphSourcePanel/SQLDatabaseGraphSourcePanel.cxx
+++ Plugins/SQLDatabaseGraphSourcePanel/SQLDatabaseGraphSourcePanel.cxx
@@ -45,27 +45,33 @@ SQLDatabaseGraphSourcePanel::SQLDatabaseGraphSourcePanel(pqProxy* proxy, QWidget
   QObject::connect(this->Widgets.databasePort, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.edgeQuery, SIGNAL(textChanged()), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexQuery, SIGNAL(textChanged()), this, SLOT(setModified()));
-//  QObject::connect(this->Widgets.directed, SIGNAL(stateChanged(int)), this, SLOT(setModified()));
+
+  QObject::connect(this->Widgets.Directed, SIGNAL(stateChanged(int)), this, SLOT(setModified()));
 
   QObject::connect(this->Widgets.vertexField1, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexField2, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexField3, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
+  QObject::connect(this->Widgets.vertexField4, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
 
   QObject::connect(this->Widgets.vertexDomain1, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexDomain2, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexDomain3, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
+  QObject::connect(this->Widgets.vertexDomain4, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
 
   QObject::connect(this->Widgets.vertexHidden1, SIGNAL(clicked(bool)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexHidden2, SIGNAL(clicked(bool)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.vertexHidden3, SIGNAL(clicked(bool)), this, SLOT(setModified()));
+  QObject::connect(this->Widgets.vertexHidden4, SIGNAL(clicked(bool)), this, SLOT(setModified()));
 
   QObject::connect(this->Widgets.edgeSource1, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.edgeSource2, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.edgeSource3, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
+  QObject::connect(this->Widgets.edgeSource4, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
 
   QObject::connect(this->Widgets.edgeTarget1, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.edgeTarget2, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.edgeTarget3, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
+  QObject::connect(this->Widgets.edgeTarget4, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
 
   QObject::connect(this->Widgets.edgePedigreeIdArrayName, SIGNAL(textEdited(const QString&)), this, SLOT(setModified()));
   QObject::connect(this->Widgets.generateEdgePedigreeIds, SIGNAL(currentIndexChanged(int)), this, SLOT(setModified()));
@@ -118,13 +124,16 @@ void SQLDatabaseGraphSourcePanel::accept()
   pqPropertyHelper(proxy, "Password").Set(this->Widgets.databasePassword->text());
   pqPropertyHelper(proxy, "EdgeQuery").Set(this->Widgets.edgeQuery->toPlainText());
   pqPropertyHelper(proxy, "VertexQuery").Set(this->Widgets.vertexQuery->toPlainText());
-    QStringList link_vertices;
+
+  QStringList link_vertices;
   if(!this->Widgets.vertexField1->text().isEmpty() && !this->Widgets.vertexDomain1->text().isEmpty())
     link_vertices << this->Widgets.vertexField1->text() << this->Widgets.vertexDomain1->text() << (this->Widgets.vertexHidden1->isChecked() ? "1" : "0");
   if(!this->Widgets.vertexField2->text().isEmpty() && !this->Widgets.vertexDomain2->text().isEmpty())
     link_vertices << this->Widgets.vertexField2->text() << this->Widgets.vertexDomain2->text() << (this->Widgets.vertexHidden2->isChecked() ? "1" : "0");
   if(!this->Widgets.vertexField3->text().isEmpty() && !this->Widgets.vertexDomain3->text().isEmpty())
     link_vertices << this->Widgets.vertexField3->text() << this->Widgets.vertexDomain3->text() << (this->Widgets.vertexHidden3->isChecked() ? "1" : "0");
+  if(!this->Widgets.vertexField4->text().isEmpty() && !this->Widgets.vertexDomain4->text().isEmpty())
+    link_vertices << this->Widgets.vertexField4->text() << this->Widgets.vertexDomain4->text() << (this->Widgets.vertexHidden4->isChecked() ? "1" : "0");
 
   QStringList link_edges;
   if(!this->Widgets.edgeSource1->text().isEmpty() && !this->Widgets.edgeTarget1->text().isEmpty())
@@ -133,13 +142,15 @@ void SQLDatabaseGraphSourcePanel::accept()
     link_edges << this->Widgets.edgeSource2->text() << this->Widgets.edgeTarget2->text();
   if(!this->Widgets.edgeSource3->text().isEmpty() && !this->Widgets.edgeTarget3->text().isEmpty())
     link_edges << this->Widgets.edgeSource3->text() << this->Widgets.edgeTarget3->text();
+  if(!this->Widgets.edgeSource4->text().isEmpty() && !this->Widgets.edgeTarget4->text().isEmpty())
+    link_edges << this->Widgets.edgeSource4->text() << this->Widgets.edgeTarget4->text();
 
   pqPropertyHelper(proxy, "LinkVertices").Set(link_vertices);
   pqPropertyHelper(proxy, "LinkEdges").Set(link_edges);
   pqPropertyHelper(proxy, "EdgePedigreeIdArrayName").Set(this->Widgets.edgePedigreeIdArrayName->text());
   vtkSMPropertyHelper(proxy, "GenerateEdgePedigreeIds").Set(this->Widgets.generateEdgePedigreeIds->currentIndex());
 
-//  vtkSMPropertyHelper(proxy, "Directed").set(this->Widgets.directed->isChecked());
+  vtkSMPropertyHelper(proxy, "Directed").Set(this->Widgets.Directed->isChecked());
  
   proxy->UpdateVTKObjects();
 
@@ -253,6 +264,12 @@ void SQLDatabaseGraphSourcePanel::loadInitialState()
     this->Widgets.vertexDomain3->setText(link_vertices[7]);
     this->Widgets.vertexHidden3->setChecked(link_vertices[8] == "1");
     }
+  if(link_vertices.size() > 11)
+    {
+    this->Widgets.vertexField4->setText(link_vertices[9]);
+    this->Widgets.vertexDomain4->setText(link_vertices[10]);
+    this->Widgets.vertexHidden4->setChecked(link_vertices[11] == "1");
+    }
 
   const QStringList link_edges = pqPropertyHelper(proxy, "LinkEdges").GetAsStringList();
   if(link_edges.size() > 1)
@@ -269,6 +286,11 @@ void SQLDatabaseGraphSourcePanel::loadInitialState()
     {
     this->Widgets.edgeSource3->setText(link_edges[4]);
     this->Widgets.edgeTarget3->setText(link_edges[5]);
+    }
+  if(link_edges.size() > 7)
+    {
+    this->Widgets.edgeSource4->setText(link_edges[6]);
+    this->Widgets.edgeTarget4->setText(link_edges[7]);
     }
 
   this->Widgets.edgePedigreeIdArrayName->setText(pqPropertyHelper(proxy, "EdgePedigreeIdArrayName").GetAsString());
