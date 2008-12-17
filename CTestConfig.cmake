7048e9171c21cf95f9ef38d27b2cfb6df386c12d
Berk Geveci <berk.geveci@kitware.com>
2008-12-17 09:53:59 -0500

    ENH: Updated the drop location to that of CDash instead of Dart

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,13 @@
-SET (CTEST_PROJECT_NAME "ParaView")
-SET (CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
-SET (CTEST_DROP_METHOD "http")
-SET (CTEST_DROP_SITE "www.paraview.org")
-SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-ParaView3-TestingResults.cgi")
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(Dart)
+set(CTEST_PROJECT_NAME "ParaView3")
+set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "www.cdash.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ParaView3")
+set(CTEST_DROP_SITE_CDASH TRUE)
