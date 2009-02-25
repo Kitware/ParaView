5d3a2fc4150a38daad4a10c25f201b3a0ec66289
Ken Moreland <kmorel@sandia.gov>
2009-02-25 12:20:53 -0500

    ENH: Starting a 'playground' for tools designed around work with the Standford Linear Accelerator Center.

---
+++ Plugins/SLACTools/vtkSamplePlaneSource.cxx
@@ -0,0 +1,191 @@
+// -*- c++ -*-
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkSamplePlaneSource.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/*----------------------------------------------------------------------------
+ Copyright (c) Sandia Corporation
+ See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+----------------------------------------------------------------------------*/
+
+#include "vtkSamplePlaneSource.h"
+
+#include "vtkCommunicator.h"
+#include "vtkCompositeDataIterator.h"
+#include "vtkCompositeDataSet.h"
+#include "vtkDummyController.h"
+#include "vtkInformation.h"
+#include "vtkObjectFactory.h"
+#include "vtkPlaneSource.h"
+#include "vtkPolyData.h"
+
+#include "vtkSmartPointer.h"
+#define VTK_CREATE(type, name) \
+  vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+
+#include <math.h>
+
+//=============================================================================
+vtkCxxRevisionMacro(vtkSamplePlaneSource, "1.1");
+vtkStandardNewMacro(vtkSamplePlaneSource);
+
+vtkCxxSetObjectMacro(vtkSamplePlaneSource, Controller,
+                     vtkMultiProcessController);
+
+//-----------------------------------------------------------------------------
+vtkSamplePlaneSource::vtkSamplePlaneSource()
+{
+  this->Center[0] = this->Center[1] = this->Center[2] = 0.0;
+  this->Normal[0] = 0.0;
+  this->Normal[1] = 0.0;
+  this->Normal[2] = 1.0;
+  this->Resolution = 100;
+
+  this->Controller = NULL;
+  this->SetController(vtkMultiProcessController::GetGlobalController());
+  if (!this->Controller)
+    {
+    VTK_CREATE(vtkDummyController, dummyController);
+    this->SetController(dummyController);
+    }
+}
+
+vtkSamplePlaneSource::~vtkSamplePlaneSource()
+{
+  this->SetController(NULL);
+}
+
+void vtkSamplePlaneSource::PrintSelf(ostream &os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+
+  os << indent << "Center: (" << this->Center[0] << ", "
+                              << this->Center[1] << ", "
+                              << this->Center[2] << ")" << endl;
+  os << indent << "Normal: (" << this->Normal[0] << ", "
+                              << this->Normal[1] << ", "
+                              << this->Normal[2] << ")" << endl;;
+  os << indent << "Resolution: " << this->Resolution << endl;
+}
+
+//-----------------------------------------------------------------------------
+int vtkSamplePlaneSource::FillInputPortInformation(int port,
+                                                   vtkInformation *info)
+{
+  if (port == 0)
+    {
+    info->Remove(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE());
+    info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
+    info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkCompositeDataSet");
+    return 1;
+    }
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+int vtkSamplePlaneSource::RequestData(vtkInformation *vtkNotUsed(request),
+                                      vtkInformationVector **inputVector,
+                                      vtkInformationVector *outputVector)
+{
+  vtkDataObject *input = vtkDataObject::GetData(inputVector[0]);
+  vtkPolyData *output = vtkPolyData::GetData(outputVector);
+
+  double bounds[6];
+  this->ComputeLocalBounds(input, bounds);
+  this->ResolveParallelBounds(bounds);
+  this->CreatePlane(bounds, output);
+
+  return 1;
+}
+
+//-----------------------------------------------------------------------------
+void vtkSamplePlaneSource::ComputeLocalBounds(vtkDataObject *input,
+                                              double bounds[6])
+{
+  bounds[0] = bounds[2] = bounds[4] = VTK_DOUBLE_MAX;
+  bounds[1] = bounds[3] = bounds[5] = -VTK_DOUBLE_MAX;
+
+  vtkDataSet *dsInput = vtkDataSet::SafeDownCast(input);
+  if (dsInput)
+    {
+    dsInput->GetBounds(bounds);
+    return;
+    }
+
+  vtkCompositeDataSet *cInput = vtkCompositeDataSet::SafeDownCast(input);
+  if (cInput)
+    {
+    vtkSmartPointer<vtkCompositeDataIterator> iter;
+    iter.TakeReference(cInput->NewIterator());
+    for (iter->InitTraversal(); !iter->IsDoneWithTraversal();
+         iter->GoToNextItem())
+      {
+      double subbounds[6];
+      this->ComputeLocalBounds(iter->GetCurrentDataObject(), subbounds);
+      if (bounds[0] > subbounds[0]) bounds[0] = subbounds[0];
+      if (bounds[1] < subbounds[1]) bounds[1] = subbounds[1];
+      if (bounds[2] > subbounds[2]) bounds[2] = subbounds[2];
+      if (bounds[3] < subbounds[3]) bounds[3] = subbounds[3];
+      if (bounds[4] > subbounds[4]) bounds[4] = subbounds[4];
+      if (bounds[5] < subbounds[5]) bounds[5] = subbounds[5];
+      }
+    return;
+    }
+
+  vtkWarningMacro(<< "Unknown data type: " << input->GetClassName());
+}
+
+//-----------------------------------------------------------------------------
+void vtkSamplePlaneSource::ResolveParallelBounds(double bounds[6])
+{
+  this->Controller->AllReduce(&bounds[0],&bounds[0],1,vtkCommunicator::MIN_OP);
+  this->Controller->AllReduce(&bounds[1],&bounds[1],1,vtkCommunicator::MAX_OP);
+  this->Controller->AllReduce(&bounds[2],&bounds[2],1,vtkCommunicator::MIN_OP);
+  this->Controller->AllReduce(&bounds[3],&bounds[3],1,vtkCommunicator::MAX_OP);
+  this->Controller->AllReduce(&bounds[4],&bounds[4],1,vtkCommunicator::MIN_OP);
+  this->Controller->AllReduce(&bounds[5],&bounds[5],1,vtkCommunicator::MAX_OP);
+}
+
+//-----------------------------------------------------------------------------
+void vtkSamplePlaneSource::CreatePlane(const double bounds[6],
+                                       vtkPolyData *output)
+{
+  double dims[3];
+  for (int i = 0; i < 3; i++)
+    {
+    dims[i] = bounds[2*i+1]-bounds[2*i];
+    if (dims[i] < 0) dims[i] = 0;
+    }
+
+  double diagonal = sqrt(dims[0]*dims[0] + dims[1]*dims[1] + dims[2]*dims[2]);
+  if (diagonal <= 0)
+    {
+    // No space.  Just do nothing.
+    return;
+    }
+
+  // Use the plane source to create the geometry.
+  VTK_CREATE(vtkPlaneSource, planeSource);
+  planeSource->SetXResolution(2*this->Resolution);
+  planeSource->SetYResolution(2*this->Resolution);
+
+  planeSource->SetOrigin(0.0, 0.0, 0.0);
+  planeSource->SetPoint1(2*diagonal, 0.0, 0.0);
+  planeSource->SetPoint2(0.0, 2*diagonal, 0.0);
+
+  planeSource->SetCenter(this->Center);
+  planeSource->SetNormal(this->Normal);
+
+  planeSource->Update();
+  output->ShallowCopy(planeSource->GetOutput());
+}
---
+++ Plugins/SLACTools/vtkSamplePlaneSource.h
@@ -0,0 +1,104 @@
+// -*- c++ -*-
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkSamplePlaneSource.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/*----------------------------------------------------------------------------
+ Copyright (c) Sandia Corporation
+ See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+----------------------------------------------------------------------------*/
+
+// .NAME vtkSamplePlaneSource - Create an oriented plane that can be used to sample the input.
+//
+// .SECTION Description
+//
+// This filter creates a plane with a given center point and normal.  This
+// algorithm is designed to have an input whose bounds are used to establish the
+// size (in world coordinates) of the plane.  The number of points in the x and
+// y direction of the plane are generally set by the resolution, but the output
+// will actually have more points than specified.  The plane geometry will be
+// made larger than the bounds to ensure that, if used to probe the input, it
+// will cover the entire geometry.
+//
+
+#ifndef __vtkSamplePlaneSource_h
+#define __vtkSamplePlaneSource_h
+
+#include "vtkPolyDataAlgorithm.h"
+
+class vtkMultiProcessController;
+
+class vtkSamplePlaneSource : public vtkPolyDataAlgorithm
+{
+public:
+  vtkTypeRevisionMacro(vtkSamplePlaneSource, vtkPolyDataAlgorithm);
+  static vtkSamplePlaneSource *New();
+  virtual void PrintSelf(ostream &os, vtkIndent indent);
+
+  // Description:
+  // The location of the center of the plane.  A point is guaranteed to be here.
+  vtkGetVector3Macro(Center, double);
+  vtkSetVector3Macro(Center, double);
+
+  // Description:
+  // The normal to the plane.
+  vtkGetVector3Macro(Normal, double);
+  vtkSetVector3Macro(Normal, double);
+
+  // Description:
+  // The approximate number of samples in each direction that will intersect the
+  // input bounds.
+  vtkGetMacro(Resolution, int);
+  vtkSetMacro(Resolution, int);
+
+  // Description:
+  // The controller used to determine the actual bounds of the input.  Use
+  // a dummy controller if not running in parallel.
+  vtkGetObjectMacro(Controller, vtkMultiProcessController);
+  virtual void SetController(vtkMultiProcessController *);
+
+protected:
+  vtkSamplePlaneSource();
+  ~vtkSamplePlaneSource();
+
+  double Center[3];
+  double Normal[3];
+  int Resolution;
+
+  vtkMultiProcessController *Controller;
+
+  virtual int FillInputPortInformation(int port, vtkInformation *info);
+
+  virtual int RequestData(vtkInformation *request,
+                          vtkInformationVector **inputVector,
+                          vtkInformationVector *outputVector);
+
+  // Description:
+  // Finds the bounds of a data object (can be either a vtkDataSet or
+  // a vtkCompositeDataSet containing vtkDataSets).
+  virtual void ComputeLocalBounds(vtkDataObject *input, double bounds[6]);
+
+  // Description:
+  // Resolves the bounds of all parallel processes.
+  virtual void ResolveParallelBounds(double bounds[6]);
+
+  // Description:
+  // Creates the output poly data based on the bounds and ivars.
+  virtual void CreatePlane(const double bounds[6], vtkPolyData *output);
+
+private:
+  vtkSamplePlaneSource(const vtkSamplePlaneSource &); // Not implemented
+  void operator=(const vtkSamplePlaneSource &);       // Not implemented
+};
+
+#endif //__vtkSamplePlaneSource_h
