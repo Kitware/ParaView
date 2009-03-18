caeb82bbd98e87d43c330720f444a47224a502ce
Ken Moreland <kmorel@sandia.gov>
2009-03-18 14:22:45 -0400

    ENH: Change the radius of the circle on which to choose rotate vs. roll to be based on the larger viewport dimensions rather than the smaller one.

--- Servers/Filters/vtkPVTrackballMultiRotate.cxx
+++ Servers/Filters/vtkPVTrackballMultiRotate.cxx
@@ -27,11 +27,11 @@
 #include "vtkPVTrackballRotate.h"
 #include "vtkRenderer.h"
 
-#define MY_MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MY_MAX(x, y) ((x) < (y) ? (y) : (x))
 #define MY_SQR(x)    ((x)*(x))
 
 //=============================================================================
-vtkCxxRevisionMacro(vtkPVTrackballMultiRotate, "1.1");
+vtkCxxRevisionMacro(vtkPVTrackballMultiRotate, "1.2");
 vtkStandardNewMacro(vtkPVTrackballMultiRotate);
 
 //-----------------------------------------------------------------------------
@@ -62,7 +62,7 @@ void vtkPVTrackballMultiRotate::OnButtonDown(int x, int y, vtkRenderer *ren,
   viewSize = ren->GetSize();
   double  viewCenter[2];
   viewCenter[0] = 0.5*viewSize[0];  viewCenter[1] = 0.5*viewSize[1];
-  double rotateRadius = 0.9*(MY_MIN(viewCenter[0], viewCenter[1]));
+  double rotateRadius = 0.9*(MY_MAX(viewCenter[0], viewCenter[1]));
   double dist2 = MY_SQR(viewCenter[0]-x) + MY_SQR(viewCenter[1]-y);
 
   if (rotateRadius*rotateRadius > dist2)
