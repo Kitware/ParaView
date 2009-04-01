0dc52490316f2546054020c998bbedc5793833bc
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-04-01 15:44:17 -0400

    BUG: Fixed segfault when custom bin ranges were used (BUG #5861).

--- Servers/Filters/vtkExtractHistogram.cxx
+++ Servers/Filters/vtkExtractHistogram.cxx
@@ -51,7 +51,7 @@ struct vtkEHInternals
 };
 
 vtkStandardNewMacro(vtkExtractHistogram);
-vtkCxxRevisionMacro(vtkExtractHistogram, "1.24");
+vtkCxxRevisionMacro(vtkExtractHistogram, "1.25");
 //-----------------------------------------------------------------------------
 vtkExtractHistogram::vtkExtractHistogram() :
   Component(0),
@@ -290,6 +290,7 @@ void vtkExtractHistogram::BinAnArray(vtkDataArray *data_array,
     int index = static_cast<int>((value - min) / bin_delta);
     // If the value is equal to max, include it in the last bin.
     index = ( index == this->BinCount ? index - 1 : index );
+    index = ( index < 0  ? 0 : index );
     bin_values->SetValue(index, bin_values->GetValue(index)+1);
     
     if (this->CalculateAverages)
