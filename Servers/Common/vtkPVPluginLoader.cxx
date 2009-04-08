53c79fba9af4f2fe6d66d8fb6610287ccf5661a0
Burlen Loring <burlen.loring@gmail.com>
2009-04-08 11:40:11 -0400

    ENH: Bug# 00008673. Added the path to a plugin to the shared library search path. This is OK but a better solution is to let the plugin tell us what paths to add. TODO

--- Servers/Common/vtkPVPluginLoader.cxx
+++ Servers/Common/vtkPVPluginLoader.cxx
@@ -22,9 +22,12 @@
 #include "vtkPVOptions.h"
 #include "vtkStringArray.h"
 #include <vtksys/SystemTools.hxx>
+#include <vtkstd/string>
+using vtkstd::string;
+#include <cstdlib>
 
 vtkStandardNewMacro(vtkPVPluginLoader);
-vtkCxxRevisionMacro(vtkPVPluginLoader, "1.9");
+vtkCxxRevisionMacro(vtkPVPluginLoader, "1.10");
 
 #ifdef _WIN32
 // __cdecl gives an unmangled name
@@ -124,7 +127,6 @@ void vtkPVPluginLoader::SetFileName(const char* file)
     {
     return;
     }
-  
   if(this->FileName)
     {
     delete [] this->FileName;
@@ -136,12 +138,65 @@ void vtkPVPluginLoader::SetFileName(const char* file)
     this->FileName = new char[len+1];
     strcpy(this->FileName, file);
     }
-            
+
   if(!this->Loaded && FileName && FileName[0] != '\0')
     {
     vtkLibHandle lib = vtkDynamicLoader::OpenLibrary(FileName);
     if(lib)
       {
+      // BUG # 0008673
+      // Tell the platform to look in the plugin's directory for 
+      // its dependencies. This isn't the right thing to do. A better
+      // solution would be to let the plugin tell us where to look so
+      // that a list of locations could be added.
+      string ldLibPath;
+#if defined(_WIN32) && !defined(__CYGWIN__)
+      const char LIB_PATH_SEP=';';
+      const char PATH_SEP='\\';
+      const char *LIB_PATH_NAME="PATH";
+      ldLibPath=LIB_PATH_NAME;
+      ldLibPath+='=';
+#elif defined (__APPLE__)
+      const char LIB_PATH_SEP=':';
+      const char PATH_SEP='/';
+      const char *LIB_PATH_NAME="DYLD_LIBRARY_PATH";
+#else
+      const char LIB_PATH_SEP=':';
+      const char PATH_SEP='/';
+      const char *LIB_PATH_NAME="LD_LIBRARY_PATH";
+#endif
+      // Trim the plugin name from the end of its path.
+      string thisPluginsPath(file);
+      size_t eop=thisPluginsPath.rfind(PATH_SEP);
+      thisPluginsPath=thisPluginsPath.substr(0,eop);
+      // Load the shared libarary search path.
+      const char *pLdLibPath=getenv(LIB_PATH_NAME);
+      bool pluginPathPresent
+        = pLdLibPath==NULL?false:strstr(pLdLibPath,thisPluginsPath.c_str())!=NULL;
+      // Update it.
+      if (!pluginPathPresent)
+        {
+        // Make sure we are only adding it once, because there can
+        // be multiple plugins in the same folder.
+        if (pLdLibPath)
+          {
+          ldLibPath+=pLdLibPath;
+          ldLibPath+=LIB_PATH_SEP;
+          }
+        ldLibPath+=thisPluginsPath;
+#if defined(_WIN32) && !defined(__CYGWIN__)
+        // Note: POSIX putenv does a pointer copy not a string copy.
+        // POSIX setenv does a string copy (the right thing), but unavailable
+        // on windows :( .
+        size_t n=ldLibPath.size()+1;
+        char *newLdLibPath=static_cast<char *>(malloc(n));
+        strncpy(newLdLibPath,ldLibPath.c_str(),n);
+        putenv(newLdLibPath);
+#else
+        setenv(LIB_PATH_NAME,ldLibPath.c_str(),1);
+#endif
+        }
+
       // old SM xml, new plugins don't have this method
       PluginXML1 xml1 = 
         (PluginXML1)vtkDynamicLoader::GetSymbolAddress(lib, "ParaViewPluginXML");
