06354b221e8fffcf3b9d00af2debf28940e74ad9
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-05-02 10:54:02 -0400

    BUG: Fixing test in plots now have a minimum size which is making it difficult to render identical images on all platforms with the old test.

--- Applications/Client/Testing/Python/SaveLargeScreenshot.py
+++ Applications/Client/Testing/Python/SaveLargeScreenshot.py
@@ -3,41 +3,28 @@
 import QtTesting
 import QtTestingImage
 
-object2 = 'pqClientMainWindow/menubar/menuSources'
-QtTesting.playCommand(object2, 'activate', 'HierarchicalFractal')
-object3 = 'pqClientMainWindow/objectInspectorDock/1pqProxyTabWidget0/qt_tabwidget_stackedwidget/objectInspector/ScrollArea/qt_scrollarea_viewport/PanelArea/Editor/TwoDimensional'
-QtTesting.playCommand(object3, 'set_boolean', 'false')
-object4 = 'pqClientMainWindow/objectInspectorDock/1pqProxyTabWidget0/qt_tabwidget_stackedwidget/objectInspector/ScrollArea/qt_scrollarea_viewport/PanelArea/Editor/GhostLevels'
-QtTesting.playCommand(object4, 'set_boolean', 'false')
-object5 = 'pqClientMainWindow/objectInspectorDock/1pqProxyTabWidget0/qt_tabwidget_stackedwidget/objectInspector/Accept'
+object1 = 'pqClientMainWindow/menubar/menuSources'
+QtTesting.playCommand(object1, 'activate', 'RTAnalyticSource')
+object2 = 'pqClientMainWindow/objectInspectorDock/1pqProxyTabWidget0/qt_tabwidget_stackedwidget/objectInspector/Accept'
+QtTesting.playCommand(object2, 'activate', '')
+object3 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/0/MultiViewFrameMenu/SplitHorizontalButton'
+QtTesting.playCommand(object3, 'activate', '')
+object4 = 'pqClientMainWindow/menubar/menuFilters/DataAnalysis'
+QtTesting.playCommand(object4, 'activate', 'ProbeLine')
+QtTesting.playCommand(object2, 'activate', '')
+object5 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/1/MultiViewFrameMenu/SplitVerticalButton'
 QtTesting.playCommand(object5, 'activate', '')
-object6 = 'pqClientMainWindow/menubar/menuFilters/Alphabetical'
-
-objectA = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/0/MultiViewFrameMenu/SplitHorizontalButton'
-QtTesting.playCommand(objectA, 'activate', '')
-QtTesting.playCommand(object6, 'activate', 'ProbeLine')
-QtTesting.playCommand(object5, 'activate', '')
-
-object1 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/0/MultiViewFrameMenu/WindowCaption'
-QtTesting.playCommand(object1, 'mousePress', '1,1,0,23,6')
-QtTesting.playCommand(object1, 'mouseRelease', '1,0,0,23,6')
-QtTesting.playCommand(object2, 'activate', 'RTAnalyticSource')
-QtTesting.playCommand(object5, 'activate', '')
-
-object2 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/1/MultiViewFrameMenu/WindowCaption'
-QtTesting.playCommand(object2, 'mousePress', '1,1,0,89,9')
-QtTesting.playCommand(object2, 'mouseRelease', '1,0,0,89,9')
-
-objectB = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/1/MultiViewFrameMenu/SplitVerticalButton'
-QtTesting.playCommand(objectB, 'activate', '')
-
-QtTesting.playCommand(object6, 'activate', 'ExtractHistogram')
-QtTesting.playCommand(object5, 'activate', '')
+object6 = 'pqClientMainWindow/pipelineBrowserDock/pipelineBrowser/PipelineView'
+QtTesting.playCommand(object6, 'mousePress', '1,1,0,51,13,/0:0/0:0')
+QtTesting.playCommand(object6, 'mouseRelease', '1,0,0,51,13,/0:0/0:0')
+QtTesting.playCommand(object4, 'activate', 'ExtractHistogram')
+QtTesting.playCommand(object2, 'activate', '')
 object7 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/0/MultiViewFrameMenu/SplitVerticalButton'
 QtTesting.playCommand(object7, 'activate', '')
-object1 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/MultiViewSplitter:0/0/MultiViewFrameMenu/WindowCaption'
-QtTesting.playCommand(object1, 'mousePress', '1,1,0,22,8')
-QtTesting.playCommand(object1, 'mouseRelease', '1,0,0,22,8')
+object8 = 'pqClientMainWindow/MultiViewManager/SplitterFrame/MultiViewSplitter/MultiViewSplitter:0/0/MultiViewFrameMenu/WindowCaption'
+QtTesting.playCommand(object8, 'mousePress', '1,1,0,42,4')
+QtTesting.playCommand(object8, 'mouseRelease', '1,0,0,42,4')
+
 object9 = 'pqClientMainWindow/menubar/menuFile'
 QtTesting.playCommand(object9, 'activate', 'actionFileSaveScreenshot')
 object12 = 'pqClientMainWindow/SaveSnapshotDialog/selectedViewOnly'
