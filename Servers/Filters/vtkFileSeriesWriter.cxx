bbee0d2c4267c6ede8a95942bde0951ee917f1ae
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-05-23 12:30:01 -0400

    COMP: Fixed warning

--- Servers/Filters/vtkFileSeriesWriter.cxx
+++ Servers/Filters/vtkFileSeriesWriter.cxx
@@ -30,7 +30,7 @@
 #include <vtkstd/string>
 
 vtkStandardNewMacro(vtkFileSeriesWriter);
-vtkCxxRevisionMacro(vtkFileSeriesWriter, "1.1");
+vtkCxxRevisionMacro(vtkFileSeriesWriter, "1.2");
 vtkCxxSetObjectMacro(vtkFileSeriesWriter, Writer, vtkAlgorithm);
 //-----------------------------------------------------------------------------
 vtkFileSeriesWriter::vtkFileSeriesWriter()
@@ -144,7 +144,7 @@ int vtkFileSeriesWriter::RequestUpdateExtent(
 int vtkFileSeriesWriter::RequestData(
   vtkInformation* request,
   vtkInformationVector** inputVector,
-  vtkInformationVector* outputVector)
+  vtkInformationVector* vtkNotUsed(outputVector))
 {
   // this->Writer has already written out the file, just manage the looping for
   // timesteps.
