4e253a1ac71b6d8bafc35d90533e720ed05fda37
Dave Partyka <dave.partyka@kitware.com>
2009-07-19 19:54:30 -0400

    ENH: increment required cmake version to 2.6.3 to match VTK and change CTEST_PROJECT_NAME to ParaView instead of ParaView3 so that new style scripts will work on ParaView.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,7 +4,7 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(Dart)
-set(CTEST_PROJECT_NAME "ParaView3")
+set(CTEST_PROJECT_NAME "ParaView")
 set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
