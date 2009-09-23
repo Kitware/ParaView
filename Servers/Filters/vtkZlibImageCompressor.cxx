9b95d1514793961cb924cac90389351b706164f7
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-09-23 09:12:00 -0400

    COMP: fixed build error.

--- Servers/Filters/vtkZlibImageCompressor.cxx
+++ Servers/Filters/vtkZlibImageCompressor.cxx
@@ -20,7 +20,7 @@
 #include <vtksys/ios/sstream>
 
 vtkStandardNewMacro(vtkZlibImageCompressor);
-vtkCxxRevisionMacro(vtkZlibImageCompressor, "1.3");
+vtkCxxRevisionMacro(vtkZlibImageCompressor, "1.4");
 
 
 //=============================================================================
@@ -422,8 +422,7 @@ int vtkZlibImageCompressor::Compress()
   this->Conditioner->PreProcess(this->Input,inImage,inImageComps,inImageSize,freeInImage);
 
   // Compress
-  size_t outImageSize=
-    static_cast<size_t>(1.001*inImageSize+17);
+  uLongf outImageSize= static_cast<uLongf>(1.001*inImageSize+17);
     // zlib requires 100.1% + 16, 1 byte for strip alpha
   unsigned char *outImage=static_cast<unsigned char *>(malloc(outImageSize));
   outImage[0]=inImageComps;
