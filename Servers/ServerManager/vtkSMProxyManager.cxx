09bde44005b1b1289d9bb4c639fcd7bc89a915ec
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-12-02 13:33:30 -0500

    BUG: Fixed BUG #9876. Incorrect initialization of iterator.

--- Servers/ServerManager/vtkSMProxyManager.cxx
+++ Servers/ServerManager/vtkSMProxyManager.cxx
@@ -102,7 +102,7 @@ protected:
 
 //*****************************************************************************
 vtkStandardNewMacro(vtkSMProxyManager);
-vtkCxxRevisionMacro(vtkSMProxyManager, "1.85");
+vtkCxxRevisionMacro(vtkSMProxyManager, "1.86");
 //---------------------------------------------------------------------------
 vtkSMProxyManager::vtkSMProxyManager()
 {
@@ -818,7 +818,7 @@ void vtkSMProxyManager::UnRegisterProxy(vtkSMProxy* proxy)
   for (; it != this->Internals->RegisteredProxyMap.end(); it++)
     {
     vtkSMProxyManagerProxyMapType::iterator it2;
-    for (it->second.begin(); it2 != it->second.end(); ++it2)
+    for (it2 = it->second.begin(); it2 != it->second.end(); ++it2)
       {
       if (it2->second.Contains(proxy))
         {
