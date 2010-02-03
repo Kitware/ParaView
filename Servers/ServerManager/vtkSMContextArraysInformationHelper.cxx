9c24a484019d7352a2fda4565dcd7e0e2bd8e1ea
Marcus Hanwell <marcus.hanwell@kitware.com>
2010-02-03 15:23:04 -0500

    ENH: Added a helper to enumerate the column names in a vtkTable.

---
+++ Servers/ServerManager/vtkSMContextArraysInformationHelper.cxx
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMContextArraysInformationHelper.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSMContextArraysInformationHelper.h"
+
+#include "vtkObjectFactory.h"
+
+#include "vtkSMXYChartRepresentationProxy.h"
+#include "vtkSMStringVectorProperty.h"
+
+vtkStandardNewMacro(vtkSMContextArraysInformationHelper);
+vtkCxxRevisionMacro(vtkSMContextArraysInformationHelper, "1.1");
+//----------------------------------------------------------------------------
+vtkSMContextArraysInformationHelper::vtkSMContextArraysInformationHelper()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkSMContextArraysInformationHelper::~vtkSMContextArraysInformationHelper()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkSMContextArraysInformationHelper::UpdateProperty(
+  vtkIdType vtkNotUsed(connectionId),
+  int vtkNotUsed(serverIds), vtkClientServerID vtkNotUsed(objectId),
+  vtkSMProperty* prop)
+{
+  vtkSMXYChartRepresentationProxy* rep =
+    vtkSMXYChartRepresentationProxy::SafeDownCast(
+      prop->GetParent());
+  if (!rep)
+    {
+    vtkWarningMacro("vtkSMContextArraysInformationHelper can only be used on"
+      " XY Chart representation proxies.");
+    return;
+    }
+
+  vtkSMStringVectorProperty* svp =
+    vtkSMStringVectorProperty::SafeDownCast(prop);
+  if (!svp)
+    {
+    vtkWarningMacro("vtkSMContextArraysInformationHelper can only update "
+      "vtkSMStringVectorProperty.");
+    return;
+    }
+
+  int num_series = rep->GetNumberOfSeries();
+  svp->SetNumberOfElements(num_series);
+  for (int i = 0; i < num_series; ++i)
+    {
+    svp->SetElement(i, rep->GetSeriesName(i));
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSMContextArraysInformationHelper::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+
---
+++ Servers/ServerManager/vtkSMContextArraysInformationHelper.h
@@ -0,0 +1,58 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkSMContextArraysInformationHelper.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkSMContextArraysInformationHelper - gather array names for a table
+// .SECTION Description
+//
+// This class iterates through a vtkTable and fills the supplied property with
+// column names matching their current column index.
+
+#ifndef __vtkSMContextArraysInformationHelper_h
+#define __vtkSMContextArraysInformationHelper_h
+
+#include "vtkSMInformationHelper.h"
+
+class VTK_EXPORT vtkSMContextArraysInformationHelper : public vtkSMInformationHelper
+{
+public:
+  static vtkSMContextArraysInformationHelper* New();
+  vtkTypeRevisionMacro(vtkSMContextArraysInformationHelper, vtkSMInformationHelper);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  //BTX
+  // Description:
+  // Updates the property using value obtained for server. It creates
+  // an instance of the server helper class vtkPVServerArraySelection
+  // and passes the objectId (which the helper class gets as a pointer)
+  // and populates the property using the values returned.
+  // Each array is represented by two components:
+  // name, state (on/off)
+  virtual void UpdateProperty(
+    vtkIdType connectionId,
+    int serverIds, vtkClientServerID objectId, vtkSMProperty* prop);
+  //ETX
+
+//BTX
+protected:
+  vtkSMContextArraysInformationHelper();
+  ~vtkSMContextArraysInformationHelper();
+
+private:
+  vtkSMContextArraysInformationHelper(const vtkSMContextArraysInformationHelper&); // Not implemented
+  void operator=(const vtkSMContextArraysInformationHelper&); // Not implemented
+//ETX
+};
+
+#endif
+
