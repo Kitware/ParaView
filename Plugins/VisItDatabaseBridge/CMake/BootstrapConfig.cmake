1a2ecb3dfb07a977ba3beafe4f8df9c85bf599b5
Dave Partyka <dave.partyka@kitware.com>
2010-03-23 12:32:14 -0400

    COMP: Fixes for building the VisitPlugin from ParaView3.6.2 where by we simple use the xml checked in once rather than regenerate it on the fly everytime we build.

--- Plugins/VisItDatabaseBridge/CMake/BootstrapConfig.cmake
+++ Plugins/VisItDatabaseBridge/CMake/BootstrapConfig.cmake
@@ -11,7 +11,7 @@ configure_file(
 
 set(BOOTSTRAP_CONFIGURE_EXLUDES "${PROJECT_SOURCE_DIR}/PVDatabaseExcludes.txt")
 link_directories(${VISIT_LIB_PATH})
-include_directories(${VISIT_INCLUDE_PATH} ${PROJECT_BINARY_DIR})
+include_directories(${VISIT_INCLUDE_PATH} ${PROJECT_SOURCE_DIR})
 add_executable(BootstrapConfigure BootstrapConfigure.cpp)
 target_link_libraries(BootstrapConfigure ${VISIT_LIBS} vtkCommon vtkFiltering vtkIO vtkGraphics vtkRendering)
 
@@ -33,13 +33,10 @@ set(VISIT_RUN_TIME
   ${VISIT_LOCAL} 
   CACHE FILEPATH
   "Path that is used by the PluginManager to load database plugins at runtime.")
-  
-execute_process(
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${PROJECT_BINARY_DIR}/CMake")
 
 add_custom_command(
   OUTPUT ${_configOut}
-  DEPENDS BootstrapConfigure ${VISIT_LIBS} vtkCommon vtkFiltering vtkIO vtkGraphics vtkRendering
+  DEPENDS ${_configIn}
   COMMAND BootstrapConfigure
   ARGS
     ${VISIT_LOCAL}
