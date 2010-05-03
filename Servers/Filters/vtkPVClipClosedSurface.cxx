7e36894b70f72600c54d989f890cbc1fc8ee72e9
Zhanping Liu <zhanping.liu@kitware.com>
2010-05-03 14:38:28 -0400

    Exposed vtkClipClosedSurface to ParaView.
    Added a new class vtkPVClipClosedSurface to expose
    vtkClipClosedSurface to ParaView.

---
+++ Servers/Filters/vtkPVClipClosedSurface.cxx
@@ -0,0 +1,90 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkPVClipClosedSurface.cxx,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVClipClosedSurface.h"
+
+#include "vtkPlane.h"
+#include "vtkPlaneCollection.h"
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro( vtkPVClipClosedSurface, "$Revision: 1.0 $" );
+vtkStandardNewMacro( vtkPVClipClosedSurface );
+
+//-----------------------------------------------------------------------------
+vtkPVClipClosedSurface::vtkPVClipClosedSurface()
+{
+  this->InsideOut = 0;
+  this->ClippingPlane  = NULL;
+  this->ClippingPlanes = NULL;
+}
+
+//-----------------------------------------------------------------------------
+vtkPVClipClosedSurface::~vtkPVClipClosedSurface()
+{
+  this->ClippingPlane  = NULL;
+
+  // leave the plane collection to be deleted by the parent class
+}
+
+//-----------------------------------------------------------------------------
+void vtkPVClipClosedSurface::SetClippingPlane( vtkPlane * plane )
+{
+  this->ClippingPlane = plane;
+
+  if ( this->ClippingPlanes )
+    {
+    this->ClippingPlanes->Delete();
+    this->ClippingPlanes = NULL;
+    }
+
+  this->ClippingPlanes = vtkPlaneCollection::New();
+  this->ClippingPlanes->AddItem( plane );
+}
+
+//-----------------------------------------------------------------------------
+int vtkPVClipClosedSurface::RequestData( vtkInformation        * request,
+                                         vtkInformationVector ** inputVector,
+                                         vtkInformationVector  * outputVector )
+{
+  if ( this->InsideOut )
+    {
+    // the normal vector of the plane specified by the user
+    double normalVec[3];
+    this->ClippingPlane->GetNormal( normalVec );
+
+    // the actual normal vector (of the plane) employed by the clipper
+    double actualVec[3] = { -normalVec[0], -normalVec[1], -normalVec[2] };
+    this->ClippingPlane->SetNormal( actualVec );
+
+    int  retVal = Superclass::RequestData( request, inputVector, outputVector );
+
+    // restore the clipping plane with the normal vector specified by the user
+    this->ClippingPlane->SetNormal( normalVec );
+
+    return retVal;
+    }
+  else
+    {
+    return Superclass::RequestData( request, inputVector, outputVector );
+    }
+}
+
+//-----------------------------------------------------------------------------
+void vtkPVClipClosedSurface::PrintSelf( ostream & os, vtkIndent indent )
+{
+  this->Superclass::PrintSelf( os, indent );
+
+  os << indent << "Insideout: "      << this->InsideOut     << endl;
+  os << indent << "Clipping Plane: " << this->ClippingPlane << endl;
+}
---
+++ Servers/Filters/vtkPVClipClosedSurface.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile: vtkPVClipClosedSurface.cxx,v $
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPVClipClosedSurface - Clipper for generating closed surfaces
+//
+// .SECTION Description
+//  This is a subclass of vtkClipClosedSurface
+
+#ifndef __vtkPVClipClosedSurface_h
+#define __vtkPVClipClosedSurface_h
+
+#include "vtkClipClosedSurface.h"
+
+class vtkPlane;
+
+class VTK_EXPORT vtkPVClipClosedSurface : public vtkClipClosedSurface
+{
+public:
+  vtkTypeRevisionMacro( vtkPVClipClosedSurface, vtkClipClosedSurface );
+  void PrintSelf( ostream & os, vtkIndent indent );
+  static vtkPVClipClosedSurface * New();
+
+  // Description:
+  // Set/Get the InsideOut flag (off by default)
+  vtkSetMacro( InsideOut, int );
+  vtkGetMacro( InsideOut, int );
+  vtkBooleanMacro( InsideOut, int );
+
+  // Description:
+  // Set the clipping plane.
+  void   SetClippingPlane( vtkPlane * plane );
+
+protected:
+  vtkPVClipClosedSurface();
+  ~vtkPVClipClosedSurface();
+
+  virtual int RequestData( vtkInformation        * request,
+                           vtkInformationVector ** inputVector,
+                           vtkInformationVector  * outputVector );
+
+  int         InsideOut;
+  vtkPlane  * ClippingPlane;
+
+private:
+
+  vtkPVClipClosedSurface( const vtkPVClipClosedSurface & ); // Not implemented
+  void operator = ( const vtkPVClipClosedSurface & );       // Not implemented
+};
+
+#endif
