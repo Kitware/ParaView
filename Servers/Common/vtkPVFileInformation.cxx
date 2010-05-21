Kitware/ParaView

commit 32594b13640cf22b9a56ca0a743292e985f5128e

Fixed bug 9452 to show dirs with trailing names in file dialog.
Now directories with trailing names can be shown in the file
dialog on Unix.

Servers/Common/vtkPVFileInformation.cxx
@@ -878,6 +878,14 @@ void vtkPVFileInformation::GetDirectoryListing()
     info->SetName(d->d_name);
     info->SetFullPath((prefix + d->d_name).c_str());
     info->Type = INVALID;
+
+    // fix to bug #09452 such that directories with trailing names can be
+    // shown in the file dialog
+    if ( d->d_type & DT_DIR )
+      {
+      info->Type = DIRECTORY;
+      }
+
     info->FastFileTypeDetection = this->FastFileTypeDetection;
     info_set.insert(info);
     info->Delete();

commit 40edbbfebdf12552a68a4562e994e4bc3f905400

COMP: Workaround for bug #09452 on Solaris SPARC. Original fix 32594b13640cf22b9a56ca0a743292e985f5128e broke compilation on Solaris SPARC.

Servers/Common/vtkPVFileInformation.cxx
@@ -881,10 +881,19 @@ void vtkPVFileInformation::GetDirectoryListing()
 
     // fix to bug #09452 such that directories with trailing names can be
     // shown in the file dialog
+#if defined (__SVR4) && defined (__sun)
+    struct stat status;
+    stat( d->d_name, &status );
+    if ( status.st_mode & S_IFDIR )
+     {
+     info->Type = DIRECTORY;
+     }
+#else
     if ( d->d_type & DT_DIR )
       {
       info->Type = DIRECTORY;
       }
+#endif
 
     info->FastFileTypeDetection = this->FastFileTypeDetection;
     info_set.insert(info);
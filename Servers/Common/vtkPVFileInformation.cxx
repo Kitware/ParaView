40edbbfebdf12552a68a4562e994e4bc3f905400
Sven Buijssen <sven.buijssen@tu-dortmund.de>
2010-05-21 07:07:58 +0200

    COMP: Workaround for bug #09452 on Solaris SPARC. Original fix 32594b13640cf22b9a56ca0a743292e985f5128e broke compilation on Solaris SPARC.

--- Servers/Common/vtkPVFileInformation.cxx
+++ Servers/Common/vtkPVFileInformation.cxx
@@ -881,10 +881,19 @@ void vtkPVFileInformation::GetDirectoryListing()
 
     // fix to bug #09452 such that directories with trailing names can be
     // shown in the file dialog
+#if defined (__SVR4) && defined (__sun)
+    struct stat status;
+    stat( d->d_name, &status );
+    if ( status.st_mode & S_IFDIR )
+     {
+     info->Type = DIRECTORY;
+     }
+#else
     if ( d->d_type & DT_DIR )
       {
       info->Type = DIRECTORY;
       }
+#endif
 
     info->FastFileTypeDetection = this->FastFileTypeDetection;
     info_set.insert(info);
