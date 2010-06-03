1d0ef3df85299d469b8cb4fe02812db5f329c345
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2010-06-03 11:20:48 -0400

    Fixed BUG 0010712 (attempt #2).
    Committing fix from Thierry Carrard. Basically removed the time seeking
    optimizations in vtkEnSightGolBinaryReader2 that are buggy. It solves the
    freezing problem. For sure, time seeking optimization has to be put back at some
    point, because sequential reading is way too slow on big files but the first
    goal is to maintain functionality.

--- Servers/Filters/vtkEnSightReader2.cxx
+++ Servers/Filters/vtkEnSightReader2.cxx
@@ -205,7 +205,7 @@ int vtkEnSightReader2::RequestData(
     this->ActualTimeValue = steps[cnt];
     }
 
-  cout << "Executing with: " << this->ActualTimeValue << endl;
+  vtkDebugMacro("Executing with: " << this->ActualTimeValue);
 
   int i, timeSet, fileSet, timeStep, timeStepInFile, fileNum;
   vtkDataArray *times;
@@ -1567,7 +1567,7 @@ int vtkEnSightReader2::ReadVariableFiles(vtkMultiBlockDataSet *output)
         numStepsList = static_cast<vtkIdList*>(this->FileSetNumberOfSteps->
                                GetItemAsObject(this->FileSets->IsId(fileSet)));
 
-        if (timeStep > numStepsList->GetId(0))
+        if (numStepsList!=0 && timeStep > numStepsList->GetId(0))
           {
           numSteps = numStepsList->GetId(0);
           timeStepInFile -= numSteps;
