Kitware/ParaView

commit 7c4ec9b4197330c50762bf1592b924f1a986e1f8

Use spreadsheet view by default for tables.

Qt/Core/pqDisplayPolicy.cxx
@@ -52,6 +52,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "pqPipelineSource.h"
 #include "pqRenderView.h"
 #include "pqServer.h"
+#include "pqSpreadSheetView.h"
 #include "pqTwoDRenderView.h"
 #include "pqXYBarChartView.h"
 #include "pqXYChartView.h"
@@ -166,6 +167,12 @@ QString pqDisplayPolicy::getPreferredViewType(pqOutputPort* opPort,
       // No cell data, but some point data -- may be a XY line plot.
       view_type = pqXYChartView::XYChartViewType();
       }
+    }
+
+  // Show table in spreadsheet view by default.
+  if (className == "vtkTable")
+    {
+    return pqSpreadSheetView::spreadsheetViewType(); 
     }
 
    // The proxy gives us no hint. In that case we try to determine the

commit f424a335f4ab16b7dd60e44bf92d0085ba4894bc

Fixed issue introduced by 7c4ec9b4197330c50762bf1592b924f1a986e1f8.
Now we were ending up with spreadsheet even for text sources (since they produce
tables too). Fixed that by ensuring that pqDisplayPolicy checks if the table is
really a text-source and then just do the default thing.

Qt/Core/pqDisplayPolicy.cxx
@@ -84,27 +84,39 @@ QString pqDisplayPolicy::getPreferredViewType(pqOutputPort* opPort,
     source->updatePipeline();
     }
 
+  // Some sources have hints that say that the output is to be treated as raw
+  // text. We flag such sources.
+  bool is_text = false;
+
   vtkPVXMLElement* hints = source->getHints();
   if (hints)
     {
     for (unsigned int cc=0; cc < hints->GetNumberOfNestedElements(); cc++)
       {
       vtkPVXMLElement* child = hints->GetNestedElement(cc);
-      if (child && child->GetName() &&
-        strcmp(child->GetName(), "View") == 0)
+      if (child && child->GetName())
         {
-        int port;
-        // If port exists, then it must match the port number for this port.
-        if (child->GetScalarAttribute("port", &port))
+        if (strcmp(child->GetName(), "View") == 0)
           {
-          if (opPort->getPortNumber() != port)
+          int port;
+          // If port exists, then it must match the port number for this port.
+          if (child->GetScalarAttribute("port", &port))
             {
-            continue;
+            if (opPort->getPortNumber() != port)
+              {
+              continue;
+              }
+            }
+          if (child->GetAttribute("type"))
+            {
+            return child->GetAttribute("type");
             }
           }
-        if (child->GetAttribute("type"))
+        else if (strcmp(child->GetName(), "OutputPort") == 0 &&
+          child->GetAttribute("type") &&
+          strcmp(child->GetAttribute("type"), "text") ==  0)
           {
-          return child->GetAttribute("type");
+          is_text = true;
           }
         }
       }
@@ -169,8 +181,9 @@ QString pqDisplayPolicy::getPreferredViewType(pqOutputPort* opPort,
       }
     }
 
-  // Show table in spreadsheet view by default.
-  if (className == "vtkTable")
+  // Show table in spreadsheet view by default (unless the table is to be
+  // treated as a "string" source).
+  if (className == "vtkTable" && !is_text)
     {
     return pqSpreadSheetView::spreadsheetViewType(); 
     }