8d3dd910d92e0f8df61837a6a2bc19117abb44f4
Andy Bauer <andy.bauer@kitware.com>
2010-06-28 16:05:36 -0400

    Adding sample python code for coprocessing.
    Adding a sample python driver that creates an unstructured
    grid on each process and then uses cp_pythonadaptorscript
    to perform coprocessing (when requested) on the inputed
    grid.
    
    I also cleaned out old code that wasn't being used or tested.

---
+++ CoProcessing/Adaptors/PythonAdaptor/cp_pythonadaptorscript.py
@@ -0,0 +1,90 @@
+try: paraview.simple
+except: from paraview.simple import *
+
+cp_writers = []
+
+def RequestDataDescription(datadescription):
+    "Callback to populate the request for current timestep"
+    timestep = datadescription.GetTimeStep()
+
+
+    input_name = 'input'
+    if (timestep % 2 == 0) :
+        datadescription.GetInputDescriptionByName(input_name).AllFieldsOn()
+        datadescription.GetInputDescriptionByName(input_name).GenerateMeshOn()
+    else:
+        datadescription.GetInputDescriptionByName(input_name).AllFieldsOff()
+        datadescription.GetInputDescriptionByName(input_name).GenerateMeshOff()
+
+
+def DoCoProcessing(datadescription):
+    "Callback to do co-processing for current timestep"
+    global cp_writers
+    cp_writers = []
+    timestep = datadescription.GetTimeStep()
+
+    PythonAdaptorDriver_vti = CreateProducer( datadescription, "input" )
+
+    Slice1 = Slice( guiName="Slice1", SliceOffsetValues=[0.0], SliceType="Plane" )
+    Slice1.SliceType.Offset = 0.0
+    Slice1.SliceType.Origin = [0.25, 0.5, 0.5]
+    Slice1.SliceType.Normal = [0.0, 1.0, 0.0]
+
+    ParallelPolyDataWriter1 = CreateWriter( XMLPPolyDataWriter, "filename_%t.pvtp", 2 )
+
+
+
+    for writer in cp_writers:
+        if timestep % writer.cpFrequency == 0:
+            writer.FileName = writer.cpFileName.replace("%t", str(timestep))
+            writer.UpdatePipeline()
+
+    if timestep % 1 == 0:
+        renderviews = servermanager.GetRenderViews()
+        imagefilename = ""
+        for view in range(len(renderviews)):
+            fname = imagefilename.replace("%v", str(view))
+            fname = fname.replace("%t", str(timestep))
+            WriteImage(fname, renderviews[view])
+
+    # explicitly delete the proxies -- we do it this way to avoid problems with prototypes
+    tobedeleted = GetProxiesToDelete()
+    while len(tobedeleted) > 0:
+        Delete(tobedeleted[0])
+        tobedeleted = GetProxiesToDelete()
+
+def GetProxiesToDelete():
+    iter = servermanager.vtkSMProxyIterator()
+    iter.Begin()
+    tobedeleted = []
+    while not iter.IsAtEnd():
+      if iter.GetGroup().find("prototypes") != -1:
+         iter.Next()
+         continue
+      proxy = servermanager._getPyProxy(iter.GetProxy())
+      proxygroup = iter.GetGroup()
+      iter.Next()
+      if proxygroup != 'timekeeper' and proxy != None and proxygroup.find("pq_helper_proxies") == -1 :
+          tobedeleted.append(proxy)
+
+    return tobedeleted
+
+def CreateProducer(datadescription, gridname):
+  "Creates a producer proxy for the grid"
+  if not datadescription.GetInputDescriptionByName(gridname):
+    raise RuntimeError, "Simulation input name '%s' does not exist" % gridname
+  grid = datadescription.GetInputDescriptionByName(gridname).GetGrid()
+  producer = TrivialProducer()
+  producer.GetClientSideObject().SetOutput(grid)
+  producer.UpdatePipeline()
+  return producer
+
+
+def CreateWriter(proxy_ctor, filename, freq):
+    global cp_writers
+    writer = proxy_ctor()
+    writer.FileName = filename
+    writer.add_attribute("cpFrequency", freq)
+    writer.add_attribute("cpFileName", filename)
+    cp_writers.append(writer)
+    return writer
---
+++ CoProcessing/Adaptors/PythonAdaptor/pythonadaptordriver.py
@@ -0,0 +1,67 @@
+# This python script is an example of using the coprocessing library directly
+# from a python code.  The user must properly set PYTHONPATH and LD_LIBRARY_PATH
+# based on the installation on their machine.  Examples below are for a
+# Linux machine.
+# export PYTHONPATH=${PYTHONPATH}:<ParaView build directory>/Utilities/VTKPythonWrapping:<ParaView build directory>/VTK/Wrapping/Python:<ParaView build directory>/debuginstall/bin
+#export LD_LIBRARY_PATH=<ParaView build directory>/bin:<Qt library directory>
+
+import mpi  # available through pyMPI
+print 'process ', mpi.rank, ' is running!'
+
+# set up for running paraview server in parallel
+import paraview
+paraview.options.batch = True
+paraview.options.symmetric = True
+
+# used to create the vtkUnstructuredGrid and add point data to it
+import vtk
+
+# arbitrary simulation parameters
+numtimesteps =  10
+starttime = 0.
+endtime = 1.
+timestep = (endtime-starttime)/numtimesteps
+
+# importing the coprocessing libraries needed for the adaptor
+import libvtkCoProcessorPython # on windows do import vtkCoProcessorPython
+
+# this is the actual coprocessing script created from the ParaView plugin
+import cp_pythonadaptorscript
+
+for step in range(numtimesteps):
+    currenttime = step*timestep
+    datadescription = libvtkCoProcessorPython.vtkCPDataDescription()
+    datadescription.SetTimeData(currenttime, step)
+    datadescription.AddInput("input")
+
+    # determine if we want to perform coprocessing this time step
+    cp_pythonadaptorscript.RequestDataDescription(datadescription)
+    inputdescription = datadescription.GetInputDescriptionByName("input")
+
+    if inputdescription.GetIfGridIsNecessary() == True:
+	# since we do want to perform coprocessing, create grid and field data
+	points = vtk.vtkPoints()
+	for i in range(2):
+	    points.InsertNextPoint(float(i+mpi.rank), 0, 0)
+	    points.InsertNextPoint(float(i+mpi.rank), 0, 1)
+	    points.InsertNextPoint(float(i+mpi.rank), 1, 1)
+	    points.InsertNextPoint(float(i+mpi.rank), 1, 0)
+
+	grid = vtk.vtkUnstructuredGrid()
+	grid.SetPoints(points)
+	pointIds = vtk.vtkIdList()
+	for i in range(8):
+            pointIds.InsertNextId(i)
+
+	grid.InsertNextCell(vtk.VTK_HEXAHEDRON, pointIds)
+
+        pointdata = vtk.vtkDoubleArray()
+        for point in range(grid.GetNumberOfPoints()):
+            loc = grid.GetPoint(point)
+            pointdata.InsertNextValue(currenttime*loc[0])
+
+        pointdata.SetName("varying array")
+        grid.GetPointData().AddArray(pointdata)
+
+        inputdescription.SetGrid(grid)
+        cp_pythonadaptorscript.DoCoProcessing(datadescription)
