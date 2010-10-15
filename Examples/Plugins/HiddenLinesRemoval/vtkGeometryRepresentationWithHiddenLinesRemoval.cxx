5befa0e96ee6c0cec95e24be9b41977e2ecfd542
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2010-10-14 21:36:17 -0400

    Fixed HiddenLinesRemoval plugin.

---
+++ Examples/Plugins/HiddenLinesRemoval/vtkGeometryRepresentationWithHiddenLinesRemoval.cxx
@@ -0,0 +1,66 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    $RCSfile$
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkGeometryRepresentationWithHiddenLinesRemoval.h"
+
+#include "vtkCompositePolyDataMapper2.h"
+#include "vtkDefaultPainter.h"
+#include "vtkObjectFactory.h"
+#include "vtkVisibleLinesPainter.h"
+
+vtkStandardNewMacro(vtkGeometryRepresentationWithHiddenLinesRemoval);
+//----------------------------------------------------------------------------
+vtkGeometryRepresentationWithHiddenLinesRemoval::vtkGeometryRepresentationWithHiddenLinesRemoval()
+{
+  if (this->Mapper)
+    {
+    // Replace the representation painter to the vtkVisibleLinesPainter,
+    // so when rendering as wireframe, we remove the hidden lines.
+    vtkVisibleLinesPainter* painter = vtkVisibleLinesPainter::New();
+    vtkDefaultPainter* dfPainter = vtkDefaultPainter::SafeDownCast(
+      this->Mapper->GetPainter());
+    dfPainter->SetRepresentationPainter(painter);
+    painter->Delete();
+
+    // Disable display lists, since vtkVisibleLinesPainter is not designed to work
+    // with display lists.
+    dfPainter->SetDisplayListPainter(0);
+    }
+
+  if (this->LODMapper)
+    {
+    // Replace the representation painter to the vtkVisibleLinesPainter,
+    // so when rendering as wireframe, we remove the hidden lines.
+    vtkVisibleLinesPainter* painter = vtkVisibleLinesPainter::New();
+    vtkDefaultPainter* dfPainter = vtkDefaultPainter::SafeDownCast(
+      this->LODMapper->GetPainter());
+    dfPainter->SetRepresentationPainter(painter);
+    painter->Delete();
+
+    // Disable display lists, since vtkVisibleLinesPainter is not designed to work
+    // with display lists.
+    dfPainter->SetDisplayListPainter(0);
+    }
+}
+
+//----------------------------------------------------------------------------
+vtkGeometryRepresentationWithHiddenLinesRemoval::~vtkGeometryRepresentationWithHiddenLinesRemoval()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkGeometryRepresentationWithHiddenLinesRemoval::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
