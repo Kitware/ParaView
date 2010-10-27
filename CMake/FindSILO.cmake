17bf7e74efb83b409b3d805d6cbe03c5542d6cdf
Robert Maynard <robert.maynard@kitware.com>
2010-10-27 11:44:26 -0400

    BUG: FindSilo now properly errors out if required.

--- CMake/FindSILO.cmake
+++ CMake/FindSILO.cmake
@@ -29,7 +29,11 @@ IF(SILO_INCLUDE_DIR)
     #The following deprecated settings are for backwards compatibility with CMake1.4
     SET (SILO_INCLUDE_PATH ${SILO_INCLUDE_DIR})
 
-  ENDIF(SILO_LIBRARY)
+  ELSE(SILO_LIBRARY)
+    IF(SILO_FIND_REQURIED)
+      message(SEND_ERROR "Unable to find the requested SILO libraries.")
+    ENDIF(SILO_FIND_REQURIED)
+  ENDIF
 ENDIF(SILO_INCLUDE_DIR)
 
 MARK_AS_ADVANCED(
