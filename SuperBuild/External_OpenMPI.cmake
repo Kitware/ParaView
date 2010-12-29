c791910ccb543bc83875efd1741140b27bd04044
David Partyka <david.partyka@kitware.com>
2010-12-29 16:49:08 -0500

    SuperBuild: If on Windows build with CMake otherwise ./configure.

--- SuperBuild/External_OpenMPI.cmake
+++ SuperBuild/External_OpenMPI.cmake
@@ -4,7 +4,8 @@ set(OpenMPI_source "${CMAKE_CURRENT_BINARY_DIR}/OpenMPI")
 set(OpenMPI_binary "${CMAKE_CURRENT_BINARY_DIR}/OpenMPI-build")
 set(OpenMPI_install "${CMAKE_CURRENT_BINARY_DIR}/OpenMPI-install")
 
-
+# If Windows we use CMake otherwise ./configure
+if(WIN32)
 ExternalProject_Add(OpenMPI
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${OpenMPI_source}
@@ -21,6 +22,20 @@ ExternalProject_Add(OpenMPI
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
   )
+
+else()
+  ExternalProject_Add(OpenMPI
+    DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+    SOURCE_DIR ${OpenMPI_source}
+    BINARY_DIR ${OpenMPI_build}
+    INSTALL_DIR ${OpenMPI_install}
+    URL ${OPENMPI_URL}/${OPENMPI_GZ}
+    URL_MD5 ${OPENMPI_MD5}
+    BUILD_IN_SOURCE 1
+    PATCH_COMMAND ""
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  )
+endif()
 
 set(OpenMPI_DIR "${OpenMPI_binary}" CACHE PATH
   "OpenMPI binary directory" FORCE)
