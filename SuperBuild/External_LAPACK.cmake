e3a51c1f19c2ea9a5a2e8a0fd6d0b4ce11c7dc61
David Partyka <dave.partyka@kitware.com>
2010-12-29 17:31:19 -0500

    SuperBuild: Configure NUMPY to build with either LAPACK or CLAPACK.

--- SuperBuild/External_LAPACK.cmake
+++ SuperBuild/External_LAPACK.cmake
@@ -2,22 +2,20 @@
 
 set(lapack_source "${CMAKE_CURRENT_BINARY_DIR}/LAPACK")
 set(lapack_binary "${CMAKE_CURRENT_BINARY_DIR}/LAPACK-build")
+set(NUMPY_LAPACK_binary ${lapack_binary})
 
 ExternalProject_Add(LAPACK
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${lapack_source}
   BINARY_DIR ${lapack_binary}
-  URL ${lapack_file}
-  URL_MD5 ${lapack_md5}
+  URL ${LAPACK_URL}/${LAPACK_GZ}
+  URL_MD5 ${LAPACK_MD5}
   CMAKE_ARGS
-    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
-    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
+    -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
+    -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     ${LAPACK_EXTRA_ARGS}
   INSTALL_COMMAND ""
   )
 list(APPEND trilinos_depends LAPACK)
-set(trilinos_blas_args
-  -DTPL_BLAS_LIBRARIES:PATH=${lapack_binary}/SRC/liblapack.so
-  -DTPL_LAPACK_LIBRARIES:PATH=${lapack_binary}/BLAS/SRC/libblas.so)
