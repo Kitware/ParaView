93273521436a6393b08b5246184a58aa2701191e
David Partyka <dave.partyka@kitware.com>
2010-12-29 15:53:00 -0500

    SuperBuild: Make it easier to customize C and CXX Flags.

--- SuperBuild/External_CLAPACK.cmake
+++ SuperBuild/External_CLAPACK.cmake
@@ -3,6 +3,15 @@
 set(CLAPACK_source "${CMAKE_CURRENT_BINARY_DIR}/CLAPACK")
 set(CLAPACK_binary "${CMAKE_CURRENT_BINARY_DIR}/CLAPACK-build")
 
+#
+# To fix compilation problem: relocation R_X86_64_32 against `a local symbol' can not be
+# used when making a shared object; recompile with -fPIC
+# See http://www.cmake.org/pipermail/cmake/2007-May/014350.html
+#
+if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+  set(CMAKE_C_FLAGS_CLAPACK "-fPIC ${CMAKE_C_FLAGS_CLAPACK}")
+endif()
+
 ExternalProject_Add(CLAPACK
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${CLAPACK_source}
@@ -10,6 +19,7 @@ ExternalProject_Add(CLAPACK
   URL ${CLAPACK_URL}/${CLAPACK_GZ}
   URL_MD5 ${CLAPACK_MD5}
   CMAKE_CACHE_ARGS
+    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS_CLAPACK}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
     -DBUILD_SHARED_LIBS:BOOL=OFF
     ${pv_tpl_compiler_args}
