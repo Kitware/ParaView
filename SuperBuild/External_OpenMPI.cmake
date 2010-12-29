4d3845b5299e6ccb0fcde5495d7dc1c8b084a6a4
David Partyka <dave.partyka@kitware.com>
2010-12-29 17:32:06 -0500

    SuperBuild: Indentation fixes and use C/CXX flags with warnings disabled.

--- SuperBuild/External_OpenMPI.cmake
+++ SuperBuild/External_OpenMPI.cmake
@@ -6,21 +6,22 @@ set(OpenMPI_install "${CMAKE_CURRENT_BINARY_DIR}/OpenMPI-install")
 
 # If Windows we use CMake otherwise ./configure
 if(WIN32)
-ExternalProject_Add(OpenMPI
-  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR ${OpenMPI_source}
-  BINARY_DIR ${OpenMPI_build}
-  INSTALL_DIR ${OpenMPI_install}
-  URL ${OPENMPI_URL}/${OPENMPI_GZ}
-  URL_MD5 ${OPENMPI_MD5}
-  CMAKE_CACHE_ARGS
-    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
-    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
-    ${pv_tpl_compiler_args}
-    ${OpenMPI_EXTRA_ARGS}
-  CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+
+  ExternalProject_Add(OpenMPI
+    DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+    SOURCE_DIR ${OpenMPI_source}
+    BINARY_DIR ${OpenMPI_build}
+    INSTALL_DIR ${OpenMPI_install}
+    URL ${OPENMPI_URL}/${OPENMPI_GZ}
+    URL_MD5 ${OPENMPI_MD5}
+    CMAKE_CACHE_ARGS
+      -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
+      -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
+      ${pv_tpl_compiler_args}
+      ${OpenMPI_EXTRA_ARGS}
+    CMAKE_ARGS
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
   )
 
 else()
@@ -35,8 +36,8 @@ else()
     PATCH_COMMAND ""
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
   )
+
 endif()
 
-set(OpenMPI_DIR "${OpenMPI_binary}" CACHE PATH
-  "OpenMPI binary directory" FORCE)
+set(OpenMPI_DIR "${OpenMPI_binary}" CACHE PATH "OpenMPI binary directory" FORCE)
 mark_as_advanced(OpenMPI_DIR)
