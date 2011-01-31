e56d0d51a04f615f45794392f509db213f94327b
David Partyka <david.partyka@kitware.com>
2011-01-31 16:54:57 -0500

    SuperBuild: Add MD5 check and --confirm-license args.

--- SuperBuild/External_PyQt.cmake
+++ SuperBuild/External_PyQt.cmake
@@ -1,14 +1,12 @@
-# The PYQT external project
-
-set(PYQT_binary "${CMAKE_CURRENT_BINARY_DIR}/PYQT/")
+# The PyQt external project
 
 ExternalProject_Add(PyQt
   URL ${PYQT_URL}/${PYQT_GZ}
+  URL_MD5 ${PYQT_MD5}
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/PyQt
-  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/PyQt
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE}
+  CONFIGURE_COMMAND ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE} --confirm-license
   DEPENDS
     ${PYQT_dependencies}
   )
