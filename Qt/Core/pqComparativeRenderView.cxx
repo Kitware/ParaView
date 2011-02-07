005b28cb07d633e4f0f6b25f656fd5c63916da6d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2011-02-07 18:11:36 -0500

    Bug #9337 : Fix the API to use the proxy property

--- Qt/Core/pqComparativeRenderView.cxx
+++ Qt/Core/pqComparativeRenderView.cxx
@@ -275,12 +275,11 @@ vtkImageData* pqComparativeRenderView::captureImage(int magnification)
         currentViews->GetItemAsObject(index));
       if (view)
         {
-#ifdef FIXME
         // There seems to be a bug where offscreen rendering
         // does not work with comparative view screenshots, so we
         // will force offscreen rendering off... FIXME!
-        view->SetUseOffscreenRenderingForScreenshots(0);
-#endif
+        vtkSMPropertyHelper(view, "UseOffscreenRenderingForScreenshots").Set(0);
+
         // Capture the image
         vtkImageData * image = view->CaptureWindow(magnification);
 
