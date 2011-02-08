9561206839ce113ae91160f6d91d132f8399dd43
Dave DeMarle <dave.demarle@kitware.com>
2011-02-07 21:07:43 -0500

    Make prioritization handle more than one array.
    MetaInfoDataBase now keeps ranges for a set of arrays not just one. Updated the
    streaming readers and sources correspondingly, especially the SPOP reader,
    which now allows you to pick any or all arrays instead of being hardcoded to
    the only the first array.
    
    Change-Id: Ic8d0bc1eb7820a04d288ef9bfb0285cbca3629f2

--- Plugins/StreamingView/VTK/vtkMetaInfoDatabase.h
+++ Plugins/StreamingView/VTK/vtkMetaInfoDatabase.h
@@ -12,26 +12,23 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-// .NAME vtkMetaInfoDatabase - A place to store scalar ranges for pieces of dataset.
+// .NAME vtkMetaInfoDatabase - A place to store array ranges for pieces of dataset.
 // .SECTION Description
-// This class maintains hierarchical records of scalar ranges for different parts of a file.
-// As information is obtained, it is inserted into the database. Lower resolution records are
-// assumed to be less precise than their higher resolution contents. So when new records are
-// added, old records are updated to reflect their contents.
-// When ranges are queried, the most precise information for the piece is returned.
+// This class maintains hierarchical records of array ranges for different parts
+// of a file. As information is obtained, it is inserted into the database. Lower
+// resolution records are assumed to be less precise than their higher resolution
+// contents. So when new records are added, old records are updated to reflect
+// their contents. When ranges are queried, the most precise information for the
+// piece is returned.
 //
 // LIMITATIONS:
 // 1) A binary relationship is used to determine piece parentage,
 // which may not be true if you do not use the default extent translator.
-// 2) I intend to keep geometric metainformation here as well, not just attribute
-// metainformation.
 
 #ifndef __vtkMetaInfoDatabase_h
 #define __vtkMetaInfoDatabase_h
 
 #include "vtkObject.h"
-
-class vtkMIDBInternals;
 
 class VTK_EXPORT vtkMetaInfoDatabase : public vtkObject
 {
@@ -40,19 +37,27 @@ public:
   vtkTypeMacro(vtkMetaInfoDatabase,vtkObject);
   virtual void PrintSelf(ostream& os, vtkIndent indent);
 
-  //insert a scalar range record for piece p/np at resolution res
+  //Description:
+  //insert a range for an array within piece p/np at resolution res
+  //will update any ancestor pieces
   //NOTE:ext is currently ignored
-  void Insert(int p, int np, int ext[6], double range[2], double r);
+  void Insert(int p, int np, int ext[6], double resolution,
+              int field_association, const char *ArrayName, int component,
+              double range[2]);
 
-  //request the scalar range record for piece p/np
+  //Description:
+  //request the range record for piece p/np
   //NOTE:ext is currently ignored
-  int Search(int p, int np, int ext[6], double *range);
+  int Search(int p, int np, int ext[6],
+             int field_association, const char *ArrayName, int component,
+             double *range);
 
 protected:
   vtkMetaInfoDatabase();
   ~vtkMetaInfoDatabase();
 
-  vtkMIDBInternals *Internals;
+  class ArrayRecords;
+  ArrayRecords *Records;
 
 private:
   vtkMetaInfoDatabase(const vtkMetaInfoDatabase&);  // Not implemented.
