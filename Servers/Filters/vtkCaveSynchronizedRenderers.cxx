2a76050e8c7c4c3cc572b334cd330412af2cafd3
Nikhil Shetty <nikhil.shetty@kitware.com>
2011-02-08 18:02:42 -0500

    Fixed bug which reset displays on all screens
    Change-Id: I44eeb2ba99a4a54f7c90070d29a3e8edd9805697

--- Servers/Filters/vtkCaveSynchronizedRenderers.cxx
+++ Servers/Filters/vtkCaveSynchronizedRenderers.cxx
@@ -50,6 +50,9 @@ vtkCaveSynchronizedRenderers::vtkCaveSynchronizedRenderers()
   this->DisplayY[2] = 0.0;
   this->DisplayY[3] = 1.0;
 
+  // Screen surface rotation matrix
+  SurfaceRot = vtkMatrix4x4::New();
+  once =1;
   this->SetParallelController(vtkMultiProcessController::GetGlobalController());
 
   // Initilize using pvx file specified on the command line options.
@@ -69,9 +72,7 @@ vtkCaveSynchronizedRenderers::vtkCaveSynchronizedRenderers()
         options->GetLowerRight(cc), options->GetUpperRight(cc));
       }
     }
-
-  // Screen surface rotation matrix
-  SurfaceRot = vtkMatrix4x4::New();
+  this->SetDisplayConfig();
 }
 
 //----------------------------------------------------------------------------
@@ -106,6 +107,7 @@ void vtkCaveSynchronizedRenderers::SetNumberOfDisplays(int numberOfDisplays)
       newDisplays[i] = new double[12];
       if (i < this->NumberOfDisplays)
         {
+
         memcpy(newDisplays[i], this->Displays[i], 12 * sizeof(double));
         }
       else
@@ -166,32 +168,20 @@ void vtkCaveSynchronizedRenderers::DefineDisplay(
 // It is the room camera transformed by the world camera.
 void vtkCaveSynchronizedRenderers::ComputeCamera(vtkCamera* cam)
 {
-  if ( cam->GetLeftEye() == 1 )
-    return;
-
-  this->SetDisplayConfig();
-  cam->SetHeadTracked( true );
-
-  // Set O2Screen, O2Right, O2Left, O2Bottom, O2Top
-  double O2Screen = - this->DisplayOrigin[2];
-  double O2Right  =   this->DisplayX[0];
-  double O2Left   = - this->DisplayOrigin[0];
-  double O2Top    =   this->DisplayY[1];
-  double O2Bottom = - this->DisplayX[1];
-  cam->SetConfigParams( O2Screen, O2Right, O2Left,O2Top, O2Bottom,
-                        0.065, 1.0,
-                        this->SurfaceRot);
+  if(once)
+    {
+      cam->SetHeadTracked( true );
+      cam->SetConfigParams( this->O2Screen, this->O2Right,
+                            this->O2Left, this->O2Top, this->O2Bottom,
+                            0.065, 1.0, this->SurfaceRot);
+      once =0;
+    }
 }
 
 //------------------------------------------------------------------HeadTracked
 // This enables the display config for head tracking
 void vtkCaveSynchronizedRenderers::SetDisplayConfig()
 {
-  // The room coordinates
-  double xRoom[3]={1.0, 0.0, 0.0 };
-  double yRoom[3]={0.0, 1.0, 0.0 };
-  double zRoom[3]={0.0, 0.0, 1.0 };
-
   // Base coordinates of the screen
   double xBase[3], yBase[3], zBase[3];
   for (int i = 0; i < 3; ++i)
@@ -201,20 +191,24 @@ void vtkCaveSynchronizedRenderers::SetDisplayConfig()
     }
   vtkMath::Cross( xBase, yBase, zBase );
 
-  this->SetSurfaceRotation( xBase, yBase, zBase, xRoom, yRoom, zRoom );
+  this->SetSurfaceRotation( xBase, yBase, zBase);
 
   // Get the new DisplayOrigin, DisplayX and DisplayY after transfromation
   this->SurfaceRot->MultiplyPoint( this->DisplayOrigin, this->DisplayOrigin );
   this->SurfaceRot->MultiplyPoint( this->DisplayX, this->DisplayX );
   this->SurfaceRot->MultiplyPoint( this->DisplayY, this->DisplayY );
+
+  // Set O2Screen, O2Right, O2Left, O2Bottom, O2Top
+  this->O2Screen = - this->DisplayOrigin[2];
+  this->O2Right  =   this->DisplayX[0];
+  this->O2Left   = - this->DisplayOrigin[0];
+  this->O2Top    =   this->DisplayY[1];
+  this->O2Bottom = - this->DisplayX[1];
 }
 
 void vtkCaveSynchronizedRenderers::SetSurfaceRotation( double xBase[3],
                                                        double yBase[3],
-                                                       double zBase[3],
-                                                       double xRoom[3],
-                                                       double yRoom[3],
-                                                       double zRoom[3])
+                                                       double zBase[3])
 {
   vtkMath::Normalize( xBase );
   vtkMath::Normalize( yBase );
@@ -231,23 +225,37 @@ void vtkCaveSynchronizedRenderers::SetSurfaceRotation( double xBase[3],
   this->SurfaceRot->SetElement( 2, 0, zBase[0]);
   this->SurfaceRot->SetElement( 2, 1, zBase[1]);
   this->SurfaceRot->SetElement( 2, 2, zBase[2]);
-
-  // Calculate directional cosine matrix to get surface rotation
-  // SurfaceRot->SetElement( 0, 0, vtkMath::Dot( xBase, xRoom ) );
-  // SurfaceRot->SetElement( 0, 1, vtkMath::Dot( yBase, xRoom ) );
-  // SurfaceRot->SetElement( 0, 2, vtkMath::Dot( zBase, xRoom ) );
-
-  // SurfaceRot->SetElement( 1, 0, vtkMath::Dot( xBase, yRoom ) );
-  // SurfaceRot->SetElement( 1, 1, vtkMath::Dot( yBase, yRoom ) );
-  // SurfaceRot->SetElement( 1, 2, vtkMath::Dot( zBase, yRoom ) );
-
-  // SurfaceRot->SetElement( 2, 0, vtkMath::Dot( xBase, zRoom ) );
-  // SurfaceRot->SetElement( 2, 1, vtkMath::Dot( yBase, zRoom ) );
-  // SurfaceRot->SetElement( 2, 2, vtkMath::Dot( zBase, zRoom ) );
 }
 
 //----------------------------------------------------------------------------
 void vtkCaveSynchronizedRenderers::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
+  os << indent << "NumberOfDisplays: "
+    << this->NumberOfDisplays << endl;
+  vtkIndent rankIndent = indent.GetNextIndent();
+  for (int i = 0; i < this->NumberOfDisplays; ++i)
+    {
+    os << rankIndent;
+    for (int j = 0; j < 12; ++j)
+      {
+      os << this->Displays[i][j] << " ";
+      }
+    os << endl;
+    }
+  os << indent << "Origin: "
+     << this->DisplayOrigin[0] << " "
+     << this->DisplayOrigin[1] << " "
+     << this->DisplayOrigin[2] << " "
+     << this->DisplayOrigin[3] << endl;
+  os << indent << "X: "
+     << this->DisplayX[0] << " "
+     << this->DisplayX[1] << " "
+     << this->DisplayX[2] << " "
+     << this->DisplayX[3] << endl;
+  os << indent << "Y: "
+     << this->DisplayY[0] << " "
+     << this->DisplayY[1] << " "
+     << this->DisplayY[2] << " "
+     << this->DisplayY[3] << endl;
 }
