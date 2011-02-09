ba47f16d130c24eefae3d321d8afba5cbaad584e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2011-02-09 10:35:17 -0500

    Fix to automaticaly enable Save Data

--- Servers/ServerManager/vtkSMDataTypeDomain.cxx
+++ Servers/ServerManager/vtkSMDataTypeDomain.cxx
@@ -167,6 +167,14 @@ int vtkSMDataTypeDomain::IsInDomain(vtkSMSourceProxy* proxy,
 
   // Make sure the outputs are created.
   proxy->CreateOutputPorts();
+
+  // FIXME_COLLABORATION:
+  //      This update might need to be done somewhere else ??? It has been added
+  //      here so the "Save Data" became enable once a sphere is created before
+  //      the apply button get hit !!!
+  proxy->UpdatePipeline();
+
+
   vtkPVDataInformation* info = proxy->GetDataInformation(outputport);
   if (!info)
     {
