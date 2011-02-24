88a435babb1e97737168ffdd649708c612e092d2
David Partyka <david.partyka@kitware.com>
2011-02-23 20:24:30 -0500

    SuperBuild: Add logic to build HDF5 and Silo.

--- SuperBuild/External_HDF5.cmake
+++ SuperBuild/External_HDF5.cmake
@@ -1,18 +1,20 @@
 
-# The png external project for ParaView
-set(png_source "${CMAKE_CURRENT_BINARY_DIR}/png")
-set(png_binary "${CMAKE_CURRENT_BINARY_DIR}/png-build")
-set(png_install "${CMAKE_CURRENT_BINARY_DIR}/png-install")
+# The HDF5 external project for ParaView
+set(hdf5_source "${CMAKE_CURRENT_BINARY_DIR}/hdf5")
+set(hdf5_install "${CMAKE_CURRENT_BINARY_DIR}/hdf5-install")
 
 # If Windows we use CMake otherwise ./configure
 if(WIN32)
 
-  ExternalProject_Add(png
-  URL ${PNG_URL}/${PNG_GZ}
-  URL_MD5 ${PNG_MD5}
+  set(hdf5_binary "${CMAKE_CURRENT_BINARY_DIR}/hdf5-build")
+
+  ExternalProject_Add(HDF5
+  URL ${HDF5_URL}/${HDF5_GZ}
+  URL_MD5 ${HDF5_MD5}
   UPDATE_COMMAND ""
-  SOURCE_DIR ${png_source}
-  BINARY_DIR ${png_binary}
+  SOURCE_DIR ${hdf5_source}
+  BINARY_DIR ${hdf5_binary}
+  INSTALL_DIR ${hdf5_install}
   CMAKE_CACHE_ARGS
     -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
     -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
@@ -22,18 +24,17 @@ if(WIN32)
     -DZLIB_LIBRARY:STRING=${ZLIB_LIBRARY}
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-  DEPENDS ${png_dependencies}
+  DEPENDS ${HDF5_dependencies}
   )
 
 else()
 
-  ExternalProject_Add(png
+  ExternalProject_Add(HDF5
     DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-    SOURCE_DIR ${png_source}
-    BINARY_DIR ${png_build}
-    INSTALL_DIR ${png_install}
-    URL ${PNG_URL}/${PNG_GZ}
-    URL_MD5 ${PNG_MD5}
+    SOURCE_DIR ${hdf5_source}
+    INSTALL_DIR ${hdf5_install}
+    URL ${HDF5_URL}/${HDF5_GZ}
+    URL_MD5 ${HDF5_MD5}
     BUILD_IN_SOURCE 1
     PATCH_COMMAND ""
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
