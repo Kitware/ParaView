3cc258068d31d4067e978a872da1fe026d621ea3
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2011-03-04 11:36:41 -0500

    BUG 9473: Fix Fecth of definition from server

--- ParaViewCore/ServerManager/vtkSMSessionClient.cxx
+++ ParaViewCore/ServerManager/vtkSMSessionClient.cxx
@@ -29,6 +29,7 @@
 #include "vtkSMProxy.h"
 #include "vtkSMProxyManager.h"
 #include "vtkSMSessionServer.h"
+#include "vtkSMProxyDefinitionManager.h"
 #include "vtkSocketCommunicator.h"
 
 #include <vtkstd/string>
@@ -269,6 +270,13 @@ bool vtkSMSessionClient::Connect(const char* url)
 void vtkSMSessionClient::Initialize()
 {
   this->Superclass::Initialize();
+
+  // Update definition from server
+  vtkSMMessage msg;
+  msg.set_global_id(vtkPVSession::RESERVED_PROXY_DEFINITION_MANAGER_ID);
+  msg.set_location(vtkProcessModule::DATA_SERVER); // We want to request data server
+  this->PullState(&msg);
+  this->GetProxyDefinitionManager()->LoadXMLDefinitionState(&msg);
 
   // Setup the socket connnection between data-server and render-server.
   if (this->DataServerController && this->RenderServerController)
