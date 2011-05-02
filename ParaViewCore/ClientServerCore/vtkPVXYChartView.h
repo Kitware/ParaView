88a1f696868cc2c343dda8a4a23f0ba51388cdd2
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2011-05-02 14:52:53 -0400

    BUG: #11623 - set legend location in charts.
    Added a new property to set the legend location in the charts, linked
    this up to the GUI. Hid the flow for now as that is not implemented in
    VTK, there is also no feedback from the VTK chart when the legend was
    moved around using mouse interations (the location becomes CUSTOM).
    
    Change-Id: I287683327f3100da82c0d08f6db68cc798f47ae0

--- ParaViewCore/ClientServerCore/vtkPVXYChartView.h
+++ ParaViewCore/ClientServerCore/vtkPVXYChartView.h
@@ -73,6 +73,13 @@ public:
   void SetLegendVisibility(int visible);
 
   // Description:
+  // Set the legend location.
+  // These methods should not be called directly. They are made public only so
+  // that the client-server-stream-interpreter can invoke them. Use the
+  // corresponding properties to change these values.
+  void SetLegendLocation(int location);
+
+  // Description:
   // Sets whether or not the given axis is visible.
   // These methods should not be called directly. They are made public only so
   // that the client-server-stream-interpreter can invoke them. Use the
