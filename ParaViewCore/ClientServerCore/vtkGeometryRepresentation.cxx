Kitware/ParaView

commit cb9bc043970d96dc5f36ca9b7e06c74d0b78eda6

Fixed BUG #12546. RequestData was called twice in parallel.
The issue was introduced by fix for BUG #11811. We were now requesting
ghost-cells in representations to use the ghost-cell information when available
during rendering. However that meant that all the request changed since previous
update on the pipeline, since the GUI would call UpdatePipeline() when creating
the representation but without the extra ghost cells.

Fixed this issue by making the GUI also ask for ghost-cells in a similar fashion
to vtkGeometryRepresentation. This was done by updating
vtkSISourceProxy::UpdatePipeline to request ghost cells using the logic in
vtkGeometryRepresentation.

vtkGeometryRepresentation was checking for data-type when deciding whether to
ask for ghost cells. That meant that vtkSISourceProxy couldn't use it since the
algorithm had not produced any data yet. Anyways, the datatype check was
unnecessary since we were using the presence of WHOLE_EXTENT() key to exclude
any structure data pipelines. So removed that type check. I verified that it
doesn't lead to any wrong ghost cell requests when contouring, for example.

ParaViewCore/ClientServerCore/vtkGeometryRepresentation.cxx
@@ -294,6 +294,28 @@ int vtkGeometryRepresentation::ProcessViewRequest(
 }
 
 //----------------------------------------------------------------------------
+bool vtkGeometryRepresentation::DoRequestGhostCells(vtkInformation* info)
+{
+  vtkMultiProcessController* controller =
+    vtkMultiProcessController::GetGlobalController();
+  if (controller == NULL || controller->GetNumberOfProcesses() <= 1)
+    {
+    return false;
+    }
+
+  // ensure that there's no WholeExtent to ensure
+  // that this UG was never born out of a structured dataset.
+  bool has_whole_extent = (info->Has(
+      vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT()) != 0);
+  if (!has_whole_extent)
+    {
+    return true;
+    }
+
+  return false;
+}
+
+//----------------------------------------------------------------------------
 int vtkGeometryRepresentation::RequestUpdateExtent(vtkInformation* request,
   vtkInformationVector** inputVector,
   vtkInformationVector* outputVector)
@@ -302,9 +324,7 @@ int vtkGeometryRepresentation::RequestUpdateExtent(vtkInformation* request,
 
   // ensure that the ghost-level information is setup correctly to avoid
   // internal faces for unstructured grids.
-  vtkMultiProcessController* controller =
-    vtkMultiProcessController::GetGlobalController();
-  for (int cc=0; cc < this->GetNumberOfInputPorts() && controller != NULL; cc++)
+  for (int cc=0; cc < this->GetNumberOfInputPorts(); cc++)
     {
     for (int kk=0; kk < inputVector[cc]->GetNumberOfInformationObjects(); kk++)
       {
@@ -313,21 +333,9 @@ int vtkGeometryRepresentation::RequestUpdateExtent(vtkInformation* request,
       vtkStreamingDemandDrivenPipeline* sddp =
         vtkStreamingDemandDrivenPipeline::SafeDownCast(this->GetExecutive());
       int ghostLevels = sddp->GetUpdateGhostLevel(inInfo);
-      if (controller->GetNumberOfProcesses() > 1)
+      if (vtkGeometryRepresentation::DoRequestGhostCells(inInfo))
         {
-        vtkUnstructuredGrid* ug_input = vtkUnstructuredGrid::GetData(inInfo);
-        vtkCompositeDataSet* cd_input = vtkCompositeDataSet::GetData(inInfo);
-        if (ug_input || cd_input)
-          {
-          // ensure that there's no WholeExtent to ensure
-          // that this UG was never born out of a structured dataset.
-          bool has_whole_extent = (inInfo->Has(
-              vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT()) != 0);
-          if (!has_whole_extent)
-            {
-            ghostLevels++;
-            }
-          }
+        ghostLevels++;
         }
       sddp->SetUpdateGhostLevel(inInfo, ghostLevels);
       }

ParaViewCore/ClientServerCore/vtkGeometryRepresentation.h
@@ -124,6 +124,11 @@ public:
   // Returns the data object that is rendered from the given input port.
   virtual vtkDataObject* GetRenderedDataObject(int port);
 
+  // Description:
+  // Returns true if this class would like to get ghost-cells if available for
+  // the connection whose information object is passed as the argument.
+  static bool DoRequestGhostCells(vtkInformation* information); 
+
   //***************************************************************************
   // Forwarded to vtkPVGeometryFilter
   void SetUseOutline(int);

ParaViewCore/ServerImplementation/vtkSISourceProxy.cxx
@@ -19,6 +19,7 @@
 #include "vtkClientServerInterpreter.h"
 #include "vtkCommand.h"
 #include "vtkCompositeDataPipeline.h"
+#include "vtkGeometryRepresentation.h"
 #include "vtkInformation.h"
 #include "vtkInstantiator.h"
 #include "vtkMultiProcessController.h"
@@ -354,9 +355,17 @@ void vtkSISourceProxy::UpdatePipeline(int port, double time, bool doTime)
   vtkStreamingDemandDrivenPipeline* sddp =
     vtkStreamingDemandDrivenPipeline::SafeDownCast(
       algo->GetExecutive());
-
   int real_port = output_port->GetIndex();
-  sddp->SetUpdateExtent(real_port, processid, numprocs, /*ghost level*/0);
+
+  // Refer to BUG #11811 and BUG #12546. vtkGeometryRepresentation needs
+  // ghost-cells if available (11811), but not asking for ghost-cells earlier than the
+  // representation results in multiple executes (12546). Hence, we request
+  // ghost-cells in UpdatePipeline().
+  bool req_ghost_cells = vtkGeometryRepresentation::DoRequestGhostCells(
+    sddp->GetOutputInformation(real_port));
+
+  sddp->SetUpdateExtent(real_port, processid, numprocs, /*ghost level*/
+    req_ghost_cells?1 : 0);
   if (doTime)
     {
     sddp->SetUpdateTimeStep(real_port, time);

commit 57c3c95a1ac6bf6ca0a689b544bd6dc59d9a83d2

Fixes issues caused by cb9bc043970d96.
(1) We had to bring back the type checking since we ended up requesting
   ghost-levels for filters like probe-over-line which was not expected.
(2) To ensure type-checking works, added a call to UpdateDataObject() in
   vtkSISourceProxy::UpdatePipeline().
(3) vtkSelectionRepresentation has an internal vtkGeometryRepresentation which
   always ended up asking for ghost cells since the internal data-object is a
   composite dataset. That was resulting in BUG #11811 all over again, after
   (2). Fixed that.

ParaViewCore/ClientServerCore/vtkGeometryRepresentation.cxx
@@ -104,6 +104,7 @@ vtkGeometryRepresentation::vtkGeometryRepresentation()
   this->LODUpdateSuppressor = vtkPVUpdateSuppressor::New();
   this->DeliverySuppressor = vtkPVUpdateSuppressor::New();
   this->LODDeliverySuppressor = vtkPVUpdateSuppressor::New();
+  this->RequestGhostCellsIfNeeded = true;
 
   this->ColorArrayName = 0;
   this->ColorAttributeType = VTK_SCALAR_MODE_DEFAULT;
@@ -303,13 +304,18 @@ bool vtkGeometryRepresentation::DoRequestGhostCells(vtkInformation* info)
     return false;
     }
 
-  // ensure that there's no WholeExtent to ensure
-  // that this UG was never born out of a structured dataset.
-  bool has_whole_extent = (info->Has(
-      vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT()) != 0);
-  if (!has_whole_extent)
+  if (vtkUnstructuredGrid::GetData(info) != NULL ||
+    vtkCompositeDataSet::GetData(info) != NULL)
     {
-    return true;
+    // ensure that there's no WholeExtent to ensure
+    // that this UG was never born out of a structured dataset.
+    bool has_whole_extent = (info->Has(
+        vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT()) != 0);
+    if (!has_whole_extent)
+      {
+      //cout << "Need ghosts" << endl;
+      return true;
+      }
     }
 
   return false;
@@ -333,7 +339,8 @@ int vtkGeometryRepresentation::RequestUpdateExtent(vtkInformation* request,
       vtkStreamingDemandDrivenPipeline* sddp =
         vtkStreamingDemandDrivenPipeline::SafeDownCast(this->GetExecutive());
       int ghostLevels = sddp->GetUpdateGhostLevel(inInfo);
-      if (vtkGeometryRepresentation::DoRequestGhostCells(inInfo))
+      if (this->RequestGhostCellsIfNeeded &&
+        vtkGeometryRepresentation::DoRequestGhostCells(inInfo))
         {
         ghostLevels++;
         }

ParaViewCore/ClientServerCore/vtkGeometryRepresentation.h
@@ -129,6 +129,14 @@ public:
   // the connection whose information object is passed as the argument.
   static bool DoRequestGhostCells(vtkInformation* information); 
 
+  // Description:
+  // Representations that use geometry representation as the internal
+  // representation should turn this flag off so that we don't end up requesting
+  // ghost cells twice.
+  vtkSetMacro(RequestGhostCellsIfNeeded, bool);
+  vtkGetMacro(RequestGhostCellsIfNeeded, bool);
+  vtkBooleanMacro(RequestGhostCellsIfNeeded, bool);
+
   //***************************************************************************
   // Forwarded to vtkPVGeometryFilter
   void SetUseOutline(int);
@@ -260,6 +268,7 @@ protected:
   int Representation;
   bool SuppressLOD;
   bool AllowSpecularHighlightingWithScalarColoring;
+  bool RequestGhostCellsIfNeeded;
 
 private:
   vtkGeometryRepresentation(const vtkGeometryRepresentation&); // Not implemented

ParaViewCore/ClientServerCore/vtkSelectionRepresentation.cxx
@@ -31,6 +31,7 @@ vtkSelectionRepresentation::vtkSelectionRepresentation()
   this->GeometryRepresentation = vtkGeometryRepresentation::New();
   this->GeometryRepresentation->SetPickable(0);
   this->GeometryRepresentation->SetDebugString("vtkSelectionRepresentation");
+  this->GeometryRepresentation->RequestGhostCellsIfNeededOff();
 
   this->LabelRepresentation = vtkDataLabelRepresentation::New();
   this->LabelRepresentation->SetPointLabelMode(VTK_LABEL_FIELD_DATA);

ParaViewCore/ServerImplementation/vtkSISourceProxy.cxx
@@ -351,10 +351,14 @@ void vtkSISourceProxy::UpdatePipeline(int port, double time, bool doTime)
   vtkAlgorithm* algo = output_port->GetProducer();
   assert(algo);
 
-  algo->UpdateInformation();
+
   vtkStreamingDemandDrivenPipeline* sddp =
     vtkStreamingDemandDrivenPipeline::SafeDownCast(
       algo->GetExecutive());
+
+  sddp->UpdateInformation();
+  sddp->UpdateDataObject();
+
   int real_port = output_port->GetIndex();
 
   // Refer to BUG #11811 and BUG #12546. vtkGeometryRepresentation needs