767d76d27bc18222a008509b56510d05f456e7fd
David Partyka <david.partyka@kitware.com>
2011-09-27 10:28:05 -0400

    Fix name of libg2c.
    Change-Id: Ib8bb8d98fea973785467dad2a0e0f6ddbfca3228

--- SuperBuild/install_g2c.cmake
+++ SuperBuild/install_g2c.cmake
@@ -2,7 +2,7 @@
 # needed if g77 was used
 if (UNIX AND PARAVIEW_ENABLE_PYTHON)
 
-  find_file(g2c_library libg2c.so.0 /usr/lib)
+  find_file(g2c_library libg2c.so /usr/lib)
 
   get_filename_component(g2c_library_dir ${g2c_library} PATH)
   get_filename_component(g2c_library_name ${g2c_library} NAME)
