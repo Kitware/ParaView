d08caebbfbc00c43bc7fa6a6e6c1b602b750106a
George Zagaris <george.zagaris@kitware.com>
2011-11-04 09:58:28 -0400

    BUGFIX: Corrected file path
    Change-Id: I6846057d31aa3ecc7d5e13fe206129024ddabf63

--- ParaViewCore/VTKExtensions/Testing/TestPVAMRDualContour.cxx
+++ ParaViewCore/VTKExtensions/Testing/TestPVAMRDualContour.cxx
@@ -25,53 +25,24 @@ typedef vtkSmartPointer<vtkDummyController> vtkDummyControllerRefPtr;
 typedef vtkSmartPointer<vtkSpyPlotReader> vtkSpyPlotReaderRefPtr;
 typedef vtkSmartPointer<vtkPVAMRDualContour> vtkPVAMRDualContourRefPtr;
 
+
 int main( int argc, char* argv[] )
 {
   vtkDummyControllerRefPtr controller (vtkDummyControllerRefPtr::New());
   vtkMultiProcessController::SetGlobalController(controller);
 
   int rc            = 0;
-  const char *fname ="/media/shared/Data/ParaViewData/Data/SPCTH/spcth.0";
+  const char *fname =
+      "/media/shared/Data/ParaViewData/Data/SPCTH/Dave_Karelitz_Small/spcth.0";
 
-  std::cout << "Opening file...";
-  std::cout.flush();
   vtkSpyPlotReaderRefPtr reader = vtkSpyPlotReaderRefPtr::New();
-  std::cout << "Set file name...";
-  std::cout.flush();
   reader->SetFileName( fname );
-  std::cout << "[DONE]\n";
-  std::cout.flush();
-
-  std::cout << "Set global controller...";
   reader->SetGlobalController(controller);
-  std::cout << "[DONE]\n";
-  std::cout.flush();
-
-  std::cout << "MergeXYZComponents...";
   reader->MergeXYZComponentsOn();
-  std::cout << "[DONE]\n";
-  std::cout.flush();
-
-  std::cout << "DownConvertVolumeFractionOn...";
   reader->DownConvertVolumeFractionOn();
-  std::cout << "[DONE]\n";
-  std::cout.flush();
-
-  std::cout << "DistributeFilesOn...";
   reader->DistributeFilesOn();
-  std::cout << "[DONE]\n";
-  std::cout.flush();
-
-  std::cout << "SetCellArrayStatus...";
-  reader->SetCellArrayStatus("Material volume fraction -3", 1);
-  std::cout << "[DONE]\n";
-  std::cout.flush();
-
-  std::cout << "Reader.Update()...";
-  std::cout.flush();
+  reader->SetCellArrayStatus("Material volume fraction - 2", 1);
   reader->Update();
-  std::cout << "[DONE]\n";
-  std::cout.flush();
 
   vtkPVAMRDualContourRefPtr contour = vtkPVAMRDualContourRefPtr::New();
   contour->SetInput( reader->GetOutputDataObject(0) );
@@ -79,7 +50,7 @@ int main( int argc, char* argv[] )
   contour->SetEnableMergePoints(1);
   contour->SetEnableDegenerateCells(1);
   contour->SetEnableMultiProcessCommunication(1);
-  contour->AddInputCellArrayToProcess("Material volume fraction - 3");
+  contour->AddInputCellArrayToProcess("Material volume fraction - 2");
   contour->Update();
 
   return( rc );
