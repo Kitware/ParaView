718e1477dc2b38db447ae065694acd4d8fbd4acd
David Partyka <david.partyka@kitware.com>
2011-12-20 16:06:26 -0500

    Just build head of AcuSolve repo for now.
    Change-Id: I162219cfdb83d5ed63e37f051f66f1a0196a36b3

--- SuperBuild/External_AcuSolveReaderPlugin.cmake
+++ SuperBuild/External_AcuSolveReaderPlugin.cmake
@@ -5,7 +5,6 @@ set(AcuSolveReaderPlugin_source "${CMAKE_CURRENT_BINARY_DIR}/AcuSolveReaderPlugi
 # and configure and build it
 ExternalProject_Add(AcuSolveReaderPlugin
   GIT_REPOSITORY "git://kwsource.kitwarein.com/paraview/acusolvereaderplugin.git"
-  GIT_TAG 3338d1e44b97353b298ed9be87b69aef56153538
   SOURCE_DIR ${AcuSolveReaderPlugin_source}
   BINARY_DIR ""
   CONFIGURE_COMMAND ""
