66f845398b4a2946affaa3c4fd90d36460ebbf0a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2011-12-27 15:19:57 -0500

    Updated tests

--- Applications/ParaView/Testing/Python/SaveLargeScreenshot.py
+++ Applications/ParaView/Testing/Python/SaveLargeScreenshot.py
@@ -3,63 +3,45 @@
 import QtTesting
 import QtTestingImage
 
-object1 = 'pqClientMainWindow/menubar/menuSources'
-QtTesting.playCommand(object1, 'activate', 'RTAnalyticSource')
-object2 = 'pqClientMainWindow/objectInspectorDock/objectInspector/Accept'
-QtTesting.playCommand(object2, 'activate', '')
-object3 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/0/MultiViewFrameMenu/SplitHorizontalButton'
+object1 = 'pqClientMainWindow/menubar'
+QtTesting.playCommand(object1, 'activate', 'menuSources')
+object2 = 'pqClientMainWindow/menubar/menuSources'
+QtTesting.playCommand(object2, 'activate', 'SphereSource')
+object3 = 'pqClientMainWindow/summaryDock/summaryWidget/1QFrame0/Accept'
 QtTesting.playCommand(object3, 'activate', '')
-object4 = 'pqClientMainWindow/menubar/menuFilters/DataAnalysis'
-QtTesting.playCommand(object4, 'activate', 'ProbeLine')
-QtTesting.playCommand(object2, 'activate', '')
-object5 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/1/MultiViewFrameMenu/SplitVerticalButton'
-QtTesting.playCommand(object5, 'activate', '')
-object6 = 'pqClientMainWindow/pipelineBrowserDock/pipelineBrowser'
-QtTesting.playCommand(object6, 'mousePress', '1,1,0,51,13,/0:0/0:0')
-QtTesting.playCommand(object6, 'mouseRelease', '1,0,0,51,13,/0:0/0:0')
-QtTesting.playCommand(object4, 'activate', 'ExtractHistogram')
-QtTesting.playCommand(object2, 'activate', '')
-object7 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/0/MultiViewFrameMenu/SplitVerticalButton'
+QtTesting.playCommand(object1, 'activate', 'menuFilters')
+object4 = 'pqClientMainWindow/menubar/menuFilters/Alphabetical'
+QtTesting.playCommand(object4, 'activate', 'ElevationFilter')
+QtTesting.playCommand(object3, 'activate', '')
+QtTesting.playCommand(object1, 'activate', 'menuFilters')
+object5 = 'pqClientMainWindow/menubar/menuFilters/DataAnalysis'
+QtTesting.playCommand(object5, 'activate', 'PlotAttributes')
+QtTesting.playCommand(object3, 'activate', '')
+object6 = 'pqClientMainWindow/centralwidget/MultiViewWidget/CoreWidget/qt_tabwidget_stackedwidget/MultiViewWidget1/Splitter.0/Frame.2/SplitVertical'
+QtTesting.playCommand(object6, 'activate', '')
+object7 = 'pqClientMainWindow/centralwidget/MultiViewWidget/CoreWidget/qt_tabwidget_stackedwidget/MultiViewWidget1/Splitter.0/Splitter.2/Frame.6/EmptyView/scrollArea/qt_scrollarea_viewport/widgetFoo/ConvertActionsFrame/RenderView'
 QtTesting.playCommand(object7, 'activate', '')
-object8 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/MultiViewSplitter:0/0/MultiViewFrameMenu/WindowCaption'
-QtTesting.playCommand(object8, 'mousePress', '1,1,0,42,4')
-QtTesting.playCommand(object8, 'mouseRelease', '1,0,0,42,4')
+object8 = 'pqClientMainWindow/centralwidget/MultiViewWidget/CoreWidget/qt_tabwidget_stackedwidget/MultiViewWidget1/Splitter.0/Frame.1/SplitVertical'
+QtTesting.playCommand(object8, 'activate', '')
+object9 = 'pqClientMainWindow/centralwidget/MultiViewWidget/CoreWidget/qt_tabwidget_stackedwidget/MultiViewWidget1/Splitter.0/Splitter.2/Frame.5/TitleBar'
+QtTesting.playCommand(object9, 'mousePress', '1,1,0,148,7')
+QtTesting.playCommand(object9, 'mouseRelease', '1,0,0,148,7')
+QtTesting.playCommand(object1, 'activate', 'menu_File')
+object10 = 'pqClientMainWindow/menubar/menu_File'
+QtTesting.playCommand(object10, 'activate', 'actionFileSaveScreenshot')
+object11 = 'pqClientMainWindow/SaveSnapshotDialog/selectedViewOnly'
+QtTesting.playCommand(object11, 'set_boolean', 'false')
+object12 = 'pqClientMainWindow/SaveSnapshotDialog/width'
+QtTesting.playCommand(object12, 'set_string', '900')
+object13 = 'pqClientMainWindow/SaveSnapshotDialog/height'
+QtTesting.playCommand(object13, 'set_string', '900')
+object14 = 'pqClientMainWindow/SaveSnapshotDialog/ok'
+QtTesting.playCommand(object14, 'activate', '')
 
-###
-object15 = 'pqClientMainWindow/pipelineBrowserDock/pipelineBrowser'
-QtTesting.playCommand(object15, 'keyEvent', '7,16777220,0,,0,1')
-object16 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/MultiViewSplitter:1/0/MultiViewFrameMenu/CloseAction'
-QtTesting.playCommand(object16, 'activate', '')
-object17 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/1/MultiViewFrameMenu/SplitVerticalAction'
-QtTesting.playCommand(object17, 'activate', '')
-object18 = 'pqClientMainWindow/centralwidget/MultiViewManager/SplitterFrame/MultiViewSplitter/MultiViewSplitter:1/1/1QWidget0/1QScrollArea0/qt_scrollarea_viewport/EmptyView/ConvertActionsFrame/3D View'
-QtTesting.playCommand(object18, 'activate', '')
-###
-
-object9 = 'pqClientMainWindow/menubar/menu_File'
-QtTesting.playCommand(object9, 'activate', 'actionFileSaveScreenshot')
-object12 = 'pqClientMainWindow/SaveSnapshotDialog/selectedViewOnly'
-QtTesting.playCommand(object12, 'set_boolean', 'false')
-object10 = 'pqClientMainWindow/SaveSnapshotDialog/width'
-QtTesting.playCommand(object10, 'set_string', '1')
-QtTesting.playCommand(object10, 'set_string', '10')
-QtTesting.playCommand(object10, 'key', '16777219')
-QtTesting.playCommand(object10, 'key', '16777219')
-QtTesting.playCommand(object10, 'set_string', '9')
-QtTesting.playCommand(object10, 'set_string', '90')
-QtTesting.playCommand(object10, 'set_string', '900')
-object11 = 'pqClientMainWindow/SaveSnapshotDialog/height'
-QtTesting.playCommand(object11, 'key', '16777217')
-QtTesting.playCommand(object11, 'set_string', '90')
-QtTesting.playCommand(object11, 'set_string', '90')
-QtTesting.playCommand(object11, 'set_string', '900')
-object13 = 'pqClientMainWindow/SaveSnapshotDialog/ok'
-QtTesting.playCommand(object13, 'activate', '')
-object14 = 'pqClientMainWindow/FileSaveScreenshotDialog'
+object15 = 'pqClientMainWindow/FileSaveScreenshotDialog'
 #remove old file, if any
-QtTesting.playCommand(object14, 'remove',
-  '$PARAVIEW_TEST_ROOT/savelargescreenshot.test.png')
-QtTesting.playCommand(object14, 'filesSelected', '$PARAVIEW_TEST_ROOT/savelargescreenshot.test.png')
+QtTesting.playCommand(object15, 'remove', '$PARAVIEW_TEST_ROOT/savelargescreenshot.test.png')
+QtTesting.playCommand(object15, 'filesSelected', '$PARAVIEW_TEST_ROOT/savelargescreenshot.test.png')
 
 import time
 print "Wait for 2 secs"
