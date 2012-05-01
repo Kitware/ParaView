9d052c364d30a7cdd9be9db7b62e332e21302f19
Chris Harris <chris.harris@kitware.com>
2012-05-01 10:28:43 -0400

    Doesn't need to reset CMAKE_LIBRARY_OUTPUT_DIRECTORY
    The client/server wrapping code reset CMAKE_LIBRARY_OUTPUT_DIRECTORY
    this is not necessary.
    
    Change-Id: I68554b250361baa5fcd61fdb4cc89cd3e1940f46

--- Utilities/VTKClientServer/vtkClientServer.cmake
+++ Utilities/VTKClientServer/vtkClientServer.cmake
@@ -4,7 +4,6 @@
 INCLUDE (${VTKCS_CONFIG_DIR}/vtkWrapClientServer.cmake)
 #------------------------------------------------------------------------------
 MACRO(CS_INITIALIZE_WRAP)
-  SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY    ${VTKCS_BINARY_DIR}/bin)
   SET(EXECUTABLE_OUTPUT_PATH ${VTKCS_BINARY_DIR}/bin CACHE PATH "Single output path for executable")
 ENDMACRO(CS_INITIALIZE_WRAP)
 
