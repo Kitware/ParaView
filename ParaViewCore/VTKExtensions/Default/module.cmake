3c0a7dcc87ac759387f135523dd786a9ef1a80d3
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-06-28 17:13:33 -0400

    Restructuring PAraVIewCore
    Change-Id: I722cc07e91582d1e9d9549537a6e4f5d47ecebc6

---
+++ ParaViewCore/VTKExtensions/Default/module.cmake
@@ -0,0 +1,34 @@
+#set (_dependencies)
+#if (PARAVIEW_USE_MPI)
+#  list(APPEND _dependencies
+#    vtkFiltersParallelImaging
+#    vtkFiltersParallelMPI
+##    vtkFiltersParallelTracers
+#    vtkIOMPIImage
+#    vtkIOParallelNetCDF
+#    vtkParallelMPI
+#    )
+#
+#  if (PARAVIEW_USE_ICE_T)
+#    list(APPEND _dependencies
+#      vtkicet)
+#  endif()
+#endif()
+
+vtk_module(vtkPVVTKExtensionsDefault
+  GROUPS
+    ParaView
+  DEPENDS
+    vtkFiltersAMR
+    vtkFiltersParallelStatistics
+    vtkImagingFourier
+    vtkImagingSources
+    vtkInteractionWidgets
+    vtkIOEnSight
+    vtkIOImport
+    vtkIOParallelExodus
+    vtkPVVTKExtensionsCore
+    vtkPVVTKExtensionsRendering
+    vtkIOParallel
+    ${_dependencies}
+)
---
+++ ParaViewCore/VTKExtensions/Rendering/module.cmake
@@ -0,0 +1,29 @@
+set (__dependencies)
+if (PARAVIEW_USE_MPI)
+  set (__dependencies
+    vtkFiltersParallelMPI
+    )
+
+  if (PARAVIEW_USE_ICE_T)
+    list(APPEND __dependencies
+      vtkicet)
+  endif()
+endif()
+
+vtk_module(vtkPVVTKExtensionsRendering
+  GROUPS
+    ParaViewRendering
+  DEPENDS
+    vtkChartsCore
+    vtkFiltersExtraction
+    vtkFiltersGeneric
+    vtkFiltersHyperTree
+    vtkFiltersParallel
+    vtkInteractionStyle
+    vtkPVVTKExtensionsCore
+    vtkRenderingAnnotation
+    vtkRenderingParallel
+    ${__dependencies}
+  COMPILE_DEPENDS
+    vtkUtilitiesEncodeString
+)
