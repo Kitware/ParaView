30bf81306cfc88f3de1ce06e2427f25a07cc1df5
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-07-05 11:22:45 -0400

    One possible strategy for building Xdmf.
    Change-Id: I7845eddc10c83391c61edc8fa8eb1ba3715bd0d9

--- ThirdParty/Xdmf2/module.cmake
+++ ThirdParty/Xdmf2/module.cmake
@@ -1,8 +1,17 @@
-vtk_module(Xdmf2
+set (__dependencies)
+if (PARAVIEW_USE_MPI)
+  list(APPEND __dependencies vtkParallelMPI)
+endif ()
+
+vtk_module(vtkxdmf2
   GROUPS
     ParaView
   DEPENDS
     vtkRenderingOpenGL
     vtkRenderingFreeTypeOpenGL
     vtkIOCore
+    vtkhdf5
+    vtklibxml2
+    ${__dependencies}
+  EXCLUDE_FROM_WRAPPING
  )
