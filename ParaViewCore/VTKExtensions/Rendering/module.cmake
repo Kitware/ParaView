1810f9917786e919245116ec7a8d06c95f06fc69
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-07-26 11:22:52 -0400

    Fixing mpich build issues.
    Change-Id: I3e30ff06678395ee6929fb2486f0aeb69d5cae24

--- ParaViewCore/VTKExtensions/Rendering/module.cmake
+++ ParaViewCore/VTKExtensions/Rendering/module.cmake
@@ -1,13 +1,12 @@
 set (__dependencies)
 if (PARAVIEW_USE_MPI)
-  set (__dependencies
-    vtkFiltersParallelMPI
-    )
-
+  set (__dependencies vtkFiltersParallelMPI)
   if (PARAVIEW_USE_ICE_T)
-    list(APPEND __dependencies
-      vtkicet)
+    list(APPEND __dependencies vtkicet)
   endif()
+
+  # needed for mpich
+  add_definitions("-DMPICH_IGNORE_CXX_SEEK")
 endif()
 
 vtk_module(vtkPVVTKExtensionsRendering
