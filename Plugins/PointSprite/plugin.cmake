f21c3775dacd466e18bed851c282af75cfafc7ed
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-08-09 12:50:52 -0400

    Disable point sprite plugin when visitbridge is enabled.
    Change-Id: I7ba68baad9d36108cecbd08aae596281841cc9d5

--- Plugins/PointSprite/plugin.cmake
+++ Plugins/PointSprite/plugin.cmake
@@ -1,18 +1,13 @@
 # this plugin doesn't work for static builds.
-if (BUILD_SHARED_LIBS)
-  if (TARGET vtkIOVisItBridge)
-    message(STATUS
-      "VisitBridge is enabled. "
-      "Due to current limitations, PointSprite plugin cannot be built with "
-      "VisitBridge is enabled, hence disabling it.")
-  else()
-    pv_plugin(PointSprite
-      DESCRIPTION "Point Sprites"
-      DEFAULT_ENABLED
-      PLUGIN_NAMES "PointSprite_Plugin"
-      )
-  endif()
-else()
+if (TARGET vtkIOVisItBridge)
   message(STATUS
-    "PointSprite plugin is not available in static builds.")
+    "VisitBridge is enabled. "
+    "Due to current limitations, PointSprite plugin cannot be built with "
+    "VisitBridge is enabled, hence disabling it.")
+else()
+  pv_plugin(PointSprite
+    DESCRIPTION "Point Sprites"
+    DEFAULT_ENABLED
+    PLUGIN_NAMES "PointSprite_Plugin"
+    )
 endif()
