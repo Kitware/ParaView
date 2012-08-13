11f0874a4c089231b0763046f139bc6a6b351b11
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-08-12 20:10:15 -0400

    ParaView was failing when building applications. This fixes that issue.
    We were getting the following error when building examples:
    pqparaview_revisedInitializer.cxx.o: undefined reference to symbol
    'vtkIOMPIImage_AutoInit_Construct()'
    
    Change-Id: Ibd22c2c2cb045782e7f0dd54e6375c6ace60e824

--- ParaViewCore/ServerManager/Default/module.cmake
+++ ParaViewCore/ServerManager/Default/module.cmake
@@ -1,9 +1,16 @@
+set (extra_depends)
+if (PARAVIEW_USE_MPI)
+  list(APPEND extra_depends
+    vtkIOMPIImage)
+endif()
+
 vtk_module(vtkPVServerManagerDefault
   DEPENDS
     vtkPVServerImplementationDefault
     vtkPVServerManagerRendering
     vtkTestingRendering
-
+    ${extra_depends}
   TEST_DEPENDS
     vtkPVServerManagerApplication
 )
+unset(extra_depends)
