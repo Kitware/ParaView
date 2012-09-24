90ca38936d8209322b67c83d079ba4894355a690
David Thompson <david.thompson@kitware.com>
2012-09-24 12:56:16 -0400

    Don't require vtkRenderingMatplotlib.
    Change-Id: I7a4e0bd7df2a10463422a99ef3b15aa86979e099

--- ParaViewCore/VTKExtensions/Rendering/module.cmake
+++ ParaViewCore/VTKExtensions/Rendering/module.cmake
@@ -24,7 +24,6 @@ vtk_module(vtkPVVTKExtensionsRendering
     vtkPVVTKExtensionsCore
     vtkRenderingAnnotation
     vtkRenderingMathText
-    vtkRenderingMatplotlib
     vtkRenderingFreeTypeOpenGL
     vtkRenderingOpenGL
     vtkRenderingParallel
