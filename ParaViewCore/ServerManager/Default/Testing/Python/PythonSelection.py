ce7f080e0c0bc799e62ee8e6eea3857e82d795e1
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-10-04 18:19:39 -0400

    Making test more reliable across platforms.
    Change-Id: I6dfc08e0c313f696341c04098f2c8c0a0b46bf22

--- ParaViewCore/ServerManager/Default/Testing/Python/PythonSelection.py
+++ ParaViewCore/ServerManager/Default/Testing/Python/PythonSelection.py
@@ -3,15 +3,14 @@ from paraview.simple import *
 import SMPythonTesting
 SMPythonTesting.ProcessCommandLineArguments()
 
-filename = SMPythonTesting.DataDir + '/Data/can.ex2'
-can_ex2 = OpenDataFile(filename)
+s = Sphere()
+c =Cone(Resolution=10)
+GroupDatasets(Input=[s,c])
 Show()
-SelectCells("id > 1000")
 
-RenderView1 = Render()
-RenderView1.CameraViewUp = [0.0, 0.0, 1.0]
-RenderView1.CameraPosition = [0.21706008911132812, 55.740573746856327, -5.1109471321105957]
-RenderView1.CameraFocalPoint = [0.21706008911132812, 4.0, -5.1109471321105957]
+SelectCells("id > 2")
+
+
 RenderView1 = Render()
 
 if not SMPythonTesting.DoRegressionTesting(RenderView1.SMProxy):
