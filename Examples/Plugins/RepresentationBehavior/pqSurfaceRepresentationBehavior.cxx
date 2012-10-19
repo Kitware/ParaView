022995ad11d32c2532730d4fae626fc7857e10db
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2012-10-19 14:47:18 -0400

    Improve the example code with more complex automatic setup
    Change-Id: Ib8643f81aa5938ab23d7e70c42323da5f5bf0dcf

--- Examples/Plugins/RepresentationBehavior/pqSurfaceRepresentationBehavior.cxx
+++ Examples/Plugins/RepresentationBehavior/pqSurfaceRepresentationBehavior.cxx
@@ -35,15 +35,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "pqObjectBuilder.h"
 #include "pqPipelineRepresentation.h"
 #include "pqRepresentation.h"
+#include "pqSMAdaptor.h"
+#include "pqScalarsToColors.h"
 #include "pqServerManagerModel.h"
 #include "pqView.h"
 #include "vtkPVArrayInformation.h"
 #include "vtkPVDataInformation.h"
 #include "vtkPVDataSetAttributesInformation.h"
+#include "vtkSMProperty.h"
 #include "vtkSMPropertyHelper.h"
 #include "vtkSMProxy.h"
 #include "vtkSMRepresentationProxy.h"
 #include "vtkSMSourceProxy.h"
+
+#include <QVariant>
 
 //-----------------------------------------------------------------------------
 pqSurfaceRepresentationBehavior::pqSurfaceRepresentationBehavior(QObject* parentObject)
@@ -62,10 +67,45 @@ void pqSurfaceRepresentationBehavior::onRepresentationAdded(pqRepresentation* re
       vtkSMRepresentationProxy::SafeDownCast(rep->getProxy());
   if(pipelineRep && smRep)
     {
-    // Change to surface
-    pipelineRep->setRepresentation("Surface");
 
-    // Setup data array
+    // ------------------------------------------------------------------------
+    // Let's pick a reptesentation type that we like instead of the default one
+    // We will use our own set of priority order.
+    // ------------------------------------------------------------------------
+    QList<QVariant> list =
+        pqSMAdaptor::getEnumerationPropertyDomain(
+          pipelineRep->getProxy()->GetProperty("Representation"));
+
+    // Set a representation type based on a priority
+    int priority = 0;
+    std::string finalValue;
+    foreach(QVariant v, list)
+      {
+      if(v.toString() == "Surface" && priority < 1)
+        {
+        finalValue = "Surface";
+        priority = 1;
+        }
+
+      if(v.toString() == "Slices" && priority < 2)
+        {
+        finalValue = "Slices";
+        priority = 2;
+        }
+
+      }
+
+    // Apply the new representation type is any available
+    if(!finalValue.empty())
+      {
+      pipelineRep->setRepresentation(finalValue.c_str());
+      }
+    // ------------------------------------------------------------------------
+
+    // ------------------------------------------------------------------------
+    // Let's select some data array by default
+    // ------------------------------------------------------------------------
+
     vtkSMProxy* input = vtkSMPropertyHelper(smRep, "Input").GetAsProxy();
     vtkSMSourceProxy *sourceProxy = vtkSMSourceProxy::SafeDownCast(input);
     if(sourceProxy)
@@ -77,16 +117,39 @@ void pqSurfaceRepresentationBehavior::onRepresentationAdded(pqRepresentation* re
         const char* scalarName =
             dataInfo->GetPointDataInformation()->GetArrayInformation(0)->GetName();
         pipelineRep->colorByArray(scalarName, 0); // 0: POINT_DATA / 1:CELL_DATA
+
+        // --------------------------------------------------------------------
+        // Let's change the data range for the lookup table to remove 1/8 of
+        // the data range from the min and the max of the scalar range
+        // --------------------------------------------------------------------
+        QPair<double, double> range = pipelineRep->getLookupTable()->getScalarRange();
+        double min = range.first + (range.second - range.first)/8;
+        double max = range.second - (range.second - range.first)/8;
+
+        // Apply changes
+        pipelineRep->getLookupTable()->setScalarRangeLock(true);
+        pipelineRep->getLookupTable()->setScalarRange(min, max);
         }
       else if (dataInfo->GetCellDataInformation()->GetNumberOfArrays() > 0)
         {
         const char* scalarName =
             dataInfo->GetCellDataInformation()->GetArrayInformation(0)->GetName();
         pipelineRep->colorByArray(scalarName, 1); // 0: POINT_DATA / 1:CELL_DATA
+
+        // --------------------------------------------------------------------
+        // Let's change the data range for the lookup table to remove 1/8 of
+        // the data range from the min and the max of the scalar range
+        // --------------------------------------------------------------------
+        QPair<double, double> range = pipelineRep->getLookupTable()->getScalarRange();
+        double min = range.first + (range.second - range.first)/8;
+        double max = range.second - (range.second - range.first)/8;
+
+        // Apply changes
+        pipelineRep->getLookupTable()->setScalarRangeLock(true);
+        pipelineRep->getLookupTable()->setScalarRange(min, max);
         }
       }
     }
-
 }
 
 //-----------------------------------------------------------------------------
