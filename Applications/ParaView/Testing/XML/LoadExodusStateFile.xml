Kitware/ParaView

commit 74ec34b1ffce6f917de5544c57496d67b761d193

Add a flag to vtkPVView to mark if the view time has been set.
Change-Id: I3a63503dfdfea24dbaf2017168cfb4b1fd520fe4

ParaViewCore/ClientServerCore/vtkPVView.cxx
@@ -51,6 +51,7 @@ vtkPVView::vtkPVView()
   this->RequestInformation = vtkInformation::New();
   this->ReplyInformationVector = vtkInformationVector::New();
 
+  this->ViewTimeValid = false;
   this->LastRenderOneViewAtATime = false;
 
   this->Size[1] = this->Size[0] = 300;
@@ -107,6 +108,7 @@ void vtkPVView::SetViewTime(double time)
   if (this->ViewTime != time)
     {
     this->ViewTime = time;
+    this->ViewTimeValid = true;
     this->InvokeEvent(ViewTimeChangedEvent);
     this->Modified();
     }
@@ -206,7 +208,11 @@ void vtkPVView::CallProcessViewRequest(
       if (pvrepr)
         {
         // Pass the view time information to the representation
-        pvrepr->SetUpdateTime(this->GetViewTime());
+        if(this->ViewTimeValid)
+          {
+          pvrepr->SetUpdateTime(this->GetViewTime());
+          }
+
         pvrepr->SetUseCache(this->GetUseCache());
         pvrepr->SetCacheKey(this->GetCacheKey());
         }

ParaViewCore/ClientServerCore/vtkPVView.h
@@ -194,6 +194,7 @@ private:
   static vtkWeakPointer<vtkPVSynchronizedRenderWindows>
     SingletonSynchronizedWindows;
 
+  bool ViewTimeValid;
   bool LastRenderOneViewAtATime;
 //ETX
 };

commit a9fe43219e3710b54a310dd24ddffafbe35ce846

BUG #13533. Fixes state issue with exodus reader.
Fixes bug introduced by 74ec34b1. When state files were loaded with timestep 0,
the time value wasn't being set on vtkPVView. vtkPVView::SetViewTime() compared
the old time value to the new one without regard for whether VimeTimeValid was
set. This fixes that issue.

Added a test that loads Exodus state file (for ParaView 3.14.1 to test backwards
compatibility as well). The state file is loaded using the command line
argument for loading state file.

Change-Id: Ib5c1e237617cfb47a0fa3e948e4be554db89d9ed

ParaViewCore/ClientServerCore/Rendering/vtkPVView.cxx
@@ -177,7 +177,7 @@ void vtkPVView::SetSize(int x, int y)
 //----------------------------------------------------------------------------
 void vtkPVView::SetViewTime(double time)
 {
-  if (this->ViewTime != time)
+  if (!this->ViewTimeValid || this->ViewTime != time)
     {
     this->ViewTime = time;
     this->ViewTimeValid = true;

Applications/ParaView/Testing/XML/CMakeLists.txt
@@ -663,4 +663,18 @@ IF (PARAVIEW_DATA_ROOT)
      BASELINE_DIR ${PARAVIEW_DATA_ROOT}/Baseline
      TEST_SCRIPTS ${CMAKE_CURRENT_SOURCE_DIR}/TileDisplay3DTesting-2x2.xml)
 
+
+  #------------------------------------------------------------------
+  # Add tests that test command line arguments (among other things).
+  #------------------------------------------------------------------
+  if (PARAVIEW_DATA_ROOT)
+    add_pv_test("pv" "_DISABLE_C"
+      COMMAND --client $<TARGET_FILE:paraview>
+              -dr
+              --test-directory=${PARAVIEW_TEST_DIR}
+              --state=${PARAVIEW_DATA_ROOT}/Data/exodusStateFile.3.14.1.pvsm
+      BASELINE_DIR ${PARAVIEW_DATA_ROOT}/Baseline
+      TEST_SCRIPTS ${CMAKE_CURRENT_SOURCE_DIR}/LoadExodusStateFile.xml)
+  endif()
+
 ENDIF (PARAVIEW_DATA_ROOT)

Applications/ParaView/Testing/XML/LoadExodusStateFile.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0" ?>
+<pqevents>
+  <pqevent object="pqClientMainWindow/cameraToolbar/actionPositiveY" command="activate" arguments="" />
+  <pqevent object="pqClientMainWindow/pipelineBrowserDock/pipelineBrowser" command="mousePress" arguments="1,1,0,52,12,/0:0/0:0" />
+  <pqevent object="pqClientMainWindow/pipelineBrowserDock/pipelineBrowser" command="mouseRelease" arguments="1,0,0,52,12,/0:0/0:0" />
+  <pqevent object="pqClientMainWindow/variableToolbar/displayColor/Variables" command="set_string" arguments="EQPS (partial)" />
+</pqevents>