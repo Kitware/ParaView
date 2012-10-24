3a7fa312268014c28b7481b3e46bdb7e035eac6e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2012-10-24 16:04:20 -0400

    Remove debug output
    Change-Id: I15cf523e00848f53695ea68702e74debe561ca2d

--- ParaViewCore/ClientServerCore/Core/vtkExtractsDeliveryHelper.cxx
+++ ParaViewCore/ClientServerCore/Core/vtkExtractsDeliveryHelper.cxx
@@ -156,12 +156,12 @@ void vtkExtractsDeliveryHelper::Update()
 {
   if (this->ProcessIsProducer)
     {
-    cout << "Push extracts for: " << endl;
-    for (ExtractProducersType::iterator iter = this->ExtractProducers.begin();
-      iter != this->ExtractProducers.end(); ++iter)
-      {
-      cout << "  " << iter->first.c_str() << endl;
-      }
+//    cout << "Push extracts for: " << endl;
+//    for (ExtractProducersType::iterator iter = this->ExtractProducers.begin();
+//      iter != this->ExtractProducers.end(); ++iter)
+//      {
+//      cout << "  " << iter->first.c_str() << endl;
+//      }
 
     // update all inputs. We shouldn't call Update() here since that messes up
     // the time/piece requests that'd be set by paraview. The co-processing code
@@ -234,7 +234,7 @@ void vtkExtractsDeliveryHelper::Update()
           {
           break;
           }
-        cout << "Received extract for: " << key.c_str() << endl;
+//        cout << "Received extract for: " << key.c_str() << endl;
         vtkDataObject* extract = comm->ReceiveDataObject(1, 12001);
         ExtractConsumersType::iterator iter;
         iter = this->ExtractConsumers.find(key);
