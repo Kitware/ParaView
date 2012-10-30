50380145de8aeccf9cb9e10217920874a5591c31
Andrew Bauer <andy.bauer@kitware.com>
2012-10-30 18:04:24 -0400

    Fixing test ParallelImageWriter-Batch.
    The previous fix only worked in symmetric parallel mode
    and it broke the way it worked in non-symmetric mode. This
    fix checks which mode we're running pvbatch in and makes
    sure that the quantity used to test whether the tests
    pass or fail works for both cases.

--- ParaViewCore/ServerManager/Default/Testing/Python/ParallelImageWriter.py
+++ ParaViewCore/ServerManager/Default/Testing/Python/ParallelImageWriter.py
@@ -6,6 +6,8 @@ import os.path
 import sys
 from paraview.simple import *
 
+print 'starting'
+
 paraview.simple._DisableFirstRenderCameraReset()
 
 SMPythonTesting.ProcessCommandLineArguments()
@@ -30,21 +32,26 @@ r = XMLPartitionedImageDataReader()
 r.FileName = fname
 r.UpdatePipeline()
 
-# numcells is the local value, we need to get the global value
+# if we're running in symmetric mode, numcells is the local process's
+# number of cells. if not, it is the global number of cells and
+# only the process that reads in this script gets that information
 numcells =  r.GetDataInformation().DataInformation.GetNumberOfCells()
 
-# sum up the cell count on process 0 and process 0 checks it
-import paraview.vtk as vtk
-da = vtk.vtkIntArray()
-da.SetNumberOfTuples(1)
-da.SetValue(0, numcells)
-da2 = vtk.vtkIntArray()
-da2.SetNumberOfTuples(1)
+# check which mode we're in
+if pm.GetSymmetricMPIMode() == True:
+    # we're in symmetric mode so we have to do a global reduce for
+    # process 0 to get the global amount of cells to check
+    import paraview.vtk as vtk
+    da = vtk.vtkIntArray()
+    da.SetNumberOfTuples(1)
+    da.SetValue(0, numcells)
+    da2 = vtk.vtkIntArray()
+    da2.SetNumberOfTuples(1)
+    pm.GetGlobalController().Reduce(da, da2, 2, 0)
+    numcells = da2.GetValue(0)
 
-pm.GetGlobalController().Reduce(da, da2, 2, 0)
-
-if processId == 0 and da2.GetValue(0) != 8000:
-    print "ERROR: ", fname, " has ", da2.GetValue(0), " but should have 8000."
+if processId == 0 and numcells != 8000:
+    print "ERROR: ", fname, " has ", numcells, " but should have 8000."
     sys.exit(1)
 
 print "Test passed."
