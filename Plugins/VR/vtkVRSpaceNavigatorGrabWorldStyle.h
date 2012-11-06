46475d9717ea2a14b9118ff0c6df75e7ac101574
Cory Quammen <cquammen@cs.unc.edu>
2012-11-06 16:01:54 -0500

    Added interactor style for the 3DConnexion SpaceNavigator
    This is a grab-world interactor style for use with a
    SpaceNavigator from 3DConnexion.
    
    Change-Id: Ia194899984224cd757355681eb75efc2c43abe73

---
+++ Plugins/VR/vtkVRSpaceNavigatorGrabWorldStyle.h
@@ -0,0 +1,63 @@
+/*=========================================================================
+
+   Program: ParaView
+   Module:    vtkVRSpaceNavigatorGrabWorldStyle.h
+
+   Copyright (c) 2005,2006 Sandia Corporation, Kitware Inc.
+   All rights reserved.
+
+   ParaView is a free software; you can redistribute it and/or modify it
+   under the terms of the ParaView license version 1.2.
+
+   See License_v1.2.txt for the full ParaView license.
+   A copy of this license can be obtained by contacting
+   Kitware Inc.
+   28 Corporate Drive
+   Clifton Park, NY 12065
+   USA
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+========================================================================*/
+#ifndef __vtkVRSpaceNavigatorGrabWorldStyle_h_
+#define __vtkVRSpaceNavigatorGrabWorldStyle_h_
+
+#include "vtkVRInteractorStyle.h"
+
+class vtkSMDoubleVectorProperty;
+class vtkSMIntVectorProperty;
+class vtkSMProxy;
+class vtkSMRenderViewProxy;
+class vtkTransform;
+
+struct vtkVREventData;
+
+class vtkVRSpaceNavigatorGrabWorldStyle : public vtkVRInteractorStyle
+{
+public:
+  static vtkVRSpaceNavigatorGrabWorldStyle *New();
+  vtkTypeMacro(vtkVRSpaceNavigatorGrabWorldStyle, vtkVRInteractorStyle)
+  void PrintSelf(ostream &os, vtkIndent indent);
+
+protected:
+  vtkVRSpaceNavigatorGrabWorldStyle();
+  ~vtkVRSpaceNavigatorGrabWorldStyle();
+
+  virtual void HandleAnalog ( const vtkVREventData& data );
+
+private:
+  vtkVRSpaceNavigatorGrabWorldStyle(const vtkVRSpaceNavigatorGrabWorldStyle&); // Not implemented.
+  void operator=(const vtkVRSpaceNavigatorGrabWorldStyle&); // Not implemented.
+};
+
+#endif //__vtkVRSpaceNavigatorGrabWorldStyle.h_
