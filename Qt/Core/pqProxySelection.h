09c09eda3c8bedd4ba795682068cc5bda5dc3ca6
David Thompson <david.thompson@kitware.com>
2012-12-19 15:46:10 -0500

    Whitespace and documentation fixes.
    Change-Id: I9ab77b92c8a8db87b07a796bdd7c236e1480056d

--- Qt/Core/pqProxySelection.h
+++ Qt/Core/pqProxySelection.h
@@ -7,8 +7,8 @@
    All rights reserved.
 
    ParaView is a free software; you can redistribute it and/or modify it
-   under the terms of the ParaView license version 1.2. 
-   
+   under the terms of the ParaView license version 1.2.
+
    See License_v1.2.txt for the full ParaView license.
    A copy of this license can be obtained by contacting
    Kitware Inc.
@@ -29,7 +29,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ========================================================================*/
-#ifndef __pqProxySelection_h 
+#ifndef __pqProxySelection_h
 #define __pqProxySelection_h
 
 #include <QSet>
@@ -38,7 +38,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 class pqServerManagerModelItem;
 class vtkSMProxySelectionModel;
 
-/// pqProxySelection is used to specify a selection comprising of proxies.
+/// pqProxySelection is used to specify a selection comprising proxies.
 /// pqProxySelection provides methods to convert to and from
 /// vtkSMProxySelectionModel.
 class PQCORE_EXPORT pqProxySelection : public QSet<pqServerManagerModelItem*>
