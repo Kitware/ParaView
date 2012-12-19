Kitware/ParaView

commit d8446c652c82c4eb88122c4f77a254afbcd4b10a

BUG #13726. Fix variable selection performance.
Addresses extreme slowness when dealing with lists containing large number of
variables on panels such as exodus. Using a timer to collapse all updates into a
single update thus avoiding the O(N^2) updates.

Change-Id: Icd1ca94eb7919e22a938f857e6dc09421dc9d259

Qt/Components/pqExodusIIVariableSelectionWidget.cxx
@@ -37,8 +37,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QPointer>
 #include <QtDebug>
 
-#include "pqTreeWidgetItemObject.h"
 #include "pqSMAdaptor.h"
+#include "pqTimer.h"
+#include "pqTreeWidgetItemObject.h"
 
 namespace
 {
@@ -113,6 +114,7 @@ class pqExodusIIVariableSelectionWidget::pqInternals
 {
 public:
   pqPixmapMap Pixmaps;
+  pqTimer Timer;
 
   QMap<QString, QList<QPointer<pqTreeWidgetItemObject> > > Items;
   pqTreeWidgetItemObject* findItem(
@@ -198,6 +200,8 @@ pqExodusIIVariableSelectionWidget::pqExodusIIVariableSelectionWidget(QWidget* pa
   Internals(new pqInternals())
 {
   this->installEventFilter(this);
+  QObject::connect(&this->Internals->Timer, SIGNAL(timeout()),
+    this, SLOT(updateProperty()));
 }
 
 //-----------------------------------------------------------------------------
@@ -265,8 +269,17 @@ void pqExodusIIVariableSelectionWidget::setStatus(
 {
   pqTreeWidgetItemObject* item = this->Internals->findItem(this, key, text);
   item->setChecked(value);
+
+  // BUG #13726. To avoid dramatic performance degradation when dealing with
+  // large list of variables, we use a timer to collapse all update requests.
   QObject::connect(item, SIGNAL(checkedStateChanged(bool)),
-    this, SLOT(updateProperty()), Qt::UniqueConnection);
+    this, SLOT(updatePropertyOnce()), Qt::UniqueConnection);
+}
+
+//-----------------------------------------------------------------------------
+void pqExodusIIVariableSelectionWidget::updatePropertyOnce()
+{
+  this->Internals->Timer.start(0);
 }
 
 //-----------------------------------------------------------------------------

Qt/Components/pqExodusIIVariableSelectionWidget.h
@@ -61,6 +61,7 @@ signals:
 
 protected slots:
   void updateProperty();
+  void updatePropertyOnce();
 
 protected:
   /// overridden to handle QDynamicPropertyChangeEvent when properties are

commit dc331ccc5f1bede7a84af0ef988a8bd1a864be67

Fix BUG #13726.
Commit d8446c652c82c introduced a bug where in the widget could no longer
determine which item was changed by the user (since it was relying on
this->sender(), which was no longer valid after a timer was added). This fixes
that issue.

Change-Id: I655e0af8cc8ba1e36e5ec0d01ac9d7d8c5c91375

Qt/Components/pqExodusIIVariableSelectionWidget.cxx
@@ -32,6 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "pqExodusIIVariableSelectionWidget.h"
 
 #include <QDynamicPropertyChangeEvent>
+#include <QList>
 #include <QMap>
 #include <QPixmap>
 #include <QPointer>
@@ -116,6 +117,7 @@ class pqExodusIIVariableSelectionWidget::pqInternals
 public:
   pqPixmapMap Pixmaps;
   pqTimer Timer;
+  QList<QPointer<QObject> > DeferredObjects;
 
   QMap<QString, QList<QPointer<pqTreeWidgetItemObject> > > Items;
   pqTreeWidgetItemObject* findItem(
@@ -200,6 +202,8 @@ pqExodusIIVariableSelectionWidget::pqExodusIIVariableSelectionWidget(QWidget* pa
   : Superclass(parentObject),
   Internals(new pqInternals())
 {
+  this->Internals->Timer.setSingleShot(true);
+
   this->installEventFilter(this);
   QObject::connect(&this->Internals->Timer, SIGNAL(timeout()),
     this, SLOT(updateProperty()));
@@ -277,28 +281,35 @@ void pqExodusIIVariableSelectionWidget::setStatus(
   // BUG #13726. To avoid dramatic performance degradation when dealing with
   // large list of variables, we use a timer to collapse all update requests.
   QObject::connect(item, SIGNAL(checkedStateChanged(bool)),
-    this, SLOT(updatePropertyOnce()), Qt::UniqueConnection);
+    this, SLOT(delayedUpdateProperty()), Qt::UniqueConnection);
 }
 
 //-----------------------------------------------------------------------------
-void pqExodusIIVariableSelectionWidget::updatePropertyOnce()
+void pqExodusIIVariableSelectionWidget::delayedUpdateProperty()
 {
   this->Internals->Timer.start(0);
+  // save the pqTreeWidgetItemObject that fired this signal so we can update
+  // property using its state later.
+  this->Internals->DeferredObjects.push_back(this->sender());
 }
 
 //-----------------------------------------------------------------------------
 void pqExodusIIVariableSelectionWidget::updateProperty()
 {
-  pqTreeWidgetItemObject* item = qobject_cast<pqTreeWidgetItemObject*>(
-    this->sender());
-  if (item)
+  foreach (QObject* qobject, this->Internals->DeferredObjects)
     {
-    QString key = item->data(0, Qt::UserRole).toString();
-    QVariant newValue = this->Internals->value(key);
-    if (this->property(key.toAscii().data()) != newValue)
+    pqTreeWidgetItemObject* item =
+      qobject_cast<pqTreeWidgetItemObject*>(qobject);
+    if (item)
       {
-      this->setProperty(key.toAscii().data(), newValue);
+      QString key = item->data(0, Qt::UserRole).toString();
+      QVariant newValue = this->Internals->value(key);
+      if (this->property(key.toAscii().data()) != newValue)
+        {
+        this->setProperty(key.toAscii().data(), newValue);
+        }
       }
     }
+  this->Internals->DeferredObjects.clear();
   emit this->widgetModified();
 }

Qt/Components/pqExodusIIVariableSelectionWidget.h
@@ -61,7 +61,7 @@ signals:
 
 protected slots:
   void updateProperty();
-  void updatePropertyOnce();
+  void delayedUpdateProperty();
 
 protected:
   /// overridden to handle QDynamicPropertyChangeEvent when properties are