Kitware/ParaView

commit 38c98eb5f8a0e517193a19cb0d6f3e34f6508b09

Fixed segfaults during quitting.
As objects were being deleted, we were attempting to update their visibility and
failing. Fixed by delaying refresh as well as checking for null pointers.

Change-Id: I487b56f364cd0483d95a12dda94c543bb047dccc

Qt/Components/pqPipelineModel.cxx
@@ -291,7 +291,8 @@ private:
     // I'm not a huge fan of this hacky way we are dealing with catalyst. We'll
     // have to come back and cleanly address how the pqPipelineModel deals with
     // "visibility" in a more generic, session-centric way.
-    if (port && port->getServer()->getResource().scheme() == "catalyst")
+    if (port && port->getServer() &&
+        port->getServer()->getResource().scheme() == "catalyst")
       {
       pqLiveInsituVisualizationManager* mgr=
         qobject_cast<pqLiveInsituVisualizationManager*>(
@@ -1008,7 +1009,8 @@ void pqPipelineModel::addSource(pqPipelineSource* source)
 
   QObject::connect(source,
     SIGNAL(visibilityChanged(pqPipelineSource*, pqDataRepresentation*)),
-    this, SLOT(updateVisibility(pqPipelineSource*)));
+    this, SLOT(updateVisibility(pqPipelineSource*)),
+    Qt::QueuedConnection);
 
   QObject::connect(source,
     SIGNAL(nameChanged(pqServerManagerModelItem*)),

commit 0eca8ac665dadd9ccc5dd91e29c79f4649bf9eaf

BUG #13746. Fix segfaults after change-input.
The bug was introduced by 38c98eb. Due to the QueuedConnection added, slots were
being called with dangling pointer which resulted in the segfaults. Fixed by
keeping track of the pointer of interest in a QList with QPointers and then
on-idle handling non-null items in the QList.

Change-Id: I149e86389c5abec8c05050f85dd982c89b1dfc8a

Qt/Components/pqPipelineModel.cxx
@@ -363,18 +363,23 @@ public:
     Root(parent, NULL, pqPipelineModel::Invalid, parent)
   {
   this->ModifiedFont.setBold(true);
+  this->DelayedUpdateVisibilityTimer.setSingleShot(true);
   }
 
   QFont ModifiedFont;
   pqPipelineModelDataItem Root;
+  pqTimer DelayedUpdateVisibilityTimer;
+  QList<QPointer<pqPipelineSource> > DelayedUpdateVisibilityItems;
 };
-
 
 //-----------------------------------------------------------------------------
 void pqPipelineModel::constructor()
 {
   this->FilterRoleSession = NULL;
   this->Internal = new pqPipelineModelInternal(this);
+  QObject::connect(&this->Internal->DelayedUpdateVisibilityTimer,
+    SIGNAL(timeout()), this, SLOT(delayedUpdateVisibilityTimeout()));
+
   this->Editable = true;
   this->View = NULL;
 
@@ -1009,8 +1014,7 @@ void pqPipelineModel::addSource(pqPipelineSource* source)
 
   QObject::connect(source,
     SIGNAL(visibilityChanged(pqPipelineSource*, pqDataRepresentation*)),
-    this, SLOT(updateVisibility(pqPipelineSource*)),
-    Qt::QueuedConnection);
+    this, SLOT(delayedUpdateVisibility(pqPipelineSource*)));
 
   QObject::connect(source,
     SIGNAL(nameChanged(pqServerManagerModelItem*)),
@@ -1241,6 +1245,28 @@ void pqPipelineModel::setView(pqView *newview)
   this->View = newview;
   // update all VisibilityIcons.
   this->Internal->Root.updateVisibilityIcon(newview, true);
+}
+
+//-----------------------------------------------------------------------------
+void pqPipelineModel::delayedUpdateVisibility(pqPipelineSource* source)
+{
+  this->Internal->DelayedUpdateVisibilityItems.removeAll(source);
+  this->Internal->DelayedUpdateVisibilityItems.push_front(source);
+  this->Internal->DelayedUpdateVisibilityTimer.start(0);
+}
+
+//-----------------------------------------------------------------------------
+void pqPipelineModel::delayedUpdateVisibilityTimeout()
+{
+  foreach (pqPipelineSource* source,
+    this->Internal->DelayedUpdateVisibilityItems)
+    {
+    if (source)
+      {
+      this->updateVisibility(source);
+      }
+    }
+  this->Internal->DelayedUpdateVisibilityItems.clear();
 }
 
 //-----------------------------------------------------------------------------

Qt/Components/pqPipelineModel.h
@@ -321,6 +321,11 @@ private slots:
   /// called when visibility of the source may have changed.
   void updateVisibility(pqPipelineSource*);
 
+  /// provides a mechanism to delay updating of visibility while safely handling
+  /// the case where the pqPipelineSource itself gets deleted in the mean time.
+  void delayedUpdateVisibility(pqPipelineSource*);
+  void delayedUpdateVisibilityTimeout();
+
   /// called when the item's name changes.
   void updateData(pqServerManagerModelItem*);
 