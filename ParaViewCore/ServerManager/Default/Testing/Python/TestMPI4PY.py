ea95b4be74fe8119960be561c4ddecf4d3c58574
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-03-03 17:06:25 -0500

    Adding a test to test mpi4py.
    Change-Id: Iae6b31310a266901f15ea58a03673edcdfa1fd98

---
+++ ParaViewCore/ServerManager/Default/Testing/Python/TestMPI4PY.py
@@ -0,0 +1,20 @@
+from paraview.servermanager import vtkProcessModule
+pvoptions = vtkProcessModule.GetProcessModule().GetOptions()
+if pvoptions.GetSymmetricMPIMode() == False:
+    print "ERROR: Please run ParaView in SymmetricMPI mode."
+    import sys
+    sys.exit(1)
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = { 'pi': 3.142 }
+else:
+    data = None
+
+data = comm.bcast(data, root=0)
+assert data["pi"] == 3.142
+print "All's well that ends well!"
