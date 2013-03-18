f6be2d6015e4533d09997819d3489fb1fc825ce3
Andrew Bauer <andy.bauer@kitware.com>
2013-03-18 17:51:36 -0400

    Adding in a vtkMultiPieceDataSet example.
    Also fixed a bug for CxxFullExample.

--- CxxMultiPieceExample/FEAdaptor.cxx
+++ CxxMultiPieceExample/FEAdaptor.cxx
@@ -4,62 +4,74 @@
 
 #include <vtkCellData.h>
 #include <vtkCellType.h>
+#include <vtkCommunicator.h>
 #include <vtkCPDataDescription.h>
 #include <vtkCPInputDataDescription.h>
 #include <vtkCPPythonProcessor.h>
 #include <vtkCPPythonScriptPipeline.h>
 #include <vtkDoubleArray.h>
 #include <vtkFloatArray.h>
+#include <vtkImageData.h>
+#include <vtkMultiBlockDataSet.h>
+#include <vtkMultiPieceDataSet.h>
+#include <vtkMultiProcessController.h>
 #include <vtkNew.h>
 #include <vtkPoints.h>
 #include <vtkPointData.h>
-#include <vtkUnstructuredGrid.h>
+
+#include <mpi.h>
 
 namespace
 {
   vtkCPPythonProcessor* Processor = NULL;
-  vtkUnstructuredGrid* VTKGrid;
+  vtkMultiBlockDataSet* VTKGrid;
 
   void BuildVTKGrid(Grid& grid)
   {
-    // create the points information
-    vtkNew<vtkDoubleArray> pointArray;
-    pointArray->SetNumberOfComponents(3);
-    pointArray->SetArray(grid.GetPointsArray(), static_cast<vtkIdType>(grid.GetNumberOfPoints()*3), 1);
-    vtkNew<vtkPoints> points;
-    points->SetData(pointArray.GetPointer());
-    VTKGrid->SetPoints(points.GetPointer());
-
-    // create the cells
-    size_t numCells = grid.GetNumberOfCells();
-    VTKGrid->Allocate(static_cast<vtkIdType>(numCells*9));
-    for(size_t cell=0;cell<numCells;cell++)
+    vtkNew<vtkImageData> imageData;
+    imageData->SetSpacing(grid.GetSpacing());
+    imageData->SetOrigin(0, 0, 0); // Not necessary for (0,0,0)
+    unsigned int* extents = grid.GetExtents();
+    int extents2[6];
+    for(int i=0;i<6;i++)
       {
-      unsigned int* cellPoints = grid.GetCellPoints(cell);
-      vtkIdType tmp[8] = {cellPoints[0], cellPoints[1], cellPoints[2], cellPoints[3],
-                          cellPoints[4], cellPoints[5], cellPoints[6], cellPoints[7]};
-      VTKGrid->InsertNextCell(VTK_HEXAHEDRON, 8, tmp);
+      extents2[i] = static_cast<int>(extents[i]);
       }
+    imageData->SetExtent(extents2);
+    int mpiSize = 1;
+    int mpiRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+    vtkNew<vtkMultiPieceDataSet> multiPiece;
+    multiPiece->SetNumberOfPieces(mpiSize);
+    multiPiece->SetPiece(mpiRank, imageData.GetPointer());
+    VTKGrid->SetNumberOfBlocks(1);
+    VTKGrid->SetBlock(0, multiPiece.GetPointer());
   }
 
   void UpdateVTKAttributes(Grid& grid, Attributes& attributes)
   {
-    if(VTKGrid->GetPointData()->GetNumberOfArrays() == 0)
+    int mpiRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+    vtkMultiPieceDataSet* multiPiece = vtkMultiPieceDataSet::SafeDownCast(
+      VTKGrid->GetBlock(0));
+    vtkDataSet* dataSet = vtkDataSet::SafeDownCast(multiPiece->GetPiece(mpiRank));
+    if(dataSet->GetPointData()->GetNumberOfArrays() == 0)
       {
       // velocity array
       vtkNew<vtkDoubleArray> velocity;
       velocity->SetName("velocity");
       velocity->SetNumberOfComponents(3);
-      velocity->SetNumberOfTuples(static_cast<vtkIdType>(grid.GetNumberOfPoints()));
-      VTKGrid->GetPointData()->AddArray(velocity.GetPointer());
+      velocity->SetNumberOfTuples(static_cast<vtkIdType>(grid.GetNumberOfLocalPoints()));
+      dataSet->GetPointData()->AddArray(velocity.GetPointer());
       // pressure array
       vtkNew<vtkFloatArray> pressure;
       pressure->SetName("pressure");
       pressure->SetNumberOfComponents(1);
-      VTKGrid->GetPointData()->AddArray(pressure.GetPointer());
+      dataSet->GetPointData()->AddArray(pressure.GetPointer());
       }
     vtkDoubleArray* velocity = vtkDoubleArray::SafeDownCast(
-      VTKGrid->GetPointData()->GetArray("velocity"));
+      dataSet->GetPointData()->GetArray("velocity"));
     // The velocity array is ordered as vx0,vx1,vx2,..,vy0,vy1,vy2,..,vz0,vz1,vz2,..
     // so we need to create a full copy of it with VTK's ordering of
     // vx0,vy0,vz0,vx1,vy1,vz1,..
@@ -73,11 +85,11 @@ namespace
       }
 
     vtkFloatArray* pressure = vtkFloatArray::SafeDownCast(
-      VTKGrid->GetPointData()->GetArray("pressure"));
+      dataSet->GetPointData()->GetArray("pressure"));
     // The pressure array is a scalar array so we can reuse
     // memory as long as we ordered the points properly.
     float* pressureData = attributes.GetPressureArray();
-    pressure->SetArray(pressureData, static_cast<vtkIdType>(grid.GetNumberOfPoints()), 1);
+    pressure->SetArray(pressureData, static_cast<vtkIdType>(grid.GetNumberOfLocalPoints()), 1);
   }
 
   void BuildVTKDataStructures(Grid& grid, Attributes& attributes)
@@ -87,7 +99,7 @@ namespace
       // The grid structure isn't changing so we only build it
       // the first time it's needed. If we needed the memory
       // we could delete it and rebuild as necessary.
-      VTKGrid = vtkUnstructuredGrid::New();
+      VTKGrid = vtkMultiBlockDataSet::New();
       BuildVTKGrid(grid);
       }
     UpdateVTKAttributes(grid, attributes);
@@ -146,6 +158,21 @@ namespace FEAdaptor
       {
       BuildVTKDataStructures(grid, attributes);
       dataDescription->GetInputDescriptionByName("input")->SetGrid(VTKGrid);
+
+      // figure out the whole extent of the grid
+      unsigned int* extent = grid.GetExtents();
+      int wholeExtent[6], tmp[6];
+      for(int i=0;i<3;i++)
+        {
+        wholeExtent[i*2] = - static_cast<int>(extent[i*2]); // negate for parallel communication
+        wholeExtent[i*2+1] = static_cast<int>(extent[i*2+1]);
+        }
+      vtkMultiProcessController::GetGlobalController()->AllReduce(wholeExtent, tmp, 6, vtkCommunicator::MAX_OP);
+      for(int i=0;i<3;i++)
+        {
+        tmp[i*2] = -tmp[i*2];
+        }
+      dataDescription->GetInputDescriptionByName("input")->SetWholeExtent(tmp);
       Processor->CoProcess(dataDescription.GetPointer());
       }
   }
---
+++ CxxMultiPieceExample/FEAdaptor.h
@@ -0,0 +1,17 @@
+#ifndef FEADAPTOR_HEADER
+#define FEADAPTOR_HEADER
+
+class Attributes;
+class Grid;
+
+namespace FEAdaptor
+{
+  void Initialize(int numScripts, char* scripts[]);
+
+  void Finalize();
+
+  void CoProcess(Grid& grid, Attributes& attributes, double time,
+                 unsigned int timeStep, bool lastTimeStep);
+}
+
+#endif
