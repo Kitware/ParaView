2f894d7a93f8e9c5b5eadd402db057e4fdf058c1
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-03-23 10:17:17 -0400

    Fix warnings
    Change-Id: I2f6a86dce7134215492492112fc34a863cd76bfd

--- ParaViewCore/ClientServerCore/Core/vtkPVServerOptions.h
+++ ParaViewCore/ClientServerCore/Core/vtkPVServerOptions.h
@@ -51,11 +51,11 @@ public:
   double* GetLowerLeft(unsigned int idx);
   double* GetLowerRight(unsigned int idx);
   double* GetUpperRight(unsigned int idx);
-  
+
   // Returns -1 to indicate not stereo type was specified. 0 indicate no stereo
   // is to be used.
   int GetStereoType(unsigned int idx);
-
+  virtual const char* GetStereoType() { return this->Superclass::GetStereoType(); }
 protected:
   // Description:
   // Add machine information from the xml tag <Machine ....>
