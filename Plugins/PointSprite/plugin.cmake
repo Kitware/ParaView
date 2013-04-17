e82bdfd440bd7e2dfb28481e1b3bbcd18b925ba5
Robert Maynard <robert.maynard@kitware.com>
2013-04-17 17:34:12 -0400

    PointSprite and VisItBridge can exist together in static builds
    Change-Id: I3c350e644e73541e97f67647c66a0e87cb522a45

--- Plugins/PointSprite/plugin.cmake
+++ Plugins/PointSprite/plugin.cmake
@@ -1,13 +1,5 @@
-if (TARGET vtkIOVisItBridge AND NOT BUILD_SHARED_LIBS)
-  # this plugin doesn't work for static builds.
-  message(STATUS
-    "VisitBridge is enabled. "
-    "Due to current limitations, PointSprite plugin cannot be built with "
-    "VisitBridge is enabled, hence disabling it.")
-else()
-  pv_plugin(PointSprite
-    DESCRIPTION "Point Sprites"
-    DEFAULT_ENABLED
-    PLUGIN_NAMES "PointSprite_Plugin"
-    )
-endif()
+pv_plugin(PointSprite
+  DESCRIPTION "Point Sprites"
+  DEFAULT_ENABLED
+  PLUGIN_NAMES "PointSprite_Plugin"
+  )
