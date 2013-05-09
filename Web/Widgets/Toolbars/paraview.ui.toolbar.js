8429459c4d063cccac0da314b99b207fbe7c0082
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2013-05-09 17:24:27 -0400

    Add exit button on sample applications
    Change-Id: I033cc1b5f8e25910fc84e3a66eb925ae4bbc425e

---
+++ Web/Widgets/Toolbars/paraview.ui.toolbar.js
@@ -0,0 +1,40 @@
+/**
+ * ParaViewWeb JavaScript Library.
+ *
+ * This module extend jQuery object to add control button to ParaViewWeb usage.
+ *
+ * @class jQuery.paraview.ui.toolbar
+ */
+(function (GLOBAL, $) {
+
+    // =======================================================================
+    // ==== jQuery based methods =============================================
+    // =======================================================================
+
+    /**
+     * Graphical component use to show and interact with the ParaViewWeb
+     * pipeline.
+     *
+     * @member jQuery.paraview.ui.toolbar
+     * @method disconnectButton
+     * @param {pv.Session} session
+     * ParaViewWeb session object.
+     *
+     * Usage:
+     *      $('.disconnect').disconnectButton(session);
+     */
+    $.fn.disconnectButton = function(session) {
+        // Handle data with default values
+        return this.each(function() {
+            var me = $(this);
+
+            function disconnect() {
+                session.call("pv:exit");
+                setTimeout("window.close()", 100);
+            }
+
+            me.click(disconnect);
+        });
+    };
+
+}(window, jQuery));
