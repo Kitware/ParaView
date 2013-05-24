d19f1fd3aa00252163edaecd413c90ff8700af22
Pat Marion <pat.marion@kitware.com>
2013-05-24 15:37:46 +1000

    Fix the SetParameter API overloads of the vtkPythonProgrammableFilter
    Commit fc8cabcd (April 2007) introduced a SetParameter function on the
    programmable filter.  It added a string vector property to filter proxy
    that could be used to set arbitrary parameter name/value pairs that would
    be defined in the filter python script.
    
    Commit 8311d712 (July 2011) introduced several SetParameter overloads so
    that custom string, int, and double vector properties were supported.
    This allowed proxy xml to declare custom properties on the python
    programmable filter that would be presented in the GUI like regular
    properties.  But, this commit also changed the behavior of the original
    Parameter string vector property.
    
    Commit 9176ba5c82 (March 2012) accidentally broke the SetParameter
    overloads.
    
    This commit (May 2013) fixes the SetParameter overloads and original
    Parameter string vector property (but both cannot be used together).
    This commit adds a test for the SetParameter API and adds an example
    proxy xml file that demonstrates how to define a python programmable
    filter with custom parameters.
    
    Change-Id: I3583fbfdd7d9d442799c714bb2c34693d34ed2ea

---
+++ ParaViewCore/ServerManager/Default/Testing/Python/ProgrammableFilterProperties.py
@@ -0,0 +1,56 @@
+import os
+import random
+import textwrap
+
+import SMPythonTesting
+import paraview.simple as smp
+
+
+def testScript(programmableFilter, script):
+
+    arrayName = str(random.random())
+    programmableFilter.Script = script + textwrap.dedent('''
+        passedArray = vtk.vtkIntArray()
+        passedArray.SetName('%s')
+        self.GetOutput().GetFieldData().AddArray(passedArray)''' % arrayName)
+
+    programmableFilter.UpdatePipeline()
+    return programmableFilter.GetClientSideObject().GetOutput().GetFieldData().GetArray(arrayName) is not None
+
+
+SMPythonTesting.ProcessCommandLineArguments()
+tempDir = SMPythonTesting.TempDir
+stateDir = SMPythonTesting.SMStatesDir
+
+
+sphere = smp.Sphere()
+
+f = smp.ProgrammableFilter(sphere)
+
+
+script = '''
+assert 1+1 == 2
+'''
+
+assert testScript(f, script)
+
+
+script = '''
+assert foo == 'bar'
+'''
+
+f.SetPropertyWithName('Parameters', ['foo', '"bar"'])
+
+assert testScript(f, script)
+
+
+smp.LoadPlugin(os.path.join(stateDir, 'ProgrammableFilterPropertiesTest.xml'))
+
+f = smp.ProgrammableFilterPropertiesTest(sphere)
+
+
+assert f.DoubleTest == 1.23
+assert f.IntTest == 123
+assert f.StringTest == 'string value'
+
+assert testScript(f, f.Script)
