199bca531492aba5bfdb98a74d95ab141ff04923
Casey Goodlett <casey.goodlett@kitware.com>
2013-06-06 14:59:37 -0400

    Basic test for CTK range sliders
    This test does not verification of the code as that is assumed to be
    tested in CTK.  Instead this provides a simple example that can be run
    to illustrate the use of the two widgets.
    
    Change-Id: I96d934429331e3328d728cdb793ccc48441d601e

---
+++ Qt/Widgets/Testing/Cxx/CTKSliders.cxx
@@ -0,0 +1,44 @@
+#include "ctkRangeSlider.h"
+#include "ctkDoubleRangeSlider.h"
+
+#include <QTimer>
+#include <QStandardItemModel>
+#include <QItemSelectionModel>
+#include <QVBoxLayout>
+#include <QObject>
+
+#include "QTestApp.h"
+#include "SignalCatcher.h"
+
+int CTKSliders(int argc, char* argv[])
+{
+  QTestApp app(argc, argv);
+
+  QWidget* root = new QWidget;
+  QVBoxLayout* layout = new QVBoxLayout;
+
+  ctkRangeSlider* rangeSlider = new ctkRangeSlider(Qt::Horizontal, 0);
+  ctkDoubleRangeSlider* doubleSlider = new ctkDoubleRangeSlider(Qt::Horizontal);
+  
+  layout->addWidget(rangeSlider);
+  layout->addWidget(doubleSlider);
+
+  root->setLayout(layout);
+
+  SignalCatcher* catcher = new SignalCatcher();
+
+  rangeSlider->connect(rangeSlider, SIGNAL(valuesChanged(int, int)),
+                       catcher, SLOT(onValuesChanged(int,int)));
+
+  doubleSlider->connect(doubleSlider, SIGNAL(valuesChanged(double, double)),
+                        catcher, SLOT(onValuesChanged(double,double)));
+
+  root->show();
+
+  int status = QTestApp::exec();
+
+  delete root;
+  delete catcher;
+
+  return status;
+}
