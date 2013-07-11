574c6ae7a943bea0c93183c8a0eb04832d5ee7ab
Andrew Bauer <andy.bauer@kitware.com>
2013-07-11 10:12:59 -0400

    Turning off auto-enabling of the TemporalParallelism plugin.
    This was turned on just to check if it built and ran properly
    everywhere. Now that it has I'm turning it off by default.
    
    Change-Id: I94ce7b0bd615de73ed476fad4e1bf72837efc4ab

--- Plugins/TemporalParallelismScriptGenerator/plugin.cmake
+++ Plugins/TemporalParallelismScriptGenerator/plugin.cmake
@@ -1,5 +1,4 @@
 if (PARAVIEW_BUILD_QT_GUI AND PARAVIEW_ENABLE_PYTHON)
   pv_plugin(TemporalParallelismScriptGenerator
-    DEFAULT_ENABLED
     DESCRIPTION "Plugin for creating Python spatio-temporal processing scripts")
 endif()
