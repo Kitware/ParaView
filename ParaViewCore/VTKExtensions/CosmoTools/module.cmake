ba8152b52772c93c1e5b1086fa6f13310e27f25f
Jingyuan Wang <jingyuan@utk.edu>
2013-09-12 11:13:59 -0400

    Import Voronoi reader and associated plugins
    This commit imports the voronoi reader which provides functionality
    for reading voronoi tesselations generated by tess. Further, it
    adds two plugins/filters to post-process the tesselation: (1) a
    filter to merger connected voronoi cells based on the region ID
    and (2) a filter to compute minkowski functionals.
    
    Change-Id: I2997a798e15fd07d0c46150270bf6b5f1b4e9091

--- ParaViewCore/VTKExtensions/CosmoTools/module.cmake
+++ ParaViewCore/VTKExtensions/CosmoTools/module.cmake
@@ -13,10 +13,13 @@ set_property(GLOBAL PROPERTY
     vtkPVVTKExtensionsCosmoTools_SERVERMANAGER_XMLS
 
     ## CosmoTools Readers
-    ${CMAKE_CURRENT_LIST_DIR}/resources/CosmoReader.xml
     ${CMAKE_CURRENT_LIST_DIR}/resources/AdaptiveCosmoReader.xml
+    ${CMAKE_CURRENT_LIST_DIR}/resources/CosmoReader.xml
     ${CMAKE_CURRENT_LIST_DIR}/resources/GenericIOReader.xml
+    ${CMAKE_CURRENT_LIST_DIR}/resources/VoronoiReader.xml
 
     ## CosmoTools Filters
     ${CMAKE_CURRENT_LIST_DIR}/resources/LANLHaloFinder.xml
+    ${CMAKE_CURRENT_LIST_DIR}/resources/MergeConnected.xml
+    ${CMAKE_CURRENT_LIST_DIR}/resources/MinkowskiFilter.xml
     )
---
+++ ParaViewCore/VTKExtensions/CosmoTools/vtkMinkowskiFilter.h
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+ Program:   Visualization Toolkit
+ Module:    vtkMinkowskiFilter.h
+
+ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ All rights reserved.
+ See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+ =========================================================================*/
+// .NAME vtkMinkowskiFilter.h -- Computes Minkowski functionals
+//
+// .SECTION Description
+// Given as input a voronoi tesselation, stored in a vtkUnstructuredGrid, this
+// filter computes the Minkowski functionals on each cell.
+
+#ifndef __vtkMinkowskiFilter_h
+#define __vtkMinkowskiFilter_h
+
+#include "vtkUnstructuredGridAlgorithm.h"
+#include "vtkPVVTKExtensionsCosmoToolsModule.h" // For export macro
+
+class vtkUnstructuredGrid;
+class vtkCell;
+class vtkDoubleArray;
+class vtkPolyhedron;
+
+class VTKPVVTKEXTENSIONSCOSMOTOOLS_EXPORT vtkMinkowskiFilter :
+  public vtkUnstructuredGridAlgorithm
+{
+ public:
+  static vtkMinkowskiFilter *New();
+  vtkTypeMacro(vtkMinkowskiFilter, vtkUnstructuredGridAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+ protected:
+  vtkMinkowskiFilter();
+  ~vtkMinkowskiFilter();
+
+  int RequestData(vtkInformation*,
+                  vtkInformationVector**,
+                  vtkInformationVector*);
+  int FillOutputPortInformation(int, vtkInformation*);
+
+ private:
+  vtkMinkowskiFilter(const vtkMinkowskiFilter&);  // Not implemented.
+  void operator=(const vtkMinkowskiFilter&);  // Not implemented.
+
+  void compute_mf(vtkUnstructuredGrid *ugrid, vtkDoubleArray *S, vtkDoubleArray *V, vtkDoubleArray *C, vtkDoubleArray *X, vtkDoubleArray *G, vtkDoubleArray *T, vtkDoubleArray *B, vtkDoubleArray *L, vtkDoubleArray *P, vtkDoubleArray *F);
+  double compute_S(vtkPolyhedron *cell); // surface area
+  double compute_V(vtkPolyhedron *cell); // volume1
+  double compute_V(vtkUnstructuredGrid *ugrid, int cid); // volume2
+  double compute_C(vtkPolyhedron *cell); // integrated mean curvature
+  double compute_X(vtkPolyhedron *cell); // euler characteristic
+  double compute_G(double X);            // genus
+  double compute_T(double V, double S);  // thickness
+  double compute_B(double S, double C);  // breadth
+  double compute_L(double C, double G);  // length
+  double compute_P(double B, double L);  // planarity
+  double compute_F(double B, double T);  // filamenarity
+
+  void compute_normal(vtkCell *face, double normal[3]);
+  int compute_epsilon(vtkCell *f1, vtkCell *f2, vtkCell *e);
+  double compute_face_area(vtkCell *face);
+  double compute_edge_length(vtkCell *edge);
+  double compute_face_angle(vtkCell *f1, vtkCell *f2);
+};
+
+#endif //  __vtkMinkowskiFilter_h
