46b9b0287fe592ae4b44b2097ed596029d41bf2b
Scott Wittenburg <scott.wittenburg@kitware.com>
2013-10-24 09:09:38 -0600

    Added a new ParaViewWeb application to be used only for testing.
    Added a new python server and a very basic index.html for a new
    ParaViewWeb application which will be used only to test some new
    module registration functions and to test the various paraview
    web protocols.  Also added some bits to the web applications
    CMakeLists.txt which will copy the new app to the build tree only
    when testing is enabled.  Modified this same file to recursively
    copy the new test folders inside any web application to the build
    tree, but again, only when testing is enabled.
    
    Change-Id: I65dbcd2be7bd6a0ffbc5c496069ebacb26046db4

---
+++ Web/Applications/TestApp/server/pv_web_test_app.py
@@ -0,0 +1,107 @@
+r"""
+    This module is a ParaViewWeb server application, designed simply to have an
+    application to be used in web-based testing.
+
+    The following command line illustrate how to use it::
+
+        $ pvpython .../pv_web_test_app.py --data-dir /.../path-to-your-data-directory
+
+    --data-dir is used to list that directory on the server and let the client
+               choose a file to load.
+
+    Any ParaViewWeb executable script come with a set of standard arguments that
+    can be overriden if need be::
+
+        --port 8080
+             Port number on which the HTTP server will listen to.
+
+        --content /path-to-web-content/
+             Directory that you want to server as static web content.
+             By default, this variable is empty which mean that we rely on another server
+             to deliver the static content and the current process only focus on the
+             WebSocket connectivity of clients.
+
+        --authKey vtkweb-secret
+             Secret key that should be provided by the client to allow it to make any
+             WebSocket communication. The client will assume if none is given that the
+             server expect "vtkweb-secret" as secret key.
+"""
+
+# import to process args
+import sys
+import os
+
+# import paraview modules.
+from paraview import simple
+from paraview.web import wamp      as pv_wamp
+from paraview.web import protocols as pv_protocols
+
+from vtk.web import server
+from vtkWebCorePython import *
+
+try:
+    import argparse
+except ImportError:
+    # since  Python 2.6 and earlier don't have argparse, we simply provide
+    # the source for the same as _argparse and we use it instead.
+    import _argparse as argparse
+
+# =============================================================================
+# Create custom class to handle clients requests
+# =============================================================================
+
+class _TestApp(pv_wamp.PVServerProtocol):
+
+    # Application configuration
+    dataDir = None
+    authKey = "vtkweb-secret"
+    echoStr = ""
+
+    def initialize(self):
+        # Bring used components
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebMouseHandler())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebViewPort())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebViewPortImageDelivery())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebViewPortGeometryDelivery())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebTimeHandler())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebPipelineManager())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebFileManager(_TestApp.dataDir))
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebRemoteConnection())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebStartupRemoteConnection())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebStateLoader())
+
+        # Update authentication key to use
+        self.updateSecret(_TestApp.authKey)
+
+
+# =============================================================================
+# Main: Parse args and start server
+# =============================================================================
+
+if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="ParaView/Web Testing-application")
+
+    # Add default arguments
+    server.add_arguments(parser)
+
+    # Add local arguments
+    parser.add_argument("--data-dir",
+                        default=os.getcwd(),
+                        help="path to ParaView data directory",
+                        dest="path")
+    parser.add_argument("--echo",
+                        default="",
+                        help="A string that will be available through echo protocol",
+                        dest="echoString")
+
+    # Exctract arguments
+    args = parser.parse_args()
+
+    # Configure our current application
+    _TestApp.dataDir    = args.path
+    _TestApp.authKey    = args.authKey
+    _TestApp.echoStr    = args.echoString
+
+    # Start server
+    server.start_webserver(options=args, protocol=_TestApp)
