58328f8815221d885ce4bd6c4973669f9203f392
Scott Wittenburg <scott.wittenburg@kitware.com>
2013-11-04 11:40:54 -0700

    Fix urls in web test scripts, rename tests uniformly, move test dirs.
    Due to change in web application names, test scripts had to be updated
    to reflect new names in their urls.  Also changed some test names so
    that all have names which fit into the common naming scheme.  Also
    moved test directories containing python web test scripts into the
    new folders.
    
    Change-Id: I9e4b652077b681ad89d22a375d88fc2e4fee02ad

--- Web/Applications/Visualizer/test/test_pv_web_visualizer_can_test.py
+++ Web/Applications/Visualizer/test/test_pv_web_visualizer_can_test.py
@@ -7,7 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 
 from vtk.web import testing
 
@@ -23,17 +22,16 @@ def runTest(args) :
     # print 'We were passed the following args: ' + str(args)
 
     # This name is used in error reporting
-    testName = 'pv_web_file_loader_open_browser_and_click_renderer.py'
+    testName = 'pv_web_visualizer_open_browser_and_click_renderer.py'
 
     # Request the WebVisualizer index.html
-    urlToRetrieve = 'http://localhost:' + str(args.port) + '/apps/WebFileViewer'
+    urlToRetrieve = 'http://localhost:' + str(args.port) + '/apps/Visualizer'
 
     # The author of pv_web_visualizer.py grabbed the --data-dir argument
     # from the command line and put it in a variable called "path" in the
     # arguments object, so that's where we look for the ParaViewData dir
     # inside this test script.
     baselineImgDir = args.baselineImgDir
-    print 'The baseline images are located here: ' + baselineImgDir
 
     # Create a Chrome window driver.
     browser = webdriver.Chrome()
@@ -41,16 +39,36 @@ def runTest(args) :
     browser.get(urlToRetrieve)
 
     sleepSeconds = 8
-    print "Going to sleep for " + str(sleepSeconds) + " seconds to let browser load page"
     time.sleep(sleepSeconds)
-    print "Ok, page should be loaded by now...continuing."
 
-    # First we need to hit the enter key to make the modal pop-up go away
-    browser.switch_to_alert().accept()
+    # Click on the "Open file" icon to start the process of loading a file
+    filesDiv = browser.find_element_by_css_selector(".action.files")
+    filesDiv.click()
+    time.sleep(1)
+
+    # Click on the "can" link to load some paraview data.  We have the
+    # expectation here that the paraview data dir with which we started the
+    # server points to the "Data" folder in the standard ParaViewData git
+    # repo.
+    canLi = browser.execute_script("return $('.open-file:contains(can.ex2)')[0]")
+    canLi.click()
     time.sleep(3)
 
-    sphereDataLi = browser.execute_script("return $('.jstree-leaf:contains(dualSphereAnimation_P00T0003.vtp)')[0]")
-    sphereDataLi.click()
+    # Now choose how to color the object
+    colorByLink = browser.find_element_by_css_selector(".colorBy.color")
+    colorByLink.click()
+    time.sleep(1)
+
+    colorByDispLi = browser.find_element_by_css_selector(".points[name=DISPL]")
+    colorByDispLi.click()
+    time.sleep(1)
+
+    endTimeLi = browser.find_element_by_css_selector(".action[action=last]")
+    endTimeLi.click()
+    time.sleep(1)
+
+    rescaleIcon = browser.find_element_by_css_selector(".rescale-data")
+    rescaleIcon.click()
     time.sleep(1)
 
     # Now click the resetCamera icon so that we change the center of
@@ -59,15 +77,25 @@ def runTest(args) :
     resetCameraIcon.click()
     time.sleep(1)
 
+    '''
+    # Click-and-drag on the cone to change its orientation a bit.
+    listenerDiv = browser.find_element_by_css_selector(".mouse-listener")
+    drag = ActionChains(browser)
+    drag.move_to_element(listenerDiv)
+    drag.click_and_hold()
+    drag.move_by_offset(100, 0)
+    drag.release()
+    drag.perform()
+    time.sleep(1)
+    '''
+
     # Now grab the renderer image and write it to disk
     imgdata = testing.get_image_data(browser, ".image.active>img")
-    filename = 'image_sphere_part.jpg'
+    filename = 'test_pv_web_visualizer_can_test.jpg'
     testing.write_image_to_disk(imgdata, filename)
 
-    print 'About to compare images...'
-
     knownGoodFileName = testing.concat_paths(baselineImgDir,
-                                             'image_sphere_part_known_good.jpg')
+                                             'test_pv_web_visualizer_can_test_known_good.jpg')
 
     compareResult = -1
 
