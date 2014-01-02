Kitware/ParaView

commit b8f9e7675b8cf75fbe3ee4cdf3db3c0daec13806

BUG #14452: Fixed animation in pvbatch.
Animation scene didn't have the time-track setup by default in pvbatch/pvpython.
This was causing GoToLast/GoToFirst not work correctly in pvbatch/pvpython.
Fixed that.

Change-Id: Ibdb7b69421a4c540d64c688fa9e6fe016c627218

Wrapping/Python/paraview/simple.py
@@ -1370,6 +1370,10 @@ def _CreateEssentialProxies():
        except NameError:
            paraview.print_error("Error: Could not create AnimationScene")
 
+    # Ensure that the time-track is setup so animations on readers work as
+    # expected (BUG #14452).
+    GetTimeTrack()
+
     servermanager.ProxyManager().EnableStateUpdateNotification()
     servermanager.ProxyManager().TriggerStateUpdate()
 

ParaViewCore/ServerManager/Default/Testing/Python/PythonPVSimpleExII.py
@@ -47,3 +47,25 @@ if 'ACCL' in fields:
 
 reader.UpdatePipeline()
 
+# Now test that the default animation is setup correctly for the data.
+Show()
+
+RenderView1 = Render()
+RenderView1.CameraPosition = [0.21706008911132812, 55.74057374685633, -5.110947132110596]
+RenderView1.CameraViewUp = [0.0, 0.0, 1.0]
+RenderView1.CameraFocalPoint = [0.21706008911132812, 4.0, -5.110947132110596]
+ResetCamera()
+
+GetAnimationScene().GoToLast()
+GetAnimationScene().Play()
+GetAnimationScene().GoToFirst()
+GetAnimationScene().GoToLast()
+GetAnimationScene().GoToPrevious()
+GetAnimationScene().GoToPrevious()
+GetAnimationScene().GoToPrevious()
+GetAnimationScene().GoToPrevious()
+GetAnimationScene().GoToNext()
+
+if not smtesting.DoRegressionTesting(GetActiveView().SMProxy):
+  # This will lead to VTK object leaks.
+  sys.exit(1)

Wrapping/Python/paraview/smtrace.py
@@ -2,9 +2,6 @@ r"""
 smtrace module is used for tracing.
 """
 # BUG LIST:
-# - GetTimeTrack() needs to be called for time-track to be setup for readers to
-#   work correctly. Needs to be fixed. Fix for this maybe to fix
-#   simple.GetAnimationScene() to set up the time-track too.
 # - stepping through animation doesn't get traced properly.
 
 import simple

commit d0b05d1c132752bffba2cbb2e10434feebdbc520

Fix issue with Catalyst (Python) editions.
This issue was introduced by commit b8f9e7675. We were now getting an exception
when the animation-scene proxy couldn't be created, which happens in Catalyst.
Fixed that.

Change-Id: Ia32b84b8dfafeaa3931edac57ab5f183092350e5

Wrapping/Python/paraview/simple.py
@@ -1366,13 +1366,16 @@ def _CreateEssentialProxies():
     if not scene:
        try:
            scene = AnimationScene()
+           # no need to register, since we use the function to create the
+           # AnimationScene object, that automatically registers it.
            scene.TimeKeeper = tk
+
+           # Ensure that the time-track is setup so animations on readers work as
+           # expected (BUG #14452).
+           GetTimeTrack()
        except NameError:
            paraview.print_error("Error: Could not create AnimationScene")
 
-    # Ensure that the time-track is setup so animations on readers work as
-    # expected (BUG #14452).
-    GetTimeTrack()
 
     servermanager.ProxyManager().EnableStateUpdateNotification()
     servermanager.ProxyManager().TriggerStateUpdate()