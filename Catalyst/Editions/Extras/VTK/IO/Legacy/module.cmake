fb8c76501959b632d70eb96f79843192d01e4422
Ben Boeckel <ben.boeckel@kitware.com>
2014-01-24 15:29:23 -0500

    Add data writers to Extras edition
    Change-Id: I9bd49a55f6b1ee45f8b758df670dc712c4f8a199

---
+++ Catalyst/Editions/Extras/VTK/IO/Legacy/module.cmake
@@ -0,0 +1,16 @@
+vtk_module(vtkIOLegacy
+  GROUPS
+    StandAlone
+  DEPENDS
+    vtkCommonDataModel
+    vtkCommonSystem
+    vtkCommonMisc
+    vtkIOCore
+  PRIVATE_DEPENDS
+    vtksys
+  TEST_DEPENDS
+    vtkFiltersAMR
+    vtkInteractionStyle
+    vtkRenderingOpenGL
+    vtkTestingRendering
+  )
