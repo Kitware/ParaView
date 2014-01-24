f3e98bd61dcb15404bcc7e5b8c5698029808db18
Ben Boeckel <ben.boeckel@kitware.com>
2014-01-24 15:30:23 -0500

    Add files missing from Rendering-Base
    Change-Id: I4cc90b4cb9962634f7da6dc5c9875aea1e1021c4

---
+++ Catalyst/Editions/Rendering-Base/ParaViewCore/ServerManager/Default/module.cmake
@@ -0,0 +1,23 @@
+# If FFMPEG support is enabled, we need to depend on FFMPEG.
+set (__extra_dependencies)
+if (PARAVIEW_ENABLE_FFMPEG)
+  list(APPEND __extra_dependencies vtkIOFFMPEG)
+endif()
+
+vtk_module(vtkPVServerManagerDefault
+  DEPENDS
+    #vtkIOMovie
+    #vtkIOParallelExodus
+    #vtkPVServerImplementationDefault
+    vtkPVServerManagerRendering
+    #vtkRenderingVolumeOpenGL
+    #vtkTestingRendering
+    ${__extra_dependencies}
+  PRIVATE_DEPENDS
+    vtksys
+  TEST_DEPENDS
+    vtkPVServerManagerApplication
+  TEST_LABELS
+    PARAVIEW
+)
+unset(__extra_dependencies)
