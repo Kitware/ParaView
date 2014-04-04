6e01341e86e87f6fb04e4e48e0df7ff29281cc1d
Ben Boeckel <ben.boeckel@kitware.com>
2014-04-04 17:36:34 -0400

    Add support for Gerrit with ExternalData
    Change-Id: I6a7a9b5a147c992d5a61e163331342892ea73e7b

--- Utilities/Scripts/SetupGitAliases.sh
+++ Utilities/Scripts/SetupGitAliases.sh
@@ -21,4 +21,4 @@ ${GITCONFIG} alias.stage-merge-next "!sh -c \"${stage_cmd} merge -b next ${git_b
 ${GITCONFIG} alias.stage-merge-master "!sh -c \"${stage_cmd} merge -b master ${git_branch}\""
 ${GITCONFIG} alias.stage-merge "!sh -c \"${stage_cmd} merge ${git_branch}\""
 # Alias to push the current topic branch to Gerrit
-${GITCONFIG} alias.gerrit-push "!sh -c \"git fetch gerrit && git push gerrit HEAD:refs/for/master/${git_branch}\""
+${GITCONFIG} alias.gerrit-push "!bash Utilities/Scripts/git-gerrit-push"
