9ccbad3d0622a693e3de3c0e0033844551361554
Ben Boeckel <ben.boeckel@kitware.com>
2014-04-22 15:51:25 -0400

    qt: Disable Qt modules when support is unavailable
    Change-Id: Icef6c460a46daeaef3c1d2d551c9c4bb1fe514ab

--- Qt/Core/module.cmake
+++ Qt/Core/module.cmake
@@ -1,14 +1,16 @@
-vtk_module(pqCore
-  GROUPS
-    ParaViewQt
-  DEPENDS
-    pqWidgets
-    vtkGUISupportQt
-    vtkPVServerManagerApplication
-    vtkPVServerManagerDefault
-  PRIVATE_DEPENDS
-    vtksys
-  EXCLUDE_FROM_WRAPPING
-  TEST_LABELS
-    PARAVIEW
-)
+if (PARAVIEW_ENABLE_QT_GUI OR PARAVIEW_ENABLE_QT_SUPPORT)
+  vtk_module(pqCore
+    GROUPS
+      ParaViewQt
+    DEPENDS
+      pqWidgets
+      vtkGUISupportQt
+      vtkPVServerManagerApplication
+      vtkPVServerManagerDefault
+    PRIVATE_DEPENDS
+      vtksys
+    EXCLUDE_FROM_WRAPPING
+    TEST_LABELS
+      PARAVIEW
+  )
+endif ()
--- Qt/Widgets/module.cmake
+++ Qt/Widgets/module.cmake
@@ -1,12 +1,14 @@
-vtk_module(pqWidgets
-  GROUPS
-    Qt
-    ParaViewQt
-  DEPENDS
-    vtkqttesting
-    vtkPVServerManagerCore
-    vtkGUISupportQt
-  EXCLUDE_FROM_WRAPPING
-  TEST_LABELS
-    PARAVIEW
-)
+if (PARAVIEW_ENABLE_QT_GUI OR PARAVIEW_ENABLE_QT_SUPPORT)
+  vtk_module(pqWidgets
+    GROUPS
+      Qt
+      ParaViewQt
+    DEPENDS
+      vtkqttesting
+      vtkPVServerManagerCore
+      vtkGUISupportQt
+    EXCLUDE_FROM_WRAPPING
+    TEST_LABELS
+      PARAVIEW
+  )
+endif ()
