57ca92ba7a7121f72fb96bb3c46aa0509d7dcd58
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-06-02 12:48:06 -0400

    Support adding title texts to pqViewFrame.
    We always had the API to set the title, but it never did anything. This
    commit fixes it to add a label if a non-empty title is specified.
    
    Change-Id: I4bb93a7fe0169946dbc4cac07311e41c28cc99e0

--- Qt/Components/pqViewFrame.h
+++ Qt/Components/pqViewFrame.h
@@ -41,6 +41,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 class QDragEnterEvent;
 class QDragMoveEvent;
 class QDropEvent;
+class QLabel;
 class QMenu;
 class QToolBar;
 class QToolButton;
@@ -59,8 +60,8 @@ public:
 
   /// Get/Set the window title. If not empty, the title is shown in the
   /// title-bar.
-  void setTitle(const QString& text) { this->Title = text; }
-  const QString& title() const { return this->Title; }
+  void setTitle(const QString& text);
+  QString title() const;
 
   /// Get/Set the central widget shown in this frame.
   void setCentralWidget(QWidget* widget);
@@ -165,11 +166,11 @@ protected:
   bool TitleBarVisible;
   bool BorderVisible;
   QColor BorderColor;
-  QString Title;
   StandardButtons Buttons;
   QPointer<QWidget> CentralWidget;
   QPointer<QWidget> TitleBar;
   QPointer<QToolBar> ToolBar;
+  QPointer<QLabel> TitleLabel;
   QMenu* ContextMenu;
   QUuid UniqueID;
   QPoint DragStartPosition;
