962dc33f6463ae92d6a4f47d4fe3080d12fabd7a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-06-22 18:24:46 -0400

    Update dropsite based on changes to dashboard.
    Change-Id: I86b0d6bbf9ffbc5e630be52ca108a520a70a24f5

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,13 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
-##   INCLUDE(Dart)
+##   INCLUDE(CTest)
+
 set(CTEST_PROJECT_NAME "ParaView")
 set(CTEST_NIGHTLY_START_TIME "19:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ParaView")
+set(CTEST_DROP_SITE "open.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=ParaView")
 set(CTEST_DROP_SITE_CDASH TRUE)
