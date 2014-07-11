64d650981f39932a98e7f9afb43a6d6e438a1c93
Cory Quammen <cory.quammen@kitware.com>
2014-07-11 09:50:41 -0400

    Added two Python View tests
    One tests creation of a view image from a numpy array and the other
    tests creation of an image using only VTK. Added baselines as well.
    
    Change-Id: I03ce0abaa029ebbc8284712e3dde11f13a51865f

---
+++ Applications/ParaView/Testing/Python/TestPythonViewNumpyScript.py
@@ -0,0 +1,42 @@
+# Set up a basic scene for rendering.
+from paraview.simple import *
+import sys
+
+script = """
+import numpy
+import paraview.numpy_support
+
+def setup_data(view):
+  # Don't actually need any data
+  pass
+
+def render(view, width, height):
+  cb = numpy.zeros((height, width, 4), dtype=numpy.uint8)
+  for i in xrange(width):
+    cb[:,i,0] = i%255
+
+  for i in xrange(height):
+    cb[i,:,1] = i%255
+
+  from paraview.python_view import numpy_to_image
+
+  return numpy_to_image(cb)
+"""
+
+view = CreateView("PythonView")
+view.Script = script
+
+Render()
+
+try:
+  baselineIndex = sys.argv.index('-B')+1
+  baselinePath = sys.argv[baselineIndex]
+except:
+  print "Could not get baseline directory. Test failed."
+
+import os
+baseline_file = os.path.join(baselinePath, "TestPythonViewNumpyScript.png")
+import vtk.test.Testing
+vtk.test.Testing.VTK_TEMP_DIR = vtk.util.misc.vtkGetTempDir()
+vtk.test.Testing.compareImage(view.GetRenderWindow(), baseline_file, threshold=25)
+vtk.test.Testing.interact()
