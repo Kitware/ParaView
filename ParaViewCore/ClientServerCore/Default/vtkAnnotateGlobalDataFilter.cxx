e60817acfc7814cb4dc25cd694b82a32256b7bf2
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-07-22 22:20:44 -0400

    BUG #14771: Fix vtkAnnotateGlobalDataFilter.
    Fix (and upgrade) vtkAnnotateGlobalDataFilter after numpy support
    changes in VTK/ParaView. Also fixes the bug reported in BUG #14771.
    Instead of putting the logic to come up with the expression in C++, we
    add a new method in annotation.py. It's easier to do that determination
    in Python. Restructured code from vtkPythonAnnotationFilter to make that
    possible.
    
    Change-Id: I027ee8321897b93a8e24401e7e5b190a6b55981d

--- ParaViewCore/ClientServerCore/Default/vtkAnnotateGlobalDataFilter.cxx
+++ ParaViewCore/ClientServerCore/Default/vtkAnnotateGlobalDataFilter.cxx
@@ -14,20 +14,27 @@
 =========================================================================*/
 #include "vtkAnnotateGlobalDataFilter.h"
 
-#include "vtkAbstractArray.h"
-#include "vtkCompositeDataIterator.h"
-#include "vtkCompositeDataSet.h"
-#include "vtkDataArray.h"
 #include "vtkDataObject.h"
-#include "vtkFieldData.h"
-#include "vtkInformation.h"
-#include "vtkMultiBlockDataSet.h"
 #include "vtkObjectFactory.h"
-#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkPythonInterpreter.h"
 
-#include <vector>
-#include <vtkSmartPointer.h>
+#include <string>
 #include <vtksys/ios/sstream>
+#include <vtksys/SystemTools.hxx>
+
+//----------------------------------------------------------------------------
+static std::string vtkGetReferenceAsString(void* ref)
+{
+  // Set self to point to this
+  char addrofthis[1024];
+  sprintf(addrofthis, "%p", ref);
+  char *aplus = addrofthis;
+  if ((addrofthis[0] == '0') && ((addrofthis[1] == 'x') || addrofthis[1] == 'X'))
+    {
+    aplus += 2; //skip over "0x"
+    }
+  return std::string(aplus);
+}
 
 vtkStandardNewMacro(vtkAnnotateGlobalDataFilter);
 //----------------------------------------------------------------------------
@@ -35,6 +42,7 @@ vtkAnnotateGlobalDataFilter::vtkAnnotateGlobalDataFilter()
 {
   this->Prefix = 0;
   this->FieldArrayName = 0;
+  this->SetArrayAssociation(vtkDataObject::FIELD);
 }
 
 //----------------------------------------------------------------------------
@@ -45,80 +53,29 @@ vtkAnnotateGlobalDataFilter::~vtkAnnotateGlobalDataFilter()
 }
 
 //----------------------------------------------------------------------------
-int vtkAnnotateGlobalDataFilter::RequestData(
-    vtkInformation* request,
-    vtkInformationVector** inputVector,
-    vtkInformationVector* outputVector)
+void vtkAnnotateGlobalDataFilter::EvaluateExpression()
 {
-  vtkDataObject* input = vtkDataObject::GetData(inputVector[0]);
-  vtkInformation* outInfo = this->GetExecutive()->GetOutputInformation(0);
+  vtksys_ios::ostringstream stream;
+  stream
+    << "def vtkAnnotateGlobalDataFilter_EvaluateExpression():" << endl
+    << "    from paraview import annotation as pv_ann" << endl
+    << "    from vtkPVClientServerCoreDefaultPython import vtkAnnotateGlobalDataFilter" << endl
+    << "    me = vtkAnnotateGlobalDataFilter('" << vtkGetReferenceAsString(this) << " ')" << endl
+    << "    pv_ann.execute_on_global_data(me)" << endl
+    << "    del me" << endl
+    << "vtkAnnotateGlobalDataFilter_EvaluateExpression()" << endl
+    << "del vtkAnnotateGlobalDataFilter_EvaluateExpression" << endl;
 
-  // Is it a time dependent field ?
-  bool timeDependent = false;
-  bool isDataArray = false;
-  bool validExpression = false;
-  bool isFieldOnABlock =
-      (NULL == input->GetFieldData()->GetAbstractArray(this->GetFieldArrayName()));
-  int nbTimeSteps = outInfo->Has(vtkStreamingDemandDrivenPipeline::TIME_STEPS()) ?
-        outInfo->Length(vtkStreamingDemandDrivenPipeline::TIME_STEPS()) : -1;
-
-  vtkAbstractArray* data = input->GetFieldData()->GetAbstractArray(this->GetFieldArrayName());
-  if(data)
-    {
-    isDataArray = (data->IsA("vtkDataArray") != 0);
-    int nbFieldsValues = data->GetNumberOfTuples();
-    timeDependent = (nbTimeSteps == nbFieldsValues);
-    validExpression = true;
-    }
-  else if(input->IsA("vtkCompositeDataSet") != 0)
-    {
-    vtkCompositeDataSet* multiBlock = vtkCompositeDataSet::SafeDownCast(input);
-    vtkSmartPointer<vtkCompositeDataIterator> iter;
-    iter.TakeReference(multiBlock->NewIterator());
-    iter->GoToFirstItem();
-    while(!iter->IsDoneWithTraversal())
-      {
-      data = iter->GetCurrentDataObject()->GetFieldData()->GetAbstractArray(this->GetFieldArrayName());
-      if(data)
-        {
-        isDataArray = (data->IsA("vtkDataArray") != 0);
-        int nbFieldsValues = data->GetNumberOfTuples();
-        timeDependent = (nbTimeSteps == nbFieldsValues);
-        validExpression = true;
-        }
-      iter->GoToNextItem();
-      }
-    }
-
-  // Create the expression based on our local properties
-  vtksys_ios::ostringstream expression;
-
-  if(!validExpression)
-    {
-    expression << "'Invalid expression'";
-    }
-  else if(isDataArray)
-    {
-    expression << "\"" << this->GetPrefix() << " %s\" % str("
-               << (isFieldOnABlock ? "inputMB[0]" : "input")
-               << ".FieldData['" << this->GetFieldArrayName() << "']"
-               << "[" << (timeDependent ? "t_index" : "0") << ",0])";
-    }
-  else
-    {
-    expression << "\"" << this->GetPrefix() << " %s\" % str("
-               << (isFieldOnABlock ? "inputMB[0]" : "input")
-               << ".FieldData['" << this->GetFieldArrayName() << "'].GetValue("
-               << (timeDependent ? "t_index" : "0") << "))";
-    }
-
-  this->SetExpression(expression.str().c_str());
-
-  return this->Superclass::RequestData(request, inputVector, outputVector);;
+  // ensure Python is initialized.
+  vtkPythonInterpreter::Initialize();
+  vtkPythonInterpreter::RunSimpleString(stream.str().c_str());
 }
 
 //----------------------------------------------------------------------------
 void vtkAnnotateGlobalDataFilter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
+  os << indent << "FieldArrayName: " << (this->FieldArrayName?
+    this->FieldArrayName : "(none)") << endl;
+  os << indent << "Prefix: " << (this->Prefix? this->Prefix : "(none)") << endl;
 }
