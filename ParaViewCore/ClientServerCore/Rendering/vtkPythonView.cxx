85602edfe425cd537d220a7e90076feef5eb7f8c
Cory Quammen <cory.quammen@kitware.com>
2014-07-24 11:38:18 -0400

    Added removal of DOS line-endings in Python code
    Cribbed from vtkPythonInterpreter.cxx.
    
    Change-Id: I2b8de47ce3f2e8d93e5c7cded3af0ea969445aad

--- ParaViewCore/ClientServerCore/Rendering/vtkPythonView.cxx
+++ ParaViewCore/ClientServerCore/Rendering/vtkPythonView.cxx
@@ -28,6 +28,7 @@
 #include "vtkTexture.h"
 #include "vtkTimerLog.h"
 
+#include <algorithm>
 #include <vtksys/ios/sstream>
 
 
@@ -470,8 +471,13 @@ bool vtkPythonView::IsLocalDataAvailable()
 //----------------------------------------------------------------------------
 int vtkPythonView::RunSimpleStringWithCustomLocals(const char* code)
 {
+  // The embedded python interpreter cannot handle DOS line-endings, see
+  // http://sourceforge.net/tracker/?group_id=5470&atid=105470&func=detail&aid=1167922
+  std::string buffer = code ? code : "";
+  buffer.erase(std::remove(buffer.begin(), buffer.end(), '\r'), buffer.end());
+
   PyObject* context = this->Internals->GetCustomLocalsPyObject();
-  PyObject* result = PyRun_String(const_cast<char*>(code), Py_file_input, context, context);
+  PyObject* result = PyRun_String(const_cast<char*>(buffer.c_str()), Py_file_input, context, context);
 
   if (result == NULL)
     {
