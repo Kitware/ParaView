6575a800af801426a49484c6f7227c1e40117946
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2014-09-03 16:04:00 -0600

    Remove AUTOLOAD from RGBZ View plugin
    Change-Id: I556294172a7392bbae9b4abe1856be0d9f4111b9

--- Applications/ParaView/Testing/Python/CinemaTest.py
+++ Applications/ParaView/Testing/Python/CinemaTest.py
@@ -6,6 +6,12 @@ from paraview import data_exploration as cinema
 
 smtesting.ProcessCommandLineArguments()
 work_directory = os.path.join(smtesting.TempDir, 'cinema')
+has_rgbz_view = True
+
+try:
+    simple.LoadDistributedPlugin('RGBZView', ns=globals())
+except:
+    has_rgbz_view = False
 
 # === Data to explore and configuration =======================================
 
@@ -87,32 +93,33 @@ exp.UpdatePipeline()
 
 # === CompositeImageExporter ==================================================
 # CompositeImageExporter(file_name_generator, data_list, colorBy_list, luts, camera_handler, view_size, data_list_pipeline, axisVisibility=1, orientationVisibility=1, format='jpg')
-analysis.register_analysis(
-    "composite",
-    "Composite rendering",
-    "Performing composite on contour",
-    '{theta}/{phi}/{filename}', cinema.CompositeImageExporter.get_data_type())
-fng = analysis.get_file_name_generator("composite")
+if has_rgbz_view:
+    analysis.register_analysis(
+        "composite",
+        "Composite rendering",
+        "Performing composite on contour",
+        '{theta}/{phi}/{filename}', cinema.CompositeImageExporter.get_data_type())
+    fng = analysis.get_file_name_generator("composite")
 
-# Create pipeline to compose
-contour_values = [ 64.0, 90.6, 117.2, 143.8, 170.4, 197.0, 223.6, 250.2]
-color_type = [('POINT_DATA', "RTData")]
-luts = { "RTData": lut }
-filters = [ data_to_explore ]
-filters_description = [ {'name': 'Wavelet'} ]
-color_by = [ color_type ]
+    # Create pipeline to compose
+    contour_values = [ 64.0, 90.6, 117.2, 143.8, 170.4, 197.0, 223.6, 250.2]
+    color_type = [('POINT_DATA', "RTData")]
+    luts = { "RTData": lut }
+    filters = [ data_to_explore ]
+    filters_description = [ {'name': 'Wavelet'} ]
+    color_by = [ color_type ]
 
-for iso_value in contour_values:
-    filters.append( simple.Contour( Input=data_to_explore, PointMergeMethod="Uniform Binning", ContourBy = ['POINTS', 'RTData'], Isosurfaces = [iso_value], ComputeScalars = 1 ) )
-    color_by.append( color_type )
-    filters_description.append({'name': 'iso=%s' % str(iso_value)})
+    for iso_value in contour_values:
+        filters.append( simple.Contour( Input=data_to_explore, PointMergeMethod="Uniform Binning", ContourBy = ['POINTS', 'RTData'], Isosurfaces = [iso_value], ComputeScalars = 1 ) )
+        color_by.append( color_type )
+        filters_description.append({'name': 'iso=%s' % str(iso_value)})
 
 
-# Data exploration ------------------------------------------------------------
-camera_handler = cinema.ThreeSixtyCameraHandler(fng, None, [ float(r) for r in range(0, 360, 72)], [ float(r) for r in range(-60, 61, 45)], center_of_rotation, rotation_axis, distance)
-exporter = cinema.CompositeImageExporter(fng, filters, color_by, luts, camera_handler, [400,400], filters_description, 0, 0)
-exporter.set_analysis(analysis)
-exporter.UpdatePipeline()
+    # Data exploration ------------------------------------------------------------
+    camera_handler = cinema.ThreeSixtyCameraHandler(fng, None, [ float(r) for r in range(0, 360, 72)], [ float(r) for r in range(-60, 61, 45)], center_of_rotation, rotation_axis, distance)
+    exporter = cinema.CompositeImageExporter(fng, filters, color_by, luts, camera_handler, [400,400], filters_description, 0, 0)
+    exporter.set_analysis(analysis)
+    exporter.UpdatePipeline()
 
 # === TODO ====================================================================
 # ThreeSixtyImageStackExporter(file_name_generator, view_proxy, focal_point=[0.0,0.0,0.0], distance=100.0, rotation_axis=[0,0,1], angular_steps=[10,15])
