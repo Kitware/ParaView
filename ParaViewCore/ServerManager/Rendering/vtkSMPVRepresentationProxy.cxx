53f73a40a68ce22991191091a9553d9382220b8c
Tristan Coulange <tristan.coulange@kitware.com>
2014-11-24 09:17:26 +0100

    Bug #0015007: Fix color range length

--- ParaViewCore/ServerManager/Rendering/vtkSMPVRepresentationProxy.cxx
+++ ParaViewCore/ServerManager/Rendering/vtkSMPVRepresentationProxy.cxx
@@ -33,6 +33,7 @@
 #include "vtkSMTransferFunctionManager.h"
 #include "vtkSMTransferFunctionProxy.h"
 
+#include <cmath>
 #include <set>
 #include <string>
 #include <vtksys/ios/sstream>
@@ -220,7 +221,7 @@ bool vtkSMPVRepresentationProxy::RescaleTransferFunctionToDataRange(
     vtkWarningMacro("No input present. Cannot determine data ranges.");
     return false;
     }
-  
+
   vtkPVDataInformation* dataInfo = inputProxy->GetDataInformation(port);
   vtkPVArrayInformation* info = dataInfo->GetArrayInformation(
     arrayname, attribute_type);
@@ -263,7 +264,7 @@ bool vtkSMPVRepresentationProxy::RescaleTransferFunctionToDataRangeOverTime(
     vtkWarningMacro("No input present. Cannot determine data ranges.");
     return false;
     }
- 
+
   vtkPVTemporalDataInformation* dataInfo =
     inputProxy->GetOutputPort(port)->GetTemporalDataInformation();
   vtkPVArrayInformation* info = dataInfo->GetArrayInformation(
@@ -305,9 +306,20 @@ bool vtkSMPVRepresentationProxy::RescaleTransferFunctionToDataRange(
     info->GetComponentRange(component, range);
     if (range[1] >= range[0])
       {
-      if ( (range[1] - range[0]) < 1e-16 )
+      // the range must be large enough, compared to values order of magnitude
+      double rangeOrderOfMagnitude = 1e-11;
+      if (rangeOrderOfMagnitude < std::abs(range[0]))
         {
-        range[1] = range[0] + 1e-16;
+        rangeOrderOfMagnitude = std::abs(range[0]);
+        }
+      if (rangeOrderOfMagnitude < std::abs(range[1]))
+        {
+        rangeOrderOfMagnitude = std::abs(range[1]);
+        }
+      double rangeMinLength = rangeOrderOfMagnitude * 1e-5;
+      if ((range[1] - range[0]) < rangeMinLength)
+        {
+        range[1] = range[0] + rangeMinLength;
         }
       // If data range is too small then we tweak it a bit so scalar mapping
       // produces valid/reproducible results.
