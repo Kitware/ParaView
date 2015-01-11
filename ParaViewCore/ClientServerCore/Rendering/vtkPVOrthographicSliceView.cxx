Kitware/ParaView

commit e34444b0515c18532113639b1c35c312ef370282

Cleanup interactor styles.
Miscellaneous cleanups to interactor styles in ParaView.

* vtkPVInteractorStyle was unnecessarily subclassing from
  vtkInteractorStyleTrackballCamera. It should really just be a
  vtkInteractorStyle subclass. Fixed that.

* vtkPVGenericRenderWindowInteractor was messing with
  FindPokedRenderer() due to an old hack to avoid the interactor from
  picking the 2D renderer. This was really an hack. Setting Interactive
  to Off on the vtkRenderer is the better way. vtkPVRenderView already
  does that for the 2D renderer. Hence removed this hack.

Change-Id: I5ce82b16e48a632b6d272d73f73dd351b900fa35

ParaViewCore/VTKExtensions/Rendering/vtkPVInteractorStyle.h
@@ -24,17 +24,17 @@
 #ifndef __vtkPVInteractorStyle_h
 #define __vtkPVInteractorStyle_h
 
-#include "vtkInteractorStyleTrackballCamera.h"
+#include "vtkInteractorStyle.h"
 #include "vtkPVVTKExtensionsRenderingModule.h" // needed for export macro
 
 class vtkCameraManipulator;
 class vtkCollection;
 
-class VTKPVVTKEXTENSIONSRENDERING_EXPORT vtkPVInteractorStyle : public vtkInteractorStyleTrackballCamera
+class VTKPVVTKEXTENSIONSRENDERING_EXPORT vtkPVInteractorStyle : public vtkInteractorStyle
 {
 public:
   static vtkPVInteractorStyle *New();
-  vtkTypeMacro(vtkPVInteractorStyle, vtkInteractorStyleTrackballCamera);
+  vtkTypeMacro(vtkPVInteractorStyle, vtkInteractorStyle);
   void PrintSelf(ostream& os, vtkIndent indent);
 
   // Description:

ParaViewCore/ClientServerCore/Rendering/vtkPVRenderView.cxx
@@ -255,7 +255,6 @@ vtkPVRenderView::vtkPVRenderView()
         this->ThreeDInteractorStyle = vtkPVInteractorStyle::New();
     this->TwoDInteractorStyle = vtkPVInteractorStyle::New();
 
-    this->Interactor->SetRenderer(this->GetRenderer());
     this->Interactor->SetRenderWindow(this->GetRenderWindow());
     this->Interactor->SetInteractorStyle(this->ThreeDInteractorStyle);
 

ParaViewCore/VTKExtensions/Rendering/vtkPVGenericRenderWindowInteractor.cxx
@@ -20,7 +20,6 @@
 #include "vtkPVInteractorStyle.h"
 #include "vtkPVRenderViewProxy.h"
 #include "vtkRendererCollection.h"
-#include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
 #include "vtkPVConfig.h"
 
@@ -156,13 +155,11 @@ protected:
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkPVGenericRenderWindowInteractor);
-vtkCxxSetObjectMacro(vtkPVGenericRenderWindowInteractor,Renderer,vtkRenderer);
 
 //----------------------------------------------------------------------------
 vtkPVGenericRenderWindowInteractor::vtkPVGenericRenderWindowInteractor()
 {
   this->PVRenderView = NULL;
-  this->Renderer = NULL;
   this->InteractiveRenderEnabled = 0;
   this->Observer = vtkPVGenericRenderWindowInteractorObserver::New();
   this->Observer->SetTarget(this);
@@ -189,7 +186,6 @@ vtkPVGenericRenderWindowInteractor::~vtkPVGenericRenderWindowInteractor()
   this->Timer->Delete();
 
   this->SetPVRenderView(NULL);
-  this->SetRenderer(NULL);
 }
 
 //----------------------------------------------------------------------------
@@ -275,18 +271,6 @@ void vtkPVGenericRenderWindowInteractor::SetPVRenderView(vtkPVRenderViewProxy *v
       }
     }
 }
-
-//----------------------------------------------------------------------------
-vtkRenderer* vtkPVGenericRenderWindowInteractor::FindPokedRenderer(int,int)
-{
-  if (this->Renderer == NULL)
-    {
-    vtkErrorMacro("Renderer has not been set.");
-    }
-
-  return this->Renderer;
-}
-
 
 //----------------------------------------------------------------------------
 void vtkPVGenericRenderWindowInteractor::Render()
@@ -414,7 +398,6 @@ void vtkPVGenericRenderWindowInteractor::PrintSelf(ostream& os, vtkIndent indent
   os << indent << "PVRenderView: " << this->GetPVRenderView() << endl;
   os << indent << "InteractiveRenderEnabled: "
      << this->InteractiveRenderEnabled << endl;
-  os << indent << "Renderer: " << this->Renderer << endl;
   os << indent << "CenterOfRotation: " << this->CenterOfRotation[0] << ", "
     << this->CenterOfRotation[1] << ", " << this->CenterOfRotation[2] << endl;
   os << indent << "RotationFactor: " << this->RotationFactor << endl;

ParaViewCore/VTKExtensions/Rendering/vtkPVGenericRenderWindowInteractor.h
@@ -22,7 +22,6 @@
 #include "vtkPVVTKExtensionsRenderingModule.h" // needed for export macro
 
 class vtkPVRenderViewProxy;
-class vtkRenderer;
 class vtkPVGenericRenderWindowInteractorObserver;
 class vtkPVGenericRenderWindowInteractorTimer;
 
@@ -35,14 +34,6 @@ public:
 
   void SetPVRenderView(vtkPVRenderViewProxy *view);
   vtkGetObjectMacro(PVRenderView, vtkPVRenderViewProxy);
-
-  // Description:
-  // My sollution to the poked renderer problem.
-  // This interactor class always returns this renderer as poked render.
-  // This insures the 2D renderer will never be poked.
-  void SetRenderer(vtkRenderer *view);
-  vtkGetObjectMacro(Renderer,vtkRenderer);
-  virtual vtkRenderer *FindPokedRenderer(int,int);
 
   // Description:
   // 3D widgets call render on this interactor directly.
@@ -119,7 +110,6 @@ protected:
 
   vtkPVRenderViewProxy *PVRenderView;
   int InteractiveRenderEnabled;
-  vtkRenderer* Renderer;
 
   unsigned long NonInteractiveRenderDelay;
   double CenterOfRotation[3];

ParaViewCore/VTKExtensions/Rendering/vtkPVInteractorStyle.cxx
@@ -90,14 +90,11 @@ void vtkPVInteractorStyle::OnButtonDown(int button, int shift, int control)
     }
 
   // Get the renderer.
-  if (!this->CurrentRenderer)
+  this->FindPokedRenderer(this->Interactor->GetEventPosition()[0],
+    this->Interactor->GetEventPosition()[1]);
+  if (this->CurrentRenderer == NULL)
     {
-    this->FindPokedRenderer(this->Interactor->GetEventPosition()[0],
-                            this->Interactor->GetEventPosition()[1]);
-    if (this->CurrentRenderer == NULL)
-      {
-      return;
-      }
+    return;
     }
 
   // Look for a matching camera interactor.
@@ -163,10 +160,15 @@ void vtkPVInteractorStyle::OnButtonUp(int button)
 //-------------------------------------------------------------------------
 void vtkPVInteractorStyle::OnMouseMove()
 {
-  if (!this->CurrentRenderer)
+  if (this->CurrentRenderer && this->Current)
+    {
+    // When an interaction is active, we should not change the renderer being
+    // interacted with.
+    }
+  else
     {
     this->FindPokedRenderer(this->Interactor->GetEventPosition()[0],
-                            this->Interactor->GetEventPosition()[1]);
+      this->Interactor->GetEventPosition()[1]);
     }
 
   if (this->Current)

commit 723efae90e131f1eab880980e3c5f96275e0fbcd

BUG #15247: Restore mouse-wheel.
The wheel was broken in e34444b0515c. The change was cleaning up
vtkPVInteractorStyle. vtkPVInteractorStyle has traditionally been
subclass of vtkInteractorStyleTrackballCamera. Restoring that behaviour.
Ideally, we want to add better support for mouse-wheel. Leaving the
incidental support for now. Also manually disabled wheel in
vtkPVOrthographicSliceView since the wheel conflicts with the user
interactions for moving slices in that view.

Change-Id: I4ae5db174e60f208c64a53ecf2dcf00459d2be7c

ParaViewCore/ClientServerCore/Rendering/vtkPVOrthographicSliceView.cxx
@@ -101,6 +101,10 @@ public:
       }
     }
 
+  // Disable wheel-to-zoom in this view.
+  virtual void OnMouseWheelForward() {}
+  virtual void OnMouseWheelBackward() {}
+
 protected:
   vtkPVOrthographicSliceViewInteractorStyle():
     PrimaryInteractorStyle(NULL),

ParaViewCore/VTKExtensions/Rendering/vtkPVInteractorStyle.h
@@ -24,17 +24,17 @@
 #ifndef __vtkPVInteractorStyle_h
 #define __vtkPVInteractorStyle_h
 
-#include "vtkInteractorStyle.h"
+#include "vtkInteractorStyleTrackballCamera.h"
 #include "vtkPVVTKExtensionsRenderingModule.h" // needed for export macro
 
 class vtkCameraManipulator;
 class vtkCollection;
 
-class VTKPVVTKEXTENSIONSRENDERING_EXPORT vtkPVInteractorStyle : public vtkInteractorStyle
+class VTKPVVTKEXTENSIONSRENDERING_EXPORT vtkPVInteractorStyle : public vtkInteractorStyleTrackballCamera
 {
 public:
   static vtkPVInteractorStyle *New();
-  vtkTypeMacro(vtkPVInteractorStyle, vtkInteractorStyle);
+  vtkTypeMacro(vtkPVInteractorStyle, vtkInteractorStyleTrackballCamera);
   void PrintSelf(ostream& os, vtkIndent indent);
 
   // Description: