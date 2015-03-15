86ad1061833bc7d42463660bb3fd5d285456976c
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-03-15 16:35:16 -0400

    Switch developer setup to use the GitLab workflow
    In SetupForDevelopment, switch from setup-gerrit to setup-gitlab.
    Also invoke setup-upstream to switch the "origin" remote to GitLab.
    Update the version of the SetupForDevelopment script so that existing
    work tree hooks will ask developers to run the script again.

--- Utilities/SetupForDevelopment.sh
+++ Utilities/SetupForDevelopment.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/env bash
 
 # Make sure we are inside the repository.
-cd "${BASH_SOURCE%/*}/.." || exit
-
-Utilities/Git/GitInfo # Help CMake find Git
-
-echo "Configuring push urls..."
-git config remote.origin.pushurl git@paraview.org:ParaView.git
+cd "${BASH_SOURCE%/*}/.." &&
+Utilities/GitSetup/setup-user && echo &&
+Utilities/GitSetup/setup-hooks && echo &&
+Utilities/Scripts/SetupGitAliases.sh && echo &&
+Utilities/GitSetup/setup-upstream && echo &&
+(Utilities/GitSetup/setup-gitlab ||
+ echo 'Failed to setup GitLab.  Run this again to retry.') && echo &&
+Utilities/Scripts/SetupExternalData.sh && echo &&
+Utilities/GitSetup/tips
 
 echo "Initializing and updating git submodules..."
 git submodule update --init --recursive
+
 # Rebase master by default
 git config rebase.stat true
 git config branch.master.rebase true
 
-Utilities/GitSetup/setup-user && echo &&
-Utilities/GitSetup/setup-hooks && echo &&
-Utilities/GitSetup/setup-stage && echo &&
-(Utilities/GitSetup/setup-ssh ||
- echo 'Failed to setup SSH.  Run this again to retry.') && echo &&
-(Utilities/GitSetup/setup-gerrit ||
- echo 'Failed to setup Gerrit.  Run this again to retry.') && echo &&
-Utilities/Scripts/SetupGitAliases.sh && echo &&
-Utilities/Scripts/SetupExternalData.sh && echo &&
-Utilities/Scripts/SetupPVVTK.sh && echo &&
-Utilities/GitSetup/tips
-
+# Disable Gerrit hook explicitly so the commit-msg hook will
+# not complain even if some gerrit remotes are still configured.
+git config hooks.GerritId false
 
 # Record the version of this setup so Scripts/pre-commit can check it.
-SetupForDevelopment_VERSION=1
+SetupForDevelopment_VERSION=2
 git config hooks.SetupForDevelopment ${SetupForDevelopment_VERSION}
