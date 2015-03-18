58881ad7aba92506edbee6d23e21febc84de5e3e
Shawn Waldon <shawn.waldon@kitware.com>
2015-03-18 10:05:03 -0400

    Disable plugins so that an OpenGL2 build configures

--- Plugins/SciberQuestToolKit/plugin.cmake
+++ Plugins/SciberQuestToolKit/plugin.cmake
@@ -1,4 +1,6 @@
-pv_plugin(SciberQuestToolKit
-  DESCRIPTION
-    "SciberQuestToolKit - Domain specific visualization tools for magnetospheric sciences."
-  DEFAULT_ENABLED)
+if(VTK_RENDERING_BACKEND STREQUAL "OpenGL")
+  pv_plugin(SciberQuestToolKit
+    DESCRIPTION
+      "SciberQuestToolKit - Domain specific visualization tools for magnetospheric sciences."
+    DEFAULT_ENABLED)
+endif()
