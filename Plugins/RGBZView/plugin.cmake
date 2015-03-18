93cbaa540d744314607d2ea5820ed51f3e5da62e
Shawn Waldon <shawn.waldon@kitware.com>
2015-03-18 10:12:37 -0400

    Paraview default build with OpenGL2 backend builds

--- Plugins/RGBZView/plugin.cmake
+++ Plugins/RGBZView/plugin.cmake
@@ -1,4 +1,6 @@
-pv_plugin(RGBZView
-  # provide a description for the plugin.
-  DESCRIPTION "3D View that dump into a file the RGB and Z buffer for each render."
-  DEFAULT_ENABLED)
+if(VTK_RENDERING_BACKEND STREQUAL "OpenGL")
+  pv_plugin(RGBZView
+    # provide a description for the plugin.
+    DESCRIPTION "3D View that dump into a file the RGB and Z buffer for each render."
+    DEFAULT_ENABLED)
+endif()
--- Plugins/UncertaintyRendering/plugin.cmake
+++ Plugins/UncertaintyRendering/plugin.cmake
@@ -1,3 +1,5 @@
-pv_plugin(UncertaintyRendering
-  DESCRIPTION "Add uncertainty visualization support"
-  DEFAULT_ENABLED)
+if(VTK_RENDERING_BACKEND STREQUAL "OpenGL")
+  pv_plugin(UncertaintyRendering
+    DESCRIPTION "Add uncertainty visualization support"
+    DEFAULT_ENABLED)
+endif()
