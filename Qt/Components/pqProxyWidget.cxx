Kitware/ParaView

commit d41938973caa9fa4ff54affda755133a757d26ef

Add support for showing a header-label in pqProxyWidget.
Adding support for ShowProxyDocumentationInPanel hint. When this hint
is present on the proxy, the pqProxyWidget will generate a label at the
head of the widget containing the documentation for the proxy.

Qt/Components/pqProxyWidget.cxx
@@ -540,6 +540,7 @@ public:
   bool CachedShowAdvanced;
   QString CachedFilterText;
   vtkStringList* Properties;
+  QPointer<QLabel> ProxyDocumentationLabel; // used when showProxyDocumentationInPanel is true.
 
   pqInternals(vtkSMProxy* smproxy, QStringList properties):
     Proxy(smproxy), CachedShowAdvanced(false)
@@ -623,6 +624,9 @@ void pqProxyWidget::constructor(
   // if the proxy wants a more descriptive layout for the panel, use it.
   this->UseDocumentationForLabels = pqProxyWidget::useDocumentationForLabels(smproxy);
   this->Internals = new pqProxyWidget::pqInternals(smproxy, properties);
+  this->Internals->ProxyDocumentationLabel = new QLabel(this);
+  this->Internals->ProxyDocumentationLabel->hide();
+  this->Internals->ProxyDocumentationLabel->setWordWrap(true);
 
   QGridLayout* gridLayout = new QGridLayout(this);
   gridLayout->setMargin(pqPropertiesPanel::suggestedMargin());
@@ -674,6 +678,23 @@ QString pqProxyWidget::documentationText(vtkSMProperty* smProperty)
     {
     return pqProxy::rstToHtml(xmlDocumentation).c_str();
     }
+}
+
+//-----------------------------------------------------------------------------
+QString pqProxyWidget::documentationText(vtkSMProxy* smProxy)
+{
+  const char* xmlDocumentation = smProxy->GetDocumentation()?
+    smProxy->GetDocumentation()->GetDescription() : NULL;
+  return (!xmlDocumentation || xmlDocumentation[0] == 0)?
+    QString() : pqProxy::rstToHtml(xmlDocumentation).c_str();
+}
+
+//-----------------------------------------------------------------------------
+bool pqProxyWidget::showProxyDocumentationInPanel(vtkSMProxy* smproxy)
+{
+  return (smproxy &&
+    smproxy->GetHints() &&
+    smproxy->GetHints()->FindNestedElementByName("ShowProxyDocumentationInPanel"));
 }
 
 //-----------------------------------------------------------------------------
@@ -1169,7 +1190,7 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
   delete this->layout();
   QVBoxLayout* vboxLayout = NULL;
   QGridLayout* gridLayout = NULL;
-  
+
   if (this->UseDocumentationForLabels)
     {
     vboxLayout = new QVBoxLayout(this);
@@ -1189,6 +1210,21 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
   int row_index = 0;
   const pqProxyWidgetItem* prevItem = NULL;
   vtkSMProxy* smProxy = this->Internals->Proxy;
+
+  if (this->showProxyDocumentationInPanel(smProxy))
+    {
+    QString doc = this->documentationText(smProxy);
+    cout << doc.toStdString().c_str() << endl;
+    this->Internals->ProxyDocumentationLabel->setText(doc);
+    this->Internals->ProxyDocumentationLabel->setVisible(!doc.isEmpty());
+    gridLayout->addWidget(this->Internals->ProxyDocumentationLabel, row_index, 0, 1, 2);
+    row_index++;
+    }
+  else
+    {
+    this->Internals->ProxyDocumentationLabel->hide();
+    }
+
   foreach (const pqProxyWidgetItem* item, this->Internals->Items)
     {
     bool visible = item->canShowWidget(show_advanced, filterText, smProxy);

Qt/Core/pqProxy.cxx
@@ -437,5 +437,5 @@ std::string pqProxy::rstToHtml(const char* rstStr)
     htmlStr.replace (start, src.size(), "\n<p>\n");
     }
   }
-  return htmlStr;
+  return "<html><head/><body><p>" + htmlStr + "</p></body></html>";
 }

Qt/Components/pqProxyWidget.h
@@ -83,8 +83,22 @@ public:
   /// the same purpose, as needed.
   static QWidget* newGroupLabelWidget(const QString& label, QWidget* parentWidget);
 
+  /// Returns true of the proxy provided has XML hints indicating that labels
+  /// should use documentation instead of the XML label for the widgets in the
+  /// UI.
   static bool useDocumentationForLabels(vtkSMProxy* proxy);
+
+  /// Returns formatted (HTML or plainText) documentation for the property.
   static QString documentationText(vtkSMProperty* property);
+
+  /// Returns formatted (HTML or plainText) documentation for the proxy.
+  /// This only returns the text returned by vtkSMDocumentation::GetDescription().
+  static QString documentationText(vtkSMProxy* property);
+
+  /// Returns true if the proxy has XML hints indicating that the panel should
+  /// show a header label for the documentation. pqProxyWidget uses the
+  /// <ShowProxyDocumentationInPanel /> hint for this purpose.
+  static bool showProxyDocumentationInPanel(vtkSMProxy* proxy);
 
 signals:
   /// This signal is fired as soon as the user starts editing in the widget. The

commit 05cb09356c34e6434165363467e5f276cf293f02

Fixed label formatting in Settings dialog.
This bug was introduced by d4193897. Reverted the change to add HTML
headers in pqProxy::rstToHtml(). Instead, adding the formatting headers
in pqProxyWidget when using the text returned from
pqProxyWidget::documentationText() -- as seems to be the norm in the
current implementation.

Qt/Components/pqProxyWidget.cxx
@@ -1214,8 +1214,7 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
   if (this->showProxyDocumentationInPanel(smProxy))
     {
     QString doc = this->documentationText(smProxy);
-    cout << doc.toStdString().c_str() << endl;
-    this->Internals->ProxyDocumentationLabel->setText(doc);
+    this->Internals->ProxyDocumentationLabel->setText("<p>" + doc + "</p>");
     this->Internals->ProxyDocumentationLabel->setVisible(!doc.isEmpty());
     gridLayout->addWidget(this->Internals->ProxyDocumentationLabel, row_index, 0, 1, 2);
     row_index++;

Qt/Core/pqProxy.cxx
@@ -437,5 +437,5 @@ std::string pqProxy::rstToHtml(const char* rstStr)
     htmlStr.replace (start, src.size(), "\n<p>\n");
     }
   }
-  return "<html><head/><body><p>" + htmlStr + "</p></body></html>";
+  return htmlStr;
 }