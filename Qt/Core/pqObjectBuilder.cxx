692ea2414fbc01354ef52e3fe657e24fe71eacf5
Dan Lipsa <dan.lipsa@kitware.com>
2015-07-17 16:55:06 -0400

    BUG: vtkDebugLeaksManager needs to be included for static objects.

--- Qt/Core/pqObjectBuilder.cxx
+++ Qt/Core/pqObjectBuilder.cxx
@@ -31,6 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ========================================================================*/
 #include "pqObjectBuilder.h"
 
+#include "vtkDebugLeaksManager.h"
 #include "vtkNew.h"
 #include "vtkProcessModule.h"
 #include "vtkPVXMLElement.h"
