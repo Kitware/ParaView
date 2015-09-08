Kitware/ParaView

commit bfa4d1bd0dbd6a3ce522a2bab2f8d80d26862675

Fix 15673: wrong scalar bar range
The scalar bar range did not match the range in the the information panel

ParaViewCore/ServerManager/Rendering/vtkSMPVRepresentationProxy.cxx
@@ -628,15 +628,6 @@ vtkPVArrayInformation* vtkSMPVRepresentationProxy::GetArrayInformationForColorAr
 
   // now, determine a name for it if possible.
   vtkSMPropertyHelper colorArrayHelper(this, "ColorArrayName");
-  vtkPVArrayInformation* arrayInfo =
-    this->GetRepresentedDataInformation()->GetArrayInformation(
-      colorArrayHelper.GetInputArrayNameToProcess(),
-      colorArrayHelper.GetInputArrayAssociation());
-  if (arrayInfo)
-    {
-    return arrayInfo;
-    }
-
   vtkSMPropertyHelper inputHelper(this, "Input");
   vtkSMSourceProxy* input = vtkSMSourceProxy::SafeDownCast(inputHelper.GetAsProxy());
   unsigned int port = inputHelper.GetOutputPort();
@@ -646,6 +637,16 @@ vtkPVArrayInformation* vtkSMPVRepresentationProxy::GetArrayInformationForColorAr
       colorArrayHelper.GetInputArrayNameToProcess(),
       colorArrayHelper.GetInputArrayAssociation());
     }
+
+  vtkPVArrayInformation* arrayInfo =
+    this->GetRepresentedDataInformation()->GetArrayInformation(
+      colorArrayHelper.GetInputArrayNameToProcess(),
+      colorArrayHelper.GetInputArrayAssociation());
+  if (arrayInfo)
+    {
+    return arrayInfo;
+    }
+
   return NULL;
 }
 

commit 6a41e726a2155fa3e0597873aa52c6807875dfdb

Fix wrong scalar bar range with vtkProcessId
This regression has been introduced in commit bfa4d1bd to fix 15673.

ParaViewCore/ServerManager/Rendering/vtkSMPVRepresentationProxy.cxx
@@ -682,9 +682,13 @@ vtkPVArrayInformation* vtkSMPVRepresentationProxy::GetArrayInformationForColorAr
   unsigned int port = inputHelper.GetOutputPort();
   if (input)
     {
-    return input->GetDataInformation(port)->GetArrayInformation(
+    vtkPVArrayInformation* arrayInfoFromData = input->GetDataInformation(port)->GetArrayInformation(
       colorArrayHelper.GetInputArrayNameToProcess(),
       colorArrayHelper.GetInputArrayAssociation());
+    if (arrayInfoFromData)
+      {
+        return arrayInfoFromData; 
+      }
     }
 
   vtkPVArrayInformation* arrayInfo =