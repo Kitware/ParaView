f632aa5d8ecc10473a6c6a8dcc938430fe8393b8
Ken Martin <ken.martin@kitware.com>
2015-09-09 13:19:23 -0400

    PVAxis was nto releasing its resources properly
    PVAxisWidget was not releaseing its resources properly

--- ParaViewCore/VTKExtensions/Rendering/vtkPVAxesWidget.cxx
+++ ParaViewCore/VTKExtensions/Rendering/vtkPVAxesWidget.cxx
@@ -185,6 +185,8 @@ void vtkPVAxesWidget::SetEnabled(int enabling)
     this->AxesActor->SetVisibility(0);
     if (this->ParentRenderer)
       {
+      // release the resources of the renderer we own
+      this->Renderer->ReleaseGraphicsResources(this->ParentRenderer->GetRenderWindow());
       if (this->ParentRenderer->GetRenderWindow())
         {
         this->ParentRenderer->GetRenderWindow()->RemoveRenderer(this->Renderer);
@@ -194,6 +196,10 @@ void vtkPVAxesWidget::SetEnabled(int enabling)
         {
         this->ParentRenderer->RemoveObserver(this->StartEventObserverId);
         }
+      }
+    else
+      {
+      vtkErrorMacro("Widget disabled without parent window, this should never happen.");
       }
 
     this->InvokeEvent(vtkCommand::DisableEvent, NULL);
