fda812c807cba8ffe391ff3f495eb22ed859a409
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-11-03 10:15:06 -0500

    Fix ParallelPythonImport test.
    VTK_PYTHONPATH variable is no longer available. Simply letting `pvbatch`
    set the environment variables makes this test more robust and still test
    the availability of mpi4py.

--- ParaViewCore/ServerManager/Default/Testing/Python/ParallelPythonImport.py
+++ ParaViewCore/ServerManager/Default/Testing/Python/ParallelPythonImport.py
@@ -2,7 +2,9 @@
 # from a regular Python shell. Note that PYTHONPATH needs to be set
 # to know where to find ParaView.
 import sys
+print 'sys.path = ', sys.path
 from mpi4py import MPI
+
 
 print 'proc', MPI.COMM_WORLD.Get_rank()
 
@@ -12,9 +14,6 @@ paraview.options.batch = True
 paraview.options.symmetric = True
 
 print 'about to import paraview.simple', MPI.COMM_WORLD.Get_rank()
-
 import paraview.simple
 
-import paraview.servermanager as sm
-sm.Finalize()
 print 'finished',  MPI.COMM_WORLD.Get_rank()
