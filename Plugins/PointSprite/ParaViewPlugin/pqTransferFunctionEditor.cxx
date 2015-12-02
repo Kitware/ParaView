Kitware/ParaView

commit d4647cbd0a6c3f6dbce8691a9bac0a780c8bb8ca

Partially fix build issue with pqTransferFunctionEditor.
Change-Id: I9238a092c6f29f353915909c514934ec71d31668

Plugins/PointSprite/ParaViewPlugin/pqTransferFunctionEditor.cxx
@@ -133,10 +133,12 @@ static const char * white_xpm[] = { "40 20 1 1", "+    c #FFFFFF",
 #include "pqPipelineRepresentation.h"
 #include "pqPropertyLinks.h"
 #include "pqSMAdaptor.h"
-#include "vtkSMProxy.h"
-#include "vtkSMProperty.h"
+#include "vtkPVArrayInformation.h"
 #include "vtkPVDataInformation.h"
+#include "vtkSMPVRepresentationProxy.h"
 #include "vtkSMPointSpriteRepresentationProxy.h"
+#include "vtkSMProperty.h"
+#include "vtkSMProxy.h"
 #include "vtkSMStringVectorProperty.h"
 
 // vtk includes
@@ -658,10 +660,19 @@ void pqTransferFunctionEditor::onAutoScalarRange(bool autoRange)
         array, "") == 0)
       return;
 
-    QPair<double, double> range = repr->getColorFieldRange(array, comp);
-
-    this->Internals->scalarMin->setValue(range.first);
-    this->Internals->scalarMax->setValue(range.second);
+    double drange[2] = {0.0, 1.0};
+    if (vtkPVArrayInformation* arrayInfo =
+      vtkSMPVRepresentationProxy::GetArrayInformationForColorArray(reprProxy))
+      {
+      arrayInfo->GetComponentRange(comp, drange);
+      if (drange[1] < drange[0])
+        {
+        drange[0] = 0.0;
+        drange[1] = 1.0;
+        }
+      }
+    this->Internals->scalarMin->setValue(drange[0]);
+    this->Internals->scalarMax->setValue(drange[1]);
 
     if (pqSMAdaptor::getElementProperty(reprProxy->GetProperty(
         this->Internals->IsProportional)).toInt() == 1)

commit 5be999e4bff85f8738352452e4e5593214339587

Fix the range computation for radius and opacity arrays.
The bug was introduced on March 2014 in d4647cbd.
The problem was that vtkSMPVRepresentationProxy::GetArrayInformationForColorArray()
was returning NULL if no color array currently selected (ie. in solid color mode).
I bypass this by directly accessing the input array information.

Plugins/PointSprite/ParaViewPlugin/pqTransferFunctionEditor.cxx
@@ -30,6 +30,9 @@
 #include "pqTransferFunctionEditor.h"
 #include "ui_pqTransferFunctionEditor.h"
 
+#include <vtkDataObject.h>
+#include <vtkSMPropertyHelper.h>
+
 #include <QDoubleValidator>
 #include <QTimer>
 #include <QChar>
@@ -651,24 +654,32 @@ void pqTransferFunctionEditor::onAutoScalarRange(bool autoRange)
       }
 
     const char* array =
-        vtkSMStringVectorProperty::SafeDownCast(reprProxy->GetProperty(
-            this->Internals->ArrayNameProperty))->GetElement(4);
+      vtkSMStringVectorProperty::SafeDownCast(reprProxy->GetProperty(
+      this->Internals->ArrayNameProperty))->GetElement(4);
     int comp = pqSMAdaptor::getElementProperty(reprProxy->GetProperty(
-        this->Internals->ArrayComponentProperty)).toInt();
+      this->Internals->ArrayComponentProperty)).toInt();
 
-    if (strcmp(array, this->Internals->ConstantNameProperty) == 0 || strcmp(
-        array, "") == 0)
-      return;
-
-    double drange[2] = {0.0, 1.0};
-    if (vtkPVArrayInformation* arrayInfo =
-      vtkSMPVRepresentationProxy::GetArrayInformationForColorArray(reprProxy))
+    if (strcmp(array, this->Internals->ConstantNameProperty) == 0
+      || strcmp(array, "") == 0)
       {
-      arrayInfo->GetComponentRange(comp, drange);
-      if (drange[1] < drange[0])
+      return;
+      }
+
+    double drange[2] = { 0.0, 1.0 };
+    vtkSMPropertyHelper inputHelper(reprProxy, "Input");
+    vtkSMSourceProxy* input = vtkSMSourceProxy::SafeDownCast(inputHelper.GetAsProxy());
+    if (input)
+      {
+      vtkPVArrayInformation* arrayInfo = input->GetDataInformation(
+        inputHelper.GetOutputPort())->GetArrayInformation(array, vtkDataObject::FIELD_ASSOCIATION_POINTS);
+      if (arrayInfo)
         {
-        drange[0] = 0.0;
-        drange[1] = 1.0;
+        arrayInfo->GetComponentRange(comp, drange);
+        if (drange[1] < drange[0])
+          {
+          drange[0] = 0.0;
+          drange[1] = 1.0;
+          }
         }
       }
     this->Internals->scalarMin->setValue(drange[0]);