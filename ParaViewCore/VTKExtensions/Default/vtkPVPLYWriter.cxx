dc87e9cca84a3d744ae4a4357103b6f0264b9bbb
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2016-03-31 09:56:21 -0400

    BUG #15997: Add ability to save color information in PLY files.
    The PLY writer now allows user to choose whether color information is to
    be saved in the output file. When enabled, ParaView uses the data
    sources coloring state in the active view i.e. the array to color with
    and the LUT to use, to pass along to the writer so that it can save RGB
    color values in the output dataset.

---
+++ ParaViewCore/VTKExtensions/Default/vtkPVPLYWriter.cxx
@@ -0,0 +1,112 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVPLYWriter.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVPLYWriter.h"
+
+#include "vtkDataSetAttributes.h"
+#include "vtkInformation.h"
+#include "vtkObjectFactory.h"
+#include "vtkPLYWriter.h"
+#include "vtkPolyData.h"
+#include "vtkScalarsToColors.h"
+#include "vtkUnsignedCharArray.h"
+
+#include <algorithm>
+
+vtkStandardNewMacro(vtkPVPLYWriter);
+//----------------------------------------------------------------------------
+vtkPVPLYWriter::vtkPVPLYWriter() : EnableColoring(false)
+{
+  this->SetInputArrayToProcess(0, 0, 0, vtkDataObject::FIELD_ASSOCIATION_POINTS,
+    static_cast<const char*>(NULL));
+}
+
+//----------------------------------------------------------------------------
+vtkPVPLYWriter::~vtkPVPLYWriter()
+{
+}
+
+//----------------------------------------------------------------------------
+int vtkPVPLYWriter::FillInputPortInformation(int, vtkInformation *info)
+{
+  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPLYWriter::SetDataByteOrder(int dbo)
+{
+  this->Writer->SetDataByteOrder(dbo);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPLYWriter::SetFileType(int ftype)
+{
+  this->Writer->SetFileType(ftype);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPLYWriter::SetFileName(const char* fname)
+{ this->Writer->SetFileName(fname);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPLYWriter::SetLookupTable(vtkScalarsToColors* lut)
+{
+  if (this->LookupTable != lut)
+    {
+    this->LookupTable = lut;
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPLYWriter::WriteData()
+{
+  int fieldAssociation;
+  vtkPolyData* input = vtkPolyData::SafeDownCast(this->GetInputDataObject(0, 0));
+  if (vtkAbstractArray* scalars = (this->EnableColoring && this->LookupTable != NULL)?
+    this->GetInputAbstractArrayToProcess(0, input, fieldAssociation) : NULL)
+    {
+    this->Writer->SetColorModeToDefault();
+    this->Writer->SetArrayName("vtkPVPLYWriterColors");
+    vtkUnsignedCharArray* rgba = this->LookupTable->MapScalars(
+      scalars, VTK_COLOR_MODE_MAP_SCALARS, -1);
+    rgba->SetName("vtkPVPLYWriterColors");
+
+    vtkSmartPointer<vtkPolyData> clone;
+    clone.TakeReference(input->NewInstance());
+    clone->ShallowCopy(input);
+    clone->GetAttributes(fieldAssociation)->AddArray(rgba);
+    rgba->FastDelete();
+
+    this->Writer->SetInputDataObject(0, clone.GetPointer());
+    }
+  else
+    {
+    this->Writer->SetInputDataObject(0, input);
+    this->Writer->SetColorModeToOff();
+    }
+  this->Writer->Write();
+  this->Writer->SetInputDataObject(0, NULL);
+}
+
+//----------------------------------------------------------------------------
+void vtkPVPLYWriter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ ParaViewCore/VTKExtensions/Default/vtkPVPLYWriter.h
@@ -0,0 +1,78 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVPLYWriter.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPVPLYWriter - provides a ParaView friendly API for vtkPLYWriter.
+// .SECTION Description
+// This is a facade for vtkPLYWriter that provides an API more amiable to the
+// GUI shown in the ParaView application.
+
+#ifndef vtkPVPLYWriter_h
+#define vtkPVPLYWriter_h
+
+#include "vtkPVVTKExtensionsDefaultModule.h" //needed for exports
+#include "vtkNew.h" // needed for vtkNew
+#include "vtkSmartPointer.h" // needed for vtkSmartPointer
+#include "vtkWriter.h"
+
+class vtkPLYWriter;
+class vtkScalarsToColors;
+
+class VTKPVVTKEXTENSIONSDEFAULT_EXPORT vtkPVPLYWriter : public vtkWriter
+{
+public:
+  static vtkPVPLYWriter* New();
+  vtkTypeMacro(vtkPVPLYWriter, vtkWriter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Enable coloring.
+  vtkSetMacro(EnableColoring, bool);
+  vtkGetMacro(EnableColoring, bool);
+
+  // Description:
+  // If the file type is binary, then the user can specify which
+  // byte order to use (little versus big endian).
+  void SetDataByteOrder(int dbo);
+
+  // Description:
+  // Specify file type (ASCII or BINARY) for vtk data file.
+  void SetFileType(int ftype);
+
+  // Description:
+  // Specify file name of vtk polygon data file to write.
+  void SetFileName(const char* fname);
+
+  // Description:
+  // A lookup table can be specified in order to convert data arrays to
+  // RGBA colors.
+  void SetLookupTable(vtkScalarsToColors* lut);
+
+//BTX
+protected:
+  vtkPVPLYWriter();
+  ~vtkPVPLYWriter();
+
+  virtual int FillInputPortInformation(int port, vtkInformation *info);
+  virtual void WriteData();
+
+  bool EnableColoring;
+  vtkNew<vtkPLYWriter> Writer;
+  vtkSmartPointer<vtkScalarsToColors> LookupTable;
+private:
+  vtkPVPLYWriter(const vtkPVPLYWriter&); // Not implemented
+  void operator=(const vtkPVPLYWriter&); // Not implemented
+//ETX
+};
+
+#endif
