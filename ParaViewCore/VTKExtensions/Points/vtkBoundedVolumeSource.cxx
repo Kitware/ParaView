a903b3271dc67f545f955ecee6c27b25756b57fd
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2016-04-03 19:14:05 -0400

    Added mechanism to specify volume for point interpolation.

---
+++ ParaViewCore/VTKExtensions/Points/vtkBoundedVolumeSource.cxx
@@ -0,0 +1,81 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkBoundedVolumeSource.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkBoundedVolumeSource.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkImageData.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+
+vtkStandardNewMacro(vtkBoundedVolumeSource);
+//----------------------------------------------------------------------------
+vtkBoundedVolumeSource::vtkBoundedVolumeSource()
+{
+  this->SetNumberOfInputPorts(0);
+  this->Origin[0] = this->Origin[1] = this->Origin[2] = 0.0;
+  this->Scale[0] = this->Scale[1] = this->Scale[2] = 1.0;
+  this->Resolution[0] = this->Resolution[1] = this->Resolution[2] = 64;
+}
+
+//----------------------------------------------------------------------------
+vtkBoundedVolumeSource::~vtkBoundedVolumeSource()
+{
+}
+
+//----------------------------------------------------------------------------
+int vtkBoundedVolumeSource::RequestInformation(vtkInformation *,
+  vtkInformationVector **, vtkInformationVector *outputVector)
+{
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+  outInfo->Set(vtkDataObject::ORIGIN(), this->Origin, 3);
+
+  int ext[6] = { 0, this->Resolution[0], 0, this->Resolution[1], 0, this->Resolution[2] };
+  outInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(), ext, 6);
+
+  double spacing[3] = {
+    this->Scale[0] / this->Resolution[0],
+    this->Scale[1] / this->Resolution[1],
+    this->Scale[2] / this->Resolution[2] };
+  outInfo->Set(vtkDataObject::SPACING(), spacing, 3);
+  vtkDataObject::SetPointDataActiveScalarInfo(outInfo, VTK_FLOAT, 1);
+  outInfo->Set(CAN_PRODUCE_SUB_EXTENT(), 1);
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkBoundedVolumeSource::ExecuteDataWithInformation(
+  vtkDataObject *vtkNotUsed(odata), vtkInformation *outInfo)
+{
+  vtkImageData *data = vtkImageData::GetData(outInfo);
+  int* execExt = outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT());
+
+  double spacing[3] = {
+    this->Scale[0] / this->Resolution[0],
+    this->Scale[1] / this->Resolution[1],
+    this->Scale[2] / this->Resolution[2] };
+  data->SetSpacing(spacing);
+  data->SetOrigin(this->Origin);
+  data->SetExtent(execExt);
+}
+
+//----------------------------------------------------------------------------
+void vtkBoundedVolumeSource::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+  os << indent << "Origin: " << this->Origin[0] << ", " << this->Origin[1] << ", " << this->Origin[2] << endl;
+  os << indent << "Scale: " << this->Scale[0] << ", " << this->Scale[1] << ", " << this->Scale[2] << endl;
+  os << indent << "Resolution: " << this->Resolution[0] << ", " << this->Resolution[1] << ", " << this->Resolution[2] << endl;
+}
---
+++ ParaViewCore/VTKExtensions/Points/vtkBoundedVolumeSource.h
@@ -0,0 +1,69 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkBoundedVolumeSource.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkBoundedVolumeSource - a source to generate an image volume.
+// .SECTION Description
+// vtkBoundedVolumeSource generate an image data given the position and
+// scale factors for a unit volume.
+
+#ifndef vtkBoundedVolumeSource_h
+#define vtkBoundedVolumeSource_h
+
+#include "vtkImageAlgorithm.h"
+#include "vtkPVVTKExtensionsPointsModule.h" // for export macro
+
+class VTKPVVTKEXTENSIONSPOINTS_EXPORT vtkBoundedVolumeSource : public vtkImageAlgorithm
+{
+public:
+  static vtkBoundedVolumeSource* New();
+  vtkTypeMacro(vtkBoundedVolumeSource, vtkImageAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Get/Set the origin or translation for the unit volume.
+  vtkSetVector3Macro(Origin, double);
+  vtkGetVector3Macro(Origin, double);
+
+  // Description:
+  // Get/Set the scale factor for a unit volume. Note that scaling is applied
+  // before the translation.
+  vtkSetVector3Macro(Scale, double);
+  vtkGetVector3Macro(Scale, double);
+
+  // Description:
+  // Get/Set the output image resolution.
+  vtkSetVector3Macro(Resolution, int);
+  vtkGetVector3Macro(Resolution, int);
+
+//BTX
+protected:
+  vtkBoundedVolumeSource();
+  ~vtkBoundedVolumeSource();
+
+  virtual int RequestInformation(vtkInformation *request,
+                                 vtkInformationVector **inputVector,
+                                 vtkInformationVector *outputVector);
+  virtual void ExecuteDataWithInformation(vtkDataObject *data, vtkInformation *outInfo);
+
+
+  double Origin[3];
+  double Scale[3];
+  int Resolution[3];
+private:
+  vtkBoundedVolumeSource(const vtkBoundedVolumeSource&); // Not implemented
+  void operator=(const vtkBoundedVolumeSource&); // Not implemented
+//ETX
+};
+
+#endif
