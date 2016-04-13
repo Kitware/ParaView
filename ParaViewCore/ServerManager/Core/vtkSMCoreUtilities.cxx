604cbe44c15ce547a631508a30ad744b6984c402
Robert Maynard <robert.maynard@kitware.com>
2016-04-13 17:35:35 -0400

    vtkSMCoreUtilities now adjusts ranges properly when min~=max.
    Previously when min and max where very close to being equal but also very
    small numbers they would not offset from each other sufficiently.

--- ParaViewCore/ServerManager/Core/vtkSMCoreUtilities.cxx
+++ ParaViewCore/ServerManager/Core/vtkSMCoreUtilities.cxx
@@ -139,8 +139,12 @@ bool vtkSMCoreUtilities::AdjustRangeForLog(double range[2])
   return false;
 }
 
+namespace
+{
+
 //----------------------------------------------------------------------------
-bool vtkSMCoreUtilities::AdjustRange(double range[2])
+template<typename T, typename EquivSizeIntT>
+bool AdjustTRange(T range[2], EquivSizeIntT)
 {
   if (range[1] < range[0])
     {
@@ -148,23 +152,63 @@ bool vtkSMCoreUtilities::AdjustRange(double range[2])
     return false;
     }
 
-  // the range must be large enough, compared to values order of magnitude
-  double rangeOrderOfMagnitude = 1e-11;
-  if (rangeOrderOfMagnitude < std::abs(range[0]))
-    {
-    rangeOrderOfMagnitude = std::abs(range[0]);
+  const bool spans_zero_boundary = range[0] < 0 && range[1] > 0;
+  if(spans_zero_boundary)
+    { //nothing needs to be done, but this check is required.
+      //if we convert into integer space the delta difference will overflow
+      //an integer
+    return false;
     }
-  if (rangeOrderOfMagnitude < std::abs(range[1]))
+
+  EquivSizeIntT irange[2] = { *reinterpret_cast<EquivSizeIntT*>(&range[0]),
+                              *reinterpret_cast<EquivSizeIntT*>(&range[1]) };
+  const EquivSizeIntT minDelta = 65536;
+
+  //determine the absolute delta between these two numbers.
+  EquivSizeIntT delta = std::abs(irange[1] - irange[0]);
+
+  //if our delta is smaller than the min delta push out the max value
+  //so that it is equal to minRange + minDelta. When our range is entirely
+  //negative we should instead subtract from our max, to max a larger negative
+  //value
+  if(delta < minDelta && irange[1] < 0)
     {
-    rangeOrderOfMagnitude = std::abs(range[1]);
+    irange[1] = irange[0] - minDelta;
+    range[1] = *reinterpret_cast<T*>(&irange[1]);
+    return true;
     }
-  double rangeMinLength = rangeOrderOfMagnitude * 1e-5;
-  if ((range[1] - range[0]) < rangeMinLength)
+  if(delta < minDelta)
     {
-    range[1] = range[0] + rangeMinLength;
+    irange[1] = irange[0] + minDelta;
+    range[1] = *reinterpret_cast<T*>(&irange[1]);
     return true;
     }
   return false;
+}
+
+}
+
+//----------------------------------------------------------------------------
+bool vtkSMCoreUtilities::AdjustRange(double range[2])
+{
+  //when the values are between [-2, 2] and you push the max out you need to
+  //make sure you do it in float space, since if you do it in double space the
+  //values will be truncated downstream and be made equivalent again. Once
+  //you move out of this range the double space is generally not truncated
+  if( range[0] > -2 && range[0] < 2 )
+    {
+    float frange[2] = { static_cast<float>(range[0]), static_cast<float>(range[1]) };
+    bool result = AdjustTRange( frange, int() );
+    if(result)
+      { //range should be left untouched to avoid loss of precision when no
+        //adjustment was needed
+      range[0] = frange[0];
+      range[1] = frange[1];
+      }
+    return result;
+    }
+  vtkTypeInt64 intType;
+  return AdjustTRange(range, intType);
 }
 
 //----------------------------------------------------------------------------
