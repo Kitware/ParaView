cf0a08e65f21eed88a79c41e301d29730bd37c69
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2016-06-08 14:08:41 -0400

    Move hardcoded version number to a separate file.
    This makes it easier to script the code that updates the version number
    for each release.

--- CMake/ParaViewDetermineVersion.cmake
+++ CMake/ParaViewDetermineVersion.cmake
@@ -20,7 +20,7 @@
 #   ${var_prefix}_VERSION_PATCH
 #   ${var_prefix}_VERSION_PATCH_EXTRA
 #   ${var_prefix}_VERSION_FULL
-#   ${var_prefix}_VERSION_IS_RELEASE is patch-extra is empty.
+#   ${var_prefix}_VERSION_IS_RELEASE is true, if patch-extra is empty.
 #
 # If git is not found, or git describe cannot be run successfully, then these
 # variables are left unchanged and status message is printed.
@@ -30,15 +30,8 @@
 #   git_command : git executable
 #   var_prefix : prefix for variables e.g. "PARAVIEW".
 function(determine_version source_dir git_command var_prefix)
-  set (major)
-  set (minor)
-  set (patch)
-  set (full)
-  set (patch_extra)
-  set (result -1)
-
-  if (NOT PARAVIEW_GIT_DESCRIBE)
-    if (EXISTS ${git_command})
+  if(NOT PARAVIEW_GIT_DESCRIBE)
+    if(EXISTS ${git_command})
       execute_process(
         COMMAND ${git_command} describe
         WORKING_DIRECTORY ${source_dir}
@@ -52,35 +45,40 @@ function(determine_version source_dir git_command var_prefix)
     set(result 0)
     set(output ${PARAVIEW_GIT_DESCRIBE})
   endif()
-
-  if (${result} EQUAL 0)
-    string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)[-]*(.*)"
-      version_matches "${output}")
-    if (CMAKE_MATCH_0)
-      message(STATUS "Determined Source Version : ${CMAKE_MATCH_0}")
-      set (full ${CMAKE_MATCH_0})
-      set (major ${CMAKE_MATCH_1})
-      set (minor ${CMAKE_MATCH_2})
-      set (patch ${CMAKE_MATCH_3})
-      set (patch_extra ${CMAKE_MATCH_4})
-    endif()
-  endif()
-
-  if (full)
-    set (${var_prefix}_VERSION "${major}.${minor}" PARENT_SCOPE)
-    set (${var_prefix}_VERSION_MAJOR ${major} PARENT_SCOPE)
-    set (${var_prefix}_VERSION_MINOR ${minor} PARENT_SCOPE)
-    set (${var_prefix}_VERSION_PATCH ${patch} PARENT_SCOPE)
-    set (${var_prefix}_VERSION_PATCH_EXTRA ${patch_extra} PARENT_SCOPE)
-    set (${var_prefix}_VERSION_FULL ${full} PARENT_SCOPE)
-    if ("${major}.${minor}.${patch}" EQUAL "${full}")
-      set (${var_prefix}_VERSION_IS_RELEASE TRUE PARENT_SCOPE)
-    else ()
-      set (${var_prefix}_VERSION_IS_RELEASE FALSE PARENT_SCOPE)
-    endif()
+  extract_version_components("${output}" tmp)
+  if(DEFINED tmp_VERSION)
+    message(STATUS "Determined Source Version : ${tmp_VERSION_FULL}")
+    foreach(suffix VERSION VERSION_MAJOR VERSION_MINOR VERSION_PATCH
+                   VERSION_PATCH_EXTRA VERSION_FULL VERSION_IS_RELEASE)
+      set(${var_prefix}_${suffix} ${tmp_${suffix}} PARENT_SCOPE)
+    endforeach()
   else()
     message(STATUS
-      "Could not use git to determine source version, using version ${${var_prefix}_VERSION_FULL}"
-    )
+      "Could not use git to determine source version, using version ${${var_prefix}_VERSION_FULL}")
+  endif()
+endfunction()
+
+# Extracts components from a version string. See determine_version() for usage.
+function(extract_version_components version_string var_prefix)
+  string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)[-]*(.*)"
+    version_matches "${version_string}")
+  if(CMAKE_MATCH_0)
+    set(full ${CMAKE_MATCH_0})
+    set(major ${CMAKE_MATCH_1})
+    set(minor ${CMAKE_MATCH_2})
+    set(patch ${CMAKE_MATCH_3})
+    set(patch_extra ${CMAKE_MATCH_4})
+
+    set(${var_prefix}_VERSION "${major}.${minor}" PARENT_SCOPE)
+    set(${var_prefix}_VERSION_MAJOR ${major} PARENT_SCOPE)
+    set(${var_prefix}_VERSION_MINOR ${minor} PARENT_SCOPE)
+    set(${var_prefix}_VERSION_PATCH ${patch} PARENT_SCOPE)
+    set(${var_prefix}_VERSION_PATCH_EXTRA ${patch_extra} PARENT_SCOPE)
+    set(${var_prefix}_VERSION_FULL ${full} PARENT_SCOPE)
+    if("${major}.${minor}.${patch}" VERSION_EQUAL "${full}")
+      set(${var_prefix}_VERSION_IS_RELEASE TRUE PARENT_SCOPE)
+    else()
+      set(${var_prefix}_VERSION_IS_RELEASE FALSE PARENT_SCOPE)
+    endif()
   endif()
 endfunction()
