Kitware/ParaView

commit 4dfc084777d6f32a2bbfe1cfb6568fb93c5f96a3

Fixed flickering when Qt widgets were updated/changed
Re-enabled hiding the pqProxyWidget in pqProxyWidget::filterWidgets().
This fixes some flickering issues when switching between indexed color
mode a standard color mode.

Qt/Components/pqProxyWidget.cxx
@@ -1222,7 +1222,7 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
     show_advanced = true;
     }
 
-  // this->Panel->hide();
+  this->hide();
 
   delete this->layout();
   QVBoxLayout* vboxLayout = NULL;
@@ -1284,7 +1284,7 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
       }
     }
 
-  // this->Panel->show();
+  this->show();
   return (prevItem != NULL);
 }
 

commit 6cc919c6949f4ffbc2177e5ee95e4ab429e84e2b

Fixes BUG #15966.
Fixes regression introduced by 4dfc084. Do avoid flicker, the best way
is to disable updates, and not hide/show. Using show() at the end was
forcing the widget to show itself even when it was hidden by other code
namely pqPropertiesPanel.

Qt/Components/pqProxyWidget.cxx
@@ -1214,7 +1214,9 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
     show_advanced = true;
     }
 
-  this->hide();
+  // disable updates to avoid flicker
+  bool prevUE = this->updatesEnabled();
+  this->setUpdatesEnabled(false);
 
   delete this->layout();
   QVBoxLayout* vboxLayout = NULL;
@@ -1275,8 +1277,7 @@ bool pqProxyWidget::filterWidgets(bool show_advanced, const QString& filterText)
       item->hide();
       }
     }
-
-  this->show();
+  this->setUpdatesEnabled(prevUE);
   return (prevItem != NULL);
 }
 