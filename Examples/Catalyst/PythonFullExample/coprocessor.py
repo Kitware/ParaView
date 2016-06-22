b77faa4c0b2c569e34eb71a472f119505ff20b7f
Andrew Bauer <andy.bauer@kitware.com>
2016-06-22 08:45:24 -0400

    Fixing Examples/Catalyst/PythonFullExample when running with pvpython
    When running the example with pvpython or pvbatch we don't need to
    do some ParaView finalization stuff so these changes skip that
    finalization step. Also, some clean up as well.
    
    (cherry picked from commit a2b9decf275ef2b36037e72fbfa3397eac64748f)

--- Examples/Catalyst/PythonFullExample/coprocessor.py
+++ Examples/Catalyst/PythonFullExample/coprocessor.py
@@ -1,56 +1,57 @@
 coProcessor = None
-usecp = True
 
 def initialize():
-    global coProcessor, usecp
-    if usecp:
-        import paraview
-        import vtkParallelCorePython
-        import vtk
-        from mpi4py import MPI
-        import os, sys
+    global coProcessor
+    import paraview
+    import vtkParallelCorePython
+    import vtk
+    from mpi4py import MPI
+    import os, sys
 
-        paraview.options.batch = True
-        paraview.options.symmetric = True
-        import vtkPVClientServerCoreCorePython as CorePython
-        try:
-            import vtkPVServerManagerApplicationPython as ApplicationPython
-        except:
-            paraview.print_error("Error: Cannot import vtkPVServerManagerApplicationPython")
+    paraview.options.batch = True
+    paraview.options.symmetric = True
+    import vtkPVClientServerCoreCorePython as CorePython
+    try:
+        import vtkPVServerManagerApplicationPython as ApplicationPython
+    except:
+        paraview.print_error("Error: Cannot import vtkPVServerManagerApplicationPython")
 
-        if not CorePython.vtkProcessModule.GetProcessModule():
-            pvoptions = None
-            if paraview.options.batch:
-                pvoptions = CorePython.vtkPVOptions();
-                pvoptions.SetProcessType(CorePython.vtkPVOptions.PVBATCH)
-                if paraview.options.symmetric:
-                    pvoptions.SetSymmetricMPIMode(True)
-            ApplicationPython.vtkInitializationHelper.Initialize(sys.executable, CorePython.vtkProcessModule.PROCESS_BATCH, pvoptions)
+    if not CorePython.vtkProcessModule.GetProcessModule():
+        pvoptions = None
+        if paraview.options.batch:
+            pvoptions = CorePython.vtkPVOptions();
+            pvoptions.SetProcessType(CorePython.vtkPVOptions.PVBATCH)
+            if paraview.options.symmetric:
+                pvoptions.SetSymmetricMPIMode(True)
+        ApplicationPython.vtkInitializationHelper.Initialize(sys.executable, CorePython.vtkProcessModule.PROCESS_BATCH, pvoptions)
 
-        import paraview.servermanager as pvsm
-        # we need ParaView 4.2 since ParaView 4.1 doesn't properly wrap
-        # vtkPVPythonCatalystPython
-        if pvsm.vtkSMProxyManager.GetVersionMajor() < 4 or (pvsm.vtkSMProxyManager.GetVersionMajor() == 4 and pvsm.vtkSMProxyManager.GetVersionMinor() < 2):
-            print 'Must use ParaView v4.2 or greater'
-            sys.exit(0)
+    import paraview.servermanager as pvsm
+    # we need ParaView 4.2 since ParaView 4.1 doesn't properly wrap
+    # vtkPVPythonCatalystPython
+    if pvsm.vtkSMProxyManager.GetVersionMajor() < 4 or (pvsm.vtkSMProxyManager.GetVersionMajor() == 4 and pvsm.vtkSMProxyManager.GetVersionMinor() < 2):
+        print 'Must use ParaView v4.2 or greater'
+        sys.exit(0)
 
-        import numpy
-        import vtkPVCatalystPython as catalyst
-        import vtkPVPythonCatalystPython as pythoncatalyst
-        import paraview.simple
-        import paraview.vtk as vtk
-        from paraview import numpy_support
-        paraview.options.batch = True
-        paraview.options.symmetric = True
+    import numpy
+    import vtkPVCatalystPython as catalyst
+    import vtkPVPythonCatalystPython as pythoncatalyst
+    import paraview.simple
+    import paraview.vtk as vtk
+    from paraview import numpy_support
+    paraview.options.batch = True
+    paraview.options.symmetric = True
 
-        coProcessor = catalyst.vtkCPProcessor()
-        pm = paraview.servermanager.vtkProcessModule.GetProcessModule()
-        from mpi4py import MPI
+    coProcessor = catalyst.vtkCPProcessor()
+    pm = paraview.servermanager.vtkProcessModule.GetProcessModule()
+    from mpi4py import MPI
 
 def finalize():
-    global coProcessor, usecp
-    if usecp:
-        coProcessor.Finalize()
+    global coProcessor
+    coProcessor.Finalize()
+    # if we are running through Python we need to finalize extra stuff
+    # to avoid memory leak messages.
+    import sys, ntpath
+    if ntpath.basename(sys.executable) == 'python':
         import vtkPVServerManagerApplicationPython as ApplicationPython
         ApplicationPython.vtkInitializationHelper.Finalize()
 
