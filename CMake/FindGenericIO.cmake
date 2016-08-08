634ae3a36e1297f4c3dc35c09f6dcd634444c1e8
Ben Boeckel <ben.boeckel@kitware.com>
2016-08-08 09:58:29 -0400

    FindGenericIO: fall back to a config file

--- CMake/FindGenericIO.cmake
+++ CMake/FindGenericIO.cmake
@@ -5,17 +5,20 @@
 # GENERIC_IO_INCLUDE_DIR -- the include path for GenericIO
 # GENERIC_IO_LIBRARIES -- the GenericIO libraries to link against
 
-## Try to find include directory
-find_path(GENERIC_IO_INCLUDE_DIR
-            NAMES GenericIO.h
-            PATHS /usr/include /usr/local/include
-            )
+find_package(GenericIO CONFIG)
+if (NOT GenericIO_FOUND)
+  ## Try to find include directory
+  find_path(GENERIC_IO_INCLUDE_DIR
+              NAMES GenericIO.h
+              PATHS /usr/include /usr/local/include
+              )
 
-## Try to find the GenericIO library
-find_library(GENERIC_IO_LIBRARIES
-    NAMES GenericIO
-    PATHS /usr/lib
-    )
+  ## Try to find the GenericIO library
+  find_library(GENERIC_IO_LIBRARIES
+      NAMES GenericIO
+      PATHS /usr/lib
+      )
+endif ()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
