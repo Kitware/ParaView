9b3c84a3abf00289c5492621659c379dce53989b
Alvaro Sanchez <alvaro.sanchez@kitware.com>
2016-08-19 11:47:00 -0400

    Fixed test VolumeCellSelection.
    Changed test case to avoid transfer function/opacity variations in the volume by
    transforming the selection to polygons using the ExtractSelection filter.

---
+++ Applications/ParaView/Testing/Python/VolumeCellSelection.py
@@ -0,0 +1,42 @@
+from paraview.simple import *
+from paraview import smtesting
+import vtk
+from paraview.vtk.vtkCommonCore import vtkCollection;
+import vtk.vtkRenderingVolume
+import os
+
+paraview.simple._DisableFirstRenderCameraReset()
+
+smtesting.ProcessCommandLineArguments()
+smtesting.LoadServerManagerState(smtesting.StateXMLFileName)
+
+view = GetRenderView()
+view.ViewSize = [400, 400]
+view.RemoteRenderThreshold = 0
+SetActiveView(view)
+Render()
+
+# Select cells from a wavelet volume
+wav = FindSource('Wavelet1')
+SetActiveSource(wav)
+
+rep = vtkCollection()
+sources = vtkCollection()
+view.SelectSurfaceCells([0, 0, 200, 200], rep, sources)
+sel = sources.GetItemAsObject(0)
+selPyProxy = servermanager._getPyProxy(sel)
+extract = ExtractSelection(Selection = selPyProxy)
+Show()
+
+# Hide the volume and show the ExtractSelection filter
+wav_rep = GetRepresentation(wav)
+wav_rep.Visibility = False
+
+extract_rep = GetRepresentation(extract)
+extract_rep.Visibility = True
+
+## Compare with baseline
+if not smtesting.DoRegressionTesting(view.SMProxy):
+  raise smtesting.TestError, 'Test failed.'
+
+print '\nTest passed.'
