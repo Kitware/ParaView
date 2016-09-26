87ab4179aad276ce179c71ccced9030c65a909cd
Shawn Waldon <shawn.waldon@kitware.com>
2016-09-26 14:46:22 -0400

    Add a filter to create a ruler on its input
    This filter creates a line along one of the axis aligned bounding box's
    sides.  The user can select which axis the line is along.  The filter's
    XML specifies to use the ruler representation.

---
+++ ParaViewCore/VTKExtensions/Default/vtkRulerLineForInput.cxx
@@ -0,0 +1,138 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkRulerLineForInput.cxx
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+  Copyright 2013 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+  the U.S. Government retains certain rights in this software.
+
+=========================================================================*/
+#include "vtkRulerLineForInput.h"
+
+#include "vtkBoundingBox.h"
+#include "vtkCompositeDataIterator.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkLineSource.h"
+#include "vtkMath.h"
+#include "vtkMultiBlockDataSet.h"
+#include "vtkMultiProcessController.h"
+#include "vtkNew.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+
+vtkStandardNewMacro(vtkRulerLineForInput);
+vtkCxxSetObjectMacro(vtkRulerLineForInput, Controller, vtkMultiProcessController);
+
+vtkRulerLineForInput::vtkRulerLineForInput()
+  : Controller(NULL)
+  , Axis(0)
+{
+  this->SetController(vtkMultiProcessController::GetGlobalController());
+}
+
+vtkRulerLineForInput::~vtkRulerLineForInput()
+{
+  this->SetController(NULL);
+}
+
+void vtkRulerLineForInput::PrintSelf(ostream& os, vtkIndent indent)
+{
+}
+
+int vtkRulerLineForInput::FillInputPortInformation(int, vtkInformation* info)
+{
+  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+  info->Append(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkMultiBlockDataSet");
+  return 1;
+}
+
+int vtkRulerLineForInput::RequestInformation(
+  vtkInformation* request, vtkInformationVector** inVectors, vtkInformationVector* outVector)
+{
+  vtkInformation* outInfo = outVector->GetInformationObject(0);
+  outInfo->Set(CAN_HANDLE_PIECE_REQUEST(), 1);
+
+  return 1;
+}
+int vtkRulerLineForInput::RequestData(
+  vtkInformation* request, vtkInformationVector** inVectors, vtkInformationVector* outVector)
+{
+  vtkDataObject* inputData = vtkDataObject::GetData(inVectors[0], 0);
+  vtkBoundingBox bbox;
+  vtkDataSet* dataset;
+  if ((dataset = vtkDataSet::SafeDownCast(inputData)))
+  {
+    double bounds[6];
+    dataset->GetBounds(bounds);
+    bbox.AddBounds(bounds);
+  }
+  else
+  {
+    vtkMultiBlockDataSet* multiBlock = vtkMultiBlockDataSet::SafeDownCast(inputData);
+    assert(multiBlock);
+    vtkSmartPointer<vtkCompositeDataIterator> itr =
+      vtkSmartPointer<vtkCompositeDataIterator>::Take(multiBlock->NewIterator());
+    for (itr->InitTraversal(); !itr->IsDoneWithTraversal(); itr->GoToFirstItem())
+    {
+      vtkDataObject* block = itr->GetCurrentDataObject();
+      vtkDataSet* dataset = vtkDataSet::SafeDownCast(block);
+      if (dataset)
+      {
+        double tmpBounds[6];
+        dataset->GetBounds(tmpBounds);
+        bbox.AddBounds(tmpBounds);
+      }
+    }
+  }
+
+  double globalBounds[6];
+  if (this->Controller)
+  {
+    double processBounds[6];
+    bbox.GetBounds(processBounds);
+    for (int i = 0; i < 3; ++i)
+    {
+      processBounds[2 * i] *= -1;
+    }
+    this->Controller->Reduce(&processBounds[0], &globalBounds[0], 6, vtkCommunicator::MAX_OP, 0);
+    for (int i = 0; i < 3; ++i)
+    {
+      globalBounds[2 * i] *= -1;
+    }
+  }
+  else
+  {
+    bbox.GetBounds(globalBounds);
+  }
+
+  vtkInformation* outInfo = outVector->GetInformationObject(0);
+  if (outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_PIECE_NUMBER()) > 0)
+  {
+    return 1;
+  }
+
+  vtkNew<vtkLineSource> line;
+  line->SetPoint1(globalBounds[0], globalBounds[2], globalBounds[4]);
+  switch (this->Axis)
+  {
+    case 1:
+      line->SetPoint2(globalBounds[0], globalBounds[3], globalBounds[4]);
+      break;
+    case 2:
+      line->SetPoint2(globalBounds[0], globalBounds[2], globalBounds[5]);
+      break;
+    case 0:
+    default:
+      line->SetPoint2(globalBounds[1], globalBounds[2], globalBounds[4]);
+      break;
+  }
+  line->Update();
+  vtkPolyData* outputDataObject = vtkPolyData::GetData(outVector, 0);
+  outputDataObject->ShallowCopy(line->GetOutput());
+  return 1;
+}
