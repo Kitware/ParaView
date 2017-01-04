8d53bdae73b48b82bce9da873147c439dcf57dcc
Ken Martin <ken.martin@kitware.com>
2017-01-04 15:38:56 -0500

    allow eye dome lighting to be built with OpenGl2
    CMake changes to support EDL with OpenGL2

--- Plugins/EyeDomeLighting/plugin.cmake
+++ Plugins/EyeDomeLighting/plugin.cmake
@@ -1,7 +1,5 @@
-if(VTK_RENDERING_BACKEND STREQUAL "OpenGL")
-  pv_plugin(EyeDomeLighting
-    # provide a description for the plugin.
-    DESCRIPTION "Add 3D View with eye-dome Lighting support"
-    PLUGIN_NAMES EyeDomeLightingView
-    DEFAULT_ENABLED)
-endif()
+pv_plugin(EyeDomeLighting
+  # provide a description for the plugin.
+  DESCRIPTION "Add 3D View with eye-dome Lighting support"
+  PLUGIN_NAMES EyeDomeLightingView
+  DEFAULT_ENABLED)
