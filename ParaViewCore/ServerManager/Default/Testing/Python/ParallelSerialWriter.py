db6b479af261954b5aa4182aae0484da4c6fa20d
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2017-01-10 14:01:33 -0500

    Make batch tests more reliable.
    Using offscreen rendering for batch tests.

--- ParaViewCore/ServerManager/Default/Testing/Python/ParallelSerialWriter.py
+++ ParaViewCore/ServerManager/Default/Testing/Python/ParallelSerialWriter.py
@@ -26,22 +26,13 @@ if pm.GetSymmetricMPIMode() == True:
 reader = servermanager.sources.stlreader(FileNames=(fname,))
 
 view = servermanager.CreateRenderView();
+# using offscreen avoids issues with overlapping windows and such.
+view.UseOffscreenRendering = 1
 view.Background = (.5,.1,.5)
 if view.GetProperty("RemoteRenderThreshold"):
     view.RemoteRenderThreshold = 100;
 
 repr = servermanager.CreateRepresentation(reader, view);
-
-# view.UseOffscreenRenderingForScreenshots = 0
-# Hackery to ensure that we don't end up with overlapping windows when running
-# this test.
-try:
-    pm = servermanager.vtkProcessModule.GetProcessModule()
-    if pm.GetPartitionId() == 0:
-        window = view.GetRenderWindow()
-        window.SetPosition(450, 0)
-except:
-    pass
 view.StillRender()
 view.ResetCamera()
 view.StillRender()
--- ParaViewCore/ServerManager/Default/Testing/Python/Simple.py
+++ ParaViewCore/ServerManager/Default/Testing/Python/Simple.py
@@ -16,24 +16,15 @@ servermanager.Connect()
 sphere = servermanager.sources.SphereSource()
 
 view = servermanager.CreateRenderView();
+
+# using offscreen avoids issues with overlapping windows and such.
+view.UseOffscreenRendering = 1
 view.Background = (.5,.1,.5)
 if view.GetProperty("RemoteRenderThreshold"):
     view.RemoteRenderThreshold = 100;
 
 repr = servermanager.CreateRepresentation(sphere, view);
 repr.Input.append(sphere)
-
-# view.UseOffscreenRenderingForScreenshots = 0
-
-# Hackery to ensure that we don't end up with overlapping windows when running
-# this test.
-try:
-    pm = servermanager.vtkProcessModule.GetProcessModule()
-    if pm.GetPartitionId() == 0:
-        window = view.GetRenderWindow()
-        window.SetPosition(450, 0)
-except:
-    pass
 
 view.StillRender()
 view.ResetCamera()
