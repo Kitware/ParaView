7d0a4983bc8f14467c2db196e5fb6fbe53db6919
Shawn Waldon <shawn.waldon@kitware.com>
2017-01-25 10:30:32 -0500

    Update Catalyst editions to reflect moved files
    The last commit reorganized the files in ParaView so the Extras edition
    doesn't need to include the ParaViewCore/VTKExtensions/Rendering module.
    This commit is the changes to Catalyst that remove that dependency.

---
+++ Catalyst/Editions/Base/ParaViewCore/VTKExtensions/Core/Base-wrap-exclude-classes.catalyst.cmake
@@ -0,0 +1,12 @@
+set_source_files_properties(
+  vtkCommunicationErrorCatcher
+  vtkMultiProcessControllerHelper
+  vtkPVInformationKeys
+  vtkMemberFunctionCommand
+  WRAP_EXCLUDE
+  )
+
+set_source_files_properties(
+  vtkCommunicationErrorCatcher
+  vtkUndoElement
+  ABSTRACT)
