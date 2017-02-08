69706b83d70c61ca54e705fb1021e3e07682b32b
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2017-02-07 19:54:54 -0500

    Ignore warning message generated by potential Qt issue.
    Qt issue has been reported here:
    https://bugreports.qt.io/browse/QTBUG-58699

--- Utilities/TestDriver/vtkSMTestDriver.cxx
+++ Utilities/TestDriver/vtkSMTestDriver.cxx
@@ -568,6 +568,11 @@ int vtkSMTestDriver::OutputStringHasError(const char* pname, std::string& output
   const char* nonErrors[] = { "Memcheck, a memory error detector", // valgrind
     "error in locking authority file",                             // IceT
     "WARNING: Far depth failed sanity check, resetting.",          // IceT
+
+#if defined(__APPLE__)
+    // reported as https://bugreports.qt.io/browse/QTBUG-58699
+    "Layout still needs update after calling -[QNSPanelContentsWrapper layout]",
+#endif
     0 };
 
   if (this->AllowErrorInOutput)
