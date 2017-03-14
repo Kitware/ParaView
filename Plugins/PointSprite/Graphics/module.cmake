a873a65b01d4eb473e063ac49e6b0c572ea969c4
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2017-03-14 10:11:08 -0400

    Remove "PointSprite" plugin.
    PointSprite are supported natively with OpenGL2 backend. OpenGL1 backend
    is now on borrowed time hence these plugins no longer need to be
    supported.

--- Plugins/PointSprite/Graphics/module.cmake
+++
@@ -1,7 +0,0 @@
-vtk_module(vtkPointSpriteGraphics
-  DEPENDS
-    vtkFiltersCore
-  EXCLUDE_FROM_WRAP_HIERARCHY
-  TEST_LABELS
-    PARAVIEW
-)
--- Plugins/PointSprite/Qvis/module.cmake
+++
@@ -1,7 +0,0 @@
-if (PARAVIEW_BUILD_QT_GUI)
-  vtk_module(vtkQvis
-    DEPENDS
-      vtkCommonCore
-    EXCLUDE_FROM_WRAPPING
-    TEST_LABELS PARAVIEW)
-endif()
