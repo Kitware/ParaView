Kitware/ParaView

commit 77daa785a2ad493bb688657b3a332a6e6517aab8

Don't show inspector for non-composite datasets.
Don't show a hierarchy for non-composite datasets in
pqMultiBlockInspectorWidget.

Qt/Components/pqMultiBlockInspectorWidget.cxx
@@ -684,16 +684,26 @@ public:
     {
       this->CDTModel->setUserCheckable(false);
     }
-    this->CDTModel->reset(port != nullptr ? port->getDataInformation() : nullptr);
-    this->Ui.treeView->expandToDepth(1);
 
-    QHeaderView* header = this->Ui.treeView->header();
-    if (header->count() == 3 && header->logicalIndex(2) != 0)
+    bool is_composite =
+      this->CDTModel->reset(port != nullptr ? port->getDataInformation() : nullptr);
+    if (!is_composite)
     {
-      header->moveSection(0, 2);
+      this->Ui.treeView->setModel(nullptr);
     }
-    expandState.restore(this->Ui.treeView);
+    else
+    {
+      this->Ui.treeView->setModel(this->ProxyModel);
+      this->Ui.treeView->expandToDepth(1);
 
+      QHeaderView* header = this->Ui.treeView->header();
+      if (header->count() == 3 && header->logicalIndex(2) != 0)
+      {
+        header->moveSection(0, 2);
+      }
+    }
+
+    expandState.restore(this->Ui.treeView);
     this->SelectionModel->blockSelectionPropagation(prev);
   }
 

commit df1134a60cb331f112a097e880099f44793a5259

Bring back selection support to pqMultiBlockInspectorWidget.
Accidentally broke it in 77daa785. Also updated MultiBlockInspector test
to test for this case.

Qt/Components/pqMultiBlockInspectorWidget.cxx
@@ -740,6 +740,8 @@ public:
     else
     {
       this->Ui.treeView->setModel(this->ProxyModel);
+      // selection model need to be reset.
+      this->Ui.treeView->setSelectionModel(this->SelectionModel);
       this->Ui.treeView->expandToDepth(1);
 
       QHeaderView* header = this->Ui.treeView->header();

Applications/ParaView/Testing/XML/MultiBlockInspector.xml
@@ -1,5 +1,6 @@
 <?xml version="1.0" ?>
 <pqevents>
+  <pqevent object="pqClientMainWindow/menubar/menuTools" command="activate" arguments="actionTesting_Window_Size" />
   <pqevent object="pqClientMainWindow/menubar/menu_View" command="activate" arguments="Multi-block Inspector" />
   <pqevent object="pqClientMainWindow/MainControlsToolbar/actionOpenData" command="activate" arguments="" />
   <pqevent object="pqClientMainWindow/FileOpenDialog" command="filesSelected" arguments="$PARAVIEW_DATA_ROOT/can.ex2" />
@@ -28,10 +29,22 @@
   <pqevent object="pqClientMainWindow/multiBlockInspectorDock/multiBlockInspectorPanel/treeView" command="setCheckState" arguments="0.0.0.0.1.1,0" />
   <pqevent object="pqClientMainWindow/multiBlockInspectorDock/multiBlockInspectorPanel/treeView" command="activate" arguments="0.0.0.2" />
   <pqevent object="pqClientMainWindow/multiBlockInspectorDock/multiBlockInspectorPanel/treeView" command="setCheckState" arguments="0.0.0.0.1.0,0" />
+
+  <!-- change active source and back and select node
+       to make sure that selection linking works -->
   <pqevent object="pqClientMainWindow/centralwidget/MultiViewWidget/CoreWidget/qt_tabwidget_stackedwidget/MultiViewWidget1/Frame.0/actionClearSelection" command="activate" arguments="" />
+  <pqevent object="pqClientMainWindow/pipelineBrowserDock/pipelineBrowser" command="mousePress" arguments="1,1,0,43,12,/0:0" />
+  <pqevent object="pqClientMainWindow/pipelineBrowserDock/pipelineBrowser" command="mouseRelease" arguments="1,0,0,43,12,/0:0" />
+  <pqevent object="pqClientMainWindow/pipelineBrowserDock/pipelineBrowser" command="mousePress" arguments="1,1,0,41,12,/0:0/0:0" />
+  <pqevent object="pqClientMainWindow/pipelineBrowserDock/pipelineBrowser" command="mouseRelease" arguments="1,0,0,41,12,/0:0/0:0" />
+  <pqevent object="pqClientMainWindow/multiBlockInspectorDock/multiBlockInspectorPanel/treeView" command="setCurrent" arguments="0.0.0.0.0.0" />
+
+  <!-- since I don't want to compare baselines with selection highlighted
+       I'll apply an extract selection filter -->
+  <pqevent object="pqClientMainWindow/menubar" command="activate" arguments="menuFilters" />
+  <pqevent object="pqClientMainWindow/menubar/menuFilters/DataAnalysis" command="activate" arguments="ExtractSelection" />
+  <pqevent object="pqClientMainWindow/propertiesDock/propertiesPanel/Accept" command="activate" arguments="" />
   <pqevent object="pqClientMainWindow/variableToolbar/actionScalarBarVisibility" command="set_boolean" arguments="false" />
-  <pqevent object="pqClientMainWindow/menubar" command="activate" arguments="menuTools" />
-  <pqevent object="pqClientMainWindow/menubar/menuTools" command="activate" arguments="actionTesting_Window_Size" />
   <pqevent object="pqClientMainWindow/cameraToolbar/actionResetCamera" command="activate" arguments="" />
 
 </pqevents>