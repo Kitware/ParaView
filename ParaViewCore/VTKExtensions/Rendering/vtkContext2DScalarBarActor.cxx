80f6c75c0f0aae7abe75c2b1cfbbb2af4dc3d1a9
Cory Quammen <cory.quammen@kitware.com>
2017-05-22 19:07:49 -0400

    Label, tick, and title placement improvements
    Based on community feedback, tweak the tick marks to make them not
    appear to be negative signs in front of labels. Also, base the tick
    length and spacing between tick marks and labels on the label font
    size, and base the spacing between the title and labels on the title
    font size. These tweaks improve the aesthetics of the color legend.

--- ParaViewCore/VTKExtensions/Rendering/vtkContext2DScalarBarActor.cxx
+++ ParaViewCore/VTKExtensions/Rendering/vtkContext2DScalarBarActor.cxx
@@ -1,6 +1,7 @@
 #include "vtkContext2DScalarBarActor.h"
 
 #include "vtkAxis.h"
+#include "vtkBoundingRectContextDevice2D.h"
 #include "vtkBrush.h"
 #include "vtkColorTransferFunction.h"
 #include "vtkContext2D.h"
@@ -145,9 +146,6 @@ vtkContext2DScalarBarActor::vtkContext2DScalarBarActor()
 
   this->Axis = vtkAxis::New();
   this->Axis->SetScene(localScene);
-
-  // Add a little offset between labels and tick marks
-  this->Axis->SetLabelOffset(this->Axis->GetLabelOffset() + 2);
 }
 
 //----------------------------------------------------------------------------
@@ -762,20 +760,56 @@ void vtkContext2DScalarBarActor::PaintAxis(vtkContext2D* painter, double size[2]
 {
   vtkRectf rect = this->GetColorBarRect(size);
 
+  vtkWindow* renWin = this->CurrentViewport->GetVTKWindow();
+  int tileScale[2];
+  renWin->GetTileScale(tileScale);
+
+  // Use the length of the character "|" at the label font size for various
+  // measurements.
+  float bounds[4];
+  painter->ApplyTextProp(this->LabelTextProperty);
+  painter->ComputeStringBounds("|", bounds);
+  float pipeHeight = bounds[3];
+
+  // Compute a horizontal shift amount for tick marks when the orientation is
+  // vertical.
+  float axisShift = 0.25 * pipeHeight;
+
+  // Compute tick lengths and label offsets
+  float tickLength = pipeHeight;
+  if (this->Orientation == VTK_ORIENT_VERTICAL)
+  {
+    // Make the tick marks half the thickness of the color bar
+    this->Axis->SetTickLength(tickLength);
+
+    // Offset the labels from the tick marks a bit
+    float labelOffset = tickLength + (0.5 * tickLength);
+    this->Axis->SetLabelOffset(labelOffset);
+  }
+  else
+  {
+    tickLength = 0.70 * pipeHeight;
+    this->Axis->SetTickLength(tickLength);
+
+    float labelOffset = tickLength + (0.5 * tickLength);
+    this->Axis->SetLabelOffset(labelOffset);
+  }
+
+  // Position the axis
   if (this->TextPosition == PrecedeScalarBar)
   {
     // Left
     if (this->Orientation == VTK_ORIENT_VERTICAL)
     {
-      this->Axis->SetPoint1(rect.GetX() - 2, rect.GetY());
-      this->Axis->SetPoint2(rect.GetX() - 2, rect.GetY() + rect.GetHeight());
+      this->Axis->SetPoint1(rect.GetX() + axisShift, rect.GetY());
+      this->Axis->SetPoint2(rect.GetX() + axisShift, rect.GetY() + rect.GetHeight());
       this->Axis->SetPosition(vtkAxis::LEFT);
     }
     else
     {
       // Bottom
-      this->Axis->SetPoint1(rect.GetX(), rect.GetY());
-      this->Axis->SetPoint2(rect.GetX() + rect.GetWidth(), rect.GetY());
+      this->Axis->SetPoint1(rect.GetX(), rect.GetY() + axisShift);
+      this->Axis->SetPoint2(rect.GetX() + rect.GetWidth(), rect.GetY() + axisShift);
       this->Axis->SetPosition(vtkAxis::BOTTOM);
     }
   }
@@ -784,15 +818,17 @@ void vtkContext2DScalarBarActor::PaintAxis(vtkContext2D* painter, double size[2]
     // Right
     if (this->Orientation == VTK_ORIENT_VERTICAL)
     {
-      this->Axis->SetPoint1(rect.GetX() + rect.GetWidth() + 2, rect.GetY());
-      this->Axis->SetPoint2(rect.GetX() + rect.GetWidth() + 2, rect.GetY() + rect.GetHeight());
+      this->Axis->SetPoint1(rect.GetX() + rect.GetWidth() - axisShift, rect.GetY());
+      this->Axis->SetPoint2(
+        rect.GetX() + rect.GetWidth() - axisShift, rect.GetY() + rect.GetHeight());
       this->Axis->SetPosition(vtkAxis::RIGHT);
     }
     else
     {
       // Top
-      this->Axis->SetPoint1(rect.GetX(), rect.GetY() + rect.GetHeight());
-      this->Axis->SetPoint2(rect.GetX() + rect.GetWidth(), rect.GetY() + rect.GetHeight());
+      this->Axis->SetPoint1(rect.GetX(), rect.GetY() + rect.GetHeight() - axisShift);
+      this->Axis->SetPoint2(
+        rect.GetX() + rect.GetWidth(), rect.GetY() + rect.GetHeight() - axisShift);
       this->Axis->SetPosition(vtkAxis::TOP);
     }
   }
@@ -892,7 +928,9 @@ void vtkContext2DScalarBarActor::PaintTitle(vtkContext2D* painter, double size[2
   float titleWidth = titleBounds[2];
   float titleHeight = titleBounds[3];
 
-  vtkRectf axisRect = this->Axis->GetBoundingRect(painter);
+  // vtkAxis::GetBoundingRect() is not accurate.  Compute it ourselves.
+  vtkRectf axisRect =
+    vtkBoundingRectContextDevice2D::GetBoundingRect(this->Axis, this->CurrentViewport);
 
   vtkRectf rect = this->GetColorBarRect(size);
   float titleX = rect.GetX() + 0.5 * rect.GetWidth();
@@ -909,11 +947,11 @@ void vtkContext2DScalarBarActor::PaintTitle(vtkContext2D* painter, double size[2
     }
     if (this->GetTextPosition() == vtkContext2DScalarBarActor::PrecedeScalarBar)
     {
-      titleY = axisRect.GetY() - axisRect.GetHeight() - titleHeight;
+      titleY = axisRect.GetY() - titleHeight - 0.25 * titleHeight;
     }
     else
     {
-      titleY = axisRect.GetY() + axisRect.GetHeight() + rect.GetHeight();
+      titleY = axisRect.GetY() + axisRect.GetHeight() + 0.25 * titleHeight;
     }
   }
   else // Vertical orientation
