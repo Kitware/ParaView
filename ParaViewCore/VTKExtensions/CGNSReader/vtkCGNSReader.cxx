Kitware/ParaView

commit 2ac496945940599780a6f481b8112a26b69e2c90

Address CGNS build isssue with 32bit IDs.
A workaround to get vtkCGNSReader compile when VTK_USE_64BIT_IDS=0. It's
not very robust, but includes a warning to warn users about it.

Fixes #17299.

ParaViewCore/VTKExtensions/CGNSReader/vtkCGNSReader.cxx
@@ -1050,23 +1050,17 @@ int vtkCGNSReader::GetUnstructuredZone(
 {
   cgsize_t* zsize = reinterpret_cast<cgsize_t*>(v_zsize);
 
-  //========================================================================
-  // Test at compilation time with static assert ...
-  // In case  cgsize_t < vtkIdType one could try to start from the array end
-  // Next line is commented since static_assert requires c++11
-  // static_assert(!(sizeof(cgsize_t) > sizeof(vtkIdType)),
-  //              "Impossible to load data with sizeof cgsize_t bigger than sizeof vtkIdType\n");
-  typedef int static_assert_sizeOfvtkIdType[!(sizeof(cgsize_t) > sizeof(vtkIdType)) ? 1 : -1];
-  //=========================================================================
-  // TODO Warning at compilation time ??
-  const bool warningIdTypeSize = sizeof(cgsize_t) != sizeof(vtkIdType);
+  ////=========================================================================
+  const bool warningIdTypeSize = sizeof(cgsize_t) > sizeof(vtkIdType);
   if (warningIdTypeSize == true)
   {
-    vtkDebugMacro(<< "Warning cgsize_t do not have same size as vtkIdType\n"
-                  << "sizeof vtkIdType = " << sizeof(vtkIdType) << "\n"
-                  << "sizeof cgsize_t = " << sizeof(cgsize_t) << "\n");
+    vtkWarningMacro(<< "Warning cgsize_t is larger than the size as vtkIdType\n"
+                    << "  sizeof vtkIdType = " << sizeof(vtkIdType) << "\n"
+                    << "  sizeof cgsize_t = " << sizeof(cgsize_t) << "\n"
+                    << "This may cause unexpected issues. If so, please recompile with "
+                    << "VTK_USE_64BIT_IDS=ON.");
   }
-  //========================================================================
+  ////========================================================================
 
   int rind[6];
   // source layout
@@ -1102,7 +1096,16 @@ int vtkCGNSReader::GetUnstructuredZone(
   memDims[0] = zsize[0];
 
   // Compute number of points
+  if ((sizeof(vtkIdType) < sizeof(cgsize_t)) &&
+    (static_cast<cgsize_t>(vtkTypeTraits<vtkIdType>::Max()) < zsize[0]))
+  {
+    // overflow! cannot open the file in current configuration.
+    vtkErrorMacro("vtkIdType overflow. Please compile with VTK_USE_64BIT_IDS:BOOL=ON.");
+    return 1;
+  }
+
   nPts = static_cast<vtkIdType>(zsize[0]);
+  assert(nPts == zsize[0]);
 
   // Set up points
   vtkPoints* points = vtkPoints::New();
@@ -1297,6 +1300,12 @@ int vtkCGNSReader::GetUnstructuredZone(
     sizeSec.push_back(eDataSize);
     startSec.push_back(sectionInfoList[sec].range[0] - 1);
     elementCoreSize += (eDataSize);
+
+    if (static_cast<cgsize_t>(vtkTypeTraits<vtkIdType>::Max()) < (elementSize + numCoreCells))
+    {
+      vtkErrorMacro("vtkIdType overflow. Please compile with VTK_USE_64BIT_IDS:BOOL=ON.");
+      return 1;
+    }
     numCoreCells += elementSize;
     coreSec.push_back(sec);
     //

commit 2c9ab37cadd945ca716945e989fe2581c2eacd64

Fix IdType size mismatch with cgsize_t logic.
Address issue introduced by 2ac49694594059978.

ParaViewCore/VTKExtensions/CGNSReader/vtkCGNSReader.cxx
@@ -70,6 +70,16 @@ vtkStandardNewMacro(vtkCGNSReader);
 namespace
 {
 
+/**
+ * A quick function to check if vtkIdType can hold the value being
+ * saved into vtkIdType
+ */
+template <class T>
+bool IsIdTypeBigEnough(const T& val)
+{
+  return (sizeof(vtkIdType) >= sizeof(T) || static_cast<T>(vtkTypeTraits<vtkIdType>::Max()) >= val);
+}
+
 static const char* NO_FAMILY_TAG = "no-family";
 struct duo_t
 {
@@ -1504,8 +1514,7 @@ int vtkCGNSReader::GetUnstructuredZone(
   memDims[0] = zsize[0];
 
   // Compute number of points
-  if ((sizeof(vtkIdType) < sizeof(cgsize_t)) &&
-    (static_cast<cgsize_t>(vtkTypeTraits<vtkIdType>::Max()) < zsize[0]))
+  if (!IsIdTypeBigEnough(zsize[0]))
   {
     // overflow! cannot open the file in current configuration.
     vtkErrorMacro("vtkIdType overflow. Please compile with VTK_USE_64BIT_IDS:BOOL=ON.");
@@ -1709,7 +1718,7 @@ int vtkCGNSReader::GetUnstructuredZone(
     startSec.push_back(sectionInfoList[sec].range[0] - 1);
     elementCoreSize += (eDataSize);
 
-    if (static_cast<cgsize_t>(vtkTypeTraits<vtkIdType>::Max()) < (elementSize + numCoreCells))
+    if (!IsIdTypeBigEnough(elementSize + numCoreCells))
     {
       vtkErrorMacro("vtkIdType overflow. Please compile with VTK_USE_64BIT_IDS:BOOL=ON.");
       return 1;