8130dbf2744c56b099a8494be4340e5fd03197ff
Andrew Bauer <andy.bauer@kitware.com>
2018-01-11 12:09:05 -0500

    Adding a Catalyst volume rendering edition
    Volume rendering isn't available in the rendering-base edition. It
    now is added as a separate edition since it adds about 6 MB to a
    statically linked executable.

---
+++ Catalyst/Editions/Rendering-Volume/VTK/Imaging/Math/module.cmake
@@ -0,0 +1,12 @@
+vtk_module(vtkImagingMath
+  GROUPS
+    Imaging
+    StandAlone
+  KIT
+    vtkImaging
+  DEPENDS
+    vtkCommonExecutionModel
+  PRIVATE_DEPENDS
+    vtkCommonCore
+    vtkCommonDataModel
+    )
