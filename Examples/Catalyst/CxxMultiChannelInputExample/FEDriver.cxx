1849045d17c38cfb85b3441591e3aa67cd00b29a
Andrew Bauer <andy.bauer@kitware.com>
2018-01-25 10:24:21 -0500

    Adding in a Catalyst example with multiple channels/inputs
    Updated the README file with description of the changes too.

---
+++ Examples/Catalyst/CxxMultiChannelInputExample/FEDriver.cxx
@@ -0,0 +1,56 @@
+#include "FEDataStructures.h"
+#include <mpi.h>
+
+#ifdef USE_CATALYST
+#include "FEAdaptor.h"
+#endif
+
+// Example of a C++ adaptor for a simulation code
+// where the simulation code has a fixed topology
+// grid. We treat the grid as an unstructured
+// grid even though in the example provided it
+// would be best described as a vtkImageData.
+// Also, the points are stored in an inconsistent
+// manner with respect to the velocity vector.
+// This is purposefully done to demonstrate
+// the different approaches for getting data
+// into Catalyst. Note that through configuration
+// that the driver can be run without linking
+// to Catalyst.
+
+int main(int argc, char* argv[])
+{
+  MPI_Init(&argc, &argv);
+  unsigned int numPoints[3] = { 70, 60, 44 };
+  double spacing[3] = { 1, 1.1, 1.3 };
+  Grid grid(numPoints, spacing);
+  Attributes attributes;
+  attributes.Initialize(&grid);
+
+  size_t numParticlesPerProcess = 20;
+  Particles particles(grid, numParticlesPerProcess);
+
+#ifdef USE_CATALYST
+  // The first argument is the program name
+  FEAdaptor feaAdaptor(argc - 1, argv + 1);
+#endif
+  unsigned int numberOfTimeSteps = 100;
+  for (unsigned int timeStep = 0; timeStep < numberOfTimeSteps; timeStep++)
+  {
+    // use a time step length of 0.1
+    double time = timeStep * 0.1;
+    attributes.UpdateFields(time);
+    particles.Advect();
+#ifdef USE_CATALYST
+    feaAdaptor.CoProcess(
+      grid, attributes, particles, time, timeStep, timeStep == numberOfTimeSteps - 1);
+#endif
+  }
+
+#ifdef USE_CATALYST
+  feaAdaptor.Finalize();
+#endif
+  MPI_Finalize();
+
+  return 0;
+}
