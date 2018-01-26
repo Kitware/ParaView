c048f5109aca74a3ca65407f6c23549983605c9f
Andrew Bauer <andy.bauer@kitware.com>
2018-01-26 13:00:04 -0500

    Adding in a Catalyst example with ghost cells

---
+++ Examples/Catalyst/CxxGhostCellsExample/FEDataStructures.h
@@ -0,0 +1,22 @@
+#ifndef FEDATASTRUCTURES_HEADER
+#define FEDATASTRUCTURES_HEADER
+
+#include <vtkDataSet.h>
+#include <vtkSmartPointer.h>
+
+class Grid
+{
+public:
+  Grid(const unsigned int numberOfPoints[3], const bool outputImageData,
+    const int numberOfGhostLevels);
+
+  void UpdateField(double time);
+  vtkDataSet* GetVTKGrid();
+
+private:
+  void CreateImageData(int extent[6]);
+  void CreateUnstructuredGrid(int extent[6]);
+  vtkSmartPointer<vtkDataSet> VTKGrid;
+  int WholeExtent[6];
+};
+#endif
