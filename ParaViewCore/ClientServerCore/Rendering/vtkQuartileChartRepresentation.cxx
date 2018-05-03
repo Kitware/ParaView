376cff447c7cf71582781dbf7e9705fd8d8f99fe
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2018-05-03 13:06:09 -0400

    Add min/max lines to Quartile chart view.
    Fixes #18092.

--- ParaViewCore/ClientServerCore/Rendering/vtkQuartileChartRepresentation.cxx
+++ ParaViewCore/ClientServerCore/Rendering/vtkQuartileChartRepresentation.cxx
@@ -41,7 +41,7 @@ public:
   // Subclasses can override this method to assign a role for a specific data
   // array in the input dataset. This is useful when multiple plots are to be
   // created for a single series.
-  std::string GetSeriesRole(
+  std::vector<std::string> GetSeriesRoles(
     const std::string& vtkNotUsed(tableName), const std::string& columnName) override
   {
     if (StatsArrayRe.find(columnName))
@@ -49,15 +49,15 @@ public:
       const std::string fn = StatsArrayRe.match(1);
       if (fn == "min" || fn == "max")
       {
-        return "minmax";
+        return { std::string("minmax"), fn };
       }
       else if (fn == "q1" || fn == "q3")
       {
-        return "q1q3";
+        return { "q1q3" };
       }
-      return fn;
+      return { fn };
     }
-    return std::string();
+    return { std::string() };
   }
 
   vtkPlot* NewPlot(vtkXYChartRepresentation* self, const std::string& tableName,
@@ -72,7 +72,8 @@ public:
       }
       return plot;
     }
-    else if ((role == "avg") || (role == "med") || role.empty())
+    else if ((role == "avg") || (role == "med") || (role == "min") || (role == "max") ||
+      role.empty())
     {
       assert(self);
       vtkChartXY* chartXY = self->GetChart();
@@ -110,12 +111,16 @@ public:
   // Export visible plots to a CSV file.
   bool Export(vtkXYChartRepresentation* self, vtkCSVExporter* exporter) override
   {
+    // used to avoid adding duplicate columns.
+    std::set<std::string> added_column_names;
+
     for (PlotsMap::iterator iter1 = this->SeriesPlots.begin(); iter1 != this->SeriesPlots.end();
          ++iter1)
     {
       for (PlotsMapItem::const_iterator iter2 = iter1->second.begin(); iter2 != iter1->second.end();
            ++iter2)
       {
+        const std::string role = iter2->first;
         const PlotInfo& plotInfo = iter2->second;
         vtkPlot* plot = plotInfo.Plot;
         if (!plot->GetVisible())
@@ -127,43 +132,43 @@ public:
         vtkDataArray* xarray = self->GetUseIndexForXAxis()
           ? NULL
           : vtkDataArray::SafeDownCast(table->GetColumnByName(self->GetXAxisSeriesName()));
-        if (StatsArrayRe.find(columnName))
+
+        if (role == "minmax" || role == "q1q3")
         {
-          std::string fn = StatsArrayRe.match(1);
-          if (fn == "max" || fn == "q3")
+          // need to add two separate columns for these.
+          const std::string& tableName = plotInfo.TableName;
+          const std::string default_label = self->GetDefaultSeriesLabel(tableName, columnName);
+          const std::string label = this->GetSeriesParameter(
+            self, tableName, columnName, role, this->Labels, default_label);
+
+          std::vector<std::pair<std::string, std::string> > yNames;
+          if (role == "minmax")
           {
-            const std::string& tableName = plotInfo.TableName;
-            const std::string role = this->GetSeriesRole(tableName, columnName);
-            std::string default_label = self->GetDefaultSeriesLabel(tableName, columnName);
-            std::string label = this->GetSeriesParameter(
-              self, tableName, columnName, role, this->Labels, default_label);
-
-            std::vector<std::pair<std::string, std::string> > yNames;
-            yNames.push_back(std::make_pair(columnName, fn + " " + label));
-            if (fn == "max")
-            {
-              yNames.push_back(std::make_pair(columnName.replace(0, 3, "min"), "min " + label));
-            }
-            else
-            {
-              yNames.push_back(std::make_pair(columnName.replace(0, 2, "q1"), "q1 " + label));
-            }
-
-            for (auto iter3 = yNames.rbegin(); iter3 != yNames.rend(); ++iter3)
-            {
-              vtkAbstractArray* yarray = table->GetColumnByName(iter3->first.c_str());
-              if (yarray != NULL)
-              {
-                exporter->AddColumn(yarray, iter3->second.c_str(), xarray);
-              }
-            }
-            continue;
+            yNames.push_back(std::make_pair(columnName.replace(0, 3, "min"), "min " + label));
+            yNames.push_back(std::make_pair(columnName, "max " + label));
           }
+          else
+          {
+            yNames.push_back(std::make_pair(columnName.replace(0, 2, "q1"), "q1 " + label));
+            yNames.push_back(std::make_pair(columnName, "q3 " + label));
+          }
+
+          for (const auto& apair : yNames)
+          {
+            vtkAbstractArray* yarray = table->GetColumnByName(apair.first.c_str());
+            if (yarray != NULL && added_column_names.find(apair.second) == added_column_names.end())
+            {
+              added_column_names.insert(apair.second);
+              exporter->AddColumn(yarray, apair.second.c_str(), xarray);
+            }
+          }
+          continue;
         }
 
         vtkAbstractArray* yarray = table->GetColumnByName(columnName.c_str());
-        if (yarray != NULL)
+        if (yarray != NULL && added_column_names.find(plot->GetLabel()) == added_column_names.end())
         {
+          added_column_names.insert(plot->GetLabel());
           exporter->AddColumn(yarray, plot->GetLabel().c_str(), xarray);
         }
       }
@@ -179,7 +184,9 @@ protected:
     if ((role == "minmax" && (qcr->GetRangeVisibility() == false || qcr->HasOnlyOnePoint)) ||
       (role == "q1q3" && (qcr->GetQuartileVisibility() == false || qcr->HasOnlyOnePoint)) ||
       (role == "avg" && qcr->GetAverageVisibility() == false) ||
-      (role == "med" && (qcr->GetMedianVisibility() == false || qcr->HasOnlyOnePoint)))
+      (role == "med" && (qcr->GetMedianVisibility() == false || qcr->HasOnlyOnePoint)) ||
+      (role == "min" && (qcr->GetMinVisibility() == false || qcr->HasOnlyOnePoint)) ||
+      (role == "max" && (qcr->GetMaxVisibility() == false || qcr->HasOnlyOnePoint)))
     {
       plot->SetVisible(false);
       return false;
@@ -207,6 +214,16 @@ protected:
         plot->GetBrush()->SetOpacityF(0.3);
         plot->GetPen()->SetOpacityF(0.3);
       }
+      else if (role == "min")
+      {
+        plot->GetBrush()->SetOpacityF(0.3);
+        plot->GetPen()->SetOpacityF(0.3);
+        plot->GetPen()->SetLineType(vtkPen::DASH_LINE);
+      }
+      else if (role == "max")
+      {
+        plot->GetPen()->SetLineType(vtkPen::DASH_LINE);
+      }
       if (!qcr->HasOnlyOnePoint)
       {
         plot->SetLabel(role + " " + plot->GetLabel());
@@ -219,11 +236,13 @@ protected:
 vtkStandardNewMacro(vtkQuartileChartRepresentation);
 //----------------------------------------------------------------------------
 vtkQuartileChartRepresentation::vtkQuartileChartRepresentation()
-  : QuartileVisibility(true)
-  , RangeVisibility(true)
-  , AverageVisibility(true)
-  , MedianVisibility(true)
+  : AverageVisibility(true)
   , HasOnlyOnePoint(false)
+  , MaxVisibility(false)
+  , MedianVisibility(true)
+  , MinVisibility(false)
+  , QuartileVisibility(true)
+  , RangeVisibility(true)
 {
   delete this->Internals;
   this->Internals = new vtkQCRInternals();
@@ -280,8 +299,10 @@ void vtkQuartileChartRepresentation::PrepareForRendering()
 void vtkQuartileChartRepresentation::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
+  os << indent << "AverageVisibility: " << this->AverageVisibility << endl;
+  os << indent << "MaxVisibility: " << this->MinVisibility << endl;
+  os << indent << "MedianVisibility: " << this->MedianVisibility << endl;
+  os << indent << "MinVisibility: " << this->MaxVisibility << endl;
   os << indent << "QuartileVisibility: " << this->QuartileVisibility << endl;
   os << indent << "RangeVisibility: " << this->RangeVisibility << endl;
-  os << indent << "AverageVisibility: " << this->AverageVisibility << endl;
-  os << indent << "MedianVisibility: " << this->MedianVisibility << endl;
 }
