ca9214aab368ad9680cda2ba515256b81e6d4503
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2018-05-06 11:43:33 -0400

    pqTreeView: remove Qt 4 code.

--- Qt/Widgets/pqTreeView.cxx
+++ Qt/Widgets/pqTreeView.cxx
@@ -77,11 +77,7 @@ pqTreeView::pqTreeView(QWidget* widgetParent)
   checkable->setStretchLastSection(this->header()->stretchLastSection());
   this->setHeader(checkable);
   this->installEventFilter(checkable);
-#if QT_VERSION >= 0x050000
   checkable->setSectionsClickable(true);
-#else
-  checkable->setClickable(true);
-#endif
 
   // Listen for the show/hide events of the horizontal scroll bar.
   this->horizontalScrollBar()->installEventFilter(this);
