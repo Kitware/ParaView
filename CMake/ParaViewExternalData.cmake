5ab6f3ab54497ddbf98c0624c23c33fe1a8a1a59
Brad King <brad.king@kitware.com>
2018-05-18 13:22:05 -0400

    ExternalData: Update URLs for paraview.org to use https
    Using `http://` redirects to `https://`, so use the latter directly.

--- CMake/ParaViewExternalData.cmake
+++ CMake/ParaViewExternalData.cmake
@@ -60,7 +60,7 @@ list(APPEND ExternalData_URL_TEMPLATES
   "https://data.kitware.com/api/v1/file/hashsum/%(algo)/%(hash)/download"
 
   # Data published by developers using git-gitlab-push
-  "http://www.paraview.org/files/ExternalData/%(algo)/%(hash)"
+  "https://www.paraview.org/files/ExternalData/%(algo)/%(hash)"
   )
 
 # Tell ExternalData commands to transform raw files to content links.
