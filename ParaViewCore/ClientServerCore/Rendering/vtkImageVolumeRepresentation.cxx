Kitware/ParaView

commit 2352686f2e70d476aa00a125d5459444e2d4ce73

Add a new checkbox in Volume representation to avoid confusion with MapScalars
When volume representation is selected and **Map Scalars** and selected data
array has 2 or 4 components, a new property called **Mutliple Components Mapping**
is available.

When this new feature is enabled:
* for a two-components array, the first component is mapped to the color, the
  second component is mapped to the opacity.
* for a four-components array, the first, second and third components are used
  as RGB values and the fourth component is mapped to the opacity.

Note that:
* this feature is only available when **MapScalars** is *ON* ;
* this feature forces **Use Separate Color Map** ;
* it scales the color and the opacity range with the correct component.

ParaViewCore/ClientServerCore/Rendering/vtkImageVolumeRepresentation.cxx
@@ -466,6 +466,12 @@ void vtkImageVolumeRepresentation::SetIndependentComponents(bool val)
 }
 
 //----------------------------------------------------------------------------
+void vtkImageVolumeRepresentation::SetMultiComponentsMapping(bool val)
+{
+  this->Property->SetIndependentComponents(!val);
+}
+
+//----------------------------------------------------------------------------
 void vtkImageVolumeRepresentation::SetRequestedRenderMode(int mode)
 {
   this->VolumeMapper->SetRequestedRenderMode(mode);

ParaViewCore/ClientServerCore/Rendering/vtkImageVolumeRepresentation.h
@@ -95,6 +95,7 @@ public:
   void SetSpecularPower(double);
   void SetShade(bool);
   void SetIndependentComponents(bool);
+  void SetMultiComponentsMapping(bool);
 
   //@{
   /**

ParaViewCore/ServerManager/SMApplication/Resources/views_and_representations.xml
@@ -3693,6 +3693,8 @@
           <PropertyGroup label="Scalar Coloring">
             <Property name="MapScalars"
                       panel_visibility="advanced" />
+            <Property name="MultiComponentsMapping"
+                      panel_visibility="advanced" />
             <Property name="InterpolateScalarsBeforeMapping"
                       panel_visibility="advanced" />
           </PropertyGroup>
@@ -4490,13 +4492,15 @@
         <BooleanDomain name="bool" />
         <Documentation>Enable/Disable shading.</Documentation>
       </IntVectorProperty>
-      <IntVectorProperty command="SetIndependentComponents"
-                         default_values="1"
-                         name="MapScalars"
+      <IntVectorProperty command="SetMultiComponentsMapping"
+                         default_values="0"
+                         name="MultiComponentsMapping"
                          number_of_elements="1">
         <BooleanDomain name="bool" />
-        <Documentation>Enable/Disable independent component usage such a
-        unsigned char RGBA data array.</Documentation>
+        <Hints>
+          <PropertyWidgetDecorator type="MultiComponentsDecorator"
+                                   components="2 4" />
+        </Hints>
       </IntVectorProperty>
       <ProxyProperty command="SetScalarOpacity"
                      name="ScalarOpacityFunction" >
@@ -4664,6 +4668,7 @@
         floating point type) and then directly used as
         color.</Documentation>
       </IntVectorProperty>
+
       <ProxyProperty command="SetLookupTable"
                      name="LookupTable"
                      skip_dependency="1">
@@ -5308,13 +5313,14 @@
                          name="MapScalars"
                          number_of_elements="1">
         <BooleanDomain name="bool" />
-        <Documentation>When set to true, LookupTable will always be
+        <Documentation>When set to True, LookupTable will always be
         used for scalar mapping. Otherwise, when up to 4 component
         scalars are present, the components are clamped to a valid
         color interval (0-255 for an integral type and 0.0-1.0 for a
         floating point type) and then directly used as
         color.</Documentation>
       </IntVectorProperty>
+
       <ProxyProperty command="SetLookupTable"
                      name="LookupTable" >
         <Documentation>Set the lookup table to use for scalar
@@ -11117,12 +11123,35 @@
                          name="MapScalars"
                          number_of_elements="1">
         <BooleanDomain name="bool" />
-        <Documentation>When set to True, LookupTable will always be
+        <Documentation>When enabled, LookupTable will always be
         used for scalar mapping. Otherwise, when up to 4 component
         scalars are present, the components are clamped to a valid
         color interval (0-255 for an integral type and 0.0-1.0 for a
         floating point type) and then directly used as
         color.</Documentation>
+      </IntVectorProperty>
+      <IntVectorProperty default_values="0"
+                         name="MultiComponentsMapping"
+                         number_of_elements="1">
+        <BooleanDomain name="bool" />
+        <Documentation>Enable multi components mapping. For a two-components array,
+        the first component is mapped to the color, the second component is mapped to the
+        opacity. For a four-components array, the first, second and third components are used as
+        RGB values and the fourth component is mapped to the opacity. Only available
+        when MapScalars is ON. This feature forces "Separate Color Map" and scales the color and
+        the opacity range with the correct component.</Documentation>
+        <Hints>
+          <PropertyWidgetDecorator type="GenericDecorator"
+                                   mode="visibility"
+                                   property="Representation"
+                                   value="Volume" />
+          <PropertyWidgetDecorator type="GenericDecorator"
+                                   mode="visibility"
+                                   property="MapScalars"
+                                   value="1" />
+          <PropertyWidgetDecorator type="MultiComponentsDecorator"
+                                   components="2 4" />
+        </Hints>
       </IntVectorProperty>
       <IntVectorProperty command="SetInterpolateScalarsBeforeMapping"
                          default_values="1"

Applications/ParaView/Testing/XML/CMakeLists.txt
@@ -81,6 +81,7 @@ paraview_test_load_data(""
   tube.exo
   vehicle_data.csv
   viscoplastic-ring.h5
+  waveletElevation.vti
   waveletMaterial.vti
   )
 
@@ -450,6 +451,7 @@ list(APPEND TESTS_WITH_BASELINES
   ${CMAKE_CURRENT_SOURCE_DIR}/UndoRedo8.xml
   ${CMAKE_CURRENT_SOURCE_DIR}/UnstructuredOutline.xml
   ${CMAKE_CURRENT_SOURCE_DIR}/VolumeRenderingWithContour.xml
+  ${CMAKE_CURRENT_SOURCE_DIR}/VolumeReprTwoIndepComp.xml
   ${CMAKE_CURRENT_SOURCE_DIR}/VariableSelector.xml
   ${CMAKE_CURRENT_SOURCE_DIR}/VariableSelector1.xml
   ${CMAKE_CURRENT_SOURCE_DIR}/ViewSettingsDialog.xml
@@ -723,6 +725,7 @@ set (LoadSaveStateVolume_FORCE_SERIAL TRUE)
 # We don't support volume rendering of image data in data server & render server mode
 set(LoadSaveStateVolume_DISABLE_CRS TRUE)
 set(SeparatedColorMapOpacity_DISABLE_CRS TRUE)
+set(VolumeReprTwoIndepComp_DISABLE_CRS TRUE)
 
 # Set properties for CTH tests
 set(CTHAMRContour_DISABLE_CS TRUE)

Applications/ParaView/Testing/XML/VolumeReprTwoIndepComp.xml
@@ -1,0 +1,11 @@
+<?xml version="1.0" ?>
+<pqevents>
+  <pqevent object="pqClientMainWindow/MainControlsToolbar/actionOpenData" command="activate" arguments="" />
+  <pqevent object="pqClientMainWindow/FileOpenDialog" command="filesSelected" arguments="$PARAVIEW_DATA_ROOT/waveletElevation.vti" />
+  <pqevent object="pqClientMainWindow/propertiesDock/propertiesPanel/Accept" command="activate" arguments="" />
+  <pqevent object="pqClientMainWindow/outputWidgetDock/qt_dockwidget_closebutton" command="activate" arguments="" />
+  <pqevent object="pqClientMainWindow/propertiesDock/propertiesPanel/SearchBox/AdvancedButton" command="set_boolean" arguments="true" />
+  <pqevent object="pqClientMainWindow/representationToolbar/displayRepresentation/comboBox" command="activated" arguments="Volume" />
+  <pqevent object="pqClientMainWindow/propertiesDock/propertiesPanel/scrollArea/qt_scrollarea_viewport/scrollAreaWidgetContents/DisplayFrame/ProxyPanel/MultiComponentsMapping/CheckBox" command="set_boolean" arguments="true" />
+  <pqevent object="pqClientMainWindow/cameraToolbar/actionPositiveX" command="activate" arguments="" />
+</pqevents>

ParaViewCore/ServerManager/Rendering/vtkSMPVRepresentationProxy.cxx
@@ -351,31 +351,42 @@ bool vtkSMPVRepresentationProxy::RescaleTransferFunctionToDataRange(
     }
     else
     {
-      double range[2];
-      info->GetComponentFiniteRange(component, range);
-      if (range[1] >= range[0])
-      {
-        // the range must be large enough, compared to values order of magnitude
-        // If data range is too small then we tweak it a bit so scalar mapping
-        // produces valid/reproducible results.
-        vtkSMCoreUtilities::AdjustRange(range);
-        if (lut)
-        {
-          vtkSMTransferFunctionProxy::RescaleTransferFunction(lut, range, extend);
-          vtkSMProxy* sof_lut =
-            vtkSMPropertyHelper(lut, "ScalarOpacityFunction", true).GetAsProxy();
-          if (sof_lut && sof != sof_lut)
-          {
-            vtkSMTransferFunctionProxy::RescaleTransferFunction(sof_lut, range, extend);
-          }
-        }
-        if (sof)
-        {
-          vtkSMTransferFunctionProxy::RescaleTransferFunction(sof, range, extend);
-        }
+      double rangeColor[2];
+      double rangeOpacity[2];
 
-        return (lut || sof);
+      if (this->GetVolumeIndependentRanges())
+      {
+        info->GetComponentFiniteRange(0, rangeColor);
+        info->GetComponentFiniteRange(1, rangeOpacity);
       }
+      else
+      {
+        info->GetComponentFiniteRange(component, rangeColor);
+        rangeOpacity[0] = rangeColor[0];
+        rangeOpacity[1] = rangeColor[1];
+      }
+
+      // the range must be large enough, compared to values order of magnitude
+      // If data range is too small then we tweak it a bit so scalar mapping
+      // produces valid/reproducible results.
+      vtkSMCoreUtilities::AdjustRange(rangeColor);
+      vtkSMCoreUtilities::AdjustRange(rangeOpacity);
+
+      if (lut && rangeColor[1] >= rangeColor[0])
+      {
+        vtkSMTransferFunctionProxy::RescaleTransferFunction(lut, rangeColor, extend);
+        vtkSMProxy* sof_lut = vtkSMPropertyHelper(lut, "ScalarOpacityFunction", true).GetAsProxy();
+        if (sof_lut && sof != sof_lut && rangeOpacity[1] >= rangeOpacity[0])
+        {
+          vtkSMTransferFunctionProxy::RescaleTransferFunction(sof_lut, rangeOpacity, extend);
+        }
+      }
+
+      if (sof && rangeOpacity[1] >= rangeOpacity[0])
+      {
+        vtkSMTransferFunctionProxy::RescaleTransferFunction(sof, rangeOpacity, extend);
+      }
+      return (lut || sof);
     }
   }
   return false;
@@ -899,4 +910,21 @@ int vtkSMPVRepresentationProxy::GetEstimatedNumberOfAnnotationsOnScalarBar(vtkSM
 
   sbProxy->UpdatePropertyInformation();
   return vtkSMPropertyHelper(sbProxy, "EstimatedNumberOfAnnotations").GetAsInt();
+}
+
+//----------------------------------------------------------------------------
+bool vtkSMPVRepresentationProxy::GetVolumeIndependentRanges()
+{
+  // the representation is Volume
+  vtkSMProperty* repProperty = this->GetProperty("Representation");
+  if (strcmp(vtkSMPropertyHelper(repProperty).GetAsString(), "Volume") != 0)
+  {
+    return false;
+  }
+
+  // MapScalars and MultiComponentsMapping are checked
+  vtkSMProperty* msProperty = this->GetProperty("MapScalars");
+  vtkSMProperty* mcmProperty = this->GetProperty("MultiComponentsMapping");
+  return (vtkSMPropertyHelper(msProperty).GetAsInt() != 0 &&
+    vtkSMPropertyHelper(mcmProperty).GetAsInt() != 0);
 }

ParaViewCore/ServerManager/Rendering/vtkSMPVRepresentationProxy.h
@@ -327,6 +327,12 @@ public:
    */
   bool SetRepresentationType(const char* type) VTK_OVERRIDE;
 
+  /**
+   * True if ranges have to be computed independently on component 0 for the color
+   * and 1 for the opacity on the Volume representation.
+   */
+  bool GetVolumeIndependentRanges();
+
 protected:
   vtkSMPVRepresentationProxy();
   ~vtkSMPVRepresentationProxy() override;

Qt/ApplicationComponents/CMakeLists.txt
@@ -206,6 +206,8 @@ set (Module_SRCS
   pqMasterOnlyReaction.h
   pqModelTransformSupportBehavior.cxx
   pqModelTransformSupportBehavior.h
+  pqMultiComponentsDecorator.cxx
+  pqMultiComponentsDecorator.h
   pqObjectPickingBehavior.cxx
   pqObjectPickingBehavior.h
   pqOpacityTableModel.cxx
@@ -423,6 +425,7 @@ set (Module_MOC_HDRS
   pqManagePluginsReaction.h
   pqMasterOnlyReaction.h
   pqModelTransformSupportBehavior.h
+  pqMultiComponentsDecorator.h
   pqObjectPickingBehavior.h
   pqOpacityTableModel.h
   pqOSPRayHidingDecorator.h

commit ac9e346713ede383340b11e9d4be8b5e1ddf5a93

better support direct rendering of arrays for volumes.
2352686f broke support for unchecking MapScalar to directly render color
arrays when volume rendering. Fixed that.

Fixes #17862.

ParaViewCore/ClientServerCore/Rendering/vtkImageVolumeRepresentation.cxx
@@ -130,6 +130,9 @@ vtkImageVolumeRepresentation::vtkImageVolumeRepresentation()
   this->Spacing[0] = this->Spacing[1] = this->Spacing[2] = 0;
   this->WholeExtent[0] = this->WholeExtent[2] = this->WholeExtent[4] = 0;
   this->WholeExtent[1] = this->WholeExtent[3] = this->WholeExtent[5] = -1;
+
+  this->MapScalars = true;
+  this->MultiComponentsMapping = false;
 }
 
 //----------------------------------------------------------------------------
@@ -342,6 +345,22 @@ void vtkImageVolumeRepresentation::UpdateMapperParameters()
 
   if (this->Property)
   {
+    if (this->MapScalars)
+    {
+      if (this->MultiComponentsMapping)
+      {
+        this->Property->SetIndependentComponents(false);
+      }
+      else
+      {
+        this->Property->SetIndependentComponents(true);
+      }
+    }
+    else
+    {
+      this->Property->SetIndependentComponents(false);
+    }
+
     // Update the mapper's vector mode
     vtkColorTransferFunction* ctf = this->Property->GetRGBTransferFunction(0);
 
@@ -460,15 +479,21 @@ void vtkImageVolumeRepresentation::SetShade(bool val)
 }
 
 //----------------------------------------------------------------------------
-void vtkImageVolumeRepresentation::SetIndependentComponents(bool val)
+void vtkImageVolumeRepresentation::SetMapScalars(bool val)
 {
-  this->Property->SetIndependentComponents(val);
+  this->MapScalars = val;
+  // the value is passed on to the vtkVolumeProperty in UpdateMapperParameters
+  // since SetMapScalars and SetMultiComponentsMapping both control the same
+  // vtkVolumeProperty ivar i.e. IndependentComponents.
 }
 
 //----------------------------------------------------------------------------
 void vtkImageVolumeRepresentation::SetMultiComponentsMapping(bool val)
 {
-  this->Property->SetIndependentComponents(!val);
+  this->MultiComponentsMapping = val;
+  // the value is passed on to the vtkVolumeProperty in UpdateMapperParameters
+  // since SetMapScalars and SetMultiComponentsMapping both control the same
+  // vtkVolumeProperty ivar i.e. IndependentComponents.
 }
 
 //----------------------------------------------------------------------------

ParaViewCore/ClientServerCore/Rendering/vtkImageVolumeRepresentation.h
@@ -94,7 +94,7 @@ public:
   void SetSpecular(double);
   void SetSpecularPower(double);
   void SetShade(bool);
-  void SetIndependentComponents(bool);
+  void SetMapScalars(bool);
   void SetMultiComponentsMapping(bool);
 
   //@{
@@ -174,6 +174,9 @@ protected:
   double Origin[3];
   double Spacing[3];
   int WholeExtent[6];
+
+  bool MapScalars;
+  bool MultiComponentsMapping;
 
 private:
   vtkImageVolumeRepresentation(const vtkImageVolumeRepresentation&) = delete;

ParaViewCore/ServerManager/SMApplication/Resources/views_and_representations.xml
@@ -4525,6 +4525,18 @@
         <BooleanDomain name="bool" />
         <Documentation>Enable/Disable shading.</Documentation>
       </IntVectorProperty>
+      <IntVectorProperty command="SetMapScalars"
+                         default_values="1"
+                         name="MapScalars"
+                         number_of_elements="1">
+        <BooleanDomain name="bool" />
+        <Documentation>When enabled, LookupTable will always be
+        used for scalar mapping. Otherwise, when up to 4 component
+        scalars are present, the components are clamped to a valid
+        color interval (0-255 for an integral type and 0.0-1.0 for a
+        floating point type) and then directly used as
+        color.</Documentation>
+      </IntVectorProperty>
       <IntVectorProperty command="SetMultiComponentsMapping"
                          default_values="0"
                          name="MultiComponentsMapping"