84d85b6c0f392864a2c54338a672ea57e60a914a
David E. DeMarle <dave.demarle@kitware.com>
2018-08-22 17:33:47 -0400

    minor cleanup to use a class instead of a weird struct

--- Wrapping/Python/paraview/cpstate.py
+++ Wrapping/Python/paraview/cpstate.py
@@ -325,83 +325,79 @@ class cpstate_filter_proxies_to_serialize(object):
 
 # -----------------------------------------------------------------------------
 class NewStyleWriters(object):
-  """Helper to dump configured writer proxies, which are not n the pipeline,
-  into the script."""
-  __cnt = 1
+    """Helper to dump configured writer proxies, which are not in the pipeline,
+    into the script."""
 
-  @classmethod
-  def reset(cls):
-      cls.__cnt = 1
+    def __init__(self):
+        self.__cnt = 1
 
-  @classmethod
-  def make_name(cls, name):
-    """
-    emulating name uniqueness that trace brings to variable names.
-    This may not be necessary because we register everything we make immediately
-    so var names can probably conflict.
-    """
-    ret = smtrace.Trace.get_varname(name)+str(cls.__cnt)
-    cls.__cnt = cls.__cnt + 1
-    return ret
+    def __make_name(self, name):
+        """
+        emulating name uniqueness that trace brings to variable names.
+        This may not be necessary because we register everything we make immediately
+        so var names can probably conflict.
+        """
+        ret = smtrace.Trace.get_varname(name)+str(self.__cnt)
+        self.__cnt = self.__cnt + 1
+        return ret
 
-  @classmethod
-  def make_trace(cls):
-    """gather trace for the writer proxies that are not in the trace pipeline but
-    rather in the new export state.
+    def make_trace(self):
+        """gather trace for the writer proxies that are not in the trace pipeline but
+        rather in the new export state.
 
-    aDIOSWriter1 = servermanager.writers.ADIOSWriter(Input=wavelet1)
-    coprocessor.RegisterWriter(aDIOSWriter1, filename='filename.vta', freq=1, paddingamount=0)
-    """
-    res = []
-    res.append("")
-    res.append("# Now any catalyst writers")
-    pxm = servermanager.ProxyManager()
-    globalepxy = pxm.GetProxy("export_global", "catalyst")
-    exports = pxm.GetProxiesInGroup("export_writers") #todo should use ExportDepot
-    for x in exports:
-        xs = x[0]
-        pxy = pxm.GetProxy('export_writers', xs)
-        if not pxy.HasAnnotation('enabled'):
-            continue
-
-        xmlname = pxy.GetXMLName()
-        if xmlname == "Cinema image options":
-            # skip the array and property export information we stuff in this proxy
-            continue
-
-        inputname = xs.split('_')[0].lower()
-        writername = xs.split('_')[1]
-
-        xmlgroup = pxy.GetXMLGroup()
-
-        padding_amount = globalepxy.ZeroPadding
-        write_frequency = pxy.GetProperty("WriteFrequency").GetElement(0)
-        filename = pxy.GetProperty("CatalystFilePattern").GetElement(0)
-
-
-        sim_inputs = locate_simulation_inputs(pxy)
-        for sim_input_name in sim_inputs:
-            if not write_frequency in cpstate_globals.write_frequencies[sim_input_name]:
-                cpstate_globals.write_frequencies[sim_input_name].append(write_frequency)
-                cpstate_globals.write_frequencies[sim_input_name].sort()
-
-            if not sim_input_name in cpstate_globals.channels_needed:
-                cpstate_globals.channels_needed.append(sim_input_name)
-
-        prototype = pxm.GetPrototypeProxy(xmlgroup, xmlname)
-        if not prototype:
-            varname = cls.make_name(xmlname)
-        else:
-            varname = cls.make_name(prototype.GetXMLLabel())
-        f = "%s = servermanager.writers.%s(Input=%s)" % (varname, writername, inputname)
-        res.append(f)
-        f = "coprocessor.RegisterWriter(%s, filename='%s', freq=%s, paddingamount=%s)" % (
-            varname, filename, write_frequency, padding_amount)
-        res.append(f)
+        aDIOSWriter1 = servermanager.writers.ADIOSWriter(Input=wavelet1)
+        coprocessor.RegisterWriter(aDIOSWriter1, filename='filename.vta', freq=1, paddingamount=0)
+        """
+        res = []
         res.append("")
-    if len(res) == 2:
-        return [] # don't clutter output if there are no writers
-    return res
+        res.append("# Now any catalyst writers")
+        pxm = servermanager.ProxyManager()
+        globalepxy = pxm.GetProxy("export_global", "catalyst")
+        exports = pxm.GetProxiesInGroup("export_writers") #todo should use ExportDepot
+        for x in exports:
+            xs = x[0]
+            pxy = pxm.GetProxy('export_writers', xs)
+            if not pxy.HasAnnotation('enabled'):
+                continue
+
+            xmlname = pxy.GetXMLName()
+            if xmlname == "Cinema image options":
+                # skip the array and property export information we stuff in this proxy
+                continue
+
+            inputname = xs.split('_')[0].lower()
+            writername = xs.split('_')[1]
+
+            xmlgroup = pxy.GetXMLGroup()
+
+            padding_amount = globalepxy.ZeroPadding
+            write_frequency = pxy.GetProperty("WriteFrequency").GetElement(0)
+            filename = pxy.GetProperty("CatalystFilePattern").GetElement(0)
+
+
+            sim_inputs = locate_simulation_inputs(pxy)
+            for sim_input_name in sim_inputs:
+                if not write_frequency in cpstate_globals.write_frequencies[sim_input_name]:
+                    cpstate_globals.write_frequencies[sim_input_name].append(write_frequency)
+                    cpstate_globals.write_frequencies[sim_input_name].sort()
+
+                if not sim_input_name in cpstate_globals.channels_needed:
+                    cpstate_globals.channels_needed.append(sim_input_name)
+
+            prototype = pxm.GetPrototypeProxy(xmlgroup, xmlname)
+            if not prototype:
+                varname = self.__make_name(xmlname)
+            else:
+                varname = self.__make_name(prototype.GetXMLLabel())
+            f = "%s = servermanager.writers.%s(Input=%s)" % (varname, writername, inputname)
+            res.append(f)
+            f = "coprocessor.RegisterWriter(%s, filename='%s', freq=%s, paddingamount=%s)" % (
+                varname, filename, write_frequency, padding_amount)
+            res.append(f)
+            res.append("")
+        if len(res) == 2:
+            return [] # don't clutter output if there are no writers
+        return res
 
 # -----------------------------------------------------------------------------
 def DumpPipeline(export_rendering, simulation_input_map, screenshot_info,
@@ -474,8 +470,7 @@ def DumpPipeline(export_rendering, simulation_input_map, screenshot_info,
     smtrace.RealProxyAccessor.unregister_create_callback(cp_hook)
 
     # add in the new style writer proxies
-    NewStyleWriters.reset()
-    state = state + NewStyleWriters.make_trace()
+    state = state + NewStyleWriters().make_trace()
 
     # iterate over all views that were saved in state and update write requencies
     if export_rendering:
