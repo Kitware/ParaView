6e946bec8fdf307b507884e515b84b49dcd0618e
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2018-09-17 12:03:08 -0400

    update pqSampleScalarAddRangeDialog to use pqDoubleLineEdit.

--- Qt/Components/pqSampleScalarAddRangeDialog.h
+++ Qt/Components/pqSampleScalarAddRangeDialog.h
@@ -29,9 +29,8 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
-
-#ifndef _pqSampleScalarAddRangeDialog_h
-#define _pqSampleScalarAddRangeDialog_h
+#ifndef pqSampleScalarAddRangeDialog_h
+#define pqSampleScalarAddRangeDialog_h
 
 #include "pqComponentsModule.h"
 
