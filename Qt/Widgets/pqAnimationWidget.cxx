Kitware/ParaView

commit 9f34821f6e861b693a19207d92cc0281d5e92088

pqAnimationWidget: resize widget's contents in `resizeEvent`
pqAnimationWidget was not resizing headers in `resizeEvent`. Fixed that.
Fixes #18305.

Qt/Widgets/pqAnimationWidget.cxx
@@ -44,7 +44,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "pqAnimationTrack.h"
 
 pqAnimationWidget::pqAnimationWidget(QWidget* p)
-  : QAbstractScrollArea(p)
+  : Superclass(p)
 {
   this->View = new QGraphicsView(this->viewport());
   this->viewport()->setBackgroundRole(QPalette::Window);
@@ -217,7 +217,7 @@ void pqAnimationWidget::scrollContentsBy(int dx, int dy)
     this->EnabledHeader->setOffset(this->verticalScrollBar()->value());
   }
   this->updateWidgetPosition();
-  QAbstractScrollArea::scrollContentsBy(dx, dy);
+  this->Superclass::scrollContentsBy(dx, dy);
 }
 
 void pqAnimationWidget::updateScrollBars()
@@ -275,17 +275,13 @@ bool pqAnimationWidget::event(QEvent* e)
   {
     this->Model->setRowHeight(this->Header->defaultSectionSize());
   }
-  if (e->type() == QEvent::Show)
-  {
-    this->updateGeometries();
-  }
-  return QAbstractScrollArea::event(e);
+  return this->Superclass::event(e);
 }
 
 void pqAnimationWidget::resizeEvent(QResizeEvent* e)
 {
-  this->updateScrollBars();
-  QAbstractScrollArea::resizeEvent(e);
+  this->Superclass::resizeEvent(e);
+  this->updateGeometries();
 }
 
 void pqAnimationWidget::headerDeleteClicked(int which)

Qt/Widgets/pqAnimationWidget.h
@@ -45,7 +45,9 @@ class pqAnimationTrack;
 
 class PQWIDGETS_EXPORT pqAnimationWidget : public QAbstractScrollArea
 {
-  Q_OBJECT
+  Q_OBJECT;
+  using Superclass = QAbstractScrollArea;
+
 public:
   pqAnimationWidget(QWidget* p = 0);
   ~pqAnimationWidget() override;

commit 9e02d7840241bcbfbfabaaac7122140151052331

ensure pqAnimationWidget is resized on show.
pqAnimationWidget needs to resize itself when its being shown to ensure
all widgets are placed correctly. This bug was introduced in 9f34821f6.

Fixes #18493.

Qt/Widgets/pqAnimationWidget.cxx
@@ -284,6 +284,12 @@ void pqAnimationWidget::resizeEvent(QResizeEvent* e)
   this->updateGeometries();
 }
 
+void pqAnimationWidget::showEvent(QShowEvent* e)
+{
+  this->Superclass::showEvent(e);
+  this->updateGeometries();
+}
+
 void pqAnimationWidget::headerDeleteClicked(int which)
 {
   if (which > 0)

Qt/Widgets/pqAnimationWidget.h
@@ -86,6 +86,7 @@ protected:
   void scrollContentsBy(int dx, int dy) override;
   bool event(QEvent* e) override;
   void resizeEvent(QResizeEvent* e) override;
+  void showEvent(QShowEvent* e) override;
 
 private:
   QGraphicsView* View;