f49b6a4c88d403182acce700dd732f33197f1f00
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2018-11-22 11:06:21 -0500

    cleanup vtkSMFieldDataDomain
    vtkSMFieldDataDomain no longer updates itself based on input type. That
    was generally not useful and added unnecessary complication to the
    domain and the corresponding UI. Cleaned that up.
    
    vtkSMFieldDataDomain now raises warnings if used in
    vtkSMStringVectorProperty as we tended to do in the past.

--- ParaViewCore/ServerManager/Core/vtkSMFieldDataDomain.h
+++ ParaViewCore/ServerManager/Core/vtkSMFieldDataDomain.h
@@ -13,31 +13,38 @@
 
 =========================================================================*/
 /**
- * @class   vtkSMFieldDataDomain
- * @brief   enumeration with point and cell data entries
+ * @class vtkSMFieldDataDomain
+ * @brief enumeration domain to select array association or attribute type.
  *
- * vtkSMFieldDataDomain is a sub-class vtkSMEnumerationDomain that looks at
- * the input in Update() and populates the entry list based on whether
- * there are valid arrays in point/cell/vertex/edge/row data.
- * At most it consists of two
- * entries: ("Point Data", vtkDataObject::FIELD_ASSOCIATION_POINTS) and
- * ("Cell Data",  vtkDataObject::FIELD_ASSOCIATION_CELLS) in case of vtkDataSet
- * subclasses
- * OR
- * ("Vertex Data", vtkDataObject::FIELD_ASSOCIATION_VERTICES) and
- * ("Edge Data", vtkDataObject::FIELD_ASSOCIATION_EDGES) in case of vtkGraph and
- * subclasses
- * OR
- * ("Row Data", vtkDataObject::FIELD_ASSOCIATION_ROWS) in case of vtkTable and
- * subclasses.
- * It requires Input (vtkSMProxyProperty) property.
- * If attribute "disable_update_domain_entries" is set to true (false by
- * default),
- * then the domain values will not changed based on input field availability.
- * Only the default value setting will be affected by that.
- * @sa
- * vtkSMEnumerationDomain vtkSMProxyProperty
-*/
+ * vtkSMFieldDataDomain can be used on a vtkSMIntVectorProperty that is intended
+ * to specify the attribute type (`vtkDataObject::AttributeTypes`) or field association
+ * (`vtkDataObject::FieldAssociations`).
+ *
+ * By default, `vtkDataObject::Field` (or vtkDataObject::FIELD_ASSOCIATION_NONE) is
+ * not added to the domain. If you want to support field data, then use `enable_field_data="1"`
+ * attribute in the XML configuration.
+ *
+ * @section DefaultValue Selecting the default value
+ *
+ * vtkSMFieldDataDomain picks the default value to be the first attribute type that has non empty
+ * arrays
+ * and non-zero tuples. If all attributes have no tuples, then the first attribute with non empty
+ * arrays is selected. If all attributes have no arrays, then vtkSMEnumerationDomain
+ * picks the default. For this to work, the domain must be provided an `Input` property as a
+ * required property for function "Input".
+ *
+ * @section DeprecatedUsage Deprecated Usage
+ *
+ * Previously (5.6 and earlier), vtkSMFieldDataDomain was added to vtkSMStringVectorProperty
+ * instances that allowed user to choose the array to process. This is no longer needed
+ * or supported. Simply remove the vtkSMFieldDataDomain from the XML for such properties.
+ *
+ * The domain provided ability to limit the attribute types to the data type of the input dataset.
+ * This was clumsy since it did not correctly handle cases where data type changes or
+ * non empty attribute types changed. Hence we've dropped support for that. Simply remove
+ * `disable_update_domain_entries` and `force_point_cell_data` attributes from the XML
+ * for this domain since they are no longer supported.
+ */
 
 #ifndef vtkSMFieldDataDomain_h
 #define vtkSMFieldDataDomain_h
@@ -52,20 +59,19 @@ class VTKPVSERVERMANAGERCORE_EXPORT vtkSMFieldDataDomain : public vtkSMEnumerati
 public:
   static vtkSMFieldDataDomain* New();
   vtkTypeMacro(vtkSMFieldDataDomain, vtkSMEnumerationDomain);
-  void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
-
-  /**
-   * Check the input and appropriate fields (point data or cell data)
-   * to the enumeration. This uses the Input property with a
-   * vtkSMInputArrayDomain.
-   */
-  void Update(vtkSMProperty* prop) VTK_OVERRIDE;
+  void PrintSelf(ostream& os, vtkIndent indent) override;
 
   /**
    * Overridden to ensure that the property's default value is valid for the
    * enumeration, if not it will be set to the first enumeration value.
    */
-  int SetDefaultValues(vtkSMProperty*, bool use_unchecked_values) VTK_OVERRIDE;
+  int SetDefaultValues(vtkSMProperty*, bool use_unchecked_values) override;
+
+  /**
+   * Convenience method to return the string for an attribute type. Will return nullptr
+   * for unsupported or invalid type.
+   */
+  static const char* GetAttributeTypeAsString(int attrType);
 
 protected:
   vtkSMFieldDataDomain();
@@ -75,31 +81,16 @@ protected:
    * Set the appropriate ivars from the xml element. Should
    * be overwritten by subclass if adding ivars.
    */
-  int ReadXMLAttributes(vtkSMProperty* prop, vtkPVXMLElement* elem) VTK_OVERRIDE;
+  int ReadXMLAttributes(vtkSMProperty* prop, vtkPVXMLElement* elem) override;
 
   // When true, "Field Data" option is added to the domain.
   bool EnableFieldDataSelection;
 
-  // When true, we don't update the available list of attributes based on what's
-  // actually available in the input (false by default).
-  bool DisableUpdateDomainEntries;
-
-  // When true, "Point Data" and "Cell Data" is included to the domain even
-  // if they don't have any properties. This is used by the spreadsheet
-  // view.( false by default )
-  bool ForcePointAndCellDataSelection;
-
 private:
-  // Used by SetDefaultValues.
-  int DefaultValue;
-
-  /**
-   * Utility functions called by Update()
-   */
-  void UpdateDomainEntries(int acceptable_association, vtkPVDataInformation* dataInfo);
-
   vtkSMFieldDataDomain(const vtkSMFieldDataDomain&) = delete;
   void operator=(const vtkSMFieldDataDomain&) = delete;
+
+  int ComputeDefaultValue();
 };
 
 #endif
