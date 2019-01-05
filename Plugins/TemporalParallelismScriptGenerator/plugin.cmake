76edf187cddf8fcc18b874c676b2d01720722a36
Ben Boeckel <ben.boeckel@kitware.com>
2019-01-05 13:31:36 -0500

    TemporalParallelismScriptGenerator: update to the new module system

--- Plugins/TemporalParallelismScriptGenerator/plugin.cmake
+++ Plugins/TemporalParallelismScriptGenerator/plugin.cmake
@@ -1,4 +1,10 @@
-if (PARAVIEW_BUILD_QT_GUI AND PARAVIEW_ENABLE_PYTHON)
-  pv_plugin(TemporalParallelismScriptGenerator
-    DESCRIPTION "Plugin for creating Python spatio-temporal processing scripts")
-endif()
+NAME
+  TemporalParallelismScriptGenerator
+DESCRIPTION
+  Plugin for creating Python spatio-temporal processing scripts
+CONDITION
+  PARAVIEW_BUILD_QT_GUI AND PARAVIEW_ENABLE_PYTHON
+REQUIRES_MODULES
+  ParaView::Common
+  ParaView::ServerManagerCore
+  ParaView::ServerManagerRendering
