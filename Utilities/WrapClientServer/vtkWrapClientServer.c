e1b06586aa3ea128535c3d5320981b9ab5f238a4
Ben Boeckel <ben.boeckel@kitware.com>
2019-02-12 13:20:32 -0500

    vtkWrapClientServer: support multiple hierarchy files

--- Utilities/WrapClientServer/vtkWrapClientServer.c
+++ Utilities/WrapClientServer/vtkWrapClientServer.c
@@ -1208,9 +1208,10 @@ int main(int argc, char* argv[])
   options = vtkParse_GetCommandLineOptions();
 
   /* get the hierarchy info for accurate typing */
-  if (options->HierarchyFileName)
+  if (options->HierarchyFileNames)
   {
-    hierarchyInfo = vtkParseHierarchy_ReadFile(options->HierarchyFileName);
+    hierarchyInfo =
+      vtkParseHierarchy_ReadFiles(options->NumberOfHierarchyFileNames, options->HierarchyFileNames);
   }
 
   /* get the output file */
