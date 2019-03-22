871cf7da3f7cebd62996a21e652fbbf48571c7e4
Cory Quammen <cory.quammen@kitware.com>
2019-03-21 22:36:25 -0400

    Add vtkPointSet as dataset type requiring output being same as input

--- ParaViewCore/VTKExtensions/Core/vtkReductionFilter.cxx
+++ ParaViewCore/VTKExtensions/Core/vtkReductionFilter.cxx
@@ -103,7 +103,8 @@ int vtkReductionFilter::RequestDataObject(
 
     const char* hOT = helpersInfo->Get(vtkDataObject::DATA_TYPE_NAME());
     const char* helpersOutType = hOT;
-    if ((!strcmp(hOT, "vtkDataSet") || !strcmp(hOT, "vtkDataObject")))
+    if ((!strcmp(hOT, "vtkPointSet") || !strcmp(hOT, "vtkDataSet") ||
+          !strcmp(hOT, "vtkDataObject")))
     {
       // Output type must be same as input.
       vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
