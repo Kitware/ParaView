fd8d05845ab83519850e71d7392e23ec449e504d
David E. DeMarle <dave.demarle@kitware.com>
2019-04-17 12:46:46 -0400

    prep to bump cinema

--- ThirdParty/cinema/update.sh
+++ ThirdParty/cinema/update.sh
@@ -8,7 +8,7 @@ readonly name="cinema"
 readonly ownership="Cinema Python Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/cinema/paraview/tpl"
 readonly repo="https://gitlab.kitware.com/third-party/cinema_python.git"
-readonly tag="for/paraview-20190221-master"
+readonly tag="9204e650"
 
 readonly paths="
 .gitignore
