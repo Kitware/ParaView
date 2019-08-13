0700f3cfd60beedc52ecdeb2a2276c668b9a07a3
Ben Boeckel <ben.boeckel@kitware.com>
2019-08-13 11:11:18 -0400

    ParaViewClient: support parallel-installable Qt install trees

--- CMake/ParaViewClient.cmake
+++ CMake/ParaViewClient.cmake
@@ -458,6 +458,7 @@ function (paraview_client_documentation)
   find_program(qt_xmlpatterns_executable
     NAMES xmlpatterns-qt5 xmlpatterns
     HINTS "${Qt5_DIR}/../../../bin"
+          "${Qt5_DIR}/../../../libexec/qt5/bin"
     DOC   "Path to xmlpatterns")
   mark_as_advanced(qt_xmlpatterns_executable)
 
