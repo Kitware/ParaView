01936925c73c740280b5dc5f99a290d2a5993cc2
Cory Quammen <cory.quammen@kitware.com>
2019-09-24 10:09:27 -0400

    Fix struct/class mismatch in header and implementation file

--- Plugins/VRPlugin/pqVRConnectionManager.cxx
+++ Plugins/VRPlugin/pqVRConnectionManager.cxx
@@ -69,8 +69,9 @@ pqVRConnectionManager* pqVRConnectionManager::instance()
 // IMPORTANT: Make sure that this struct has no pointers.  All pointers should
 // be put in the class declaration. For all newly defined pointers make sure to
 // update constructor and destructor methods.
-struct pqVRConnectionManager::pqInternals
+class pqVRConnectionManager::pqInternals
 {
+public:
 #if PARAVIEW_PLUGIN_VRPlugin_USE_VRPN
   QList<QPointer<pqVRPNConnection> > VRPNConnections;
 #endif
