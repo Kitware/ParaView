46f03e628f4f3ba4f53c2ce7b8e67f5a3563188a
Ben Boeckel <ben.boeckel@kitware.com>
2020-01-20 16:28:13 -0500

    cmake: install PARAVIEW_USE_FILE compatibility

---
+++ CMake/paraview-use-file-deprecated.cmake
@@ -0,0 +1,2 @@
+message(DEPRECATION
+  "The `PARAVIEW_USE_FILE` is no longer used starting with 5.7.")
---
+++ CMake/paraview-use-file-error.cmake
@@ -0,0 +1,4 @@
+message(FATAL_ERROR
+  "The `PARAVIEW_USE_FILE` is no longer used starting with 5.7. The "
+  "`include(\${PARAVIEW_USE_FILE})` line is no longer necessary and can just "
+  "be removed.")
