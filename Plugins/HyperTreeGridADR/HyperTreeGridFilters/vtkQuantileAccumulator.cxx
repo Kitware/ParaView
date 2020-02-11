f9fbaa8220cd626ebd932a2af7260b7a30a1f20f
Yohann Bearzi <yohann.bearzi@kitware.com>
2020-02-11 15:06:35 -0500

    fixed quantile measurement in resample2htg

--- Plugins/HyperTreeGridADR/HyperTreeGridFilters/vtkQuantileAccumulator.cxx
+++ Plugins/HyperTreeGridADR/HyperTreeGridFilters/vtkQuantileAccumulator.cxx
@@ -46,8 +46,7 @@ void vtkQuantileAccumulator::Add(vtkAbstractAccumulator* accumulator)
     std::size_t i = 0;
     ListType out;
     while (i < quantileAccumulator->SortedList->size() &&
-      (*quantileAccumulator->SortedList)[i].Weight <
-        (*this->SortedList)[this->PercentileIdx].Weight)
+      (*quantileAccumulator->SortedList)[i].Value < (*this->SortedList)[this->PercentileIdx].Value)
     {
       this->PercentileWeight += (*quantileAccumulator->SortedList)[i++].Weight;
     }
@@ -56,32 +55,21 @@ void vtkQuantileAccumulator::Add(vtkAbstractAccumulator* accumulator)
       quantileAccumulator->SortedList->cbegin(), quantileAccumulator->SortedList->cend(),
       std::back_inserter(out));
     this->SortedList = std::make_shared<ListType>(out);
-    this->TotalWeight = quantileAccumulator->TotalWeight;
-    this->PercentileIdx = quantileAccumulator->PercentileIdx;
-    if (i)
+    this->TotalWeight += quantileAccumulator->TotalWeight;
+
+    // Move the percentile in the left direction.
+    while (this->PercentileIdx != 0 &&
+      this->Percentile - 100.0 * this->PercentileWeight / this->TotalWeight <= 0)
     {
-      // Move the percentile in the left direction accordingly.
-      while (this->PercentileIdx != 0 &&
-        this->Percentile -
-            100.0 * (this->PercentileWeight - (*this->SortedList)[this->PercentileIdx - 1].Weight) /
-              this->TotalWeight >=
-          0)
-      {
-        --this->PercentileIdx;
-        this->PercentileWeight -= (*this->SortedList)[this->PercentileIdx].Weight;
-      }
+      this->PercentileWeight -= (*this->SortedList)[this->PercentileIdx].Weight;
+      --this->PercentileIdx;
     }
-    else
+    // Move the percentile in the right direction.
+    while (this->PercentileIdx != this->SortedList->size() - 1 &&
+      this->Percentile - 100.0 * this->PercentileWeight / this->TotalWeight > 0)
     {
-      while (this->PercentileIdx < this->SortedList->size() &&
-        this->Percentile -
-            100.0 * (this->PercentileWeight + (*this->SortedList)[this->PercentileIdx + 1].Weight) /
-              this->TotalWeight <=
-          0)
-      {
-        ++this->PercentileIdx;
-        this->PercentileWeight += (*this->SortedList)[this->PercentileIdx].Weight;
-      }
+      ++this->PercentileIdx;
+      this->PercentileWeight += (*this->SortedList)[this->PercentileIdx].Weight;
     }
   }
   else
@@ -100,36 +88,33 @@ void vtkQuantileAccumulator::Add(vtkAbstractAccumulator* accumulator)
 //----------------------------------------------------------------------------
 void vtkQuantileAccumulator::Add(double value, double weight)
 {
+  if (this->SortedList->size() && (*this->SortedList)[this->PercentileIdx].Value >= value)
+  {
+    this->PercentileWeight += weight;
+    ++this->PercentileIdx;
+  }
+  else if (!this->SortedList->size())
+  {
+    this->PercentileWeight = weight;
+  }
   this->TotalWeight += weight;
   auto it = std::lower_bound(
     this->SortedList->begin(), this->SortedList->end(), ListElement(value, weight));
   this->SortedList->insert(it, ListElement(value, weight));
-  if ((*this->SortedList)[this->PercentileIdx].Value >= value)
-  {
-    this->PercentileWeight += weight;
 
-    // Move the percentile in the left direction accordingly.
-    while (this->PercentileIdx != 0 &&
-      this->Percentile -
-          100.0 * (this->PercentileWeight - (*this->SortedList)[this->PercentileIdx - 1].Weight) /
-            this->TotalWeight >=
-        0)
-    {
-      --this->PercentileIdx;
-      this->PercentileWeight -= (*this->SortedList)[this->PercentileIdx].Weight;
-    }
-  }
-  else
+  // Move the percentile in the left direction.
+  while (this->PercentileIdx != 0 &&
+    this->Percentile - 100.0 * this->PercentileWeight / this->TotalWeight <= 0)
   {
-    while (this->PercentileIdx < this->SortedList->size() &&
-      this->Percentile -
-          100.0 * (this->PercentileWeight + (*this->SortedList)[this->PercentileIdx + 1].Weight) /
-            this->TotalWeight <=
-        0)
-    {
-      ++this->PercentileIdx;
-      this->PercentileWeight += (*this->SortedList)[this->PercentileIdx].Weight;
-    }
+    this->PercentileWeight -= (*this->SortedList)[this->PercentileIdx].Weight;
+    --this->PercentileIdx;
+  }
+  // Move the percentile in the right direction.
+  while (this->PercentileIdx != this->SortedList->size() - 1 &&
+    this->Percentile - 100.0 * this->PercentileWeight / this->TotalWeight > 0)
+  {
+    ++this->PercentileIdx;
+    this->PercentileWeight += (*this->SortedList)[this->PercentileIdx].Weight;
   }
   this->Modified();
 }
@@ -147,10 +132,13 @@ void vtkQuantileAccumulator::Initialize()
 void vtkQuantileAccumulator::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
+  os << indent << "PercentileIdx " << this->PercentileIdx << std::endl;
+  os << indent << "PercentileWeight " << this->PercentileWeight << std::endl;
+  os << indent << "TotalWeight " << this->TotalWeight << std::endl;
   os << indent << "Sorted list:" << std::endl;
   for (std::size_t i = 0; i < this->SortedList->size(); ++i)
   {
-    os << indent << "Index " << i << ": (Value: " << (*this->SortedList)[i].Value
+    os << indent << indent << "Index " << i << ": (Value: " << (*this->SortedList)[i].Value
        << ", Weight: " << (*this->SortedList)[i].Weight << ")" << std::endl;
   }
 }
