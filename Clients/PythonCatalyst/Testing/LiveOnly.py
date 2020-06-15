e1532de20d36852e26d33433ac26a7c39a432589
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-06-15 11:45:52 -0400

    Adding new set of coprocessing tests.

---
+++ Clients/PythonCatalyst/Testing/LiveOnly.py
@@ -0,0 +1,19 @@
+# Test confirms that scripts with just the live triggers
+# are handled correctly by Catalyst
+
+#--------------------------------------
+# catalyst options
+from paraview import catalyst
+options = catalyst.Options()
+options.EnableCatalystLive = 1
+options.CatalystLiveTrigger = 'Time'
+options.CatalystLiveURL = "localhost:22222"
+
+
+def catalyst_finalize():
+    from paraview.simple import GetSources
+    # since this is live-only script, ParaView should have
+    # automatically created source for all in situ data sources
+    # let's confirm that.
+    if not GetSources():
+        raise RuntimeError("No sources found!")
