39326c5e83e6841326a07a87264e6c6823f83888
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-06-19 10:02:03 -0400

    prototyping Catalyst full workflow tests.

---
+++ Clients/PythonCatalyst/Testing/WaveletHistogramExtractsData.py
@@ -0,0 +1,10 @@
+
+BatchExtracts = {
+     "dir" : "BatchExtracts"
+     "contents" : [{
+         "dir" : "dataset_000000",
+         "contents" : ["dataset_000000_0.vti"],
+         },
+         "dataset_000000.pvti",
+     ]
+}
