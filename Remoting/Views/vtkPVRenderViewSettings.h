88bba20ef6221e6452c85cc610ccbd59ca8858d3
Cory Quammen <cory.quammen@kitware.com>
2020-07-01 15:45:07 -0400

    Remove some implementation details from settings description
    Retain the important part, which is what the end result is.

--- Remoting/Views/vtkPVRenderViewSettings.h
+++ Remoting/Views/vtkPVRenderViewSettings.h
@@ -105,8 +105,8 @@ public:
   //@{
   /**
    * Enable fast preselection. When enabled, the preselection is computed using
-   * the visible geometry. This avoid a call to the extract selection filter each time
-   * the mouse move, improving a lot the preselection highlight display, especially for
+   * the visible geometry. This avoids a call to the extract selection filter each time
+   * the mouse moves, improving a lot the preselection highlight display, especially for
    * big datasets.
    */
   vtkSetMacro(EnableFastPreselection, bool);
