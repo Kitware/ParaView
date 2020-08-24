cfd006e434248632acab0e22b0c26b7216735b4f
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2020-08-24 17:25:59 -0400

    gitlab-ci: generates doxygen page in a GL job
    Signed-off-by: Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>

--- .gitlab/ci/cmake.sh
+++ .gitlab/ci/cmake.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-readonly version="3.15.6"
-readonly sha256sum="05510247c4ef28d011b5787972159b05456c49630781deba2bfb71fcf81a1b40"
+readonly version="3.17.4"
+readonly sha256sum="126cc8356907913787d4ff35237ae1854c09b927a35dbe5270dd571ae224bdd3"
 readonly filename="cmake-$version-Linux-x86_64"
 readonly tarball="$filename.tar.gz"
 
