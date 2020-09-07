8df7abf0b843c7bb13dc19f3f1a99c2afafb40cd
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-09-07 07:13:55 -0400

    ci: adding centos container

---
+++ .gitlab/ci/docker/centos7/install_deps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install extra dependencies for ParaView
+yum install -y \
+   bzip2 patch \
+   python-twisted-core python-twisted-web python-twisted-words \
+   python3 python3-devel qt5-qtbase-devel qt5-qttools-devel \
+   qt5-qtsvg-devel qt5-qtxmlpatterns-devel doxygen openmpi-devel \
+   python36-numpy
+
+yum clean all
