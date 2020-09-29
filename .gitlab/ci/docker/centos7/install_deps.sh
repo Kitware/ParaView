44e9eeb775209247ec15bd432244b74f4a663bc0
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-09-29 07:23:36 -0400

    update ci image to include 'pandas'

--- .gitlab/ci/docker/centos7/install_deps.sh
+++ .gitlab/ci/docker/centos7/install_deps.sh
@@ -6,7 +6,7 @@ yum install -y \
    python-twisted-core python-twisted-web python-twisted-words \
    python3 python3-devel qt5-qtbase-devel qt5-qttools-devel \
    qt5-qtsvg-devel qt5-qtxmlpatterns-devel doxygen openmpi-devel \
-   python36-numpy
+   python36-numpy python3-pandas python3-pandas-datareader
 
 pip3 install sphinx
 
