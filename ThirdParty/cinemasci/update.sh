05944da58b777a91fbfa03fa91ea4c091f3a7d65
Ben Boeckel <ben.boeckel@kitware.com>
2020-10-02 12:32:55 -0400

    cinemasci: import with submodules

--- ThirdParty/cinemasci/update.sh
+++ ThirdParty/cinemasci/update.sh
@@ -8,20 +8,33 @@ readonly name="cinemasci"
 readonly ownership="Cinemasci Python Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/cinemasci/paraview/tpl"
 readonly repo="https://github.com/cinemascience/cinemasci.git"
-readonly tag="5e6c7e96083474ed52de30459602dd79ac600aa1"
+readonly tag="79c3cdbbc53c4b020191c09b5a37e04f5c807490"
 
 readonly paths="
 .gitignore
 license.md
-cinemasci/
+cinemasci/__init__.py
+cinemasci/cdb/
+cinemasci/cis/
+cinemasci/pynb/
+cinemasci/server/
+
+cinemasci/viewers/.gitignore
+cinemasci/viewers/*.html
+cinemasci/viewers/*.md
+cinemasci/viewers/__init__.py
+cinemasci/viewers/cinema/
 "
 
 extract_source () {
-    git_archive
+    git_archive_all
 
     echo "* -whitespace" > "$extractdir/$name-reduced/.gitattributes"
-    echo >> "$extractdir/$name-reduced/cinemasci/cview/cinema/lib/d3.v4.min.js"
-    echo >> "$extractdir/$name-reduced/cinemasci/cview/cinema/view/1.2/css/range-css.css"
+    echo >> "$extractdir/$name-reduced/cinemasci/viewers/cinema/lib/d3.v4.min.js"
+    echo >> "$extractdir/$name-reduced/cinemasci/viewers/cinema/view/2.0/css/range-css.css"
+    echo >> "$extractdir/$name-reduced/cinemasci/viewers/.gitignore"
+
+    rm -rfv "$extractdir/$name-reduced/cinemasci/viewers/cinema/testImages"
 }
 
 . "${BASH_SOURCE%/*}/../update-common.sh"
