115bd96665839267c11162e0ab4bae03526bf613
Ben Boeckel <ben.boeckel@kitware.com>
2020-11-03 15:28:43 -0500

    ci: add documentation bits to the fedora31 container

--- .gitlab/ci/docker/fedora31/install_deps.sh
+++ .gitlab/ci/docker/fedora31/install_deps.sh
@@ -3,9 +3,14 @@
 # Install extra dependencies for ParaView
 yum install -y \
    bzip2 patch \
+   python3-twisted python3-autobahn \
    python3 python3-devel qt5-qtbase-devel qt5-qttools-devel \
    qt5-qtsvg-devel qt5-qtxmlpatterns-devel doxygen openmpi-devel \
    python3-numpy openmpi-devel mesa-libOSMesa-devel \
-   mesa-libOSMesa-devel mesa-libOSMesa
+   python3-pandas python3-pandas-datareader \
+   mesa-libOSMesa-devel mesa-libOSMesa \
+   python3-sphinx python3-pip
+
+python3 -m pip install wslink
 
 yum clean all
