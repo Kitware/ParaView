56d3a3a6421dacb72c4095e745f8ec330c2aeb6a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-11-11 12:13:10 -0500

    Adding filedriver_miniapp
    Adding filedriver_miniapp to make it easier to test Catalyst using a
    collection of files acting as input.

---
+++ Clients/PythonCatalyst/Testing/ValidateChangingTime.py
@@ -0,0 +1,11 @@
+def RequestDataDescription(datadescription):
+    for i in range(datadescription.GetNumberOfInputDescriptions()):
+        datadescription.GetInputDescription(i).AllFieldsOn()
+        datadescription.GetInputDescription(i).GenerateMeshOn()
+
+last_time = None
+def DoCoProcessing(datadescription):
+    global last_time
+    if last_time is not None:
+        assert last_time != datadescription.GetTime()
+    last_time = datadescription.GetTime()
