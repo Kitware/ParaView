8ef3839b49c3caf4df5629db1e78bfb4f987f305
Ben Boeckel <ben.boeckel@kitware.com>
2020-11-18 21:10:59 -0500

    ci: factor out Fedora logic

--- .gitlab/ci/configure_fedora33.cmake
+++ .gitlab/ci/configure_fedora33.cmake
@@ -1,9 +1,1 @@
-set(PARAVIEW_ENABLE_ADIOS ON CACHE BOOL "")
-
-if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "offscreen")
-  set(VTK_USE_X OFF CACHE BOOL "")
-else ()
-  set(VTK_USE_X ON CACHE BOOL "")
-endif ()
-
-include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora_common.cmake")
