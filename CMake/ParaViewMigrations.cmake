a9d79f110d603604abfc94bb38acf3cc71467398
Ben Boeckel <ben.boeckel@kitware.com>
2021-01-08 13:53:47 -0500

    ParaViewMigrations: migrate CGNS module settings

--- CMake/ParaViewMigrations.cmake
+++ CMake/ParaViewMigrations.cmake
@@ -8,3 +8,18 @@ function (paraview_migrate_setting oldname newname doc)
       "The `${oldname}` setting has been migrated to `${newname}`.")
   endif ()
 endfunction ()
+
+# XXX(paraview-5.10): Once there is a version number that users can use to
+# determine what is needed, migrate ParaView::cgns settings for VTK::cgns.
+paraview_migrate_setting(
+  VTK_MODULE_ENABLE_ParaView_cgns
+  VTK_MODULE_ENABLE_VTK_cgns
+  "Migration of ParaView::cgns settings")
+paraview_migrate_setting(
+  VTK_MODULE_USE_EXTERNAL_ParaView_cgns
+  VTK_MODULE_USE_EXTERNAL_VTK_cgns
+  "Migration of ParaView::cgns settings")
+paraview_migrate_setting(
+  VTK_MODULE_ENABLE_ParaView_VTKExtensionsCGNSReader
+  VTK_MODULE_ENABLE_VTK_IOCGNSReader
+  "Migration of ParaView::VTKExtensionsCGNSReader settings")
