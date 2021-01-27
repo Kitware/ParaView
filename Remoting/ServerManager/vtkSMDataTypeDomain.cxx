bd12f8f2fc6009dbc0e5fa2645c0563d00f8bb9f
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2021-01-27 13:53:48 -0500

    vtkSMDataTypeDomain: cleanups to use `vtkDataObjecTypes`
    vtkSMDataTypeDomain now relies on API on vtkDataObjecTypes to handle
    type casting. Also uses new API on vtkPVDataInformation to better
    support composite datasets and dataset types present in the composite
    dataset.

--- Remoting/ServerManager/vtkSMDataTypeDomain.cxx
+++ Remoting/ServerManager/vtkSMDataTypeDomain.cxx
@@ -15,18 +15,14 @@
 #include "vtkSMDataTypeDomain.h"
 
 #include "vtkClientServerStreamInstantiator.h"
-#include "vtkDataObject.h"
+#include "vtkDataObjectTypes.h"
 #include "vtkObjectFactory.h"
-#include "vtkPVCompositeDataInformation.h"
-#include "vtkPVCompositeDataInformationIterator.h"
 #include "vtkPVDataInformation.h"
 #include "vtkPVXMLElement.h"
-#include "vtkProcessModule.h"
 #include "vtkSMInputProperty.h"
 #include "vtkSMSourceProxy.h"
-#include "vtkSmartPointer.h"
 
-#include <map>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -35,13 +31,9 @@
 //*****************************************************************************
 struct vtkSMDataTypeDomainAllowedType
 {
-  vtkSMDataTypeDomainAllowedType() = delete;
-  vtkSMDataTypeDomainAllowedType(std::string Name, bool HasChildren = false, bool MatchAny = false,
-    std::vector<std::string> Children = {});
-  std::string typeName;
-  bool hasChildren;
-  bool childMatchAny;
-  std::vector<std::string> childAllowedTypes;
+  int Type = -1;
+  std::vector<int> ChildTypes;
+  bool ChildMatchAny = false;
 };
 
 struct vtkSMDataTypeDomainInternals
@@ -49,112 +41,20 @@ struct vtkSMDataTypeDomainInternals
   std::vector<vtkSMDataTypeDomainAllowedType> DataTypes;
 };
 
-vtkSMDataTypeDomainAllowedType::vtkSMDataTypeDomainAllowedType(
-  std::string Name, bool HasChildren, bool MatchAny, std::vector<std::string> Children)
-  : typeName(Name)
-  , hasChildren(HasChildren)
-  , childMatchAny(MatchAny)
-  , childAllowedTypes(Children){};
-//*****************************************************************************
-namespace vtkSMDataTypeDomainCache
-{
-static std::map<std::string, vtkSmartPointer<vtkDataObject> > DataObjectMap;
-
-// Only instantiate classes once and use cache after...
-static vtkDataObject* GetDataObjectOfType(const char* classname)
-{
-  if (classname == nullptr)
-  {
-    return nullptr;
-  }
-
-  // Since we can not instantiate these classes, we'll replace
-  // them with a subclass
-  if (strcmp(classname, "vtkDataSet") == 0)
-  {
-    classname = "vtkImageData";
-  }
-  else if (strcmp(classname, "vtkPointSet") == 0)
-  {
-    classname = "vtkPolyData";
-  }
-  else if (strcmp(classname, "vtkCompositeDataSet") == 0)
-  {
-    classname = "vtkHierarchicalDataSet";
-  }
-  else if (strcmp(classname, "vtkUnstructuredGridBase") == 0)
-  {
-    classname = "vtkUnstructuredGrid";
-  }
-
-  std::map<std::string, vtkSmartPointer<vtkDataObject> >::iterator it;
-  it = DataObjectMap.find(classname);
-  if (it != DataObjectMap.end())
-  {
-    return it->second.GetPointer();
-  }
-
-  auto object = vtkClientServerStreamInstantiator::CreateInstance(classname);
-  vtkDataObject* dobj = vtkDataObject::SafeDownCast(object);
-  if (!dobj)
-  {
-    if (object)
-    {
-      object->Delete();
-    }
-    return nullptr;
-  }
-
-  DataObjectMap[classname] = dobj;
-  dobj->Delete();
-  return dobj;
-}
-}
-
 //*****************************************************************************
 vtkStandardNewMacro(vtkSMDataTypeDomain);
 //---------------------------------------------------------------------------
 vtkSMDataTypeDomain::vtkSMDataTypeDomain()
+  : CompositeDataSupported(true)
+  , CompositeDataRequired(false)
+  , DTInternals(new vtkSMDataTypeDomainInternals())
 {
-  this->DTInternals = new vtkSMDataTypeDomainInternals;
-  this->CompositeDataSupported = 1;
-  this->CompositeDataRequired = 0;
 }
 
 //---------------------------------------------------------------------------
 vtkSMDataTypeDomain::~vtkSMDataTypeDomain()
 {
   delete this->DTInternals;
-}
-
-//---------------------------------------------------------------------------
-unsigned int vtkSMDataTypeDomain::GetNumberOfDataTypes()
-{
-  return static_cast<unsigned int>(this->DTInternals->DataTypes.size());
-}
-
-//---------------------------------------------------------------------------
-const char* vtkSMDataTypeDomain::GetDataTypeName(unsigned int idx)
-{
-  return this->DTInternals->DataTypes[idx].typeName.c_str();
-}
-
-//---------------------------------------------------------------------------
-bool vtkSMDataTypeDomain::DataTypeHasChildren(unsigned int idx)
-{
-  return this->DTInternals->DataTypes[idx].hasChildren;
-}
-
-//---------------------------------------------------------------------------
-const char* vtkSMDataTypeDomain::GetDataTypeChildMatchTypeAsString(unsigned int idx)
-{
-  return this->DTInternals->DataTypes[idx].childMatchAny ? "any" : "all";
-}
-
-//---------------------------------------------------------------------------
-const std::vector<std::string>& vtkSMDataTypeDomain::GetDataTypeChildren(unsigned int idx)
-{
-  return this->DTInternals->DataTypes[idx].childAllowedTypes;
 }
 
 //---------------------------------------------------------------------------
@@ -198,8 +98,7 @@ int vtkSMDataTypeDomain::IsInDomain(vtkSMSourceProxy* proxy, int outputport /*=0
     return 0;
   }
 
-  unsigned int numTypes = this->GetNumberOfDataTypes();
-  if (numTypes == 0)
+  if (this->DTInternals->DataTypes.size() == 0)
   {
     return 1;
   }
@@ -208,137 +107,101 @@ int vtkSMDataTypeDomain::IsInDomain(vtkSMSourceProxy* proxy, int outputport /*=0
   proxy->CreateOutputPorts();
 
   vtkPVDataInformation* info = proxy->GetDataInformation(outputport);
-  if (!info)
+  if (!info || info->IsNull())
   {
     return 0;
   }
 
-  if (info->GetCompositeDataClassName() && !this->CompositeDataSupported)
+  if (info->IsCompositeDataSet() && !this->CompositeDataSupported)
   {
     return 0;
   }
-  if (!info->GetCompositeDataClassName() && this->CompositeDataRequired)
+  if (!info->IsCompositeDataSet() && this->CompositeDataRequired)
   {
-    return 0;
-  }
-
-  // Get an actual instance of the same type as the data represented
-  // by the information object. This is later used to check match
-  // with IsA.
-  vtkDataObject* dobj = vtkSMDataTypeDomainCache::GetDataObjectOfType(info->GetDataClassName());
-  if (!dobj)
-  {
-    const char* classname = info->GetDataClassName();
-    if (classname && classname[0] != '\0')
-    {
-      vtkWarningMacro("Unable to create instance of class '" << classname << "'.");
-    }
     return 0;
   }
 
-  for (unsigned int i = 0; i < numTypes; i++)
+  for (auto& dtype : this->DTInternals->DataTypes)
   {
-    if (this->DataTypeHasChildren(i))
+    if (info->DataSetTypeIsA(dtype.Type))
     {
-      continue;
-    }
-    // Unfortunately, vtkDataSet, vtkPointSet, and vtkUnstructuredGridBase have
-    // to be handled specially. These classes are abstract and can not be
-    // instantiated.
-    if (strcmp(info->GetDataClassName(), "vtkDataSet") == 0)
-    {
-      if (strcmp(this->GetDataTypeName(i), "vtkDataSet") == 0)
+      if (dtype.ChildTypes.size() == 0)
       {
         return 1;
       }
-      continue;
-    }
-    if (strcmp(info->GetDataClassName(), "vtkPointSet") == 0)
-    {
-      if ((strcmp(this->GetDataTypeName(i), "vtkPointSet") == 0) ||
-        (strcmp(this->GetDataTypeName(i), "vtkDataSet") == 0))
+
+      size_t match_count = 0;
+      for (auto& childType : dtype.ChildTypes)
+      {
+        match_count += info->HasDataSetType(childType) ? 1 : 0;
+      }
+
+      if (dtype.ChildMatchAny && match_count > 0)
       {
         return 1;
       }
-      continue;
-    }
-    if (strcmp(info->GetDataClassName(), "vtkUnstructuredGridBase") == 0)
-    {
-      if ((strcmp(this->GetDataTypeName(i), "vtkPointSet") == 0) ||
-        (strcmp(this->GetDataTypeName(i), "vtkDataSet") == 0) ||
-        (strcmp(this->GetDataTypeName(i), "vtkUnstructuredGridBase") == 0))
+      else if (!dtype.ChildMatchAny && match_count == dtype.ChildTypes.size())
       {
         return 1;
       }
-      continue;
-    }
-    if (dobj->IsA(this->GetDataTypeName(i)))
-    {
-      return 1;
     }
   }
 
-  if (info->GetCompositeDataClassName())
-  {
-    vtkDataObject* cDobj =
-      vtkSMDataTypeDomainCache::GetDataObjectOfType(info->GetCompositeDataClassName());
-    // Composite data with specified child
-    for (unsigned int i = 0; i < numTypes; i++)
-    {
-      if (cDobj->IsA(this->GetDataTypeName(i)))
-      {
-        if (!this->DataTypeHasChildren(i))
-        {
-          return 1;
-        }
-        // Check child types
-        bool childMatchAny =
-          strcmp(this->GetDataTypeChildMatchTypeAsString(i), "any") == 0 ? true : false;
-        bool allChildrenMatched = true;
-        auto& dataTypes = this->GetDataTypeChildren(i);
-        vtkNew<vtkPVCompositeDataInformationIterator> iter;
-        iter->SetDataInformation(info);
-        for (iter->InitTraversal(); !iter->IsDoneWithTraversal(); iter->GoToNextItem())
-        {
-          vtkPVDataInformation* childInfo = iter->GetCurrentDataInformation();
-          if (!childInfo)
-          {
-            continue;
-          }
-          // Skip non-leaf children
-          vtkPVCompositeDataInformation* compositeInfo = childInfo->GetCompositeDataInformation();
-          if (compositeInfo->GetDataIsComposite() && !compositeInfo->GetDataIsMultiPiece())
-          {
-            continue;
-          }
-          vtkDataObject* childObj =
-            vtkSMDataTypeDomainCache::GetDataObjectOfType(childInfo->GetDataClassName());
-          bool childMatched = false;
-          for (auto& type : dataTypes)
-          {
-            if (childObj->IsA(type.c_str()))
-            {
-              childMatched = true;
-              break;
-            }
-          }
-          if (childMatched && childMatchAny)
-          {
-            return 1;
-          }
-          else if (!childMatched)
-          {
-            allChildrenMatched = false;
-          }
-        }
-        if (!childMatchAny && info->GetNumberOfBlockLeafs(true) && allChildrenMatched)
-        {
-          return 1;
-        }
-      }
-    }
-  }
   return 0;
+}
+
+//---------------------------------------------------------------------------
+std::string vtkSMDataTypeDomain::GetDomainDescription() const
+{
+  auto& types = this->DTInternals->DataTypes;
+  if (types.size() == 0)
+  {
+    return std::string{};
+  }
+
+  std::vector<std::string> phrases;
+  for (auto& typeInfo : types)
+  {
+    if (typeInfo.ChildTypes.size() == 0)
+    {
+      phrases.push_back(vtkDataObjectTypes::GetClassNameFromTypeId(typeInfo.Type));
+    }
+    else
+    {
+      std::ostringstream subPhrase;
+      subPhrase << vtkDataObjectTypes::GetClassNameFromTypeId(typeInfo.Type);
+      subPhrase << " containing";
+      for (size_t cc = 0; cc < typeInfo.ChildTypes.size(); ++cc)
+      {
+        if (cc > 0)
+        {
+          subPhrase << ",";
+          if (cc + 1 == typeInfo.ChildTypes.size())
+          {
+            subPhrase << (typeInfo.ChildMatchAny ? " or" : " and");
+          }
+        }
+        subPhrase << " " << vtkDataObjectTypes::GetClassNameFromTypeId(typeInfo.ChildTypes[cc]);
+      }
+      phrases.push_back(subPhrase.str());
+    }
+  }
+
+  std::ostringstream stream;
+  stream << "Input must be ";
+  for (size_t cc = 0; cc < phrases.size(); ++cc)
+  {
+    if (cc > 0)
+    {
+      stream << ", ";
+      if (cc + 1 == phrases.size())
+      {
+        stream << "or ";
+      }
+    }
+    stream << phrases[cc].c_str();
+  }
+  return stream.str();
 }
 
 //---------------------------------------------------------------------------
@@ -349,18 +212,17 @@ int vtkSMDataTypeDomain::ReadXMLAttributes(vtkSMProperty* prop, vtkPVXMLElement*
   int compositeDataSupported;
   if (element->GetScalarAttribute("composite_data_supported", &compositeDataSupported))
   {
-    this->SetCompositeDataSupported(compositeDataSupported);
+    this->CompositeDataSupported = (compositeDataSupported == 1);
   }
 
   int compositeDataRequired;
   if (element->GetScalarAttribute("composite_data_required", &compositeDataRequired))
   {
-    this->SetCompositeDataRequired(compositeDataRequired);
+    this->CompositeDataRequired = (compositeDataRequired == 1);
   }
 
   // Loop over the top-level elements.
-  unsigned int i;
-  for (i = 0; i < element->GetNumberOfNestedElements(); ++i)
+  for (unsigned int i = 0; i < element->GetNumberOfNestedElements(); ++i)
   {
     vtkPVXMLElement* selement = element->GetNestedElement(i);
     if (strcmp("DataType", selement->GetName()) != 0)
@@ -370,66 +232,52 @@ int vtkSMDataTypeDomain::ReadXMLAttributes(vtkSMProperty* prop, vtkPVXMLElement*
     const char* value = selement->GetAttribute("value");
     if (!value)
     {
-      vtkErrorMacro(<< "Can not find required attribute: value. "
-                    << "Can not parse domain xml.");
+      vtkErrorMacro(<< "Can not find required attribute 'value'");
       return 0;
     }
-    // For composite data with specified child data type
-    const char* childMatch = selement->GetAttribute("child_match");
-    if (childMatch)
+
+    const int typeId = vtkDataObjectTypes::GetTypeIdFromClassName(value);
+    if (typeId == -1)
     {
-      bool compositeChildMatchAny = true;
-      if (strcmp("any", childMatch) == 0)
+      vtkErrorMacro("'" << value << "' not a known data object type!");
+      return 0;
+    }
+
+    vtkSMDataTypeDomainAllowedType typeInfo;
+    typeInfo.Type = typeId;
+
+    const char* childMatch = selement->GetAttributeOrDefault("child_match", "any");
+    if (strcmp(childMatch, "any") != 0 && strcmp(childMatch, "all") != 0)
+    {
+      vtkErrorMacro("'child_match' can only be 'any' or 'all', got '" << childMatch << "'.");
+      return 0;
+    }
+    typeInfo.ChildMatchAny = (strcmp(childMatch, "any") == 0);
+    for (unsigned int cc = 0; cc < selement->GetNumberOfNestedElements(); ++cc)
+    {
+      vtkPVXMLElement* childElement = selement->GetNestedElement(cc);
+      if (strcmp("DataType", childElement->GetName()) != 0)
       {
-        compositeChildMatchAny = true;
+        continue;
       }
-      else if (strcmp("all", childMatch) == 0)
+      const char* childValue = childElement->GetAttribute("value");
+      if (!childValue)
       {
-        compositeChildMatchAny = false;
-      }
-      else
-      {
-        vtkErrorMacro("The value of child_match should be \"any\" or \"all\"."
-          << "Can not parse domain xml.");
+        vtkErrorMacro("Missing required attribute 'value'. ");
         return 0;
       }
 
-      if (!selement->GetNumberOfNestedElements())
+      const auto childTypeId = vtkDataObjectTypes::GetTypeIdFromClassName(childValue);
+      if (childTypeId == -1)
       {
-        vtkErrorMacro(<< "child_match set but no nested datatypes."
-                      << "Can not parse domain xml.");
+        vtkErrorMacro("'" << childValue << "' not a known data object type!");
         return 0;
       }
-      unsigned int j;
-      std::vector<std::string> childValues;
-      for (j = 0; j < selement->GetNumberOfNestedElements(); ++j)
-      {
-        vtkPVXMLElement* childElement = selement->GetNestedElement(j);
-        if (strcmp("DataType", childElement->GetName()) != 0)
-        {
-          continue;
-        }
-        const char* childValue = childElement->GetAttribute("value");
-        if (!childValue)
-        {
-          vtkErrorMacro(<< "Can not find required attribute: value. "
-                        << "Can not parse domain xml.");
-          return 0;
-        }
-        childValues.push_back(childValue);
-      }
-      this->DTInternals->DataTypes.emplace_back(value, true, compositeChildMatchAny, childValues);
+
+      typeInfo.ChildTypes.push_back(childTypeId);
     }
-    else if (selement->GetNumberOfNestedElements())
-    {
-      vtkErrorMacro(<< "Child type set but child_match not specified."
-                    << "Can not parse domain xml.");
-      return 0;
-    }
-    else
-    {
-      this->DTInternals->DataTypes.emplace_back(value);
-    }
+
+    this->DTInternals->DataTypes.push_back(typeInfo);
   }
   return 1;
 }
