dfdce321fd5ced791b42cff7b43cf2bd07ceb9eb
Ben Boeckel <ben.boeckel@kitware.com>
2021-01-29 13:24:47 -0500

    vtkEnsembleDataReader: avoid usage of std::ptr_fun
    Deprecated in C++11.

--- VTKExtensions/IOGeneral/vtkEnsembleDataReader.cxx
+++ VTKExtensions/IOGeneral/vtkEnsembleDataReader.cxx
@@ -156,12 +156,14 @@ int vtkEnsembleDataReader::ProcessRequest(
 // string trimmin'
 static void ltrim(std::string& s)
 {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(isspace))));
+  s.erase(s.begin(),
+    std::find_if(s.begin(), s.end(), [](std::string::value_type c) { return !isspace(c); }));
 }
 static void rtrim(std::string& s)
 {
-  s.erase(
-    std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(isspace))).base(), s.end());
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](std::string::value_type c) { return !isspace(c); })
+            .base(),
+    s.end());
 }
 static void trim(std::string& s)
 {
