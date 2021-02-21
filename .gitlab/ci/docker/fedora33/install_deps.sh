6f436e6f3f3182e8ffe50ba192fb5115e8a6fb01
Ben Boeckel <ben.boeckel@kitware.com>
2021-02-21 14:17:00 -0500

    ci: install dev tools and matplotlib into the CI image

--- .gitlab/ci/docker/fedora33/install_deps.sh
+++ .gitlab/ci/docker/fedora33/install_deps.sh
@@ -16,6 +16,10 @@ dnf install -y --setopt=install_weak_deps=False \
 dnf install -y --setopt=install_weak_deps=False \
    mesa-libOSMesa-devel mesa-libOSMesa
 
+# Development tools
+dnf install -y --setopt=install_weak_deps=False \
+    libasan libtsan libubsan clang-tools-extra
+
 # External dependencies
 dnf install -y --setopt=install_weak_deps=False \
     libXcursor-devel libharu-devel utf8cpp-devel pugixml-devel libtiff-devel \
@@ -28,7 +32,7 @@ dnf install -y --setopt=install_weak_deps=False \
 dnf install -y --setopt=install_weak_deps=False \
    python3-twisted python3-autobahn python3 python3-devel python3-numpy \
    python3-pandas python3-pandas-datareader python3-sphinx python3-pip \
-   python3-mpi4py-mpich python3-mpi4py-openmpi
+   python3-mpi4py-mpich python3-mpi4py-openmpi python3-matplotlib
 
 python3 -m pip install wslink
 
