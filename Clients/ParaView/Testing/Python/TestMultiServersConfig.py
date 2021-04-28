441ffb19cd6b1d772943a8a2e790ce2d6e8bd4b7
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2021-04-27 21:33:59 -0400

    TestMultiServersConfig: cleanup / modernize test.

--- Clients/ParaView/Testing/Python/TestMultiServersConfig.py
+++ Clients/ParaView/Testing/Python/TestMultiServersConfig.py
@@ -1,10 +1,10 @@
 from paraview.simple import *
-from paraview import servermanager
+from paraview import servermanager, print_info, print_error
 
 import time
 
 # Make sure the test driver know that process has properly started
-print ("Process started")
+print_info ("Process started")
 errors = 0
 
 #-------------------- Helpers methods ----------------
@@ -31,7 +31,7 @@ def findInSubdirectory(filename, subdirectory=''):
     raise RuntimeError ('File not found')
 #--------------------
 
-print ("Start multi-server testing")
+print_info ("Start multi-server testing")
 
 options = servermanager.vtkProcessModule.GetProcessModule().GetOptions()
 available_server_urls = options.GetServerURL().split('|')
@@ -40,57 +40,59 @@ built_in_connection = servermanager.ActiveConnection
 # Test if the built-in connection is here
 if (len(servermanager.Connections) != 1):
   errors += 1
-  print ("Error pvpython should be connected to a built-in session. Currently connected to ", servermanager.Connections)
+  print_error("Error pvpython should be connected to a built-in session. Currently connected to ", servermanager.Connections)
 
 url = available_server_urls[0]
-print ("Connect to first server ", url)
+print_info ("Connect to first server %s", url)
 server1_connection = Connect(getHost(url), getPort(url))
 
 # Test that we have one more connection
 if (len(servermanager.Connections) != 2):
   errors += 1
-  print ("Error pvpython should be connected to a built-in session + one remote one. Currently connected to ", servermanager.Connections)
+  print_error("Error pvpython should be connected to a built-in session + one remote one. Currently connected to ", servermanager.Connections)
 
 url = available_server_urls[1]
-print ("Connect to second server ", url)
+print_info ("Connect to second server %s", url)
 server2_connection = Connect(getHost(url), getPort(url))
 
 # Test that we have one more connection
 if (len(servermanager.Connections) != 3):
   errors += 1
-  print ("Error pvpython should be connected to a built-in session + two remote one. Currently connected to ", servermanager.Connections)
+  print_error("Error pvpython should be connected to a built-in session + two remote one. Currently connected to ", servermanager.Connections)
 
-print ("Available connections: ", servermanager.Connections)
+print_info ("Available connections: %s", servermanager.Connections)
 
 # Test that last created connection is the active one
 if ( servermanager.ActiveConnection != server2_connection):
   errors += 1
-  print ("Error Invalid active connection. Expected ", server2_connection, " and got ", servermanager.ActiveConnection)
+  print_error("Error Invalid active connection. Expected %s and got %s", server2_connection, servermanager.ActiveConnection)
 
 # Test that SetActiveConnection is working as expected
 SetActiveConnection(server1_connection, globals())
 if ( servermanager.ActiveConnection != server1_connection):
   errors += 1
-  print ("Error Invalid active connection. Expected ", server1_connection, " and got ", servermanager.ActiveConnection)
+  print_error("Error Invalid active connection. Expected %s and got %s", server1_connection, servermanager.ActiveConnection)
 
 # Test that SetActiveConnection is working as expected
 SetActiveConnection(built_in_connection, globals())
 if ( servermanager.ActiveConnection != built_in_connection):
   errors += 1
-  print ("Error Invalid active connection. Expected ", built_in_connection, " and got ", servermanager.ActiveConnection)
+  print_info ("Error Invalid active connection. Expected %s and got %s", built_in_connection, servermanager.ActiveConnection)
 
 # Test that SetActiveConnection is working as expected
 SetActiveConnection(server2_connection, globals())
 if ( servermanager.ActiveConnection != server2_connection):
   errors += 1
-  print ("Error Invalid active connection. Expected ", server2_connection, " and got ", servermanager.ActiveConnection)
+  print_error("Error Invalid active connection. Expected %s and got %s", server2_connection, servermanager.ActiveConnection)
 
+assert "PacMan" not in globals()
 
 # Load plugin on server2
 SetActiveConnection(server2_connection, globals())
 LoadDistributedPlugin("PacMan", True, globals())
 
 # Create PacMan on server2
+assert "PacMan" in globals()
 pacMan_s2 = PacMan()
 
 # Switch to server1 and Create PacMan ==> This should fail
@@ -98,21 +100,22 @@ SetActiveConnection(server1_connection, globals())
 try:
   pacMan_s1 = PacMan()
   errors += 1
-  print ("Error: PacMan should not be available on Server1")
+  print_error("Error: PacMan should not be available on Server1")
 except NameError:
-  print ("OK: PacMan is not available on server1")
+  print_info ("OK: PacMan is not available on server1")
 
 # Switch to server2 with globals and switch back to server1 with not updating the globals
 SetActiveConnection(server2_connection, globals())
+
 SetActiveConnection(server1_connection)
 
 # Create PacMan ==> This should fail
 try:
   pacMan_s1 = PacMan()
   errors += 1
-  print ("Error: PacMan should not be available on Server1")
+  print_error("Error: PacMan should not be available on Server1")
 except RuntimeError:
-  print ("OK: PacMan is not available on server1")
+  print_info ("OK: PacMan is not available on server1")
 
 # Make sure built-in as not the pacMan
 SetActiveConnection(server2_connection, globals())
@@ -120,33 +123,33 @@ SetActiveConnection(built_in_connection, globals())
 try:
   pacMan_builtin = PacMan()
   errors += 1
-  print ("Error: PacMan should not be available on built-in")
+  print_error("Error: PacMan should not be available on built-in")
 except NameError:
-  print ("OK: PacMan is not available on built-in")
+  print_info ("OK: PacMan is not available on built-in")
 
 # Load plugin localy for built-in
 # Create PacMan ==> This should be OK on built-in
 SetActiveConnection(built_in_connection, globals())
 LoadDistributedPlugin("PacMan", False, globals())
 pacMan_builtin = PacMan()
-print ("After loading the plugin locally in built-in, the PacMan definition is available")
+print_info ("After loading the plugin locally in built-in, the PacMan definition is available")
 
 # Switch to server1 and Create PacMan ==> This should fail
 SetActiveConnection(server1_connection, globals())
 try:
   pacMan_s1 = PacMan()
   errors += 1
-  print ("Error: PacMan should not be available on Server1")
+  print_error("Error: PacMan should not be available on Server1")
 except NameError:
-  print ("OK: PacMan is still not available on server1")
+  print_info ("OK: PacMan is still not available on server1")
 
 # Disconnect and quit application...
 Disconnect()
-print ("Available connections after disconnect: ", servermanager.Connections)
+print_info ("Available connections after disconnect: %s", servermanager.Connections)
 Disconnect()
-print ("Available connections after disconnect: ", servermanager.Connections)
+print_info ("Available connections after disconnect: %s", servermanager.Connections)
 Disconnect()
-print ("Available connections after disconnect: ", servermanager.Connections)
+print_info ("Available connections after disconnect: %s", servermanager.Connections)
 
 if errors > 0:
   raise RuntimeError ("An error occurred during the execution")
