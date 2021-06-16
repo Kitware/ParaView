c3fa991196b35a312dea906c1c04a6061a5664e3
Yohann Bearzi <yohann.bearzi@kitware.com>
2021-06-16 14:25:43 -0400

    Adding `vtkDataSet` compatibility for ghost cell gen
    A test is also being added to ensure the gost cell generator is
    available

---
+++ Clients/ParaView/Testing/Python/TestGhostCellsGenerator.py
@@ -0,0 +1,13 @@
+#### import the simple module from the paraview
+from paraview.simple import *
+
+# create 2 wavelets
+wavelet1 = Wavelet()
+wavelet2 = Wavelet()
+
+composite = GroupDatasets(Input=[wavelet1, wavelet2])
+
+# We test if the ghost cells generator can be instantiated for composite data sets as well as
+# data sets
+generator1 = UniversalGhostCellsGenerator(Input=composite)
+generator2 = UniversalGhostCellsGenerator(Input=wavelet1)
