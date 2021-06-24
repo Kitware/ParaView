010e803f69eea72b34a7de41f6ace4457385dac7
Ben Boeckel <ben.boeckel@kitware.com>
2021-06-24 11:20:15 -0400

    FindGraphviz: add imported targets

--- Plugins/NodeEditor/CMake/FindGraphviz.cmake
+++ Plugins/NodeEditor/CMake/FindGraphviz.cmake
@@ -30,13 +30,51 @@ find_library(Graphviz_PATHPLAN_LIBRARY
     )
 mark_as_advanced(Graphviz_PATHPLAN_LIBRARY)
 
-if(Graphviz_INCLUDE_DIR
-  AND Graphviz_CDT_LIBRARY
-  AND Graphviz_GVC_LIBRARY
-  AND Graphviz_CGRAPH_LIBRARY
-  AND Graphviz_PATHPLAN_LIBRARY
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Graphviz
+  REQUIRED_VARS
+    Graphviz_INCLUDE_DIR
+    Graphviz_CDT_LIBRARY
+    Graphviz_GVC_LIBRARY
+    Graphviz_CGRAPH_LIBRARY
+    Graphviz_PATHPLAN_LIBRARY
 )
-  set(Graphviz_FOUND TRUE)
-else()
-  set(Graphviz_FOUND FALSE)
+
+if(Graphviz_FOUND)
+  set(Graphviz_INCLUDE_DIRS "${Graphviz_INCLUDE_DIR}")
+  set(Graphviz_LIBRARIES
+    "${Graphviz_CDT_LIBRARY}"
+    "${Graphviz_GVC_LIBRARY}"
+    "${Graphviz_CGRAPH_LIBRARY}"
+    "${Graphviz_PATHPLAN_LIBRARY}")
+
+  if (NOT TARGET Graphviz::cdt)
+    add_library(Graphviz::cdt UNKNOWN IMPORTED)
+    set_target_properties(Graphviz::cdt PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${Graphviz_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${Graphviz_CDT_LIBRARY}")
+  endif ()
+
+  if (NOT TARGET Graphviz::pathplan)
+    add_library(Graphviz::pathplan UNKNOWN IMPORTED)
+    set_target_properties(Graphviz::pathplan PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${Graphviz_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${Graphviz_PATHPLAN_LIBRARY}")
+  endif ()
+
+  if (NOT TARGET Graphviz::cgraph)
+    add_library(Graphviz::cgraph UNKNOWN IMPORTED)
+    set_target_properties(Graphviz::cgraph PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${Graphviz_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${Graphviz_CGRAPH_LIBRARY}"
+      INTERFACE_LINK_LIBRARIES "Graphviz::cdt")
+  endif ()
+
+  if (NOT TARGET Graphviz::gvc)
+    add_library(Graphviz::gvc UNKNOWN IMPORTED)
+    set_target_properties(Graphviz::gvc PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${Graphviz_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${Graphviz_GVC_LIBRARY}"
+      INTERFACE_LINK_LIBRARIES "Graphviz::cdt;Graphviz::cgraph;Graphviz::pathplan")
+  endif ()
 endif()
