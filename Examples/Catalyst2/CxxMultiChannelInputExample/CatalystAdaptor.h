1952162685b20003917159e82c79f3d47f9e056f
Corey Wetterer-Nelson <c.wetterer-nelson@kitware.com>
2021-07-01 00:40:50 -0400

    add optional channel state with time and timestep

--- Examples/Catalyst2/CxxMultiChannelInputExample/CatalystAdaptor.h
+++ Examples/Catalyst2/CxxMultiChannelInputExample/CatalystAdaptor.h
@@ -103,6 +103,10 @@ void Execute(int cycle, double time, Grid& grid, Attributes& attribs, Particles&
   // Now add the second channel, called "particles".
   auto channel_particles = exec_params["catalyst/channels/particles"];
 
+  // make the particles' time update every other step of the mesh's
+  channel_particles["state/cycle"].set(cycle - (cycle % 2));
+  channel_particles["state/time"].set(time - (cycle % 2) * 0.1);
+
   // Since this example is using Conduit Mesh Blueprint to define the mesh,
   // we set the channel_particles's type to "mesh".
   channel_particles["type"].set_string("mesh");
--- Examples/Catalyst2/CxxMultiChannelInputExample/catalyst_pipeline.py
+++ Examples/Catalyst2/CxxMultiChannelInputExample/catalyst_pipeline.py
@@ -10,12 +10,20 @@ def catalyst_execute(info):
     grid.UpdatePipeline()
     particles.UpdatePipeline()
 
+    # test that the particles time lags behind the mesh's
+    gridTime = grid.GetDataInformation().GetTime()
+    particleTime = particles.GetDataInformation().GetTime()
+    particleTimeTest = gridTime - (info.cycle % 2)*0.1
+    assert(abs(particleTimeTest - particleTime) < 1e-12)
+
     print("-----------------------------------")
     print("executing (cycle={}, time={})".format(info.cycle, info.time))
     print("grid:")
+    print("  time:", gridTime)
     print("  bounds:", grid.GetDataInformation().GetBounds())
     print("  velocity-range:", grid.PointData["velocity"].GetRange())
     print("  pressure-range:", grid .CellData["pressure"].GetRange(0))
 
     print("particles:")
+    print("  time:", particleTime)
     print("  bounds:", particles.GetDataInformation().GetBounds())
