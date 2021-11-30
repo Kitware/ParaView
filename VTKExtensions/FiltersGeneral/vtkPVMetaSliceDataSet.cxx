73d253ec544badb1107cac8aa80f28c0a1b4af8b
Yohann Bearzi <yohann.bearzi@kitware.com>
2021-11-30 16:49:00 -0500

    Fixing pvpython crash (Clip and Slice)
    When calling `Clip()` and `Slice()`, a pvpythong script was crashing.
    This was due to a `nullptr` test missing in the meta clip and cut
    filters.
    
    Fixes #21111
    
    Backport: release

--- VTKExtensions/FiltersGeneral/vtkPVMetaSliceDataSet.cxx
+++ VTKExtensions/FiltersGeneral/vtkPVMetaSliceDataSet.cxx
@@ -163,8 +163,9 @@ int vtkPVMetaSliceDataSet::RequestDataObject(
     return 0;
   }
 
-  if (vtkHyperTreeGrid::SafeDownCast(
-        inputVector[0]->GetInformationObject(0)->Get(vtkDataObject::DATA_OBJECT())))
+  vtkInformation* info = inputVector[0]->GetInformationObject(0);
+
+  if (info && vtkHyperTreeGrid::SafeDownCast(info->Get(vtkDataObject::DATA_OBJECT())))
   {
     this->Internal->Cutter->SetCutFunction(this->ImplicitFunctions[METASLICE_HYPERTREEGRID]);
     this->Internal->ExtractCells->SetImplicitFunction(
