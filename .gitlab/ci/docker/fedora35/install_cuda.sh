65cf405395b979b540aef515cda2fe8f171ae658
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-11 13:16:12 -0500

    ci: update CI image to Fedora 35
    Also lift off of the superbuild CI and add its command to
    `install_deps.sh`.

--- .gitlab/ci/docker/fedora35/install_cuda.sh
+++ .gitlab/ci/docker/fedora35/install_cuda.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+set -e
+
 # Install tools to manage repositories.
 dnf install -y --setopt=install_weak_deps=False \
     'dnf-command(config-manager)'
 
 # Install the CUDA repository.
 dnf config-manager --add-repo \
-    https://developer.download.nvidia.com/compute/cuda/repos/fedora33/x86_64/cuda-fedora33.repo
+    https://developer.download.nvidia.com/compute/cuda/repos/fedora35/x86_64/cuda-fedora35.repo
 
 # CUDA toolchain
 dnf install -y --setopt=install_weak_deps=False \
