dc5767f17f97d0fa2e1ab65e5ba96ea1b1bba45c
Ben Boeckel <ben.boeckel@kitware.com>
2022-01-25 18:00:25 -0500

    ci: disable floating point optimizations with Intel
    At any optimization level, the `isinf` and `isnan` functions break
    otherwise.

--- .gitlab/ci/configure_el8_shared_icc_mpi_python.cmake
+++ .gitlab/ci/configure_el8_shared_icc_mpi_python.cmake
@@ -1,1 +1,6 @@
+# Disable floating point optimizations which break `isinf` and `isnan`.
+set(CMAKE_C_FLAGS "-fp-model=precise" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-fp-model=precise" CACHE STRING "")
+set(CMAKE_Fortran_FLAGS "-fp-model=precise" CACHE STRING "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_el8.cmake")
