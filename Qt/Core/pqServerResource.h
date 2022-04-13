70d626ecadc0d02c28c38a5d40f9fec4b687126a
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-04-13 11:13:58 +0200

    pqServerResource doc improvements

--- Qt/Core/pqServerResource.h
+++ Qt/Core/pqServerResource.h
@@ -105,15 +105,27 @@ class PQCORE_EXPORT pqServerResource
 {
 public:
   pqServerResource();
-  pqServerResource(const QString&);
-  pqServerResource(const QString&, const pqServerConfiguration&);
-  pqServerResource(const pqServerResource&);
-  pqServerResource& operator=(const pqServerResource&);
   ~pqServerResource();
+  pqServerResource& operator=(const pqServerResource&);
 
   /**
-   * Returns the pqServerConfiguration from which this resource was created, if
-   * any.
+   * Create an pqServerResource from a provided URI
+   */
+  pqServerResource(const QString& uri);
+
+  /**
+   * Create an pqServerResource from a provided URI and store related configuration
+   * If the configuration is named, the configuration name will be used as a serverName
+   */
+  pqServerResource(const QString& uri, const pqServerConfiguration& config);
+
+  /**
+   * Create a pqServerResource by copy
+   */
+  pqServerResource(const pqServerResource&);
+
+  /**
+   * Returns the pqServerConfiguration from which this resource was created, if any.
    */
   const pqServerConfiguration& configuration() const;
 
@@ -123,13 +135,13 @@ public:
   QString toURI() const;
 
   /**
-   * Returns a compact string representation of the resource including extra
-   * data
+   * Returns a compact string representation of the resource including extra data
    */
   QString serializeString() const;
 
-  /** Returns the resource scheme -
-  builtin, cs, csrc, cdsrs or cdsrsrc */
+  /**
+   * Returns the resource scheme, builtin, cs, csrc, cdsrs or cdsrsrc
+   */
   QString scheme() const;
 
   /**
@@ -142,64 +154,160 @@ public:
    */
   bool isReverse() const;
 
-  /** Returns the resource host, or empty string for builtin,
-  cdsrs, and cdsrsrc schemes */
+  /**
+   * Returns the resource host, or empty string for builtin, cdsrs, and cdsrsrc schemes
+   */
   QString host() const;
+
   /**
    * Sets the resource host
    */
   void setHost(const QString&);
 
+  /**
+   * Return the resource port, or -1 for builtin, cdsrs, and cdsrsrc schemes
+   */
   int port() const;
+
+  /**
+   * Return the resource port, or default_port for builtin, cdsrs, and cdsrsrc schemes
+   */
   int port(int default_port) const;
+
+  /**
+   * Set the resource port
+   */
   void setPort(int);
 
+  /**
+   * Return the data server host, if any
+   */
   QString dataServerHost() const;
+
+  /**
+   * Set the data server host
+   */
   void setDataServerHost(const QString&);
 
+  /**
+   * Return the data server port, if any, or -1
+   */
   int dataServerPort() const;
+
+  /**
+   * Return the data server port, if any, or default_port
+   */
   int dataServerPort(int default_port) const;
+
+  /**
+   * Set the data server port
+   */
   void setDataServerPort(int);
 
+  /**
+   * Return the render server host, if any
+   */
   QString renderServerHost() const;
+
+  /**
+   * Set the render server host
+   */
   void setRenderServerHost(const QString&);
 
+  /**
+   * Return the render server port, if any, or -1
+   */
   int renderServerPort() const;
+
+  /**
+   * Return the render server port, if any, or default_port
+   */
   int renderServerPort(int default_port) const;
+
+  /**
+   * Set the render server port
+   */
   void setRenderServerPort(int);
 
+  /**
+   * Return the path to the resource, if any
+   */
   QString path() const;
+
+  /**
+   * Set the path to the resource
+   */
   void setPath(const QString&);
 
+  /**
+   * Return the server name of the resource, if any
+   */
   QString serverName() const;
+
+  /**
+   * Set the server name of the resource
+   */
   void setServerName(const QString& name);
 
-  // add extra data to this resource
+  /**
+   * Add a data on this resource, with a key/value logic
+   */
   void addData(const QString& key, const QString& value);
-  // get extra data from this resource
+
+  /**
+   * Get data from this resource, associated with the provided key, if any
+   */
   QString data(const QString& key) const;
+
+  /**
+   * Get data from this resource, associated with the provided key, if any. If not, return
+   * default_value.
+   */
   QString data(const QString& key, const QString& default_value) const;
+
+  /**
+   * Return true if this resource has data associated with the provided key, false otherwise.
+   */
   bool hasData(const QString& key) const;
 
+  /**
+   * Returns a copy of this resource containing complete server information -
+   * scheme, host, port numbers and server name
+   */
   pqServerResource schemeHostsPortsServerName() const;
 
-  /** Returns a copy of this resource containing only server information -
-  scheme, host, and port numbers */
+  /**
+   * Returns a copy of this resource containing only server connection information -
+   * scheme, host, and port numbers
+   */
   pqServerResource schemeHostsPorts() const;
 
-  /** Returns a copy of this resource containing a subset of server information -
-  scheme and host (no port numbers */
+  /**
+   * Returns a copy of this resource containing a subset of server information -
+   * scheme and host (no port numbers)
+   */
   pqServerResource schemeHosts() const;
 
-  /** Returns a copy of this resource containing only host and path information -
-  scheme, and port numbers are excluded */
+  /**
+   * Returns a copy of this resource containing only host and path information -
+   * scheme and port numbers are excluded
+   */
   pqServerResource hostPath() const;
 
+  /**
+   * Returns a copy of this resource containing only path and server name information -
+   * scheme, host and port numbers are excluded
+   */
   pqServerResource pathServerName() const;
 
+  //@{
+  /**
+   * Operators comparing all elements of the resource
+   */
   bool operator==(const pqServerResource&) const;
   bool operator!=(const pqServerResource&) const;
   bool operator<(const pqServerResource&) const;
+  //@}
 
 private:
   class pqImplementation;
