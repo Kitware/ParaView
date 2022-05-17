2b3edf5dcdd846d27c08631ce2037054b4380d61
Ben Boeckel <ben.boeckel@kitware.com>
2022-05-16 21:00:03 -0400

    gitlab-ci: update to the VS 2022 17.5 toolchain

--- .gitlab/ci/configure_windows_vs2022_kits_mpi_python_qt.cmake
+++ .gitlab/ci/configure_windows_vs2022_kits_mpi_python_qt.cmake
@@ -1,4 +1,4 @@
 # Enable default-off plugin with Python dependency
 set(PARAVIEW_PLUGIN_ENABLE_NetCDFTimeAnnotationPlugin ON CACHE BOOL "")
 
-include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_vs2019.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_vs2022.cmake")
