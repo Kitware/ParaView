0b5ed63d180e069e937a086c886efad96b4570f6
Nicolas Vuaille <nicolas.vuaille@kitware.com>
2022-06-02 09:51:17 +0200

    Update copyright text in CDI Plugin

--- Plugins/CDIReader/Reader/cdi_tools.h
+++ Plugins/CDIReader/Reader/cdi_tools.h
@@ -1,17 +1,38 @@
 /*=========================================================================
- *
- *  Program:   Visualization Toolkit
- *  Module:    cdi_tools.h
- *
- *  Copyright (c) 2018 Niklas Roeber, DKRZ Hamburg
- *  All rights reserved.
- *  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
- *
- *     This software is distributed WITHOUT ANY WARRANTY; without even
- *     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- *     PURPOSE.  See the above copyright notice for more information.
- *
- *  =========================================================================*/
+
+   Program: ParaView
+   Module:  cdi_tools.h
+
+   Copyright (c) 2005,2006 Sandia Corporation, Kitware Inc.
+   All rights reserved.
+
+   ParaView is a free software; you can redistribute it and/or modify it
+   under the terms of the ParaView license version 1.2.
+
+   See License_v1.2.txt for the full ParaView license.
+   A copy of this license can be obtained by contacting
+   Kitware Inc.
+   28 Corporate Drive
+   Clifton Park, NY 12065
+   USA
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+========================================================================*/
+
+/*-------------------------------------------------------------------------
+   Copyright (c) 2018 Niklas Roeber, DKRZ Hamburg
+  -------------------------------------------------------------------------*/
 
 #ifndef CDI_TOOLS
 #define CDI_TOOLS
