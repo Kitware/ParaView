5fce10ced1e58d75fab5d57471e15b292a44c9c4
Lucas Givord <lucas.givord@kitware.com>
2022-06-13 15:13:39 +0200

    fix clearCache called for the fastpreselection

--- Remoting/Views/vtkSMRenderViewProxy.cxx
+++ Remoting/Views/vtkSMRenderViewProxy.cxx
@@ -34,6 +34,7 @@
 #include "vtkPVDataInformation.h"
 #include "vtkPVEncodeSelectionForServer.h"
 #include "vtkPVRenderView.h"
+#include "vtkPVRenderViewSettings.h"
 #include "vtkPVRenderingCapabilitiesInformation.h"
 #include "vtkPVServerInformation.h"
 #include "vtkPVXMLElement.h"
@@ -819,7 +820,12 @@ void vtkSMRenderViewProxy::MarkDirty(vtkSMProxy* modifiedProxy)
     const bool isSelectionRepresentation =
       strcmp(modifiedProxy->GetXMLGroup(), "representations") == 0 &&
       strcmp(modifiedProxy->GetXMLName(), "SelectionRepresentation") == 0;
-    forceClearCache = !(isPVExtractSelectionFilter || isSelectionRepresentation);
+
+    const bool isFastPreSelection = strcmp(modifiedProxy->GetXMLGroup(), "representations") == 0 &&
+      vtkPVRenderViewSettings::GetInstance()->GetEnableFastPreselection();
+
+    forceClearCache =
+      !(isPVExtractSelectionFilter || isSelectionRepresentation || isFastPreSelection);
   }
 
   const bool cacheCleared = this->ClearSelectionCache(forceClearCache);
