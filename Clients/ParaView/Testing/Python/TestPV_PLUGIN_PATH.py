251904f0d50317761aeb13d0627e1a37a3cee82b
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-07-08 16:41:30 +0200

    Adding a PV_PLUGIN_PATH test

---
+++ Clients/ParaView/Testing/Python/TestPV_PLUGIN_PATH.py
@@ -0,0 +1,18 @@
+from paraview.simple import *
+PythonbasedSuperquadricSourceExample()
+Show()
+rv = Render()
+
+import os
+import sys
+try:
+  baselineIndex = sys.argv.index('-B')+1
+  baselinePath = sys.argv[baselineIndex]
+except:
+  print("Could not get baseline directory. Test failed.")
+baseline_file = os.path.join(baselinePath, "TestPV_PLUGIN_PATH.png")
+from paraview.vtk.test import Testing
+from paraview.vtk.util.misc import vtkGetTempDir
+Testing.VTK_TEMP_DIR = vtkGetTempDir()
+Testing.compareImage(rv.GetRenderWindow(), baseline_file, threshold=40)
+Testing.interact()
