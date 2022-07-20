Kitware/ParaView

commit 13afd1ec223272cd739c7a8c6ba2b06844865893

pqApplicationCore: guard the `QHelpEngine` include
The implementation is guarded, but the header doesn't necessarily exist,
so guard against QtHelp being enabled.

Qt/Core/pqApplicationCore.cxx
@@ -43,7 +43,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QApplication>
 #include <QDebug>
 #include <QFile>
-#include <QHelpEngine>
 #include <QMainWindow>
 #include <QMap>
 #include <QPointer>
@@ -82,6 +81,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkCommand.h"
 #include "vtkInitializationHelper.h"
 #include "vtkLegacy.h"
+#include "vtkPVConfig.h"
 #include "vtkPVGeneralSettings.h"
 #include "vtkPVLogger.h"
 #include "vtkPVPluginTracker.h"
@@ -102,6 +102,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkSmartPointer.h"
 
 #include <cassert>
+
+#ifdef PARAVIEW_USE_QTHELP
+#include <QHelpEngine>
+#endif
 
 //-----------------------------------------------------------------------------
 class pqApplicationCore::pqInternals

commit 9bd08ab823becc1fbe79b71f95bd8f8ddc0aea03

pqApplicationCore: remove deprecated header include
This header was deprecated.

Fixes: 13afd1ec22 (pqApplicationCore: guard the `QHelpEngine` include, 2022-07-19)

Qt/Core/pqApplicationCore.cxx
@@ -81,7 +81,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkCommand.h"
 #include "vtkInitializationHelper.h"
 #include "vtkLegacy.h"
-#include "vtkPVConfig.h"
 #include "vtkPVGeneralSettings.h"
 #include "vtkPVLogger.h"
 #include "vtkPVPluginTracker.h"