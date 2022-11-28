71910b1744065ebd7df024373a7cca4d6f4bc534
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2022-11-28 15:31:55 -0500

    pqOSPRayHidingDecorator: Deactivate based environment variables
    Enable Ray Tracing only when the backend is available and the environment
    variable is not set.

--- Qt/ApplicationComponents/pqOSPRayHidingDecorator.cxx
+++ Qt/ApplicationComponents/pqOSPRayHidingDecorator.cxx
@@ -31,6 +31,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ========================================================================*/
 #include "pqOSPRayHidingDecorator.h"
 
+#if VTK_MODULE_ENABLE_VTK_RenderingRayTracing
+#include "vtkOSPRayPass.h"
+#endif
+
 //-----------------------------------------------------------------------------
 pqOSPRayHidingDecorator::pqOSPRayHidingDecorator(
   vtkPVXMLElement* config, pqPropertyWidget* parentObject)
@@ -45,9 +49,18 @@ pqOSPRayHidingDecorator::~pqOSPRayHidingDecorator() = default;
 bool pqOSPRayHidingDecorator::canShowWidget(bool show_advanced) const
 {
 #if VTK_MODULE_ENABLE_VTK_RenderingRayTracing
-  return this->Superclass::canShowWidget(show_advanced);
-#else
-  (void)show_advanced;
-  return false;
+  bool enableVisRTX = vtkOSPRayPass::IsBackendAvailable("optix pathtracer") &&
+    vtksys::SystemTools::GetEnv("VTK_DISABLE_VISRTX") == nullptr;
+  bool enableOSPRay = vtkOSPRayPass::IsBackendAvailable("OSPRay pathtracer") &&
+    vtksys::SystemTools::GetEnv("VTK_DISABLE_OSPRAY") == nullptr;
+  if (enableOSPRay || enableVisRTX)
+  {
+    return this->Superclass::canShowWidget(show_advanced);
+  }
+  else
 #endif
+  {
+    (void)show_advanced;
+    return false;
+  }
 }
