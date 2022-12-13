4400dac31d63f3ff5ecba2be4955d7cbae897445
Tiffany Chhim <tiffany.chhim@kitware.com>
2022-12-13 12:01:08 +0100

    XRInterface: Remove string label comparisons for combobox values
    The properties for the right trigger action and movement style were
    handled using string comparisons. This would create issues e.g. when
    labels are modified or translated in another language.

--- Plugins/XRInterface/Plugin/vtkPVXRInterfaceHelper.cxx
+++ Plugins/XRInterface/Plugin/vtkPVXRInterfaceHelper.cxx
@@ -729,11 +729,11 @@ void vtkPVXRInterfaceHelper::SetHoverPick(bool val)
 }
 
 //----------------------------------------------------------------------------
-void vtkPVXRInterfaceHelper::SetRightTriggerMode(std::string const& text)
+void vtkPVXRInterfaceHelper::SetRightTriggerMode(int index)
 {
   this->HideBillboard();
   this->CollaborationClient->HideBillboard();
-  this->RightTriggerMode = text;
+  this->RightTriggerMode = static_cast<vtkPVXRInterfaceHelper::RightTriggerAction>(index);
 
   auto style = this->Interactor
     ? vtkVRInteractorStyle::SafeDownCast(this->Interactor->GetInteractorStyle())
@@ -751,41 +751,44 @@ void vtkPVXRInterfaceHelper::SetRightTriggerMode(std::string const& text)
 
     if (vrmodel)
     {
-      vrmodel->SetShowRay(this->QWidgetWidget->GetEnabled() || text == "Pick");
+      vrmodel->SetShowRay(this->QWidgetWidget->GetEnabled() ||
+        this->RightTriggerMode == vtkPVXRInterfaceHelper::PICK);
     }
 
     style->GrabWithRayOff();
 
-    if (text == "Grab")
+    switch (this->RightTriggerMode)
     {
-      style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_POSITION_PROP);
-    }
-    else if (text == "Pick")
-    {
-      style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_POSITION_PROP);
-      style->GrabWithRayOn();
-    }
-    else if (text == "Interactive Crop")
-    {
-      style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_CLIP);
-    }
-    else if (text == "Probe")
-    {
-      style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_PICK);
+      case vtkPVXRInterfaceHelper::GRAB:
+        style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_POSITION_PROP);
+        break;
+      case vtkPVXRInterfaceHelper::PICK:
+        style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_POSITION_PROP);
+        style->GrabWithRayOn();
+        break;
+      case vtkPVXRInterfaceHelper::INTERACTIVE_CROP:
+        style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_CLIP);
+        break;
+      case vtkPVXRInterfaceHelper::PROBE:
+        style->MapInputToAction(vtkCommand::Select3DEvent, VTKIS_PICK);
+        break;
+      default:
+        break;
     }
   }
 }
 
 //----------------------------------------------------------------------------
-void vtkPVXRInterfaceHelper::SetMovementStyle(vtkVRInteractorStyle::MovementStyle style)
+void vtkPVXRInterfaceHelper::SetMovementStyle(int index)
 {
   // Get interactor style
   auto interactorStyle = this->Interactor
     ? vtkVRInteractorStyle::SafeDownCast(this->Interactor->GetInteractorStyle())
     : nullptr;
+
   if (interactorStyle)
   {
-    interactorStyle->SetStyle(style);
+    interactorStyle->SetStyle(static_cast<vtkVRInteractorStyle::MovementStyle>(index));
   }
 }
 
@@ -821,7 +824,7 @@ bool vtkPVXRInterfaceHelper::InteractorEventCallback(
     }
 
     // in add point mode, then do that
-    if (this->RightTriggerMode == "Add Point To Source")
+    if (this->RightTriggerMode == vtkPVXRInterfaceHelper::ADD_POINT_TO_SOURCE)
     {
       if (edd->GetAction() == vtkEventDataAction::Press)
       {
@@ -2108,9 +2111,9 @@ void vtkPVXRInterfaceHelper::SendToXR(vtkSMViewProxy* smview)
   if (vrRenWin && vrRenWin->GetInitialized())
   {
     // Set initial values
-    this->SetRightTriggerMode("Pick");
-    this->XRInterfaceControls->SetRightTriggerMode("Pick");
-    this->XRInterfaceControls->SetMovementStyle("Flying");
+    this->SetRightTriggerMode(vtkPVXRInterfaceHelper::PICK);
+    this->XRInterfaceControls->SetRightTriggerMode(vtkPVXRInterfaceHelper::PICK);
+    this->XRInterfaceControls->SetMovementStyle(vtkVRInteractorStyle::FLY_STYLE);
     this->XRInterfaceControls->SetCurrentScaleFactor(1);
     this->XRInterfaceControls->SetCurrentMotionFactor(1);
     this->XRInterfaceControls->SetCurrentSavedPosition(-1);
