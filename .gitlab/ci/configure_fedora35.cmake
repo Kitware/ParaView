7fb1bfce8c3ccc0476f0f2bbafd78a2c065f6a9a
Thomas Galland <thomas.galland@kitware.com>
2022-12-21 12:14:42 +0100

    Add "sample rate from time data" option
    Allow the player to deduce the sample rate directly from the "time" row data if found.
    The value is set to the value contained in the "sample_rate" field data otherwise.
    If both are not found, it's set to 44100Hz.

--- .gitlab/ci/configure_fedora35.cmake
+++ .gitlab/ci/configure_fedora35.cmake
@@ -1,5 +1,6 @@
 
 # Enable default-off plugin with Python dependency
 set(PARAVIEW_PLUGIN_ENABLE_NetCDFTimeAnnotationPlugin ON CACHE BOOL "")
+set(PARAVIEW_PLUGIN_dsp_enable_audio_player ON CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora_common.cmake")
