75d477003c78d0ad486ba228fb6280b3fb133d50
Charly Bollinger <charly.bollinger@kitware.com>
2023-02-17 11:03:43 +0100

    Add tests for slice filter's locator property

---
+++ Remoting/Application/Testing/Python/SliceBackwardsCompatibilityTest.py
@@ -0,0 +1,18 @@
+import paraview
+from paraview.simple import *
+
+# This script tests backwards compatibility for the Slice filter.
+# Replaces MergePoints property with Locator one
+
+paraview.compatibility.major = 5
+paraview.compatibility.minor = 11
+
+wavelet = Wavelet()
+
+sliceFilter = Slice(Input=wavelet)
+assert(sliceFilter.MergePoints == 1)
+assert(sliceFilter.GetProperty("Locator").GetData().SMProxy.GetXMLLabel() == "Uniform Binning")
+
+sliceFilter.MergePoints = 0
+assert(sliceFilter.MergePoints == 0)
+assert(sliceFilter.GetProperty("Locator").GetData().SMProxy.GetXMLLabel() == "Don't Merge Points")
