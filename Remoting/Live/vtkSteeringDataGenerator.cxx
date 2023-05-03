e2fdf678eaf01f9b1723c865cc8c0e7210b2916e
Lucas Givord <lucas.givord@kitware.com>
2023-05-03 14:19:32 +0200

    vtkSteeringDataGenerator: add optional selection input
    Usefull to send to the simulation a selection performed in ParaView

--- Remoting/Live/vtkSteeringDataGenerator.cxx
+++ Remoting/Live/vtkSteeringDataGenerator.cxx
@@ -26,9 +26,15 @@
 #include "vtkObjectFactory.h"
 #include "vtkPoints.h"
 #include "vtkPolyData.h"
+#include "vtkSelection.h"
+#include "vtkSelectionNode.h"
 #include "vtkSmartPointer.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkStringArray.h"
 #include "vtkTable.h"
+
+#include "vtkCellData.h"
+#include "vtkPointData.h"
 
 #include <array>
 #include <cassert>
@@ -38,7 +44,7 @@
 class vtkSteeringDataGenerator::vtkInternals
 {
 public:
-  std::map<std::string, vtkSmartPointer<vtkDataArray>> Arrays;
+  std::map<std::string, vtkSmartPointer<vtkAbstractArray>> Arrays;
 
   template <typename VTKArrayType, int NumComponents,
     typename ValueType = typename VTKArrayType::ValueType>
@@ -69,6 +75,33 @@ public:
     vtkarray->SetTypedTuple(index, tuple.data());
   }
 
+  template <typename VTKArrayType, typename ValueType = typename VTKArrayType::ValueType>
+  void SetTupleFromVector(
+    const std::string& arrayname, vtkIdType index, const std::vector<ValueType>& tuple)
+  {
+    VTKArrayType* vtkarray = nullptr;
+    auto iter = this->Arrays.find(arrayname);
+    if (iter == this->Arrays.end())
+    {
+      vtkarray = VTKArrayType::New();
+      vtkarray->SetNumberOfComponents(1);
+      vtkarray->SetName(arrayname.c_str());
+      this->Arrays[arrayname].TakeReference(vtkarray);
+    }
+    else
+    {
+      vtkarray = VTKArrayType::SafeDownCast(iter->second.GetPointer());
+      assert(vtkarray != nullptr);
+    }
+
+    if (vtkarray->GetNumberOfTuples() < (index + 1))
+    {
+      vtkarray->Resize(vtkarray->GetSize() * 2 + 1);
+      vtkarray->SetNumberOfTuples(index + 1);
+    }
+    vtkarray->SetTypedTuple(index, tuple.data());
+  }
+
   bool Validate()
   {
     // ensure all arrays have same number of tuples.
@@ -76,6 +109,13 @@ public:
       this->Arrays.empty() ? 0 : this->Arrays.begin()->second->GetNumberOfTuples();
     for (const auto& pair : this->Arrays)
     {
+      // As selection will be pass through 'state/fields', ignore it  because arrays can have
+      // different number of tuples
+      if (pair.first.find("selection") != std::string::npos)
+      {
+        continue;
+      }
+
       if (pair.second->GetNumberOfTuples() != numTuples)
       {
         vtkLogF(ERROR, "Incorrect number of tuples for array '%s' expected %d, got %d",
@@ -95,7 +135,7 @@ vtkSteeringDataGenerator::vtkSteeringDataGenerator()
   , FieldAssociation{ vtkDataObject::FIELD_ASSOCIATION_ROWS }
   , Internals(new vtkSteeringDataGenerator::vtkInternals())
 {
-  this->SetNumberOfInputPorts(0);
+  this->SetNumberOfInputPorts(1);
   this->SetNumberOfOutputPorts(1);
 }
 
@@ -104,6 +144,18 @@ vtkSteeringDataGenerator::~vtkSteeringDataGenerator()
 {
   delete this->Internals;
   this->Internals = nullptr;
+}
+
+//------------------------------------------------------------------------------
+int vtkSteeringDataGenerator::FillInputPortInformation(int port, vtkInformation* info)
+{
+  if (port == 0)
+  {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkSelection");
+    info->Set(vtkAlgorithm::INPUT_IS_OPTIONAL(), 1);
+  }
+
+  return 1;
 }
 
 //----------------------------------------------------------------------------
@@ -125,7 +177,7 @@ int vtkSteeringDataGenerator::RequestInformation(
 
 //----------------------------------------------------------------------------
 int vtkSteeringDataGenerator::RequestData(vtkInformation* vtkNotUsed(request),
-  vtkInformationVector** vtkNotUsed(inputVector), vtkInformationVector* outputVector)
+  vtkInformationVector** inputVector, vtkInformationVector* outputVector)
 {
   vtkMultiBlockDataSet* output = vtkMultiBlockDataSet::GetData(outputVector);
   output->Initialize();
@@ -148,6 +200,12 @@ int vtkSteeringDataGenerator::RequestData(vtkInformation* vtkNotUsed(request),
     return 1;
   }
 
+  vtkSelection* selection = vtkSelection::GetData(inputVector[0]);
+  if (selection)
+  {
+    this->TransferSelectionToInternals(selection);
+  }
+
   if (!this->Internals->Validate())
   {
     return 0;
@@ -164,7 +222,8 @@ int vtkSteeringDataGenerator::RequestData(vtkInformation* vtkNotUsed(request),
     arrays.find("coords") != arrays.end() && vtkPointSet::SafeDownCast(data) != nullptr)
   {
     vtkNew<vtkPoints> pts;
-    pts->SetData(arrays["coords"]);
+    auto* dataArray = vtkDataArray::SafeDownCast(arrays["coords"]);
+    pts->SetData(dataArray);
 
     auto ps = vtkPointSet::SafeDownCast(data);
     ps->SetPoints(pts);
@@ -172,15 +231,85 @@ int vtkSteeringDataGenerator::RequestData(vtkInformation* vtkNotUsed(request),
     arrays.erase("coords");
   }
 
-  if (auto fd = data->GetAttributesAsFieldData(this->FieldAssociation))
+  for (const auto& pair : arrays)
   {
-    for (const auto& pair : arrays)
+    vtkFieldData* fd = nullptr;
+    if (pair.first.find("selection") != std::string::npos)
     {
-      fd->AddArray(pair.second);
+      fd = data->GetFieldData();
     }
+    else
+    {
+      fd = data->GetAttributesAsFieldData(this->FieldAssociation);
+    }
+
+    if (!fd)
+    {
+      continue;
+    }
+
+    fd->AddArray(pair.second);
   }
 
   return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkSteeringDataGenerator::TransferSelectionToInternals(vtkSelection* selection)
+{
+  if (selection->GetNumberOfNodes() > 1)
+  {
+    vtkWarningMacro(
+      "Selection with multiple node isn't supported for now as expression isn't evaluate. Only the "
+      "first node of the current selection will be considered for the steering");
+  }
+
+  // Recreate the list of id selection
+  bool shouldTransferFieldType = true;
+  for (unsigned int i = 0; i < selection->GetNumberOfNodes(); i++)
+  {
+    vtkSelectionNode* node = selection->GetNode(i);
+    int contentType = node->GetContentType();
+    if (strcmp(node->GetContentTypeAsString(contentType), "INDICES") != 0)
+    {
+      vtkWarningMacro(
+        "Only selection based on id are supported: " << node->GetContentTypeAsString(contentType));
+      return;
+    }
+
+    // for usability on the simulation side, we need also to provide the fieldtype of the currrent
+    // selection
+    if (shouldTransferFieldType)
+    {
+      int fieldtype = node->GetFieldType();
+      const char* fieldtypeAsString = node->GetFieldTypeAsString(fieldtype);
+
+      vtkStringArray* selectionFieldType;
+      selectionFieldType = vtkStringArray::New();
+      selectionFieldType->SetName("selection_fieldtype");
+      selectionFieldType->InsertNextValue(fieldtypeAsString);
+
+      this->Internals->Arrays["selection_fieldtype"].TakeReference(selectionFieldType);
+
+      shouldTransferFieldType = false;
+    }
+
+    // then for the selection id itself
+    vtkIntArray* selectionIdx;
+    selectionIdx = vtkIntArray::New();
+    selectionIdx->SetName("selection_indices");
+    auto* selectionData = node->GetSelectionData();
+    auto* dataArray = selectionData->GetArray(0);
+    for (vtkIdType id = 0; id < dataArray->GetNumberOfTuples(); id++)
+    {
+      int dataValue = dataArray->GetTuple1(id);
+      selectionIdx->InsertNextValue(dataValue);
+    }
+    this->Internals->Arrays["selection_indices"].TakeReference(selectionIdx);
+
+    // As we didn't support expression for now, we only handle the first node
+    break;
+  }
 }
 
 //----------------------------------------------------------------------------
