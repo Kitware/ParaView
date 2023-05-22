b40d905c651cc06f8cb21f3956720a575da4d230
Tiffany Chhim <tiffany.chhim@kitware.com>
2023-05-22 13:57:30 +0200

    Temporal Multiplexing: Add filter to create 3D arrays from temporal data
    This filter creates a vtkTable with 3D arrays based on a temporal input.
    Each array of the input is converted to a 3D array defined by (index, tuple,
    component), corresponding to (point/cell, timestep, component).
    The arrays are vtkMultiDimensionalArray objects.

---
+++ Plugins/DSP/Filters/vtkTemporalMultiplexing.cxx
@@ -0,0 +1,376 @@
+/*=========================================================================
+
+  Plugin:   DigitalSignalProcessing
+  Module:   vtkTemporalMultiplexing.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkTemporalMultiplexing.h"
+
+#include "vtkArrayDispatch.h"
+#include "vtkArrayDispatchArrayList.h"
+#include "vtkCellData.h"
+#include "vtkCommand.h"
+#include "vtkDataArray.h"
+#include "vtkDataArrayRange.h"
+#include "vtkDataObject.h"
+#include "vtkDataSet.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkMultiDimensionalArray.h"
+#include "vtkMultiDimensionalImplicitBackend.h"
+#include "vtkObjectFactory.h"
+#include "vtkPointData.h"
+#include "vtkSMPTools.h"
+#include "vtkSmartPointer.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkTable.h"
+
+#include <map>
+#include <vector>
+
+struct vtkTemporalMultiplexing::vtkInternals
+{
+  std::map<std::string, std::vector<vtkSmartPointer<vtkDataArray>>> Arrays;
+};
+
+//------------------------------------------------------------------------------
+namespace
+{
+//------------------------------------------------------------------------------
+struct CreateArrayVector
+{
+  template <typename TArray, typename ValueType = vtk::GetAPIType<TArray>>
+  void operator()(TArray* vtkNotUsed(inArray), vtkSmartPointer<vtkDataArray>& outArray)
+  {
+    outArray.TakeReference(vtkAOSDataArrayTemplate<ValueType>::New());
+  }
+};
+
+//------------------------------------------------------------------------------
+struct ConstructMDArray
+{
+  template <typename TArray, typename ValueType = vtk::GetAPIType<TArray>>
+  void operator()(TArray* vtkNotUsed(dummyArray), const std::string& name,
+    const std::vector<vtkSmartPointer<vtkDataArray>>& arrayVector, vtkTable* output)
+  {
+    // Downcast each vtkDataArray into vtkAOSDataArrayTemplate for the backend
+    std::vector<vtkSmartPointer<vtkAOSDataArrayTemplate<ValueType>>> aosVector;
+    aosVector.reserve(arrayVector.size());
+
+    vtkSMPTools::For(0, arrayVector.size(), [&](vtkIdType begin, vtkIdType end) {
+      for (vtkIdType idx = begin; idx < end; idx++)
+      {
+        vtkSmartPointer<vtkAOSDataArrayTemplate<ValueType>> aosArray =
+          vtkAOSDataArrayTemplate<ValueType>::FastDownCast(arrayVector[idx]);
+        aosVector.emplace_back(aosArray);
+      }
+    });
+
+    vtkNew<vtkMultiDimensionalArray<ValueType>> mdArray;
+    mdArray->SetName(name.c_str());
+    mdArray->ConstructBackend(aosVector);
+    output->AddColumn(mdArray);
+  }
+};
+
+//------------------------------------------------------------------------------
+struct ConstructVTKIdTypeMDArray
+{
+  // Template specialization of ConstructMDArray is not possible due to vtkIdType
+  // being a typedef
+  template <typename TArray>
+  void operator()(TArray* vtkNotUsed(dummyArray), const std::string& name,
+    const std::vector<vtkSmartPointer<vtkDataArray>>& arrayVector, vtkTable* output)
+  {
+    // Downcast each vtkDataArray into vtkAOSDataArrayTemplate for the backend
+    std::vector<vtkSmartPointer<vtkAOSDataArrayTemplate<vtkIdType>>> aosVector;
+    aosVector.reserve(arrayVector.size());
+
+    vtkSMPTools::For(0, arrayVector.size(), [&](vtkIdType begin, vtkIdType end) {
+      for (vtkIdType idx = begin; idx < end; idx++)
+      {
+        vtkSmartPointer<vtkAOSDataArrayTemplate<vtkIdType>> aosArray =
+          vtkAOSDataArrayTemplate<vtkIdType>::FastDownCast(arrayVector[idx]);
+        aosVector.emplace_back(aosArray);
+      }
+    });
+
+    vtkNew<vtkMultiDimensionalArray<vtkIdType>> mdArray;
+    mdArray->SetName(name.c_str());
+    mdArray->ConstructBackend(aosVector);
+    output->AddColumn(mdArray);
+  }
+};
+}
+
+//------------------------------------------------------------------------------
+vtkStandardNewMacro(vtkTemporalMultiplexing);
+
+//------------------------------------------------------------------------------
+vtkTemporalMultiplexing::vtkTemporalMultiplexing()
+  : Internals(new vtkTemporalMultiplexing::vtkInternals())
+{
+}
+
+//------------------------------------------------------------------------------
+void vtkTemporalMultiplexing::EnableAttributeArray(const std::string& arrName)
+{
+  if (!arrName.empty())
+  {
+    if (this->SelectedArrays.insert(arrName).second)
+    {
+      this->Modified();
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+void vtkTemporalMultiplexing::ClearAttributeArrays()
+{
+  if (!this->SelectedArrays.empty())
+  {
+    this->SelectedArrays.clear();
+    this->Modified();
+  }
+}
+
+//------------------------------------------------------------------------------
+int vtkTemporalMultiplexing::FillInputPortInformation(int vtkNotUsed(port), vtkInformation* info)
+{
+  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+int vtkTemporalMultiplexing::RequestInformation(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+  // Retrieve total number of timesteps
+  vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
+
+  if (inInfo->Has(vtkStreamingDemandDrivenPipeline::TIME_STEPS()))
+  {
+    this->NumberOfTimeSteps = inInfo->Length(vtkStreamingDemandDrivenPipeline::TIME_STEPS());
+  }
+  else
+  {
+    this->NumberOfTimeSteps = 1;
+  }
+
+  // Output is not temporal
+  auto outInfo = outputVector->GetInformationObject(0);
+  outInfo->Remove(vtkStreamingDemandDrivenPipeline::TIME_STEPS());
+  outInfo->Remove(vtkStreamingDemandDrivenPipeline::TIME_RANGE());
+
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+int vtkTemporalMultiplexing::RequestUpdateExtent(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** inputVector, vtkInformationVector* vtkNotUsed(outputVector))
+{
+  vtkInformation* inInfo = inputVector[0]->GetInformationObject(0);
+  double* inTimes = inInfo->Get(vtkStreamingDemandDrivenPipeline::TIME_STEPS());
+
+  if (inTimes)
+  {
+    inInfo->Set(
+      vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP(), inTimes[this->CurrentTimeIndex]);
+  }
+
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+int vtkTemporalMultiplexing::RequestData(
+  vtkInformation* request, vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+  vtkDataSet* input = vtkDataSet::GetData(inputVector[0]);
+  vtkTable* output = vtkTable::GetData(outputVector, 0);
+
+  if (!output)
+  {
+    vtkErrorMacro("Missing valid output.");
+    return 0;
+  }
+
+  if (this->SelectedArrays.empty())
+  {
+    // Reset table to empty state
+    output->Initialize();
+    return 1;
+  }
+
+  // Check that dataset is actually temporal
+  if (this->NumberOfTimeSteps <= 0)
+  {
+    vtkWarningMacro("There should be at least one timestep (non temporal).");
+    return 0;
+  }
+
+  // Retrieve data set attributes (point or cell data)
+  vtkDataSetAttributes* dataAttributes = nullptr;
+  vtkIdType nbArrays = 0;
+
+  if (this->FieldAssociation == vtkDataObject::POINT)
+  {
+    dataAttributes = input->GetPointData();
+    nbArrays = input->GetNumberOfPoints();
+  }
+  else if (this->FieldAssociation == vtkDataObject::CELL)
+  {
+    dataAttributes = input->GetCellData();
+    nbArrays = input->GetNumberOfCells();
+  }
+  else
+  {
+    vtkWarningMacro("Invalid field association. Only point and cell associations are supported. "
+                    "Defaulting to point association.");
+    dataAttributes = input->GetPointData();
+    nbArrays = input->GetNumberOfPoints();
+  }
+
+  // For the first request, let the pipeline know it should loop and setup arrays
+  if (this->CurrentTimeIndex == 0)
+  {
+    request->Set(vtkStreamingDemandDrivenPipeline::CONTINUE_EXECUTING(), 1);
+    this->Internals->Arrays.clear();
+    this->PrepareVectorsOfArrays(dataAttributes, nbArrays);
+  }
+
+  // Retrieve each data array then add it to the vector
+  // of arrays for the current timestep
+  this->FillArraysForCurrentTimestep(dataAttributes);
+
+  // Stop looping when the last timestep has been processed and prepare output
+  this->CurrentTimeIndex++;
+
+  if (this->CurrentTimeIndex == this->NumberOfTimeSteps)
+  {
+    request->Remove(vtkStreamingDemandDrivenPipeline::CONTINUE_EXECUTING());
+    this->CurrentTimeIndex = 0;
+    this->CreateMultiDimensionalArrays(output);
+  }
+
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+void vtkTemporalMultiplexing::PrepareVectorsOfArrays(
+  vtkDataSetAttributes* attributes, vtkIdType nbArrays)
+{
+  using SupportedArrays = vtkArrayDispatch::Arrays;
+  using Dispatcher = vtkArrayDispatch::DispatchByArray<SupportedArrays>;
+  CreateArrayVector worker;
+
+  for (const auto& name : this->SelectedArrays)
+  {
+    vtkDataArray* array = vtkDataArray::SafeDownCast(attributes->GetAbstractArray(name.c_str()));
+
+    if (!array)
+    {
+      continue;
+    }
+
+    // AOS arrays must be created for multidimensional arrays
+    std::vector<vtkSmartPointer<vtkDataArray>> arrays;
+    arrays.reserve(nbArrays);
+
+    // Create AOS array with correct type
+    vtkSmartPointer<vtkDataArray> refArray;
+
+    if (!Dispatcher::Execute(array, worker, refArray))
+    {
+      worker(array, refArray);
+    }
+
+    for (vtkIdType idx = 0; idx < nbArrays; idx++)
+    {
+      vtkSmartPointer<vtkDataArray> newArray;
+      newArray.TakeReference(refArray->NewInstance());
+      newArray->SetNumberOfComponents(array->GetNumberOfComponents());
+      newArray->SetNumberOfTuples(this->NumberOfTimeSteps);
+      newArray->SetName(name.c_str());
+      arrays.emplace_back(newArray);
+    }
+
+    this->Internals->Arrays[name] = arrays;
+  }
+}
+
+//------------------------------------------------------------------------------
+void vtkTemporalMultiplexing::FillArraysForCurrentTimestep(vtkDataSetAttributes* attributes)
+{
+  vtkIdType nbArrays = this->Internals->Arrays.begin()->second.size();
+
+  for (auto arrayVec : this->Internals->Arrays)
+  {
+    vtkDataArray* array = attributes->GetArray(arrayVec.first.c_str());
+
+    if (!array)
+    {
+      continue;
+    }
+
+    vtkIdType nbComp = array->GetNumberOfComponents();
+
+    vtkSMPTools::For(0, nbArrays, [&](vtkIdType begin, vtkIdType end) {
+      for (vtkIdType idx = begin; idx < end; idx++)
+      {
+        for (vtkIdType comp = 0; comp < nbComp; comp++)
+        {
+          arrayVec.second[idx]->SetComponent(
+            this->CurrentTimeIndex, comp, array->GetComponent(idx, comp));
+        }
+      }
+    });
+  }
+}
+
+//------------------------------------------------------------------------------
+void vtkTemporalMultiplexing::CreateMultiDimensionalArrays(vtkTable* output)
+{
+  // Create multi dimensional arrays from each vector of arrays
+  using SupportedArrays = vtkArrayDispatch::Arrays;
+  using Dispatcher = vtkArrayDispatch::DispatchByArray<SupportedArrays>;
+  ConstructMDArray worker;
+  ConstructVTKIdTypeMDArray idTypeWorker;
+
+  for (const auto& arrayInfo : this->Internals->Arrays)
+  {
+    if (vtkAOSDataArrayTemplate<vtkIdType>::FastDownCast(arrayInfo.second[0]))
+    {
+      if (!Dispatcher::Execute(
+            arrayInfo.second[0], idTypeWorker, arrayInfo.first, arrayInfo.second, output))
+      {
+        idTypeWorker(arrayInfo.second[0].Get(), arrayInfo.first, arrayInfo.second, output);
+      }
+    }
+    else
+    {
+      if (!Dispatcher::Execute(
+            arrayInfo.second[0], worker, arrayInfo.first, arrayInfo.second, output))
+      {
+        worker(arrayInfo.second[0].Get(), arrayInfo.first, arrayInfo.second, output);
+      }
+    }
+  }
+}
+
+//--------------------------------------- --------------------------------------
+void vtkTemporalMultiplexing::PrintSelf(std::ostream& os, vtkIndent indent)
+{
+  os << indent << "NumberOfTimeSteps: " << this->NumberOfTimeSteps << endl;
+  os << indent << "CurrentTimeIndex: " << this->CurrentTimeIndex << endl;
+  os << indent << "FieldAssociation: " << this->FieldAssociation << endl;
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ Plugins/DSP/Filters/vtkTemporalMultiplexing.h
@@ -0,0 +1,108 @@
+/*=========================================================================
+
+  Plugin:   DigitalSignalProcessing
+  Module:   vtkTemporalMultiplexing.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+/**
+ * @class   vtkTemporalMultiplexing
+ *
+ * This filter creates a vtkTable containing 3D arrays based on a temporal input.
+ * Each point/cell array of the input is converted to a 3D array defined by
+ * (index, tuple, component), corresponding to (point/cell, timestep, component).
+ * The arrays are vtkMultiDimensionalArray objects.
+ *
+ * Note: the filter currently does not support composite datasets.
+ */
+
+#ifndef vtkTemporalMultiplexing_h
+#define vtkTemporalMultiplexing_h
+
+#include "vtkDSPFiltersPluginModule.h" // for export macro
+#include "vtkNew.h"                    // for vtkNew
+#include "vtkTableAlgorithm.h"
+
+#include <memory> // for unique_ptr
+#include <set>    // for std::set
+#include <string> // for std::string
+
+class vtkDataSetAttributes;
+class vtkTable;
+
+class VTKDSPFILTERSPLUGIN_EXPORT vtkTemporalMultiplexing : public vtkTableAlgorithm
+{
+public:
+  static vtkTemporalMultiplexing* New();
+  vtkTypeMacro(vtkTemporalMultiplexing, vtkTableAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  ///@{
+  /**
+   * Set/get the field association for arrays.
+   * Default is point.
+   */
+  vtkSetMacro(FieldAssociation, int);
+  vtkGetMacro(FieldAssociation, int);
+  ///@}
+
+  //@{
+  /**
+   * Handle attribute arrays listing.
+   */
+  void EnableAttributeArray(const std::string& arrName);
+  void ClearAttributeArrays();
+  ///@}
+
+protected:
+  vtkTemporalMultiplexing();
+  ~vtkTemporalMultiplexing() override = default;
+
+  int FillInputPortInformation(int port, vtkInformation* info) override;
+  int RequestInformation(vtkInformation* request, vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
+  int RequestUpdateExtent(vtkInformation* request, vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
+  int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
+
+private:
+  vtkTemporalMultiplexing(const vtkTemporalMultiplexing&) = delete;
+  void operator=(const vtkTemporalMultiplexing&) = delete;
+
+  /**
+   * Create and allocate vectors of arrays based on the given point or cell data.
+   * Each vector has a number of elements matching the number of points or cells.
+   * Each array in a vector has a number of tuples equal to the number of input
+   * timesteps.
+   */
+  void PrepareVectorsOfArrays(vtkDataSetAttributes* attributes, vtkIdType nbArrays);
+
+  /**
+   * Retrieve the values for each array at the current timestep and store them in
+   * the corresponding vectors of arrays.
+   */
+  void FillArraysForCurrentTimestep(vtkDataSetAttributes* attributes);
+
+  /**
+   * Create multi dimensional arrays once all timesteps have been processed.
+   * Should run after the last call to RequestData().
+   */
+  void CreateMultiDimensionalArrays(vtkTable* output);
+
+  struct vtkInternals;
+  std::unique_ptr<vtkInternals> Internals;
+  int NumberOfTimeSteps = 0;
+  int CurrentTimeIndex = 0;
+  int FieldAssociation = 0;
+  std::set<std::string> SelectedArrays;
+};
+
+#endif // vtkTemporalMultiplexing_h
