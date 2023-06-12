550a87bd32c6615ca8b324f6603b30e26e388ea3
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2023-06-12 13:08:12 -0400

    Add StringReader Proxy

---
+++ VTKExtensions/IOCore/vtkStringReader.h
@@ -0,0 +1,64 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkStringReader.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/**
+ * @class   vtkStringReader
+ * @brief   Read a file and return a string.
+ */
+#ifndef vtkStringReader_h
+#define vtkStringReader_h
+
+#include "vtkAlgorithm.h"
+#include "vtkPVVTKExtensionsIOCoreModule.h" //needed for exports
+
+class VTKPVVTKEXTENSIONSIOCORE_EXPORT vtkStringReader : public vtkAlgorithm
+{
+public:
+  static vtkStringReader* New();
+  vtkTypeMacro(vtkStringReader, vtkAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  ///@{
+  /**
+   * Get the read string.
+   */
+  vtkGetCharFromStdStringMacro(String);
+  ///@}
+
+  ///@{
+  /**
+   * Specify the file name to read from.
+   */
+  vtkSetStdStringFromCharMacro(FileName);
+  vtkGetCharFromStdStringMacro(FileName);
+  ///@}
+protected:
+  vtkStringReader();
+  ~vtkStringReader() override;
+
+  int FillInputPortInformation(int port, vtkInformation* info) override;
+  int FillOutputPortInformation(int port, vtkInformation* info) override;
+  vtkTypeBool ProcessRequest(vtkInformation* request, vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
+  virtual int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
+
+private:
+  vtkStringReader(const vtkStringReader&) = delete;
+  void operator=(const vtkStringReader&) = delete;
+
+  std::string String;
+  std::string FileName;
+};
+
+#endif // vtkStringReader_h
