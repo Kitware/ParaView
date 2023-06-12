b4a489574eb2f0815d7bc9fa6124e0a814b49105
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2023-06-12 13:10:38 -0400

    Add StringWriter proxy

---
+++ VTKExtensions/IOCore/vtkStringWriter.cxx
@@ -0,0 +1,100 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkStringWriter.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkStringWriter.h"
+
+#include "vtkDemandDrivenPipeline.h"
+#include "vtkInformation.h"
+#include "vtkObjectFactory.h"
+
+#include "vtksys/FStream.hxx"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkStringWriter);
+
+//----------------------------------------------------------------------------
+vtkStringWriter::vtkStringWriter()
+{
+  this->SetNumberOfInputPorts(0);
+  this->SetNumberOfOutputPorts(0);
+}
+
+//----------------------------------------------------------------------------
+vtkStringWriter::~vtkStringWriter() = default;
+
+//----------------------------------------------------------------------------
+void vtkStringWriter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+  os << indent << "String: " << this->String << endl;
+  os << indent << "FileName: " << this->FileName << endl;
+}
+
+//----------------------------------------------------------------------------
+int vtkStringWriter::FillInputPortInformation(
+  int vtkNotUsed(port), vtkInformation* vtkNotUsed(info))
+{
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkStringWriter::FillOutputPortInformation(
+  int vtkNotUsed(port), vtkInformation* vtkNotUsed(info))
+{
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+vtkTypeBool vtkStringWriter::ProcessRequest(
+  vtkInformation* request, vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+  // generate the data
+  if (request->Has(vtkDemandDrivenPipeline::REQUEST_DATA()))
+  {
+    return this->RequestData(request, inputVector, outputVector);
+  }
+
+  return this->Superclass::ProcessRequest(request, inputVector, outputVector);
+}
+
+//----------------------------------------------------------------------------
+int vtkStringWriter::RequestData(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** vtkNotUsed(inputVector), vtkInformationVector* vtkNotUsed(outputVector))
+{
+  if (this->String.empty() || this->FileName.empty())
+  {
+    vtkErrorMacro("String or FileName not set.");
+    return 0;
+  }
+
+  vtksys::ofstream ofs(this->FileName.c_str(), ios::out);
+  if (!ofs.is_open())
+  {
+    vtkErrorMacro("Could not open file: " << this->FileName);
+    return 0;
+  }
+  ofs << this->String;
+  ofs.close();
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkStringWriter::Write()
+{
+  // always write even if the data hasn't changed
+  this->Modified();
+  this->Update();
+  return 1;
+}
---
+++ VTKExtensions/IOCore/vtkStringWriter.h
@@ -0,0 +1,71 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkStringWriter.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/**
+ * @class   vtkStringWriter
+ * @brief   Given a string, write it to a file.
+ */
+#ifndef vtkStringWriter_h
+#define vtkStringWriter_h
+
+#include "vtkAlgorithm.h"
+#include "vtkPVVTKExtensionsIOCoreModule.h" // needed for exports
+
+class VTKPVVTKEXTENSIONSIOCORE_EXPORT vtkStringWriter : public vtkAlgorithm
+{
+public:
+  static vtkStringWriter* New();
+  vtkTypeMacro(vtkStringWriter, vtkAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  ///@{
+  /**
+   * Specify the string to write.
+   */
+  vtkSetStdStringFromCharMacro(String);
+  vtkGetCharFromStdStringMacro(String);
+  ///@}
+
+  ///@{
+  /**
+   * Specify the file name to write to.
+   */
+  vtkSetStdStringFromCharMacro(FileName);
+  vtkGetCharFromStdStringMacro(FileName);
+  ///@}
+
+  /**
+   * Write the string to the file.
+   */
+  int Write();
+
+protected:
+  vtkStringWriter();
+  ~vtkStringWriter() override;
+
+  int FillInputPortInformation(int port, vtkInformation* info) override;
+  int FillOutputPortInformation(int port, vtkInformation* info) override;
+  vtkTypeBool ProcessRequest(vtkInformation* request, vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
+  virtual int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
+
+private:
+  vtkStringWriter(const vtkStringWriter&) = delete;
+  void operator=(const vtkStringWriter&) = delete;
+
+  std::string String;
+  std::string FileName;
+};
+
+#endif // vtkStringWriter_h
