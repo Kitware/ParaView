b46c888ab641dbde2e88303399910e8c16f477b3
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-07-26 18:16:25 +0200

    Manual replace cpy header

--- Examples/Catalyst/PythonDolfinExample/simulation-catalyst-step3.py
+++ Examples/Catalyst/PythonDolfinExample/simulation-catalyst-step3.py
@@ -1,28 +1,14 @@
+# SPDX-FileCopyrightText: Copyright (C) 2010-2011 Anders Logg
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 """This demo program solves the incompressible Navier-Stokes equations
 on an L-shaped domain using Chorin's splitting method."""
 
-# Copyright (C) 2010-2011 Anders Logg
-#
-# This file is part of DOLFIN.
-#
-# DOLFIN is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# DOLFIN is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-#
+# Created by Anders Logg
 # Modified by Mikael Mortensen 2011
 #
 # First added:  2010-08-30
 # Last changed: 2011-06-30
-
 
 #
 # SC14 Paraview's Catalyst tutorial
