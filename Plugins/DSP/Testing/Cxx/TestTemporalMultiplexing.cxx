df9073ca31cca71d25ffb41d8bfde2a96197d3a9
Tiffany Chhim <tiffany.chhim@kitware.com>
2023-08-01 13:47:19 +0200

    Temporal Multiplexing: Support composite datasets
    Previously, the filter only applied to non composite datasets to produce
    arrays where:
      - Number of tuples = number of timesteps
      - Number of components = original number of components
      - Number of arrays (3rd dimension) = number of points/cells
    
    For composite datasets, an iterator goes through each dataset to append
    values along the third dimension. The output remains a vtkTable where
    the number of arrays inside each multidimensional array is equal to the
    total number of points/cells across all datasets.

--- Plugins/DSP/Testing/Cxx/TestTemporalMultiplexing.cxx
+++ Plugins/DSP/Testing/Cxx/TestTemporalMultiplexing.cxx
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include "vtkTemporalMultiplexing.h"
 
+#include "vtkDataObject.h"
+#include "vtkExodusIIReader.h"
 #include "vtkHDFReader.h"
 #include "vtkMathUtilities.h"
 #include "vtkMultiDimensionalArray.h"
@@ -78,6 +80,73 @@ int TestTemporalMultiplexing(int argc, char* argv[])
   if (!vtkMathUtilities::FuzzyCompare(array->GetValue(1), 0.725975f, 0.0001f))
   {
     std::cerr << "Expected 0.725975 but got " << array->GetValue(1) << "." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Test with composite dataset
+  fname = vtkTestUtilities::ExpandDataFileName(argc, argv, "Testing/Data/can.ex2");
+  vtkNew<vtkExodusIIReader> exReader;
+  exReader->SetFileName(fname);
+  exReader->UpdateInformation();
+  exReader->SetAllArrayStatus(vtkExodusIIReader::ELEM_BLOCK, 1);
+  multiplexer->SetInputConnection(exReader->GetOutputPort());
+  multiplexer->SetFieldAssociation(vtkDataObject::CELL);
+  multiplexer->ClearAttributeArrays();
+  multiplexer->EnableAttributeArray("EQPS");
+  multiplexer->Update();
+  delete[] fname;
+
+  output = vtkTable::SafeDownCast(multiplexer->GetOutputDataObject(0));
+
+  if (!output)
+  {
+    std::cerr << "Missing output of vtkTemporalMultiplexing filter." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Check array
+  vtkMultiDimensionalArray<double>* arrayComposite =
+    vtkMultiDimensionalArray<double>::SafeDownCast(output->GetColumnByName("EQPS"));
+
+  if (!arrayComposite)
+  {
+    std::cerr << "Missing 'EQPS' multidimensional array." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Check number of tuples (number of timesteps)
+  if (arrayComposite->GetNumberOfTuples() != 44)
+  {
+    std::cerr << "'EQPS' multidimensional array should have 44 tuples but has "
+              << arrayComposite->GetNumberOfTuples() << "." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Check number of arrays (number of mesh points)
+  if (arrayComposite->GetNumberOfArrays() != 7152)
+  {
+    std::cerr << "'EQPS' multidimensional array should have 7152 arrays but has "
+              << arrayComposite->GetNumberOfArrays() << "." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Change index for point on block 0
+  arrayComposite->SetIndex(222);
+
+  // Check timestep 43 for point 222
+  if (!vtkMathUtilities::FuzzyCompare(arrayComposite->GetValue(43), 1.91565, 0.0001))
+  {
+    std::cerr << "Expected 1.91565 but got " << arrayComposite->GetValue(43) << "." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Change index for point on block 1
+  arrayComposite->SetIndex(5000);
+
+  // Check timestep 28 for point 5000
+  if (!vtkMathUtilities::FuzzyCompare(arrayComposite->GetValue(28), 0.0, 0.0001))
+  {
+    std::cerr << "Expected 0.0 but got " << arrayComposite->GetValue(28) << "." << std::endl;
     return EXIT_FAILURE;
   }
 
