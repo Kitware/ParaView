74abcbbb07b54f8418a323a969c10d70b9771f67
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-08-21 10:24:39 +0200

    Disabling spdx on el8

--- .gitlab/ci/configure_el8.cmake
+++ .gitlab/ci/configure_el8.cmake
@@ -1,4 +1,7 @@
 # el8 docker image does not have catalyst installed
 set(PARAVIEW_ENABLE_CATALYST OFF CACHE BOOL "")
 
+# SPDX generation requires a more recent python
+set(PARAVIEW_GENERATE_SPDX OFF CACHE BOOL "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora_common.cmake")
