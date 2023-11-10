4cee10d2188802b2b993372d18e2f0bda990239d
Ben Boeckel <ben.boeckel@kitware.com>
2023-11-10 01:32:58 -0500

    deprecation: ignore deprecated warnings from deprecated classes

--- Qt/ApplicationComponents/pqResetScalarRangeReaction.cxx
+++ Qt/ApplicationComponents/pqResetScalarRangeReaction.cxx
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
 // SPDX-FileCopyrightText: Copyright (c) Sandia Corporation
 // SPDX-License-Identifier: BSD-3-Clause
+
+// Hide PARAVIEW_DEPRECATED_IN_5_12_0() warnings for this class.
+#define PARAVIEW_DEPRECATION_LEVEL 0
+
 #include "pqResetScalarRangeReaction.h"
 #include "ui_pqResetScalarRangeToDataOverTime.h"
 
