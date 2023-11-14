e18c32257e91df07b710056dc1807b7f3cff1ce2
Ben Boeckel <ben.boeckel@kitware.com>
2023-11-14 15:03:22 -0500

    vtkParaViewDeprecation: update 5.12 deprecation check
    If we're on a post-5.11 date-based patch version, 5.12 deprecation
    warnings should fire.

--- Utilities/Versioning/vtkParaViewDeprecation.h
+++ Utilities/Versioning/vtkParaViewDeprecation.h
@@ -93,10 +93,10 @@
 #endif
 #endif
 
-// APIs deprecated in the next release.
+// APIs deprecated in 5.12.0.
 #if defined(__VTK_WRAP__)
 #define PARAVIEW_DEPRECATED_IN_5_12_0(reason) [[vtk::deprecated(reason, "5.12.0")]]
-#elif PARAVIEW_DEPRECATION_LEVEL >= PARAVIEW_VERSION_CHECK(5, 12, 0)
+#elif PARAVIEW_DEPRECATION_LEVEL >= PARAVIEW_VERSION_CHECK(5, 11, 20230101)
 #define PARAVIEW_DEPRECATED_IN_5_12_0(reason) PARAVIEW_DEPRECATION(reason)
 #else
 #define PARAVIEW_DEPRECATED_IN_5_12_0(reason)
