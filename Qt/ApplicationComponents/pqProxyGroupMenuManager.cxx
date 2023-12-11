1b1173fde9c6f73c8669a8c8ca91f57e0613ccde
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-12-11 14:20:38 +0100

    Fix shortcut config file

--- Qt/ApplicationComponents/pqProxyGroupMenuManager.cxx
+++ Qt/ApplicationComponents/pqProxyGroupMenuManager.cxx
@@ -657,11 +657,11 @@ QAction* pqProxyGroupMenuManager::getAction(const QString& pgroup, const QString
       QStringList data_list;
       data_list << pgroup << pname;
       action << pqSetName(name) << pqSetData(data_list);
-      pqSettings settings;
+      pqSettings* settings = pqApplicationCore::instance()->settings();
       if (pgroup == "filters" || pgroup == "sources")
       {
         QString menuName = pgroup == "filters" ? "Filters" : "Sources";
-        auto variant = settings.value(
+        auto variant = settings->value(
           QString("pqCustomShortcuts/%1/Alphabetical/%2").arg(menuName, label), QVariant());
         if (variant.canConvert<QKeySequence>())
         {
