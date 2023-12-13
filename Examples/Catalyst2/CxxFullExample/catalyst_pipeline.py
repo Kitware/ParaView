28a51541d062b6f885718cac41d71f8dae0b14b9
Christos Tsolakis <christos.tsolakis@kitware.com>
2023-12-13 12:26:30 -0500

    Update CxxFullExample to show how to access and test the new functionality

--- Examples/Catalyst2/CxxFullExample/catalyst_pipeline.py
+++ Examples/Catalyst2/CxxFullExample/catalyst_pipeline.py
@@ -15,3 +15,21 @@ def catalyst_execute(info):
     print("bounds:", producer.GetDataInformation().GetBounds())
     print("velocity-magnitude-range:", producer.PointData["velocity"].GetRange(-1))
     print("pressure-range:", producer.CellData["pressure"].GetRange(0))
+
+    # access the node pass through catalyst_execute from the simulation
+    # make sure that CATALYST_PYTHONPATH is in your PYTHONPATH
+    node = info.catalyst_params
+    print(f"{node=}")
+
+    ######################################
+    # This is only for testing
+    # validate that some of the expected fields are part of the conduit node
+    assert node.has_path("catalyst/state")
+    assert node.has_path("catalyst/channels/grid")
+    assert node["catalyst/channels/grid/data/topologies/mesh/type"] ==  "unstructured"
+    assert node["catalyst/channels/grid/data/fields"][0].name() == "velocity"
+    assert node["catalyst/channels/grid/data/fields"][1].name() == "pressure"
+
+    # validate that the node comes from this timestep
+    assert node["catalyst/state/timestep"] == info.timestep
+    ######################################
