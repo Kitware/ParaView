b40148edf6eb9cbdd0c13e218c3016c84a1133e2
Jordi Subirana <jordi.subirana@kitware.com>
2024-02-07 14:19:33 +0100

    pqScopedOverrideCursor: Add class to manage mouse cursor overrides

---
+++ Qt/Widgets/pqScopedOverrideCursor.cxx
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
+// SPDX-FileCopyrightText: Copyright (c) Sandia Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "pqScopedOverrideCursor.h"
+
+#include <QApplication>
+#include <QCursor>
+
+pqScopedOverrideCursor::pqScopedOverrideCursor(Qt::CursorShape cursorShape)
+{
+  QApplication::setOverrideCursor(QCursor(cursorShape));
+}
+
+pqScopedOverrideCursor::~pqScopedOverrideCursor()
+{
+  QApplication::restoreOverrideCursor();
+}
