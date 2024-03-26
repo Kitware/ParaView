Kitware/ParaView

commit 3bdd394f02daab549995692a2cccd5545d88271d

Clients/InSitu: remove unused dependencies

Clients/InSitu/vtk.module
@@ -19,8 +19,6 @@ PRIVATE_DEPENDS
   ParaView::VTKExtensionsCore
 OPTIONAL_DEPENDS
   ParaView::PythonCatalyst
-  ParaView::PythonInitializer
-  ParaView::RemotingLive
   VTK::IOCatalystConduit
   VTK::IOFides
   VTK::IOIOSS

commit 69bf3101f273d1449c905b5f0b22e8ee598e552e

Clients/InSitu: add back optional dependency
The guard was incorrect on the conditional API in
`vtkInSituInitializationHelper` and removed in !6766 incorrectly.

Fixes: 3bdd394f02 (Clients/InSitu: remove unused dependencies, 2024-03-17)

Clients/InSitu/vtk.module
@@ -19,6 +19,7 @@ PRIVATE_DEPENDS
   ParaView::VTKExtensionsCore
 OPTIONAL_DEPENDS
   ParaView::PythonCatalyst
+  ParaView::PythonInitializer
   VTK::IOCatalystConduit
   VTK::ParallelMPI
   VTK::WrappingPythonCore

Clients/InSitu/vtkInSituInitializationHelper.cxx
@@ -40,7 +40,7 @@
 #include <catalyst_conduit.hpp>
 #endif
 
-#if VTK_MODULE_ENABLE_ParaView_PythonCatalyst
+#if VTK_MODULE_ENABLE_ParaView_PythonInitializer
 extern "C"
 {
   void vtkPVInitializePythonModules();
@@ -174,7 +174,7 @@ void vtkInSituInitializationHelper::Initialize(vtkTypeUInt64 comm)
   // deprecate Legacy Catalyst API.
   internals.CPCxxHelper.TakeReference(vtkCPCxxHelper::New());
 
-#if VTK_MODULE_ENABLE_ParaView_PythonCatalyst
+#if VTK_MODULE_ENABLE_ParaView_PythonInitializer
   // register static Python modules built, if any.
   vtkPVInitializePythonModules();
 #endif