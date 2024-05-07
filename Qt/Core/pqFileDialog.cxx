2942bd85d6b69a7a5214c72be619c9aef9855dfd
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2024-05-07 17:10:47 -0400

    Qt6: Remove use of obsoleted `QKeyCombination::+` operator

--- Qt/Core/pqFileDialog.cxx
+++ Qt/Core/pqFileDialog.cxx
@@ -322,7 +322,7 @@ pqFileDialog::pqFileDialog(pqServer* server, QWidget* p, const QString& title,
     QObject::connect(
       impl.Ui.NavigateForward, SIGNAL(clicked(bool)), this, SLOT(onNavigateForward()));
     impl.Ui.NavigateUp->setIcon(style()->standardPixmap(QStyle::SP_FileDialogToParent));
-    impl.Ui.NavigateUp->setShortcut(Qt::ALT + Qt::Key_Up);
+    impl.Ui.NavigateUp->setShortcut(Qt::ALT | Qt::Key_Up);
     impl.Ui.NavigateUp->setToolTip(
       tr("Navigate Up (%1)").arg(impl.Ui.NavigateUp->shortcut().toString()));
     impl.Ui.CreateFolder->setIcon(style()->standardPixmap(QStyle::SP_FileDialogNewFolder));
