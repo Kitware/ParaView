d02f27356634e8b79e4a336794000eb4a103a440
Cory Quammen <cory.quammen@kitware.com>
2024-07-12 15:45:33 -0400

    vtkPolarAxesRepresentation.cxx: hide 5.13.0 deprecation warnings
    The EnableCustomRadius member itself is dedicated, and all assignments
    to or from it generate deprecation warnings.
    
    (cherry picked from commit 3627fa039a8e873781afe6b6348ffbb935d8feca)

--- Remoting/Views/vtkPolarAxesRepresentation.cxx
+++ Remoting/Views/vtkPolarAxesRepresentation.cxx
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
 // SPDX-License-Identifier: BSD-3-Clause
 
+// Hide PARAVIEW_DEPRECATED_IN_5_13_0() warnings for this class.
+#define PARAVIEW_DEPRECATION_LEVEL 0
+
 #include "vtkPolarAxesRepresentation.h"
 
 #include "vtkAlgorithmOutput.h"
