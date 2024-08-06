d279a3e079ca6ccc7a3cb317e655eaf005094dd1
Léon Victor <leon.victor@kitware.com>
2024-08-06 09:52:32 +0200

    Add a test for the new API

--- Remoting/Core/Testing/Cxx/TestPVArrayInformation.cxx
+++ Remoting/Core/Testing/Cxx/TestPVArrayInformation.cxx
@@ -2,10 +2,13 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include "vtkFieldData.h"
 #include "vtkFloatArray.h"
+#include "vtkLogger.h"
 #include "vtkMathUtilities.h"
 #include "vtkNew.h"
 #include "vtkPVArrayInformation.h"
 #include "vtkSmartPointer.h"
+
+#include <cstdlib>
 
 vtkSmartPointer<vtkFloatArray> GetPolyData()
 {
@@ -19,6 +22,35 @@ vtkSmartPointer<vtkFloatArray> GetPolyData()
     array->SetTypedComponent(cc, 0, static_cast<float>(cc));
   }
   return array;
+}
+
+bool AreArrayInformationsEqual(vtkPVArrayInformation* lhs, vtkPVArrayInformation* rhs)
+{
+  bool equal = true;
+  equal &= lhs->GetIsPartial() == rhs->GetIsPartial();
+  equal &= lhs->GetDataTypeAsString() == rhs->GetDataTypeAsString();
+  equal &= lhs->GetNumberOfComponents() == rhs->GetNumberOfComponents();
+  equal &= lhs->GetNumberOfTuples() == rhs->GetNumberOfTuples();
+  equal &= strcmp(lhs->GetName(), rhs->GetName()) == 0;
+
+  for (int compIdx = 0; compIdx < lhs->GetNumberOfComponents(); ++compIdx)
+  {
+    equal &= strcmp(lhs->GetComponentName(compIdx), rhs->GetComponentName(compIdx)) == 0;
+
+    std::array<double, 2> lhRange, rhRange;
+    lhs->GetComponentRange(compIdx, lhRange.data());
+    rhs->GetComponentRange(compIdx, rhRange.data());
+    equal &= lhRange[0] == rhRange[0];
+    equal &= lhRange[1] == rhRange[1];
+
+    std::array<double, 2> lhFiniteRange, rhFiniteRange;
+    lhs->GetComponentFiniteRange(compIdx, lhFiniteRange.data());
+    rhs->GetComponentFiniteRange(compIdx, rhFiniteRange.data());
+    equal &= lhFiniteRange[0] == rhFiniteRange[0];
+    equal &= lhFiniteRange[1] == rhFiniteRange[1];
+  }
+
+  return equal;
 }
 
 int TestPVArrayInformation(int, char*[])
@@ -127,6 +159,18 @@ int TestPVArrayInformation(int, char*[])
   if (!vtkMathUtilities::FuzzyCompare(rangeArray[1], 100.0))
   {
     cerr << "ERROR: failed to find finite range maximum: " << rangeArray[1] << " " << 100.0 << endl;
+    return EXIT_FAILURE;
+  }
+
+  // Compare CopyFromArray overloads
+  vtkNew<vtkPVArrayInformation> infoCopiedFromArray;
+  infoCopiedFromArray->CopyFromArray(array);
+
+  if (!AreArrayInformationsEqual(info, infoCopiedFromArray))
+  {
+    vtkLogF(ERROR,
+      "Copying information from array as simple array or as field data did not yield the same "
+      "result");
     return EXIT_FAILURE;
   }
 
