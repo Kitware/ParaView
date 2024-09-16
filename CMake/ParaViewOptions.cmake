6e762b79d09ce3188992695a3817d5e166fec47f
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2024-09-16 09:24:15 -0400

    Replace VTK::glew with VTK::glad

--- CMake/ParaViewOptions.cmake
+++ CMake/ParaViewOptions.cmake
@@ -669,7 +669,7 @@ if (NOT PARAVIEW_ENABLE_RENDERING)
   # This ensures that we don't ever enable OpenGL
   # modules when PARAVIEW_ENABLE_RENDERING is OFF.
   set(rendering_modules
-    VTK::glew
+    VTK::glad
     VTK::opengl)
   list(APPEND paraview_rejected_modules
     ${rendering_modules})
