e71769bd2e03d309e5fb96d1cb44d0b7f566a3dd
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2024-12-17 08:51:06 -0700

    Update deprecated API in RemotingApplicationPython-PythonPVSimpleSphere

--- Remoting/Application/Testing/Python/PythonPVSimpleSphere.py
+++ Remoting/Application/Testing/Python/PythonPVSimpleSphere.py
@@ -1,5 +1,4 @@
 from paraview.simple import *
-import sys
 from paraview import smtesting
 
 sphere = Sphere()
@@ -8,7 +7,7 @@ sphere.ThetaResolution = 20
 
 clientsphere = servermanager.Fetch(sphere)
 if clientsphere.GetNumberOfPolys() != 720:
-    raise smtesting.TestError('Test failed: Problem fetching polydata.')
+    raise smtesting.TestError("Test failed: Problem fetching polydata.")
 
 elev = Elevation(sphere)
 mm = servermanager.filters.MinMax()
@@ -17,23 +16,23 @@ mm.Operation = "MIN"
 mindata = servermanager.Fetch(elev, mm, mm)
 
 if mindata.GetPointData().GetNumberOfArrays() != 2:
-    raise smtesting.TestError('Test failed: Wrong number of arrays.')
+    raise smtesting.TestError("Test failed: Wrong number of arrays.")
 
-array = mindata.GetPointData().GetArray('Elevation')
+array = mindata.GetPointData().GetArray("Elevation")
 print("%d %f" % (array.GetNumberOfTuples(), array.GetTuple1(0)))
 
 if array.GetTuple1(0) < 0.2 and array.GetTuple1(0) > 0.29:
-    raise smtesting.TestError('Test failed: Bad array value.')
+    raise smtesting.TestError("Test failed: Bad array value.")
 
 rep = Show(elev)
 ai = elev.PointData[1]
-if ai.GetName() != 'Elevation':
+if ai.GetName() != "Elevation":
     pd = elev.GetPointDataInformation()
-    ai = pd.GetArray('Elevation')
+    ai = pd.GetArray("Elevation")
 
 rng = ai.GetRange()
-rep.LookupTable = MakeBlueToRedLT(rng[0], rng[1])
-rep.ColorArrayName = ("POINT_DATA", 'Elevation')
+ColorBy(rep, ("POINT_DATA", "Elevation"))
+AssignFieldToColorPreset("Elevation", "Blue to Red Rainbow", rng)
 
 camera = GetActiveCamera()
 camera.Elevation(45)
@@ -41,4 +40,4 @@ camera.Elevation(45)
 ren = Render()
 
 if not smtesting.DoRegressionTesting(ren.SMProxy):
-    raise smtesting.TestError('Image comparison failed.')
+    raise smtesting.TestError("Image comparison failed.")
