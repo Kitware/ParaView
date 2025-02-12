f82272606181cfd25752171abc8ea3b663cce2b4
Cory Quammen <cory.quammen@kitware.com>
2025-02-11 22:36:58 -0500

    vtkGenericIOUtilities.h: replace std::ptr_fun with lambda
    std::ptr_fun has been removed from C++17.

--- VTKExtensions/CosmoTools/vtkGenericIOUtilities.h
+++ VTKExtensions/CosmoTools/vtkGenericIOUtilities.h
@@ -39,8 +39,7 @@ namespace vtkGenericIOUtilities
  */
 inline std::string& ltrim(std::string& s)
 {
-  s.erase(
-    s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) { return !std::isspace(ch); }));
   return s;
 }
 //@}
@@ -53,8 +52,7 @@ inline std::string& ltrim(std::string& s)
 inline std::string& rtrim(std::string& s)
 {
   s.erase(
-    std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
-    s.end());
+    std::find_if(s.rbegin(), s.rend(), [](int ch) { return !std::isspace(ch); }).base(), s.end());
   return s;
 }
 //@}
