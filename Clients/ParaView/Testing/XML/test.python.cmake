96c589be81f4d794617efc6c5204754caa614866
Guillaume Gisbert <guillaume.gisbert@kitware.com>
2025-02-13 15:41:25 +0100

    Testing XML on HTGSeclectionOvertime
    (cherry picked from commit 0ea6dd48e997c881c29947e18e22fe278aa59c2f)

--- Clients/ParaView/Testing/XML/test.python.cmake
+++ Clients/ParaView/Testing/XML/test.python.cmake
@@ -91,6 +91,7 @@ if (numpy_found)
     ExpressionChooser.xml
     ExpressionClear.xml
     FieldDataDomainDefault.xml
+    HTGPlotSelectionOverTime.xml
     HyperTreeGridVisibleLeavesSize.xml
     PlotOverLine_htg.xml # needs find data
     ProgrammableFilterFieldData.xml
