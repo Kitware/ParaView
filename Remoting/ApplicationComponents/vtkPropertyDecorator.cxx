c0908cd6e1679f3752a4926e0a19035a6487b5fb
Christos Tsolakis <christos.tsolakis@kitware.com>
2025-04-04 18:11:57 -0400

    cmake: Add new module vtkRemotingApplicationComponents

---
+++ Remoting/ApplicationComponents/vtkPropertyDecorator.cxx
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
+// SPDX-FileCopyrightText: Copyright (c) Sandia Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+#include "vtkPropertyDecorator.h"
+#include "vtkLogger.h"
+#include "vtkObjectFactory.h"
+#include "vtkPVXMLElement.h"
+
+//-----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkPropertyDecorator);
+
+//-----------------------------------------------------------------------------
+vtkPropertyDecorator::vtkPropertyDecorator() = default;
+
+//-----------------------------------------------------------------------------
+vtkPropertyDecorator::~vtkPropertyDecorator() = default;
+
+//-----------------------------------------------------------------------------
+void vtkPropertyDecorator::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//-----------------------------------------------------------------------------
+void vtkPropertyDecorator::Initialize(vtkPVXMLElement* config, vtkSMProxy* proxy)
+{
+  // TODO
+  this->xml = config;
+  this->proxy = proxy;
+}
+//-----------------------------------------------------------------------------
+vtkPVXMLElement* vtkPropertyDecorator::XML() const
+{
+  return this->xml.GetPointer();
+}
+//-----------------------------------------------------------------------------
+void vtkPropertyDecorator::InvokeVisibilityChangedEvent()
+{
+  this->InvokeEvent(VisibilityChangedEvent);
+}
+
+//-----------------------------------------------------------------------------
+void vtkPropertyDecorator::InvokeEnableStateChangedEvent()
+{
+  this->InvokeEvent(EnableStateChangedEvent);
+}
+
+//-----------------------------------------------------------------------------
+std::string vtkPropertyDecorator::GetDecoratorType() const
+{
+
+  if (this->xml == nullptr || strcmp(this->xml->GetName(), "PropertyDecorator") != 0 ||
+    this->xml->GetAttribute("type") == nullptr)
+  {
+    vtkLogF(WARNING, "Invalid xml config specified. Cannot create a decorator.");
+    return "";
+  }
+  return this->xml->GetAttribute("type");
+}
---
+++ Remoting/ApplicationComponents/vtkPropertyDecorator.h
@@ -0,0 +1,101 @@
+// SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
+// SPDX-License-Identifier: BSD-3-Clause
+#ifndef vtkPropertyDecorator_h
+#define vtkPropertyDecorator_h
+
+#include "vtkRemotingApplicationComponentsModule.h"
+
+#include "vtkCommand.h"
+#include "vtkObject.h"
+#include "vtkSMProxy.h"
+
+#include "vtkSmartPointer.h" // needed for vtkSmartPointer
+#include "vtkWeakPointer.h"
+
+#include <string>
+
+class vtkPVXMLElement;
+class vtkSMProxy;
+
+/**
+ * vtkPropertyDecorator hold the logic of pqPropertyDecorator
+ * TODO provides a mechanism to decorate pqProperty
+ * instances to add logic to the widget to add additional control logic.
+ * Subclasses can be used to logic to control when the widget is
+ * enabled/disabled, hidden/visible, etc. based on values of other properties
+ * of UI elements.
+ */
+class VTKREMOTINGAPPLICATIONCOMPONENTS_EXPORT vtkPropertyDecorator : public vtkObject
+{
+
+public:
+  static vtkPropertyDecorator* New();
+  vtkTypeMacro(vtkPropertyDecorator, vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  /**
+   * Constructor.
+   *
+   * @param xml The XML element from the `<Hints/>` section for the proxy/property that
+   * resulted in the creation of the decorator. Decorators can be provided
+   * configuration parameters from the XML.
+   */
+
+  void virtual Initialize(vtkPVXMLElement* config, vtkSMProxy* proxy);
+
+  /**
+   * Override this method to override the visibility of the widget in the
+   * panel. This is called after the generic tests for advanced and text
+   * filtering are passed. Since there can be multiple decorators, the first
+   * decorator that returns 'false' wins. Default implementation returns true.
+   * Thus subclasses typically override this method only to force the widget
+   * invisible given the current state.
+   */
+  virtual bool CanShow(bool show_advanced) const
+  {
+    (void)show_advanced;
+    return true;
+  }
+
+  /**
+   * Override this method to override the enable state of the widget in the
+   * panel. This is called after the generic tests for advanced and text
+   * filtering are passed. Since there can be multiple decorators, the first
+   * decorator that returns 'false' wins. Default implementation returns true.
+   * Thus subclasses typically override this method only to force the widget
+   * disabled given the current state.
+   */
+  virtual bool Enable() const { return true; }
+
+  /**
+   * This event is fired whenever the decorator has determined that the panel
+   * *may* need a refresh since the state of the system has changed which would
+   * deem changes in the widget visibility or enable state.
+   */
+  enum
+  {
+    VisibilityChangedEvent = vtkCommand::UserEvent + 1000,
+    EnableStateChangedEvent = vtkCommand::UserEvent + 1001
+  };
+
+  // returns "type" attribute of the xml config
+  std::string GetDecoratorType() const;
+
+protected:
+  vtkPropertyDecorator();
+  ~vtkPropertyDecorator() override;
+  vtkPVXMLElement* XML() const;
+  vtkSMProxy* Proxy() const;
+
+  void InvokeVisibilityChangedEvent();
+  void InvokeEnableStateChangedEvent();
+
+private:
+  vtkPropertyDecorator(const vtkPropertyDecorator&) = delete;
+  void operator=(const vtkPropertyDecorator&) = delete;
+
+  vtkWeakPointer<vtkPVXMLElement> xml;
+  vtkWeakPointer<vtkSMProxy> proxy;
+};
+
+#endif
