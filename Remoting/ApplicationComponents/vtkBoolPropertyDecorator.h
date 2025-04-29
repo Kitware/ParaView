ae41ee3654a447ecb6e1d878b48247959089aaab
Christos Tsolakis <christos.tsolakis@kitware.com>
2025-04-29 13:26:42 -0400

    Fix add missing copyright vtk{,Bool}PropertyDecorators
    missed in the initial copy of the code from pq Classes

--- Remoting/ApplicationComponents/vtkBoolPropertyDecorator.h
+++ Remoting/ApplicationComponents/vtkBoolPropertyDecorator.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
+// SPDX-FileCopyrightText: Copyright (c) Sandia Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 #ifndef vtkBoolPropertyDecorator_h
 #define vtkBoolPropertyDecorator_h
--- Remoting/ApplicationComponents/vtkPropertyDecorator.h
+++ Remoting/ApplicationComponents/vtkPropertyDecorator.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
+// SPDX-FileCopyrightText: Copyright (c) Sandia Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 #ifndef vtkPropertyDecorator_h
 #define vtkPropertyDecorator_h
