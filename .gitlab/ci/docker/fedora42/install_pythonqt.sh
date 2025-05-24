b24761b4f3723de80e73e98f00477073c0a4863b
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2025-05-24 17:10:23 -0400

    ci: disable PythonQt
    I tried to go back to the main project, but it doesn't understand the
    `uitools` module and gives up. The CMake patches don't support Python
    3.11 yet (`eval.h` includes fail).

--- .gitlab/ci/docker/fedora42/install_pythonqt.sh
+++ .gitlab/ci/docker/fedora42/install_pythonqt.sh
@@ -4,8 +4,8 @@ set -e
 
 # Use commontk pythonqt with cmake support
 # CMake support may be added to the official repository in the future. https://github.com/commontk/PythonQt/issues/86
-readonly pythonqt_repo="https://github.com/commontk/PythonQt"
-readonly pythonqt_commit="patched-10"
+readonly pythonqt_repo="https://github.com/MeVisLab/pythonqt"
+readonly pythonqt_commit="v3.3.0"
 
 readonly pythonqt_root="$HOME/pythonqt"
 readonly pythonqt_src="$pythonqt_root/src"
@@ -14,6 +14,14 @@ readonly pythonqt_build_root="$pythonqt_root/build"
 git clone "$pythonqt_repo" "$pythonqt_src"
 git -C "$pythonqt_src" checkout "$pythonqt_commit"
 
+# Allow cherry picking.
+git -C "$pythonqt_src" config user.name "VTK CI"
+git -C "$pythonqt_src" config user.email "kwrobot+vtkci@kitware.com"
+
+# Apply support for 5.15 (PR #64)
+git -C "$pythonqt_src" fetch origin refs/pull/64/head
+git -C "$pythonqt_src" cherry-pick FETCH_HEAD
+
 pythonqt_build () {
     local prefix="$1"
     shift
@@ -21,16 +29,14 @@ pythonqt_build () {
     # For python 3.10 compatibility https://github.com/commontk/PythonQt/pull/85
     sed '/#include <pydebug\.h>/d' -i "$pythonqt_src/src/PythonQt.cpp"
 
-    cmake -GNinja \
-        -S "$pythonqt_src" \
-        -B "$pythonqt_build_root" \
-        -DPythonQt_Wrap_Qtcore=ON \
-        -DPythonQt_Wrap_Qtgui=ON \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DLIB_SUFFIX=64 \
-        "-DCMAKE_INSTALL_PREFIX=$prefix" \
-        "$@"
-    cmake --build "$pythonqt_build_root" --target install
+    mkdir -p "$pythonqt_build_root"
+    pushd "$pythonqt_build_root"
+    qmake-qt5 -r "$pythonqt_src/PythonQt.pro" CONFIG+=release \
+        QT_SELECT=qt5 \
+        PYTHON_VERSION="$( python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")' )"
+    make "-j$( nproc )"
+    make install
+    popd
 }
 
 pythonqt_build /usr
