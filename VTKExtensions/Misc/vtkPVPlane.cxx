8c005deb1c9426df0b26078638583169b8f23250
Cory Quammen <cory.quammen@kitware.com>
2025-05-26 20:40:06 -0400

    vtkPVPlane: Silence deprecation warnings

--- VTKExtensions/Misc/vtkPVPlane.cxx
+++ VTKExtensions/Misc/vtkPVPlane.cxx
@@ -1,5 +1,9 @@
 // SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
 // SPDX-License-Identifier: BSD-3-Clause
+
+// Hide PARAVIEW_DEPRECATED_IN_6_0_0() warnings for this class.
+#define PARAVIEW_DEPRECATION_LEVEL 0
+
 #include "vtkPVPlane.h"
 
 #include "vtkObjectFactory.h"
