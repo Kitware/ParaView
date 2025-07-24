803466eb65d790faf1ecb398edd024d9853bdffe
Ben Boeckel <ben.boeckel@kitware.com>
2025-07-24 11:02:24 -0400

    vtkPVStandardPaths: match with the comment
    The comment indicates that `X.Y` was searched; actually do so.

--- Remoting/Application/vtkPVStandardPaths.cxx
+++ Remoting/Application/vtkPVStandardPaths.cxx
@@ -146,7 +146,7 @@ std::vector<std::string> GetInstallDirectories()
   // If the application path ends with lib/<app_name>-X.X, shared
   // forwarding of the executable was used. Remove that part of the
   // path to get back to the installation root.
-  auto appPathEnds = app_dir.find("/lib/" + app_name + "-" PARAVIEW_VERSION);
+  auto appPathEnds = app_dir.find("/lib/" + app_name + "-" PARAVIEW_VERSION_SHORT);
   std::string rootDirectory = app_dir;
   if (appPathEnds != std::string::npos)
   {
