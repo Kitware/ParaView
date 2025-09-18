93c911d4f3dba5d2c9f656fc24f315df3cfc7473
Ben Boeckel <ben.boeckel@kitware.com>
2025-09-18 08:44:41 -0400

    pqInteractivePropertyWidgetAbstract: guard `setupUserObserver`
    Sanity check that the user observer setup is being performed as
    expected. Current uses follow it, but it is a good idea to put up guards
    just in case.

--- Qt/ApplicationComponents/pqInteractivePropertyWidgetAbstract.cxx
+++ Qt/ApplicationComponents/pqInteractivePropertyWidgetAbstract.cxx
@@ -11,6 +11,7 @@
 #include "pqServer.h"
 #include "pqView.h"
 #include "vtkCommand.h"
+#include "vtkLogger.h"
 #include "vtkNew.h"
 #include "vtkPVDataInformation.h"
 #include "vtkPVXMLElement.h"
@@ -113,6 +114,20 @@ void pqInteractivePropertyWidgetAbstract::setupConnections(
 //-----------------------------------------------------------------------------
 void pqInteractivePropertyWidgetAbstract::setupUserObserver(vtkSMProxy* smproxy)
 {
+  if (this->Internals->UserEventObserverId > 0)
+  {
+    vtkLogF(ERROR,
+      "pqInteractivePropertyWidgetAbstract::setupUserObserver may only be called once per "
+      "instance");
+    return;
+  }
+  if (smproxy != this->proxy())
+  {
+    vtkLogF(ERROR,
+      "pqInteractivePropertyWidgetAbstract::setupUserObserver may only be called with the widget's "
+      "own proxy");
+    return;
+  }
   this->Internals->UserEventObserverId = smproxy->AddObserver(
     vtkCommand::UserEvent, this, &pqInteractivePropertyWidgetAbstract::handleUserEvent);
   smproxy->Register(nullptr);
