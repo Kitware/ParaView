e762909e56f580fb8100672ab3cbd1edbbefab01
Mathurin Lemarie <mathurin.lemarie@kitware.com>
2025-10-06 10:26:15 +0200

    Improve registration name plugin example for more testing

--- Examples/Plugins/RegistrationName/Plugin/MyReader/vtkMyPNGReader.cxx
+++ Examples/Plugins/RegistrationName/Plugin/MyReader/vtkMyPNGReader.cxx
@@ -4,6 +4,8 @@
 
 #include <vtkObjectFactory.h>
 
+#include <vtksys/SystemTools.hxx>
+
 vtkStandardNewMacro(vtkMyPNGReader);
 
 //----------------------------------------------------------------------------
@@ -15,7 +17,23 @@ vtkMyPNGReader::~vtkMyPNGReader() = default;
 //----------------------------------------------------------------------------
 const char* vtkMyPNGReader::GetRegistrationName()
 {
-  return "ReaderDefinedName";
+  const std::string& fileStem = this->GetFileStem(this->FileName);
+
+  this->RegistrationName = "MyPNGReaderFile-" + fileStem;
+  return this->RegistrationName.c_str();
+}
+
+//----------------------------------------------------------------------------
+std::string vtkMyPNGReader::GetFileStem(const std::string& filePath)
+{
+  std::vector<std::string> components;
+  vtksys::SystemTools::SplitPath(filePath, components);
+  const std::string& fileName = components.back();
+
+  std::vector<std::string> splittedFileName = vtksys::SystemTools::SplitString(fileName, '.');
+  const std::string& fileStem = splittedFileName.front();
+
+  return fileStem;
 }
 
 //----------------------------------------------------------------------------
