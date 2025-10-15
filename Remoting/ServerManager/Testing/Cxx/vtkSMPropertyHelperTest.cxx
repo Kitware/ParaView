91dc91ab867ff3e0b89d29a7f1e9e10ea063b973
Jean Fechter <jean.fechter@kitware.com>
2025-10-15 15:56:56 +0200

    Add test for ReplaceReaderFileName time keeper

--- Remoting/ServerManager/Testing/Cxx/vtkSMPropertyHelperTest.cxx
+++ Remoting/ServerManager/Testing/Cxx/vtkSMPropertyHelperTest.cxx
@@ -26,3 +26,41 @@ void vtkSMPropertyHelperTest::Set()
   proxy->Delete();
   session->Delete();
 }
+
+void vtkSMPropertyHelperTest::Contains()
+{
+  vtkSMSession* session = vtkSMSession::New();
+  vtkSMSessionProxyManager* pxm = session->GetSessionProxyManager();
+
+  vtkSMProxy* sphere1 = pxm->NewProxy("sources", "SphereSource");
+  vtkSMProxy* sphere2 = pxm->NewProxy("sources", "SphereSource");
+  vtkSMProxy* group = pxm->NewProxy("filters", "GroupDataSets");
+
+  QVERIFY(sphere1 != nullptr);
+  QVERIFY(sphere2 != nullptr);
+  QVERIFY(group != nullptr);
+
+  vtkSMProperty* inputProp = group->GetProperty("Input");
+  QVERIFY(inputProp != nullptr);
+
+  vtkSMPropertyHelper helper(inputProp);
+
+  QVERIFY(!helper.Contains(sphere1));
+
+  helper.Add(sphere1, 0);
+  QVERIFY(helper.Contains(sphere1));
+  QVERIFY(!helper.Contains(sphere2));
+
+  helper.Add(sphere2, 0);
+  QVERIFY(helper.Contains(sphere1));
+  QVERIFY(helper.Contains(sphere2));
+
+  helper.Remove(sphere1);
+  QVERIFY(!helper.Contains(sphere1));
+  QVERIFY(helper.Contains(sphere2));
+
+  sphere1->Delete();
+  sphere2->Delete();
+  group->Delete();
+  session->Delete();
+}
