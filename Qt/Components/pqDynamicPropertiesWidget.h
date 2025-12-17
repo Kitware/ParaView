f7460a133284bba54a935a02be7fc5e1de8c14d6
Dan Lipsa <dan.lipsa@kitware.com>
2025-12-17 09:57:43 -0500

    Copy/paste typo: not contributed by Sandia

--- Qt/Components/pqDynamicPropertiesWidget.h
+++ Qt/Components/pqDynamicPropertiesWidget.h
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: Copyright (c) Kitware Inc.
-// SPDX-FileCopyrightText: Copyright (c) Sandia Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 
 #ifndef pqDynamicPropertiesWidget_h
