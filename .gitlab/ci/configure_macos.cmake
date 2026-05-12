5fa43f7bb061b67fd24a25f8ccc1544e2786d967
Cory Quammen <cory.quammen@kitware.com>
2026-05-11 20:00:44 -0400

    ci: update macos builders to Python 3.14

--- .gitlab/ci/configure_macos.cmake
+++ .gitlab/ci/configure_macos.cmake
@@ -1,7 +1,4 @@
 # Add rpath entries for Xcode frameworks.
-set(CMAKE_BUILD_RPATH "$ENV{DEVELOPER_DIR}/Library/Frameworks" CACHE STRING "")
-set(CMAKE_INSTALL_RPATH "$ENV{DEVELOPER_DIR}/Library/Frameworks" CACHE STRING "")
-
 # Test CAVEInteraction with VRUI support
 set(PARAVIEW_PLUGIN_ENABLE_CAVEInteraction ON CACHE BOOL "")
 set(PARAVIEW_PLUGIN_CAVEInteraction_USE_VRUI ON CACHE BOOL "")
@@ -13,5 +10,18 @@ set(PARAVIEW_PLUGIN_ENABLE_ONNXPlugin ON CACHE BOOL "")
 set(PARAVIEW_PLUGIN_ENABLE_pvNVIDIAIndeX ON CACHE BOOL "")
 
 set(PARAVIEW_BUILD_TRANSLATIONS ON CACHE BOOL "")
+
+set(rpaths
+  # Add rpath entries for Xcode frameworks.
+  "$ENV{DEVELOPER_DIR}/Library/Frameworks")
+
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "python")
+  list(APPEND rpaths
+    # Add rpath entry for the CI-downloaded Python.
+    "$ENV{GIT_CLONE_PATH}/.gitlab/python/Python.framework")
+endif ()
+
+set(CMAKE_BUILD_RPATH "${rpaths}" CACHE STRING "")
+set(CMAKE_INSTALL_RPATH "${rpaths}" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
---
+++ .gitlab/ci/download_wheel_python.cmake
@@ -0,0 +1,126 @@
+cmake_minimum_required(VERSION 3.12)
+
+# Where Python for wheels are stored.
+set(python_url_root "https://www.paraview.org/files/dependencies/python-for-wheels")
+
+# Python version specifics.
+set(python310_version "3.10.11") # Source-only releases after this.
+set(python311_version "3.11.9") # Source-only releases after this.
+set(python312_version "3.12.7")
+set(python313_version "3.13.0")
+set(python314_version "3.14.0")
+
+# We have different patch versions between windows and mac
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows")
+  set(python314t_version "3.14.2t")
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos")
+  set(python314t_version "3.14.0t")
+else ()
+  message(FATAL_ERROR
+    "Unknown platform for Python")
+endif ()
+
+# Hashes for various deployments.
+set(python310_windows_x86_64_hash "b02692c7905dea2829e4204eab2343b226f0c9f244df89502ba8d483d5f8f9d3")
+set(python311_windows_x86_64_hash "d2e7567c29d4c02b708f42a2ed0be51859df42566faee4df844b5ee00094b8a1")
+set(python312_windows_x86_64_hash "f4edfaa23ee00a9b1afc8072ea823d485496637cfeb8129057e92d05f1b80a80")
+set(python313_windows_x86_64_hash "b733a8c7d8d30aa5d0742c00de419294ec385797586672076c337885a440d701")
+set(python314_windows_x86_64_hash "1cf39a0c36aa6047f0982deaf400dbb327e676be60aedeef274088520a5a2887")
+set(python314t_windows_x86_64_hash "28a94269cf82bd3a4dab3229e7ebe9357e777c012b4a65608c7737b74b3b10b1")
+
+set(python310_macos_arm64_hash "5e5a2124abfdc3bb85751e6a544ab81d0624473afe7bab41a7cb78c72e3ccc8d")
+set(python310_macos_x86_64_hash "edb762a34ea20c6876f9f583158e7f65c1a428aecea1971717fc05e26415f55b")
+set(python311_macos_arm64_hash "ea62d4a6b7b3c397280994d6204227095188c8641671286bf5a220ec95ccc637")
+set(python311_macos_x86_64_hash "c296ba92c7fa47bb2953d466846ca46ff9d6f822a7b342afbd2e17ba4c0f16d8")
+set(python312_macos_arm64_hash "866db20c9153509bc2edff7799d96ed2a467e48efd1a18c43cf9c84f8b922522")
+set(python312_macos_x86_64_hash "96d3149615bf76e1ea0ba2d6fa5590ad3d4f7dbe039647a98a9ccfdba6642742")
+set(python313_macos_arm64_hash "0833901d0b91c5c59bfeff3e155e3bc0bfe41d4ba730e94e5f6db4f6eb72cbe4")
+set(python313_macos_x86_64_hash "367962d9e5e7cb7346e78840d806c117cae12830fa57e739f88d5bb7b052924e")
+set(python314_macos_arm64_hash "51e27f2ee656cd578cd95caa8178e7f0e058514e1bb01357f597cf6a2805cd83")
+set(python314_macos_x86_64_hash "5da3ede989b800d5997df0a160b7dc5dc15bf8e98233f9dafb8763fd83e7e17f")
+set(python314t_macos_arm64_hash "6c2aa86a4fa24d0a4c51c1fbd8f54936b0b4ee97ffc3902f743448ed927e87d0")
+set(python314t_macos_x86_64_hash "a04567e6320e45edb4f7234fb5f1cef951abc281ab20042462b91b537004065a")
+
+# Extracting information from the build configuration.
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows")
+  set(python_platform "windows")
+  set(python_ext "zip")
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos")
+  set(python_platform "macos")
+  set(python_ext "tar.xz")
+else ()
+  message(FATAL_ERROR
+    "Unknown platform for Python")
+endif ()
+
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "310_")
+  set(python_version 310)
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "311_")
+  set(python_version 311)
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "312_")
+  set(python_version 312)
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "313_")
+  set(python_version 313)
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "314_")
+  set(python_version 314)
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "314t_")
+  set(python_version 314t)
+else ()
+  message(FATAL_ERROR
+    "Unknown version for Python")
+endif ()
+
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "x86_64")
+  set(python_arch "x86_64")
+elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "arm64")
+  set(python_arch "arm64")
+else ()
+  message(FATAL_ERROR
+    "Unknown architecture for Python")
+endif ()
+
+# Figure out what file we're supposed to download.
+set(python_subdir "python-${python${python_version}_version}-${python_platform}-${python_arch}")
+set(filename "${python_subdir}.${python_ext}")
+set(sha256sum "${python${python_version}_${python_platform}_${python_arch}_hash}")
+
+# Verify that we have a hash to validate.
+if (NOT sha256sum)
+  message(FATAL_ERROR
+    "Unsupported configuration ${python_platform}/${python_arch} ${python${python_version}_version}")
+endif ()
+
+# Download the file.
+file(DOWNLOAD
+  "${python_url_root}/${filename}"
+  ".gitlab/${filename}"
+  STATUS download_status
+  EXPECTED_HASH "SHA256=${sha256sum}")
+
+# Check the download status.
+list(GET download_status 0 res)
+if (res)
+  list(GET download_status 1 err)
+  message(FATAL_ERROR
+    "Failed to download ${filename}: ${err}")
+endif ()
+
+# Extract the file.
+execute_process(
+  COMMAND
+    "${CMAKE_COMMAND}"
+    -E tar
+    xf "${filename}"
+  WORKING_DIRECTORY ".gitlab"
+  RESULT_VARIABLE res
+  ERROR_VARIABLE err
+  ERROR_STRIP_TRAILING_WHITESPACE)
+if (res)
+  message(FATAL_ERROR
+    "Failed to extract ${filename}: ${err}")
+endif ()
+
+# Move to a predictable prefix.
+file(RENAME
+  ".gitlab/${python_subdir}"
+  ".gitlab/python")
